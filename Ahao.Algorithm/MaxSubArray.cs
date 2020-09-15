154320efd6f29670554bda7a953c3ee6d37e02c6
ahao214 <haochen214@163.com>
2020-09-15 17:15:51 +0800

    53. 最大子序和

---
+++ Ahao.Algorithm/MaxSubArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Algorithm
+{
+    /// <summary>
+    /// 53. 最大子序和
+    /// </summary>
+    public class MaxSubArray
+    {
+        public int Methd(int[] nums)
+        {
+            int result = nums[0];
+            int sum = 0;
+            foreach (int x in nums)
+            {
+                sum = x + Math.Max(sum, 0);
+                result = Math.Max(result, sum);
+            }
+            return result;
+        }
+    }
+}
