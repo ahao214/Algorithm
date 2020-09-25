b173a3d0fd13802883a41a04ed098f83ab3cec21
ahao214 <haochen214@163.com>
2020-09-25 11:10:58 +0800

    119.杨辉三角(2)

--- Ahao.Algorithm/Simple/GetRow.cs
+++ Ahao.Algorithm/Simple/GetRow.cs
@@ -13,31 +13,42 @@ namespace Ahao.Algorithm.Simple
     {
         public IList<int> Method(int rowIndex)
         {
-            var result = new List<IList<int>>();
-            for (int i = 0; i < rowIndex + 1; i++)
-            {
-                var list = new List<int>(i + 1);
-                for (int j = 0; j < i + 1; j++)
-                {
-                    int number;
-                    if (i == 0 || j == 0 || j == i)
-                    {
-                        number = 1;
-                    }
-                    else
-                    {
-                        number = result[i - 1][j - 1] + result[i - 1][j];
-                    }
-                    list.Add(number);
-                }
-                if (rowIndex == i)
-                {
-                    return list;
-                }
-                result.Add(list);
-            }
+            //var result = new List<IList<int>>();
+            //for (int i = 0; i < rowIndex + 1; i++)
+            //{
+            //    var list = new List<int>(i + 1);
+            //    for (int j = 0; j < i + 1; j++)
+            //    {
+            //        int number;
+            //        if (i == 0 || j == 0 || j == i)
+            //        {
+            //            number = 1;
+            //        }
+            //        else
+            //        {
+            //            number = result[i - 1][j - 1] + result[i - 1][j];
+            //        }
+            //        list.Add(number);
+            //    }
+            //    if (rowIndex == i)
+            //    {
+            //        return list;
+            //    }
+            //    result.Add(list);
+            //}
 
-            return result.Last();
+            //return result.Last();
+
+            int[] arr = new int[rowIndex + 1];
+            for (int i = 0; i <= rowIndex; i++)
+            {
+                arr[i] = 1;
+                for (int j = i; j > 1; j--)
+                {
+                    arr[j - 1] = arr[j - 2] + arr[j - 1];
+                }
+            }
+            return arr;
         }
     }
 }
