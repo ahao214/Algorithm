04bc1a452be51a7055dee2e4053b06dcd98c857f
ahao214 <haochen214@163.com>
2021-02-06 18:05:36 +0800

    对链表进行重新排序

---
+++ Ahao.Algorithm/LNode/TestFindMiddleNode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Algorithm.LNode
+{
+    /// <summary>
+    /// 对链表进行重新排序
+    /// </summary>
+    public class TestFindMiddleNode
+    {
+        /// <summary>
+        /// 方法功能：找出链表head的中间点，把链表从中间断成两个链表
+        /// 输入参数：head-链表头结点
+        /// 返回值：指向链表的中间结点的指针
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        private static LNode FindMiddleNode(LNode head)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 方法功能：对不带头结点的单链表翻转
+        /// 输入参数：head-指向链表头结点
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        private static LNode Reverse(LNode head)
+        {
+
+        }
+
+        /// <summary>
+        /// 方法功能：对链表进行排序
+        /// 输入参数：head-指向链表头结点
+        /// </summary>
+        /// <param name="head"></param>
+        public static void Reorder(LNode head)
+        {
+
+        }
+
+    }
+}
