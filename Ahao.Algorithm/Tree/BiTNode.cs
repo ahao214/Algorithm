b4834a3fa2dc76b0c0b9ca408cdd75a8ccc71964
ahao214 <haochen214@163.com>
2021-02-13 15:37:26 +0800

    二叉树结构

---
+++ Ahao.Algorithm/Tree/BiTNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Algorithm.Tree
+{
+    /// <summary>
+    /// 二叉树的结构
+    /// </summary>
+    public class BiTNode
+    {
+        int Data { get; set; }
+        BiTNode Lchild { get; set; }
+        BiTNode Rchild { get; set; }        
+    }
+}
