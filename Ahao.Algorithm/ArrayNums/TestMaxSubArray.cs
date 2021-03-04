c217aa3eb5bbf2317c196473a0ed8654ec5855d6
ahao214 <haochen214@163.com>
2021-03-04 09:58:41 +0800

    求数组连续最大和-动态规划方法

--- Ahao.Algorithm/ArrayNums/TestMaxSubArray.cs
+++ Ahao.Algorithm/ArrayNums/TestMaxSubArray.cs
@@ -11,6 +11,7 @@ namespace Ahao.Algorithm.ArrayNums
     /// </summary>
     public class TestMaxSubArray
     {
+
         #region 蛮力法
 
         public static int MaxSubArray(int[] arr)
@@ -40,7 +41,6 @@ namespace Ahao.Algorithm.ArrayNums
 
         #endregion
 
-
         #region 重复利用已经计算的子数组和
 
         public static int MaxSubArr(int[] arr)
@@ -63,6 +63,35 @@ namespace Ahao.Algorithm.ArrayNums
                 }
             }
             return maxSum;
+        }
+
+
+        #endregion
+
+        #region 动态规划方法
+
+        public static int MaxSubArrayDT(int [] arr )
+        {
+            if (arr ==null || arr .Length <=0)
+            {
+                Console.WriteLine("参数不合法");
+                return -1;
+            }
+
+            int n = arr.Length;
+            int[] End = new int[n];
+            int[] All = new int[n];
+            End[n - 1] = arr[n - 1];
+            All[n - 1] = arr[n - 1];
+            End[0] = All[0] = arr[0];
+            for (int i=1;i<n;i++)
+            {
+                End[i] = Math.Max(End[i - 1] + arr[i], arr[i]);
+                All[i] = Math.Max(End[i], All[i - 1]);
+            }
+            int maxSum = All[n - 1];
+            return maxSum;
+
         }
 
 
