88f9533e4f2630d5410e994a22cbeb56caad7899
ahao214 <haochen214@163.com>
2021-04-05 18:16:41 +0800

    88. 合并两个有序数组

--- Ahao.Algorithm/Program.cs
+++ Ahao.Algorithm/Program.cs
@@ -2110,7 +2110,16 @@ namespace Ahao.Algorithm
 
             #endregion
 
+            #region 88. 合并两个有序数组
 
+            Merge merge = new Merge();
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = { 2, 5, 6 };
+            int n = 3;
+            merge.Method(nums1, m, nums2, n);
+
+            #endregion
 
 
 
