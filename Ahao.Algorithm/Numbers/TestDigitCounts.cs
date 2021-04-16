38ee395870d381960dd8d57c68d3ffa1c12aba04
ahao214 <haochen214@163.com>
2021-04-16 20:58:14 +0800

    查找连续数字中某个数字出现的次数

--- Ahao.Algorithm/Numbers/TestDigitCounts.cs
+++ Ahao.Algorithm/Numbers/TestDigitCounts.cs
@@ -11,5 +11,31 @@ namespace Ahao.Algorithm.Numbers
     /// </summary>
     public class TestDigitCounts
     {
+        public static int DigitCounts(int k, int n)
+        {
+            int result = 0;
+            int baseLine = 1;
+            while (n / baseLine > 0)
+            {
+                int cur = (n / baseLine) % 10;
+                int low = n - (n / baseLine) * baseLine;
+                int high = n / (baseLine * 10);
+
+                if (cur == k)
+                {
+                    result += high * baseLine + low + 1;
+                }
+                else if (cur < k)
+                {
+                    result += high * baseLine;
+                }
+                else
+                {
+                    result += (high + 1) * baseLine;
+                }
+                baseLine *= 10;
+            }
+            return result;
+        }
     }
 }
