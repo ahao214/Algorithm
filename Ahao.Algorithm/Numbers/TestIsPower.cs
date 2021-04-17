dff1c39e61deb36e7996e3d95a8432bfc0da7586
ahao214 <haochen214@163.com>
2021-04-17 10:14:24 +0800

    判断一个自然数是否是某个数的二次方

---
+++ Ahao.Algorithm/Numbers/TestIsPower.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Algorithm.Numbers
+{
+    /// <summary>
+    /// 判断一个自然数是否是某个数的二次方
+    /// </summary>
+    public class TestIsPower
+    {
+        public static bool IsPower(int n)
+        {
+            int i;
+            int m;
+            if (n <= 0)
+            {
+                Console.WriteLine(n + "不是自然数");
+                return false;
+            }
+            for (i = 1; i < n; i++)
+            {
+                m = i * i;
+                if (m == n)
+                {
+                    return true;
+                }
+                else if (m > n)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
