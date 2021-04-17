f0bedad195f10d294cc472a06fd38dbaf53b3acc
ahao214 <haochen214@163.com>
2021-04-17 10:27:41 +0800

    判断一个数是否为2的n次方-"与"操作法

--- Ahao.Algorithm/Numbers/TestIsPowerN.cs
+++ Ahao.Algorithm/Numbers/TestIsPowerN.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,11 @@ namespace Ahao.Algorithm.Numbers
     /// </summary>
     public class TestIsPowerN
     {
+        /// <summary>
+        /// 构造法
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
         public static bool IsPowerN(int n)
         {
             if (n < 1)
@@ -24,5 +30,19 @@ namespace Ahao.Algorithm.Numbers
             }
             return false;
         }
+
+        /// <summary>
+        /// "与"操作法
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static bool IsPowerNT(int n)
+        {
+            if (n < 1)
+                return false;
+            int m = n & (n - 1);
+            return m == 0;
+        }
+
     }
 }
