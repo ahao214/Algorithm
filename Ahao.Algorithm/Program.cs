bb7f11e7ceb91749e5c0a44d85b813e31518cbc1
ahao214 <haochen214@163.com>
2021-05-01 11:47:25 +0800

    690. 员工的重要性

--- Ahao.Algorithm/Program.cs
+++ Ahao.Algorithm/Program.cs
@@ -2686,6 +2686,50 @@ namespace Ahao.Algorithm
 
             #endregion
 
+            #region 690. 员工的重要性
+
+            Employee employee = new Employee();
+            IList<Employee> employees = new List<Employee>();
+            IList<int> lst = new List<int>();
+            employee.id = 1;
+            employee.importance = 5;
+            lst.Add(2);
+            lst.Add(3);
+            employee.subordinates = lst;
+            employees.Add(employee);
+
+            employee = new Employee();
+            employee.id = 2;
+            employee.importance = 3;
+            lst = new List<int>();
+            lst.Add(4);
+            employee.subordinates = lst;
+            employees.Add(employee);
+
+            employee = new Employee();
+            employee.id = 3;
+            employee.importance = 4;
+            lst = new List<int>();
+            employee.subordinates = lst;
+            employees.Add(employee);
+
+            employee = new Employee();
+            employee.id = 4;
+            employee.importance = 1;
+            lst = new List<int>();
+            employee.subordinates = lst;
+            employees.Add(employee);
+
+            GetImportance getImportance = new GetImportance();
+            WriteLine(getImportance.Method(employees, 1));
+
+            #endregion
+
+
+
+
+
+
 
 
             ReadLine();
---
+++ Ahao.Algorithm/Simple/GetImportance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Algorithm.Simple
+{
+    /// <summary>
+    /// 690. 员工的重要性
+    /// </summary>
+    public class GetImportance
+    {
+        Dictionary<int, Employee> dic = new Dictionary<int, Employee>();
+        public int Method(IList<Employee> employees, int id)
+        {
+            foreach (Employee employee in employees)
+            {
+                dic.Add(employee.id, employee);
+            }
+            return DFS(id);
+        }
+
+        public int DFS(int id)
+        {
+            Employee employee = dic[id];
+            int total = employee.importance;
+            IList<int> subordinates = employee.subordinates;
+            foreach (int subId in subordinates)
+            {
+                total += DFS(subId);
+            }
+            return total;
+        }
+
+    }
+
+
+    public class Employee
+    {
+        public int id;
+        public int importance;
+        public IList<int> subordinates;
+    }
+}
