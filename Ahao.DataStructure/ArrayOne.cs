49e76f48101b8dd1873d6dd7ca559e3a36adf06d
ahao214 <haochen214@163.com>
2021-05-07 15:39:01 +0800

    动态数组

---
+++ Ahao.DataStructure/ArrayOne.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.DataStructure
+{
+    /// <summary>
+    /// 动态数组
+    /// </summary>
+    class ArrayOne
+    {
+        private int[] data;
+        private int N;
+
+        public ArrayOne(int capacity)
+        {
+            data = new int[capacity];
+            N = 0;
+        }
+
+        public ArrayOne() : this(10) { }
+
+        public int Capacity
+        {
+            get { return data.Length; }
+        }
+
+        public int Count
+        {
+            get { return N; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return N == 0; }
+        }
+
+    }
+}
---
+++ Ahao.DataStructure/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+
+namespace Ahao.DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = i;
+                Write(arr[i] + " ");
+                
+            }
+            WriteLine();
+
+            ArrayList array = new ArrayList(10);
+            for (int i = 0; i < 15; i++)
+            {
+                array.Add(i);
+                Write(array[i] + " ");                
+            }
+            WriteLine();
+
+            List<int> lst = new List<int>(10);
+            for (int i = 0; i < 15; i++)
+            {
+                lst.Add(i);
+                Write(lst[i] + " ");
+            }
+
+
+            ReadLine();
+        }
+    }
+}
