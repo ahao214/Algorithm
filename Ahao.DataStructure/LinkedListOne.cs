2944f52331f11972889f3d5b231a40f8d5aced2b
ahao214 <haochen214@163.com>
2021-05-07 23:06:13 +0800

    自定义链表

---
+++ Ahao.DataStructure/LinkedListOne.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.DataStructure
+{
+    class LinkedListOne<E>
+    {
+        private class Node
+        {
+            public E e;
+            public Node next;
+
+            public Node(E e, Node next)
+            {
+                this.e = e;
+                this.next = next;
+            }
+
+            public Node(E e)
+            {
+                this.e = e;
+                this.next = null;
+            }
+
+            public override string ToString()
+            {
+                return e.ToString();
+            }
+        }
+        //链表头部
+        private Node head;
+        //链表个数
+        private int N;
+        public LinkedListOne()
+        {
+            head = null;
+            N = 0;
+        }
+
+        public int Count
+        {
+            get { return N; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return N == 0; }
+        }
+
+
+
+    }
+}
