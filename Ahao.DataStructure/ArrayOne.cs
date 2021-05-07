b3ad4f902cafc7d2edab895000d4ba928b642fb4
ahao214 <haochen214@163.com>
2021-05-07 15:50:08 +0800

    向数组中添加元素

--- Ahao.DataStructure/ArrayOne.cs
+++ Ahao.DataStructure/ArrayOne.cs
@@ -23,19 +23,50 @@ namespace Ahao.DataStructure
 
         public ArrayOne() : this(10) { }
 
+        /// <summary>
+        /// 数组的容量
+        /// </summary>
         public int Capacity
         {
             get { return data.Length; }
         }
 
+        /// <summary>
+        /// 数组中元素的个数
+        /// </summary>
         public int Count
         {
             get { return N; }
         }
 
+        /// <summary>
+        /// 判断数组是否为空
+        /// </summary>
         public bool IsEmpty
         {
             get { return N == 0; }
+        }
+
+        /// <summary>
+        /// 向数组中添加元素
+        /// </summary>
+        /// <param name="index">添加的位置</param>
+        /// <param name="ele">添加的元素</param>
+        public void Add(int index, int ele)
+        {
+            if (index < 0 || index > N)
+                throw new ArgumentException("数组索引越界");
+            if (N == data.Length)
+                throw new ArgumentException("数组已满");
+            for (int i = N - 1; i >= index; i--)
+            {
+                //把前面一个元素赋值给后面
+                data[i + 1] = data[i];
+            }
+            //把元素ele赋值到index位置
+            data[index] = ele;
+            //数组个数加1
+            N++;
         }
 
     }
