cdaef9985468f80c25a98d58915a6b9c9d218cac
ahao214 <haochen214@163.com>
2021-05-09 10:56:49 +0800

    循环数组

---
+++ Ahao.DataStructure/Arrays/ArrayTwo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.DataStructure
+{
+    /// <summary>
+    /// 循环数组
+    /// </summary>
+    /// <typeparam name="E"></typeparam>
+    class ArrayTwo<E>
+    {
+        private E[] data;
+        private int first;
+        private int last;
+        private int N;
+
+        public ArrayTwo(int capacity)
+        {
+            data = new E[capacity];
+            first = 0;
+            last = 0;
+            N = 0;
+        }
+
+        public ArrayTwo() : this(10) { }
+
+        public int Count { get { return N; } }
+        public bool IsEmpty { get { return N == 0; } }
+
+        //添加
+        public void Add(E e)
+        {
+            if (N == data.Length)
+                ResetCapacity(2 * data.Length);
+
+            data[last] = e;
+            last = (last + 1) % data.Length;
+            N++;
+        }
+
+
+        //删除
+        public E RemoveFirst()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("数组为空");
+
+            E res = data[first];
+            data[first] = default(E);
+            first = (first + 1) % data.Length;
+            N--;
+
+            if (N == data.Length / 4)
+                ResetCapacity(data.Length / 2);
+
+            return res;
+        }
+
+
+        public E GetFirst()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("数组为空");
+            return data[first];
+        }
+
+        //扩充容量大小
+        public void ResetCapacity(int newCapacity)
+        {
+            E[] newData = new E[newCapacity];
+            for (int i = 0; i < N; i++)
+                newData[i] = data[(first + 1) % data.Length];
+            data = newData;
+            first = 0;
+            last = N;
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+
+            for (int i = 0; i < N; i++)
+            {
+                sb.Append(data[(first + i) % data.Length]);
+                if ((first + i + 1) % data.Length != last)
+                    sb.Append(",");
+            }
+
+            sb.Append("]");
+            return sb.ToString();
+        }
+
+
+    }
+}
---
+++ Ahao.DataStructure/StackFiles/ArrayTwoQueue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.DataStructure.StackFiles
+{
+    class ArrayTwoQueue<E> : IQueue<E>
+    {
+        private ArrayTwo<E> arr;
+        public ArrayTwoQueue(int capacity)
+        {
+            arr = new ArrayTwo<E>(capacity);
+        }
+
+        public ArrayTwoQueue()
+        {
+            arr = new ArrayTwo<E>();
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                return arr.Count;
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return arr.IsEmpty;
+            }
+        }
+        //出队
+        public E Dequeue()
+        {
+            return arr.RemoveFirst();
+        }
+        //入队
+        public void Enqueue(E e)
+        {
+            arr.Add(e);
+        }
+
+        public E Peek()
+        {
+            return arr.GetFirst();
+        }
+
+        public override string ToString()
+        {
+            return "Queue:front " + arr.ToString() + " tail";
+        }
+    }
+}
