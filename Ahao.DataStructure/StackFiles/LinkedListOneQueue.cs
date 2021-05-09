f1dcfcf86a4e9a0ff8f841b9a9361d359b3ab8ed
ahao214 <haochen214@163.com>
2021-05-09 13:02:01 +0800

    链表队列

---
+++ Ahao.DataStructure/StackFiles/LinkedListOneQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.DataStructure.StackFiles
+{
+    /// <summary>
+    /// 链表队列
+    /// </summary>
+    class LinkedListOneQueue<E> : IQueue<E>
+    {
+        private LinkedListOne<E> list;
+        public LinkedListOneQueue()
+        {
+            list = new LinkedListOne<E>();
+        }
+
+        public int Count
+        {
+            get { return list.Count; }
+        }
+
+
+        public bool IsEmpty
+        {
+            get { return list.IsEmpty; }
+        }
+
+        public E Dequeue()
+        {
+            return list.RemoveFirst();
+        }
+
+        //时间复杂度O(n)      
+        public void Enqueue(E e)
+        {
+            list.AddLast(e);
+        }
+
+        public E Peek()
+        {
+            return list.GetFirst();
+        }
+
+        public override string ToString()
+        {
+            return "Queue front:" + list.ToString() + "tail";
+        }
+    }
+}
