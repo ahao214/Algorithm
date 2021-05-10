7c772b12f3ce57819872faea5bcc0e3fb94013cf
ahao214 <haochen214@163.com>
2021-05-10 15:44:42 +0800

    基于有序数组实现集合

---
+++ Ahao.DataStructure/SetFiles/SortedArrayOneSet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.DataStructure
+{
+    class SortedArrayOneSet<Key> : ISet<Key> where Key : IComparable<Key>
+    {
+        private SortedArrayOne<Key> s;
+        public SortedArrayOneSet(int capacity)
+        {
+            s = new SortedArrayOne<Key>(capacity);
+        }
+
+        public SortedArrayOneSet()
+        {
+            s = new SortedArrayOne<Key>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return s.Count;
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return s.IsEmpty;
+            }
+        }
+
+        public void Add(Key e)
+        {
+            s.Add(e);
+        }
+
+        public bool Contains(Key e)
+        {
+            return s.Contains(e);
+        }
+
+        public void Remove(Key e)
+        {
+            s.Remove(e);
+        }
+    }
+}
