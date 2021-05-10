e1cfd94559000d9a21a809e181e60dc1e5bff286
ahao214 <haochen214@163.com>
2021-05-10 17:07:44 +0800

    二叉查找树

---
+++ Ahao.DataStructure/BSTree/BSTOne.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ahao.DataStructure
+{
+    class BSTOne<E> where E : IComparable<E>
+    {
+        private class Node
+        {
+            public E e;
+            public Node left;
+            public Node righ;
+
+            public Node(E e)
+            {
+                this.e = e;
+                this.left = null;
+                this.righ = null;
+            }
+        }
+
+        //根结点
+        private Node root;
+        //数量
+        private int N;
+        public BSTOne()
+        {
+            root = null;
+            N = 0;
+        }
+
+        //属性：数量
+        public int Count
+        {
+            get
+            {
+                return N;
+            }
+        }
+
+        //属性：是否为空
+        public bool IsEmpty
+        {
+            get
+            {
+                return N == 0;
+            }
+        }
+
+    }
+}
