e6008a3f662862a056c478d4fda22594d0043cbb
ahao214 <haochen214@163.com>
2021-05-10 18:38:42 +0800

    递归添加二叉查找树

--- Ahao.DataStructure/BSTree/BSTOne.cs
+++ Ahao.DataStructure/BSTree/BSTOne.cs
@@ -8,13 +8,13 @@ namespace Ahao.DataStructure
         {
             public E e;
             public Node left;
-            public Node righ;
+            public Node right;
 
             public Node(E e)
             {
                 this.e = e;
                 this.left = null;
-                this.righ = null;
+                this.right = null;
             }
         }
 
@@ -67,16 +67,39 @@ namespace Ahao.DataStructure
                 if (e.CompareTo(cur.e) < 0)
                     cur = cur.left;
                 else
-                    cur = cur.righ;
+                    cur = cur.right;
             }
 
             cur = new Node(e);
             if (e.CompareTo(cur.e) < 0)
                 pre.left = cur;
             else
-                pre.righ = cur;
+                pre.right = cur;
             N++;
 
+        }
+
+        //递归添加
+        public void Add(E e)
+        {
+            root = Add(root, e);
+        }
+
+
+        //以node为根的树中添加元素e，添加后返回根节点root
+        private Node Add(Node node, E e)
+        {
+            if (node == null)
+            {
+                N++;
+                return new Node(e);
+            }
+            if (e.CompareTo(node.e) < 0)
+                node.left = Add(node.left, e);
+            else if (e.CompareTo(node.e) > 0)
+                node.right = Add(node.right, e);
+
+            return node;
         }
 
     }
