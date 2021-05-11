6ad3ded24b43de5722345d5dd5a68d2cd043fb4e
ahao214 <haochen214@163.com>
2021-05-11 21:15:52 +0800

    基于二叉树实现集合

---
+++ Ahao.DataStructure/SetFiles/BSTOneSet.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.DataStructure
+{
+    /// <summary>
+    /// 基于二叉树实现集合
+    /// </summary>
+    /// <typeparam name="E"></typeparam>
+    class BSTOneSet<E> : ISet<E> where E : IComparable<E>
+    {
+        private BSTOne<E> bst;
+        public BSTOneSet()
+        {
+            bst = new BSTOne<E>();
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                return bst.Count;
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return bst.IsEmpty;
+            }
+        }
+
+
+        public void Add(E e)
+        {
+            bst.add(e);
+        }
+
+        public bool Contains(E e)
+        {
+            return bst.Contains(e);
+        }
+
+        public void Remove(E e)
+        {
+            bst.Remove(e);
+        }
+
+        public int MaxHeight()
+        {
+            return bst.MaxHeight();
+        }
+
+    }
+}
