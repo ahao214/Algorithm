20d649b810868d53f3cc30d37a0f58760b98dd3b
ahao214 <haochen214@163.com>
2021-05-12 15:46:17 +0800

    哈希冲突处理

---
+++ Ahao.DataStructure/HashFiles/HashSTOne.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.DataStructure
+{
+    class HashSTOne<Key>
+    {
+        private LinkedListOne<Key>[] hashtable;
+        private int N;
+        private int M;
+        public HashSTOne(int M)
+        {
+            this.M = M;
+            N = 0;
+            hashtable = new LinkedListOne<Key>[M];
+            for (int i = 0; i < M; i++)
+            {
+                hashtable[i] = new LinkedListOne<Key>();
+            }
+        }
+
+        public HashSTOne() : this(97) { }
+
+        public int Count { get { return N; } }
+        public bool IsEmpty { get { return N == 0; } }
+        private int Hash(Key key)
+        {
+            return (key.GetHashCode() & 0x7fffffff) % M;
+        }
+
+
+    }
+}
