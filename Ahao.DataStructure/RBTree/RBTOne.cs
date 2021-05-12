9dfb9ca92ab110b2fdd458cbd673fc342765c320
ahao214 <haochen214@163.com>
2021-05-12 14:29:16 +0800

    红黑树-添加

--- Ahao.DataStructure/RBTree/RBTOne.cs
+++ Ahao.DataStructure/RBTree/RBTOne.cs
@@ -95,5 +95,45 @@ namespace Ahao.DataStructure
             node.left.color = Black;
         }
 
+        #region 添加
+
+        //往红黑树中添加元素，递归实现
+        public void Add(E e)
+        {
+            root = Add(root, e);
+            root.color = Black;
+        }
+
+        private Node Add(Node node, E e)
+        {
+            if (node == null)
+            {
+                N++;
+                return new Node(e);     //默认为红结点
+            }
+
+            if (e.CompareTo(node.e) < 0)
+                node.left = Add(node.left, e);
+            else if (e.CompareTo(node.e) > 0)
+                node.right = Add(node.right, e);
+
+            //如果出现右子节点是红色，而左子结点是黑色，进行左旋转
+            if (IsRed(node.right) && !IsRed(node.left))
+                node = LeftRotate(node);
+
+            //如果出现连续的左子结点都为红色，进行右旋转
+            if (IsRed(node.left) && IsRed(node.left.left))
+                node = RightRotate(node);
+
+            //如果出现左右子结点均为红色，进行颜色翻转
+            if (IsRed(node.left) && IsRed(node.right))
+                FlipColor(node);
+
+            return node;
+
+        }
+
+        #endregion
+
     }
 }
