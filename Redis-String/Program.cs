4e4a17580a47bdf771301b11e2e455fa98330cbf
ahao214 <haochen214@163.com>
2021-07-12 18:01:09 +0800

    redis中string的批量操作

--- Redis-String/Program.cs
+++ Redis-String/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using ServiceStack.Redis;
 using static System.Console;
@@ -8,7 +9,7 @@ namespace Redis_String
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
             using (IRedisClient client = new RedisClient("127.0.0.1", 6379))
             {
                 //清空当前数据库
@@ -17,27 +18,52 @@ namespace Redis_String
 
                 #region 单个操作
 
-                client.Set<string>("name", "ahao");
+                //client.Set<string>("name", "ahao");
 
-                //读取数据
-                var valueOne = client.GetValue("name");
-                WriteLine("第一个出读取数据:", valueOne);
+                ////读取数据
+                //var valueOne = client.GetValue("name");
+                //WriteLine("第一个出读取数据:", valueOne);
 
-                var valueTwo = client.Get<string>("name");
-                WriteLine("第二次读取数据：",valueTwo);
+                //var valueTwo = client.Get<string>("name");
+                //WriteLine("第二次读取数据：",valueTwo);
 
-                var valueThree = client.GetValue("name");
-                WriteLine(JsonConvert.DeserializeObject<string>(valueThree));
-
-
+                //var valueThree = client.GetValue("name");
+                //WriteLine(JsonConvert.DeserializeObject<string>(valueThree));
 
                 #endregion
 
                 #region 批量操作
 
+                var dicts = new Dictionary<string, string>();
+                dicts.Add("id", "101");
+                dicts.Add("name", "ahao");
+                dicts.Add("addresss", "shanghai");
+                //批量写入
+                client.SetAll(dicts);
+                //读取
+                var lst = client.GetAll<string>(new string[] { "id", "name", "address" });
+                foreach (var item in lst)
+                {
+                    WriteLine(item);
+                    WriteLine("key:" + item.Key);
+                    WriteLine("value:" + item.Value);
+                }
 
                 #endregion
+
+
+
+
+
+
+
+
+
+
             };
+
+
+
             Console.ReadLine();
 
         }
