63442ab20505ec705626d041a6d2db770cd83e72
ahao214 <haochen214@163.com>
2021-07-13 14:43:53 +0800

    Redis中set和zset

---
+++ Redis_SetAndZset/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Redis;
+using ServiceStack.Text;
+using static System.Console;
+
+namespace Redis_SetAndZset
+{
+    class Program
+    {
+        /// <summary>
+        /// Redis中set和zset
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            using (IRedisClient client = new RedisClient("127.0.0.1", 6379))
+            {
+                //清空当前数据库
+                client.FlushDb();
+
+            };
+        }
+    }
+}
