7678dfb36efa924f5513ce8412ff083e9e111beb
ahao214 <haochen214@163.com>
2021-07-13 21:23:53 +0800

    商品秒杀-阻塞的锁

--- Redis-LockServer/BlockingLock.cs
+++ Redis-LockServer/BlockingLock.cs
@@ -13,6 +13,31 @@ namespace Redis_LockServer
 {
     public class BlockingLock
     {
-        
+        //这个是阻塞的锁
+        public static void Show(int i, string key, TimeSpan timeout)
+        {
+            using (var client = new RedisClient("127.0.0.1", 6379))
+            {
+                //获取锁
+                using (var datalock = client.AcquireLock("DataLock:" + key, timeout))
+                {
+                    //库存数量
+                    var inventory = client.Get<int>("inventoryNum");
+                    if (inventory > 0)
+                    {
+                        client.Set<int>("inventoryNum", inventory - 1);
+                        //订单数量
+                        var orderNum = client.Incr("order Num");
+                        WriteLine($"{i}抢购成功xx线程ID：{Thread.CurrentThread.ManagedThreadId.ToString("00")},库存数量:{inventory},订单数量：{orderNum}");
+                    }
+                    else
+                    {
+                        WriteLine($"{i}抢购失败");
+                    }
+                    Thread.Sleep(100);
+                }
+
+            };
+        }
     }
 }
