c74c9cc299057bb03e884b5cd3875a3e86ca141e
ahao214 <haochen214@163.com>
2021-07-15 14:35:24 +0800

    JSON操作MongoDB

--- MongoDBServer/Program.cs
+++ MongoDBServer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace MongoDBServer
 {
@@ -10,6 +11,22 @@ namespace MongoDBServer
     {
         static void Main(string[] args)
         {
+            try
+            {
+                {
+                    WriteLine("---json操作---");
+                    JsonOperation.Show();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+
+
+            ReadLine();
         }
     }
 }
