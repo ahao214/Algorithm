4b2d337da14ef5b9b7ae429eb9f7173f0c7f5145
ahao214 <haochen214@163.com>
2021-10-15 19:06:13 +0800

    1.两数之和

---
+++ Ahao.Algorithm/Simple/LC1/TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Algorithm.Simple.LC1
+{
+    /// <summary>
+    /// 1.两数之和
+    /// </summary>
+    public class TwoSum
+    {
+        public int[] Method(int[] nums, int target)
+        {
+            int[] result = Array.Empty<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var nj = target - nums[i];
+                //Array.IndexOf 方法从数组中查找指定对象的下标
+                var j = Array.IndexOf(nums, nj, i + 1);
+                if (j >= 0)
+                    return new[] { i, j };
+            }
+            return result;
+        }
+    }
+}
