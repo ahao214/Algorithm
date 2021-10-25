f1cfb98e5ff8abc3f435fe72cf7c4a6fdc545486
ahao214 <haochen214@163.com>
2021-10-25 12:29:16 +0800

    240. 搜索二维矩阵 II

--- Ahao.Algorithm/Secondary/LC240/SearchMatrix.cs
+++ Ahao.Algorithm/Secondary/LC240/SearchMatrix.cs
@@ -25,5 +25,42 @@ namespace Ahao.Algorithm.Secondary.LC240
             }
             return false;           
         }
+
+        //二分查找
+        public bool SearchMatrixs(int[][] matrix, int target)
+        {
+            foreach (int[] row in matrix)
+            {
+                int index = Search(row, target);
+                if (index >= 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int Search(int[] nums, int target)
+        {
+            int low = 0, high = nums.Length - 1;
+            while (low <= high)
+            {
+                int mid = (high - low) / 2 + low;
+                int num = nums[mid];
+                if (num == target)
+                {
+                    return mid;
+                }
+                else if (num > target)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return -1;
+        }     
     }
 }
