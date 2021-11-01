c75f4854608a8c2e8396367434c2347ca55b3246
ahao214 <haochen214@163.com>
2021-11-02 01:19:43 +0800

    JZ28 对称的二叉树

---
+++ Ahao.Nowcoder/Easy/JZ28/IsSymmetrical.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Nowcoder.Easy.JZ28
+{
+    /// <summary>
+    /// JZ28 对称的二叉树
+    /// </summary>
+    public class IsSymmetrical
+    {
+        public bool Method(TreeNode pRoot)
+        {
+            if (pRoot == null)
+            {
+                return true;
+            }
+            return IsEqual(pRoot.left, pRoot.right);
+        }
+        public bool IsEqual(TreeNode lRoot, TreeNode rRoot)
+        {
+            if (lRoot == null && rRoot == null)
+            {
+                return true;
+            }
+            if (lRoot == null || rRoot == null || lRoot.val != rRoot.val)
+            {
+                return false;
+            }
+            bool leftEqual = IsEqual(lRoot.left, rRoot.right);
+            bool rightEqual = IsEqual(lRoot.right, rRoot.left);
+            return leftEqual && rightEqual;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+}
