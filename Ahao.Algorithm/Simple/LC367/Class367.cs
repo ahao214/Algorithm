fa8ca6226be686e5e092e9a89249fffff9781753
ahao214 <haochen214@163.com>
2021-11-04 00:21:43 +0800

    367. 有效的完全平方数

---
+++ Ahao.Algorithm/Simple/LC367/Class367.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Algorithm.Simple.LC367
+{
+    /// <summary>
+    /// 367. 有效的完全平方数
+    /// </summary>
+    public class Class367
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            long square = 0;
+            while (true)
+            {
+                long value = square * square;
+                if (value > num)
+                {
+                    return false;
+                }
+                if (value == num)
+                {
+                    return true;
+                }
+                square++;
+            }
+            return false;
+        }
+
+        public bool IsPerfectSquare2(int num)
+        {
+            int low = 0;
+            int high = num;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                long square = (long)mid * mid;
+                if (square == num)
+                {
+                    return true;
+                }
+                else if (square < num)
+                {
+                    low = low + 1;
+                }
+                else
+                {
+                    high = high - 1;
+                }
+            }
+            return false;
+        }
+    }
+}
