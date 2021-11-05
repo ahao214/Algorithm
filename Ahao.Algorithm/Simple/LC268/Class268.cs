82e3e679358a743764aaab6e3b6e9e7e647ddc36
ahao214 <haochen214@163.com>
2021-11-06 01:01:28 +0800

    268. 丢失的数字-位运算

--- Ahao.Algorithm/Simple/LC268/Class268.cs
+++ Ahao.Algorithm/Simple/LC268/Class268.cs
@@ -54,5 +54,25 @@ namespace Ahao.Algorithm.Simple.LC268
         }
 
 
+        /// <summary>
+        /// 位运算
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int MissingNumber3(int[] nums)
+        {
+            int xor = 0;
+            int n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                xor ^= nums[i];
+            }
+            for (int i = 0; i <= n; i++)
+            {
+                xor ^= i;
+            }
+            return xor;
+        }
+
     }
 }
