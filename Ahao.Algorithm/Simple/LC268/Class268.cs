423c6b804255285556e7058ea93c718d4a5f57f5
ahao214 <haochen214@163.com>
2021-11-06 00:57:40 +0800

    268. 丢失的数字-排序

---
+++ Ahao.Algorithm/Simple/LC268/Class268.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Algorithm.Simple.LC268
+{
+    /// <summary>
+    /// 268. 丢失的数字
+    /// </summary>
+    public class Class268
+    {
+        /// <summary>
+        /// 排序方法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int MissingNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            int n = nums.Length;
+            for(int i=0;i<n;i++)
+            {
+                if (nums[i] != i)
+                    return i;
+            }
+            return n;
+        }
+    }
+}
