b68ad49709e07b1a909e0a607e8c0b19efba2355
ahao214 <haochen214@163.com>
2021-11-07 00:48:52 +0800

    NC102 在二叉树中找到两个节点的最近公共祖先

---
+++ Ahao.Nowcoder/Medium/NC102/Class102.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Nowcoder.Medium.NC102
+{
+    /// <summary>
+    /// NC102 在二叉树中找到两个节点的最近公共祖先
+    /// </summary>
+    public class Class102
+    {
+        //public int lowestCommonAncestor(TreeNode root, int o1, int o2)
+        //{
+
+        //}
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+
+}
