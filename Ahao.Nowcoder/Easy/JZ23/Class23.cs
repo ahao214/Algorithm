4cda713e8413b0ec251913de47b2526cb563df08
ahao214 <haochen214@163.com>
2021-11-09 20:52:02 +0800

    JZ22 链表中倒数最后k个结点

---
+++ Ahao.Nowcoder/Easy/JZ23/Class23.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Nowcoder.Easy.JZ23
+{
+    /// <summary>
+    /// JZ23
+    /// </summary>
+    public class Class23
+    {
+    }
+}
