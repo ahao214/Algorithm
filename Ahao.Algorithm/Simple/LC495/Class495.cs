f70dd387b7575a2579e7e3dccf519714b228388a
ahao214 <haochen214@163.com>
2021-11-10 11:12:52 +0800

    495. 提莫攻击

--- Ahao.Algorithm/Simple/LC495/Class495.cs
+++ Ahao.Algorithm/Simple/LC495/Class495.cs
@@ -13,6 +13,10 @@ namespace Ahao.Algorithm.Simple
     {
         public int FindPoisonedDuration(int[] timeSeries, int duration)
         {
+            if (timeSeries == null || timeSeries.Length == 0 || duration == 0)
+            {
+                return 0;
+            }
             int res = 0;
             int expired = 0;
             for (int i = 0; i < timeSeries.Length; i++)
