d5b8b2aa9aae7682b7a5af75e1adc481c216408b
ahao214 <haochen214@163.com>
2021-11-13 14:51:46 +0800

    NC111 最大数

---
+++ Ahao.Nowcoder/Medium/NC111/Class111.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Nowcoder.Medium.NC11
+{
+    /// <summary>
+    /// NC111 最大数
+    /// </summary>
+    public class Class111
+    {
+        public string solve(int[] nums)
+        {
+            List<string> list = new List<string>();
+            int sum = 0;
+            foreach (int i in nums)
+            {
+                list.Add(i.ToString());
+                sum += i;
+            }
+            if (sum == 0)
+            {
+                return "0";
+            }
+            list.Sort((x, y) =>
+            {
+                return (x + y).CompareTo(y + x);
+            });
+            StringBuilder sb = new StringBuilder();
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                sb.Append(list[i]);
+            }
+            string res = sb.ToString();
+            return res;
+        }
+    }
+}
