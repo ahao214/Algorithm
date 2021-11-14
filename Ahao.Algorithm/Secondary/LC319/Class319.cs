fcd0a50645d0ece38763572f22ac4eb10ff5a06f
ahao214 <haochen214@163.com>
2021-11-15 00:52:48 +0800

    319. 灯泡开关

--- Ahao.Algorithm/Secondary/LC319/Class319.cs
+++ Ahao.Algorithm/Secondary/LC319/Class319.cs
@@ -15,5 +15,23 @@ namespace Ahao.Algorithm.Secondary
         {
             return (int)Math.Sqrt(n + 0.5);
         }
+
+        public int BulbSwitch2(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            int result = 1;
+            while (true)
+            {
+                if (result * result > n)
+                {
+                    break;
+                }
+                result++;
+            }
+            return result - 1;
+        }
     }
 }
