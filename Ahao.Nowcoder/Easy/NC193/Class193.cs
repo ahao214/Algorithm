b3863ff0d3bc6ba24478ede557fd414c8158a41a
ahao214 <haochen214@163.com>
2021-11-16 17:34:07 +0800

    NC192 二叉树的前序遍历

---
+++ Ahao.Nowcoder/Easy/NC193/Class193.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Nowcoder.Easy.NC193
+{
+    /// <summary>
+    /// NC193 二叉树的前序遍历
+    /// </summary>
+    public class Class193
+    {
+        public List<int> preorderTraversal(TreeNode root)
+        {
+            var lst = new List<int>();
+            preOrder(root, lst);
+            return lst;
+        }
+
+        private void preOrder(TreeNode root, List<int> lst)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            lst.Add(root.val);
+            preOrder(root.left, lst);
+            preOrder(root.right, lst);
+        }
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+}
---
+++ Ahao.Nowcoder/Medium/NC67/Class67.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Nowcoder.Medium.NC67
+{
+    /// <summary>
+    /// NC67 汉诺塔问题
+    /// </summary>
+    public class Class67
+    {
+
+    }
+}
