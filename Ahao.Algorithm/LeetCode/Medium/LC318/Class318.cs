2dbcc38d3c133f8affa8cc22626090748646eb10
ahao214 <haochen214@163.com>
2021-11-17 14:45:32 +0800

    318. 最大单词长度乘积

---
+++ Ahao.Algorithm/LeetCode/Medium/LC318/Class318.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Algorithm.LeetCode.Medium.LC318
+{
+    /// <summary>
+    /// 318. 最大单词长度乘积
+    /// </summary>
+    public class Class318
+    {
+        public int MaxProduct(string[] words)
+        {
+            int length = words.Length;
+            int[] masks = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                string word = words[i];
+                int wordLength = word.Length;
+                for (int j = 0; j < wordLength; j++)
+                {
+                    masks[i] |= 1 << (word[j] - 'a');
+                }
+            }
+            int maxProd = 0;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if ((masks[i] & masks[j]) == 0)
+                    {
+                        maxProd = Math.Max(maxProd, words[i].Length * words[j].Length);
+                    }
+                }
+            }
+            return maxProd;
+        }
+    }
+}
