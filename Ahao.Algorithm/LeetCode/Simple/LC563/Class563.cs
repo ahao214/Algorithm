98f4518b49e622020a1f1375f64a15e9b98d842c
ahao214 <haochen214@163.com>
2021-11-18 00:34:36 +0800

    563. 二叉树的坡度

---
+++ Ahao.Algorithm/LeetCode/Simple/LC563/Class563.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Algorithm.LeetCode.Simple.LC563
+{
+    /// <summary>
+    /// 563. 二叉树的坡度
+    /// </summary>
+    public class Class563
+    {
+        int res = 0;
+        public int FindTilt(TreeNode root)
+        {
+            DFS(root);
+            return res;
+        }
+
+        private int DFS(TreeNode node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            int sumLeft = DFS(node.left);
+            int sumRight = DFS(node.right);
+            res += Math.Abs(sumLeft - sumRight);
+            return sumLeft + sumRight + node.val;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
