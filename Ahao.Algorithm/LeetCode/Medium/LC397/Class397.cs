acf8511ca4d30aa13a0bc20d17b36b0023c31b98
ahao214 <haochen214@163.com>
2021-11-19 17:42:38 +0800

    397. 整数替换-贪心

--- Ahao.Algorithm/LeetCode/Medium/LC397/Class397.cs
+++ Ahao.Algorithm/LeetCode/Medium/LC397/Class397.cs
@@ -56,7 +56,41 @@ namespace Ahao.Algorithm.LeetCode.Medium
             return dic[n];
         }
 
-
-
+        /// <summary>
+        /// 贪心
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int IntegerReplacement3(int n)
+        {
+            int ans = 0;
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                {
+                    ++ans;
+                    n /= 2;
+                }
+                else if (n % 4 == 1)
+                {
+                    ans += 2;
+                    n / = 2;
+                }
+                else
+                {
+                    if (n == 3)
+                    {
+                        ans += 2;
+                        n = 1;
+                    }
+                    else
+                    {
+                        ans += 2;
+                        n = n / 2 + 1;
+                    }
+                }
+            }
+            return ans;
+        }
     }
 }
