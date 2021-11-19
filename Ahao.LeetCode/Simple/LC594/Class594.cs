75fc4ca8321334d9cac38634e18774ff59785dee
ahao214 <haochen214@163.com>
2021-11-20 01:00:02 +0800

    594. 最长和谐子序列

---
+++ Ahao.LeetCode/Simple/LC594/Class594.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.LC594
+{
+    /// <summary>
+    /// 594. 最长和谐子序列
+    /// </summary>
+    public class Class594
+    {
+        public int FindLHS(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
