ffd558dbb69346ba22add353ce5af5d995a54062
ahao214 <haochen214@163.com>
2021-11-20 18:26:19 +0800

    162. 寻找峰值

---
+++ Ahao.LeetCode/Medium/LC162/Class162.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.LC162
+{
+    /// <summary>
+    /// 162. 寻找峰值
+    /// </summary>
+    public class Class162
+    {
+        public int FindPeakElement(int[] nums)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > nums[mid + 1])
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+    }
+}
