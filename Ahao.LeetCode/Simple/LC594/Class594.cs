4122308547b5ff2f597502858ea855763e6723f8
ahao214 <haochen214@163.com>
2021-11-20 18:36:41 +0800

    594. 最长和谐子序列

--- Ahao.LeetCode/Simple/LC594/Class594.cs
+++ Ahao.LeetCode/Simple/LC594/Class594.cs
@@ -11,9 +11,59 @@ namespace Ahao.LeetCode.Simple.LC594
     /// </summary>
     public class Class594
     {
+        /// <summary>
+        /// 枚举方法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
         public int FindLHS(int[] nums)
         {
-            return 0;
+            Array.Sort(nums);
+            int begin = 0;
+            int res = 0;
+            for (int end = 0; end < nums.Length; end++)
+            {
+                while (nums[end] - nums[begin] > 1)
+                {
+                    begin++;
+                }
+                if (nums[end] - nums[begin] == 1)
+                {
+                    res = Math.Max(res, end - begin + 1);
+                }
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// 哈希表方法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int FindLHS2(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int result = 0;
+            foreach (int num in nums)
+            {
+                if (dic.ContainsKey(num))
+                {
+                    dic[num]++;
+                }
+                else
+                {
+                    dic.Add(num, 1);
+                }
+            }
+
+            foreach (int key in dic.Keys)
+            {
+                if (dic.ContainsKey(key + 1))
+                {
+                    result = Math.Max(result, dic[key] + dic[key + 1]);
+                }
+            }
+            return result;
         }
     }
 }
