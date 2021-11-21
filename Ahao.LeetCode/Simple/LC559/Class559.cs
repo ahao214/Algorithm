671277e3cbaf6a715482b412e878d37f3ba447e0
ahao214 <haochen214@163.com>
2021-11-21 08:55:12 +0800

    559. N 叉树的最大深度-深度优先搜索

--- Ahao.LeetCode/Simple/LC559/Class559.cs
+++ Ahao.LeetCode/Simple/LC559/Class559.cs
@@ -11,6 +11,41 @@ namespace Ahao.LeetCode.Simple.LC559
     /// </summary>
     public class Class559
     {
+        /// <summary>
+        /// /深度优先搜索
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int MaxDepth(Node root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int maxChildDepth = 0;
+            IList<Node> children = root.children;
+            foreach (Node child in children)
+            {
+                int childDepth = MaxDepth(child);
+                maxChildDepth = Math.Max(maxChildDepth, childDepth);
+            }
+            return maxChildDepth + 1;
+        }
+    }
 
+    public class Node
+    {
+        public int Val;
+        public IList<Node> children;
+        public Node() { }
+        public Node(int _val)
+        {
+            Val = _val;
+        }
+        public Node(int _val, IList<Node> _children)
+        {
+            Val = _val;
+            children = _children;
+        }
     }
 }
