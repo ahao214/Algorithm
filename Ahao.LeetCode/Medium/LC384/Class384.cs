f9124771229bba9e4b131bb709de9d4eb649b351
ahao214 <haochen214@163.com>
2021-11-22 13:17:52 +0800

    384. 打乱数组-暴力方法

---
+++ Ahao.LeetCode/Medium/LC384/Class384.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.LC384
+{
+    /// <summary>
+    /// 384. 打乱数组
+    /// </summary>
+    public class Class384
+    {
+        int[] nums;
+        int[] original;
+
+        public Class384(int[] nums)
+        {
+            this.nums = nums;
+            this.original = new int[nums.Length];
+            Array.Copy(nums, original, nums.Length);
+        }
+
+        public int[] Reset()
+        {
+            Array.Copy(original, nums, nums.Length);
+            return nums;
+        }
+
+        public int[] Shuffle()
+        {
+            int[] shuffled = new int[nums.Length];
+            IList<int> lst = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                lst.Add(nums[i]);
+            }
+            Random random = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int j = random.Next(lst.Count);
+                shuffled[i] = lst[j];
+                lst.Remove(lst[j]);
+            }
+            Array.Copy(shuffled, nums, nums.Length);
+            return nums;
+        }
+
+
+    }
+
+
+}
---
+++ Ahao.Nowcoder/LessHard/NC128/Class128.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Nowcoder.LessHard.NC128
+{
+    /// <summary>
+    /// NC128 接雨水问题
+    /// </summary>
+    public class Class128
+    {
+        public long maxWater(List<int> arr)
+        {
+            if (arr == null || arr.Count == 0)
+            {
+                return 0;
+            }
+            int N = arr.Count;
+            int L = 1;
+            int R = N - 2;
+            int leftMax = arr[0];
+            int rightMax = arr[N - 1];
+            int water = 0;
+            while (L <= R)
+            {
+                if (leftMax <= rightMax)
+                {
+                    water += Math.Max(0, leftMax - arr[L]);
+                    leftMax = Math.Max(leftMax, arr[L++]);
+                }
+                else
+                {
+                    water += Math.Max(0, rightMax - arr[R]);
+                    rightMax = Math.Max(rightMax, arr[R--]);
+                }
+            }
+            return water;
+        }
+    }
+}
