1b5408a25d4aedd18fbeabbfcebb3d286c42167a
ahao214 <haochen214@163.com>
2021-11-23 19:44:19 +0800

    NC168 盛水最多的容器

---
+++ Ahao.Nowcoder/Medium/NC168/Class168.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.Nowcoder.Medium.NC168
+{
+    /// <summary>
+    /// NC168 盛水最多的容器
+    /// </summary>
+    public class Class168
+    {
+        public int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            int left = 0;
+            int right = height.Length - 1;
+            while (left < right)
+            {
+                //面积 = 短的柱子的高度 * 左右柱子之间的距离
+                int nowArea = Math.Min(height[left], height[right]) * (right - left);
+                maxArea = Math.Max(nowArea, maxArea);
+                //移动指针
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
