b64b32099b7ab1bdf7855cfaea18693c03af8eea
ahao214 <haochen214@163.com>
2021-11-24 13:47:58 +0800

    200. 岛屿数量

---
+++ Ahao.LeetCode/Medium/demo200/Class200.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo200
+{
+    /// <summary>
+    /// 200. 岛屿数量
+    /// </summary>
+    public class Class200
+    {
+        public int NumIslands(char[][] grid)
+        {
+            if (grid == null || grid.Length == 0)
+            {
+                return 0;
+            }
+
+            int nr = grid.Length;
+            int nc = grid[0].Length;
+            int numIslands = 0;
+            for (int r = 0; r < nr; ++r)
+            {
+                for (int c = 0; c < nc; ++c)
+                {
+                    if (grid[r][c] == '1')
+                    {
+                        ++numIslands;
+                        dfs(grid, r, c);
+                    }
+                }
+            }
+            return numIslands;
+        }
+
+        //深度优先
+        private void dfs(char[][] grid, int r, int c)
+        {
+            int nr = grid.Length;
+            int nc = grid[0].Length;
+
+            if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0')
+            {
+                return;
+            }
+
+            grid[r][c] = '0';
+            dfs(grid, r - 1, c);
+            dfs(grid, r + 1, c);
+            dfs(grid, r, c - 1);
+            dfs(grid, r, c + 1);
+        }
+    }
+}
