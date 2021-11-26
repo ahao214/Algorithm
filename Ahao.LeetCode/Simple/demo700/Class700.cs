48e9a593a8a665450b34fe284852d2854461d5b9
ahao214 <haochen214@163.com>
2021-11-26 14:54:10 +0800

    700. 二叉搜索树中的搜索

---
+++ Ahao.LeetCode/Simple/demo700/Class700.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo700
+{
+    /// <summary>
+    /// 700. 二叉搜索树中的搜索
+    /// </summary>
+    public class Class700
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            if (val == root.val)
+            {
+                return root;
+            }
+            if (root.val > val)
+            {
+                return SearchBST(root.left, val);
+            }
+            else
+            {
+                return SearchBST(root.right, val);
+            }
+            return null;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
