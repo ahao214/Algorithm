1121c9a74a8f472e78950a7c5eb9d8653e733e3e
ahao214 <haochen214@163.com>
2021-11-26 16:49:50 +0800

    130. 被围绕的区域

---
+++ Ahao.LeetCode/Medium/demo1091/Class1091.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo1091
+{
+    /// <summary>
+    /// 1091. 二进制矩阵中的最短路径
+    /// </summary>
+    public class Class1091
+    {
+        public int ShortestPathBinaryMatrix(int[][] grid)
+        {
+            
+        }
+    }
+}
---
+++ Ahao.LeetCode/Medium/demo130/Class130.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo130
+{
+    /// <summary>
+    /// 130. 被围绕的区域
+    /// </summary>
+    public class Class130
+    {
+        int row, col;
+        public void Solve(char[][] board)
+        {
+            row = board.Length;
+            if (row == 0 || board == null)
+            {
+                return;
+            }
+
+            col = board[0].Length;
+            for (int i = 0; i < row; ++i)
+            {
+                dfs(board, i, 0);
+                dfs(board, i, col - 1);
+            }
+            for (int i = 1; i < col - 1; ++i)
+            {
+                dfs(board, 0, i);
+                dfs(board, row - 1, i);
+            }
+            for (int i = 0; i < row; ++i)
+            {
+                for (int j = 0; j < col; ++j)
+                {
+                    if (board[i][j] == '#')
+                    {
+                        board[i][j] = 'O';
+                    }
+                    else if (board[i][j] == 'O')
+                    {
+                        board[i][j] = 'X';
+                    }
+                }
+            }
+        }
+
+        public void dfs(char[][] board, int x, int y)
+        {
+            if (x < 0 || x >= row || y < 0 || y >= col || board[x][y] != 'o')
+            {
+                return;
+            }
+            board[x][y] = '#';
+            dfs(board, x + 1, y);
+            dfs(board, x - 1, y);
+            dfs(board, x, y + 1);
+            dfs(board, x, y + 1);
+        }
+    }
+}
