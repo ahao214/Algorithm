0b626a373983a82876447c3ce80628f7d2d11071
ahao214 <haochen214@163.com>
2021-11-28 11:14:41 +0800

    38. 外观数列

---
+++ Ahao.LeetCode/Medium/demo38/Class38.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo38
+{
+    /// <summary>
+    /// 38. 外观数列
+    /// </summary>
+    public class Class38
+    {
+        public string CountAndSay(int n)
+        {
+            if (n <= 0)
+            {
+                return string.Empty;
+            }
+
+            var answer = new StringBuilder("1");
+            var tmp = new StringBuilder();
+            for (int i = 1; i < n; i++)
+            {
+                var t = answer;
+                answer = tmp;
+                tmp = t;
+                answer.Clear();
+                var j = 1;
+                var count = 1;
+                var ch = tmp[0];
+                while (j < tmp.Length)
+                {
+                    if (tmp[j] == ch)
+                    {
+                        j++;
+                        count++;
+                    }
+                    else
+                    {
+                        answer.Append(count).Append(ch);
+                        ch = tmp[j];
+                        count = 1;
+                        j++;
+                    }
+                }
+                answer.Append(count).Append(ch);
+            }
+            return answer.ToString();
+        }
+    }
+}
