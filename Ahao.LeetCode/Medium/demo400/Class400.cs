daf93c76c56009b2df0d488db29dcb41371bb2d5
ahao214 <haochen214@163.com>
2021-11-30 18:15:19 +0800

    400. 第 N 位数字

---
+++ Ahao.LeetCode/Medium/demo400/Class400.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo400
+{
+    /// <summary>
+    /// 400. 第 N 位数字
+    /// </summary>
+    public class Class400
+    {
+        //直接计算
+        public int FindNthDigit(int n)
+        {
+            if (n < 10)
+            {
+                return n;
+            }
+
+            int d = 1;
+            int count = 9;
+            while (n > (long)d * count)
+            {
+                n -= d * count;
+                d++;
+                count *= 10;
+            }
+
+            int index = n - 1;
+            int start = (int)Math.Pow(10, d - 1);
+            int num = start + index / d;
+            int digitIndex = index % d;
+            int digit = (num / (int)(Math.Pow(10, d - digitIndex - 1))) % 10;
+            return digit;
+        }
+    }
+}
