f6ab8a29828524fa3f80dd5dbc5f7ea5a8475202
ahao214 <haochen214@163.com>
2021-12-02 20:52:52 +0800

    2. 两数相加

--- Ahao.LeetCode/Medium/demo2/Class2.cs
+++ Ahao.LeetCode/Medium/demo2/Class2.cs
@@ -17,6 +17,26 @@ namespace Ahao.LeetCode.Medium.demo2
     {
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
+            var node1 = l1;
+            var node2 = l2;
+            var c = 0;
+            var dummy = new ListNode(0);
+            var tail = dummy;
+            while (node1 != null | node2 != null)
+            {
+                var nOne = node1?.val ?? 0;
+                var nTwo = node2?.val ?? 0;
+                var n = nOne + nTwo + c;
+                tail = tail.next = new ListNode(n % 10);
+                c = n / 10;
+                node1 = node1?.next;
+                node2 = node2?.next;
+            }
+            if (c != 0)
+            {
+                tail = tail.next = new ListNode(c);
+            }
+            return dummy.next;
 
         }
     }
