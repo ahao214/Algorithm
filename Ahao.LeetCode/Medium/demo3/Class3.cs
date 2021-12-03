cf1c7139616ecd6ca2a5ae8a021cd17ead3d0426
ahao214 <haochen214@163.com>
2021-12-04 00:19:52 +0800

    3. 无重复字符的最长子串

--- Ahao.LeetCode/Medium/demo3/Class3.cs
+++ Ahao.LeetCode/Medium/demo3/Class3.cs
@@ -43,5 +43,47 @@ namespace Ahao.LeetCode.Medium.demo3
             }
             return len;
         }
+
+        public int LengthOfLongestSubstring2(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            var i = 0;
+            var j = 1;
+            var len = 1;
+            var map = new Dictionary<char, int>();
+            map[s[0]] = 0;
+            while (i + len <= s.Length && j < s.Length)
+            {
+                var ch = s[j];
+                if (map.TryGetValue(ch, out var m))
+                {
+                    if (j - i > len)
+                    {
+                        len = j - i;
+                    }
+                    for (int k = i; k <= m; k++)
+                    {
+                        map.Remove(s[k]);
+                    }
+                    i = m + 1;
+                    map.Add(ch, j);
+                    j++;
+                }
+                else
+                {
+                    map.Add(ch, j);
+                    j++;
+                }
+            }
+            if (j - i > len)
+            {
+                len = j - i;
+            }
+            return len;
+        }
     }
 }
