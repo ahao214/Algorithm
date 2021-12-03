7418d4d5fd41178b8c3fc12965740ffa079a8706
ahao214 <haochen214@163.com>
2021-12-04 00:31:29 +0800

    383. 赎金信

---
+++ Ahao.LeetCode/Simple/demo383/Class383.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo383
+{
+    /// <summary>
+    /// 383. 赎金信
+    /// </summary>
+    public class Class383
+    {
+    }
+}
