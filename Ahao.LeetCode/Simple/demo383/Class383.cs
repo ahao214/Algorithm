4a6c8bdfc54a654a7b9aa5e6e54b8faa546f264d
ahao214 <haochen214@163.com>
2021-12-04 18:16:31 +0800

    383. 赎金信

--- Ahao.LeetCode/Simple/demo383/Class383.cs
+++ Ahao.LeetCode/Simple/demo383/Class383.cs
@@ -11,5 +11,26 @@ namespace Ahao.LeetCode.Simple.demo383
     /// </summary>
     public class Class383
     {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (ransomNote.Length > magazine.Length)
+            {
+                return false;
+            }
+            int[] cnt = new int[26];
+            foreach (char c in magazine)
+            {
+                cnt[c - 'a']++;
+            }
+            foreach (char c in ransomNote)
+            {
+                cnt[c - 'a']--;
+                if (cnt[c - 'a'] < 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
