c67916692cff32ba1830d57625e617044d0cc3c0
ahao214 <haochen214@163.com>
2021-12-05 12:42:14 +0800

    1143. 最长公共子序列

---
+++ Ahao.LeetCode/Medium/demo1143/Class1143.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo1143
+{
+    /// <summary>
+    /// 1143. 最长公共子序列
+    /// </summary>
+    public class Class1143
+    {
+        public int LongestCommonSubsequence(string text1, string text2)
+        {
+            int m = text1.Length;
+            int n = text2.Length;
+            char[] temp1 = text1.ToCharArray();
+            char[] temp2 = text2.ToCharArray();
+            if (m == 0 || n == 0)
+            {
+                return 0;
+            }
+            int[,] dp = new int[m + 1, n + 1];
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (temp1[i - 1] == temp2[j - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Max(dp[i, j - 1], dp[i - 1, j]);
+                    }
+                }
+            }
+            return dp[m, n];
+        }
+    }
+}
---
+++ Ahao.LeetCode/Medium/demo372/Class372.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo372
+{
+    /// <summary>
+    /// 372. 超级次方
+    /// </summary>
+    public class Class372
+    {
+        public int SuperPow(int a, int[] b)
+        {
+            if (b.Length == 0)
+            {
+                return 1;
+            }
+
+            int last = b[b.Length - 1];
+            int l = 1;
+            for (int i = 1; i <= last; i++)
+            {
+                l = l * a % 1337;
+            }
+
+            int tmp = SuperPow(a, b.Take(b.Length - 1).ToArray());
+            int f = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                f = f * tmp % 1337;
+            }
+            return f * l % 1337;
+        }
+    }
+}
