d6bbb10b8393c75e07dbefa1c6f2816293bce865
ahao214 <haochen214@163.com>
2021-12-07 01:18:09 +0800

    201. 数字范围按位与

---
+++ Ahao.LeetCode/Medium/demo201/Class201.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo201
+{
+    /// <summary>
+    /// 201. 数字范围按位与
+    /// </summary>
+    public class Class201
+    {
+        public int RangeBitwiseAnd(int left, int right)
+        {
+            while (left < right)
+            {
+                right = right & (right - 1);
+            }
+            return right;
+        }
+    }
+}
