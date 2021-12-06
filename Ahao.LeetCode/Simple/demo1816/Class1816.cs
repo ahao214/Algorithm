1bcd3c89ffd7ce2dfa77d0f534c3374cae3bb4a1
ahao214 <haochen214@163.com>
2021-12-06 18:32:28 +0800

    1816. 截断句子

--- Ahao.LeetCode/Simple/demo1816/Class1816.cs
+++ Ahao.LeetCode/Simple/demo1816/Class1816.cs
@@ -18,6 +18,24 @@ namespace Ahao.LeetCode.Simple.demo1816
         }
 
 
-
+        public string TruncateSentence2(string s, int k)
+        {
+            int n = s.Length;
+            int end = 0;
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == n || s[i] == ' ')
+                {
+                    count++;
+                    if (count == k)
+                    {
+                        end = i;
+                        break;
+                    }
+                }
+            }
+            return s.Substring(0, end);
+        }
     }
 }
