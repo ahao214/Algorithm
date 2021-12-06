83ce47755713320feac51e1d5834966ba07a49cb
ahao214 <haochen214@163.com>
2021-12-06 18:31:48 +0800

    1816. 截断句子

---
+++ Ahao.LeetCode/Simple/demo1816/Class1816.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo1816
+{
+    /// <summary>
+    /// 1816. 截断句子
+    /// </summary>
+    public class Class1816
+    {
+        public string TruncateSentence(string s, int k)
+        {
+            var str = s.Split(' ').Skip(0).Take(k).ToArray();
+            return string.Join(' ', str);
+        }
+
+
+
+    }
+}
