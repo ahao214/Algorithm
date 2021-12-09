49716f3e836f699b40924aeff6bf7a698f45efe1
ahao214 <haochen214@163.com>
2021-12-09 22:00:42 +0800

    123. 买卖股票的最佳时机 III

--- Ahao.LeetCode/Hard/demo123/Class123.cs
+++ Ahao.LeetCode/Hard/demo123/Class123.cs
@@ -11,6 +11,27 @@ namespace Ahao.LeetCode.Hard.demo123
     /// </summary>
     public class Class123
     {
-        
+        public int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length == 0)
+            {
+                return 0;
+            }
+
+            int res = 0;
+            //完成第一笔交易获取到的最大值 减去 再次买入股票价格，得到的最大值
+            int doneOnceMinusBuyMax = -prices[0];
+            //完成第一笔交易获取到的最大值
+            int doneOnceMax = 0;
+            int min = prices[0];
+            for (int i = 1; i < prices.Length; i++)
+            {
+                min = Math.Min(min, prices[i]);
+                res = Math.Max(res, doneOnceMinusBuyMax + prices[i]);
+                doneOnceMax = Math.Max(doneOnceMax, prices[i] - min);
+                doneOnceMinusBuyMax = Math.Max(doneOnceMinusBuyMax, doneOnceMax - prices[i]);
+            }
+            return res;
+        }
     }
 }
