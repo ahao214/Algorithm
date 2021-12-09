065e3f48ddbd1607f7f6876981a6510d6a0fdf0b
ahao214 <haochen214@163.com>
2021-12-09 17:25:36 +0800

    6. Z 字形变换

---
+++ Ahao.LeetCode/Medium/demo6/Class6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo6
+{
+    /// <summary>
+    /// 6. Z 字形变换
+    /// </summary>
+    public class Class6
+    {
+
+    }
+}
