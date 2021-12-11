d7880c0e0f8c1da41b75b27d7a11c34aa9739891
ahao214 <haochen214@163.com>
2021-12-12 00:09:56 +0800

    709. 转换成小写字母

---
+++ Ahao.LeetCode/Medium/demo911/Class911.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo911
+{
+    /// <summary>
+    /// 911. 在线选举
+    /// </summary>
+    public class TopVotedCandidate
+    {
+        IList<int> tops;
+        Dictionary<int, int> voteCounts;
+        int[] times;
+
+        public TopVotedCandidate(int[] persons, int[] times)
+        {
+            tops = new List<int>();
+            voteCounts = new Dictionary<int, int>();
+            voteCounts.Add(-1, -1);
+            int top = -1;
+            for (int i = 0; i < persons.Length; ++i)
+            {
+                int p = persons[i];
+                if (!voteCounts.ContainsKey(p))
+                {
+                    voteCounts.Add(p, 0);
+                }
+                else
+                {
+                    ++voteCounts[p];
+                }
+                if (voteCounts[p] >= voteCounts[top])
+                {
+                    top = p;
+                }
+                tops.Add(top);
+            }
+            this.times = times;
+        }
+
+        public int Q(int t)
+        {
+            int l = 0, r = times.Length - 1;
+            // 找到满足 times[l] <= t 的最大的 l
+            while (l < r)
+            {
+                int m = l + (r - l + 1) / 2;
+                if (times[m] <= t)
+                {
+                    l = m;
+                }
+                else
+                {
+                    r = m - 1;
+                }
+            }
+            return tops[l];
+        }
+    }
+}
---
+++ Ahao.LeetCode/Simple/demo709/Class709.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo709
+{
+    /// <summary>
+    /// 709. 转换成小写字母
+    /// </summary>
+    public class Class709
+    {
+        public string ToLowerCase(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            string newS = string.Empty;
+            foreach (char c in s)
+            {
+                newS += c.ToString().ToLower();
+            }
+            return newS;
+        }
+    }
+}
