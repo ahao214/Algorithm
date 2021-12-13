746c55877689110863e18b4f33a6f210258e333b
ahao214 <haochen214@163.com>
2021-12-13 23:20:37 +0800

    807. 保持城市天际线

---
+++ Ahao.LeetCode/Medium/demo807/Class807.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo807
+{
+    /// <summary>
+    /// 807. 保持城市天际线
+    /// </summary>
+    public class Class807
+    {
+        public int MaxIncreaseKeepingSkyline(int[][] grid)
+        {
+            int n = grid.Length;
+            //记录每行的最大值
+            int[] rowMax = new int[n];
+            //记录每列的最大值
+            int[] colMax = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    rowMax[i] = Math.Max(rowMax[i], grid[i][j]);
+                    colMax[j] = Math.Max(colMax[j], grid[i][j]);
+                }
+            }
+
+            int res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    //每个建筑物所能增加的高度
+                    res += Math.Min(rowMax[i], colMax[j]) - grid[i][j];
+                }
+            }
+            return res;
+        }
+    }
+}
