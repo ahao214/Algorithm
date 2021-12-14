93d4c4f52a44ca2c0ffedfb43e539967ed65a1dd
ahao214 <haochen214@163.com>
2021-12-14 23:04:07 +0800

    538. 把二叉搜索树转换为累加树

---
+++ Ahao.LeetCode/Medium/demo538/Class538.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo538
+{
+    /// <summary>
+    /// 538. 把二叉搜索树转换为累加树
+    /// </summary>
+    public class Class538
+    {
+        int sum = 0;
+        public TreeNode ConvertBST(TreeNode root)
+        {
+            dfs(root);
+            return root;
+        }
+        private void dfs(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            dfs(node.right);
+            node.val += sum;
+            sum = node.val;
+            dfs(node.left);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
