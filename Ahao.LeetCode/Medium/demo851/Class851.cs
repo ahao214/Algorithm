585e0a72ff50aa7780a2be9e34810d6bd640f8fd
ahao214 <haochen214@163.com>
2021-12-15 17:05:06 +0800

    851. 喧闹和富有-深度优先

---
+++ Ahao.LeetCode/Medium/demo851/Class851.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo851
+{
+    /// <summary>
+    /// 851. 喧闹和富有
+    /// </summary>
+    public class Class851
+    {
+        //深度优先
+        public int[] LoudAndRich(int[][] richer, int[] quiet)
+        {
+            int n = quiet.Length;
+            IList<int>[] g = new List<int>[n];
+            for (int i = 0; i < n; ++i)
+            {
+                g[i] = new List<int>();
+            }
+            foreach (int[] r in richer)
+            {
+                g[r[1]].Add(r[0]);
+            }
+
+            int[] ans = new int[n];
+            Array.Fill(ans, -1);
+            for (int i = 0; i < n; ++i)
+            {
+                DFS(i, quiet, g, ans);
+            }
+            return ans;
+        }
+        public void DFS(int x, int[] quiet, IList<int>[] g, int[] ans)
+        {
+            if (ans[x] != -1)
+            {
+                return;
+            }
+            ans[x] = x;
+            foreach (int y in g[x])
+            {
+                DFS(y, quiet, g, ans);
+                if (quiet[ans[y]] < quiet[ans[x]])
+                {
+                    ans[x] = ans[y];
+                }
+            }
+        }
+    }
+}
