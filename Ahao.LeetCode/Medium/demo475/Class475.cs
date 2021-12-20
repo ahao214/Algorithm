917cec7580131cda5357baed5fe7a561788e4f3a
ahao214 <haochen214@163.com>
2021-12-20 17:54:12 +0800

    475. 供暖器-排序+二分

--- Ahao.LeetCode/Medium/demo475/Class475.cs
+++ Ahao.LeetCode/Medium/demo475/Class475.cs
@@ -27,5 +27,46 @@ namespace Ahao.LeetCode.Medium.demo475
             }
             return result;
         }
+
+
+        //排序+二分
+        public int FindRadius2(int[] houses, int[] heaters)
+        {
+            int ans = 0;
+            Array.Sort(heaters);
+            foreach (int house in houses)
+            {
+                int i = BinarySearch(heaters, house);
+                int j = i + 1;
+                int leftDistance = i < 0 ? int.MaxValue : house - heaters[i];
+                int rightDistance = j >= heaters.Length ? int.MaxValue : heaters[j] - house;
+                int curDistance = Math.Min(leftDistance, rightDistance);
+                ans = Math.Max(ans, curDistance);
+            }
+            return ans;
+        }
+
+        public int BinarySearch(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length - 1;
+            if (nums[left] > target)
+            {
+                return -1;
+            }
+            while (left < right)
+            {
+                int mid = (right - left + 1) / 2 + left;
+                if (nums[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid;
+                }
+            }
+            return left;
+        }
+
     }
 }
