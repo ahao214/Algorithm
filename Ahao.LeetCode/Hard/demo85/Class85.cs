2dc604f220e9cc344d99b9376ff1655756285786
ahao214 <haochen214@163.com>
2021-12-21 01:38:05 +0800

    85. 最大矩形

---
+++ Ahao.LeetCode/Hard/demo85/Class85.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ahao.LeetCode.Hard.demo85
+{
+    /// <summary>
+    /// 85. 最大矩形
+    /// </summary>
+    public class Class85
+    {
+        public int MaximalRectangle(char[][] matrix)
+        {
+            if (matrix == null || matrix.Length == 0)
+            {
+                return 0;
+            }
+            var rowCount = matrix.Length;
+            var colCount = matrix[0].Length;
+            var hcount = new int[rowCount, colCount];
+            var vcount = new int[rowCount, colCount];
+
+            for (int i = 0; i < colCount; i++)
+            {
+                vcount[rowCount - 1, i] = 0;
+            }
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                hcount[i, colCount - 1] = 0;
+            }
+
+            for (int i = rowCount - 1; i >= 0; i--)
+            {
+                for (int j = colCount - 1; j >= 0; j--)
+                {
+                    if (matrix[i][j] == '0')
+                    {
+                        hcount[i, j] = 0;
+                        vcount[i, j] = 0;
+                    }
+                    else
+                    {
+                        hcount[i, j] = j + 1 == colCount ? 0 : hcount[i, j + 1] + 1;
+                        vcount[i, j] = i + 1 == rowCount ? 0 : vcount[i + 1, j] + 1;
+                    }
+                }
+            }
+            
+            var heap = new MinHeap<Item>(new List<Item>(rowCount * colCount), 0, (x, y) => Comparer<int>.Default.Compare(x.Value, y.Value));
+
+            
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < colCount; j++)
+                {
+                    if (matrix[i][j] != '0')
+                    {
+                        heap.Insert(new Item(i * colCount + j, i, j, 1, 1));
+                    }
+                }
+            }
+
+            var maxValue = 0;
+            while (heap.Count > 0)
+            {
+                var item = heap.PopRoot();
+                var row = item.Row;
+                var col = item.Col;
+
+                if (hcount[row, col] * vcount[row, col] <= maxValue)
+                {
+                    maxValue = Math.Max(maxValue, item.Area);
+                }
+
+                if (item.HAble)
+                {
+                    if (item.NextCol < colCount && vcount[row, item.NextCol] >= item.Height)
+                    {
+                        heap.Insert(new Item(item.Value, row, col, item.Width + 1, item.Height, true));
+                    }
+                }
+
+                if (item.NextRow < rowCount && hcount[item.NextRow, col] >= item.Width)
+                {
+                    heap.Insert(new Item(item.Value, row, col, item.Width, item.Height + 1, true));
+                }
+            }
+            return maxValue;
+        }
+
+        struct Item
+        {
+            public readonly int Value;
+            public readonly int Row;
+            public readonly int Col;
+            public readonly int Height;
+            public readonly int Width;
+            public readonly bool HAble;
+
+            public int NextRow => Row + Width;
+            public int NextCol => Col + Width;
+            public int Area => Width * Height;
+
+            public Item(int value, int row, int col, int height, int width, bool hAble = true)
+            {
+                this.Value = value;
+                this.Row = row;
+                this.Col = col;
+                this.Height = height;
+                this.Width = width;
+                this.HAble = hAble;
+            }
+        }
+    }
+}
---
+++ Ahao.LeetCode/Hard/demo85/MinHeap.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Hard.demo85
+{
+
+    public class MinHeap<T> where T : IComparable
+    {
+        ///<summary>
+        /// 储存元素的List
+        ///<summary>
+        private List<T> _elements;
+        ///<summary>
+        /// 堆元素数量
+        ///<summary>
+        public int Count
+        {
+            get { return _elements.Count; }
+        }
+        ///<summary>
+        /// 构造函数
+        ///<summary>
+        public MinHeap()
+        {
+            _elements = new List<T>();
+
+        }
+        ///<summary>
+        ///构造函数
+        ///<summary>
+        /// <param name="initSize">初始堆大小</param>
+        public MinHeap(int initSize)
+        {
+            _elements = new List<T>(initSize);
+
+        }
+
+        ///<summary>
+        /// 弹出堆首元素
+        ///<summary>
+        public T Pop()
+        {
+
+            if (Count == 0)
+            {
+                throw new IndexOutOfRangeException("No element can be poped.");
+            }
+            else
+            {
+                //将堆中最后一个节点元素放置在根节点中，并移除最后一个节点元素
+                //更换位置后，将根节点元素进行下沉操作
+                T result = _elements[0];
+                _elements[0] = _elements[Count - 1];
+                _elements.RemoveAt(Count - 1);
+                Sink();
+
+                return result;
+            }
+        }
+        ///<summary>
+        /// 将根元素下沉
+        ///<summary>
+        private void Sink()
+        {
+
+            int currentIndex = 0;
+            int leftChildIndex = 1;
+            int rightChildIndex = 2;
+
+            while (true)
+            {
+
+                //如果没有左子节点，说明目前节点已经是最后一个节点了
+                if (leftChildIndex >= Count)
+                {
+                    break;
+
+                }
+
+
+                T currentElement = _elements[currentIndex];
+                T leftChildElement = _elements[leftChildIndex];
+
+                //如果没有右子节点， 则只处理左节点
+                if (rightChildIndex >= Count)
+                {
+                    if (currentElement.CompareTo(leftChildElement) > 0)
+                    {
+
+                        _elements[currentIndex] = leftChildElement;
+                        _elements[leftChildIndex] = currentElement;
+
+                        currentIndex = leftChildIndex;
+
+                    }
+                }
+                else
+                {
+                    //下面的情况为左右子节点均存在
+                    T rightChildElement = _elements[rightChildIndex];
+
+                    int compareResult = leftChildElement.CompareTo(rightChildElement);
+
+                    // 左子节点为最小值
+                    // 或
+                    // 左右子节点值相同，取左子节点
+                    if (compareResult < 0 || compareResult == 0)
+                    {
+
+                        if (currentElement.CompareTo(leftChildElement) > 0)
+                        {
+
+                            _elements[currentIndex] = leftChildElement;
+                            _elements[leftChildIndex] = currentElement;
+
+                            currentIndex = leftChildIndex;
+
+                        }
+
+                    }
+                    //右子节点为最小值
+                    else if (compareResult > 0)
+                    {
+                        if (currentElement.CompareTo(rightChildElement) > 0)
+                        {
+
+                            _elements[currentIndex] = rightChildElement;
+                            _elements[rightChildIndex] = currentElement;
+
+                            currentIndex = rightChildIndex;
+
+                        }
+
+                    }
+
+                }
+
+                leftChildIndex = currentIndex * 2 + 1;
+                rightChildIndex = currentIndex * 2 + 2;
+
+            }
+
+        }
+
+        ///<summary>
+        /// 插入元素
+        ///<summary>
+        /// <param name="element">插入的元素</param>
+        public void Insert(T element)
+        {
+
+            //将元素添加到最后一个节点
+            _elements.Add(element);
+
+            //将最后一个元素进行上浮操作
+            Heapify();
+
+        }
+        ///<summary>
+        ///重构堆
+        ///<summary>
+        private void Heapify()
+        {
+
+            int currentIndex = Count - 1;
+            int parentIndex = 0;
+
+            while (parentIndex >= 0)
+            {
+
+                parentIndex = (currentIndex - 1) / 2;
+
+                T currentElement = _elements[currentIndex];
+                T parentElement = _elements[parentIndex];
+
+                if (currentElement.CompareTo(parentElement) < 0)
+                {
+                    _elements[parentIndex] = currentElement;
+                    _elements[currentIndex] = parentElement;
+
+                    currentIndex = parentIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+    }
+
+
+}
