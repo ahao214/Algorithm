06979eab89686db27494b0facc83a146e8b6c9d5
ahao214 <haochen214@163.com>
2021-12-20 23:37:56 +0800

    84. 柱状图中最大的矩形

--- Ahao.LeetCode/Hard/demo84/Class84.cs
+++ Ahao.LeetCode/Hard/demo84/Class84.cs
@@ -56,5 +56,61 @@ namespace Ahao.LeetCode.Hard.demo84
                 return heights.Length;
             }
         }
+
+
+        public int LargestRectangleArea2(int[] heights)
+        {
+            if (heights == null || heights.Length == 0)
+            {
+                return 0;
+            }
+
+            var stack = new Stack<Item>(heights.Length);
+            var maxValue = 0;
+            stack.Push(new Item(-1, -1, -1));
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if (heights[i] >= stack.Peek().Height)
+                {
+                    stack.Push(new Item(heights[i], i, 0));
+                }
+                else
+                {
+                    while (true)
+                    {
+                        var pre = stack.Pop();
+                        var area = pre.Height * (pre.PreCount + i - pre.Index);
+                        maxValue = Math.Max(maxValue, area);
+                        if (stack.Count == 0 || stack.Peek().Height < heights[i])
+                        {
+                            break;
+                        }
+                    }
+                    stack.Push(new Item(heights[i], i, i - stack.Peek().Index - 1));
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                var pre = stack.Pop();
+                var area = pre.Height * (pre.PreCount + heights.Length - pre.Index);
+                maxValue = Math.Max(maxValue, area);
+            }
+
+            return maxValue;
+        }
+
+        struct Item
+        {
+            public readonly int Height;
+            public readonly int Index;
+            public readonly int PreCount;
+            public Item(int height, int index, int preCount)
+            {
+                this.Height = height;
+                this.Index = index;
+                this.PreCount = preCount;
+            }
+        }
     }
 }
