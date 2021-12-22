95b4f6742261eb4252a8a4b90b72b196a0978d89
ahao214 <haochen214@163.com>
2021-12-22 18:38:45 +0800

    686. 重复叠加字符串匹配-KMP算法

--- Ahao.LeetCode/Medium/demo686/Class686.cs
+++ Ahao.LeetCode/Medium/demo686/Class686.cs
@@ -80,8 +80,55 @@ namespace Ahao.LeetCode.Medium.demo686
         /// <returns></returns>
         public int RepeatedStringMatch2(string a, string b)
         {
-
+            int an = a.Length, bn = b.Length;
+            int index = StrStr2(a, b);
+            if (index == -1)
+            {
+                return -1;
+            }
+            if (an - index >= bn)
+            {
+                return 1;
+            }
+            return (bn + index - an - 1) / an + 2;
         }
 
+        public int StrStr2(string haystack, string needle)
+        {
+            int n = haystack.Length, m = needle.Length;
+            if (m == 0)
+            {
+                return 0;
+            }
+            int[] pi = new int[m];
+            for (int i = 1, j = 0; i < m; i++)
+            {
+                while (j > 0 && needle[i] != needle[j])
+                {
+                    j = pi[j - 1];
+                }
+                if (needle[i] == needle[j])
+                {
+                    j++;
+                }
+                pi[i] = j;
+            }
+            for (int i = 0, j = 0; i - j < n; i++)
+            { // b 开始匹配的位置是否超过第一个叠加的 a
+                while (j > 0 && haystack[i % n] != needle[j])
+                { // haystack 是循环叠加的字符串，所以取 i % n
+                    j = pi[j - 1];
+                }
+                if (haystack[i % n] == needle[j])
+                {
+                    j++;
+                }
+                if (j == m)
+                {
+                    return i - m + 1;
+                }
+            }
+            return -1;
+        }
     }
 }
