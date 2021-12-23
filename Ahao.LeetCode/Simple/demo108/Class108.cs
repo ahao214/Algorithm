ab67b95d6ac0a2d6cdba422d07d454a29d7fb0a4
ahao214 <haochen214@163.com>
2021-12-23 08:12:54 +0800

    108. 将有序数组转换为二叉搜索树

---
+++ Ahao.LeetCode/Simple/demo108/Class108.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo108
+{
+    /// <summary>
+    /// 108. 将有序数组转换为二叉搜索树
+    /// </summary>
+    public class Class108
+    {
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
