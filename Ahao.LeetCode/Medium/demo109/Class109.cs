80231cae721332d03080939809ae2d5c3fb2cb93
ahao214 <haochen214@163.com>
2021-12-23 08:14:23 +0800

    109. 有序链表转换二叉搜索树

---
+++ Ahao.LeetCode/Medium/demo109/Class109.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo109
+{
+    /// <summary>
+    /// 109. 有序链表转换二叉搜索树
+    /// </summary>
+    public class Class109
+    {
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
