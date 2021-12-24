a2276719619d6920523daa6810c4c1b2acd8cf20
ahao214 <haochen214@163.com>
2021-12-24 23:46:48 +0800

    128. 最长连续序列

---
+++ Ahao.LeetCode/Medium/demo128/Class128.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo128
+{
+    /// <summary>
+    /// 128. 最长连续序列
+    /// </summary>
+    public class Class128
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            var set = new HashSet<int>(nums);
+            var max = 0;
+            foreach (var n in nums)
+            {
+                if (set.Contains(n - 1))
+                {
+                    continue;
+                }
+                var m = n + 1;
+                while (set.Contains(m))
+                {
+                    m++;
+                }
+                max = Math.Max(max, m - n);
+            }
+            return max;
+        }
+    }
+}
