cfeddb574d67b466f75a8db38946a1dae8bd23e1
ahao214 <haochen214@163.com>
2021-12-27 00:03:26 +0800

    122. 买卖股票的最佳时机 II

---
+++ Ahao.LeetCode/Medium/demo122/Class122.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo122
+{
+    /// <summary>
+    /// 122. 买卖股票的最佳时机 II
+    /// </summary>
+    public class Class122
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length == 0)
+            {
+                return 0;
+            }
+            var sum = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                {
+                    sum += prices[i] - prices[i - 1];
+                }
+            }
+            return sum;
+        }
+    }
+}
