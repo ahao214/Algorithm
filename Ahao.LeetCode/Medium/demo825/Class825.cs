d645d1579c86a568069330cde4f7c957dd93bda2
ahao214 <haochen214@163.com>
2021-12-27 01:01:23 +0800

    825. 适龄的朋友

--- Ahao.LeetCode/Medium/demo825/Class825.cs
+++ Ahao.LeetCode/Medium/demo825/Class825.cs
@@ -33,6 +33,36 @@ namespace Ahao.LeetCode.Medium.demo825
             return result;
         }
 
+
+        public int NumFriendRequests1(int[] ages)
+        {
+            if (ages == null || ages.Length == 0)
+            {
+                return 0;
+            }
+
+            int[] count = new int[121];
+            foreach (var age in ages)
+            {
+                count[age]++;
+            }
+            int ans = 0;
+            for (int ageA = 0; ageA <= 120; ageA++)
+            {
+                int countA = count[ageA];
+                for (int ageB = 0; ageB <= 120; ageB++)
+                {
+                    int countB = count[ageB];
+                    if (ageA * 0.5 + 7 >= ageB) continue;
+                    if (ageA < ageB) continue;
+                    if (ageA < 100 && 100 < ageB) continue;
+                    ans += countA * countB;
+                    if (ageA == ageB) ans -= countA;
+                }
+            }
+            return ans;
+        }
+
         //如果下述任意一个条件为真，那么用户 x 将不会向用户 y（x != y）发送好友请求
         public bool IsTrue(int x, int y)
         {
