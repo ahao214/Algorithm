ffa7c374732d6f2e6ec38e54b6fe5e86524755a4
ahao214 <haochen214@163.com>
2021-12-29 07:32:30 +0800

    1995. 统计特殊四元组-暴力方法

---
+++ Ahao.LeetCode/Simple/demo1995/Class1995.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo1995
+{
+    /// <summary>
+    /// 1995. 统计特殊四元组
+    /// </summary>
+    public class Class1995
+    {
+        public int CountQuadruplets(int[] nums)
+        {
+            int n = nums.Length;
+            int res = 0;
+            for (int a = 0; a < n; a++)
+            {
+                for (int b = a + 1; b < n; b++)
+                {
+                    for (int c = b + 1; c < n; c++)
+                    {
+                        for (int d = c + 1; d < n; d++)
+                        {
+                            if (nums[a] + nums[b] + nums[c] == nums[d])
+                            {
+                                res++;
+                            }
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
