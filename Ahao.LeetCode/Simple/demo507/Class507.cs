6e404e78fcec26e8c7129149c8541e5d21e3701a
ahao214 <haochen214@163.com>
2021-12-31 18:44:50 +0800

    507. 完美数

--- Ahao.LeetCode/Simple/demo507/Class507.cs
+++ Ahao.LeetCode/Simple/demo507/Class507.cs
@@ -23,5 +23,28 @@ namespace Ahao.LeetCode.Simple.demo507
             }
             return sum == num;
         }
+
+
+        public bool CheckPerfectNumber1(int num)
+        {
+            if (num == 1)
+            {
+                return false;
+            }
+
+            int sum = 1;
+            for (int d = 2; d * d <= num; ++d)
+            {
+                if (num % d == 0)
+                {
+                    sum += d;
+                    if (d * d < num)
+                    {
+                        sum += num / d;
+                    }
+                }
+            }
+            return sum == num;
+        }
     }
 }
