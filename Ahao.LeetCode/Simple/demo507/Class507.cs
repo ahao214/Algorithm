b80a2236083fbbeb0cdb4f10bc87027f59628822
ahao214 <haochen214@163.com>
2021-12-31 18:44:09 +0800

    507. 完美数

---
+++ Ahao.LeetCode/Simple/demo507/Class507.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo507
+{
+    /// <summary>
+    /// 507. 完美数
+    /// </summary>
+    public class Class507
+    {
+        public bool CheckPerfectNumber(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum == num;
+        }
+    }
+}
