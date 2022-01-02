31a8c98d0d2181128ade0b9c4a746dca9c33a85e
ahao214 <haochen214@163.com>
2022-01-02 12:00:18 +0800

    5968. 银行中的激光束数量

---
+++ Ahao.LeetCode/Hard/demo5970/Class5970.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Hard.demo5970
+{
+    /// <summary>
+    /// 5970. 参加会议的最多员工数
+    /// </summary>
+   public class Class5970
+    {
+        public int MaximumInvitations(int[] favorite)
+        {
+            int count = 0;
+            int n = favorite.Length;
+            int liked = -1;
+            for (int i = 0; i < favorite.Length; i++)
+            {
+                int cur = favorite[i];
+                liked = cur;
+
+            }
+        }
+    }
+}
---
+++ Ahao.LeetCode/Medium/demo5968/Class5968.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo5968
+{
+    /// <summary>
+    /// 5968. 银行中的激光束数量
+    /// </summary>
+    public class Class5968
+    {
+        public int NumberOfBeams(string[] bank)
+        {
+            int m = bank.Length;
+            int n = bank[0].Length;
+            if (m == 1)   //只有一行
+            {
+                return 0;
+            }
+
+            return 0;
+        }
+    }
+}
