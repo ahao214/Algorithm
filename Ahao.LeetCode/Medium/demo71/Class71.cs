af70f1e1b79126b026561e01dccb2967c0d2b3b7
ahao214 <haochen214@163.com>
2022-01-06 07:34:24 +0800

    71. 简化路径

---
+++ Ahao.LeetCode/Medium/demo71/Class71.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo71
+{
+    /// <summary>
+    /// 71. 简化路径
+    /// </summary>
+    public class Class71
+    {
+        public string SimplifyPath(string path)
+        {
+            string[] names = path.Split("/");
+            IList<string> stack = new List<string>();
+            foreach (string name in names)
+            {
+                if ("..".Equals(name))
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.RemoveAt(stack.Count - 1);
+                    }
+                }
+                else if (name.Length > 0 && !".".Equals(name))
+                {
+                    stack.Add(name);
+                }
+            }
+            StringBuilder ans = new StringBuilder();
+            if (stack.Count == 0)
+            {
+                ans.Append('/');
+            }
+            else
+            {
+                foreach (string name in stack)
+                {
+                    ans.Append('/');
+                    ans.Append(name);
+                }
+            }
+            return ans.ToString();
+        }
+    }
+}
