db550e2ea46474ae676873ae196afd9c753b4b70
ahao214 <haochen214@163.com>
2022-01-07 07:43:25 +0800

    1614. 括号的最大嵌套深度

---
+++ Ahao.LeetCode/Simple/demo1614/Class1614.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo1614
+{
+    /// <summary>
+    /// 1614. 括号的最大嵌套深度
+    /// </summary>
+    public class Class1614
+    {
+        public int MaxDepth(string s)
+        {
+            int res = 0;
+            int count = 0;
+            if (string.IsNullOrEmpty(s))
+            {
+                return res;
+            }
+            foreach (var c in s)
+            {
+                if (c == '(')
+                {
+                    count++;
+                    res = Math.Max(res, count);
+                }
+                if (c == ')')
+                {
+                    count--;
+                }
+            }
+            return res;
+        }
+    }
+}
