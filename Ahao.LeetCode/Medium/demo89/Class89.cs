4930c498f8b4c7db9ec918a624c215aa0f0ded7b
ahao214 <haochen214@163.com>
2022-01-08 10:20:18 +0800

    89. 格雷编码

---
+++ Ahao.LeetCode/Medium/demo89/Class89.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo89
+{
+    /// <summary>
+    /// 89. 格雷编码
+    /// </summary>
+    public class Class89
+    {
+        public IList<int> GrayCode(int n)
+        {
+            if (n == 0)
+            {
+                return new int[] { 0 };
+            }
+            if (n == 1)
+            {
+                return new int[] { 0, 1 };
+            }
+
+            var result = new int[1 << n];
+            result[0] = 0;
+            result[1] = 1;
+            var m = 2;
+            while (m < result.Length)
+            {
+
+                for (int i = 0, j = m - 1; i < m; i++, j--)
+                {
+                    result[i + m] = result[j] + m;
+                }
+                m *= 2;
+            }
+            return result;
+        }
+    }
+}
