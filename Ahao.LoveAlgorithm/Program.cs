5e4806a4b1001ee5f0a029197f28003315827023
ahao214 <haochen214@163.com>
2022-01-08 16:18:25 +0800

    恋上数据结构和算法

---
+++ Ahao.LoveAlgorithm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ahao.LoveAlgorithm
+{
+    /// <summary>
+    /// 恋上数据结构和算法
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
