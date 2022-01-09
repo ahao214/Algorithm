ee54e031e6c5360bdf1a1778cb9258a0b23a1f1a
ahao214 <haochen214@163.com>
2022-01-09 11:54:24 +0800

    5976. 检查是否每一行每一列都包含全部整数

--- Ahao.LeetCode/Simple/demo5976/Class5976.cs
+++ Ahao.LeetCode/Simple/demo5976/Class5976.cs
@@ -8,6 +8,8 @@ namespace Ahao.LeetCode.Simple.demo5976
 {
     /// <summary>
     /// 5976. 检查是否每一行每一列都包含全部整数
+    /// 对一个大小为 n x n 的矩阵而言，如果其每一行和每一列都包含从 1 到 n 的 全部 整数（含 1 和 n），则认为该矩阵是一个 有效 矩阵。
+    /// 给你一个大小为 n x n 的整数矩阵 matrix ，请你判断矩阵是否为一个有效矩阵：如果是，返回 true ；否则，返回 false 。
     /// </summary>
     public class Class5976
     {
