1ad65091e001e9eed92398d89a507c78d05a67b3
ahao214 <haochen214@163.com>
2022-01-09 09:54:04 +0800

    1629. 按键持续时间最长的键

---
+++ Ahao.LeetCode/Simple/demo1629/Class1629.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo1629
+{
+    /// <summary>
+    /// 1629. 按键持续时间最长的键
+    /// </summary>
+    public class Class1629
+    {
+        public char SlowestKey(int[] releaseTimes, string keysPressed)
+        {
+            int longestTime = releaseTimes[0];
+            char res = keysPressed[0];
+            for (int i = 1; i < releaseTimes.Length; i++)
+            {
+                int time = releaseTimes[i] - releaseTimes[i - 1];
+                char key = keysPressed[i];
+                if (time > longestTime || (time == longestTime && key > res))
+                {
+                    res = key;
+                    longestTime = time;
+                }
+            }
+            return res;
+        }
+    }
+}
