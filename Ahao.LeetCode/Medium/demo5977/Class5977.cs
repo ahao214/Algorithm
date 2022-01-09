583c0f0b9ff7c2a4abcbcbf90a6d3eba251eaca7
ahao214 <haochen214@163.com>
2022-01-09 11:01:55 +0800

    5977. 最少交换次数来组合所有的 1 II

---
+++ Ahao.LeetCode/Medium/demo5977/Class5977.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo5977
+{
+    /// <summary>
+    /// 5977. 最少交换次数来组合所有的 1 II
+    /// </summary>
+    public class Class5977
+    {
+        public int MinSwaps(int[] nums)
+        {
+
+        }
+    }
+}
