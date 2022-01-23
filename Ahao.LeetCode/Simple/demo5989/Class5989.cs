f3e794a78ef230f6a2f8aff92087d1f0766b468d
ahao214 <haochen214@163.com>
2022-01-23 10:32:58 +0800

    5989. 元素计数

---
+++ Ahao.LeetCode/Simple/demo5989/Class5989.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo5989
+{
+    /*
+     5989. 元素计数
+     给你一个整数数组 nums ，统计并返回在 nums 中同时具有一个严格较小元素和一个严格较大元素的元素数目。
+
+    输入：nums = [11,7,2,15]
+输出：2
+解释：元素 7 ：严格较小元素是元素 2 ，严格较大元素是元素 11 。
+元素 11 ：严格较小元素是元素 7 ，严格较大元素是元素 15 。
+总计有 2 个元素都满足在 nums 中同时存在一个严格较小元素和一个严格较大元素。
+     */
+    public class Class5989
+    {
+        public int CountElements(int[] nums)
+        {
+
+        }
+    }
+}
