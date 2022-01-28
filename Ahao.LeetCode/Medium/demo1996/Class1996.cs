d7891f4d56b1be1961c899d0ef14beb1fcc405c7
ahao214 <haochen214@163.com>
2022-01-28 09:48:19 +0800

    1996. 游戏中弱角色的数量

---
+++ Ahao.LeetCode/Medium/demo1996/Class1996.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo1996
+{
+    /*
+     1996. 游戏中弱角色的数量
+    你正在参加一个多角色游戏，每个角色都有两个主要属性：攻击 和 防御 。给你一个二维整数数组 properties ，其中 properties[i] = [attacki, defensei] 表示游戏中第 i 个角色的属性。
+
+    如果存在一个其他角色的攻击和防御等级 都严格高于 该角色的攻击和防御等级，则认为该角色为 弱角色 。更正式地，如果认为角色 i 弱于 存在的另一个角色 j ，那么 attackj > attacki 且 defensej > defensei 。
+
+    返回 弱角色 的数量。
+     */
+    public class Class1996
+    {
+        //排序
+        public int NumberOfWeakCharacters(int[][] properties)
+        {
+            Array.Sort(properties, (p1, p2) => {
+                return p1[0] == p2[0] ? (p1[1] - p2[1]) : (p2[0] - p1[0]);
+            });
+
+            int res = 0;
+            int maxDef = 0;
+            foreach (int[] p in properties)
+            {
+                if (p[1] < maxDef)
+                {
+                    res++;
+                }
+                else
+                {
+                    maxDef = p[1];
+                }
+            }
+            return res;
+        }
+    }
+}
