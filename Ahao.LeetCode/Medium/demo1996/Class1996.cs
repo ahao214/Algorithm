bbb164d3eb47c31ed95764b7862f5b899361b20f
ahao214 <haochen214@163.com>
2022-01-28 09:51:33 +0800

    1996. 游戏中弱角色的数量-单调栈

--- Ahao.LeetCode/Medium/demo1996/Class1996.cs
+++ Ahao.LeetCode/Medium/demo1996/Class1996.cs
@@ -38,5 +38,25 @@ namespace Ahao.LeetCode.Medium.demo1996
             }
             return res;
         }
+
+        //单调栈
+        public int NumberOfWeakCharacters2(int[][] properties)
+        {
+            Array.Sort(properties, (p1, p2) => {
+                return p1[0] == p2[0] ? (p2[1] - p1[1]) : (p1[0] - p2[0]);
+            });
+            int res = 0;
+            Stack<int> stk = new Stack<int>();
+            foreach (int[] p in properties)
+            {
+                while (stk.Count > 0 && stk.Peek() < p[1])
+                {
+                    stk.Pop();
+                    res++;
+                }
+                stk.Push(p[1]);
+            }
+            return res;
+        }
     }
 }
