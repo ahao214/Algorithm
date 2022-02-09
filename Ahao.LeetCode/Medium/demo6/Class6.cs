191242137f2f45f04f60037ab43976b84761e7b9
ahao214 <haochen214@163.com>
2022-02-09 22:07:41 +0800

    6. Z 字形变换

--- Ahao.LeetCode/Medium/demo6/Class6.cs
+++ Ahao.LeetCode/Medium/demo6/Class6.cs
@@ -16,7 +16,7 @@ namespace Ahao.LeetCode.Medium.demo6
         {
             if (string.IsNullOrEmpty(s))
                 return string.Empty;
-            if(numRows == 1)
+            if (numRows == 1)
             {
                 return s;
             }
@@ -27,15 +27,16 @@ namespace Ahao.LeetCode.Medium.demo6
             }
             var row = 0;
             var step = 1;
-            for(int i = 0; i < s.Length; i++)
+            for (int i = 0; i < s.Length; i++)
             {
                 rows[row].Append(s[i]);
                 row += step;
-                if(row ==numRows)
+                if (row == numRows)
                 {
                     row = numRows - 2;
                     step = -1;
-                }else if(row == -1)
+                }
+                else if (row == -1)
                 {
                     row = 1;
                     step = 1;
@@ -45,6 +46,44 @@ namespace Ahao.LeetCode.Medium.demo6
             foreach (var line in rows)
             {
                 sb.Append(line);
+            }
+            return sb.ToString();
+        }
+
+        public string Convert1(string s, int numRows)
+        {
+            if (string.IsNullOrEmpty(s))
+                return string.Empty;
+            if (numRows == 1)
+                return s;
+            var k = numRows * 2 - 2;
+            var sb = new StringBuilder(s.Length);
+            var i = 0;
+            while (i < s.Length)
+            {
+                sb.Append(s[i]);
+                i += k;
+            }
+            for (int row = 1; row < numRows - 1; row++)
+            {
+                i = row;
+                var j = k - i;
+                while (i < s.Length)
+                {
+                    sb.Append(s[i]);
+                    if (j < s.Length)
+                    {
+                        sb.Append(s[j]);
+                    }
+                    i += k;
+                    j += k;
+                }
+            }
+            i = numRows - 1;
+            while (i < s.Length)
+            {
+                sb.Append(s[i]);
+                i += k;
             }
             return sb.ToString();
         }
