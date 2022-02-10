119caad7f0a4d5e4ce9b24521de42bf94a8544e5
ahao214 <haochen214@163.com>
2022-02-10 17:03:15 +0800

    9. 回文数

--- Ahao.LeetCode/Simple/demo9/Class9.cs
+++ Ahao.LeetCode/Simple/demo9/Class9.cs
@@ -16,7 +16,23 @@ namespace Ahao.LeetCode.Simple.demo9
     {
         public bool IsPalindrome(int x)
         {
-            return true;
+            if (x < 0)
+                return false;
+            var s = x.ToString();
+            return s.Reverse().SequenceEqual(s);
+        }
+
+        public bool IsPalindrome1(int x)
+        {
+            if (x < 0) return false;
+            if (x % 10 == 0) return x == 0;
+            var r = 0;
+            while (x > r)
+            {
+                r = r * 10 + x % 10;
+                x /= 10;
+            }
+            return x == r || x == r / 10;
         }
     }
 }
