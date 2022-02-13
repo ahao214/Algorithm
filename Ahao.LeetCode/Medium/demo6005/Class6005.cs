9292ea8d8d28ba8fd14d690b2122d6c020bb6553
ahao214 <haochen214@163.com>
2022-02-13 10:49:13 +0800

    6005. 使数组变成交替数组的最少操作数 显示英文描述

---
+++ Ahao.LeetCode/Medium/demo6005/Class6005.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo6005
+{
+    /*
+     6005. 使数组变成交替数组的最少操作数 显示英文描述 
+给你一个下标从 0 开始的数组 nums ，该数组由 n 个正整数组成。
+
+如果满足下述条件，则数组 nums 是一个 交替数组 ：
+
+nums[i - 2] == nums[i] ，其中 2 <= i <= n - 1 。
+nums[i - 1] != nums[i] ，其中 1 <= i <= n - 1 。
+在一步 操作 中，你可以选择下标 i 并将 nums[i] 更改 为 任一 正整数。
+
+返回使数组变成交替数组的 最少操作数 。
+     */
+    public class Class6005
+    {
+        public int MinimumOperations(int[] nums)
+        {
+
+        }
+    }
+}
