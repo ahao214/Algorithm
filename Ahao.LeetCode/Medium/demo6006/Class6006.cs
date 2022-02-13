04f91820cfd47429f6c17ddfcc443eec5af5ce1d
ahao214 <haochen214@163.com>
2022-02-13 10:54:12 +0800

    6006. 拿出最少数目的魔法豆 显示英文描述

---
+++ Ahao.LeetCode/Medium/demo6006/Class6006.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo6006
+{
+    /*
+     6006. 拿出最少数目的魔法豆 显示英文描述 
+给你一个 正 整数数组 beans ，其中每个整数表示一个袋子里装的魔法豆的数目。
+
+请你从每个袋子中 拿出 一些豆子（也可以 不拿出），使得剩下的 非空 袋子中（即 至少 还有 一颗 魔法豆的袋子）魔法豆的数目 相等 。一旦魔法豆从袋子中取出，你不能将它放到任何其他的袋子中。
+
+请你返回你需要拿出魔法豆的 最少数目。
+     */
+    public class Class6006
+    {
+        public long MinimumRemoval(int[] beans)
+        {
+            int res = 0;
+            return res;
+        }
+    }
+}
