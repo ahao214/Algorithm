24579f19aaea53bd4eee035d71499effabedb2b0
ahao214 <haochen214@163.com>
2022-02-14 17:08:45 +0800

    540. 有序数组中的单一元素

---
+++ Ahao.LeetCode/Medium/demo540/Class540.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo540
+{
+    /*
+     540. 有序数组中的单一元素
+给你一个仅由整数组成的有序数组，其中每个元素都会出现两次，唯有一个数只会出现一次。
+
+请你找出并返回只出现一次的那个数。
+
+你设计的解决方案必须满足 O(log n) 时间复杂度和 O(1) 空间复杂度。
+     */
+    public class Class540
+    {
+        public int SingleNonDuplicate(int[] nums)
+        {
+            int res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res ^= nums[i];
+            }
+            return res;
+        }
+    }
+}
