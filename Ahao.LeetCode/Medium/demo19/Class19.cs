86731a365a90cfde1e60775a1badc7ae86211823
ahao214 <haochen214@163.com>
2022-02-16 22:05:28 +0800

    19. 删除链表的倒数第 N 个结点

---
+++ Ahao.LeetCode/Medium/demo19/Class19.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo19
+{
+    /*
+     19. 删除链表的倒数第 N 个结点
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+     */
+    public class Class19
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var dummy = new ListNode(0) { next = head };
+            var p = dummy;
+            var q = p.next;
+            for (int i = 0; i < n; i++)
+            {
+                q = q.next;
+            }
+            while (q != null)
+            {
+                p = p.next;
+                q = q.next;
+            }
+            p.next = p.next.next;
+            return dummy.next;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
