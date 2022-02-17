430771e64947932734d3bffbee54ed40a9f4b4e6
ahao214 <haochen214@163.com>
2022-02-18 00:54:02 +0800

    1791. 找出星型图的中心节点

--- Ahao.LeetCode/Simple/demo1791/Class1791.cs
+++ Ahao.LeetCode/Simple/demo1791/Class1791.cs
@@ -27,5 +27,24 @@ namespace Ahao.LeetCode.Simple.demo1791
             else
                 return -1;
         }
+
+        public int FindCenter1(int[][] edges)
+        {
+            int n = edges.Length + 1;
+            int[] degrees = new int[n + 1];
+            foreach (int[] edge in edges)
+            {
+                degrees[edge[0]]++;
+                degrees[edge[1]]++;
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                if (degrees[i] == n - 1)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
     }
 }
