4a606799485c1f70234898fb2bdd5c80bff0c1b7
ahao214 <haochen214@163.com>
2022-02-20 13:04:20 +0800

    6013. 合并零之间的节点

--- Ahao.LeetCode/Medium/demo6013/Class6013.cs
+++ Ahao.LeetCode/Medium/demo6013/Class6013.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Ahao.LeetCode.Medium
 {
     /*
-     * 6013. 合并零之间的节点
+     6013. 合并零之间的节点
      给你一个链表的头节点 head ，该链表包含由 0 分隔开的一连串整数。链表的 开端 和 末尾 的节点都满足 Node.val == 0 。
 
 对于每两个相邻的 0 ，请你将它们之间的所有节点合并成一个节点，其值是所有已合并节点的值之和。然后将所有 0 移除，修改后的链表不应该含有任何 0 。
@@ -18,39 +18,36 @@ namespace Ahao.LeetCode.Medium
     {
         public ListNode MergeNodes(ListNode head)
         {
-            ListNode dummy = new ListNode();
-
-            int count = 0;
-            while(head !=null)
+            ListNode now = head;
+            while (now != null)
             {
-                count++;
-            }
-            int[] lst = new int[count];
-            for(int i = 0; i < count; i++)
-            {
-                while (head.val != 0)
+                if (now.val == 0)
                 {
-                    lst[i] += head.val;
-                    head = head.next;
+                    now = now.next;
+                    continue;
+                }
+                ListNode nxt = now.next;
+                if (nxt.val != 0)
+                {
+                    now.val += nxt.val;
+                    now.next = nxt.next;
+                }
+                else
+                {
+                    now = now.next;
                 }
             }
-
-            dummy.next = null;
-            ListNode tmp = null;
-            ListNode cur = dummy;
-            for(int i = 0; i < lst.Length; i++)
+            now = head.next;
+            while (now != null)
             {
-                if (lst[i] != 0)
+                ListNode nxt = now.next;
+                if (nxt.val == 0)
                 {
-                    tmp = new ListNode();
-                    tmp.val = lst[i];
-                    tmp.next = null;
-                    cur.next = tmp;
-                    cur = tmp;
-                }               
+                    now.next = nxt.next;
+                }
+                now = now.next;
             }
-
-            return dummy ;
+            return head.next;
         }
     }
 
