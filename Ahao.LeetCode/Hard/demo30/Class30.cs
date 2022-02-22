4db839fb4b7d23b36ebf585e82ef2b01c1bd93d5
ahao214 <haochen214@163.com>
2022-02-22 20:46:21 +0800

    30. 串联所有单词的子串

---
+++ Ahao.LeetCode/Hard/demo30/Class30.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Hard.demo30
+{
+    /*
+    30. 串联所有单词的子串
+给定一个字符串 s 和一些 长度相同 的单词 words 。找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
+
+注意子串要与 words 中的单词完全匹配，中间不能有其他字符 ，但不需要考虑 words 中单词串联的顺序。     
+     */
+    public class Class30
+    {
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return Array.Empty<int>();
+            }
+
+
+            return Array.Empty<int>();
+        }
+    }
+}
