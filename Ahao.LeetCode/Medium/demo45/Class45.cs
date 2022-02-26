5df6d7cd8c8062fd46a15d022ec933429887f775
ahao214 <haochen214@163.com>
2022-02-26 20:53:31 +0800

    45. 跳跃游戏 II

---
+++ Ahao.LeetCode/Medium/demo45/Class45.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo45
+{
+    /*
+     45. 跳跃游戏 II
+给你一个非负整数数组 nums ，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
+假设你总是可以到达数组的最后一个位置。
+     */
+    public class Class45
+    {
+        public int Jump(int[] nums)
+        {
+            if (nums.Length == 1)
+                return 0;
+            var pn = nums[0];
+            var step = 1;
+            var i = 1;
+            while (i < nums.Length - 1)
+            {
+                if (pn >= nums.Length - 1)
+                    break;
+                var pi = pn;
+                while (i <= pi)
+                {
+                    pn = Math.Max(i + nums[i], pn);
+                    i++;
+                }
+                step++;
+                if (pn >= nums.Length - 1)
+                    break;
+            }
+            return step;
+        }
+    }
+}
