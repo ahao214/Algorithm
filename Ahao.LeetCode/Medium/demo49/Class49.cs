161d159b2ddbdfd2c1793711bab56440184d1c6c
ahao214 <haochen214@163.com>
2022-02-28 19:19:39 +0800

    49. 字母异位词分组

---
+++ Ahao.LeetCode/Medium/demo49/Class49.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo49
+{
+    /*
+     49. 字母异位词分组
+给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+
+字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+     */
+    public class Class49
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dic = new Dictionary<string, IList<string>>();
+            IList<IList<string>> res = new List<IList<string>>();
+            for (int i = 0; i < strs.Length; i++)
+            {
+                char[] a = strs[i].ToArray();
+                Array.Sort(a);
+                string str = String.Join("", a.Select(x => x.ToString()).ToArray());
+                if (dic.ContainsKey(str))
+                    dic[str].Add(strs[i]);
+                else
+                    dic[str] = new List<string> { strs[i] };
+            }
+            foreach (var item in dic.Keys)
+            {
+                res.Add(dic[item]);
+            }
+            return res;
+        }
+
+    }
+}
