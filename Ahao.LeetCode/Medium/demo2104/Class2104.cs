ca46a1640f08c1488a4dfe7e343bd52929dd26fc
ahao214 <haochen214@163.com>
2022-03-04 00:36:23 +0800

    2104. 子数组范围和

--- Ahao.LeetCode/Medium/demo2104/Class2104.cs
+++ Ahao.LeetCode/Medium/demo2104/Class2104.cs
@@ -62,5 +62,68 @@ namespace Ahao.LeetCode.Medium
             return ret;
         }
 
+        /// <summary>
+        /// 单调栈
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public long SubArrayRanges2(int[] nums)
+        {
+            int n = nums.Length;
+            int[] minLeft = new int[n];
+            int[] minRight = new int[n];
+            int[] maxLeft = new int[n];
+            int[] maxRight = new int[n];
+            Stack<int> minStack = new Stack<int>();
+            Stack<int> maxStack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                while (minStack.Count > 0 && nums[minStack.Peek()] > nums[i])
+                {
+                    minStack.Pop();
+                }
+                minLeft[i] = minStack.Count == 0 ? -1 : minStack.Peek();
+                minStack.Push(i);
+
+                // 如果 nums[maxStack.Peek()] == nums[i], 那么根据定义，
+                // nums[maxStack.Peek()] 逻辑上小于 nums[i]，因为 maxStack.Peek() < i
+                while (maxStack.Count > 0 && nums[maxStack.Peek()] <= nums[i])
+                {
+                    maxStack.Pop();
+                }
+                maxLeft[i] = maxStack.Count == 0 ? -1 : maxStack.Peek();
+                maxStack.Push(i);
+            }
+            minStack.Clear();
+            maxStack.Clear();
+            for (int i = n - 1; i >= 0; i--)
+            {
+                // 如果 nums[minStack.Peek()] == nums[i], 那么根据定义，
+                // nums[minStack.Peek()] 逻辑上大于 nums[i]，因为 minStack.Peek() > i
+                while (minStack.Count > 0 && nums[minStack.Peek()] >= nums[i])
+                {
+                    minStack.Pop();
+                }
+                minRight[i] = minStack.Count == 0 ? n : minStack.Peek();
+                minStack.Push(i);
+
+                while (maxStack.Count > 0 && nums[maxStack.Peek()] < nums[i])
+                {
+                    maxStack.Pop();
+                }
+                maxRight[i] = maxStack.Count == 0 ? n : maxStack.Peek();
+                maxStack.Push(i);
+            }
+
+            long sumMax = 0, sumMin = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sumMax += (long)(maxRight[i] - i) * (i - maxLeft[i]) * nums[i];
+                sumMin += (long)(minRight[i] - i) * (i - minLeft[i]) * nums[i];
+            }
+            return sumMax - sumMin;
+        }
+
+
     }
 }
