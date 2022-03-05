20ca252f202994bcb11c6ce6290b503e7b8a2aa0
ahao214 <haochen214@163.com>
2022-03-05 21:06:43 +0800

    85. 最大矩形

--- Ahao.LeetCode/Hard/demo85/Class85.cs
+++ Ahao.LeetCode/Hard/demo85/Class85.cs
@@ -7,114 +7,43 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Hard.demo85
 {
-    /// <summary>
-    /// 85. 最大矩形
-    /// </summary>
+    /*
+     85. 最大矩形
+   给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+     */
     public class Class85
     {
         public int MaximalRectangle(char[][] matrix)
         {
-            if (matrix == null || matrix.Length == 0)
+            int m = matrix.Length;
+            if (m == 0) return 0;
+            int n = matrix[0].Length;
+            int[,] cube = new int[m, n];
+            for (int i = 0; i < m; i++)
             {
-                return 0;
-            }
-            var rowCount = matrix.Length;
-            var colCount = matrix[0].Length;
-            var hcount = new int[rowCount, colCount];
-            var vcount = new int[rowCount, colCount];
-
-            for (int i = 0; i < colCount; i++)
-            {
-                vcount[rowCount - 1, i] = 0;
-            }
-
-            for (int i = 0; i < rowCount; i++)
-            {
-                hcount[i, colCount - 1] = 0;
-            }
-
-            for (int i = rowCount - 1; i >= 0; i--)
-            {
-                for (int j = colCount - 1; j >= 0; j--)
+                for (int j = 0; j < n; j++)
                 {
-                    if (matrix[i][j] == '0')
+                    if (matrix[i][j] == '1')
                     {
-                        hcount[i, j] = 0;
-                        vcount[i, j] = 0;
-                    }
-                    else
-                    {
-                        hcount[i, j] = j + 1 == colCount ? 0 : hcount[i, j + 1] + 1;
-                        vcount[i, j] = i + 1 == rowCount ? 0 : vcount[i + 1, j] + 1;
+                        cube[i, j] = j == 0 ? 1 : cube[i, j - 1] + 1;
                     }
                 }
             }
-
-            //var heap = new MinHeap<Item>(new List<Item>(rowCount * colCount), 0, (x, y) => Comparer<int>.Default.Compare(x.Value, y.Value));
-
-
-
-            //for (int i = 0; i < rowCount; i++)
-            //{
-            //    for (int j = 0; j < colCount; j++)
-            //    {
-            //        if (matrix[i][j] != '0')
-            //        {
-            //            heap.Insert(new Item(i * colCount + j, i, j, 1, 1));
-            //        }
-            //    }
-            //}
-
-            var maxValue = 0;
-            //while (heap.Count > 0)
-            //{
-            //    var item = heap.PopRoot();
-            //    var row = item.Row;
-            //    var col = item.Col;
-
-            //    if (hcount[row, col] * vcount[row, col] <= maxValue)
-            //    {
-            //        maxValue = Math.Max(maxValue, item.Area);
-            //    }
-
-            //    if (item.HAble)
-            //    {
-            //        if (item.NextCol < colCount && vcount[row, item.NextCol] >= item.Height)
-            //        {
-            //            heap.Insert(new Item(item.Value, row, col, item.Width + 1, item.Height, true));
-            //        }
-            //    }
-
-            //    if (item.NextRow < rowCount && hcount[item.NextRow, col] >= item.Width)
-            //    {
-            //        heap.Insert(new Item(item.Value, row, col, item.Width, item.Height + 1, true));
-            //    }
-            //}
-            return maxValue;
-        }
-
-        struct Item
-        {
-            public readonly int Value;
-            public readonly int Row;
-            public readonly int Col;
-            public readonly int Height;
-            public readonly int Width;
-            public readonly bool HAble;
-
-            public int NextRow => Row + Width;
-            public int NextCol => Col + Width;
-            public int Area => Width * Height;
-
-            public Item(int value, int row, int col, int height, int width, bool hAble = true)
+            int res = 0;
+            for (int i = 0; i < m; i++)
             {
-                this.Value = value;
-                this.Row = row;
-                this.Col = col;
-                this.Height = height;
-                this.Width = width;
-                this.HAble = hAble;
+                for (int j = 0; j < n; j++)
+                {
+                    int w = cube[i, j];
+                    for (int k = i; k >= 0; k--)
+                    {
+                        if (cube[k, j] == 0) break;
+                        w = w < cube[k, j] ? w : cube[k, j];
+                        res = Math.Max(res, (i - k + 1) * w);
+                    }
+                }
             }
+            return res;
         }
     }
 }
