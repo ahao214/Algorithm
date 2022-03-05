41eab4c77d2502d382e73cefc7cba90ea4d893aa
ahao214 <haochen214@163.com>
2022-03-06 01:10:43 +0800

    2100. 适合打劫银行的日子

--- Ahao.LeetCode/Medium/demo2100/Class2100.cs
+++ Ahao.LeetCode/Medium/demo2100/Class2100.cs
@@ -56,5 +56,58 @@ namespace Ahao.LeetCode.Medium
 
             return res;
         }
+
+        public IList<int> GoodDaysToRobBank1(int[] security, int time)
+        {
+            int n = security.Length;
+            //左边递减
+            int[] left = new int[n];
+            //右边递增
+            int[] right = new int[n];
+            for (int i = 1; i < n; i++)
+            {
+                left[i] = (security[i] <= security[i - 1] ? left[i - 1] + 1 : 0);
+            }
+            for (int i = n - 2; i >= 0; i--)
+            {
+                right[i] = (security[i] <= security[i + 1] ? right[i + 1] + 1 : 0);
+            }
+            List<int> res = new List<int>();
+            for (int i = time; i < n - time; i++)
+            {
+                if (left[i] >= time && right[i] >= time)
+                    res.Add(i);
+            }
+            return res;
+        }
+
+        public IList<int> GoodDaysToRobBank2(int[] security, int time)
+        {
+            int n = security.Length;
+            int[] left = new int[n];
+            int[] right = new int[n];
+            for (int i = 1; i < n; i++)
+            {
+                if (security[i] <= security[i - 1])
+                {
+                    left[i] = left[i - 1] + 1;
+                }
+                if (security[n - i - 1] <= security[n - i])
+                {
+                    right[n - i - 1] = right[n - i] + 1;
+                }
+            }
+
+            IList<int> ans = new List<int>();
+            for (int i = time; i < n - time; i++)
+            {
+                if (left[i] >= time && right[i] >= time)
+                {
+                    ans.Add(i);
+                }
+            }
+            return ans;
+        }
+
     }
 }
