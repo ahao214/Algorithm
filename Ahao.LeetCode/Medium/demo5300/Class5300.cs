7818026149034747a457bcc18262b7abfca73e63
ahao214 <haochen214@163.com>
2022-03-07 00:21:33 +0800

    5300. 有向无环图中一个节点的所有祖先

---
+++ Ahao.LeetCode/Medium/demo5300/Class5300.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo5300
+{
+    /*
+     5300. 有向无环图中一个节点的所有祖先
+给你一个正整数 n ，它表示一个 有向无环图 中节点的数目，节点编号为 0 到 n - 1 （包括两者）。
+
+给你一个二维整数数组 edges ，其中 edges[i] = [fromi, toi] 表示图中一条从 fromi 到 toi 的单向边。
+
+请你返回一个数组 answer，其中 answer[i]是第 i 个节点的所有 祖先 ，这些祖先节点 升序 排序。
+
+如果 u 通过一系列边，能够到达 v ，那么我们称节点 u 是节点 v 的 祖先 节点。
+     */
+    public class Class5300
+    {
+        //public IList<IList<int>> GetAncestors(int n, int[][] edges)
+        //{
+
+        //}
+    }
+}
