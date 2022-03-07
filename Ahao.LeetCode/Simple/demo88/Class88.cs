2a210c180d3c65146d9c2d7152b565995c9167ae
ahao214 <haochen214@163.com>
2022-03-07 18:30:53 +0800

    88. 合并两个有序数组

--- Ahao.LeetCode/Simple/demo88/Class88.cs
+++ Ahao.LeetCode/Simple/demo88/Class88.cs
@@ -16,6 +16,15 @@ namespace Ahao.LeetCode.Simple.demo88
      */
     public class Class88
     {
-
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            var i = m + n - 1;
+            n--;
+            m--;
+            while (n >= 0)
+            {
+                nums1[i--] = m < 0 || nums2[n] >= nums1[m] ? nums2[n--] : nums1[m--];
+            }
+        }
     }
 }
