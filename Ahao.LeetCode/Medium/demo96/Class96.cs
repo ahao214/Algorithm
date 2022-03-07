62b17bfc473f422a44a7a07dfdccf1aa5c1e88e3
ahao214 <haochen214@163.com>
2022-03-07 19:11:57 +0800

    96. 不同的二叉搜索树

---
+++ Ahao.LeetCode/Medium/demo96/Class96.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo96
+{
+    /*
+     96. 不同的二叉搜索树
+给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+     */
+    public class Class96
+    {
+        public int NumTrees(int n)
+        {
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    dp[i] += dp[j - 1] * dp[i - j];
+                }
+            }
+            return dp[n];
+        }
+    }
+}
