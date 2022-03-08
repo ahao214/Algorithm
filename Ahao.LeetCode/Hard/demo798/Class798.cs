b616873973672063f39e30a9e696123626cfefc8
ahao214 <haochen214@163.com>
2022-03-09 00:51:28 +0800

    798. 得分最高的最小轮调

--- Ahao.LeetCode/Hard/demo798/Class798.cs
+++ Ahao.LeetCode/Hard/demo798/Class798.cs
@@ -81,7 +81,35 @@ namespace Ahao.LeetCode.Hard
             return result;
         }
 
-
+        public int BestRotation2(int[] nums)
+        {
+            int n = nums.Length;
+            int[] diffs = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int low = (i + 1) % n;
+                int high = (i - nums[i] + n + 1) % n;
+                diffs[low]++;
+                diffs[high]--;
+                if (low >= high)
+                {
+                    diffs[0]++;
+                }
+            }
+            int bestIndex = 0;
+            int maxScore = 0;
+            int score = 0;
+            for (int i = 0; i < n; i++)
+            {
+                score += diffs[i];
+                if (score > maxScore)
+                {
+                    bestIndex = i;
+                    maxScore = score;
+                }
+            }
+            return bestIndex;
+        }
 
     }
 }
