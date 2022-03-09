fdedc9665de573020efcd26e426d95baaaa0f794
ahao214 <haochen214@163.com>
2022-03-09 12:57:23 +0800

    100. 相同的树

---
+++ Ahao.LeetCode/Simple/demo100/Class100.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo100
+{
+    /*
+     100. 相同的树
+给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+     */
+    public class Class100
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null)
+                return q == null;
+            if (q == null)
+                return p == null;
+            return p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
