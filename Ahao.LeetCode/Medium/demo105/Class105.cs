cd64eb88fdae187da932868dcf32359f1e0fb46a
ahao214 <haochen214@163.com>
2022-03-09 13:24:02 +0800

    105. 从前序与中序遍历序列构造二叉树

--- Ahao.LeetCode/Medium/demo105/Class105.cs
+++ Ahao.LeetCode/Medium/demo105/Class105.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Medium.demo105
 {
-    /// <summary>
-    /// 105. 从前序与中序遍历序列构造二叉树
-    /// </summary>
+    /*
+     105. 从前序与中序遍历序列构造二叉树
+给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+     */
     public class Class105
     {
         public TreeNode BuildTree(int[] preorder, int[] inorder)
