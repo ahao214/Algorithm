1bd8a36a66c69e52657fcd78e63d9a1dc7a51605
ahao214 <haochen214@163.com>
2022-03-10 20:01:29 +0800

    109. 有序链表转换二叉搜索树

--- Ahao.LeetCode/Medium/demo109/Class109.cs
+++ Ahao.LeetCode/Medium/demo109/Class109.cs
@@ -6,11 +6,38 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Medium.demo109
 {
-    /// <summary>
-    /// 109. 有序链表转换二叉搜索树
-    /// </summary>
+    /*
+     109. 有序链表转换二叉搜索树
+给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+     */
     public class Class109
     {
+        public TreeNode SortedListToBST(ListNode head)
+        {
+            var count = 0;
+            var node = head;
+            while (node != null)
+            {
+                count++;
+                node = node.next;
+            }
+            return SortedListToBST(ref head, count);
+        }
+
+        private TreeNode SortedListToBST(ref ListNode head, int count)
+        {
+            if (count == 0)
+                return null;
+            var left = SortedListToBST(ref head, count / 2);
+            var root = new TreeNode(head.val) {
+                left = left
+            };
+            head = head.next;
+            root.right = SortedListToBST(ref head, count - count / 2 - 1);
+            return root;
+        }
     }
 
     public class TreeNode
