85f82b3972baab845f5eba9f1f0bfebe25b3f099
ahao214 <haochen214@163.com>
2022-03-10 20:11:05 +0800

    111. 二叉树的最小深度

---
+++ Ahao.LeetCode/Simple/demo111/Class111.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo111
+{
+    /*
+     111. 二叉树的最小深度
+给定一个二叉树，找出其最小深度。
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+说明：叶子节点是指没有子节点的节点。
+     */
+    public class Class111
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            if (root.left == null)
+                return MinDepth(root.right) + 1;
+            if (root.right == null)
+                return MinDepth(root.left) + 1;
+            return Math.Min(MinDepth(root.left), MinDepth(root.right)) + 1;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
