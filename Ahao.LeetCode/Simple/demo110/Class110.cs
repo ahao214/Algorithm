afdaa4d7370f951a92e116e7764fbf1776b32531
ahao214 <haochen214@163.com>
2022-03-10 20:03:45 +0800

    110. 平衡二叉树

--- Ahao.LeetCode/Simple/demo110/Class110.cs
+++ Ahao.LeetCode/Simple/demo110/Class110.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Simple.demo110
 {
-    /// <summary>
-    /// 110. 平衡二叉树
-    /// </summary>
+    /*
+     110. 平衡二叉树
+给定一个二叉树，判断它是否是高度平衡的二叉树。
+本题中，一棵高度平衡二叉树定义为：
+一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+     */
     public class Class110
     {
         public bool IsBalanced(TreeNode root)
