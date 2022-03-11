9adb546dc59d9d9bcbd85e6831bfe1397aab9631
ahao214 <haochen214@163.com>
2022-03-11 21:28:03 +0800

    114. 二叉树展开为链表

---
+++ Ahao.LeetCode/Medium/demo114/Class114.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Medium.demo114
+{
+    /*
+     114. 二叉树展开为链表
+给你二叉树的根结点 root ，请你将它展开为一个单链表：
+
+展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+展开后的单链表应该与二叉树 先序遍历 顺序相同。
+     */
+    public class Class114
+    {
+        public void Flatten(TreeNode root)
+        {
+            if (root == null) return;
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+            TreeNode last = null;
+            while (stack.Count > 0)
+            {
+                root = stack.Pop();
+                if (root.right != null)
+                {
+                    stack.Push(root.right);
+                }
+                if (root.left != null)
+                {
+                    stack.Push(root.left);
+                }
+                if (last != null)
+                {
+                    last.right = root;
+                    last.left = null;
+                }
+                last = root;
+            }
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
