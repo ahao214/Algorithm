7f5bd8dd337138e3fbbfb743f3e41ec73ba4b9c6
ahao214 <haochen214@163.com>
2022-03-17 10:09:59 +0800

    720. 词典中最长的单词

--- Ahao.LeetCode/Simple/demo720/Class720.cs
+++ Ahao.LeetCode/Simple/demo720/Class720.cs
@@ -53,5 +53,82 @@ namespace Ahao.LeetCode.Simple.demo720
             return longest;
         }
 
+
+        /// <summary>
+        /// 字典树
+        /// </summary>
+        /// <param name="words"></param>
+        /// <returns></returns>
+        public string LongestWord1(string[] words)
+        {
+            Trie trie = new Trie();
+            foreach (string word in words)
+            {
+                trie.Insert(word);
+            }
+            string longest = "";
+            foreach (string word in words)
+            {
+                if (trie.Search(word))
+                {
+                    if (word.Length > longest.Length || (word.Length == longest.Length && word.CompareTo(longest) < 0))
+                    {
+                        longest = word;
+                    }
+                }
+            }
+            return longest;
+        }
+    }
+
+    class Trie
+    {
+        public Trie[] Children
+        {
+            get; set;
+        }
+        public bool IsEnd
+        {
+            get; set;
+        }
+
+        public Trie()
+        {
+            Children = new Trie[26];
+            IsEnd = false;
+        }
+
+        public void Insert(string word)
+        {
+            Trie node = this;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char ch = word[i];
+                int index = ch - 'a';
+                if (node.Children[index] == null)
+                {
+                    node.Children[index] = new Trie();
+                }
+                node = node.Children[index];
+            }
+            node.IsEnd = true;
+        }
+
+        public bool Search(string word)
+        {
+            Trie node = this;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char ch = word[i];
+                int index = ch - 'a';
+                if (node.Children[index] == null || !node.Children[index].IsEnd)
+                {
+                    return false;
+                }
+                node = node.Children[index];
+            }
+            return node != null && node.IsEnd;
+        }
+
     }
 }
