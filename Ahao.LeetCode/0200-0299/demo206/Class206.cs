dd987f394cca218743a1b9a1258ad447a5db85c1
ahao214 <haochen214@163.com>
2022-03-18 21:26:43 +0800

    206. 反转链表

---
+++ Ahao.LeetCode/0200-0299/demo206/Class206.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0200_0299.demo206
+{
+    /*
+     206. 反转链表
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+     */
+    public class Class206
+    {
+        /// <summary>
+        /// 迭代法
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null, next;
+            ListNode cur = head;
+            while (cur != null)
+            {
+                next = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+            }
+            return prev;
+        }
+
+        /// <summary>
+        /// 递归
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode ReverseList1(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+            ListNode newHead = ReverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return newHead;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
