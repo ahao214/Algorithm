608b47b33e38c896949df300310681b713d5d7cd
ahao214 <haochen214@163.com>
2022-03-18 16:10:31 +0800

    15. 二维数组中的查找

---
+++ Ahao.AcWing/0001-0099/demo0015/Class0015.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.AcWing._0001_0099.demo0015
+{
+    /*
+     15. 二维数组中的查找
+
+在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+
+请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+
+数据范围
+二维数组中元素个数范围 [0,1000]
+样例
+输入数组：
+
+[
+  [1,2,8,9]，
+  [2,4,9,12]，
+  [4,7,10,13]，
+  [6,8,11,15]
+]
+
+如果输入查找数值为7，则返回true，
+
+如果输入查找数值为5，则返回false。
+     */
+    public class Class0015
+    {
+    }
+}
