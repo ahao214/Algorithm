0ace7d33fb0a759e22b182403ce4823ecc6ebc75
ahao214 <haochen214@163.com>
2022-03-19 10:22:57 +0800

    219. 存在重复元素 II

---
+++ Ahao.LeetCode/0200-0299/demo219/JS219.js
@@ -0,0 +1,29 @@
+﻿//方法一：哈希表
+var containsNearbyDuplicate = function (nums, k) {
+    const map = new Map();
+    const length = nums.length;
+    for (let i = 0; i < length; i++) {
+        const num = nums[i];
+        if (map.has(num) && i - map.get(num) <= k) {
+            return true;
+        }
+        map.set(num, i);
+    }
+    return false;
+};
+
+//方法二：滑动窗口
+var containsNearbyDuplicate1 = function (nums, k) {
+    const set = new Set();
+    const length = nums.length;
+    for (let i = 0; i < length; i++) {
+        if (i > k) {
+            set.delete(nums[i - k - 1]);
+        }
+        if (set.has(nums[i])) {
+            return true;
+        }
+        set.add(nums[i])
+    }
+    return false;
+};
