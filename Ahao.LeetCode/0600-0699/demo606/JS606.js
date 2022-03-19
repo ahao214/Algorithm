3dd62c461e4ac01207cbcd11b4a1c13c4907d0dc
ahao214 <haochen214@163.com>
2022-03-19 10:03:15 +0800

    606. 根据二叉树创建字符串

--- Ahao.LeetCode/0600-0699/demo606/JS606.js
+++ Ahao.LeetCode/0600-0699/demo606/JS606.js
@@ -20,3 +20,34 @@ var tree2str = function (root) {
     }
     return root.val + '(' + tree2str(root.left) + ')(' + tree2str(root.right) + ')';
 };
+
+var treeToStr = function (root) {
+    let ans = '';
+    const st = [root];
+    const vis = new Set();
+    while (st.length) {
+        const node = st[st.length - 1];
+        if (vis.has(node)) {
+            if (node !== root) {
+                ans += ')';
+            }
+            st.pop();
+        } else {
+            vis.add(node);
+            if (node !== root) {
+                ans += '(';
+            }
+            ans += '' + node.val;;
+            if (!node.left && node.right) {
+                ans += '()';
+            }
+            if (node.right) {
+                st.push(node.right);
+            }
+            if (node.left) {
+                st.push(node.left);
+            }
+        }
+    }
+    return ans;
+};
