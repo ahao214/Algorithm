71515ae7ff427f89217754adf9d779a4e1bce29f
ahao214 <haochen214@163.com>
2022-03-19 09:45:21 +0800

    606. 根据二叉树创建字符串

---
+++ Ahao.LeetCode/0600-0699/demo606/Class606.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0600_0699.demo606
+{
+    public class Class606
+    {
+        public string Tree2str(TreeNode root)
+        {
+            if (root == null)
+                return "";
+            if (root.left == null && root.right == null)
+                return root.val.ToString();
+            StringBuilder sb = new StringBuilder();
+            if (root.right == null)
+            {
+                return sb.Append(root.val).Append("(").Append(Tree2str(root.left)).Append(")").ToString();
+            }
+            return sb.Append(root.val).Append("(").Append(Tree2str(root.left)).Append(")(").Append(Tree2str(root.right)).Append(")").ToString();
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right  null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0600-0699/demo606/JS606.js
@@ -0,0 +1,13 @@
+﻿function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var tree2str = function (root) {
+
+};
