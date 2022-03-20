c524b1758e9a9a7f4ea13ccba3d6a404ebf2a07e
ahao214 <haochen214@163.com>
2022-03-20 17:47:01 +0800

    1. 两数之和

--- Ahao.LeetCode/0001-0099/demo01/Class1.cs
+++ Ahao.LeetCode/0001-0099/demo01/Class1.cs
@@ -46,5 +46,68 @@ namespace Ahao.LeetCode.Simple
             }
             return Array.Empty<int>();
         }
+
+
+        #region 两数之和-升序排列的数组
+        /// <summary>
+        /// 二分方法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum3(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int low = i, high = nums.Length - 1;
+                while (low <= high)
+                {
+                    int mid = low + (high - low) / 2;
+                    if (nums[mid] == target - nums[i])
+                    {
+                        return new int[] { i, mid };
+                    }
+                    else if (nums[mid] > target - nums[i])
+                    {
+                        high = mid - 1;
+                    }
+                    else
+                    {
+                        low = mid + 1;
+                    }
+                }
+            }
+            return new int[] { 0 };
+        }
+
+        /// <summary>
+        /// 双指针
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum4(int[] nums, int target)
+        {
+            int low = 0, high = nums.Length - 1;
+            while (low <= high)
+            {
+                int sum = nums[low] + nums[high];
+                if (sum == target)
+                {
+                    return new int[] { low, high };
+                }
+                else if (sum < target)
+                {
+                    low++;
+                }
+                else
+                {
+                    high--;
+                }
+            }
+            return new int[] { 0 };
+        }
+
+        #endregion
     }
 }
