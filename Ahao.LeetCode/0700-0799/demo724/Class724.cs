8b2a6880d76078310d279ab8346f9bc191d7ade0
ahao214 <haochen214@163.com>
2022-03-20 17:13:51 +0800

    724. 寻找数组的中心下标

---
+++ Ahao.LeetCode/0700-0799/demo724/Class724.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo724
+{
+    public class Class724
+    {
+        public int PivotIndex(int[] nums)
+        {
+            int sum = nums.Sum();
+            int total = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                total += nums[i];
+                if (total == sum)
+                    return i;
+                sum = sum - nums[i];
+            }
+            return -1;
+        }
+    }
+}
