6de32f3a8c1de9e0fb0b2f6ea724180161b1b6b1
ahao214 <haochen214@163.com>
2022-03-20 17:00:16 +0800

    521. 最长特殊序列 Ⅰ

---
+++ Ahao.LeetCode/0500-0599/demo559/JS559.js
@@ -0,0 +1,43 @@
+﻿// Definition for a Node.
+function Node(val, children) {
+    this.val = val;
+    this.children = children;
+};
+
+
+//深度优先搜索
+var maxDepth = function (root) {
+    if (!root) {
+        return 0;
+    }
+    let maxChildDepth = 0;
+    const children = root.children;
+    for (const child of children) {
+        const childDepth = maxDepth(child);
+        maxChildDepth = Math.max(maxChildDepth, childDepth);
+    }
+    return maxChildDepth + 1;
+};
+
+//广度优先搜索
+var maxDepth2 = function (root) {
+    if (!root) {
+        return 0;
+    }
+    const queue = [];
+    queue.push(root);
+    let ans = 0;
+    while (queue.length) {
+        let size = queue.length;
+        while (size > 0) {
+            const node = queue.shift();
+            const children = node.children;
+            for (const child of children) {
+                queue.push(child);
+            }
+            size--;
+        }
+        ans++;
+    }
+    return ans;
+};
