83f8e2a017eb246b3b3c151e7c7681a20323f69c
ahao214 <haochen214@163.com>
2022-03-20 09:32:13 +0800

    2039. 网络空闲的时刻

---
+++ Ahao.LeetCode/2000-2099/demo2039/Class2039.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2000_2099.demo2039
+{
+    public class Class2039
+    {
+        /// <summary>
+        /// 广度优先
+        /// </summary>
+        /// <param name="edges"></param>
+        /// <param name="patience"></param>
+        /// <returns></returns>
+        public int NetworkBecomesIdle(int[][] edges, int[] patience)
+        {
+            int n = patience.Length;
+            IList<int>[] adj = new IList<int>[n];
+            for (int i = 0; i < n; ++i)
+            {
+                adj[i] = new List<int>();
+            }
+            bool[] visit = new bool[n];
+            foreach (int[] v in edges)
+            {
+                adj[v[0]].Add(v[1]);
+                adj[v[1]].Add(v[0]);
+            }
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(0);
+            visit[0] = true;
+            int dist = 1;
+            int ans = 0;
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    int curr = queue.Dequeue();
+                    foreach (int v in adj[curr])
+                    {
+                        if (visit[v])
+                        {
+                            continue;
+                        }
+                        queue.Enqueue(v);
+                        int time = patience[v] * ((2 * dist - 1) / patience[v]) + 2 * dist + 1;
+                        ans = Math.Max(ans, time);
+                        visit[v] = true;
+                    }
+                }
+                dist++;
+            }
+            return ans;
+        }
+
+    }
+}
