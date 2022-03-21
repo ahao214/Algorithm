e1fdb8bec13f89afd98b54087f361f8e69a8a26e
ahao214 <haochen214@163.com>
2022-03-21 13:24:50 +0800

    653. 两数之和 IV - 输入 BST

--- Ahao.LeetCode/0600-0699/demo653/JS653.js
+++ Ahao.LeetCode/0600-0699/demo653/JS653.js
@@ -1,1 +1,27 @@
-﻿
+﻿function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+
+//深度优先+哈希表
+var findTarget = function (root, k) {
+    const set = new Set();
+    const helper = (root, k) => {
+        if (!root) {
+            return fasle;
+        }
+        if (set.has(k - root.val)) {
+            return true;
+        }
+        set.add(root.val);
+        return helper(root.left, k) || helper(root.right, k);
+    }
+    return helper(root, k);
+};
