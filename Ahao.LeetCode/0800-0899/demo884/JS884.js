70db3af7d2d34178be55e3f84070823ebdb2a95f
ahao214 <haochen214@163.com>
2022-03-21 18:09:39 +0800

    884. 两句话中的不常见单词

---
+++ Ahao.LeetCode/0800-0899/demo884/JS884.js
@@ -0,0 +1,21 @@
+﻿var uncommonFromSentences = function (s1, s2) {
+    let freq = new Map();
+    freq = insert(s1, freq);
+    freq = insert(s2, freq);
+
+    const ans = [];
+    for (const entry of freq.entries()) {
+        if (entry[1] === 1) {
+            ans.push(entry[0]);
+        }
+    }
+    return ans;
+};
+
+const insert = (s, freq) => {
+    const arr = s.split(" ");
+    for (const word of arr) {
+        freq.set(word, (freq.get(word) || 0) + 1);
+    }
+    return freq;
+};
