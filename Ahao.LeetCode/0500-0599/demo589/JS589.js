f02c2c8978d5e91d56254b20e27b6eb482aee29b
ahao214 <haochen214@163.com>
2022-03-21 13:55:54 +0800

    589. N 叉树的前序遍历

---
+++ Ahao.LeetCode/0500-0599/demo589/JS589.js
@@ -0,0 +1,40 @@
+﻿function Node(val, children) {
+    this.val = val;
+    this.children = children;
+};
+
+var preorder = function (root) {
+    const res = [];
+    helper(root, res);
+    return res;
+}
+
+const helper = (root, res) => {
+    if (root === null) {
+        return;
+    }
+    res.push(root.val);
+    for (const ch of root.children) {
+        helper(ch, res);
+    }
+};
+
+
+//迭代优化
+var preorder1 = function (root) {
+    const res = [];
+    if (root == null) {
+        return res;
+    }
+
+    const stack = [];
+    stack.push(root);
+    while (stack.length) {
+        const node = stack.pop();
+        res.push(node.val);
+        for (let i = node.children.length - 1; i >= 0; --i) {
+            stack.push(node.children[i]);
+        }
+    }
+    return res;
+};
