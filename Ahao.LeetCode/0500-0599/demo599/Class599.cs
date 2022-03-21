b55232313c95c1ba70e21b5af811a473f5af3998
ahao214 <haochen214@163.com>
2022-03-21 14:02:39 +0800

    599. 两个列表的最小索引总和

--- Ahao.LeetCode/0500-0599/demo599/Class599.cs
+++ Ahao.LeetCode/0500-0599/demo599/Class599.cs
@@ -6,16 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Simple.demo599
 {
-    /*
-599. 两个列表的最小索引总和
-假设 Andy 和 Doris 想在晚餐时选择一家餐厅，
-并且他们都有一个表示最喜爱餐厅的列表，
-每个餐厅的名字用字符串表示。
-
-你需要帮助他们用最少的索引和找出他们共同喜爱的餐厅。 
-如果答案不止一个，则输出所有答案并且不考虑顺序。
- 你可以假设答案总是存在。
-*/
     public class Class599
     {
         public string[] FindRestaurant(string[] list1, string[] list2)
