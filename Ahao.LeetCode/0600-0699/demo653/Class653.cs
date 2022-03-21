835a4bd18deef94ea5501a2cd6977e97c6328776
ahao214 <haochen214@163.com>
2022-03-21 13:21:47 +0800

    653. 两数之和 IV - 输入 BST

--- Ahao.LeetCode/0600-0699/demo653/Class653.cs
+++ Ahao.LeetCode/0600-0699/demo653/Class653.cs
@@ -9,8 +9,22 @@ namespace Ahao.LeetCode._0600_0699.demo653
     public class Class653
     {
         ISet<int> set = new HashSet<int>();
-        public bool FindTarget()
+        /// <summary>
+        /// 深度优先+哈希表
+        /// </summary>
+        /// <returns></returns>
+        public bool FindTarget(TreeNode root, int k)
         {
+            if (root == null)
+            {
+                return false;
+            }
+            if (set.Contains(k - root.val))
+            {
+                return true;
+            }
+            set.Add(root.val);
+            return FindTarget(root.left, k) || FindTarget(root.right, k);
         }
 
 
