023d936c72f7ec699532848bb5089196d961a90f
ahao214 <haochen214@163.com>
2022-03-22 22:32:43 +0800

    717. 1比特与2比特字符

--- Ahao.LeetCode/0700-0799/demo717/Class717.cs
+++ Ahao.LeetCode/0700-0799/demo717/Class717.cs
@@ -6,14 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Simple.demo717
 {
-    /*
-     717. 1比特与2比特字符
-有两种特殊字符：
-
-第一种字符可以用一个比特 0 来表示
-第二种字符可以用两个比特(10 或 11)来表示、
-给定一个以 0 结尾的二进制数组 bits ，如果最后一个字符必须是一位字符，则返回 true 。
-     */
     public class Class717
     {
         public bool IsOneBitCharacter(int[] bits)
---
+++ Ahao.LeetCode/0700-0799/demo717/JS717.js
@@ -0,0 +1,7 @@
+﻿var isOneBitCharacter = function (bits) {
+    let i = 0, n = bits.length;
+    while (i < n - 1) {
+        i += bits[i] + 1;
+    }
+    return i === n - 1;
+};
