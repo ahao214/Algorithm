d764c7430b749e4a80fda1c5536218eb9ba8a9eb
ahao214 <haochen214@163.com>
2022-03-22 21:24:35 +0800

    2038. 如果相邻两个颜色均相同则删除当前颜色

--- Ahao.LeetCode/2000-2099/demo2038/Class2038.cs
+++ Ahao.LeetCode/2000-2099/demo2038/Class2038.cs
@@ -33,5 +33,30 @@ namespace Ahao.LeetCode._2000_2099.demo2038
             }
             return na > nb;
         }
+
+        public bool WinnerOfGame2(string colors)
+        {
+            int[] freq = { 0, 0 };
+            char cur = 'C';
+            int cnt = 0;
+            foreach (char c in colors)
+            {
+                if (c != cur)
+                {
+                    cur = c;
+                    cnt = 1;
+                }
+                else
+                {
+                    cnt += 1;
+                    if (cnt >= 3)
+                    {
+                        freq[cur - 'A'] += 1;
+                    }
+                }
+            }
+            return freq[0] > freq[1];
+        }
+
     }
 }
