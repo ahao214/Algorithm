c9fa0563e68568a7d9a770a2bf0cfd0b3dccb0ef
ahao214 <haochen214@163.com>
2022-03-22 14:24:56 +0800

    2038. 如果相邻两个颜色均相同则删除当前颜色

--- Ahao.LeetCode/2000-2099/demo2038/Class2038.cs
+++ Ahao.LeetCode/2000-2099/demo2038/Class2038.cs
@@ -6,11 +6,32 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode._2000_2099.demo2038
 {
- public   class Class2038
+    public class Class2038
     {
         public bool WinnerOfGame(string colors)
         {
-
+            int len = colors.Length;
+            int na = 0, nb = 0, ta = 0, tb = 0;
+            for (int i = 0; i < len; i++)
+            {
+                if (colors[i] == 'A')
+                {
+                    if (++ta >= 3)
+                    {
+                        na++;
+                    }
+                    tb = 0;
+                }
+                else
+                {
+                    if (++tb>= 3)
+                    {
+                        nb++;
+                    }
+                    ta = 0;
+                }
+            }
+            return na > nb;
         }
     }
 }
