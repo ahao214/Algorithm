e97edf4443d5f9d94850db657c91072933506f7d
ahao214 <haochen214@163.com>
2022-03-23 14:00:53 +0800

    1342. 将数字变成 0 的操作次数

--- Ahao.LeetCode/1300-1399/demo1342/Class1342.cs
+++ Ahao.LeetCode/1300-1399/demo1342/Class1342.cs
@@ -6,10 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Simple.demo1342
 {
-    /*
-    1342. 将数字变成 0 的操作次数
-    给你一个非负整数 num ，请你返回将它变成 0 所需要的步数。 如果当前数字是偶数，你需要把它除以 2 ；否则，减去 1 。
-     */
     public class Class1342
     {
         public int NumberOfSteps(int num)
