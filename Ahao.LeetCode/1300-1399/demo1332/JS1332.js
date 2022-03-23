4e84648b6ef39581e182d2a034547e4ec1e95e86
ahao214 <haochen214@163.com>
2022-03-23 13:58:28 +0800

    1332. 删除回文子序列

---
+++ Ahao.LeetCode/1300-1399/demo1332/JS1332.js
@@ -0,0 +1,9 @@
+﻿var removePalindromeSub = function (s) {
+    const n = s.length;
+    for (let i = 0; i < Math.floor(n / 2); ++i) {
+        if (s[i] !== s[n - 1 - i]) {
+            return 2;
+        }
+    }
+    return 1;
+};
