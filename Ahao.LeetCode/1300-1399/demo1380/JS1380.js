bdb2eb8bc63e2ed6dbd456d864a4083e26d93aba
ahao214 <haochen214@163.com>
2022-03-23 14:02:33 +0800

    1380. 矩阵中的幸运数

---
+++ Ahao.LeetCode/1300-1399/demo1380/JS1380.js
@@ -0,0 +1,53 @@
+﻿var luckyNumbers = function (matrix) {
+    const m = matrix.length,
+        n = matrix[0].length;
+    const ret = [];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            let isMin = true,
+                isMax = true;
+            for (let k = 0; k < n; k++) {
+                if (matrix[i][k] < matrix[i][j]) {
+                    isMin = false;
+                    break;
+                }
+            }
+            if (!isMin) {
+                continue;
+            }
+            for (let k = 0; k < m; k++) {
+                if (matrix[k][j] > matrix[i][j]) {
+                    isMax = false;
+                    break;
+                }
+            }
+            if (isMax) {
+                ret.push(matrix[i][j]);
+            }
+        }
+    }
+    return ret;
+};
+
+//方法二：预处理
+var luckyNumbers1 = function (matrix) {
+    const m = matrix.length,
+        n = matrix[0].length;
+    const minRow = new Array(m).fill(Number.MAX_SAFE_INTEGER);
+    const maxCol = new Array(n).fill(0);
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            minRow[i] = Math.min(minRow[i], matrix[i][j]);
+            maxCol[j] = Math.max(maxCol[j], matrix[i][j]);
+        }
+    }
+    const ret = [];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === minRow[i] && matrix[i][j] === maxCol[j]) {
+                ret.push(matrix[i][j]);
+            }
+        }
+    }
+    return ret;
+};
