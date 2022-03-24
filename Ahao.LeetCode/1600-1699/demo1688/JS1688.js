ede4bb73f6ca48b195add8b90fe3d10db7234078
ahao214 <haochen214@163.com>
2022-03-24 14:05:06 +0800

    1688. 比赛中的配对次数

---
+++ Ahao.LeetCode/1600-1699/demo1688/JS1688.js
@@ -0,0 +1,13 @@
+﻿var numberOfMatches = function (n) {
+    let ans = 0;
+    while (n > 1) {
+        if (n % 2 === 0) {
+            ans += Math.floor(n / 2);
+            n /= 2;
+        } else {
+            ans += Math.floor((n - 1) / 2);
+            n = Math.floor((n - 1) / 2) + 1;
+        }
+    }
+    return ans;
+};
