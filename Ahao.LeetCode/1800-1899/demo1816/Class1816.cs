79cea45e723e09b8a34922b7d73e9bc1c2df36ea
ahao214 <haochen214@163.com>
2022-03-24 14:07:14 +0800

    1816. 截断句子

--- Ahao.LeetCode/1800-1899/demo1816/Class1816.cs
+++ Ahao.LeetCode/1800-1899/demo1816/Class1816.cs
@@ -6,9 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Simple.demo1816
 {
-    /// <summary>
-    /// 1816. 截断句子
-    /// </summary>
     public class Class1816
     {
         public string TruncateSentence(string s, int k)
---
+++ Ahao.LeetCode/1800-1899/demo1816/JS1816.js
@@ -0,0 +1,20 @@
+﻿/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function (s, k) {
+    const n = s.length;
+    let end = 0,
+        count = 0;
+    for (let i = 1; i <= n; i++) {
+        if (i === n || s[i] === ' ') {
+            count++;
+            if (count === k) {
+                end = i;
+                break;
+            }
+        }
+    }
+    return s.slice(0, end);
+};
