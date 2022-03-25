580e81ac1de1aa121953c5e1bb8988cae161f000
ahao214 <haochen214@163.com>
2022-03-25 10:45:11 +0800

    172. 阶乘后的零

---
+++ Ahao.LeetCode/0100-0199/demo172/Class172.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0100_0199.demo172
+{
+    public class Class172
+    {
+        public int TrailingZeroes(int n)
+        {
+            int ans = 0;
+            for (int i = 5; i <= n; i += 5)
+            {
+                for (int x = i; x % 5 == 0; x /= 5)
+                {
+                    ++ans;
+                }
+            }
+            return ans;
+        }
+
+        public int TrailingZeroes2(int n)
+        {
+            int ans = 0;
+            while (n != 0)
+            {
+                n /= 5;
+                ans += n;
+            }
+            return ans;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0100-0199/demo172/JS172.js
@@ -0,0 +1,18 @@
+﻿var trailingZeroes = function (n) {
+    let ans = 0;
+    for (let i = 5; i <= n; i += 5) {
+        for (let x = i; x % 5 == 0; x /= 5) {
+            ++ans;
+        }
+    }
+    return ans;
+}
+
+var trailingZeroes2 = function (n) {
+    let ans = 0;
+    while (n != 0) {
+        n = Math.floor(n / 5);
+        ans += n;
+    }
+    return ans;
+}
