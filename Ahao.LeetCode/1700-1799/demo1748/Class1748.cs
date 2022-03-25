458436ad815d3da8cd04b9d4084c2b72f7ffa2fb
ahao214 <haochen214@163.com>
2022-03-25 12:34:23 +0800

    1748. 唯一元素的和

--- Ahao.LeetCode/1700-1799/demo1748/Class1748.cs
+++ Ahao.LeetCode/1700-1799/demo1748/Class1748.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Simple.demo1748
 {
-    /*
-    1748. 唯一元素的和
-    给你一个整数数组 nums 。数组中唯一元素是那些只出现 恰好一次 的元素。
-    请你返回 nums 中唯一元素的 和 。
-     */
     public class Class1748
     {
         public int SumOfUnique(int[] nums)
---
+++ Ahao.LeetCode/1700-1799/demo1748/JS1748.js
@@ -0,0 +1,28 @@
+﻿var sumOfUnique = function (nums) {
+    const cnt = new Map();
+    for (const num of nums) {
+        cnt.set(num, (cnt.get(num) || 0) + 1);
+    }
+    let ans = 0;
+    for (const [num, c] of cnt.entries()) {
+        if (c == 1) {
+            ans += num;
+        }
+    }
+    return ans;
+};
+
+var sumOfUnique = function (nums) {
+    let ans = 0;
+    const state = new Map();
+    for (const num of nums) {
+        if (!state.has(num)) {
+            ans += num;
+            state.set(num, 1);
+        } else if (state.get(num) == 1) {
+            ans -= num;
+            state.set(num, 2);
+        }
+    }
+    return ans;
+};
