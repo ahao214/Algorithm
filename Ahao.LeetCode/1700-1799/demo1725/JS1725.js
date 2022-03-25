f6cf803f97b256c7920fa76c97b0fafade2b6d11
ahao214 <haochen214@163.com>
2022-03-25 12:35:36 +0800

    1725. 可以形成最大正方形的矩形数目

---
+++ Ahao.LeetCode/1700-1799/demo1725/JS1725.js
@@ -0,0 +1,16 @@
+﻿var countGoodRectangles = function (rectangles) {
+    let res = 0,
+        maxLen = 0;
+    for (const rec of rectangles) {
+        const i = rec[0],
+            j = rec[1];
+        const k = Math.max(i, j);
+        if (k == maxLen) {
+            ++res;
+        } else if (k > maxLen) {
+            res = 1;
+            maxLen = k;
+        }
+    }
+    return res;
+};
