1ce93daed2ce652138cf06ca703118750c07b033
ahao214 <haochen214@163.com>
2022-03-26 18:55:59 +0800

    203. 移除链表元素

---
+++ Ahao.LeetCode/0200-0299/demo203/Class203.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0200_0299.demo203
+{
+    public class Class203
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            if (head == null)
+                return null;
+            ListNode newHead = null;
+            ListNode newTail = null;
+            while (head != null)
+            {
+                if (head.val != val)
+                {
+                    if (newTail == null)
+                    {
+                        newHead = head;
+                        newTail = head;
+                    }
+                    else
+                    {
+                        newTail.next = head;
+                        newTail = head;
+                    }
+                }
+                head = head.next;
+            }
+            if (newTail == null)
+            {
+                return null;
+            }
+            else
+            {
+                newTail.next = null;
+            }
+            return newHead;
+        }
+
+        public ListNode RemoveElements2(ListNode head, int val)
+        {
+            if (head == null)
+                return null;
+            ListNode newHead = new ListNode(0);
+            ListNode newTail = newHead;
+            while (head != null)
+            {
+                if (head.val != val)
+                {
+                    newTail.next = head;
+                    newTail = head;
+                }
+                head = head.next;
+            }
+            newTail.next = null;
+            return newHead.next;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+}
---
+++ Ahao.LeetCode/0200-0299/demo203/JS203.js
@@ -0,0 +1,24 @@
+﻿//递归
+var removeElements = function (head, val) {
+    if (head === null) {
+        return head;
+    }
+    head.next = removeElements(head.next, val);
+    return head.val === val ? head.next : head;
+};
+
+
+//迭代
+var removeElements2 = function (head, val) {
+    const dummyHead = new ListNode(0);
+    dummyHead.next = head;
+    let temp = dummyHead;
+    while (temp.next !== null) {
+        if (temp.next.val == val) {
+            temp.next = temp.next.next;
+        } else {
+            temp = temp.next;
+        }
+    }
+    return dummyHead.next;
+};
