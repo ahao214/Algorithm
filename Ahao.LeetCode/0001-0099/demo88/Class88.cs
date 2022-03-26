1a73b4265f498fc184c032dec19d2af04971ceb3
ahao214 <haochen214@163.com>
2022-03-26 18:51:39 +0800

    88. 合并两个有序数组

--- Ahao.LeetCode/0001-0099/demo88/Class88.cs
+++ Ahao.LeetCode/0001-0099/demo88/Class88.cs
@@ -6,14 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Simple.demo88
 {
-    /*
-     88. 合并两个有序数组
- 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
-
- 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
-
- 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
-     */
     public class Class88
     {
         public void Merge(int[] nums1, int m, int[] nums2, int n)
@@ -26,5 +18,6 @@ namespace Ahao.LeetCode.Simple.demo88
                 nums1[i--] = m < 0 || nums2[n] >= nums1[m] ? nums2[n--] : nums1[m--];
             }
         }
+
     }
 }
