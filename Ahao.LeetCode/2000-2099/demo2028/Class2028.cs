296714a76d0db3d8e5789a545fa24f8940f34ae7
ahao214 <haochen214@163.com>
2022-03-27 12:24:22 +0800

    2028. 找出缺失的观测数据

---
+++ Ahao.LeetCode/2000-2099/demo2028/Class2028.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2000_2099.demo2028
+{
+    public class Class2028
+    {
+        public int[] MissingRolls(int[] rolls, int mean, int n)
+        {
+            int sum = rolls.Sum();
+            int m = rolls.Length;
+            //缺失的数字的总和
+            int missSum = (m + n) * mean - sum;
+            //缺失的数字的总和必须在n和n*6之间
+            if (missSum < n || missSum > n * 6)
+                return new int[0];
+            int a = missSum / n, b = missSum % n;
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (i < b)
+                    res[i] = a + 1;
+                else
+                    res[i] = a;
+            }
+            return res;
+        }
+    }
+}
