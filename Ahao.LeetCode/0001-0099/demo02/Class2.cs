b397718b42f8f4661bbce4635af54ebb3f8953bc
ahao214 <haochen214@163.com>
2022-03-27 21:19:50 +0800

    2. 两数相加

--- Ahao.LeetCode/0001-0099/demo02/Class2.cs
+++ Ahao.LeetCode/0001-0099/demo02/Class2.cs
@@ -32,6 +32,45 @@ namespace Ahao.LeetCode.Medium.demo2
             return dummy.next;
 
         }
+
+        public ListNode AddTwoNumbers2(ListNode l1, ListNode l2)
+        {
+            if (l1 == null)
+                return l2;
+            if (l2 == null)
+                return l1;
+
+            //虚拟头节点
+            ListNode dummyHead = new ListNode(0);
+            //进位值
+            int carry = 0;
+            ListNode last = dummyHead;
+            while (l1 != null || l2 != null)
+            {
+                int v1 = 0;
+                if (l1 != null)
+                {
+                    v1 = l1.val;
+                    l1 = l1.next;
+                }
+                int v2 = 0;
+                if (l2 != null)
+                {
+                    v2 = l2.val;
+                    l2 = l2.next;
+                }
+                int sum = v1 + v2 + carry;
+                carry = sum / 10;
+                //sum的个位数做为新节点的值
+                last.next = new ListNode(sum % 10);
+                last = last.next;
+            }
+            if (carry > 0)
+            {
+                last.next = new ListNode(carry);
+            }
+            return dummyHead.next;
+        }
     }
 
     public class ListNode
