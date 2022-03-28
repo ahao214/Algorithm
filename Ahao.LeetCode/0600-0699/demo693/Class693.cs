32c6c5ed727dd7f15557d48383d4aef0318f7624
ahao214 <haochen214@163.com>
2022-03-28 08:04:29 +0800

    693. 交替位二进制数

---
+++ Ahao.LeetCode/0600-0699/demo693/Class693.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0600_0699.demo693
+{
+    public class Class693
+    {
+        public bool HasAlternatingBits(int n)
+        {
+            int prev = -1;
+            while (n != 0)
+            {
+                int cur = n % 2;
+                if (cur == prev)
+                {
+                    return false;
+                }
+                prev = cur;
+                n /= 2;
+            }
+            return true;
+        }
+
+        public bool HasAlternatingBits2(int n)
+        {
+            int a = n ^ (n >> 1);
+            return (a & (a + 1)) == 0;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0600-0699/demo693/JS693.js
@@ -0,0 +1,18 @@
+﻿var hasAlternatingBits = function (n) {
+    let prev = 2;
+    while (n !== 0) {
+        const cur = n % 2;
+        if (cur === prev) {
+            return false;
+        }
+        prev = cur;
+        n = Math.floor(n / 2);
+    }
+    return true;
+};
+
+
+var hasAlternatingBits2 = function (n) {
+    const a = n ^ (n >> 1);
+    return (a & (a + 1)) == 0;
+};
