ea7a37c32b13d3da244d6d9df79c7d3d31a64301
ahao214 <haochen214@163.com>
2022-03-28 08:21:51 +0800

    160. 相交链表

---
+++ Ahao.LeetCode/0100-0199/demo160/Class160.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0100_0199.demo160
+{
+    public class Class160
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            /*
+             把A链表复制一份到B链表后面
+             把B链表复制一份到A链表后面
+             */
+            if (headA == null || headB == null)
+                return null;
+            ListNode curA = headA, curB = headB;
+            while (curA != curB)
+            {
+                curA = curA == null ? headB : curA.next;
+                curB = curB == null ? headA : curB.next;
+            }
+            return curA;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+
+}
---
+++ Ahao.LeetCode/0100-0199/demo160/JS160.js
@@ -0,0 +1,30 @@
+﻿//哈希集合
+var getIntersectionNode = function (headA, headB) {
+    const visited = new Set();
+    let temp = headA;
+    while (temp !== null) {
+        visited.add(temp);
+        temp = temp.next;
+    }
+    temp = headB;
+    while (temp !== null) {
+        if (visited.has(temp)) {
+            return temp;
+        }
+        temp = temp.next;
+    }
+    return null;
+};
+
+//双指针
+var getIntersectionNode2 = function (headA, headB) {
+    if (headA === null || headB === null) {
+        return null;
+    }
+    let pA = headA, pB = headB;
+    while (pA !== pB) {
+        pA = pA === null ? headB : pA.next;
+        pB = pB === null ? headA : pB.next;
+    }
+    return pA;
+};
