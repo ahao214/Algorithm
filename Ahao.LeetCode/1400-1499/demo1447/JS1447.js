dd99178b97a27999e83ddf50fb2eeee4a89be061
ahao214 <haochen214@163.com>
2022-03-28 09:13:44 +0800

    1447. 最简分数

---
+++ Ahao.LeetCode/1400-1499/demo1447/JS1447.js
@@ -0,0 +1,18 @@
+﻿var simplifiedFractions = function (n) {
+    const ans = [];
+    for (let denominator = 2; denominator <= n; ++denominator) {
+        for (let numerator = 1; numerator < denominator; ++numerator) {
+            if (gcd(numerator, denominator) == 1) {
+                ans.push(numerator + "/" + denominator);
+            }
+        }
+    }
+    return ans;
+};
+
+const gcd = (a, b) => {
+    if (b === 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+};
