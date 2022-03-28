c6e9a2ab3b3a72a3c9870ff4e4c1ef5811e054bb
ahao214 <haochen214@163.com>
2022-03-28 10:23:27 +0800

    2104. 子数组范围和

--- Ahao.LeetCode/2100-2199/demo2104/JS2104.js
+++ Ahao.LeetCode/2100-2199/demo2104/JS2104.js
@@ -1,12 +1,4 @@
-﻿/*
-2104. 子数组范围和
-给你一个整数数组 nums 。nums 中，子数组的 范围 是子数组中最大元素和最小元素的差值。
-
-返回 nums 中 所有 子数组范围的 和 。
-
-子数组是数组中一个连续 非空 的元素序列。
-*/
-//方法一：遍历子数组
+﻿//方法一：遍历子数组
 var subArrayRanges = function (nums) {
     const n = nums.length;
     let ret = 0;
