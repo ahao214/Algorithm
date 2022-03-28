89248dc59d8c08c7a16071f95d2956d370a150d7
ahao214 <haochen214@163.com>
2022-03-28 09:12:26 +0800

    1414. 和为 K 的最少斐波那契数字数目

--- Ahao.LeetCode/1400-1499/demo1414/Class1414.cs
+++ Ahao.LeetCode/1400-1499/demo1414/Class1414.cs
@@ -6,17 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Medium.demo1414
 {
-    /*
-    1414. 和为 K 的最少斐波那契数字数目
-    给你数字 k ，请你返回和为 k 的斐波那契数字的最少数目，其中，每个斐波那契数字都可以被使用多次。
-
-    斐波那契数字定义为：
-
-    F1 = 1
-    F2 = 1
-    Fn = Fn-1 + Fn-2 ， 其中 n > 2 。
-    数据保证对于给定的 k ，一定能找到可行解。
-     */
     public class Class1414
     {
         public int FindMinFibonacciNumbers(int k)
