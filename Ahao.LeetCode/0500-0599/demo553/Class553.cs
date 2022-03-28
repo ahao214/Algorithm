cc70377b48af6863e3ae1d53197465b337a7ace1
ahao214 <haochen214@163.com>
2022-03-28 08:37:22 +0800

    553. 最优除法

--- Ahao.LeetCode/0500-0599/demo553/Class553.cs
+++ Ahao.LeetCode/0500-0599/demo553/Class553.cs
@@ -6,15 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Medium.demo553
 {
-    /*
-553. 最优除法
-给定一组正整数，相邻的整数之间将会进行浮点除法操作。例如， 
-[2,3,4] -> 2 / 3 / 4 。
-
-但是，你可以在任意位置添加任意数目的括号，来改变算数的优先级。
-你需要找出怎么添加括号，才能得到最大的结果，并且返回相应的字符串格式的表达式。
-你的表达式不应该含有冗余的括号。
-*/
     public class Class553
     {
         public string OptimalDivision(int[] nums)
@@ -59,6 +50,6 @@ namespace Ahao.LeetCode.Medium.demo553
             }
             res.Append(")");
             return res.ToString();
-        }        
+        }
     }
 }
