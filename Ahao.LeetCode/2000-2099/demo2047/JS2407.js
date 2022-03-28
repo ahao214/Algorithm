1fc22cdef26685323e7bc6705880c914ec4a0cf0
ahao214 <haochen214@163.com>
2022-03-28 10:19:49 +0800

    2047. 句子中的有效单词数

---
+++ Ahao.LeetCode/2000-2099/demo2047/JS2407.js
@@ -0,0 +1,50 @@
+﻿var countValidWords = function (sentence) {
+    const n = sentence.length;
+    let l = 0,
+        r = 0;
+    let ret = 0;
+    while (true) {
+        while (l < n && sentence[l] === ' ') {
+            l++;
+        }
+        if (l >= n) {
+            break;
+        }
+        r = l + 1;
+        while (r < n && sentence[r] != ' ') {
+            r++;
+        }
+        if (isValid(sentence.slice(l, r))) { // 判断根据空格分解出来的 token 是否有效
+            ret++;
+        }
+        l = r + 1;
+    }
+    return ret;
+};
+
+const isValid = (word) => {
+    const n = word.length;
+    let hasHyphens = false;
+    for (let i = 0; i < n; i++) {
+        if (word[i] >= '0' && word[i] <= '9') {
+            return false;
+        } else if (word[i] === '-') {
+            if (hasHyphens === true || i === 0 || i === n - 1 || !isLetter(word[i - 1]) || !isLetter(word[i + 1])) {
+                return false;
+            }
+            hasHyphens = true;
+        } else if (word[i] === '!' || word[i] === '.' || word[i] === ',') {
+            if (i !== n - 1) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+const isLetter = (ch) => {
+    if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') {
+        return true;
+    }
+    return false;
+};
