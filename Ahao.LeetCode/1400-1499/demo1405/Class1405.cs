94337143d870046993a5b6cf06c3dcc830e583c9
ahao214 <haochen214@163.com>
2022-03-28 09:11:21 +0800

    1405. 最长快乐字符串

--- Ahao.LeetCode/1400-1499/demo1405/Class1405.cs
+++ Ahao.LeetCode/1400-1499/demo1405/Class1405.cs
@@ -6,15 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Medium.demo1405
 {
-    /*
-     1405. 最长快乐字符串
-    如果字符串中不含有任何 'aaa'，'bbb' 或 'ccc' 这样的字符串作为子串，那么该字符就是一个「快乐字符串」。
-    给你三个整数 a，b ，c，请你返回 任意一个 满足下列全部条件的字符串 s：
-    s 是一个尽可能长的快乐字符串。
-    s 中 最多 有a 个字母 'a'、b 个字母 'b'、c 个字母 'c' 。
-    s 中只含有 'a'、'b' 、'c' 三种字母。
-    如果不存在这样的字符串 s ，请返回一个空字符串 ""。
-     */
     public class Class1405
     {
         public string LongestDiverseString(int a, int b, int c)
