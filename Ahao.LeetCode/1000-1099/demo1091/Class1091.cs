2d61e95c7d4492823f45909293b721cd8b981013
ahao214 <haochen214@163.com>
2022-03-28 08:59:13 +0800

    1091. 二进制矩阵中的最短路径

--- Ahao.LeetCode/1000-1099/demo1091/Class1091.cs
+++ Ahao.LeetCode/1000-1099/demo1091/Class1091.cs
@@ -6,9 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Medium.demo1091
 {
-    /// <summary>
-    /// 1091. 二进制矩阵中的最短路径
-    /// </summary>
     public class Class1091
     {
         int result = 0;
