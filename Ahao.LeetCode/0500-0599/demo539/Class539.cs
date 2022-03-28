2da11351d2c0e94bbb1edba2bd337c2f9fab2d73
ahao214 <haochen214@163.com>
2022-03-28 08:23:56 +0800

    539. 最小时间差

--- Ahao.LeetCode/0500-0599/demo539/Class539.cs
+++ Ahao.LeetCode/0500-0599/demo539/Class539.cs
@@ -6,10 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Medium.demo539
 {
-    /*
-     539. 最小时间差
-    给定一个 24 小时制（小时:分钟 "HH:MM"）的时间列表，找出列表中任意两个时间的最小时间差并以分钟数表示。
-     */
     public class Class539
     {
         public int FindMinDifference(IList<string> timePoints)
