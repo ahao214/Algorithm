73da2800903ee42658348d3aa24798c813325c39
ahao214 <haochen214@163.com>
2022-03-28 10:10:58 +0800

    2016. 增量元素之间的最大差值

---
+++ Ahao.LeetCode/2000-2099/demo2016/JS2016.js
@@ -0,0 +1,13 @@
+﻿var maximumDifference = function (nums) {
+    const n = nums.length;
+    let ans = -1,
+        premin = nums[0];
+    for (let i = 1; i < n; ++i) {
+        if (nums[i] > premin) {
+            ans = Math.max(ans, nums[i] - premin);
+        } else {
+            premin = nums[i];
+        }
+    }
+    return ans;
+};
