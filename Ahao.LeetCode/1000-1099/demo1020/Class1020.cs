7837333df37b12909140480f5eea7d53bfa561ec
ahao214 <haochen214@163.com>
2022-03-28 08:53:21 +0800

    1020. 飞地的数量

--- Ahao.LeetCode/1000-1099/demo1020/Class1020.cs
+++ Ahao.LeetCode/1000-1099/demo1020/Class1020.cs
@@ -6,14 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Medium.demo1020
 {
-    /*
-     1020. 飞地的数量
-给你一个大小为 m x n 的二进制矩阵 grid ，其中 0 表示一个海洋单元格、1 表示一个陆地单元格。
-
-一次 移动 是指从一个陆地单元格走到另一个相邻（上、下、左、右）的陆地单元格或跨过 grid 的边界。
-
-返回网格中 无法 在任意次数的移动中离开网格边界的陆地单元格的数量。
-     */
     public class Class1020
     {
         public static int[][] dirs = { new int[] { -1, 0 }, new int[] { 1, 0 }, new int[] { 0, -1 }, new int[] { 0, 1 } };
