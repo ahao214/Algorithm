00db076aa3a640ef7b20a870085e0dcccfa072a9
ahao214 <haochen214@163.com>
2022-03-28 10:07:08 +0800

    2000. 反转单词前缀

---
+++ Ahao.LeetCode/2000-2099/demo2000/JS2000.js
@@ -0,0 +1,17 @@
+﻿var reversePrefix = function (word, ch) {
+    const index = word.indexOf(ch);
+    if (index >= 0) {
+        const arr = [...word];
+        let left = 0,
+            right = index;
+        while (left < right) {
+            const tmp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = tmp;
+            left++;
+            right--;
+        }
+        word = arr.join('');
+    }
+    return word;
+};
