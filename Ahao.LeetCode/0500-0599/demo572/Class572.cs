83b82d75fde3a33eed3ce2826596d5834bc315c8
ahao214 <haochen214@163.com>
2022-03-29 10:04:44 +0800

    572. 另一棵树的子树

--- Ahao.LeetCode/0500-0599/demo572/Class572.cs
+++ Ahao.LeetCode/0500-0599/demo572/Class572.cs
@@ -39,6 +39,43 @@ namespace Ahao.LeetCode.Simple.demo572
             }
             return false;
         }
+
+        public bool IsSubtree2(TreeNode root, TreeNode subRoot)
+        {
+            if (root == null || subRoot == null)
+                return false;
+            return post(root).Contains(post(subRoot));
+        }
+
+        //利用后续遍历的方式进行序列化
+        private string post(TreeNode root)
+        {
+            StringBuilder sb = new StringBuilder();
+            post(root, sb);
+            return sb.ToString();
+        }
+
+        private void post(TreeNode node, StringBuilder sb)
+        {
+            if (node.left == null)
+            {
+                sb.Append("#!");
+            }
+            else
+            {
+                post(node.left, sb);
+            }
+
+            if (node.right == null)
+            {
+                sb.Append("#!");
+            }
+            else
+            {
+                post(node.right, sb);
+            }
+            sb.Append(node.val).Append("!");
+        }
     }
 
 
