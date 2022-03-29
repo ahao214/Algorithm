986179664fe43fd33029b7e520c15b87a2e52beb
ahao214 <haochen214@163.com>
2022-03-29 10:15:42 +0800

    739. 每日温度

---
+++ Ahao.LeetCode/0700-0799/demo739/Class739.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo739
+{
+    public class Class739
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            if (temperatures == null || temperatures.Length == 0)
+            {
+                return new int[0];
+            }
+
+            if (temperatures.Length == 1)
+            {
+                return new int[] { 0 };
+            }
+
+            int[] res = new int[temperatures.Length];
+            Stack<int> stk = new Stack<int>();
+
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                while (stk.Count > 0 && temperatures[i] > temperatures[stk.Peek()])
+                {
+                    int topIdx = stk.Pop();
+                    res[topIdx] = i - topIdx;
+                }
+                stk.Push(i);
+            }
+            return res;
+        }
+    }
+}
