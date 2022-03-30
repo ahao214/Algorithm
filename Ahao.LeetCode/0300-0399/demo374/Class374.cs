fbd041a770a73b5c0afdf77c397fedbcbdb5295b
ahao214 <haochen214@163.com>
2022-03-30 17:44:51 +0800

    374. 猜数字大小

---
+++ Ahao.LeetCode/0300-0399/demo374/Class374.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0300_0399.demo374
+{
+    public class Class374
+    {
+        public int GuessNumber(int n)
+        {
+            int left = 1, right = n;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                //if (guess(mid) <= 0)
+                //    right = mid;
+                //else
+                //    left = mid + 1;
+            }
+            return left;
+        }
+
+        //int guess(int num);
+    }
+}
