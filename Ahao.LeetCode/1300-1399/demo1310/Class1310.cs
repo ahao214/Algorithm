35fe2c476f0ae700ca5716a48661063d16ad8216
ahao214 <haochen214@163.com>
2022-03-30 18:23:22 +0800

    1310. 子数组异或查询

---
+++ Ahao.LeetCode/1300-1399/demo1310/Class1310.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1300_1399
+{
+    public class Class1310
+    {
+        public int[] XorQueries(int[] arr, int[][] queries)
+        {
+            int n = arr.Length;
+            int[] xors = new int[n + 1];
+            for (int i = 0; i < n; i++)
+                xors[i + 1] = xors[i] ^ arr[i];
+            int m = queries.Length;
+            int[] ans = new int[m];
+            for (int i = 0; i < m; i++)
+            {
+                ans[i] = xors[queries[i][0]] ^ xors[queries[i][1] + 1];
+            }
+            return ans;
+        }
+    }
+}
