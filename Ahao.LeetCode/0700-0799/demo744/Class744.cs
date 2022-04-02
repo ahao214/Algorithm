da6c52b0de24e1f32c5b399eb2cd02c66f4a06ae
ahao214 <haochen214@163.com>
2022-04-03 00:23:21 +0800

    744. 寻找比目标字母大的最小字母

---
+++ Ahao.LeetCode/0700-0799/demo744/Class744.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo744
+{
+    public class Class744
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            char res = letters[0];
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i] > target)
+                {
+                    res = letters[i];
+                    break;
+                }
+            }
+
+            return res;
+        }
+    }
+}
