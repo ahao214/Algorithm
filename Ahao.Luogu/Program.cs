a0ca12451e9b802dcff4ba44f2a08bd2617de885
ahao214 <haochen214@163.com>
2022-04-04 20:57:26 +0800

    P1001 A+B Problem

---
+++ Ahao.Luogu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ahao.Luogu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
