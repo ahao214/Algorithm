ab90a8518d76f3b35517e2e7b8879b3b879d0026
ahao214 <haochen214@163.com>
2022-04-06 21:42:51 +0800

    310. 最小高度树

---
+++ Ahao.LeetCode/0300-0399/demo310/Class310.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0300_0399.demo310
+{
+    public class Class310
+    {
+        /// <summary>
+        /// 广度优先
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="edges"></param>
+        /// <returns></returns>
+        public IList<int> FindMinHeightTrees(int n, int[][] edges)
+        {
+            IList<int> ans = new List<int>();
+            if (n == 1)
+            {
+                ans.Add(0);
+                return ans;
+            }
+            IList<int>[] adj = new IList<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                adj[i] = new List<int>();
+            }
+            foreach (int[] edge in edges)
+            {
+                adj[edge[0]].Add(edge[1]);
+                adj[edge[1]].Add(edge[0]);
+            }
+
+            int[] parent = new int[n];
+            Array.Fill(parent, -1);
+            /* 找到与节点 0 最远的节点 x */
+            int x = FindLongestNode(0, parent, adj);
+            /* 找到与节点 x 最远的节点 y */
+            int y = FindLongestNode(x, parent, adj);
+            /* 求出节点 x 到节点 y 的路径 */
+            IList<int> path = new List<int>();
+            parent[x] = -1;
+            while (y != -1)
+            {
+                path.Add(y);
+                y = parent[y];
+            }
+            int m = path.Count;
+            if (m % 2 == 0)
+            {
+                ans.Add(path[m / 2 - 1]);
+            }
+            ans.Add(path[m / 2]);
+            return ans;
+        }
+
+        public int FindLongestNode(int u, int[] parent, IList<int>[] adj)
+        {
+            int n = adj.Length;
+            Queue<int> queue = new Queue<int>();
+            bool[] visit = new bool[n];
+            queue.Enqueue(u);
+            visit[u] = true;
+            int node = -1;
+
+            while (queue.Count > 0)
+            {
+                int curr = queue.Dequeue();
+                node = curr;
+                foreach (int v in adj[curr])
+                {
+                    if (!visit[v])
+                    {
+                        visit[v] = true;
+                        parent[v] = curr;
+                        queue.Enqueue(v);
+                    }
+                }
+            }
+            return node;
+        }
+
+
+        /// <summary>
+        /// 拓扑排序
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="edges"></param>
+        /// <returns></returns>
+        public IList<int> FindMinHeightTrees2(int n, int[][] edges)
+        {
+            IList<int> ans = new List<int>();
+            if (n == 1)
+            {
+                ans.Add(0);
+                return ans;
+            }
+            int[] degree = new int[n];
+            IList<int>[] adj = new IList<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                adj[i] = new List<int>();
+            }
+            foreach (int[] edge in edges)
+            {
+                adj[edge[0]].Add(edge[1]);
+                adj[edge[1]].Add(edge[0]);
+                degree[edge[0]]++;
+                degree[edge[1]]++;
+            }
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (degree[i] == 1)
+                {
+                    queue.Enqueue(i);
+                }
+            }
+            int remainNodes = n;
+            while (remainNodes > 2)
+            {
+                int sz = queue.Count;
+                remainNodes -= sz;
+                for (int i = 0; i < sz; i++)
+                {
+                    int curr = queue.Dequeue();
+                    foreach (int v in adj[curr])
+                    {
+                        degree[v]--;
+                        if (degree[v] == 1)
+                        {
+                            queue.Enqueue(v);
+                        }
+                    }
+                }
+            }
+            while (queue.Count > 0)
+            {
+                ans.Add(queue.Dequeue());
+            }
+            return ans;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/0300-0399/demo310/JS310.js
@@ -0,0 +1,96 @@
+﻿//广度优先
+var findMinHeightTrees = function (n, edges) {
+    const ans = [];
+    if (n === 1) {
+        ans.push(0);
+        return ans;
+    }
+    const adj = new Array(n).fill(0).map(() => new Array());
+    for (const edge of edges) {
+        adj[edge[0]].push(edge[1]);
+        adj[edge[1]].push(edge[0]);
+    }
+
+    const parent = new Array(n).fill(-1);
+    /* 找到与节点 0 最远的节点 x */
+    const x = findLongestNode(0, parent, adj);
+    /* 找到与节点 x 最远的节点 y */
+    let y = findLongestNode(x, parent, adj);
+    /* 求出节点 x 到节点 y 的路径 */
+    const path = [];
+    parent[x] = -1;
+    while (y !== -1) {
+        path.push(y);
+        y = parent[y];
+    }
+    const m = path.length;
+    if (m % 2 === 0) {
+        ans.push(path[Math.floor(m / 2) - 1]);
+    }
+    ans.push(path[Math.floor(m / 2)]);
+    return ans;
+}
+
+const findLongestNode = (u, parent, adj) => {
+    const n = adj.length;
+    const queue = [];
+    const visit = new Array(n).fill(false);
+    queue.push(u);
+    visit[u] = true;
+    let node = -1;
+
+    while (queue.length) {
+        const curr = queue.shift();
+        node = curr;
+        for (const v of adj[curr]) {
+            if (!visit[v]) {
+                visit[v] = true;
+                parent[v] = curr;
+                queue.push(v);
+            }
+        }
+    }
+    return node;
+};
+
+
+//拓扑排序
+var findMinHeightTrees2 = function (n, edges) {
+    const ans = [];
+    if (n === 1) {
+        ans.push(0);
+        return ans;
+    }
+    const degree = new Array(n).fill(0);
+    const adj = new Array(n).fill(0).map(() => new Array());
+    for (const edge of edges) {
+        adj[edge[0]].push(edge[1]);
+        adj[edge[1]].push(edge[0]);
+        degree[edge[0]]++;
+        degree[edge[1]]++;
+    }
+    const queue = [];
+    for (let i = 0; i < n; i++) {
+        if (degree[i] === 1) {
+            queue.push(i);
+        }
+    }
+    let remainNodes = n;
+    while (remainNodes > 2) {
+        const sz = queue.length;
+        remainNodes -= sz;
+        for (let i = 0; i < sz; i++) {
+            const curr = queue.shift();
+            for (const v of adj[curr]) {
+                degree[v]--;
+                if (degree[v] === 1) {
+                    queue.push(v);
+                }
+            }
+        }
+    }
+    while (queue.length) {
+        ans.push(queue.shift());
+    }
+    return ans;
+};
