bf5b0992b2a93133a3a70fbf645ea2d8e7d29411
ahao214 <haochen214@163.com>
2022-04-09 18:28:40 +0800

    780. 到达终点

---
+++ Ahao.LeetCode/0700-0799/demo780/Class780.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo780
+{
+    public class Class780
+    {
+        public bool ReachingPoints(int sx, int sy, int tx, int ty)
+        {
+            while (tx > sx && ty > sy && tx != ty)
+            {
+                if (tx > ty)
+                {
+                    tx %= ty;
+                }
+                else
+                {
+                    ty %= tx;
+                }
+            }
+            if (tx == sx && ty == sy)
+            {
+                return true;
+            }
+            else if (tx == sx)
+            {
+                return ty > sy && (ty - sy) % tx == 0;
+            }
+            else if (ty == sy)
+            {
+                return tx > sx && (tx - sx) % ty == 0;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
