08ef373998b8e0e8abc5cc68d4d706a6e41e743d
ahao214 <haochen214@163.com>
2022-04-10 00:17:59 +0800

    804. 唯一摩尔斯密码词

---
+++ Ahao.LeetCode/0800-0899/demo804/Class804.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo804
+{
+    public class Class804
+    {
+
+        public static string[] MORSE = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                                      "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                                      "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+                                      "...-", ".--", "-..-", "-.--", "--.."};
+
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            ISet<string> seen = new HashSet<string>();
+            foreach (string word in words)
+            {
+                StringBuilder code = new StringBuilder();
+                foreach (char c in word)
+                {
+                    code.Append(MORSE[c - 'a']);
+                }
+                seen.Add(code.ToString());
+            }
+            return seen.Count;
+        }
+
+    }
+}
