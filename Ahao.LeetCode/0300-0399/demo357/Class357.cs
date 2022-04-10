87c8c7db4f56b543b56cb090cc5d2c33a42613a0
ahao214 <haochen214@163.com>
2022-04-11 07:57:27 +0800

    357. 统计各位数字都不同的数字个数

---
+++ Ahao.LeetCode/0300-0399/demo357/Class357.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0300_0399.demo357
+{
+    public class Class357
+    {
+        public int CountNumbersWithUniqueDigits(int n)
+        {
+            if (n == 0)
+                return 1;
+            if (n == 1)
+                return 10;
+            int res = 10, cur = 9;
+            for (int i = 0; i < n - 1; i++)
+            {
+                cur *= 9 - i;
+                res += cur;
+            }
+
+            return res;
+        }
+    }
+}
