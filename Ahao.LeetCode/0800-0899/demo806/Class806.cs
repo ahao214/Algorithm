2a9a9e01677b19bbc0af753e754af8da36483305
ahao214 <haochen214@163.com>
2022-04-12 00:18:41 +0800

    806. 写字符串需要的行数

---
+++ Ahao.LeetCode/0800-0899/demo806/Class806.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo806
+{
+    public class Class806
+    {
+        public int[] NumberOfLines(int[] widths, string s)
+        {
+            int maxWidth = 100;
+            int lines = 0;
+            int width = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int wantWidth = widths[s[i] - 'a'];
+                width += wantWidth;
+                if (width > maxWidth)
+                {
+                    lines++;
+                    width = wantWidth;
+                }
+            }
+            return new int[] { lines, width };
+        }
+    }
+}
