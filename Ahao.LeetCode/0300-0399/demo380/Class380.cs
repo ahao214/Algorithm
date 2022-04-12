d39aa500f2e755c9f5195aa71f56d0ce77204795
ahao214 <haochen214@163.com>
2022-04-13 07:15:17 +0800

    380. O(1) 时间插入、删除和获取随机元素

---
+++ Ahao.LeetCode/0300-0399/demo380/Class380.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0300_0399.demo380
+{
+    public class Class380
+    {
+
+        IList<int> nums;
+        Dictionary<int, int> indices;
+        Random random;
+
+        public Class380()
+        {
+            nums = new List<int>();
+            indices = new Dictionary<int, int>();
+            random = new Random();
+        }
+
+        public bool Insert(int val)
+        {
+            if (indices.ContainsKey(val))
+            {
+                return false;
+            }
+            int index = nums.Count;
+            nums.Add(val);
+            indices.Add(val, index);
+            return true;
+        }
+
+        public bool Remove(int val)
+        {
+            if (!indices.ContainsKey(val))
+            {
+                return false;
+            }
+            int index = indices[val];
+            int last = nums[nums.Count - 1];
+            nums[index] = last;
+            indices[last] = index;
+            nums.RemoveAt(nums.Count - 1);
+            indices.Remove(val);
+            return true;
+        }
+
+        public int GetRandom()
+        {
+            int randomIndex = random.Next(nums.Count);
+            return nums[randomIndex];
+        }
+
+    }
+}
