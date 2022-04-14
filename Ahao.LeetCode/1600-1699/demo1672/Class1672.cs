3d365ea7bc2886d0fe19ba47633072b960da5e9f
ahao214 <haochen214@163.com>
2022-04-14 08:46:45 +0800

    1672. 最富有客户的资产总量

---
+++ Ahao.LeetCode/1600-1699/demo1672/Class1672.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1672
+{
+    public class Class1672
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int res = int.MinValue;
+            foreach (int[] count in accounts)
+            {
+                res = Math.Max(res, count.Sum());
+            }
+            return res;
+        }
+    }
+}
