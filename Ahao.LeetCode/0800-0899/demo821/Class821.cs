0d0bd24e96bc52ba9a4172a1c6e4b013eebef289
ahao214 <haochen214@163.com>
2022-04-19 08:24:41 +0800

    821. 字符的最短距离

---
+++ Ahao.LeetCode/0800-0899/demo821/Class821.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo821
+{
+    public class Class821
+    {
+        public int[] ShortestToChar(string s, char c)
+        {
+            int len = s.Length;
+            int[] ans = new int[len];
+            for (int i = 0, idx = -len; i < len; i++)
+            {
+                if (s[i] == c)
+                {
+                    idx = i;
+                }
+                ans[i] = i - idx;
+            }
+            for (int i = len - 1, idx = 2 * len; i >= 0; i--)
+            {
+                if (s[i] == c)
+                    idx = i;
+                ans[i] = Math.Min(ans[i], idx - i);
+            }
+            return ans;
+        }
+    }
+}
