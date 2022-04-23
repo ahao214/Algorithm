c1f000144fe713be912059c155e90bfc335fa342
liupenghao@fulan.com.cn <haochen214@163.com>
2022-04-23 14:49:53 +0800

    32. 最长有效括号

---
+++ Ahao.LeetCodeCPP/32. 最长有效括号.cpp
@@ -0,0 +1,37 @@
+using namespace std;
+#include <string>
+#include<stack>
+
+
+class Solution {
+	/*
+	32. 最长有效括号
+	给你一个只包含 '(' 和 ')' 的字符串，
+	找出最长有效（格式正确且连续）括号子串的长度。
+	*/
+public:
+	int longestValidParentheses(string s) {
+		stack<int> stk;
+		int ans = 0;
+		for (int i = 0, start = -1; i < s.length(); i++) {
+			if (s[i] == '(') {
+				stk.push(i);
+			}
+			else {
+				if (!stk.empty()) {
+					stk.pop();
+					if (!stk.empty()) {
+						ans = max(ans, i - stk.top());
+					}
+					else {
+						ans = max(ans, i - start);
+					}
+				}
+				else {
+					start = i;
+				}
+			}
+		}
+		return ans;
+	}
+};
