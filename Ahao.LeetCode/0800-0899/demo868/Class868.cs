72f54bc279da6151dd09f081bab28a3ad20c09ac
liupenghao@fulan.com.cn <haochen214@163.com>
2022-04-24 09:12:53 +0800

    868. 二进制间距

---
+++ Ahao.LeetCode/0800-0899/demo868/Class868.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo868
+{
+    public class Class868
+    {
+        public int BinaryGap(int n)
+        {
+            int last = -1, ans = 0;
+            for (int i = 0; n != 0; ++i)
+            {
+                if ((n & 1) == 1)
+                {
+                    if (last != -1)
+                    {
+                        ans = Math.Max(ans, i - last);
+                    }
+                    last = i;
+                }
+                n >>= 1;
+            }
+            return ans;
+        }
+    }
+}
