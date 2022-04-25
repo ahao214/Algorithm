a3c36eaa75264f835c843e4e9000f3b65a5774bd
liupenghao@fulan.com.cn <haochen214@163.com>
2022-04-25 16:50:00 +0800

    398. 随机数索引

---
+++ Ahao.LeetCode/0300-0399/demo398/Class398.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0300_0399.demo398
+{
+    public class Class398
+    {
+
+        Dictionary<int, IList<int>> pos;
+        Random random;
+
+        public Class398(int[] nums)
+        {
+            pos = new Dictionary<int, IList<int>>();
+            random = new Random();
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (!pos.ContainsKey(nums[i]))
+                {
+                    pos.Add(nums[i], new List<int>());
+                }
+                pos[nums[i]].Add(i);
+            }
+        }
+
+        public int Pick(int target)
+        {
+            IList<int> indices = pos[target];
+            return indices[random.Next(indices.Count)];
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/0300-0399/demo398/JS398.js
@@ -0,0 +1,15 @@
+﻿var Solution = function (nums) {
+    this.pos = new Map();
+    for (let i = 0; i < nums.length; ++i) {
+        if (!this.pos.has(nums[i])) {
+            this.pos.set(nums[i], []);
+        }
+
+        this.pos.get(nums[i]).push(i);
+    }
+};
+
+Solution.prototype.pick = function (target) {
+    const indices = this.pos.get(target);
+    return indices[Math.floor(Math.random() * indices.length)];
+};
