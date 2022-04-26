4a28a7a1583e1f9063767e55683a734de4c0db50
liupenghao@fulan.com.cn <haochen214@163.com>
2022-04-26 08:52:17 +0800

    883. 三维形体投影面积

---
+++ Ahao.LeetCode/0800-0899/demo883/Class883.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo883
+{
+    public class Class883
+    {
+        public int ProjectionArea(int[][] grid)
+        {
+            int n = grid.Length;
+            int xyArea = 0, yzArea = 0, zxArea = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int yzHeight = 0, zxHeight = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    xyArea += grid[i][j] > 0 ? 1 : 0;
+                    yzHeight = Math.Max(yzHeight, grid[i][j]);
+                    zxHeight = Math.Max(zxHeight, grid[j][i]);
+                }
+                yzArea += yzHeight;
+                zxArea += zxHeight;
+            }
+            return xyArea + yzArea + zxArea;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0800-0899/demo883/JS883.js
@@ -0,0 +1,15 @@
+﻿var projectionArea = function (grid) {
+    const n = grid.length;
+    let xyArea = 0, yzArea = 0, zxArea = 0;
+    for (let i = 0; i < n; i++) {
+        let yzHeight = 0, zxHeight = 0;
+        for (let j = 0; j < n; j++) {
+            xyArea += grid[i][j] > 0 ? 1 : 0;
+            yzHeight = Math.max(yzHeight, grid[i][j]);
+            zxHeight = Math.max(zxHeight, grid[j][i]);
+        }
+        yzArea += yzHeight;
+        zxArea += zxHeight;
+    }
+    return xyArea + yzArea + zxArea;
+};
