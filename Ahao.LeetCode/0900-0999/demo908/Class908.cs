4929cb9c1820eff81ae2a35ea53a7f8b1736b2a5
liupenghao@fulan.com.cn <haochen214@163.com>
2022-04-30 12:49:10 +0800

    908. 最小差值 I

---
+++ Ahao.LeetCode/0900-0999/demo908/Class908.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0900_0999.demo908
+{
+    public class Class908
+    {
+        public class Solution
+        {
+            public int SmallestRangeI(int[] nums, int k)
+            {
+                int minNum = nums.Min();
+                int maxNum = nums.Max();
+                return maxNum - minNum <= 2 * k ? 0 : maxNum - minNum - 2 * k;
+            }
+        }
+    }
+}
---
+++ Ahao.LeetCode/0900-0999/demo908/JS908.js
@@ -0,0 +1,5 @@
+﻿var smallestRangeI = function (nums, k) {
+    const minNum = _.min(nums);
+    const maxNum = _.max(nums);
+    return maxNum - minNum <= 2 * k ? 0 : maxNum - minNum - 2 * k;
+};
