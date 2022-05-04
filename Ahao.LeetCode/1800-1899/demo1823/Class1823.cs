1b082ca65254c1895950ce0618f3c73d92077c8c
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-04 20:23:15 +0800

    1823. 找出游戏的获胜者

---
+++ Ahao.LeetCode/1800-1899/demo1823/Class1823.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1823
+{
+    public class Class1823
+    {
+        //模拟+队列
+        public int FindTheWinner(int n, int k)
+        {
+            Queue<int> queue = new Queue<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                queue.Enqueue(i);
+            }
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i < k; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+                queue.Dequeue();
+            }
+            return queue.Peek();
+        }
+
+
+
+    }
+}
