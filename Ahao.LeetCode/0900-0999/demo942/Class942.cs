aa2d9f3b360e4fab505a0c446ce6c148f7b51048
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-09 09:01:22 +0800

    942. 增减字符串匹配

---
+++ Ahao.LeetCode/0900-0999/demo942/Class942.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0900_0999.demo942
+{
+    public class Class942
+    {
+        public int[] DiStringMatch(string s)
+        {
+            int n = s.Length, lo = 0, hi = n;
+            int[] perm = new int[n + 1];
+            for (int i = 0; i < n; ++i)
+            {
+                perm[i] = s[i] == 'I' ? lo++ : hi--;
+            }
+            perm[n] = lo; // 最后剩下一个数，此时 lo == hi
+            return perm;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/0900-0999/demo942/JS942.js
@@ -0,0 +1,9 @@
+﻿var diStringMatch = function (s) {
+    let n = s.length, lo = 0, hi = n;
+    const perm = new Array(n + 1).fill(0);
+    for (let i = 0; i < n; ++i) {
+        perm[i] = s[i] === 'I' ? lo++ : hi--;
+    }
+    perm[n] = lo; // 最后剩下一个数，此时 lo == hi
+    return perm;
+};
