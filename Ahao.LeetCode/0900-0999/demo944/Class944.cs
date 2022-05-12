f3bb896c52ba4b50e5b8bc46c21497511cba24f2
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-12 09:18:34 +0800

    944. 删列造序

---
+++ Ahao.LeetCode/0900-0999/demo944/Class944.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0900_0999.demo944
+{
+    public class Class944
+    {
+        public int MinDeletionSize(string[] strs)
+        {
+            int row = strs.Length;
+            int col = strs[0].Length;
+            int ans = 0;
+            for (int j = 0; j < col; ++j)
+            {
+                for (int i = 1; i < row; ++i)
+                {
+                    if (strs[i - 1][j] > strs[i][j])
+                    {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
