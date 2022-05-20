0a44c85f111689d57e23d625a65cbbc6d9fcd89b
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-21 00:12:25 +0800

    961. 在长度 2N 的数组中找出重复 N 次的元素

---
+++ Ahao.LeetCode/0900-0999/demo961/Class961.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0900_0999.demo961
+{
+    public class Class961
+    {
+        public int RepeatedNTimes(int[] nums)
+        {
+            ISet<int> res = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (!res.Add(num))
+                    return num;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0900-0999/demo961/JS961.js
@@ -0,0 +1,11 @@
+﻿var repeatedNTimes = function (nums) {
+    const found = new Set();
+    for (const num of nums) {
+        if (found.has(num)) {
+            return num;
+        }
+        found.add(num);
+    }
+    // 不可能的情况
+    return -1;
+};
