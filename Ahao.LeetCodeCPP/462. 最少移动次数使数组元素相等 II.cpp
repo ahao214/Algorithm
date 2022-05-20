3722f0d30848ec375216a869452fae731b15e154
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-20 23:10:20 +0800

    462. 最少移动次数使数组元素相等 II

---
+++ Ahao.LeetCodeCPP/462. 最少移动次数使数组元素相等 II.cpp
@@ -0,0 +1,58 @@
+using namespace std;
+#include<vector>
+
+
+//≈≈–Ú
+class Solution {
+public:
+	int minMoves2(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		int n = nums.size(), ret = 0, x = nums[n / 2];
+		for (int i = 0; i < n; i++) {
+			ret += abs(nums[i] - x);
+		}
+		return ret;
+	}
+};
+
+
+//øÏÀŸ—°‘Ò
+class Solution {
+public:
+	int quickSelect(vector<int>& nums, int left, int right, int index) {
+		int q = randomPartition(nums, left, right);
+		if (q == index) {
+			return nums[q];
+		}
+		else {
+			return q < index ? quickSelect(nums, q + 1, right, index) : quickSelect(nums, left, q - 1, index);
+		}
+	}
+
+	inline int randomPartition(vector<int>& nums, int left, int right) {
+		int i = rand() % (right - left + 1) + left;
+		swap(nums[i], nums[right]);
+		return partition(nums, left, right);
+	}
+
+	inline int partition(vector<int>& nums, int left, int right) {
+		int x = nums[right], i = left - 1;
+		for (int j = left; j < right; ++j) {
+			if (nums[j] <= x) {
+				swap(nums[++i], nums[j]);
+			}
+		}
+		swap(nums[i + 1], nums[right]);
+		return i + 1;
+	}
+
+	int minMoves2(vector<int>& nums) {
+		srand(time(0));
+		int n = nums.size(), x = quickSelect(nums, 0, n - 1, n / 2), ret = 0;
+		for (int i = 0; i < n; ++i) {
+			ret += abs(nums[i] - x);
+		}
+		return ret;
+	}
+};
+
