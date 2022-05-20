61e5c42d0edf560948beabf4bf46a936a8e62175
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-20 23:08:57 +0800

    462. 最少移动次数使数组元素相等 II

--- Ahao.LeetCode/0400-0499/demo462/JS462.js
+++ Ahao.LeetCode/0400-0499/demo462/JS462.js
@@ -8,7 +8,43 @@ var minMoves2 = function (nums) {
     return ret;
 };
 
-作者：LeetCode - Solution
-链接：https://leetcode.cn/problems/minimum-moves-to-equal-array-elements-ii/solution/zui-shao-yi-dong-ci-shu-shi-shu-zu-yuan-xt3r2/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+var minMoves2 = function (nums) {
+    let n = nums.length, x = quickSelect(nums, 0, n - 1, Math.floor(n / 2)), ret = 0;
+    for (let i = 0; i < n; ++i) {
+        ret += Math.abs(nums[i] - x);
+    }
+    return ret;
+}
+
+const quickSelect = (nums, left, right, index) => {
+    const q = randomPartition(nums, left, right);
+    if (q === index) {
+        return nums[q];
+    } else {
+        return q < index ? quickSelect(nums, q + 1, right, index) : quickSelect(nums, left, q - 1, index);
+    }
+}
+
+const randomPartition = (nums, left, right) => {
+    const i = Math.floor(Math.random() * (right - left + 1)) + left;
+    swap(nums, i, right);
+    return partition(nums, left, right);
+}
+
+const partition = (nums, left, right) => {
+    let x = nums[right], i = left - 1;
+    for (let j = left; j < right; ++j) {
+        if (nums[j] <= x) {
+            ++i;
+            swap(nums, i, j);
+        }
+    }
+    swap(nums, i + 1, right);
+    return i + 1;
+}
+
+const swap = (nums, index1, index2) => {
+    const temp = nums[index1];
+    nums[index1] = nums[index2];
+    nums[index2] = temp;
+}
