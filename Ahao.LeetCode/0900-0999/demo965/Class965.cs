2c5e31badf35dc4971d2bc2564dab17aa9dcae22
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-24 09:03:41 +0800

    965. 单值二叉树

---
+++ Ahao.LeetCode/0900-0999/demo965/Class965.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0900_0999.demo965
+{
+    public class Class965
+    {
+        public bool IsUnivalTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+            if (root.left != null)
+            {
+                if (root.val != root.left.val || !IsUnivalTree(root.left))
+                {
+                    return false;
+                }
+            }
+            if (root.right != null)
+            {
+                if (root.val != root.right.val || !IsUnivalTree(root.right))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
---
+++ Ahao.LeetCode/0900-0999/demo965/JS965.js
@@ -0,0 +1,22 @@
+﻿function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+var isUnivalTree = function (root) {
+    if (!root) {
+        return true;
+    }
+    if (root.left) {
+        if (root.val !== root.left.val || !isUnivalTree(root.left)) {
+            return false;
+        }
+    }
+    if (root.right) {
+        if (root.val !== root.right.val || !isUnivalTree(root.right)) {
+            return false;
+        }
+    }
+    return true;
+};
