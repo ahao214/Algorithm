5ee7c183ffa3e29d364163fc16af29147f8a730e
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-25 09:09:04 +0800

    467. 环绕字符串中唯一的子字符串

---
+++ Ahao.LeetCode/0400-0499/demo467/Class467.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0400_0499.demo467
+{
+    public class Class467
+    {
+        public int FindSubstringInWraproundString(string p)
+        {
+            int[] dp = new int[26];
+            int k = 0;
+            for (int i = 0; i < p.Length; ++i)
+            {
+                if (i > 0 && (p[i] - p[i - 1] + 26) % 26 == 1)
+                { // 字符之差为 1 或 -25
+                    ++k;
+                }
+                else
+                {
+                    k = 1;
+                }
+                dp[p[i] - 'a'] = Math.Max(dp[p[i] - 'a'], k);
+            }
+            return dp.Sum();
+        }
+    }
+}
