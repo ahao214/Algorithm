9d4c86261ca3849c9d75c18f6dd1fa2d9ebfdd12
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-27 21:04:55 +0800

    53. 最大子数组和

---
+++ Ahao.LeetCodeCPP/53. 最大子数组和.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+
+#include<vector>
+
+
+class Solution {
+	/*
+	53. 最大子数组和
+	给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组
+	（子数组最少包含一个元素），返回其最大和。
+
+	子数组 是数组中的一个连续部分。
+	*/
+public:
+	int maxSubArray(vector<int>& nums) {
+		int pre = nums[0], ans = nums[0];
+		for (int i = 1; i < nums.size(); i++)
+		{
+			pre = pre > 0 ? pre + nums[i] : nums[i];
+			ans = max(ans, pre);
+		}
+		return ans;
+	}
+};
