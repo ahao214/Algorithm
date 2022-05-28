d8732fa9a6e0f0550b99b5093d4acdcb2c52d27f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-28 17:33:52 +0800

    1021. 删除最外层的括号

---
+++ Ahao.LeetCode/1000-1099/demo1021/Class1021.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1021
+{
+    public class Class1021
+    {
+        //方法一：栈
+        public string RemoveOuterParentheses(string s)
+        {
+            StringBuilder res = new StringBuilder();
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == ')')
+                {
+                    stack.Pop();
+                }
+                if (stack.Count > 0)
+                {
+                    res.Append(c);
+                }
+                if (c == '(')
+                {
+                    stack.Push(c);
+                }
+            }
+            return res.ToString();
+        }
+
+        //方法二：计数
+        public string RemoveOuterParentheses2(string s)
+        {
+            int level = 0;
+            StringBuilder res = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (c == ')')
+                {
+                    level--;
+                }
+                if (level > 0)
+                {
+                    res.Append(c);
+                }
+                if (c == '(')
+                {
+                    level++;
+                }
+            }
+            return res.ToString();
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/1000-1099/demo1021/JS1021.js
@@ -0,0 +1,39 @@
+﻿//方法一： 栈
+var removeOuterParentheses = function (s) {
+    let res = '';
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        if (c === ')') {
+            stack.pop();
+        }
+        if (stack.length) {
+            res += c;
+        }
+        if (c === '(') {
+            stack.push(c);
+        }
+    }
+    return res;
+};
+
+
+//方法二：计数
+var removeOuterParentheses = function (s) {
+    let level = 0;
+    let res = '';
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        if (c === ')') {
+            level--;
+        }
+        if (level > 0) {
+            res += c;
+        }
+        if (c === '(') {
+            level++;
+        }
+    }
+    return res;
+};
+
