3bfe1c21c4f164d85cb8e8e2d75044a8f341f0cc
liupenghao@fulan.com.cn <haochen214@163.com>
2022-05-30 22:50:35 +0800

    1022. 从根到叶的二进制数之和

---
+++ Ahao.LeetCode/1000-1099/demo1022/Class1022.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1022
+{
+    public class Class1022
+    {
+        /// <summary>
+        /// 方法一：递归
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int SumRootToLeaf(TreeNode root)
+        {
+            return DFS(root, 0);
+        }
+
+        public int DFS(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            val = (val << 1) | root.val;
+            if (root.left == null && root.right == null)
+            {
+                return val;
+            }
+            return DFS(root.left, val) + DFS(root.right, val);
+        }
+
+
+        /// <summary>
+        /// 方法二：迭代
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int SumRootToLeaf2(TreeNode root)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            int val = 0, ret = 0;
+            TreeNode prev = null;
+            while (root != null || stack.Count > 0)
+            {
+                while (root != null)
+                {
+                    val = (val << 1) | root.val;
+                    stack.Push(root);
+                    root = root.left;
+                }
+                root = stack.Peek();
+                if (root.right == null || root.right == prev)
+                {
+                    if (root.left == null && root.right == null)
+                    {
+                        ret += val;
+                    }
+                    val >>= 1;
+                    stack.Pop();
+                    prev = root;
+                    root = null;
+                }
+                else
+                {
+                    root = root.right;
+                }
+            }
+            return ret;
+        }
+
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1000-1099/demo1022/JS1022.js
@@ -0,0 +1,48 @@
+﻿function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+//方法一：递归
+var sumRootToLeaf = function (root) {
+    const dfs = (root, val) => {
+        if (!root) {
+            return 0;
+        }
+        val = (val << 1) | root.val;
+        if (!root.left && !root.right) {
+            return val;
+        }
+        return dfs(root.left, val) + dfs(root.right, val);
+    }
+    return dfs(root, 0);
+};
+
+
+//方法二：迭代
+var sumRootToLeaf = function (root) {
+    const stack = [];
+    let val = 0, ret = 0;
+    let prev = null;
+    while (root || stack.length) {
+        while (root) {
+            val = (val << 1) | root.val;
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack[stack.length - 1];
+        if (!root.right || root.right === prev) {
+            if (!root.left && !root.right) {
+                ret += val;
+            }
+            val >>= 1;
+            stack.pop();
+            prev = root;
+            root = null;
+        } else {
+            root = root.right;
+        }
+    }
+    return ret;
+};
