4a7bc665360c0955dfda29efb43e71585fde7e8a
liupenghao@fulan.com.cn <haochen214@163.com>
2022-06-02 14:33:01 +0800

    450. 删除二叉搜索树中的节点

---
+++ Ahao.LeetCode/0400-0499/demo450/Class450.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0400_0499.demo450
+{
+    public class Class450
+    {
+        /// <summary>
+        /// 方法一：递归
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            if (root.val > key)
+            {
+                root.left = DeleteNode(root.left, key);
+                return root;
+            }
+            if (root.val < key)
+            {
+                root.right = DeleteNode(root.right, key);
+                return root;
+            }
+            if (root.val == key)
+            {
+                if (root.left == null && root.right == null)
+                {
+                    return null;
+                }
+                if (root.right == null)
+                {
+                    return root.left;
+                }
+                if (root.left == null)
+                {
+                    return root.right;
+                }
+                TreeNode successor = root.right;
+                while (successor.left != null)
+                {
+                    successor = successor.left;
+                }
+                root.right = DeleteNode(root.right, successor.val);
+                successor.right = root.right;
+                successor.left = root.left;
+                return successor;
+            }
+            return root;
+        }
+
+
+        /// <summary>
+        /// 方法二：迭代
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public TreeNode DeleteNode2(TreeNode root, int key)
+        {
+            TreeNode cur = root, curParent = null;
+            while (cur != null && cur.val != key)
+            {
+                curParent = cur;
+                if (cur.val > key)
+                {
+                    cur = cur.left;
+                }
+                else
+                {
+                    cur = cur.right;
+                }
+            }
+            if (cur == null)
+            {
+                return root;
+            }
+            if (cur.left == null && cur.right == null)
+            {
+                cur = null;
+            }
+            else if (cur.right == null)
+            {
+                cur = cur.left;
+            }
+            else if (cur.left == null)
+            {
+                cur = cur.right;
+            }
+            else
+            {
+                TreeNode successor = cur.right, successorParent = cur;
+                while (successor.left != null)
+                {
+                    successorParent = successor;
+                    successor = successor.left;
+                }
+                if (successorParent.val == cur.val)
+                {
+                    successorParent.right = successor.right;
+                }
+                else
+                {
+                    successorParent.left = successor.right;
+                }
+                successor.right = cur.right;
+                successor.left = cur.left;
+                cur = successor;
+            }
+            if (curParent == null)
+            {
+                return cur;
+            }
+            else
+            {
+                if (curParent.left != null && curParent.left.val == key)
+                {
+                    curParent.left = cur;
+                }
+                else
+                {
+                    curParent.right = cur;
+                }
+                return root;
+            }
+        }
+
+
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0400-0499/demo450/JS450.js
@@ -0,0 +1,88 @@
+﻿function TreeNode(val, left, right) {
+  this.val = (val === undefined ? 0 : val)
+         this.left = (left === undefined ? null : left)
+            this.right = (right === undefined ? null : right)
+                 }
+
+//方法一：递归
+var deleteNode = function (root, key) {
+    if (!root) {
+        return null;
+    }
+    if (root.val > key) {
+        root.left = deleteNode(root.left, key);
+        return root;
+    }
+    if (root.val < key) {
+        root.right = deleteNode(root.right, key);
+        return root;
+    }
+    if (root.val === key) {
+        if (!root.left && !root.right) {
+            return null;
+        }
+        if (!root.right) {
+            return root.left;
+        }
+        if (!root.left) {
+            return root.right;
+        }
+        let successor = root.right;
+        while (successor.left) {
+            successor = successor.left;
+        }
+        root.right = deleteNode(root.right, successor.val);
+        successor.right = root.right;
+        successor.left = root.left;
+        return successor;
+    }
+    return root;
+};
+
+
+//方法二：迭代
+var deleteNode = function (root, key) {
+    let cur = root, curParent = null;
+    while (cur && cur.val !== key) {
+        curParent = cur;
+        if (cur.val > key) {
+            cur = cur.left;
+        } else {
+            cur = cur.right;
+        }
+    }
+    if (!cur) {
+        return root;
+    }
+    if (!cur.left && !cur.right) {
+        cur = null;
+    } else if (!cur.right) {
+        cur = cur.left;
+    } else if (!cur.left) {
+        cur = cur.right;
+    } else {
+        let successor = cur.right, successorParent = cur;
+        while (successor.left) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successorParent.val === cur.val) {
+            successorParent.right = successor.right;
+        } else {
+            successorParent.left = successor.right;
+        }
+        successor.right = cur.right;
+        successor.left = cur.left;
+        cur = successor;
+    }
+    if (!curParent) {
+        return cur;
+    } else {
+        if (curParent.left && curParent.left.val === key) {
+            curParent.left = cur;
+        } else {
+            curParent.right = cur;
+        }
+        return root;
+    }
+};
