4c9a23c32596c51f7acd957ba8b8d75f6e2b4e31
liupenghao@fulan.com.cn <haochen214@163.com>
2022-06-04 00:29:39 +0800

    929. 独特的电子邮件地址

---
+++ Ahao.LeetCodeCPP/929. 独特的电子邮件地址.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+#include<string>
+#include<vector>
+#include <unordered_set>
+
+class Solution {
+public:
+	int numUniqueEmails(vector<string>& emails) {
+		unordered_set<string> emailSet;
+		for (auto& email : emails) {
+			string local;
+			for (char c : email) {
+				if (c == '+' || c == '@') {
+					break;
+				}
+				if (c != '.') {
+					local += c;
+				}
+			}
+			emailSet.emplace(local + email.substr(email.find('@')));
+		}
+		return emailSet.size();
+	}
+};
