f0c51d28e0e9e1e3a6958741e7a495e088e065be
liupenghao@fulan.com.cn <haochen214@163.com>
2022-06-13 09:41:30 +0800

    1051. 高度检查器

---
+++ Ahao.LeetCode/1000-1099/demo1051/Class1051.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1051
+{
+    public class Class1051
+    {
+        public int HeightChecker(int[] heights)
+        {
+            int n = heights.Length, ans = 0;
+            int[] expected = new int[n];
+            Array.Copy(heights, 0, expected, 0, n);
+            Array.Sort(expected);
+            for (int i = 0; i < n; ++i)
+            {
+                if (heights[i] != expected[i])
+                {
+                    ++ans;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1000-1099/demo1051/JS1051.js
@@ -0,0 +1,12 @@
+﻿var heightChecker = function (heights) {
+    let n = heights.length, ans = 0;
+    const expected = new Array(n).fill(0);
+    expected.splice(0, n, ...heights);
+    expected.sort((a, b) => a - b);
+    for (let i = 0; i < n; ++i) {
+        if (heights[i] !== expected[i]) {
+            ++ans;
+        }
+    }
+    return ans;
+};
