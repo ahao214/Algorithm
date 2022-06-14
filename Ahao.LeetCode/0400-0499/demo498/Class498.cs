987fe8610482df82f44c9b1bfc1ccea206cbf01b
liupenghao@fulan.com.cn <haochen214@163.com>
2022-06-14 08:55:35 +0800

    498. 对角线遍历

---
+++ Ahao.LeetCode/0400-0499/demo498/Class498.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0400_0499.demo498
+{
+    public class Class498
+    {
+        public int[] FindDiagonalOrder(int[][] mat)
+        {
+            int m = mat.Length;
+            int n = mat[0].Length;
+            int[] res = new int[m * n];
+            int pos = 0;
+            for (int i = 0; i < m + n - 1; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    int x = i < n ? 0 : i - n + 1;
+                    int y = i < n ? i : n - 1;
+                    while (x < m && y >= 0)
+                    {
+                        res[pos] = mat[x][y];
+                        pos++;
+                        x++;
+                        y--;
+                    }
+                }
+                else
+                {
+                    int x = i < m ? i : m - 1;
+                    int y = i < m ? 0 : i - m + 1;
+                    while (x >= 0 && y < n)
+                    {
+                        res[pos] = mat[x][y];
+                        pos++;
+                        x--;
+                        y++;
+                    }
+                }
+            }
+            return res;
+        }
+
+    }
+}
