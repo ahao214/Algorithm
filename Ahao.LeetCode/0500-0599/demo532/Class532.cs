aac2a4d0cb9b6c6205141b45043778d75146c34d
liupenghao@fulan.com.cn <haochen214@163.com>
2022-06-16 10:00:21 +0800

    532. 数组中的 k-diff 数对

---
+++ Ahao.LeetCode/0500-0599/demo532/Class532.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0500_0599.demo532
+{
+    public class Class532
+    {
+        /// <summary>
+        /// 哈希表
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int FindPairs(int[] nums, int k)
+        {
+            ISet<int> visited = new HashSet<int>();
+            ISet<int> res = new HashSet<int>();
+            foreach (int num in nums)
+            {
+                if (visited.Contains(num - k))
+                {
+                    res.Add(num - k);
+                }
+                if (visited.Contains(num + k))
+                {
+                    res.Add(num);
+                }
+                visited.Add(num);
+            }
+            return res.Count;
+        }
+
+
+
+        /// <summary>
+        /// 排序 + 双指针
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int FindPairs2(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int n = nums.Length, y = 0, res = 0;
+            for (int x = 0; x < n; x++)
+            {
+                if (x == 0 || nums[x] != nums[x - 1])
+                {
+                    while (y < n && (nums[y] < nums[x] + k || y <= x))
+                    {
+                        y++;
+                    }
+                    if (y < n && nums[y] == nums[x] + k)
+                    {
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/0500-0599/demo532/JS532.js
@@ -0,0 +1,33 @@
+﻿//哈希表
+var findPairs = function (nums, k) {
+    const visited = new Set();
+    const res = new Set();
+    for (const num of nums) {
+        if (visited.has(num - k)) {
+            res.add(num - k);
+        }
+        if (visited.has(num + k)) {
+            res.add(num);
+        }
+        visited.add(num);
+    }
+    return res.size;
+};
+
+
+//排序 + 双指针
+var findPairs = function (nums, k) {
+    nums.sort((a, b) => a - b);
+    let n = nums.length, y = 0, res = 0;
+    for (let x = 0; x < n; x++) {
+        if (x === 0 || nums[x] !== nums[x - 1]) {
+            while (y < n && (nums[y] < nums[x] + k || y <= x)) {
+                y++;
+            }
+            if (y < n && nums[y] === nums[x] + k) {
+                res++;
+            }
+        }
+    }
+    return res;
+};
