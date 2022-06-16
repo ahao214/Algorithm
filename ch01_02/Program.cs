95f7a528a80cb8dbba9109e2fcfaaca1ea9a4892
liupenghao@fulan.com.cn <haochen214@163.com>
2022-06-16 23:41:52 +0800

    斐波那契数列

---
+++ ch01_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace ch01_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string str;
+
+            WriteLine("使用递归计算斐波那契数列");
+            Write("请输入一个整数：");
+            str = ReadLine();
+            num = int.Parse(str);
+            if (num < 0)
+                WriteLine("输入的数字必须大于0\n");
+            else
+                Write("Fibonacci(" + num + ")=" + Fibonacci(num) + "\n");
+            ReadKey();
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n == 0) //第0项为0
+                return 0;
+            else if (n == 1)  //第1项为1
+                return 1;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
