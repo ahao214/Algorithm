7a3af6a7ff90907325b1e2740140703854813f14
liupenghao@fulan.com.cn <haochen214@163.com>
2022-06-22 11:08:26 +0800

    513. 找树左下角的值

---
+++ Ahao.LeetCode/0500-0599/demo513/Class513.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0500_0599.demo513
+{
+    public class Class513
+    {
+
+        #region 深度优先搜索
+
+        int curVal = 0;
+        int curHeight = 0;
+
+        public int FindBottomLeftValue(TreeNode root)
+        {
+            int curHeight = 0;
+            DFS(root, 0);
+            return curVal;
+        }
+
+        public void DFS(TreeNode root, int height)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            height++;
+            DFS(root.left, height);
+            DFS(root.right, height);
+            if (height > curHeight)
+            {
+                curHeight = height;
+                curVal = root.val;
+            }
+        }
+
+        #endregion
+
+
+        #region 广度优先搜索
+
+
+        public int FindBottomLeftValue2(TreeNode root)
+        {
+            int ret = 0;
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                TreeNode p = queue.Dequeue();
+                if (p.right != null)
+                {
+                    queue.Enqueue(p.right);
+                }
+                if (p.left != null)
+                {
+                    queue.Enqueue(p.left);
+                }
+                ret = p.val;
+            }
+            return ret;
+        }
+
+
+        #endregion
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0500-0599/demo513/JS513.js
@@ -0,0 +1,45 @@
+﻿//深度优先搜索
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+var findBottomLeftValue = function (root) {
+    const dfs = (root, height) => {
+        if (!root) {
+            return;
+        }
+        height++;
+        dfs(root.left, height);
+        dfs(root.right, height);
+        if (height > curHeight) {
+            curHeight = height;
+            curVal = root.val;
+        }
+    }
+
+    let curHeight = 0;
+    dfs(root, 0);
+    return curVal;
+};
+
+
+
+//广度优先搜索
+var findBottomLeftValue = function (root) {
+    let ret = 0;
+    const queue = [root];
+    while (queue.length) {
+        const p = queue.shift();
+        if (p.right) {
+            queue.push(p.right);
+        }
+        if (p.left) {
+            queue.push(p.left);
+        }
+        ret = p.val;
+    }
+    return ret;
+};
