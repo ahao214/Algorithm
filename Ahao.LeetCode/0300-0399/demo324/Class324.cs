0bc20c5308f25d47cd66573c7f00cecb3594b70d
liupenghao@fulan.com.cn <haochen214@163.com>
2022-06-28 09:00:38 +0800

    324. 摆动排序 II

---
+++ Ahao.LeetCode/0300-0399/demo324/Class324.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0300_0399.demo324
+{
+    public class Class324
+    {
+        public void WiggleSort(int[] nums)
+        {
+            int n = nums.Length;
+            int[] arr = new int[n];
+            Array.Copy(nums, arr, n);
+            Array.Sort(arr);
+            int x = (n + 1) / 2;
+            for (int i = 0, j = x - 1, k = n - 1; i < n; i += 2, j--, k--)
+            {
+                nums[i] = arr[j];
+                if (i + 1 < n)
+                {
+                    nums[i + 1] = arr[k];
+                }
+            }
+        }
+
+    }
+}
--- Ahao.LeetCodeCPP/324. 摆动排序 II.cpp
+++ Ahao.LeetCodeCPP/324. 摆动排序 II.cpp
@@ -19,3 +19,19 @@ public:
 		}
 	}
 };
+
+
+class Solution {
+public:
+	void wiggleSort(vector<int>& nums) {
+		int n = nums.size(), l = (n - 1) / 2, r = n - 1;
+		vector<int> a = nums;
+		sort(a.begin(), a.end());
+		for (int i = 0; i < n; i++) {
+			if (i % 2 == 0)
+				nums[i] = a[l--];
+			else
+				nums[i] = a[r--];
+		}
+	}
+};
