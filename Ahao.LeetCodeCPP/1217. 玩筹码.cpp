12a7b7c1d553fc34cdbeeef7313c36b45f6f4f94
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-08 12:35:12 +0800

    1217. 玩筹码

--- Ahao.LeetCodeCPP/1217. 玩筹码.cpp
+++ Ahao.LeetCodeCPP/1217. 玩筹码.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 class Solution {
 public:
-    int minCostToMoveChips(vector<int>& position) {
-        int even = 0, odd = 0;
-        for (int pos : position) {
-            if (pos % 2) {
-                odd++;
-            }
-            else {
-                even++;
-            }
-        }
-        return min(odd, even);
-    }
+	int minCostToMoveChips(vector<int>& position) {
+		int even = 0, odd = 0;
+		for (int pos : position) {
+			if (pos % 2) {
+				odd++;
+			}
+			else {
+				even++;
+			}
+		}
+		return min(odd, even);
+	}
 };
