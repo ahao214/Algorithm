2d779ba4147fbab2955045bd34d24ce0ecad8e0f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-08 12:40:23 +0800

    1217. 玩筹码

---
+++ Ahao.LeetCode/1200-1299/demo1217/Class1217.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1200_1299.demo1217
+{
+    public class Class1217
+    {
+        public int MinCostToMoveChips(int[] position)
+        {
+            int even = 0, odd = 0;
+            foreach (int pos in position)
+            {
+                if ((pos & 1) != 0)
+                    odd++;
+                else
+                    even++;
+            }
+            return Math.Min(odd, even);
+        }
+    }
+}
