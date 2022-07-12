ed6da546d202aae8179c09ddf18a5dca6b0555a1
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-12 21:19:43 +0800

    1252. 奇数值单元格的数目

---
+++ Ahao.LeetCode/1200-1299/demo1252/Class1252.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1200_1299.demo1252
+{
+    public class Class1252
+    {
+
+        /// <summary>
+        /// 直接模拟
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <param name="indices"></param>
+        /// <returns></returns>
+        public int OddCells(int m, int n, int[][] indices)
+        {
+            int res = 0;
+            int[,] matrix = new int[m, n];
+            foreach (int[] index in indices)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    matrix[index[0], i]++;
+                }
+                for (int i = 0; i < m; i++)
+                {
+                    matrix[i, index[1]]++;
+                }
+            }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((matrix[i, j] & 1) != 0)
+                    {
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+
+
+        /// <summary>
+        /// 方法二：模拟空间优化
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <param name="indices"></param>
+        /// <returns></returns>
+
+        public int OddCells2(int m, int n, int[][] indices)
+        {
+            int[] rows = new int[m];
+            int[] cols = new int[n];
+            foreach (int[] index in indices)
+            {
+                rows[index[0]]++;
+                cols[index[1]]++;
+            }
+            int res = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (((rows[i] + cols[j]) & 1) != 0)
+                    {
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// 方法三：计数优化
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <param name="indices"></param>
+        /// <returns></returns>
+        public int OddCells3(int m, int n, int[][] indices)
+        {
+            int[] rows = new int[m];
+            int[] cols = new int[n];
+            foreach (int[] index in indices)
+            {
+                rows[index[0]]++;
+                cols[index[1]]++;
+            }
+            int oddx = 0, oddy = 0;
+            for (int i = 0; i < m; i++)
+            {
+                if ((rows[i] & 1) != 0)
+                {
+                    oddx++;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if ((cols[i] & 1) != 0)
+                {
+                    oddy++;
+                }
+            }
+            return oddx * (n - oddy) + (m - oddx) * oddy;
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/1200-1299/demo1252/JS1252.js
@@ -0,0 +1,66 @@
+﻿//方法一：直接模拟
+var oddCells = function (m, n, indices) {
+    let res = 0;
+    const matrix = new Array(m).fill(0).map(() => new Array(n).fill(0));
+    for (const index of indices) {
+        for (let i = 0; i < n; i++) {
+            matrix[index[0]][i]++;
+        }
+        for (let i = 0; i < m; i++) {
+            matrix[i][index[1]]++;
+        }
+    }
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if ((matrix[i][j] & 1) !== 0) {
+                res++;
+            }
+        }
+    }
+    return res;
+};
+
+
+//方法二：模拟空间优化
+var oddCells = function (m, n, indices) {
+    const rows = new Array(m).fill(0);
+    const cols = new Array(n).fill(0);
+    for (const index of indices) {
+        rows[index[0]]++;
+        cols[index[1]]++;
+    }
+    let res = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (((rows[i] + cols[j]) & 1) !== 0) {
+                res++;
+            }
+        }
+    }
+    return res;
+};
+
+
+
+//方法三：计数优化
+var oddCells = function (m, n, indices) {
+    const rows = new Array(m).fill(0);
+    const cols = new Array(n).fill(0);
+    for (const index of indices) {
+        rows[index[0]]++;
+        cols[index[1]]++;
+    }
+    let oddx = 0, oddy = 0;
+    for (let i = 0; i < m; i++) {
+        if ((rows[i] & 1) !== 0) {
+            oddx++;
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        if ((cols[i] & 1) !== 0) {
+            oddy++;
+        }
+    }
+    return oddx * (n - oddy) + (m - oddx) * oddy;
+};
+
