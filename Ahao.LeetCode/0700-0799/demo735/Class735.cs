e42d664c936ce9641eff961f0a70f72683cd3bb8
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-13 19:36:54 +0800

    735. 行星碰撞

---
+++ Ahao.LeetCode/0700-0799/demo735/Class735.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo735
+{
+    public class Class735
+    {
+        public int[] AsteroidCollision(int[] asteroids)
+        {
+            Stack<int> stack = new Stack<int>();
+            foreach (int aster in asteroids)
+            {
+                bool alive = true;
+                while (alive && aster < 0 && stack.Count > 0 && stack.Peek() > 0)
+                {
+                    alive = stack.Peek() < -aster; // aster 是否存在
+                    if (stack.Peek() <= -aster)
+                    {  // 栈顶行星爆炸
+                        stack.Pop();
+                    }
+                }
+                if (alive)
+                {
+                    stack.Push(aster);
+                }
+            }
+            int count = stack.Count;
+            int[] ans = new int[count];
+            for (int i = count - 1; i >= 0; i--)
+            {
+                ans[i] = stack.Pop();
+            }
+            return ans;
+        }
+
+    }
+}
