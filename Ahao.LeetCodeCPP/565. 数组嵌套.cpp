07b6cad197d29c1a287c607bb6442e03a9c61027
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-17 13:03:12 +0800

    565. 数组嵌套

--- Ahao.LeetCodeCPP/565. 数组嵌套.cpp
+++ Ahao.LeetCodeCPP/565. 数组嵌套.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//方法一：图
 class Solution {
 public:
 	int arrayNesting(vector<int>& nums) {
@@ -12,6 +13,26 @@ public:
 			while (!vis[i]) {
 				vis[i] = true;
 				i = nums[i];
+				++cnt;
+			}
+			ans = max(ans, cnt);
+		}
+		return ans;
+	}
+};
+
+
+//方法二：原地标记
+class Solution {
+public:
+	int arrayNesting(vector<int>& nums) {
+		int ans = 0, n = nums.size();
+		for (int i = 0; i < n; ++i) {
+			int cnt = 0;
+			while (nums[i] < n) {
+				int num = nums[i];
+				nums[i] = n;
+				i = num;
 				++cnt;
 			}
 			ans = max(ans, cnt);
