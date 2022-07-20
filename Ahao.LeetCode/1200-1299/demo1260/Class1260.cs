7f46032effffaa539d4e8947c90d8f2ae67b1a72
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-20 20:20:03 +0800

    1260. 二维网格迁移

---
+++ Ahao.LeetCode/1200-1299/demo1260/Class1260.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1200_1299.demo1260
+{
+    public class Class1260
+    {
+        public IList<IList<int>> ShiftGrid(int[][] grid, int k)
+        {
+            int m = grid.Length, n = grid[0].Length;
+            IList<IList<int>> res = new List<IList<int>>();
+
+            for (int i = 0; i < m; i++)
+            {
+                IList<int> row = new List<int>();
+                for (int j = 0; j < n; j++)
+                {
+                    row.Add(0);
+                }
+                res.Add(row);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int index = (i * n + j + k) % (m * n);
+                    res[index / n][index % n] = grid[i][j];
+                }
+            }
+
+            return res;
+
+        }
+    }
+}
---
+++ Ahao.LeetCode/1200-1299/demo1260/JS1260.js
@@ -0,0 +1,20 @@
+﻿var shiftGrid = function (grid, k) {
+    const m = grid.length, n = grid[0].length;
+    const res = [];
+    for (let i = 0; i < m; i++) {
+        const row = [];
+        for (let j = 0; j < n; j++) {
+            row.push(0);
+        }
+        res.push(row);
+    }
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            const index = (i * n + j + k) % (m * n);
+            res[Math.floor(index / n)].splice(index % n, 1, grid[i][j]);
+        }
+    }
+
+    return res;
+};
