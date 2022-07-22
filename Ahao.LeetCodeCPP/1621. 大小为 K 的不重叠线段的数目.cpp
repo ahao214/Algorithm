cae0428aed71fc73ee96dd8ebd9f7a00f2fc3217
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-22 09:23:24 +0800

    1621. 大小为 K 的不重叠线段的数目

--- Ahao.LeetCodeCPP/1621. 大小为 K 的不重叠线段的数目.cpp
+++ Ahao.LeetCodeCPP/1621. 大小为 K 的不重叠线段的数目.cpp
@@ -37,3 +37,33 @@ public:
 		return (dp0[n - 1][K] + dp1[n - 1][K]) % M;
 	}
 };
+
+
+
+class Solution {
+	long dp[1001][1001];
+	long sum[1001][1001];
+	long MOD = 1e9 + 7;
+
+public:
+	int numberOfSets(int n, int k)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			dp[i][0] = 1;
+			sum[i][0] = i + 1;
+		}
+
+		for (int i = 1; i < n; i++)
+			for (int k = 1; k <= min(i, k); k++)
+			{
+				dp[i][k] += sum[i - 1][k - 1];
+				dp[i][k] += dp[i - 1][k];
+				dp[i][k] %= MOD;
+
+				sum[i][k] = sum[i - 1][k] + dp[i][k];
+				sum[i][k] %= MOD;
+			}
+		return dp[n - 1][K] % MOD;
+	}
+};
