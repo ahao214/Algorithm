2e8d2f08230a72c15acec3eb61c8898b36aa9baf
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-26 09:22:16 +0800

    219. 存在重复元素 II

---
+++ Ahao.LeetCodeCPP/219. 存在重复元素 II.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+#include<set>
+
+using namespace std;
+
+
+/*
+219. 存在重复元素 II
+时间O(N) 空间O(1)
+*/
+class Solution {
+public:
+	bool containsNearbyDuplicate(vector<int>& nums, int k) {
+		set<int> st;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (st.count(nums[i]))
+				return true;
+			st.insert(nums[i]);
+			if (st.size() > k)
+				st.erase(nums[i - k]);
+		}
+		return false;
+	}
+};
