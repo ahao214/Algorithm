cc163eb3685836dff57fbc9735676c6dec5dc407
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-26 09:31:22 +0800

    offer 55 二叉树的深度

---
+++ Ahao.OfferCPP/offer 55 二叉树的深度.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+
+using namespace std;
+
+
+/*
+offer 55 二叉树的深度
+*/
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+	TreeNode(int x) :
+		val(x), left(NULL), right(NULL) {
+	}
+};
+
+class Solution {
+public:
+	int TreeDepth(TreeNode* pRoot) {
+		if (!pRoot) return 0;
+		return max(TreeDepth(pRoot->left), TreeDepth(pRoot->right)) + 1;
+	}
+};
