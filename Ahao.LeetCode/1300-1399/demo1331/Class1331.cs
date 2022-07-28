0ecf98125fdb00931e65091d2dfddc92b6a54efc
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-28 08:57:26 +0800

    1331. 数组序号转换

---
+++ Ahao.LeetCode/1300-1399/demo1331/Class1331.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1300_1399.demo1331
+{
+    public class Class1331
+    {
+
+        public int[] ArrayRankTransform(int[] arr)
+        {
+            int len = arr.Length;
+            int[] sortedArr = new int[len];
+            Array.Copy(arr, 0, sortedArr, 0, len);
+            Array.Sort(sortedArr);
+            Dictionary<int, int> ranks = new Dictionary<int, int>();
+            int[] ans = new int[len];
+            foreach (int a in sortedArr)
+            {
+                ranks.TryAdd(a, ranks.Count + 1);
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                ans[i] = ranks[arr[i]];
+            }
+            return ans;
+        }
+
+    }
+}
---
+++ Ahao.LeetCodeCPP/1331. 数组序号转换.cpp
@@ -0,0 +1,28 @@
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+
+using namespace std;
+
+
+/*
+1331. Êý×éÐòºÅ×ª»»
+*/
+class Solution {
+public:
+	vector<int> arrayRankTransform(vector<int>& arr) {
+		vector<int> sortedArr = arr;
+		sort(sortedArr.begin(), sortedArr.end());
+		unordered_map<int, int> ranks;
+		vector<int> ans(arr.size());
+		for (auto& a : sortedArr) {
+			if (!ranks.count(a)) {
+				ranks[a] = ranks.size() + 1;
+			}
+		}
+		for (int i = 0; i < arr.size(); i++) {
+			ans[i] = ranks[arr[i]];
+		}
+		return ans;
+	}
+};
