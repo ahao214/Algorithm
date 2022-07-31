250b31408260f82059c1c34253151d7b6eac2d27
liupenghao@fulan.com.cn <haochen214@163.com>
2022-07-31 10:42:44 +0800

    1161. 最大层内元素和

---
+++ Ahao.LeetCode/1100-1199/demo1161/Class1161.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1100_1199.demo1161
+{
+    public class Class1161
+    {
+        private IList<int> sum = new List<int>();
+        /// <summary>
+        /// 深度优先搜索
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int MaxLevelSum(TreeNode root)
+        {
+            DFS(root, 0);
+            int ans = 0;
+            for (int i = 0; i < sum.Count; ++i)
+            {
+                if (sum[i] > sum[ans])
+                {
+                    ans = i;
+                }
+            }
+            return ans + 1; // 层号从 1 开始
+        }
+
+        private void DFS(TreeNode node, int level)
+        {
+            if (level == sum.Count)
+            {
+                sum.Add(node.val);
+            }
+            else
+            {
+                sum[level] += node.val;
+            }
+            if (node.left != null)
+            {
+                DFS(node.left, level + 1);
+            }
+            if (node.right != null)
+            {
+                DFS(node.right, level + 1);
+            }
+        }
+
+
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1100-1199/demo1161/JS1161.js
@@ -0,0 +1,34 @@
+﻿function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+
+//深度优先
+var maxLevelSum = function (root) {
+    const sum = [];
+    const dfs = (node, level) => {
+        if (level === sum.length) {
+            sum.push(node.val);
+        } else {
+            sum.splice(level, 1, sum[level] + node.val);
+        }
+        if (node.left) {
+            dfs(node.left, level + 1);
+        }
+        if (node.right) {
+            dfs(node.right, level + 1);
+        }
+    }
+    dfs(root, 0);
+    let ans = 0;
+    for (let i = 0; i < sum.length; ++i) {
+        if (sum[i] > sum[ans]) {
+            ans = i;
+        }
+    }
+    return ans + 1; // 层号从 1 开始
+};
+
+//广度优先
