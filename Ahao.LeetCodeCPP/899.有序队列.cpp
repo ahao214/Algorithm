fb678204fb3e270a611889d0b811be362c17e517
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-03 09:10:49 +0800

    899.有序队列

---
+++ Ahao.LeetCodeCPP/899.有序队列.cpp
@@ -0,0 +1,31 @@
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+/*
+899.”––Ú∂”¡–
+*/
+class Solution {
+public:
+	string orderlyQueue(string s, int k) {
+		if (k == 1) {
+			string smallest = s;
+			int n = s.size();
+			for (int i = 1; i < n; i++) {
+				char c = s[0];
+				s = s.substr(1);
+				s.push_back(c);
+				if (s < smallest) {
+					smallest = s;
+				}
+			}
+			return smallest;
+		}
+		else {
+			sort(s.begin(), s.end());
+			return s;
+		}
+	}
+};
