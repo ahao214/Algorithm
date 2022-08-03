5c1b2d7fd1e0f588b68a29c57a3ce20041705e8a
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-03 09:08:58 +0800

    offer 10 斐波那契数列

---
+++ Ahao.OfferCPP/offer 10 斐波那契数列.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+
+
+using namespace std;
+
+
+/*
+offer 10 ì³²¨ÄÇÆõÊıÁĞ
+*/
+class Solution {
+public:
+	int Fibonacci(int n) {
+		if (n <= 0)return 0;
+		if (n == 1)return 1;
+		int a = 0, b = 1, res = 0;
+		for (int i = 2; i <= n; ++i)
+		{
+			res = a + b;
+			a = b;
+			b = res;
+		}
+		return res;
+	}
+};
