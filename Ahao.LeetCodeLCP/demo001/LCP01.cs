76d2e6d7a432048e1b77b43cdb7d1526f2c1a10c
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-03 17:29:16 +0800

    LCP 01. 猜数字

---
+++ Ahao.LeetCodeLCP/demo001/LCP01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCodeLCP
+{
+    public class LCP01
+    {
+        /// <summary>
+        /// LCP 01. 猜数字
+        /// </summary>
+        /// <param name="guess"></param>
+        /// <param name="answer"></param>
+        /// <returns></returns>
+        public int Game(int[] guess, int[] answer)
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+                count += guess[i] == answer[i] ? 1 : 0;
+            return count;
+        }
+    }
+}
