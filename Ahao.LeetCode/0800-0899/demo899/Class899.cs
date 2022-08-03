37c3a8a255c97000c3b77b0c6556b55f0a3efea9
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-03 09:13:57 +0800

    899. 有序队列

---
+++ Ahao.LeetCode/0800-0899/demo899/Class899.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo899
+{
+    public class Class899
+    {
+        public string OrderlyQueue(string s, int k)
+        {
+            if (k == 1)
+            {
+                string smallest = s;
+                StringBuilder sb = new StringBuilder(s);
+                int n = s.Length;
+                for (int i = 1; i < n; i++)
+                {
+                    char c = sb[0];
+                    sb.Remove(0, 1);
+                    sb.Append(c);
+                    if (sb.ToString().CompareTo(smallest) < 0)
+                    {
+                        smallest = sb.ToString();
+                    }
+                }
+                return smallest;
+            }
+            else
+            {
+                char[] arr = s.ToCharArray();
+                Array.Sort(arr);
+                return new string(arr);
+            }
+        }
+    }
+}
