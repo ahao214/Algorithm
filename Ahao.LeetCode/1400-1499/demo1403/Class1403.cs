1c112646d54238bbcf136310d0d6f7146be0c18e
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-04 08:57:07 +0800

    1403. 非递增顺序的最小子序列

--- Ahao.LeetCode/1400-1499/demo1403/Class1403.cs
+++ Ahao.LeetCode/1400-1499/demo1403/Class1403.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode._1400_1499.demo1403
 {
-    class Class1403
+    public class Class1403
     {
+        public IList<int> MinSubsequence(int[] nums)
+        {
+            int total = nums.Sum();
+            Array.Sort(nums);
+            IList<int> res = new List<int>();
+            int curr = 0;
+            for (int i = nums.Length - 1; i >= 0; --i)
+            {
+                curr += nums[i];
+                res.Add(nums[i]);
+                if (curr + curr > total)
+                    break;
+            }
+            return res;
+        }
+
     }
 }
