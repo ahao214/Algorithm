a3765a12a47482c9fe6002bd51ec76d56ead99ef
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-06 00:10:03 +0800

    1408. 数组中的字符串匹配

---
+++ Ahao.LeetCodeCPP/1408. 数组中的字符串匹配.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+#include<string>
+
+using namespace std;
+
+
+/*
+1408. 数组中的字符串匹配
+*/
+class Solution {
+public:
+	vector<string> stringMatching(vector<string>& words) {
+		vector<string> ret;
+		for (int i = 0; i < words.size(); i++) {
+			for (int j = 0; j < words.size(); j++) {
+				if (i != j && words[j].find(words[i]) != string::npos) {
+					ret.push_back(words[i]);
+					break;
+				}
+			}
+		}
+		return ret;
+	}
+};
