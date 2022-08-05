30928eead32c5b0bac418302dff40d3dabc06019
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-06 00:11:39 +0800

    1408. 数组中的字符串匹配

---
+++ Ahao.LeetCode/1400-1499/demo1408/Class1408.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1400_1499.demo1408
+{
+    public class Class1408
+    {
+        public IList<string> StringMatching(string[] words)
+        {
+            IList<string> ret = new List<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (i != j && words[j].Contains(words[i]))
+                    {
+                        ret.Add(words[i]);
+                        break;
+                    }
+                }
+            }
+            return ret;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1400-1499/demo1408/JS1408.js
@@ -0,0 +1,12 @@
+﻿var stringMatching = function (words) {
+    const ret = [];
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words.length; j++) {
+            if (i !== j && words[j].search(words[i]) !== -1) {
+                ret.push(words[i]);
+                break;
+            }
+        }
+    }
+    return ret;
+};
