31d385da67a6c426727a7ef686864f2660c25beb
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-05 08:45:25 +0800

    623. 在二叉树中增加一行

---
+++ Ahao.LeetCode/0600-0699/demo623/Class623.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0600_0699.demo623
+{
+    class Class623
+    {
+    }
+}
---
+++ Ahao.LeetCode/0600-0699/demo623/JS623.js
@@ -0,0 +1,50 @@
+﻿function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+//深度优先
+var addOneRow = function (root, val, depth) {
+    if (!root) {
+        return null;
+    }
+    if (depth === 1) {
+        return new TreeNode(val, root, null);
+    }
+    if (depth === 2) {
+        root.left = new TreeNode(val, root.left, null);
+        root.right = new TreeNode(val, null, root.right);
+    } else {
+        root.left = addOneRow(root.left, val, depth - 1);
+        root.right = addOneRow(root.right, val, depth - 1);
+    }
+    return root;
+};
+
+
+//广度优先
+var addOneRow = function (root, val, depth) {
+    if (depth === 1) {
+        return new TreeNode(val, root, null);
+    }
+    let curLevel = [];
+    curLevel.push(root);
+    for (let i = 1; i < depth - 1; i++) {
+        const tmp = [];
+        for (const node of curLevel) {
+            if (node.left) {
+                tmp.push(node.left);
+            }
+            if (node.right) {
+                tmp.push(node.right);
+            }
+        }
+        curLevel = tmp;
+    }
+    for (const node of curLevel) {
+        node.left = new TreeNode(val, node.left, null);
+        node.right = new TreeNode(val, null, node.right);
+    }
+    return root;
+};
