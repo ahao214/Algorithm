232b52cdd84a5569f16a8482e9bf328c89e7ca83
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-13 11:17:18 +0800

    1877. 数组中最大数对和的最小值

---
+++ Ahao.LeetCode/1800-1899/demo1877/Class1877.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1877
+{
+    public class Class1877
+    {
+        public int MinPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+            int res = 0;
+            for (int i = 0, j = nums.Length - 1; i < j; i++, j--)
+            {
+                res = Math.Max(res, nums[i] + nums[j]);
+            }
+            return res;
+        }
+    }
+}
