785f51072889f378a95c95d7dce41014d0ee1016
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-13 11:29:28 +0800

    1876. 长度为三且各字符不同的子字符串

---
+++ Ahao.LeetCode/1800-1899/demo1876/Class1876.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1876
+{
+    public class Class1876
+    {
+        public int CountGoodSubstrings(string s)
+        {
+            int res = 0;
+            for (int i = 0; i + 2 < s.Length; i++)
+            {
+                int a = i, b = i + 1, c = i + 2;
+                if (s[a] != s[b] && s[b] != s[c] && s[a] != s[c])
+                    res++;
+            }
+            return res;
+        }
+    }
+}
