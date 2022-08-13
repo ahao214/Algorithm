d226620123a564c6a57fa293bda57da3d0793086
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-13 09:41:03 +0800

    768. 最多能完成排序的块 II

---
+++ Ahao.LeetCode/0700-0799/demo768/Class768.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo768
+{
+    public class Class768
+    {
+        public int MaxChunksToSorted(int[] arr)
+        {
+            Dictionary<int, int> cnt = new Dictionary<int, int>();
+            int res = 0;
+            int[] sortedArr = new int[arr.Length];
+            Array.Copy(arr, 0, sortedArr, 0, arr.Length);
+            Array.Sort(sortedArr);
+            for (int i = 0; i < sortedArr.Length; i++)
+            {
+                int x = arr[i], y = sortedArr[i];
+                if (!cnt.ContainsKey(x))
+                {
+                    cnt.Add(x, 0);
+                }
+                cnt[x]++;
+                if (cnt[x] == 0)
+                {
+                    cnt.Remove(x);
+                }
+                if (!cnt.ContainsKey(y))
+                {
+                    cnt.Add(y, 0);
+                }
+                cnt[y]--;
+                if (cnt[y] == 0)
+                {
+                    cnt.Remove(y);
+                }
+                if (cnt.Count == 0)
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0700-0799/demo768/JS768.js
@@ -0,0 +1,22 @@
+﻿var maxChunksToSorted = function (arr) {
+    const cnt = new Map();
+    let res = 0;
+    const sortedArr = new Array(arr.length).fill(0);
+    sortedArr.splice(0, arr.length, ...arr);
+    sortedArr.sort((a, b) => a - b);
+    for (let i = 0; i < sortedArr.length; i++) {
+        const x = arr[i], y = sortedArr[i];
+        cnt.set(x, (cnt.get(x) || 0) + 1);
+        if (cnt.get(x) === 0) {
+            cnt.delete(x);
+        }
+        cnt.set(y, (cnt.get(y) || 0) - 1);
+        if (cnt.get(y) === 0) {
+            cnt.delete(y);
+        }
+        if (cnt.size === 0) {
+            res++;
+        }
+    }
+    return res;
+};
