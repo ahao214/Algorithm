89279260fbca4d681b64550f036b9d3a56a32e3e
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-14 12:01:50 +0800

    1422. 分割字符串的最大得分

---
+++ Ahao.LeetCode/1400-1499/demo1422/Class1422.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1400_1499.demo1422
+{
+    public class Class1422
+    {
+        public int MaxScore(string s)
+        {
+            int ans = 0;
+            int n = s.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int score = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    if (s[j] == '0')
+                    {
+                        score++;
+                    }
+                }
+                for (int j = i; j < n; j++)
+                {
+                    if (s[j] == '1')
+                    {
+                        score++;
+                    }
+                }
+                ans = Math.Max(ans, score);
+            }
+            return ans;
+        }
+
+
+
+        public int MaxScore2(string s)
+        {
+            int score = 0;
+            int n = s.Length;
+            if (s[0] == '0')
+            {
+                score++;
+            }
+            for (int i = 1; i < n; i++)
+            {
+                if (s[i] == '1')
+                {
+                    score++;
+                }
+            }
+            int ans = score;
+            for (int i = 1; i < n - 1; i++)
+            {
+                if (s[i] == '0')
+                {
+                    score++;
+                }
+                else
+                {
+                    score--;
+                }
+                ans = Math.Max(ans, score);
+            }
+            return ans;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1400-1499/demo1422/JS1422.js
@@ -0,0 +1,44 @@
+﻿var maxScore = function (s) {
+    let ans = 0;
+    const n = s.length;
+    for (let i = 1; i < n; i++) {
+        let score = 0;
+        for (let j = 0; j < i; j++) {
+            if (s[j] === '0') {
+                score++;
+            }
+        }
+        for (let j = i; j < n; j++) {
+            if (s[j] === '1') {
+                score++;
+            }
+        }
+        ans = Math.max(ans, score);
+    }
+    return ans;
+};
+
+
+
+var maxScore = function (s) {
+    let score = 0;
+    const n = s.length;
+    if (s[0] === '0') {
+        score++;
+    }
+    for (let i = 1; i < n; i++) {
+        if (s[i] === '1') {
+            score++;
+        }
+    }
+    let ans = score;
+    for (let i = 1; i < n - 1; i++) {
+        if (s[i] == '0') {
+            score++;
+        } else {
+            score--;
+        }
+        ans = Math.max(ans, score);
+    }
+    return ans;
+};
