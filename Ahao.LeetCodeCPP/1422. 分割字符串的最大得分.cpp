19c55589ed3fcd1585ac113700859a1daa63acc1
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-14 11:58:57 +0800

    1422. 分割字符串的最大得分

---
+++ Ahao.LeetCodeCPP/1422. 分割字符串的最大得分.cpp
@@ -0,0 +1,56 @@
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+	int maxScore(string s) {
+		int ans = 0;
+		int n = s.size();
+		for (int i = 1; i < n; i++) {
+			int score = 0;
+			for (int j = 0; j < i; j++) {
+				if (s[j] == '0') {
+					score++;
+				}
+			}
+			for (int j = i; j < n; j++) {
+				if (s[j] == '1') {
+					score++;
+				}
+			}
+			ans = max(ans, score);
+		}
+		return ans;
+	}
+};
+
+
+
+class Solution {
+public:
+	int maxScore(string s) {
+		int score = 0;
+		int n = s.size();
+		if (s[0] == '0') {
+			score++;
+		}
+		for (int i = 1; i < n; i++) {
+			if (s[i] == '1') {
+				score++;
+			}
+		}
+		int ans = score;
+		for (int i = 1; i < n - 1; i++) {
+			if (s[i] == '0') {
+				score++;
+			}
+			else {
+				score--;
+			}
+			ans = max(ans, score);
+		}
+		return ans;
+	}
+};
