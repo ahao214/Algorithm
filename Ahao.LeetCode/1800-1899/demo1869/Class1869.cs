181ed0d0fc36d0941ad12b74a1530e0ae09e0127
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-15 13:21:37 +0800

    1869. 哪种连续子字符串更长

---
+++ Ahao.LeetCode/1800-1899/demo1869/Class1869.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1869
+{
+    public class Class1869
+    {
+        public bool CheckZeroOnes(string s)
+        {
+            int one = 0;
+            int zero = 0;
+            int s1 = 0;
+            int s0 = 0;
+            foreach (var c in s)
+            {
+                if (c == '0')
+                {
+                    s0++;
+                    s1 = 0;
+                }
+                else
+                {
+                    s1++;
+                    s0 = 0;
+                }
+                one = Math.Max(one, s1);
+                zero = Math.Max(zero, s0);
+            }
+            return one > zero;
+        }
+    }
+}
