637388d803ab21dc775a600bb587eba82e64509e
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-17 09:19:19 +0800

    1302. 层数最深叶子节点的和

---
+++ Ahao.LeetCode/1300-1399/demo1302/Class1302.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1300_1399.demo1302
+{
+    public class Class1302
+    {
+
+        int maxLevel = -1;
+        int sum = 0;
+
+        public int DeepestLeavesSum(TreeNode root)
+        {
+            DFS(root, 0);
+            return sum;
+        }
+
+        public void DFS(TreeNode node, int level)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            if (level > maxLevel)
+            {
+                maxLevel = level;
+                sum = node.val;
+            }
+            else if (level == maxLevel)
+            {
+                sum += node.val;
+            }
+            DFS(node.left, level + 1);
+            DFS(node.right, level + 1);
+        }
+
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1300-1399/demo1302/JS1302.js
@@ -0,0 +1,25 @@
+﻿function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+var deepestLeavesSum = function (root) {
+    let maxLevel = -1;
+    let sum = 0;
+    const dfs = (node, level) => {
+        if (!node) {
+            return;
+        }
+        if (level > maxLevel) {
+            maxLevel = level;
+            sum = node.val;
+        } else if (level === maxLevel) {
+            sum += node.val;
+        }
+        dfs(node.left, level + 1);
+        dfs(node.right, level + 1);
+    }
+    dfs(root, 0);
+    return sum;
+};
