851e39c5251d84d17e4eb61b23e74aa4001b3566
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-19 08:52:50 +0800

    1450. 在既定时间做作业的学生人数

---
+++ Ahao.LeetCode/1400-1499/demo1450/Class1450.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1400_1499.demo1450
+{
+    public class Class1450
+    {
+        //枚举
+        public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+        {
+            int n = startTime.Length;
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (startTime[i] <= queryTime && endTime[i] >= queryTime)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+
+        //差分数组
+        public int BusyStudent2(int[] startTime, int[] endTime, int queryTime)
+        {
+            int n = startTime.Length;
+            int maxEndTime = endTime.Max();
+            if (queryTime > maxEndTime)
+            {
+                return 0;
+            }
+            int[] cnt = new int[maxEndTime + 2];
+            for (int i = 0; i < n; i++)
+            {
+                cnt[startTime[i]]++;
+                cnt[endTime[i] + 1]--;
+            }
+            int ans = 0;
+            for (int i = 0; i <= queryTime; i++)
+            {
+                ans += cnt[i];
+            }
+            return ans;
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/1400-1499/demo1450/JS1450.js
@@ -0,0 +1,32 @@
+﻿//枚举
+var busyStudent = function (startTime, endTime, queryTime) {
+    const n = startTime.length;
+    let ans = 0;
+    for (let i = 0; i < n; i++) {
+        if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+            ans++;
+        }
+    }
+    return ans;
+};
+
+
+//差分数组
+var busyStudent = function (startTime, endTime, queryTime) {
+    const n = startTime.length;
+    const maxEndTime = _.max(endTime);
+    if (queryTime > maxEndTime) {
+        return 0;
+    }
+    const cnt = new Array(maxEndTime + 2).fill(0);
+    for (let i = 0; i < n; i++) {
+        cnt[startTime[i]]++;
+        cnt[endTime[i] + 1]--;
+    }
+    let ans = 0;
+    for (let i = 0; i <= queryTime; i++) {
+        ans += cnt[i];
+    }
+    return ans;
+};
+
