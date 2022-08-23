2e5204485efc89771784c8d28dfe36db1f5964c3
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-23 09:03:23 +0800

    782. 变为棋盘

---
+++ Ahao.LeetCode/0700-0799/demo782/Class782.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo782
+{
+    public class Class782
+    {
+    }
+}
