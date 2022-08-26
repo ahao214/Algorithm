3b6809dfbc1c7a806b7599da746e41c44941a342
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-26 09:58:32 +0800

    1464. 数组中两元素的最大乘积

---
+++ Ahao.LeetCode/1600-1699/demo1464/Class1464.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1464
+{
+    public class Class1464
+    {
+        public int MaxProduct(int[] nums)
+        {
+            Array.Sort(nums);
+            return (nums[nums.Length - 1] - 1) * (nums[nums.Length - 2] - 1);
+        }
+
+        public int MaxProduct2(int[] nums)
+        {
+            int a = nums[0], b = nums[1];
+            if (a < b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            for (int i = 2; i < nums.Length; i++)
+            {
+                if (nums[i] > a)
+                {
+                    b = a;
+                    a = nums[i];
+                }
+                else if (nums[i] > b)
+                {
+                    b = nums[i];
+                }
+            }
+            return (a - 1) * (b - 1);
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/1600-1699/demo1464/JS1464.js
@@ -0,0 +1,24 @@
+﻿var maxProduct = function (nums) {
+    nums.sort((a, b) => a - b);
+    console.log(nums)
+    return (nums[nums.length - 1] - 1) * (nums[nums.length - 2] - 1);
+};
+
+
+var maxProduct = function (nums) {
+    let a = nums[0], b = nums[1];
+    if (a < b) {
+        const temp = a;
+        a = b;
+        b = temp;
+    }
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] > a) {
+            b = a;
+            a = nums[i];
+        } else if (nums[i] > b) {
+            b = nums[i];
+        }
+    }
+    return (a - 1) * (b - 1);
+};
