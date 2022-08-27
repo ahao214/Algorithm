e084099523bb3eaeecad70c3c77246df54520939
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-27 20:05:53 +0800

    1844. 将所有数字用字符替换

---
+++ Ahao.LeetCodeCPP/1844. 将所有数字用字符替换.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+#include<string>
+
+using namespace std;
+
+
+
+/*
+1844. 将所有数字用字符替换
+给你一个下标从 0 开始的字符串 s ，它的 偶数 下标处为小写英文字母，奇数 下标处为数字。
+
+定义一个函数 shift(c, x) ，其中 c 是一个字符且 x 是一个数字，函数返回字母表中 c 后面第 x 个字符。
+
+比方说，shift('a', 5) = 'f' 和 shift('x', 0) = 'x' 。
+对于每个 奇数 下标 i ，你需要将数字 s[i] 用 shift(s[i-1], s[i]) 替换。
+
+请你替换所有数字以后，将字符串 s 返回。题目 保证 shift(s[i-1], s[i]) 不会超过 'z' 。
+*/
+class Solution {
+public:
+	string replaceDigits(string s) {
+		for (int i = 1; i < s.size(); i += 2)
+			s[i] = s[i - 1] + s[i] - '0';
+		return s;
+	}
+};
---
+++ Ahao.LeetCodeCPP/1845. 座位预约管理系统.cpp
@@ -0,0 +1,36 @@
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+
+
+/*
+1845. 座位预约管理系统
+请你设计一个管理 n 个座位预约的系统，座位编号从 1 到 n 。
+
+请你实现 SeatManager 类：
+
+SeatManager(int n) 初始化一个 SeatManager 对象，它管理从 1 到 n 编号的 n 个座位。所有座位初始都是可预约的。
+int reserve() 返回可以预约座位的 最小编号 ，此座位变为不可预约。
+void unreserve(int seatNumber) 将给定编号 seatNumber 对应的座位变成可以预约。
+*/
+class Solution {
+public:
+	priority_queue<int, vector<int>, greater<int>> heap;
+	void SeatManager(int n) {
+		for (int i = 1; i <= n; i++)
+			heap.push(i);
+	}
+
+	int reserve() {
+		int t = heap.top();
+		heap.pop();
+		return t;
+	}
+
+	void unreserve(int seatNumber) {
+		heap.push(seatNumber);
+	}
+
+};
