ec2e6d1d8a29bc8e0240756a0d3b48e8168007ef
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-28 11:14:25 +0800

    793. 阶乘函数后 K 个零

---
+++ Ahao.LeetCode/0700-0799/demo793/Class793.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo793
+{
+    public class Class793
+    {
+        public int PreimageSizeFZF(int k)
+        {
+            return (int)(Help(k + 1) - Help(k));
+        }
+
+        public long Help(int k)
+        {
+            long r = 5L * k;
+            long l = 0;
+            while (l <= r)
+            {
+                long mid = (l + r) / 2;
+                if (Zeta(mid) < k)
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+            return r + 1;
+        }
+
+        public long Zeta(long x)
+        {
+            long res = 0;
+            while (x != 0)
+            {
+                res += x / 5;
+                x /= 5;
+            }
+            return res;
+        }
+
+    }
+}
