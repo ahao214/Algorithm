e28a8ffbbc6f7ce0805943b78c7b56f5e20cd835
liupenghao@fulan.com.cn <haochen214@163.com>
2022-08-30 08:45:16 +0800

    998. 最大二叉树 II

---
+++ Ahao.LeetCode/0900-0999/demo998/Class998.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0900_0999.demo998
+{
+    public class Class998
+    {
+        public TreeNode InsertIntoMaxTree(TreeNode root, int val)
+        {
+            TreeNode parent = null;
+            TreeNode cur = root;
+            while (cur != null)
+            {
+                if (val > cur.val)
+                {
+                    if (parent == null)
+                    {
+                        return new TreeNode(val, root, null);
+                    }
+                    TreeNode node = new TreeNode(val, cur, null);
+                    parent.right = node;
+                    return root;
+                }
+                else
+                {
+                    parent = cur;
+                    cur = cur.right;
+                }
+            }
+            parent.right = new TreeNode(val);
+            return root;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0900-0999/demo998/JS998.js
@@ -0,0 +1,25 @@
+﻿function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+};
+
+var insertIntoMaxTree = function (root, val) {
+    let parent = null;
+    let cur = root;
+    while (cur) {
+        if (val > cur.val) {
+            if (!parent) {
+                return new TreeNode(val, root, null);
+            }
+            let node = new TreeNode(val, cur, null);
+            parent.right = node;
+            return root;
+        } else {
+            parent = cur;
+            cur = cur.right;
+        }
+    }
+    parent.right = new TreeNode(val);
+    return root;
+};
