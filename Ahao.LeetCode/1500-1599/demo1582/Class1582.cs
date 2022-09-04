cbeaf68633e7263d1dbcf06b6b6bee7d2b916a9b
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-04 10:58:56 +0800

    1582. 二进制矩阵中的特殊位置

---
+++ Ahao.LeetCode/1500-1599/demo1582/Class1582.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1500_1599.demo1582
+{
+    public class Class1582
+    {
+        //方法一：模拟
+        public int NumSpecial(int[][] mat)
+        {
+            int m = mat.Length, n = mat[0].Length;
+            int[] rowsSum = new int[m];
+            int[] colsSum = new int[n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    rowsSum[i] += mat[i][j];
+                    colsSum[j] += mat[i][j];
+                }
+            }
+            int res = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i][j] == 1 && rowsSum[i] == 1 && colsSum[j] == 1)
+                    {
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+
+        //方法二：列的标记值
+        public int NumSpecial2(int[][] mat)
+        {
+            int m = mat.Length, n = mat[0].Length;
+            for (int i = 0; i < m; i++)
+            {
+                int cnt1 = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i][j] == 1)
+                    {
+                        cnt1++;
+                    }
+                }
+                if (i == 0)
+                {
+                    cnt1--;
+                }
+                if (cnt1 > 0)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (mat[i][j] == 1)
+                        {
+                            mat[0][j] += cnt1;
+                        }
+                    }
+                }
+            }
+            int sum = 0;
+            foreach (int num in mat[0])
+            {
+                if (num == 1)
+                {
+                    sum++;
+                }
+            }
+            return sum;
+        }
+
+
+    }
+}
