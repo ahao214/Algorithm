03cf73a48be999567d844ae968782f9b924bea0a
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-05 10:49:57 +0800

    1828. 统计一个圆中点的数目

---
+++ Ahao.LeetCodeCPP/1828. 统计一个圆中点的数目.cpp
@@ -0,0 +1,34 @@
+#include<vector>
+
+using namespace std;
+
+
+/*
+1828. 统计一个圆中点的数目
+给你一个数组 points ，其中 points[i] = [xi, yi] ，表示第 i 个点在二维平面上的坐标。多个点可能会有 相同 的坐标。
+
+同时给你一个数组 queries ，其中 queries[j] = [xj, yj, rj] ，表示一个圆心在 (xj, yj) 且半径为 rj 的圆。
+
+对于每一个查询 queries[j] ，计算在第 j 个圆 内 点的数目。如果一个点在圆的 边界上 ，我们同样认为它在圆 内 。
+
+请你返回一个数组 answer ，其中 answer[j]是第 j 个查询的答案。
+*/
+class Solution {
+public:
+	vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+		vector<int> ans;
+		for (auto& q : queries)
+		{
+			int X = q[0], Y = q[1], R = q[2];
+			int cnt = 0;
+			for (auto& p : points)
+			{
+				int x = p[0], y = p[1];
+				if ((X - x) * (X - x) + (Y - y) * (Y - y) <= R * R)
+					cnt++;
+			}
+			ans.push_back(cnt);
+		}
+		return ans;
+	}
+};
