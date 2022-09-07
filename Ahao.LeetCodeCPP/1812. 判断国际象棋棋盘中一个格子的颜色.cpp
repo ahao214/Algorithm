d0aaace7eeb96868bc657f2f99529d9ab1e0bc9f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-07 15:15:38 +0800

    1812. 判断国际象棋棋盘中一个格子的颜色

---
+++ Ahao.LeetCodeCPP/1812. 判断国际象棋棋盘中一个格子的颜色.cpp
@@ -0,0 +1,20 @@
+﻿#include<string>
+
+using namespace std;
+
+
+
+/*
+1812. 判断国际象棋棋盘中一个格子的颜色
+给你一个坐标 coordinates ，它是一个字符串，表示国际象棋棋盘中一个格子的坐标。下图是国际象棋棋盘示意图。
+
+如果所给格子的颜色是白色，请你返回 true，如果是黑色，请返回 false 。
+
+给定坐标一定代表国际象棋棋盘上一个存在的格子。坐标第一个字符是字母，第二个字符是数字。
+*/
+class Solution {
+public:
+	bool squareIsWhite(string coordinates) {
+		return (coordinates[0] + coordinates[1]) % 2;
+	}
+};
