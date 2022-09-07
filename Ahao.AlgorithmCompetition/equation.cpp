0bfdfcd6163289e2d2491fb4f8ac8f51a94b59ee
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-07 22:30:34 +0800

    解一元三次方程——枚举法

---
+++ Ahao.AlgorithmCompetition/equation.cpp
@@ -0,0 +1,21 @@
+/*
+输入格式：四个实数即a、b、c、d
+输出格式：为排好序的三个实根
+
+输入样例：1 -5 -4 20
+输出样例：-2.00 2.00 5.00
+
+*/
+
+
+//解一元三次方程——枚举法
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
+using namespace std;
+
+int main() {
+
+
+
+}
