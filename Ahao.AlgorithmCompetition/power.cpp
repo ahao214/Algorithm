5c9b72a55e072e5561e4a481acdf798f95eb7276
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-07 22:06:43 +0800

    位优化快速幂运算

--- Ahao.AlgorithmCompetition/power.cpp
+++ Ahao.AlgorithmCompetition/power.cpp
@@ -37,3 +37,43 @@ int main() {
 	cout << pow(X, n) << endl;
 	return 0;
 }
+
+
+//位优化快速幂运算
+long long Pow(long long x, long long n) {
+	long long result;
+	if (n == 0) {
+		return 1;
+	}
+	else {
+		while ((n & 1) = 0) {		//当n为偶数时
+			n >>= 1;		//即n = n / 2;
+			x *= x;
+		}
+	}
+	result = x;
+	n >>= 1;
+	while (n != 0) {
+		x *= x;
+		if ((n & 1) != 0) {
+			result *= x;
+		}
+		n >>= 1;
+	}
+	return result;
+}
+
+int main() {
+	long long x, n;
+	cin >> x >> n;
+	cout << Pow(x, n) << endl;
+	return 0;
+}
+
+
+
+
+
+
+
+
