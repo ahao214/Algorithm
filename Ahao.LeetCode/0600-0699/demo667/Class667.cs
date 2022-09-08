189cbc19c7bde4a3dcc06ed8b13ad2ac01a11768
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-08 10:36:50 +0800

    667. 优美的排列 II

---
+++ Ahao.LeetCode/0600-0699/demo667/Class667.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0600_0699.demo667
+{
+    public class Class667
+    {
+        public int[] ConstructArray(int n, int k)
+        {
+            int[] answer = new int[n];
+            int idx = 0;
+            for (int i = 1; i < n - k; ++i)
+            {
+                answer[idx] = i;
+                ++idx;
+            }
+            for (int i = n - k, j = n; i <= j; ++i, --j)
+            {
+                answer[idx] = i;
+                ++idx;
+                if (i != j)
+                {
+                    answer[idx] = j;
+                    ++idx;
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0600-0699/demo667/JS667.js
@@ -0,0 +1,17 @@
+﻿var constructArray = function (n, k) {
+    const answer = new Array(n).fill(0);
+    let idx = 0;
+    for (let i = 1; i < n - k; ++i) {
+        answer[idx] = i;
+        ++idx;
+    }
+    for (let i = n - k, j = n; i <= j; ++i, --j) {
+        answer[idx] = i;
+        ++idx;
+        if (i !== j) {
+            answer[idx] = j;
+            ++idx;
+        }
+    }
+    return answer;
+};
