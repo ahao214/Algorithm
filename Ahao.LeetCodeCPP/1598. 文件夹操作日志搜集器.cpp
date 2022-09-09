7e1ddec09412fb4ae59653242bf7e3c64d59f458
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-09 09:37:51 +0800

    1598. 文件夹操作日志搜集器

---
+++ Ahao.LeetCodeCPP/1598. 文件夹操作日志搜集器.cpp
@@ -0,0 +1,25 @@
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int minOperations(vector<string>& logs) {
+		int depth = 0;
+		for (auto& log : logs) {
+			if (log == "./") {
+				continue;
+			}
+			else if (log == "../") {
+				if (depth > 0) {
+					depth--;
+				}
+			}
+			else {
+				depth++;
+			}
+		}
+		return depth;
+	}
+};
