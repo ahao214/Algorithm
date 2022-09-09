24251461da8d97b3173717a95245f363edb24f04
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-09 09:36:50 +0800

    1598. 文件夹操作日志搜集器

---
+++ Ahao.LeetCode/1500-1599/demo1598/Class1598.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1500_1599.demo1598
+{
+    public class Class1598
+    {
+        public int MinOperations(string[] logs)
+        {
+            int depth = 0;
+            foreach (string log in logs)
+            {
+                if ("./".Equals(log))
+                {
+                    continue;
+                }
+                else if ("../".Equals(log))
+                {
+                    if (depth > 0)
+                    {
+                        depth--;
+                    }
+                }
+                else
+                {
+                    depth++;
+                }
+            }
+            return depth;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1500-1599/demo1598/JS1598.js
@@ -0,0 +1,15 @@
+﻿var minOperations = function (logs) {
+    let depth = 0;
+    for (const log of logs) {
+        if ('./' === log) {
+            continue;
+        } else if ('../' === log) {
+            if (depth > 0) {
+                depth--;
+            }
+        } else {
+            depth++;
+        }
+    }
+    return depth;
+};
