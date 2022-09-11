2d869697315735cb0ccef631eaebe8b7df81b474
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-11 16:36:28 +0800

    解一元三次方程——二分法

--- Ahao.AlgorithmCompetition/equation.cpp
+++ Ahao.AlgorithmCompetition/equation.cpp
@@ -33,3 +33,34 @@ int main() {
 	cout << '\n';
 	return 0;
 }
+
+//解一元三次方程——二分法
+
+float a, b, c, d;
+int n;
+float ans[4];
+
+float Equation(float x)
+{
+	return ((a * x + b) * x + c) * x + d;
+}
+
+void solve(float l, float r)
+{
+	if (Equation(l) * Equation(r) > 0 && ((r - l) < 1 || n >= 2))
+		return;
+	float mid = (l + r) / 2;
+
+	if (Equation(mid) <= 1e-4 && Equation(mid) >= -1e-4)
+	{
+		ans[++n] = mid;
+		return;
+	}
+	solve(l, mid), solve(mid, r);
+}
+
+int main() {
+	cin >> a >> b >> c >> d;
+	solve(-100, 100);
+	return;
+}
