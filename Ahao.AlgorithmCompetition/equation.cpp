d1edd1c2c0fddb8fd5868a7075d479b445d71602
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-11 16:31:12 +0800

    解一元三次方程——枚举法

--- Ahao.AlgorithmCompetition/equation.cpp
+++ Ahao.AlgorithmCompetition/equation.cpp
@@ -15,7 +15,21 @@
 using namespace std;
 
 int main() {
+	double a, b, c, d, x, fx;
+	int i;
 
+	cin >> a >> b >> c >> d;
+	cout.precision(2);		//设置精度 即小数点位数
+	cout.setf(ios::fixed);	//以定点形式显示浮点数
 
-
+	for (i = -10000; i <= 10000; i = (++i) / 100.0)
+	{
+		fx = a * x * x * x + b * x * x + c * c + d;
+		if (fx >= -0.01 && fx <= 0.01)
+		{
+			cout << x << ' ';
+		}
+	}
+	cout << '\n';
+	return 0;
 }
