913ca27a88e75f9f66620b61200b7ab056a307cc
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-12 12:42:54 +0800

    1608. 特殊数组的特征值

---
+++ Ahao.LeetCode/1600-1699/demo1608/Class1608.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1608
+{
+    public class Class1608
+    {
+        public int SpecialArray(int[] nums)
+        {
+            Array.Sort(nums);
+            int n = nums.Length;
+            for (int i = 0, j = n - 1; i < j; i++, j--)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+            for (int i = 1; i <= n; ++i)
+            {
+                if (nums[i - 1] >= i && (i == n || nums[i] < i))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1600-1699/demo1608/JS1608.js
@@ -0,0 +1,15 @@
+﻿var specialArray = function (nums) {
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+    for (let i = 0, j = n - 1; i < j; i++, j--) {
+        const temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    for (let i = 1; i <= n; ++i) {
+        if (nums[i - 1] >= i && (i === n || nums[i] < i)) {
+            return i;
+        }
+    }
+    return -1;
+};
