1f47443928e3c4a286a49b2c125b3cf635cdd29d
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-13 12:21:37 +0800

    670. 最大交换

---
+++ Ahao.LeetCode/0600-0699/demo670/Class670.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0600_0699.demo670
+{
+    public class Class670
+    {
+        /// <summary>
+        /// 方法一：直接遍历
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public int MaximumSwap(int num)
+        {
+            char[] charArray = num.ToString().ToCharArray();
+            int n = charArray.Length;
+            int maxNum = num;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    Swap(charArray, i, j);
+                    maxNum = Math.Max(maxNum, int.Parse(new string(charArray)));
+                    Swap(charArray, i, j);
+                }
+            }
+            return maxNum;
+        }
+
+
+        /// <summary>
+        /// 方法二：贪心
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public int MaximumSwap2(int num)
+        {
+            char[] charArray = num.ToString().ToCharArray();
+            int n = charArray.Length;
+            int maxIdx = n - 1;
+            int idx1 = -1, idx2 = -1;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (charArray[i] > charArray[maxIdx])
+                {
+                    maxIdx = i;
+                }
+                else if (charArray[i] < charArray[maxIdx])
+                {
+                    idx1 = i;
+                    idx2 = maxIdx;
+                }
+            }
+            if (idx1 >= 0)
+            {
+                Swap(charArray, idx1, idx2);
+                return int.Parse(new string(charArray));
+            }
+            else
+            {
+                return num;
+            }
+        }
+
+        public void Swap(char[] charArray, int i, int j)
+        {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/0600-0699/demo670/JS670.js
@@ -0,0 +1,50 @@
+﻿//方法一：直接遍历
+var maximumSwap = function (num) {
+    const charArray = [...'' + num];
+    const n = charArray.length;
+    let maxNum = num;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            swap(charArray, i, j);
+            maxNum = Math.max(maxNum, parseInt(charArray.join('')));
+            swap(charArray, i, j);
+        }
+    }
+    return maxNum;
+}
+
+const swap = (charArray, i, j) => {
+    const temp = charArray[i];
+    charArray[i] = charArray[j];
+    charArray[j] = temp;
+};
+
+
+
+//方法二：贪心
+var maximumSwap = function (num) {
+    const charArray = [...'' + num];
+    const n = charArray.length;
+    let maxIdx = n - 1;
+    let idx1 = -1, idx2 = -1;
+    for (let i = n - 1; i >= 0; i--) {
+        if (charArray[i] > charArray[maxIdx]) {
+            maxIdx = i;
+        } else if (charArray[i] < charArray[maxIdx]) {
+            idx1 = i;
+            idx2 = maxIdx;
+        }
+    }
+    if (idx1 >= 0) {
+        swap(charArray, idx1, idx2);
+        return parseInt(charArray.join(''));
+    } else {
+        return num;
+    }
+}
+
+const swap = (charArray, i, j) => {
+    const temp = charArray[i];
+    charArray[i] = charArray[j];
+    charArray[j] = temp;
+};
