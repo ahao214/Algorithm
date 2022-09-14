413fcf62949cbdcd8551deb3663aeafbb5156b5d
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-14 11:13:44 +0800

    1619. 删除某些元素后的数组均值

---
+++ Ahao.LeetCode/1600-1699/demo1619/Class1619.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1619
+{
+    public class Class1619
+    {
+        public double TrimMean(int[] arr)
+        {
+            int n = arr.Length;
+            Array.Sort(arr);
+            int partialSum = 0;
+            for (int i = n / 20; i < 19 * n / 20; i++)
+            {
+                partialSum += arr[i];
+            }
+            return partialSum / (n * 0.9);
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1600-1699/demo1619/JS1619.js
@@ -0,0 +1,9 @@
+﻿var trimMean = function (arr) {
+    const n = arr.length;
+    arr.sort((a, b) => a - b);
+    let partialSum = 0;
+    for (let i = n / 20; i < 19 * n / 20; i++) {
+        partialSum += arr[i];
+    }
+    return partialSum / (n * 0.9);
+};
