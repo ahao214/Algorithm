0bb1c0ed2cf1172895af697345a46fa776e1ddf6
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-14 11:09:08 +0800

    1619. 删除某些元素后的数组均值

---
+++ Ahao.LeetCodeCPP/1619. 删除某些元素后的数组均值.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    double trimMean(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        int partialSum = accumulate(arr.begin() + n / 20, arr.begin() + (19 * n / 20), 0);
+        return partialSum / (n * 0.9);
+    }
+};
+
