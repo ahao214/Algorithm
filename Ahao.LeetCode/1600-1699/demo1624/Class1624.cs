3d38fc6ce15e15adfa97f458b2fa2c2848dfff60
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-17 09:55:25 +0800

    1624. 两个相同字符之间的最长子字符串

---
+++ Ahao.LeetCode/1600-1699/demo1624/Class1624.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1624
+{
+    public class Class1624
+    {
+        public int MaxLengthBetweenEqualCharacters(string s)
+        {
+            int[] firstIndex = new int[26];
+            Array.Fill(firstIndex, -1);
+            int maxLength = -1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (firstIndex[s[i] - 'a'] < 0)
+                {
+                    firstIndex[s[i] - 'a'] = i;
+                }
+                else
+                {
+                    maxLength = Math.Max(maxLength, i - firstIndex[s[i] - 'a'] - 1);
+                }
+            }
+            return maxLength;
+        }
+
+        public int MaxLengthBetweenEqualCharacters2(string s)
+        {
+            int maxLength = -1;
+            int n = s.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        maxLength = Math.Max(maxLength, j - i - 1);
+                    }
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1600-1699/demo1624/JS1624.js
@@ -0,0 +1,12 @@
+﻿var maxLengthBetweenEqualCharacters = function (s) {
+    const firstIndex = new Array(26).fill(-1);
+    let maxLength = -1;
+    for (let i = 0; i < s.length; i++) {
+        if (firstIndex[s[i].charCodeAt() - 'a'.charCodeAt()] < 0) {
+            firstIndex[s[i].charCodeAt() - 'a'.charCodeAt()] = i;
+        } else {
+            maxLength = Math.max(maxLength, i - firstIndex[s[i].charCodeAt() - 'a'.charCodeAt()] - 1);
+        }
+    }
+    return maxLength;
+};
