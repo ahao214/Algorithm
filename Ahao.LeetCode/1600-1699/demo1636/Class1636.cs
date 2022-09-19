dfbf153812fe4bdcd630876935395de462e0af22
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-19 15:39:14 +0800

    1636. 按照频率将数组升序排序

---
+++ Ahao.LeetCode/1600-1699/demo1636/Class1636.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1636
+{
+    public class Class1636
+    {
+        public int[] FrequencySort(int[] nums)
+        {
+            Dictionary<int, int> cnt = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                cnt.TryAdd(num, 0);
+                cnt[num]++;
+            }
+            List<int> list = new List<int>();
+            foreach (int num in nums)
+            {
+                list.Add(num);
+            }
+            list.Sort((a, b) => {
+                int cnt1 = cnt[a], cnt2 = cnt[b];
+                return cnt1 != cnt2 ? cnt1 - cnt2 : b - a;
+            });
+            return list.ToArray();
+        }
+    }
+}
