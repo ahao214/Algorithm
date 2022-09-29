88c6f6627d7025428383c20bbb672ad5b7815c30
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-29 16:58:21 +0800

    1768. 交替合并字符串

---
+++ Ahao.LeetCodeCPP/1768. 交替合并字符串.cpp
@@ -0,0 +1,24 @@
+#include<string>
+
+using namespace std;
+
+
+/*
+1768. 交替合并字符串
+给你两个字符串 word1 和 word2 。请你从 word1 开始，通过交替添加字母来合并字符串。如果一个字符串比另一个字符串长，就将多出来的字母追加到合并后字符串的末尾。
+
+返回 合并后的字符串 。
+*/
+class Solution {
+public:
+	string mergeAlternately(string word1, string word2) {
+		string res;
+		int i = 0, j = 0;
+		while (i < word1.size() || j < word2.size())
+		{
+			if (i < word1.size()) res += word1[i++];
+			if (j < word2.size()) res += word2[j++];
+		}
+		return res;
+	}
+};
