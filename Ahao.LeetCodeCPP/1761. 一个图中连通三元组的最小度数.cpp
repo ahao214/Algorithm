ff3fa9ab10d20dc41b6d63198d1ca8230d3cec18
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-30 10:15:13 +0800

    1761. 一个图中连通三元组的最小度数

---
+++ Ahao.LeetCodeCPP/1761. 一个图中连通三元组的最小度数.cpp
@@ -0,0 +1,47 @@
+#include<vector>
+#include<unordered_set>
+
+
+using namespace std;
+
+
+/*
+1761. 一个图中连通三元组的最小度数
+给你一个无向图，整数 n 表示图中节点的数目，edges 数组表示图中的边，其中 edges[i] = [ui, vi] ，表示 ui 和 vi 之间有一条无向边。
+
+一个 连通三元组 指的是 三个 节点组成的集合且这三个点之间 两两 有边。
+
+连通三元组的度数 是所有满足此条件的边的数目：一个顶点在这个三元组内，而另一个顶点不在这个三元组内。
+
+请你返回所有连通三元组中度数的 最小值 ，如果图中没有连通三元组，那么返回 -1 。
+*/
+class Solution {
+public:
+	int minTrioDegree(int n, vector<vector<int>>& edges) {
+		unordered_set<int> hash;
+		vector<int> d(n + 1);
+		for (auto& e : edges)
+		{
+			int a = e[0], b = e[1];
+			if (a > b) swap(a, b);
+			d[a]++, d[b]++;
+			hash.insert(a * 1000 + b);
+		}
+
+		int res = INT_MAX;
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = i + 1; j <= n; j++)
+			{
+				if (hash.count(i * 1000 + j))
+					for (int k = j + 1; k <= n; k++)
+						if (hash.count(i * 1000 + k) && hash.count(j * 1000 + k))
+							res = min(res, d[i] + d[j] + d[k] - 6);
+			}
+		}
+
+		if (res == INT_MAX)
+			res = -1;
+		return res;
+	}
+};
