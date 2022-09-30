c0297780ccaed747f564da3b70831f78367f8c40
liupenghao@fulan.com.cn <haochen214@163.com>
2022-09-30 15:29:19 +0800

    350. 两个数组的交集 II

---
+++ Ahao.LeetCode/0300-0399/demo350/Class350.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0300_0399.demo350
+{
+    public class Class350
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            var dict = new Dictionary<int, int>();
+            List<int> list = new List<int>();
+            if (nums1.Length == 0 || nums2.Length == 0)
+            {
+                return list.ToArray();
+            }
+            foreach (int n in nums1)
+            {
+                if (dict.ContainsKey(n))
+                {
+                    dict[n]++;
+                }
+                else
+                {
+                    dict.Add(n, 1);
+                }
+            }
+            foreach (int n in nums2)
+            {
+                if (dict.ContainsKey(n) && dict[n] > 0)
+                {
+                    dict[n]--;
+                    list.Add(n);
+                }
+            }
+            return list.ToArray();
+        }
+    }
+}
