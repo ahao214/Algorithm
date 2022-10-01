d21d68def861009b4d99539e1f2c0f2d6db12c9c
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-01 10:12:22 +0800

    1694. 重新格式化电话号码

---
+++ Ahao.LeetCode/1600-1699/demo1694/Class1694.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1694
+{
+    public class Class1694
+    {
+        public string ReformatNumber(string number)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char ch in number)
+            {
+                if (char.IsDigit(ch))
+                {
+                    sb.Append(ch);
+                }
+            }
+
+            string digits = sb.ToString();
+            int n = digits.Length;
+            int pt = 0;
+            StringBuilder ans = new StringBuilder();
+            while (n > 0)
+            {
+                if (n > 4)
+                {
+                    ans.Append(digits.Substring(pt, 3) + "-");
+                    pt += 3;
+                    n -= 3;
+                }
+                else
+                {
+                    if (n == 4)
+                    {
+                        ans.Append(digits.Substring(pt, 2) + "-" + digits.Substring(pt + 2, 2));
+                    }
+                    else
+                    {
+                        ans.Append(digits.Substring(pt, n));
+                    }
+                    break;
+                }
+            }
+            return ans.ToString();
+        }
+    }
+}
