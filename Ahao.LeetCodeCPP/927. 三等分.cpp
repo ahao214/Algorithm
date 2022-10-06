02f19c47654fcff4700572b519f0e82741be607d
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-06 12:25:08 +0800

    927. 三等分

---
+++ Ahao.LeetCodeCPP/927. 三等分.cpp
@@ -0,0 +1,64 @@
+#include<vector>
+#include <numeric>
+
+using namespace std;
+
+/*
+927. 三等分
+
+
+给定一个由 0 和 1 组成的数组 arr ，将数组分成  3 个非空的部分 ，使得所有这些部分表示相同的二进制值。
+
+如果可以做到，请返回任何 [i, j]，其中 i+1 < j，这样一来：
+
+arr[0], arr[1], ..., arr[i] 为第一部分；
+arr[i + 1], arr[i + 2], ..., arr[j - 1] 为第二部分；
+arr[j], arr[j + 1], ..., arr[arr.length - 1] 为第三部分。
+这三个部分所表示的二进制值相等。
+如果无法做到，就返回 [-1, -1]。
+
+注意，在考虑每个部分所表示的二进制时，应当将其看作一个整体。例如，[1,1,0] 表示十进制中的 6，而不会是 3。此外，前导零也是被允许的，所以 [0,1,1] 和 [1,1] 表示相同的值。
+*/
+
+class Solution {
+public:
+    vector<int> threeEqualParts(vector<int>& arr) {
+        int sum = accumulate(arr.begin(), arr.end(), 0);
+        if (sum % 3 != 0) {
+            return { -1, -1 };
+        }
+        if (sum == 0) {
+            return { 0, 2 };
+        }
+
+        int partial = sum / 3;
+        int first = 0, second = 0, third = 0, cur = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == 1) {
+                if (cur == 0) {
+                    first = i;
+                }
+                else if (cur == partial) {
+                    second = i;
+                }
+                else if (cur == 2 * partial) {
+                    third = i;
+                }
+                cur++;
+            }
+        }
+
+        int len = (int)arr.size() - third;
+        if (first + len <= second && second + len <= third) {
+            int i = 0;
+            while (third + i < arr.size()) {
+                if (arr[first + i] != arr[second + i] || arr[first + i] != arr[third + i]) {
+                    return { -1, -1 };
+                }
+                i++;
+            }
+            return { first + len - 1, second + len };
+        }
+        return { -1, -1 };
+    }
+};
