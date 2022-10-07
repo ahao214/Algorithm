f273fdc1c3fc09dcc426b03a0d622285371558a9
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-07 13:12:17 +0800

    1800. 最大升序子数组和

---
+++ Ahao.LeetCodeCPP/1800. 最大升序子数组和.cpp
@@ -0,0 +1,20 @@
+#include<vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+	int maxAscendingSum(vector<int>& nums) {
+		int res = 0;
+		int l = 0;
+		while (l < nums.size()) {
+			int cursum = nums[l++];
+			while (l < nums.size() && nums[l] > nums[l - 1]) {
+				cursum += nums[l++];
+			}
+			res = max(res, cursum);
+		}
+		return res;
+	}
+};
