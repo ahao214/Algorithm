a243cdea6f9eb46d8a71a3f47ebad302c6849e77
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-11 09:45:11 +0800

    1790. 仅执行一次字符串交换能否使两个字符串相等

---
+++ Ahao.LeetCode/1700-1799/demo1790/Class1790.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1790
+{
+    public class Class1790
+    {
+        public bool AreAlmostEqual(string s1, string s2)
+        {
+            int n = s1.Length;
+            IList<int> diff = new List<int>();
+            for (int i = 0; i < n; ++i)
+            {
+                if (s1[i] != s2[i])
+                {
+                    if (diff.Count >= 2)
+                    {
+                        return false;
+                    }
+                    diff.Add(i);
+                }
+            }
+            if (diff.Count == 0)
+            {
+                return true;
+            }
+            if (diff.Count != 2)
+            {
+                return false;
+            }
+            return s1[diff[0]] == s2[diff[1]] && s1[diff[1]] == s2[diff[0]];
+        }
+
+    }
+}
