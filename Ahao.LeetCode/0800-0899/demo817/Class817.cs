01d9c631df05be520d05b08a805d83c109bb5e56
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-12 12:40:08 +0800

    817. 链表组件

---
+++ Ahao.LeetCode/0800-0899/demo817/Class817.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo817
+{
+    public class Class817
+    {
+        public int NumComponents(ListNode head, int[] nums)
+        {
+            ISet<int> numsSet = new HashSet<int>();
+            foreach (int num in nums)
+            {
+                numsSet.Add(num);
+            }
+            bool inSet = false;
+            int res = 0;
+            while (head != null)
+            {
+                if (numsSet.Contains(head.val))
+                {
+                    if (!inSet)
+                    {
+                        inSet = true;
+                        res++;
+                    }
+                }
+                else
+                {
+                    inSet = false;
+                }
+                head = head.next;
+            }
+            return res;
+        }
+
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
---
+++ Ahao.LeetCode/0800-0899/demo817/JS817.js
@@ -0,0 +1,31 @@
+﻿function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+/**
+ * @param {ListNode} head
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numComponents = function (head, nums) {
+    const numsSet = new Set();
+    for (const num of nums) {
+        numsSet.add(num);
+    }
+    let inSet = false;
+    let res = 0;
+    while (head) {
+        if (numsSet.has(head.val)) {
+            if (!inSet) {
+                inSet = true;
+                res++;
+            }
+        } else {
+            inSet = false;
+        }
+        head = head.next;
+    }
+    return res;
+};
+
