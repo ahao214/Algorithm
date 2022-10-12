0ed80992f9f55e0a88bf165c47db7d87215a6037
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-12 11:59:36 +0800

    817. 链表组件

---
+++ Ahao.LeetCodeCPP/817. 链表组件.cpp
@@ -0,0 +1,41 @@
+#include<vector>
+#include<unordered_set>
+
+using namespace std;
+
+/*
+817. Á´±í×é¼þ
+*/
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+	int numComponents(ListNode* head, vector<int>& nums) {
+		unordered_set<int> numsSet;
+		for (int num : nums) {
+			numsSet.emplace(num);
+		}
+		bool inSet = false;
+		int res = 0;
+		while (head != nullptr) {
+			if (numsSet.count(head->val)) {
+				if (!inSet) {
+					inSet = true;
+					res++;
+				}
+			}
+			else {
+				inSet = false;
+			}
+			head = head->next;
+		}
+		return res;
+	}
+};
