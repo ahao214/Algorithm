4a7cae1aedeafefad6f60a76e110d5ce6e5e2f90
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-13 09:33:42 +0800

    769. 最多能完成排序的块

---
+++ Ahao.LeetCode/0700-0799/demo769/Class769.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo769
+{
+    public class Class769
+    {
+        public int MaxChunksToSorted(int[] arr)
+        {
+            int m = 0, res = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                m = Math.Max(m, arr[i]);
+                if (m == i)
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0700-0799/demo769/JS769.js
@@ -0,0 +1,10 @@
+﻿var maxChunksToSorted = function (arr) {
+    let m = 0, res = 0;
+    for (let i = 0; i < arr.length; i++) {
+        m = Math.max(m, arr[i]);
+        if (m === i) {
+            res++;
+        }
+    }
+    return res;
+};
