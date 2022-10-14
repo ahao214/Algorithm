39dc2df208a984ed476d5e9ccbf1888efa8fe523
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-14 09:43:49 +0800

    940. 不同的子序列 II

--- Ahao.LeetCode/0900-0999/demo940/Class940.cs
+++ Ahao.LeetCode/0900-0999/demo940/Class940.cs
@@ -39,5 +39,28 @@ namespace Ahao.LeetCode._0900_0999.demo940
             }
             return ans;
         }
+
+        public int DistinctSubseqII2(string s)
+        {
+            const int MOD = 1000000007;
+            int[] g = new int[26];
+            int n = s.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                int total = 1;
+                for (int j = 0; j < 26; ++j)
+                {
+                    total = (total + g[j]) % MOD;
+                }
+                g[s[i] - 'a'] = total;
+            }
+
+            int ans = 0;
+            for (int i = 0; i < 26; ++i)
+            {
+                ans = (ans + g[i]) % MOD;
+            }
+            return ans;
+        }
     }
 }
