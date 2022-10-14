96669293f8c6f16c37f3c306c2cdd25311d0155f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-14 09:43:10 +0800

    940. 不同的子序列 II

---
+++ Ahao.LeetCode/0900-0999/demo940/Class940.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0900_0999.demo940
+{
+    public class Class940
+    {
+        public int DistinctSubseqII(string s)
+        {
+            const int MOD = 1000000007;
+            int[] last = new int[26];
+            Array.Fill(last, -1);
+
+            int n = s.Length;
+            int[] f = new int[n];
+            Array.Fill(f, 1);
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < 26; ++j)
+                {
+                    if (last[j] != -1)
+                    {
+                        f[i] = (f[i] + f[last[j]]) % MOD;
+                    }
+                }
+                last[s[i] - 'a'] = i;
+            }
+
+            int ans = 0;
+            for (int i = 0; i < 26; ++i)
+            {
+                if (last[i] != -1)
+                {
+                    ans = (ans + f[last[i]]) % MOD;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0900-0999/demo940/JS940.js
@@ -0,0 +1,43 @@
+﻿var distinctSubseqII = function (s) {
+    const MOD = 1000000007;
+    const last = new Array(26).fill(-1);
+
+    const n = s.length;
+    const f = new Array(n).fill(1);
+    for (let i = 0; i < n; ++i) {
+        for (let j = 0; j < 26; ++j) {
+            if (last[j] !== -1) {
+                f[i] = (f[i] + f[last[j]]) % MOD;
+            }
+        }
+        last[s[i].charCodeAt() - 'a'.charCodeAt()] = i;
+    }
+
+    let ans = 0;
+    for (let i = 0; i < 26; ++i) {
+        if (last[i] !== -1) {
+            ans = (ans + f[last[i]]) % MOD;
+        }
+    }
+    return ans;
+};
+
+
+var distinctSubseqII = function (s) {
+    const MOD = 1000000007;
+    const g = new Array(26).fill(0);
+    const n = s.length;
+    for (let i = 0; i < n; ++i) {
+        let total = 1;
+        for (let j = 0; j < 26; ++j) {
+            total = (total + g[j]) % MOD;
+        }
+        g[s[i].charCodeAt() - 'a'.charCodeAt()] = total;
+    }
+
+    let ans = 0;
+    for (let i = 0; i < 26; ++i) {
+        ans = (ans + g[i]) % MOD;
+    }
+    return ans;
+};
