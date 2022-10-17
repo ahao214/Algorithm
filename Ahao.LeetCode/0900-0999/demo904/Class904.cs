336ead8955be41a677f694fec1d7a5d26cc3cd7c
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-17 09:42:55 +0800

    904. 水果成篮

---
+++ Ahao.LeetCode/0900-0999/demo904/Class904.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0900_0999.demo904
+{
+    public class Class904
+    {
+        public int TotalFruit(int[] fruits)
+        {
+            int n = fruits.Length;
+            IDictionary<int, int> cnt = new Dictionary<int, int>();
+
+            int left = 0, ans = 0;
+            for (int right = 0; right < n; ++right)
+            {
+                cnt.TryAdd(fruits[right], 0);
+                ++cnt[fruits[right]];
+                while (cnt.Count > 2)
+                {
+                    --cnt[fruits[left]];
+                    if (cnt[fruits[left]] == 0)
+                    {
+                        cnt.Remove(fruits[left]);
+                    }
+                    ++left;
+                }
+                ans = Math.Max(ans, right - left + 1);
+            }
+            return ans;
+        }
+    }
+}
