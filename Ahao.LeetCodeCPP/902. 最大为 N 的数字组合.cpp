5f689c510f23a17f24730b3c420c6fe0b2c3b15f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-18 09:42:15 +0800

    902. 最大为 N 的数字组合

---
+++ Ahao.LeetCodeCPP/902. 最大为 N 的数字组合.cpp
@@ -0,0 +1,39 @@
+#include<vector>
+#include<string>
+
+using namespace std;
+
+
+/*
+902. 最大为 N 的数字组合
+给定一个按 非递减顺序 排列的数字数组 digits 。你可以用任意次数 digits[i] 来写的数字。例如，如果 digits = ['1','3','5']，我们可以写数字，如 '13', '551', 和 '1351315'。
+
+返回 可以生成的小于或等于给定整数 n 的正整数的个数 。
+*/
+
+class Solution {
+public:
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        string s = to_string(n);
+        int m = digits.size(), k = s.size();
+        vector<vector<int>> dp(k + 1, vector<int>(2));
+        dp[0][1] = 1;
+        for (int i = 1; i <= k; i++) {
+            for (int j = 0; j < m; j++) {
+                if (digits[j][0] == s[i - 1]) {
+                    dp[i][1] = dp[i - 1][1];
+                }
+                else if (digits[j][0] < s[i - 1]) {
+                    dp[i][0] += dp[i - 1][1];
+                }
+                else {
+                    break;
+                }
+            }
+            if (i > 1) {
+                dp[i][0] += m + dp[i - 1][0] * m;
+            }
+        }
+        return dp[k][0] + dp[k][1];
+    }
+};
