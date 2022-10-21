deae24209415d989e226311543f59a8304b018cc
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-21 09:38:18 +0800

    901. 股票价格跨度

---
+++ Ahao.LeetCode/0900-0999/demo901/Class901.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0900_0999.demo901
+{
+    public class StockSpanner
+    {
+        Stack<Tuple<int, int>> stack;
+        int idx;
+
+        public StockSpanner()
+        {
+            stack = new Stack<Tuple<int, int>>();
+            stack.Push(new Tuple<int, int>(-1, int.MaxValue));
+            idx = -1;
+        }
+
+        public int Next(int price)
+        {
+            idx++;
+            while (price >= stack.Peek().Item2)
+            {
+                stack.Pop();
+            }
+            int ret = idx - stack.Peek().Item1;
+            stack.Push(new Tuple<int, int>(idx, price));
+            return ret;
+        }
+
+    }
+}
