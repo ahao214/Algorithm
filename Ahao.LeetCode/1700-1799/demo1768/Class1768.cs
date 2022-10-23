b56ba3b7c0511e8df24329d0f79803eae7e7a29b
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-23 10:23:22 +0800

    1768. 交替合并字符串

---
+++ Ahao.LeetCode/1700-1799/demo1768/Class1768.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1768
+{
+    public class Class1768
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            int m = word1.Length, n = word2.Length;
+            int i = 0, j = 0;
+
+            StringBuilder ans = new StringBuilder();
+            while (i < m || j < n)
+            {
+                if (i < m)
+                {
+                    ans.Append(word1[i]);
+                    ++i;
+                }
+                if (j < n)
+                {
+                    ans.Append(word2[j]);
+                    ++j;
+                }
+            }
+            return ans.ToString();
+        }
+    }
+}
