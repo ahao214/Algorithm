fd7c95410e84656996fd76187733b3ac49755fc7
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-26 09:44:43 +0800

    862. 和至少为 K 的最短子数组

--- Ahao.LeetCode/0800-0899/demo862/Class862.cs
+++ Ahao.LeetCode/0800-0899/demo862/Class862.cs
@@ -9,5 +9,37 @@ namespace Ahao.LeetCode._0800_0899.demo862
     public class Class862
     {
 
+        public int ShortestSubarray(int[] nums, int k)
+        {
+            int n = nums.Length;
+            long[] sums = new long[n + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                sums[i] = sums[i - 1] + nums[i - 1];
+            }
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(0);
+            int ans = -1;
+            for (int i = 1; i <= n; i++)
+            {
+                long pre = sums[i];
+                LinkedListNode<int> node = list.First;
+                while (node != null && pre - sums[node.Value] >= k)
+                {
+                    if (ans == -1) ans = i - node.Value;
+                    else ans = Math.Min(ans, i - node.Value);
+                    list.RemoveFirst();
+                    node = list.First;
+                }
+                node = list.Last;
+                while (node != null && pre <= sums[node.Value])
+                {
+                    list.RemoveLast();
+                    node = list.Last;
+                }
+                list.AddLast(i);
+            }
+            return ans;
+        }
     }
 }
