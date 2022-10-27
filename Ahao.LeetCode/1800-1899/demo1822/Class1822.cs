b7c5774e95df393910af8f18d94bb18ce1315375
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-27 10:00:51 +0800

    1822. 数组元素积的符号

---
+++ Ahao.LeetCode/1800-1899/demo1822/Class1822.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1822
+{
+    public class Class1822
+    {
+        public int ArraySign(int[] nums)
+        {
+            int sign = 1;
+            foreach (int num in nums)
+            {
+                if (num == 0)
+                {
+                    return 0;
+                }
+                if (num < 0)
+                {
+                    sign = -sign;
+                }
+            }
+            return sign;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1800-1899/demo1822/JS1822.js
@@ -0,0 +1,12 @@
+﻿var arraySign = function (nums) {
+    let sign = 1;
+    for (const num of nums) {
+        if (num === 0) {
+            return 0;
+        }
+        if (num < 0) {
+            sign = -sign;
+        }
+    }
+    return sign;
+};
