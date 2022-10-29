4682a52bc98752ce97ec43dfb4fd18a69f0b571e
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-29 09:22:02 +0800

    1773. 统计匹配检索规则的物品数量

--- Ahao.LeetCodeCPP/1773. 统计匹配检索规则的物品数量.cpp
+++ Ahao.LeetCodeCPP/1773. 统计匹配检索规则的物品数量.cpp
@@ -1,5 +1,6 @@
 #include<vector>
 #include<string>
+#include<unordered_map>
 
 using namespace std;
 
@@ -27,6 +28,21 @@ public:
 			if (ruleKey == "type" && s[0] == ruleValue) res++;
 			else if (ruleKey == "color" && s[1] == ruleValue) res++;
 			else if (ruleKey == "name" && s[2] == ruleValue) res++;
+		}
+		return res;
+	}
+};
+
+
+class Solution {
+public:
+	int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+		unordered_map<string, int> dictionary = { {"type", 0}, {"color", 1}, {"name", 2} };
+		int res = 0, index = dictionary[ruleKey];
+		for (auto&& item : items) {
+			if (item[index] == ruleValue) {
+				res++;
+			}
 		}
 		return res;
 	}
