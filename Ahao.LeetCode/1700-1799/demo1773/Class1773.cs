4086a5647fc476ffd9d87090b9009e10d26795db
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-29 09:24:51 +0800

    1773. 统计匹配检索规则的物品数量

---
+++ Ahao.LeetCode/1700-1799/demo1773/Class1773.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1773
+{
+    public class Class1773
+    {
+        public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            int index = new Dictionary<string, int>() {
+            {"type", 0}, {"color", 1}, {"name", 2}
+        }[ruleKey];
+            int res = 0;
+            foreach (IList<string> item in items)
+            {
+                if (item[index].Equals(ruleValue))
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1700-1799/demo1773/JS1773.js
@@ -0,0 +1,10 @@
+﻿var countMatches = function (items, ruleKey, ruleValue) {
+    const index = { "type": 0, "color": 1, "name": 2 }[ruleKey];
+    let res = 0;
+    for (const item of items) {
+        if (item[index] === ruleValue) {
+            res++;
+        }
+    }
+    return res;
+};
