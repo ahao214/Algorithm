2dd7c917a62f247a1bf7fe075741e39e42f7874d
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-30 11:17:06 +0800

    784. 字母大小写全排列

--- Ahao.LeetCode/0700-0799/demo784/Class784.cs
+++ Ahao.LeetCode/0700-0799/demo784/Class784.cs
@@ -36,7 +36,64 @@ namespace Ahao.LeetCode._0700_0799.demo784
             }
             return ans;
         }
-    
-        
+
+        //方法二：回溯
+        public IList<string> LetterCasePermutation2(string s)
+        {
+            IList<string> ans = new List<string>();
+            DFS(s.ToCharArray(), 0, ans);
+            return ans;
+        }
+
+        public void DFS(char[] arr, int pos, IList<string> res)
+        {
+            while (pos < arr.Length && char.IsDigit(arr[pos]))
+            {
+                pos++;
+            }
+            if (pos == arr.Length)
+            {
+                res.Add(new string(arr));
+                return;
+            }
+            arr[pos] = (char)(arr[pos] ^ 32);
+            DFS(arr, pos + 1, res);
+            arr[pos] = (char)(arr[pos] ^ 32);
+            DFS(arr, pos + 1, res);
+        }
+
+        //方法三：二进制位图
+        public IList<string> LetterCasePermutation3(string s)
+        {
+            int n = s.Length;
+            int m = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (char.IsLetter(s[i]))
+                {
+                    m++;
+                }
+            }
+            IList<string> ans = new List<string>();
+            for (int mask = 0; mask < (1 << m); mask++)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0, k = 0; j < n; j++)
+                {
+                    if (char.IsLetter(s[j]) && (mask & (1 << k++)) != 0)
+                    {
+                        sb.Append(char.ToUpper(s[j]));
+                    }
+                    else
+                    {
+                        sb.Append(char.ToLower(s[j]));
+                    }
+                }
+                ans.Add(sb.ToString());
+            }
+            return ans;
+        }
+
+
     }
 }
--- Ahao.LeetCode/0700-0799/demo784/JS784.js
+++ Ahao.LeetCode/0700-0799/demo784/JS784.js
@@ -30,3 +30,61 @@ const swapCase = (ch) => {
 const isLetter = (ch) => {
     return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z';
 }
+
+
+//方法二：回溯
+var letterCasePermutation = function (s) {
+    const ans = [];
+    const dfs = (arr, pos, res) => {
+        while (pos < arr.length && isDigit(arr[pos])) {
+            pos++;
+        }
+        if (pos === arr.length) {
+            res.push(arr.join(""));
+            return;
+        }
+        arr[pos] = String.fromCharCode(arr[pos].charCodeAt() ^ 32);
+        dfs(arr, pos + 1, res);
+        arr[pos] = String.fromCharCode(arr[pos].charCodeAt() ^ 32);
+        dfs(arr, pos + 1, res);
+    }
+    dfs([...s], 0, ans);
+    return ans;
+};
+
+const isDigit = (ch) => {
+    return parseFloat(ch).toString() === "NaN" ? false : true;
+}
+
+//方法三：二进制位图
+var letterCasePermutation = function (s) {
+    const n = s.length;
+    let m = 0;
+    for (let i = 0; i < n; i++) {
+        if (isLetter(s[i])) {
+            m++;
+        }
+    }
+    const ans = [];
+    for (let mask = 0; mask < (1 << m); mask++) {
+        let sb = '';
+        for (let j = 0, k = 0; j < n; j++) {
+            if (isLetter(s[j]) && (mask & (1 << k++)) !== 0) {
+                sb += s[j].toUpperCase();
+            } else {
+                sb += s[j].toLowerCase();
+            }
+        }
+        ans.push(sb);
+    }
+    return ans;
+};
+
+const isDigit = (ch) => {
+    return parseFloat(ch).toString() === "NaN" ? false : true;
+}
+
+const isLetter = (ch) => {
+    return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z';
+}
+
