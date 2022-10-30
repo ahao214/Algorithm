fff2cca42eb7f8f8e62b3995ca69b31f2bf92ee4
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-30 11:15:28 +0800

    784. 字母大小写全排列

---
+++ Ahao.LeetCode/0700-0799/demo784/Class784.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo784
+{
+    public class Class784
+    {
+        //方法一：广度优先搜索
+        public IList<string> LetterCasePermutation(string s)
+        {
+            IList<string> ans = new List<string>();
+            Queue<StringBuilder> queue = new Queue<StringBuilder>();
+            queue.Enqueue(new StringBuilder());
+            while (queue.Count > 0)
+            {
+                StringBuilder curr = queue.Peek();
+                if (curr.Length == s.Length)
+                {
+                    ans.Add(curr.ToString());
+                    queue.Dequeue();
+                }
+                else
+                {
+                    int pos = curr.Length;
+                    if (char.IsLetter(s[pos]))
+                    {
+                        StringBuilder next = new StringBuilder(curr.ToString());
+                        next.Append((char)(s[pos] ^ 32));
+                        queue.Enqueue(next);
+                    }
+                    curr.Append(s[pos]);
+                }
+            }
+            return ans;
+        }
+    
+        
+    }
+}
---
+++ Ahao.LeetCode/0700-0799/demo784/JS784.js
@@ -0,0 +1,32 @@
+﻿//方法一：广度优先搜索
+var letterCasePermutation = function (s) {
+    const ans = [];
+    const q = [""];
+    while (q.length !== 0) {
+        let cur = q[0];
+        const pos = cur.length;
+        if (pos === s.length) {
+            ans.push(cur);
+            q.shift();
+        } else {
+            if (isLetter(s[pos])) {
+                q.push(cur + swapCase(s[pos]));
+            }
+            q[0] += s[pos];
+        }
+    }
+    return ans;
+};
+
+const swapCase = (ch) => {
+    if ('a' <= ch && ch <= 'z') {
+        return ch.toUpperCase();
+    }
+    if ('A' <= ch && ch <= 'Z') {
+        return ch.toLowerCase();
+    }
+}
+
+const isLetter = (ch) => {
+    return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z';
+}
