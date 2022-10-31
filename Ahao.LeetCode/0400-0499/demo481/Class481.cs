706651d52ae32d2d06b2448439072be0d10d4ff9
liupenghao@fulan.com.cn <haochen214@163.com>
2022-10-31 09:40:40 +0800

    481. 神奇字符串

---
+++ Ahao.LeetCode/0400-0499/demo481/Class481.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0400_0499.demo481
+{
+    public class Class481
+    {
+        //双指针
+        public int MagicalString(int n)
+        {
+            if (n < 4)
+            {
+                return 1;
+            }
+            char[] s = new char[n];
+            s[0] = '1';
+            s[1] = '2';
+            s[2] = '2';
+            int res = 1;
+            int i = 2;
+            int j = 3;
+            while (j < n)
+            {
+                int size = s[i] - '0';
+                int num = 3 - (s[j - 1] - '0');
+                while (size > 0 && j < n)
+                {
+                    s[j] = (char)('0' + num);
+                    if (num == 1)
+                    {
+                        ++res;
+                    }
+                    ++j;
+                    --size;
+                }
+                ++i;
+            }
+            return res;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0400-0499/demo481/JS481.js
@@ -0,0 +1,26 @@
+﻿var magicalString = function (n) {
+    if (n < 4) {
+        return 1;
+    }
+    const s = new Array(n).fill(0);
+    s[0] = '1';
+    s[1] = '2';
+    s[2] = '2';
+    let res = 1;
+    let i = 2;
+    let j = 3;
+    while (j < n) {
+        let size = s[i].charCodeAt() - '0'.charCodeAt();
+        const num = 3 - (s[j - 1].charCodeAt() - '0'.charCodeAt());
+        while (size > 0 && j < n) {
+            s[j] = String.fromCharCode('0'.charCodeAt() + num);
+            if (num === 1) {
+                ++res;
+            }
+            ++j;
+            --size;
+        }
+        ++i;
+    }
+    return res;
+};
