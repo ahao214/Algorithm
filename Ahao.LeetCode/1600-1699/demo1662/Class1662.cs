a4be612c6ca7ede54ff73b6e5c9c5b45a1462778
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-01 09:40:22 +0800

    1662. 检查两个字符串数组是否相等

---
+++ Ahao.LeetCode/1600-1699/demo1662/Class1662.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1662
+{
+    public class Class1662
+    {
+        public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            return Join(word1).Equals(Join(word2));
+        }
+
+        public string Join(string[] words)
+        {
+            StringBuilder ret = new StringBuilder();
+            foreach (string s in words)
+            {
+                ret.Append(s);
+            }
+            return ret.ToString();
+        }
+
+
+
+
+        public bool ArrayStringsAreEqual2(string[] word1, string[] word2)
+        {
+            int p1 = 0, p2 = 0, i = 0, j = 0;
+            while (p1 < word1.Length && p2 < word2.Length)
+            {
+                if (word1[p1][i] != word2[p2][j])
+                {
+                    return false;
+                }
+                i++;
+                if (i == word1[p1].Length)
+                {
+                    p1++;
+                    i = 0;
+                }
+                j++;
+                if (j == word2[p2].Length)
+                {
+                    p2++;
+                    j = 0;
+                }
+            }
+            return p1 == word1.Length && p2 == word2.Length;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1600-1699/demo1662/JS1662.js
@@ -0,0 +1,34 @@
+﻿var arrayStringsAreEqual = function (word1, word2) {
+    return join(word1) === join(word2);
+}
+
+const join = (words) => {
+    let ret = '';
+    for (const s of words) {
+        ret += s;
+    }
+    return ret;
+};
+
+
+
+
+var arrayStringsAreEqual = function (word1, word2) {
+    let p1 = 0, p2 = 0, i = 0, j = 0;
+    while (p1 < word1.length && p2 < word2.length) {
+        if (word1[p1][i] !== word2[p2][j]) {
+            return false;
+        }
+        i++;
+        if (i === word1[p1].length) {
+            p1++;
+            i = 0;
+        }
+        j++;
+        if (j === word2[p2].length) {
+            p2++;
+            j = 0;
+        }
+    }
+    return p1 == word1.length && p2 == word2.length;
+};
