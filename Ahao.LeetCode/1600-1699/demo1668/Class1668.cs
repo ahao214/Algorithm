19c6d91d4ea73451a42c0690fb1e92a77d790f5b
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-03 09:36:17 +0800

    1668. 最大重复子字符串

---
+++ Ahao.LeetCode/1600-1699/demo1668/Class1668.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1668
+{
+    public class Class1668
+    {
+        public int MaxRepeating(string sequence, string word)
+        {
+            int n = sequence.Length, m = word.Length;
+            if (n < m)
+            {
+                return 0;
+            }
+
+            int[] f = new int[n];
+            for (int i = m - 1; i < n; ++i)
+            {
+                bool valid = true;
+                for (int j = 0; j < m; ++j)
+                {
+                    if (sequence[i - m + j + 1] != word[j])
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid)
+                {
+                    f[i] = (i == m - 1 ? 0 : f[i - m]) + 1;
+                }
+            }
+
+            return f.Max();
+        }
+    }
+}
