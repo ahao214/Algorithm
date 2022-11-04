af7c549ba9a9c162245d44e614f8b1d7f578a134
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-04 09:40:07 +0800

    754. 到达终点数字

---
+++ Ahao.LeetCodeCPP/754. 到达终点数字.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int reachNumber(int target) {
+        target = abs(target);
+        int k = 0;
+        while (target > 0) {
+            k++;
+            target -= k;
+        }
+        return target % 2 == 0 ? k : k + 1 + k % 2;
+    }
+};
