aaffd05283b91d6561872975c87171cbf1bb2a63
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-04 09:38:59 +0800

    754. 到达终点数字

---
+++ Ahao.LeetCode/0700-0799/demo754/Class754.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo754
+{
+    public class Class754
+    {
+        public int ReachNumber(int target)
+        {
+            target = Math.Abs(target);
+            int k = 0;
+            while (target > 0)
+            {
+                k++;
+                target -= k;
+            }
+            return target % 2 == 0 ? k : k + 1 + k % 2;
+        }
+    }
+}
