c2a9fc462f046c9e72c6976e81ad777801b21b3c
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-06 09:35:07 +0800

    1678. 设计 Goal 解析器

---
+++ Ahao.LeetCode/1600-1699/demo1678/Class1678.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1678
+{
+    public class Class1678
+    {
+        public string Interpret(string command)
+        {
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < command.Length; i++)
+            {
+                if (command[i] == 'G')
+                {
+                    res.Append("G");
+                }
+                else if (command[i] == '(')
+                {
+                    if (command[i + 1] == ')')
+                    {
+                        res.Append("o");
+                    }
+                    else
+                    {
+                        res.Append("al");
+                    }
+                }
+            }
+            return res.ToString();
+        }
+    }
+}
---
+++ Ahao.LeetCode/1600-1699/demo1678/JS1678.js
@@ -0,0 +1,15 @@
+﻿var interpret = function (command) {
+    let res = '';
+    for (let i = 0; i < command.length; i++) {
+        if (command[i] === 'G') {
+            res += 'G';
+        } else if (command[i] === '(') {
+            if (command[i + 1] === ')') {
+                res += 'o';
+            } else {
+                res += 'al';
+            }
+        }
+    }
+    return res;
+};
