0aa1d23de6197390553d4f4728f57e13f4fe8780
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-07 09:43:23 +0800

    816. 模糊坐标

---
+++ Ahao.LeetCode/0800-0899/demo816/Class816.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo816
+{
+    public class Class816
+    {
+        public IList<string> AmbiguousCoordinates(string s)
+        {
+            int n = s.Length - 2;
+            IList<string> res = new List<string>();
+            s = s.Substring(1, s.Length - 2);
+            for (int l = 1; l < n; ++l)
+            {
+                IList<string> lt = GetPos(s.Substring(0, l));
+                if (lt.Count == 0)
+                {
+                    continue;
+                }
+                IList<string> rt = GetPos(s.Substring(l));
+                if (rt.Count == 0)
+                {
+                    continue;
+                }
+                foreach (string i in lt)
+                {
+                    foreach (string j in rt)
+                    {
+                        res.Add("(" + i + ", " + j + ")");
+                    }
+                }
+            }
+            return res;
+        }
+
+        public IList<string> GetPos(string s)
+        {
+            IList<string> pos = new List<string>();
+            if (s[0] != '0' || "0".Equals(s))
+            {
+                pos.Add(s);
+            }
+            for (int p = 1; p < s.Length; ++p)
+            {
+                if ((p != 1 && s[0] == '0') || s[s.Length - 1] == '0')
+                {
+                    continue;
+                }
+                pos.Add(s.Substring(0, p) + "." + s.Substring(p));
+            }
+            return pos;
+        }
+    }
+}
