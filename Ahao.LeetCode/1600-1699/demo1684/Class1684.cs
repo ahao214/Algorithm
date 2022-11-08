2a8b7eaaefe66a7cdbf43f7853c9184484113e3d
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-08 09:36:13 +0800

    1684. 统计一致字符串的数目

---
+++ Ahao.LeetCode/1600-1699/demo1684/Class1684.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1684
+{
+    public class Class1684
+    {
+        public int CountConsistentStrings(string allowed, string[] words)
+        {
+            int mask = 0;
+            foreach (char c in allowed)
+            {
+                mask |= 1 << (c - 'a');
+            }
+            int res = 0;
+            foreach (string word in words)
+            {
+                int mask1 = 0;
+                foreach (char c in word)
+                {
+                    mask1 |= 1 << (c - 'a');
+                }
+                if ((mask1 | mask) == mask)
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+
+    }
+}
