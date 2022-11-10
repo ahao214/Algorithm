e34659d21864a5efe6258a751bbb4d5c00ec8fef
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-10 09:55:15 +0800

    1694. 重新格式化电话号码

--- Ahao.LeetCodeCPP/1694. 重新格式化电话号码.cpp
+++ Ahao.LeetCodeCPP/1694. 重新格式化电话号码.cpp
@@ -51,3 +51,52 @@ public:
 		return ans;
 	}
 };
+
+
+
+
+/*
+1694. 重新格式化电话号码
+给你一个字符串形式的电话号码 number 。number 由数字、空格 ' '、和破折号 '-' 组成。
+
+请你按下述方式重新格式化电话号码。
+
+首先，删除 所有的空格和破折号。
+其次，将数组从左到右 每 3 个一组 分块，直到 剩下 4 个或更少数字。剩下的数字将按下述规定再分块：
+2 个数字：单个含 2 个数字的块。
+3 个数字：单个含 3 个数字的块。
+4 个数字：两个分别含 2 个数字的块。
+最后用破折号将这些块连接起来。注意，重新格式化过程中 不应该 生成仅含 1 个数字的块，并且 最多 生成两个含 2 个数字的块。
+
+返回格式化后的电话号码。
+*/
+class Solution {
+public:
+	string reformatNumber(string number) {
+		string s;
+		//删除空格和横线
+		for (auto c : number)
+			if (c != ' ' && c != '-')
+				s += c;
+
+		string res;
+		for (int i = 0; i < s.size();)
+		{
+			if (s.size() - i > 4)
+			{
+				res += s.substr(i, 3);
+				i += 3;
+			}
+			else
+			{
+				int k = s.size() - i;
+				if (k <= 3) res += s.substr(i, k);
+				else res += s.substr(i, 2) + '-' + s.substr(i + 2);
+				i += 4;
+			}
+			res += '-';
+		}
+		res.pop_back();
+		return res;
+	}
+};
