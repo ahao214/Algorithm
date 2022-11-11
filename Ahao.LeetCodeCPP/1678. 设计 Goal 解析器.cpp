1cd19e96f6b208194fa144d480ee6b23fdd32853
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-11 09:43:17 +0800

    1678. 设计 Goal 解析器

--- Ahao.LeetCodeCPP/1678. 设计 Goal 解析器.cpp
+++ Ahao.LeetCodeCPP/1678. 设计 Goal 解析器.cpp
@@ -22,3 +22,33 @@ public:
         return res;
     }
 };
+
+
+
+
+/*
+1678. 设计 Goal 解析器
+请你设计一个可以解释字符串 command 的 Goal 解析器 。command 由 "G"、"()" 和/或 "(al)" 按某种顺序组成。Goal 解析器会将 "G" 解释为字符串 "G"、"()" 解释为字符串 "o" ，"(al)" 解释为字符串 "al" 。然后，按原顺序将经解释得到的字符串连接成一个字符串。
+
+给你字符串 command ，返回 Goal 解析器 对 command 的解释结果。
+*/
+class Solution {
+public:
+    string interpret(string command) {
+        string res;
+        for (int i = 0; i < command.size(); i++)
+        {
+            if (command[i] == 'G')
+                res += 'G';
+            else if (command.substr(i, 4) == "(al)") {
+                res += "al";
+                i += 3;
+            }
+            else {
+                res += 'o';
+                i++;
+            }
+        }
+        return res;
+    }
+};
