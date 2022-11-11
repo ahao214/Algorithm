946da0bc494d2942e7e8f56b4144a55362e035ed
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-11 13:51:20 +0800

    1668. 最大重复子字符串

--- Ahao.LeetCodeCPP/1668. 最大重复子字符串.cpp
+++ Ahao.LeetCodeCPP/1668. 最大重复子字符串.cpp
@@ -5,6 +5,13 @@
 using namespace std;
 
 
+/*
+1668. 最大重复子字符串
+给你一个字符串 sequence ，如果字符串 word 连续重复 k 次形成的字符串是 sequence 的一个子字符串，那么单词 word 的 重复值为 k 。单词 word 的 最大重复值 是单词 word 在 sequence 中最大的重复值。如果 word 不是 sequence 的子串，那么重复值 k 为 0 。
+
+给你一个字符串 sequence 和 word ，请你返回 最大重复值 k 。
+*/
+
 class Solution {
 public:
     int maxRepeating(string sequence, string word) {
@@ -28,5 +35,24 @@ public:
         }
 
         return *max_element(f.begin(), f.end());
+    }
+};
+
+
+
+
+
+class Solution {
+public:
+    int maxRepeating(string sequence, string word) {
+        int res = 0;
+        string s = word;
+        while (s.size() <= sequence.size())
+        {
+            if (sequence.find(s) == -1)break;
+            res++;
+            s += word;
+        }
+        return res;
     }
 };
