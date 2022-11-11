14a948746e114205c1f97a5d1ede518d46190e79
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-11 09:40:08 +0800

    1704. 判断字符串的两半是否相似

---
+++ Ahao.LeetCode/1700-1799/demo1704/Class1704.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1704
+{
+    public class Class1704
+    {
+        public bool HalvesAreAlike(string s)
+        {
+            string a = s.Substring(0, s.Length / 2);
+            string b = s.Substring(s.Length / 2);
+            string h = "aeiouAEIOU";
+            int sum1 = 0, sum2 = 0;
+            foreach (char c in a)
+            {
+                if (h.IndexOf(c) >= 0)
+                {
+                    sum1++;
+                }
+            }
+            foreach (char c in b)
+            {
+                if (h.IndexOf(c) >= 0)
+                {
+                    sum2++;
+                }
+            }
+            return sum1 == sum2;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1700-1799/demo1704/JS1704.js
@@ -0,0 +1,17 @@
+﻿var halvesAreAlike = function (s) {
+    const a = s.substring(0, s.length / 2);
+    const b = s.substring(s.length / 2);
+    const h = "aeiouAEIOU";
+    let sum1 = 0, sum2 = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (h.indexOf(a[i]) >= 0) {
+            sum1++;
+        }
+    }
+    for (let i = 0; i < b.length; i++) {
+        if (h.indexOf(b[i]) >= 0) {
+            sum2++;
+        }
+    }
+    return sum1 === sum2;
+};
