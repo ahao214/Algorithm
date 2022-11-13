ccbcc735b52d226ea8256d87274cfe0f465f7061
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-13 13:21:46 +0800

    791. 自定义字符串排序

---
+++ Ahao.LeetCode/0700-0799/demo791/Class791.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo791
+{
+    public class Class791
+    {
+        /// <summary>
+        /// 自定义排序
+        /// </summary>
+        /// <param name="order"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string CustomSortString(string order, string s)
+        {
+            int[] val = new int[26];
+            for (int i = 0; i < order.Length; ++i)
+            {
+                val[order[i] - 'a'] = i + 1;
+            }
+            char[] arr = s.ToCharArray();
+            Array.Sort(arr, (c0, c1) => val[c0 - 'a'] - val[c1 - 'a']);
+            return new string(arr);
+        }
+
+
+        /// <summary>
+        /// 计数排序
+        /// </summary>
+        /// <param name="order"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string CustomSortString2(string order, string s)
+        {
+            int[] freq = new int[26];
+            foreach (char ch in s)
+            {
+                ++freq[ch - 'a'];
+            }
+            StringBuilder ans = new StringBuilder();
+            foreach (char ch in order)
+            {
+                while (freq[ch - 'a'] > 0)
+                {
+                    ans.Append(ch);
+                    freq[ch - 'a']--;
+                }
+            }
+            for (int i = 0; i < 26; ++i)
+            {
+                while (freq[i] > 0)
+                {
+                    ans.Append((char)(i + 'a'));
+                    freq[i]--;
+                }
+            }
+            return ans.ToString();
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/0700-0799/demo791/JS791.js
@@ -0,0 +1,39 @@
+﻿//自定义排序
+var customSortString = function (order, s) {
+    const val = new Array(26).fill(0);
+    for (let i = 0; i < order.length; ++i) {
+        val[order[i].charCodeAt() - 'a'.charCodeAt()] = i + 1;
+    }
+    const arr = new Array(s.length).fill(0).map((_, i) => s[i]);
+    arr.sort((c0, c1) => val[c0.charCodeAt() - 'a'.charCodeAt()] - val[c1.charCodeAt() - 'a'.charCodeAt()])
+    let ans = '';
+    for (let i = 0; i < s.length; ++i) {
+        ans += arr[i];
+    }
+    return ans;
+};
+
+
+//计数排序
+var customSortString = function (order, s) {
+    const freq = new Array(26).fill(0);
+    for (let i = 0; i < s.length; ++i) {
+        const ch = s[i];
+        ++freq[ch.charCodeAt() - 'a'.charCodeAt()];
+    }
+    let ans = '';
+    for (let i = 0; i < order.length; ++i) {
+        const ch = order[i];
+        while (freq[ch.charCodeAt() - 'a'.charCodeAt()] > 0) {
+            ans += ch;
+            freq[ch.charCodeAt() - 'a'.charCodeAt()]--;
+        }
+    }
+    for (let i = 0; i < 26; ++i) {
+        while (freq[i] > 0) {
+            ans += String.fromCharCode(i + 'a'.charCodeAt());
+            freq[i]--;
+        }
+    }
+    return ans;
+};
