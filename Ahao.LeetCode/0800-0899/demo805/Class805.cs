92e710a09ac09f21fefd885e61f9bce7f2a5e555
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-14 09:41:39 +0800

    805. 数组的均值分割

--- Ahao.LeetCode/0800-0899/demo805/Class805.cs
+++ Ahao.LeetCode/0800-0899/demo805/Class805.cs
@@ -64,5 +64,61 @@ namespace Ahao.LeetCode._0800_0899.demo805
             }
             return false;
         }
+
+
+        /// <summary>
+        /// 动态规划
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public bool SplitArraySameAverage2(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return false;
+            }
+            int n = nums.Length, m = n / 2;
+            int sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+            bool isPossible = false;
+            for (int i = 1; i <= m; i++)
+            {
+                if (sum * i % n == 0)
+                {
+                    isPossible = true;
+                    break;
+                }
+            }
+            if (!isPossible)
+            {
+                return false;
+            }
+            ISet<int>[] dp = new ISet<int>[m + 1];
+            for (int i = 0; i <= m; i++)
+            {
+                dp[i] = new HashSet<int>();
+            }
+            dp[0].Add(0);
+            foreach (int num in nums)
+            {
+                for (int i = m; i >= 1; i--)
+                {
+                    foreach (int x in dp[i - 1])
+                    {
+                        int curr = x + num;
+                        if (curr * n == sum * i)
+                        {
+                            return true;
+                        }
+                        dp[i].Add(curr);
+                    }
+                }
+            }
+            return false;
+        }
+
     }
 }
--- Ahao.LeetCode/0800-0899/demo805/JS805.js
+++ Ahao.LeetCode/0800-0899/demo805/JS805.js
@@ -41,3 +41,40 @@
     }
     return false;
 };
+
+
+//动态规划
+var splitArraySameAverage = function (nums) {
+    if (nums.length === 1) {
+        return false;
+    }
+    const n = nums.length, m = Math.floor(n / 2);
+    let sum = 0;
+    for (const num of nums) {
+        sum += num;
+    }
+    let isPossible = false;
+    for (let i = 1; i <= m; i++) {
+        if (sum * i % n === 0) {
+            isPossible = true;
+            break;
+        }
+    }
+    if (!isPossible) {
+        return false;
+    }
+    const dp = new Array(m + 1).fill(0).map(() => new Set());
+    dp[0].add(0);
+    for (const num of nums) {
+        for (let i = m; i >= 1; i--) {
+            for (const x of dp[i - 1]) {
+                let curr = x + num;
+                if (curr * n === sum * i) {
+                    return true;
+                }
+                dp[i].add(curr);
+            }
+        }
+    }
+    return false;
+};
