5e3d10fa942ef685f10dac2107ab014b8eff0f98
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-14 10:49:17 +0800

    1464. 数组中两元素的最大乘积

--- Ahao.LeetCodeCPP/1464. 数组中两元素的最大乘积.cpp
+++ Ahao.LeetCodeCPP/1464. 数组中两元素的最大乘积.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 /*
 1464. 数组中两元素的最大乘积
+给你一个整数数组 nums，请你选择数组的两个不同下标 i 和 j，使 (nums[i]-1)*(nums[j]-1) 取得最大值。
+
+请你计算并返回该式的最大值。
 */
 class Solution {
 public:
@@ -32,5 +35,19 @@ public:
 			}
 		}
 		return (a - 1) * (b - 1);
+	}
+};
+
+
+class Solution {
+public:
+	int maxProduct(vector<int>& nums) {
+		int res = INT_MIN;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			for (int j = 0; j < i; j++)
+				res = max(res, (nums[i] - 1) * (nums[j] - 1));
+		}
+		return res;
 	}
 };
