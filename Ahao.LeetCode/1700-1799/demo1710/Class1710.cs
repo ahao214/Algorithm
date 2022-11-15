4107844d1fbd04627420c7ed9f31a3703e06e5ec
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-15 09:52:43 +0800

    1710. 卡车上的最大单元数

---
+++ Ahao.LeetCode/1700-1799/demo1710/Class1710.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1710
+{
+    public class Class1710
+    {
+        public int MaximumUnits(int[][] boxTypes, int truckSize)
+        {
+            Array.Sort(boxTypes, (a, b) => b[1] - a[1]);
+            int res = 0;
+            foreach (int[] boxType in boxTypes)
+            {
+                int numberOfBoxes = boxType[0];
+                int numberOfUnitsPerBox = boxType[1];
+                if (numberOfBoxes < truckSize)
+                {
+                    res += numberOfBoxes * numberOfUnitsPerBox;
+                    truckSize -= numberOfBoxes;
+                }
+                else
+                {
+                    res += truckSize * numberOfUnitsPerBox;
+                    break;
+                }
+            }
+            return res;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1700-1799/demo1710/JS1710.js
@@ -0,0 +1,16 @@
+﻿var maximumUnits = function (boxTypes, truckSize) {
+    boxTypes.sort((a, b) => b[1] - a[1]);
+    let res = 0;
+    for (const boxType of boxTypes) {
+        let numberOfBoxes = boxType[0];
+        let numberOfUnitsPerBox = boxType[1];
+        if (numberOfBoxes < truckSize) {
+            res += numberOfBoxes * numberOfUnitsPerBox;
+            truckSize -= numberOfBoxes;
+        } else {
+            res += truckSize * numberOfUnitsPerBox;
+            break;
+        }
+    }
+    return res;
+};
