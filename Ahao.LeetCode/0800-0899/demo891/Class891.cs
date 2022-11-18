744367e8a47bf4b489b570a16d275ad8891ef714
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-18 09:59:32 +0800

    891. 子序列宽度之和

--- Ahao.LeetCode/0800-0899/demo891/Class891.cs
+++ Ahao.LeetCode/0800-0899/demo891/Class891.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode._0800_0899.demo891
 {
-    class Class891
+    public class Class891
     {
+        public int SumSubseqWidths(int[] nums)
+        {
+            const int MOD = 1000000007;
+            Array.Sort(nums);
+            long res = 0;
+            long x = nums[0], y = 2;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                res = (res + nums[j] * (y - 1) - x) % MOD;
+                x = (x * 2 + nums[j]) % MOD;
+                y = y * 2 % MOD;
+            }
+            return (int)((res + MOD) % MOD);
+        }
     }
 }
