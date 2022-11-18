1b69cb1281285bec3602a8c6b9526390a6a7f7f1
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-18 10:40:41 +0800

    20. 有效的括号

--- Ahao.LeetCodeCPP/20. 有效的括号.cpp
+++ Ahao.LeetCodeCPP/20. 有效的括号.cpp
@@ -26,3 +26,40 @@ public:
 	}
 };
 
+
+/*
+20. 有效的括号
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+*/
+class Solution {
+public:
+	bool isValid(string s) {
+		stack<char> stk;
+		for (auto c : s)
+		{
+			if (c == ')')
+			{
+				if (stk.empty() || stk.top() != '(') return false;
+				stk.pop();
+			}
+			else if (c == ']')
+			{
+				if (stk.empty() || stk.top() != '[') return false;
+				stk.pop();
+			}
+			else if (c == '}')
+			{
+				if (stk.empty() || stk.top() != '{') return false;
+				stk.pop();
+			}
+			else
+				stk.push(c);
+		}
+		return stk.empty();
+	}
+};
