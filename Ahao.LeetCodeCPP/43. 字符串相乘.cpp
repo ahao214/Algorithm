c3aa94da8cc19fa8b53f9f21de024f21ed11a5c4
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-18 10:50:24 +0800

    43. 字符串相乘

--- Ahao.LeetCodeCPP/43. 字符串相乘.cpp
+++ Ahao.LeetCodeCPP/43. 字符串相乘.cpp
@@ -39,3 +39,42 @@ public:
 		return ans.empty() ? "0" : ans;
 	}
 };
+
+
+
+
+/*
+43. 字符串相乘
+给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+
+注意：不能使用任何内置的 BigInteger 库或直接将输入转换为整数。
+*/
+class Solution {
+public:
+	string multiply(string num1, string num2) {
+		vector<int> product(num1.size() + num2.size(), 0);
+		for (int i = 0; i < num1.size(); i++)
+		{
+			for (int j = 0; j < num2.size(); j++)
+			{
+				product[num1.size() - i + num2.size() - j - 2] += (num1[i] - '0') * (num2[j] - '0');
+			}
+		}
+		int t = 0;
+		for (int i = 0; i < product.size(); i++)
+		{
+			int& x = product[i];
+			t += x;
+			x = t % 10;
+			t /= 10;
+		}
+		string res;
+		int k = product.size() - 1;
+		while (!product[k] && k > 0) k--;
+		for (int i = k; i >= 0; i--)
+		{
+			res += to_string(product[i]);
+		}
+		return res;
+	}
+};
