cadfabd768d3a44af1776e3f7e81e6f70d37f596
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-19 09:06:53 +0800

    1732. 找到最高海拔

---
+++ Ahao.LeetCode/1700-1799/demo1732/Class1732.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1732
+{
+    public class Class1732
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            int ans = 0, sum = 0;
+            foreach (int x in gain)
+            {
+                sum += x;
+                ans = Math.Max(ans, sum);
+            }
+            return ans;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1700-1799/demo1732/JS1732.js
@@ -0,0 +1,8 @@
+﻿var largestAltitude = function(gain) {
+    let ans = 0, sum = 0;
+    for (const x of gain) {
+        sum += x;
+        ans = Math.max(ans, sum);
+    }
+    return ans;
+};
