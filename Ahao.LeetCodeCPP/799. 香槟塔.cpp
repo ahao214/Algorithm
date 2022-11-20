de34f7cd206675f03ada0f5c755310b00e1c3a5d
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-20 19:20:46 +0800

    799. 香槟塔

---
+++ Ahao.LeetCodeCPP/799. 香槟塔.cpp
@@ -0,0 +1,34 @@
+#include<vector>
+
+using namespace std;
+
+/*
+799. 香槟塔
+
+我们把玻璃杯摆成金字塔的形状，其中 第一层 有 1 个玻璃杯， 第二层 有 2 个，依次类推到第 100 层，每个玻璃杯 (250ml) 将盛有香槟。
+
+从顶层的第一个玻璃杯开始倾倒一些香槟，当顶层的杯子满了，任何溢出的香槟都会立刻等流量的流向左右两侧的玻璃杯。当左右两边的杯子也满了，就会等流量的流向它们左右两边的杯子，依次类推。（当最底层的玻璃杯满了，香槟会流到地板上）
+
+例如，在倾倒一杯香槟后，最顶层的玻璃杯满了。倾倒了两杯香槟后，第二层的两个玻璃杯各自盛放一半的香槟。在倒三杯香槟后，第二层的香槟满了 - 此时总共有三个满的玻璃杯。在倒第四杯后，第三层中间的玻璃杯盛放了一半的香槟，他两边的玻璃杯各自盛放了四分之一的香槟，如下图所示。
+
+现在当倾倒了非负整数杯香槟后，返回第 i 行 j 个玻璃杯所盛放的香槟占玻璃杯容积的比例（ i 和 j 都从0开始）。
+*/
+
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<double> row = { (double)poured };
+        for (int i = 1; i <= query_row; i++) {
+            vector<double> nextRow(i + 1, 0.0);
+            for (int j = 0; j < row.size(); j++) {
+                double volume = row[j];
+                if (volume > 1) {
+                    nextRow[j] += (volume - 1) / 2;
+                    nextRow[j + 1] += (volume - 1) / 2;
+                }
+            }
+            row = nextRow;
+        }
+        return min(1.0, row[query_glass]);
+    }
+};
