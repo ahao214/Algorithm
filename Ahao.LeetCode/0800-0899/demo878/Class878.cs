5819de1bbb539c94dc0a6cc5228d4e2b49d27ab0
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-22 09:51:10 +0800

    878. 第 N 个神奇数字

---
+++ Ahao.LeetCode/0800-0899/demo878/Class878.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo878
+{
+    public class Class878
+    {
+        const int MOD = 1000000007;
+        /// <summary>
+        /// 方法一：容斥原理 + 二分查找
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int NthMagicalNumber(int n, int a, int b)
+        {
+            long l = Math.Min(a, b);
+            long r = (long)n * Math.Min(a, b);
+            int c = LCM(a, b);
+            while (l <= r)
+            {
+                long mid = (l + r) / 2;
+                long cnt = mid / a + mid / b - mid / c;
+                if (cnt >= n)
+                {
+                    r = mid - 1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            return (int)((r + 1) % MOD);
+        }
+     
+        /// <summary>
+        /// 方法二：找规律
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int NthMagicalNumber2(int n, int a, int b)
+        {
+            int c = LCM(a, b);
+            int m = c / a + c / b - 1;
+            int r = n % m;
+            int res = (int)((long)c * (n / m) % MOD);
+            if (r == 0)
+            {
+                return res;
+            }
+            int addA = a, addB = b;
+            for (int i = 0; i < r - 1; ++i)
+            {
+                if (addA < addB)
+                {
+                    addA += a;
+                }
+                else
+                {
+                    addB += b;
+                }
+            }
+            return (res + Math.Min(addA, addB) % MOD) % MOD;
+        }
+
+        public int LCM(int a, int b)
+        {
+            return a * b / GCD(a, b);
+        }
+
+        public int GCD(int a, int b)
+        {
+            return b != 0 ? GCD(b, a % b) : a;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/0800-0899/demo878/JS878.js
@@ -0,0 +1,58 @@
+﻿//方法一：容斥原理 + 二分查找
+const MOD = 1000000007;
+var nthMagicalNumber = function (n, a, b) {
+    let l = Math.min(a, b);
+    let r = n * Math.min(a, b);
+    const c = lcm(a, b);
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2);
+        const cnt = Math.floor(mid / a) + Math.floor(mid / b) - Math.floor(mid / c);
+        if (cnt >= n) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return (r + 1) % MOD;
+}
+
+const lcm = (a, b) => {
+    return Math.floor(a * b / gcd(a, b));
+}
+
+const gcd = (a, b) => {
+    return b !== 0 ? gcd(b, a % b) : a;
+};
+
+
+
+
+//方法二：找规律
+const MOD = 1000000007;
+var nthMagicalNumber = function (n, a, b) {
+    const c = lcm(a, b);
+    const m = Math.floor(c / a) + Math.floor(c / b) - 1;
+    const r = n % m;
+    const res = c * Math.floor(n / m) % MOD;
+    if (r === 0) {
+        return res;
+    }
+    let addA = a, addB = b;
+    for (let i = 0; i < r - 1; ++i) {
+        if (addA < addB) {
+            addA += a;
+        } else {
+            addB += b;
+        }
+    }
+    return (res + Math.min(addA, addB) % MOD) % MOD;
+}
+
+const lcm = (a, b) => {
+    return Math.floor(a * b / gcd(a, b));
+}
+
+const gcd = (a, b) => {
+    return b !== 0 ? gcd(b, a % b) : a;
+};
+
