f7d414953580e60de643aa5617110f96219b351e
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-23 09:37:29 +0800

    1742. 盒子中小球的最大数量

---
+++ Ahao.LeetCode/1700-1799/demo1742/Class1742.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1742
+{
+    public class Class1742
+    {
+        public int CountBalls(int lowLimit, int highLimit)
+        {
+            IDictionary<int, int> count = new Dictionary<int, int>();
+            int res = 0;
+            for (int i = lowLimit; i <= highLimit; i++)
+            {
+                int box = 0, x = i;
+                while (x != 0)
+                {
+                    box += x % 10;
+                    x /= 10;
+                }
+                count.TryAdd(box, 0);
+                count[box]++;
+                res = Math.Max(res, count[box]);
+            }
+            return res;
+        }
+    }
+}
