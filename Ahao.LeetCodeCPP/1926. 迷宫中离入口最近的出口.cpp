ead2afca14433eb7246ef4e00a80bc3b6adda973
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-23 10:11:43 +0800

    1926. 迷宫中离入口最近的出口

--- Ahao.LeetCodeCPP/1926. 迷宫中离入口最近的出口.cpp
+++ Ahao.LeetCodeCPP/1926. 迷宫中离入口最近的出口.cpp
@@ -47,3 +47,47 @@ public:
 };
 
 
+
+
+/*
+1926. 迷宫中离入口最近的出口
+给你一个 m x n 的迷宫矩阵 maze （下标从 0 开始），矩阵中有空格子（用 '.' 表示）和墙（用 '+' 表示）。同时给你迷宫的入口 entrance ，用 entrance = [entrancerow, entrancecol] 表示你一开始所在格子的行和列。
+
+每一步操作，你可以往 上，下，左 或者 右 移动一个格子。你不能进入墙所在的格子，你也不能离开迷宫。你的目标是找到离 entrance 最近 的出口。出口 的含义是 maze 边界 上的 空格子。entrance 格子 不算 出口。
+
+请你返回从 entrance 到最近出口的最短路径的 步数 ，如果不存在这样的路径，请你返回 -1 。
+*/
+class Solution {
+public:
+	int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+		int m = maze.size();
+		int n = maze[0].size();
+		queue<vector<int>> q;
+		q.push(entrance);
+		int steps = 0;
+		maze[entrance[0]][entrance[1]] = 'x';
+		vector<int> dir{ 0,-1,0,1,0 };
+		while (!q.empty()) {
+			int size = q.size();
+			steps++;
+			for (int i = 0; i < size; i++)
+			{
+				auto point = q.front();
+				q.pop();
+				for (int k = 0; k < 4; k++) {
+					int x = point[0] + dir[k], y = point[1] + dir[k + 1];
+					if (x >= 0 && x < m && y >= 0 && y < n && maze[x][y] == '.') {
+						if (x == 0 || x == m - 1 || y == 0 || y == n - 1)
+						{
+							return steps;
+						}
+						q.push({ x,y });
+						maze[x][y] = 'x';
+					}
+				}
+			}
+		}
+		return -1;
+	}
+};
+
