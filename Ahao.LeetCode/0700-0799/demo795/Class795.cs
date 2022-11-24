089378479f3f3ec2b326fa23e312189f1580152d
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-24 09:25:21 +0800

    795. 区间子数组个数

---
+++ Ahao.LeetCode/0700-0799/demo795/Class795.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo795
+{
+    public class Class795
+    {
+        /// <summary>
+        /// 一次遍历
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public int NumSubarrayBoundedMax(int[] nums, int left, int right)
+        {
+            int res = 0, last2 = -1, last1 = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= left && nums[i] <= right)
+                {
+                    last1 = i;
+                }
+                else if (nums[i] > right)
+                {
+                    last2 = i;
+                    last1 = -1;
+                }
+                if (last1 != -1)
+                {
+                    res += last1 - last2;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0700-0799/demo795/JS795.js
@@ -0,0 +1,16 @@
+﻿//一次遍历
+var numSubarrayBoundedMax = function (nums, left, right) {
+    let res = 0, last2 = -1, last1 = -1;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= left && nums[i] <= right) {
+            last1 = i;
+        } else if (nums[i] > right) {
+            last2 = i;
+            last1 = -1;
+        }
+        if (last1 !== -1) {
+            res += last1 - last2;
+        }
+    }
+    return res;
+};
