ad1444fa5caddd83c84ddd1d718b8b2b39f9210f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-27 12:34:52 +0800

    1752. 检查数组是否经排序和轮转得到

--- Ahao.LeetCodeCPP/1752. 检查数组是否经排序和轮转得到.cpp
+++ Ahao.LeetCodeCPP/1752. 检查数组是否经排序和轮转得到.cpp
@@ -1,4 +1,5 @@
 #include<vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -32,3 +33,24 @@ public:
 };
 
 
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size(), x = 0;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] < nums[i - 1]) {
+                x = i;
+                break;
+            }
+        }
+        if (x == 0) {
+            return true;
+        }
+        for (int i = x + 1; i < n; ++i) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+        return nums[0] >= nums[n - 1];
+    }
+};
