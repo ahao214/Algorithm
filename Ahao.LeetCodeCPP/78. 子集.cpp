bf252c0921cb68c4694a683a8697832ee1d0ea6f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-27 13:24:47 +0800

    78. 子集

--- Ahao.LeetCodeCPP/78. 子集.cpp
+++ Ahao.LeetCodeCPP/78. 子集.cpp
@@ -3,12 +3,6 @@
 using namespace std;
 
 class Solution {
-	/*
-	78. 子集
-	给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
-
-	解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
-	*/
 public:
 	vector<vector<int>> subsets(vector<int>& nums) {
 		int n = nums.size();
@@ -23,5 +17,35 @@ public:
 			ans.push_back(subset);
 		}
 		return ans;
+	}
+};
+
+
+
+
+/*
+78. 子集
+给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+
+解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+*/
+class Solution {
+
+	void backtracking(vector<vector<int>>& res, vector<int>& path, vector<int>& nums, int start)
+	{
+		res.push_back(path);
+		for (int i = start; i < nums.size(); i++)
+		{
+			path.push_back(nums[i]);
+			backtracking(res, path, nums, i + 1);
+			path.pop_back();
+		}
+	}
+public:
+	vector<vector<int>> subsets(vector<int>& nums) {
+		vector<vector<int>> res;
+		vector<int> path;
+		backtracking(res, path, nums, 0);
+		return res;
 	}
 };
