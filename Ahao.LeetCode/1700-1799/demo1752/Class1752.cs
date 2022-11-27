ec4d30256d74fad599e9dbe2d3cec41979f53851
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-27 12:34:17 +0800

    1752. 检查数组是否经排序和轮转得到

--- Ahao.LeetCode/1700-1799/demo1752/Class1752.cs
+++ Ahao.LeetCode/1700-1799/demo1752/Class1752.cs
@@ -25,5 +25,32 @@ namespace Ahao.LeetCode._1700_1799.demo1752
             return false;
         }
 
+
+
+        public bool Check2(int[] nums)
+        {
+            int n = nums.Length, x = 0;
+            for (int i = 1; i < n; ++i)
+            {
+                if (nums[i] < nums[i - 1])
+                {
+                    x = i;
+                    break;
+                }
+            }
+            if (x == 0)
+            {
+                return true;
+            }
+            for (int i = x + 1; i < n; ++i)
+            {
+                if (nums[i] < nums[i - 1])
+                {
+                    return false;
+                }
+            }
+            return nums[0] >= nums[n - 1];
+        }
+
     }
 }
--- Ahao.LeetCode/1700-1799/demo1752/JS1752.js
+++ Ahao.LeetCode/1700-1799/demo1752/JS1752.js
@@ -1,1 +1,18 @@
-﻿
+﻿var check = function (nums) {
+    let n = nums.length, x = 0;
+    for (let i = 1; i < n; ++i) {
+        if (nums[i] < nums[i - 1]) {
+            x = i;
+            break;
+        }
+    }
+    if (x === 0) {
+        return true;
+    }
+    for (let i = x + 1; i < n; ++i) {
+        if (nums[i] < nums[i - 1]) {
+            return false;
+        }
+    }
+    return nums[0] >= nums[n - 1];
+};
