f73057d853a63ff20791399007347b8e64016667
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-27 13:25:51 +0800

    532. 数组中的 k-diff 数对

--- Ahao.LeetCodeCPP/532. 数组中的 k-diff 数对.cpp
+++ Ahao.LeetCodeCPP/532. 数组中的 k-diff 数对.cpp
@@ -44,3 +44,46 @@ public:
 		return res;
 	}
 };
+
+
+
+
+/*
+532. 数组中的 k-diff 数对
+给定一个整数数组和一个整数 k，你需要在数组里找到 不同的 k-diff 数对，并返回不同的 k-diff 数对 的数目。
+
+这里将 k-diff 数对定义为一个整数对 (nums[i], nums[j])，并满足下述全部条件：
+
+0 <= i < j < nums.length
+|nums[i] - nums[j]| == k
+注意，|val| 表示 val 的绝对值。
+*/
+class Solution {
+public:
+	int findPairs(vector<int>& nums, int k) {
+		int size = nums.size();
+		int left = 0, right = 1, res = 0;
+		sort(nums.begin(), nums.end());
+		while (left < size && right < size)
+		{
+			if (left == right || (nums[right] - nums[left]) < k)
+			{
+				right++;
+			}
+			else if (nums[right] - nums[left] > k)
+			{
+				left++;
+			}
+			else
+			{
+				res++;
+				left++;
+				while (left < size && nums[left] == nums[left - 1])
+				{
+					left++;
+				}
+			}
+		}
+		return res;
+	}
+};
