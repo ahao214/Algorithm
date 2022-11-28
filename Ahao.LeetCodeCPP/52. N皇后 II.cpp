f2aefceb1fe816265c26096722dfe88cae562ae2
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-28 16:47:42 +0800

    52. N皇后 II

--- Ahao.LeetCodeCPP/52. N皇后 II.cpp
+++ Ahao.LeetCodeCPP/52. N皇后 II.cpp
@@ -2,14 +2,13 @@ using namespace std;
 #include<vector>
 #include<string>
 
+/*
+52. N皇后 II
+n 皇后问题 研究的是如何将 n 个皇后放置在 n × n 的棋盘上，并且使皇后彼此之间不能相互攻击。
 
+给你一个整数 n ，返回 n 皇后问题 不同的解决方案的数量。
+*/
 class Solution {
-	/*
-	52. N皇后 II
-	n 皇后问题 研究的是如何将 n 个皇后放置在 n × n 的棋盘上，并且使皇后彼此之间不能相互攻击。
-
-	给你一个整数 n ，返回 n 皇后问题 不同的解决方案的数量。
-	*/
 	vector<bool> col, dg, udg;
 	int ans = 0;
 public:
