3e5f1988f3373de02569ac1640637fd60e4dea81
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-28 09:39:15 +0800

    813. 最大平均值和的分组

---
+++ Ahao.LeetCode/0800-0899/demo813/Class813.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo813
+{
+    public class Class813
+    {
+        public double LargestSumOfAverages(int[] nums, int k)
+        {
+            int n = nums.Length;
+            double[] prefix = new double[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                prefix[i + 1] = prefix[i] + nums[i];
+            }
+            double[][] dp = new double[n + 1][];
+            for (int i = 0; i <= n; i++)
+            {
+                dp[i] = new double[k + 1];
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                dp[i][1] = prefix[i] / i;
+            }
+            for (int j = 2; j <= k; j++)
+            {
+                for (int i = j; i <= n; i++)
+                {
+                    for (int x = j - 1; x < i; x++)
+                    {
+                        dp[i][j] = Math.Max(dp[i][j], dp[x][j - 1] + (prefix[i] - prefix[x]) / (i - x));
+                    }
+                }
+            }
+            return dp[n][k];
+        }
+    }
+}
