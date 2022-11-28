71c2210b6426d07754bc5758275c340d6f206864
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-28 14:45:59 +0800

    76. 最小覆盖子串

--- Ahao.LeetCodeCPP/76. 最小覆盖子串.cpp
+++ Ahao.LeetCodeCPP/76. 最小覆盖子串.cpp
@@ -3,12 +3,17 @@
 
 using namespace std;
 
+/*
+76. 最小覆盖子串
+给你一个字符串  s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+
+注意：
+
+对于 t 中重复字符，我们寻找的子字符串中该字符数量必须不少于 t 中该字符数量。
+如果 s 中存在这样的子串，我们保证它是唯一的答案。
+*/
 
 class Solution {
-	/*
-	76. 最小覆盖子串
-	给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
-	*/
 public:
 	/*
 	Times:O(n)
@@ -32,5 +37,29 @@ public:
 			}
 		}
 		return ans;
+	}
+};
+
+
+class Solution {
+public:
+	string minWindow(string s, string t) {
+		unordered_map<char, int> hash;
+		for (auto c : t) hash[c]++;
+		int cnt = hash.size();
+
+		string res;
+		for (int i = 0, j = 0, c = 0; i < s.size(); i++)
+		{
+			if (hash[s[i]] == 1) c++;
+			hash[s[i]]--;
+			while (hash[s[j]] < 0) hash[s[j++]]++;
+			if (c == cnt)
+			{
+				if (res.empty() || res.size() > i - j + 1)
+					res = s.substr(j, i - j + 1);
+			}
+		}
+		return res;
 	}
 };
