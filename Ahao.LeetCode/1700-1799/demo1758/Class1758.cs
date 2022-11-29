4758a90ee85c5292e3a0f5b2d053ad4fc3f2a994
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-29 09:21:36 +0800

    1758. 生成交替二进制字符串的最少操作数

---
+++ Ahao.LeetCode/1700-1799/demo1758/Class1758.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1758
+{
+    public class Class1758
+    {
+        public int MinOperations(string s)
+        {
+            int cnt = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c != (char)('0' + i % 2))
+                {
+                    cnt++;
+                }
+            }
+            return Math.Min(cnt, s.Length - cnt);
+        }
+    }
+}
