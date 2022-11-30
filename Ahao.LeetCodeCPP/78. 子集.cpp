9fc7c11cbb8a89d14e002ae037984a4a3e14773f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-30 09:48:03 +0800

    78. 子集

--- Ahao.LeetCodeCPP/78. 子集.cpp
+++ Ahao.LeetCodeCPP/78. 子集.cpp
@@ -49,3 +49,30 @@ public:
 		return res;
 	}
 };
+
+
+
+/*
+78. 子集
+给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+
+解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+*/
+class Solution {
+public:
+	vector<vector<int>> subsets(vector<int>& nums) {
+		vector<vector<int>> res;
+		for (int i = 0; i < 1 << nums.size(); i++)
+		{
+			vector<int> now;
+			for (int j = 0; j < nums.size(); j++)
+			{
+				//如果二进制的位置上的数是1
+				if (i >> j & 1)
+					now.push_back(nums[j]);
+			}
+			res.push_back(now);
+		}
+		return res;
+	}
+};
