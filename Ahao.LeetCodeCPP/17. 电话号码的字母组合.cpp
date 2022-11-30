74895da6dac4ea211c06f98271384c0eb50ed8b4
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-30 11:25:47 +0800

    17. 电话号码的字母组合

--- Ahao.LeetCodeCPP/17. 电话号码的字母组合.cpp
+++ Ahao.LeetCodeCPP/17. 电话号码的字母组合.cpp
@@ -4,12 +4,6 @@ using namespace std;
 
 
 class Solution {
-	/*
-	17. 电话号码的字母组合
-	给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
-
-	给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
-	*/
 	vector<string> ans;
 	string strs[10] = {
 		"","","abc","def",
@@ -38,3 +32,35 @@ public:
 	}
 };
 
+
+
+/*
+17. 电话号码的字母组合
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+*/
+class Solution {
+public:
+	string chars[8] = {
+		"abc","def",
+		"ghi","jkl","mno",
+		"pqrs","tuv","wxyz"
+	};
+	vector<string> letterCombinations(string digits) {
+		if (digits.empty()) return vector<string>();
+
+		vector<string> state(1, "");
+		for (auto u : digits)
+		{
+			vector<string> now;
+			for (auto c : chars[u - '2'])
+			{
+				for (auto s : state)
+					now.push_back(s + c);
+			}
+			state = now;
+		}
+		return state;
+	}
+};
