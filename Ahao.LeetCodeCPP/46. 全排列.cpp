cf62145177ca97f17dbbb18634038f2fd91b3b9b
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-30 10:17:42 +0800

    46. 全排列

--- Ahao.LeetCodeCPP/46. 全排列.cpp
+++ Ahao.LeetCodeCPP/46. 全排列.cpp
@@ -36,3 +36,45 @@ public:
 		}
 	}
 };
+
+
+
+/*
+46. 全排列
+给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+*/
+class Solution {
+public:
+	int n;
+	vector<bool> st;
+	vector<vector<int>> ans;
+	vector<int> path;
+
+	vector<vector<int>> permute(vector<int>& nums) {
+		n = nums.size();
+		st = vector<bool>(n);
+		dfs(nums, 0);
+		return ans;
+	}
+
+	void dfs(vector<int>& nums, int u)
+	{
+		if (u == n)
+		{
+			ans.push_back(path);
+			return;
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			if (!st[i])
+			{
+				st[i] = true;
+				path.push_back(nums[i]);
+				dfs(nums, u + 1);
+				path.pop_back();
+				st[i] = false;
+			}
+		}
+	}
+};
