b88e1ae162170ff86bec60e14237d5bf7c191e1c
liupenghao@fulan.com.cn <haochen214@163.com>
2022-11-30 09:48:41 +0800

    47. 全排列 II

--- Ahao.LeetCodeCPP/47. 全排列 II.cpp
+++ Ahao.LeetCodeCPP/47. 全排列 II.cpp
@@ -3,12 +3,11 @@ using namespace std;
 #include <algorithm>
 
 
-
+/*
+47. 全排列 II
+给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+*/
 class Solution {
-	/*
-	47. 全排列 II
-	给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
-	*/
 	vector<vector<int>> ans;
 	vector<int> combine;
 	vector<bool> used;
