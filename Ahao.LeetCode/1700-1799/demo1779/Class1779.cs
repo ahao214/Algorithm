4d46d82b860d8b4ec1214fa8f99b2df9a682f665
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-01 09:38:48 +0800

    1779. 找到最近的有相同 X 或 Y 坐标的点

---
+++ Ahao.LeetCode/1700-1799/demo1779/Class1779.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1779
+{
+    public class Class1779
+    {
+        public int NearestValidPoint(int x, int y, int[][] points)
+        {
+            int n = points.Length;
+            int best = int.MaxValue, bestid = -1;
+            for (int i = 0; i < n; ++i)
+            {
+                int px = points[i][0], py = points[i][1];
+                if (x == px)
+                {
+                    int dist = Math.Abs(y - py);
+                    if (dist < best)
+                    {
+                        best = dist;
+                        bestid = i;
+                    }
+                }
+                else if (y == py)
+                {
+                    int dist = Math.Abs(x - px);
+                    if (dist < best)
+                    {
+                        best = dist;
+                        bestid = i;
+                    }
+                }
+            }
+            return bestid;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1700-1799/demo1779/JS1779.js
@@ -0,0 +1,21 @@
+﻿var nearestValidPoint = function (x, y, points) {
+    const n = points.length;
+    let best = Number.MAX_VALUE, bestid = -1;
+    for (let i = 0; i < n; ++i) {
+        const px = points[i][0], py = points[i][1];
+        if (x === px) {
+            const dist = Math.abs(y - py);
+            if (dist < best) {
+                best = dist;
+                bestid = i;
+            }
+        } else if (y === py) {
+            const dist = Math.abs(x - px);
+            if (dist < best) {
+                best = dist;
+                bestid = i;
+            }
+        }
+    }
+    return bestid;
+};
