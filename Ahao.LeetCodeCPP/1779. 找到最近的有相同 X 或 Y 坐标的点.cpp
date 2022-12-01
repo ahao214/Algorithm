dc16380ebc22a50ffe92599c1810d95fd290cb11
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-01 09:40:57 +0800

    1779. 找到最近的有相同 X 或 Y 坐标的点

--- Ahao.LeetCodeCPP/1779. 找到最近的有相同 X 或 Y 坐标的点.cpp
+++ Ahao.LeetCodeCPP/1779. 找到最近的有相同 X 或 Y 坐标的点.cpp
@@ -31,3 +31,28 @@ public:
 		return res;
 	}
 };
+
+
+class Solution {
+public:
+    int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+        int n = points.size();
+        int best = numeric_limits<int>::max(), bestid = -1;
+        for (int i = 0; i < n; ++i) {
+            int px = points[i][0], py = points[i][1];
+            if (x == px) {
+                if (int dist = abs(y - py); dist < best) {
+                    best = dist;
+                    bestid = i;
+                }
+            }
+            else if (y == py) {
+                if (int dist = abs(x - px); dist < best) {
+                    best = dist;
+                    bestid = i;
+                }
+            }
+        }
+        return bestid;
+    }
+};
