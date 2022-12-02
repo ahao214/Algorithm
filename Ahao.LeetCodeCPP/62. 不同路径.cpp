8fcd1f0533bd16fcae152581b57693f649b9f380
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-02 17:54:31 +0800

    62. 不同路径

--- Ahao.LeetCodeCPP/62. 不同路径.cpp
+++ Ahao.LeetCodeCPP/62. 不同路径.cpp
@@ -2,15 +2,16 @@ using namespace std;
 
 #include<vector>
 
+
+/*
+62. 不同路径
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+
+问总共有多少条不同的路径？
+*/
 class Solution {
-	/*
-	62. 不同路径
-	一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
-
-	机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
-
-	问总共有多少条不同的路径？
-	*/
 public:
 	/*
 	Times:O(m*n)
@@ -26,5 +27,36 @@ public:
 			}
 		}
 		return f[m - 1][n - 1];
+	}
+};
+
+
+
+
+/*
+62. 不同路径
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+
+问总共有多少条不同的路径？
+*/
+class Solution {
+public:
+	int uniquePaths(int m, int n) {
+		vector<vector<int>> f(n, vector<int>(m, 0));
+		f[0][0] = 1;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				if (i || j)
+				{
+					if (i) f[i][j] += f[i - 1][j];
+					if (j) f[i][j] += f[i][j - 1];
+				}
+			}
+		}
+		return f[n - 1][m - 1];
 	}
 };
