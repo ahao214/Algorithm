92dedfec50c3e2cfe9270d05adb1b01685843bd3
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-02 09:30:04 +0800

    1769. 移动所有球到每个盒子所需的最小操作数

---
+++ Ahao.LeetCode/1700-1799/demo1769/Class1769.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1769
+{
+    public class Class1769
+    {
+
+        public int[] MinOperations(string boxes)
+        {
+            int n = boxes.Length;
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int sm = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (boxes[j] == '1')
+                    {
+                        sm += Math.Abs(j - i);
+                    }
+                }
+                res[i] = sm;
+            }
+            return res;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1700-1799/demo1769/JS1769.js
@@ -0,0 +1,14 @@
+﻿var minOperations = function(boxes) {
+    const n = boxes.length;
+    const res = [];
+    for (let i = 0; i < n; i++) {
+        let sm = 0;
+        for (let j = 0; j < n; j++) {
+            if (boxes[j] === '1') {
+                sm += Math.abs(j - i);
+            }
+        }
+        res.push(sm);
+    }
+    return res;
+};
