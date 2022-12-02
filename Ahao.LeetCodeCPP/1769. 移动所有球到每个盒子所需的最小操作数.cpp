131bcd9c70dd315e9116e92144c2fc1dee049929
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-02 09:33:37 +0800

    1769. 移动所有球到每个盒子所需的最小操作数

--- Ahao.LeetCodeCPP/1769. 移动所有球到每个盒子所需的最小操作数.cpp
+++ Ahao.LeetCodeCPP/1769. 移动所有球到每个盒子所需的最小操作数.cpp
@@ -47,3 +47,27 @@ public:
 };
 
 
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int left = boxes[0] - '0', right = 0, operations = 0;
+        int n = boxes.size();
+        for (int i = 1; i < n; i++) {
+            if (boxes[i] == '1') {
+                right++;
+                operations += i;
+            }
+        }
+        vector<int> res(n);
+        res[0] = operations;
+        for (int i = 1; i < n; i++) {
+            operations += left - right;
+            if (boxes[i] == '1') {
+                left++;
+                right--;
+            }
+            res[i] = operations;
+        }
+        return res;
+    }
+};
