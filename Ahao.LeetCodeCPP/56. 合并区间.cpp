ccdcc2bdd69582ab91ac2500e24d5bad369966cc
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-02 15:22:01 +0800

    56. 合并区间

--- Ahao.LeetCodeCPP/56. 合并区间.cpp
+++ Ahao.LeetCodeCPP/56. 合并区间.cpp
@@ -2,11 +2,12 @@ using namespace std;
 #include<vector>
 #include<algorithm>
 
+
+/*
+56. 合并区间
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+*/
 class Solution {
-	/*
-	56. 合并区间
-	以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
-	*/
 public:
 	vector<vector<int>> merge(vector<vector<int>>& intervals) {
 		vector<vector<int>> ans;
@@ -27,5 +28,36 @@ public:
 		}
 		ans.push_back({ start,end });
 		return ans;
+	}
+};
+
+
+
+
+
+
+
+/*
+56. 合并区间
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+*/
+class Solution {
+public:
+	vector<vector<int>> merge(vector<vector<int>>& intervals) {
+		if (intervals.size() == 0) {
+			return {};
+		}
+		sort(intervals.begin(), intervals.end());
+		vector<vector<int>> merged;
+		for (int i = 0; i < intervals.size(); ++i) {
+			int L = intervals[i][0], R = intervals[i][1];
+			if (!merged.size() || merged.back()[1] < L) {
+				merged.push_back({ L, R });
+			}
+			else {
+				merged.back()[1] = max(merged.back()[1], R);
+			}
+		}
+		return merged;
 	}
 };
