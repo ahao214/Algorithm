d93d3c0787f78d79ef30a14759c6a56943094989
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-03 12:01:46 +0800

    829. 连续整数求和

--- Ahao.LeetCodeCPP/829. 连续整数求和.cpp
+++ Ahao.LeetCodeCPP/829. 连续整数求和.cpp
@@ -24,3 +24,23 @@ public:
 		}
 	}
 };
+
+
+
+
+/*
+829. 连续整数求和
+给定一个正整数 n，返回 连续正整数满足所有数字之和为 n 的组数 。
+*/
+class Solution {
+public:
+	int consecutiveNumbersSum(int n) {
+		int res = 0;
+		for (int b = 1; b * (b - 1) < n * 2; b++)
+		{
+			if (2 * n % b == 0 && (2 * n / b - b + 1) % 2 == 0)
+				res++;
+		}
+		return res;
+	}
+};
