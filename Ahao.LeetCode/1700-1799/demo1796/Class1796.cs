d7f8bac417d9f798938270992d2f2d4130aeb6dc
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-03 10:14:04 +0800

    1796. 字符串中第二大的数字

--- Ahao.LeetCode/1700-1799/demo1796/Class1796.cs
+++ Ahao.LeetCode/1700-1799/demo1796/Class1796.cs
@@ -9,5 +9,26 @@ namespace Ahao.LeetCode._1700_1799.demo1796
     public class Class1796
     {
 
+        public int SecondHighest(string s)
+        {
+            int first = -1, second = -1;
+            foreach (char c in s)
+            {
+                if (char.IsDigit(c))
+                {
+                    int num = c - '0';
+                    if (num > first)
+                    {
+                        second = first;
+                        first = num;
+                    }
+                    else if (num < first && num > second)
+                    {
+                        second = num;
+                    }
+                }
+            }
+            return second;
+        }
     }
 }
