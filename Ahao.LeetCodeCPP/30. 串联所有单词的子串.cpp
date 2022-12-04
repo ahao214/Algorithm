3904bb1e97626a73d4a897b1a1f799bda12a4400
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-04 21:13:01 +0800

    30. 串联所有单词的子串

--- Ahao.LeetCodeCPP/30. 串联所有单词的子串.cpp
+++ Ahao.LeetCodeCPP/30. 串联所有单词的子串.cpp
@@ -3,15 +3,16 @@ using namespace std;
 #include<string>
 #include<unordered_map>
 
-class Solution {
-	/*
-	30. 串联所有单词的子串
-	给定一个字符串 s 和一些 长度相同 的单词 words 。
-	找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
 
-	注意子串要与 words 中的单词完全匹配，中间不能有其他字符 ，
-	但不需要考虑 words 中单词串联的顺序。
-	*/
+/*
+30. 串联所有单词的子串
+给定一个字符串 s 和一些 长度相同 的单词 words 。
+找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
+
+注意子串要与 words 中的单词完全匹配，中间不能有其他字符 ，
+但不需要考虑 words 中单词串联的顺序。
+*/
+class Solution {
 public:
 	vector<int> findSubstring(string s, vector<string>& words) {
 		vector<int> ans;
@@ -43,5 +44,57 @@ public:
 			}
 		}
 		return ans;
+	}
+};
+
+
+
+
+
+/*
+30. 串联所有单词的子串
+给定一个字符串 s 和一些 长度相同 的单词 words 。找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
+
+注意子串要与 words 中的单词完全匹配，中间不能有其他字符 ，但不需要考虑 words 中单词串联的顺序。
+*/
+class Solution {
+public:
+	vector<int> findSubstring(string s, vector<string>& words) {
+		vector<int> res;
+		int n = s.size(), m = words.size(), w = words[0].size();
+		unordered_map<string, int> hashmap;
+		for (int i = 0; i < m; i++) hashmap[words[i]]++;
+		for (int i = 0; i < w; i++)
+		{
+			unordered_map<string, int> tdict;
+			int sum = 0;
+			int l = i;
+			for (int j = l; j + w <= n; j += w)
+			{
+				string word;
+				if (j - m * w >= l)
+				{
+					word = s.substr(j - m * w, w);
+					if (tdict[word] == hashmap[word]) sum -= hashmap[word];
+					tdict[word]--;
+					if (tdict[word] == hashmap[word]) sum += hashmap[word];
+				}
+				word = s.substr(j, w);
+				if (hashmap.count(word) == 0)
+				{
+					sum = 0;
+					tdict.clear();
+					l = j + w;
+				}
+				else
+				{
+					if (tdict[word] == hashmap[word]) sum -= hashmap[word];
+					tdict[word]++;
+					if (tdict[word] == hashmap[word]) sum += hashmap[word];
+				}
+				if (sum == m) res.push_back(j - (m - 1) * w);
+			}
+		}
+		return res;
 	}
 };
