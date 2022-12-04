efff3b89c2abd744f7932a45081e615e05b66e95
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-04 12:58:55 +0800

    1774. 最接近目标价格的甜点成本

--- Ahao.LeetCodeCPP/1774. 最接近目标价格的甜点成本.cpp
+++ Ahao.LeetCodeCPP/1774. 最接近目标价格的甜点成本.cpp
@@ -1,4 +1,5 @@
 #include<vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -48,6 +49,39 @@ public:
 					res = r;
 				}
 			}
+		}
+		return res;
+	}
+};
+
+
+
+class Solution {
+public:
+	void dfs(const vector<int>& toppingCosts, int p, int curCost, int& res, const int& target) {
+		if (abs(res - target) < curCost - target) {
+			return;
+		}
+		else if (abs(res - target) >= abs(curCost - target)) {
+			if (abs(res - target) > abs(curCost - target)) {
+				res = curCost;
+			}
+			else {
+				res = min(res, curCost);
+			}
+		}
+		if (p == toppingCosts.size()) {
+			return;
+		}
+		dfs(toppingCosts, p + 1, curCost + toppingCosts[p] * 2, res, target);
+		dfs(toppingCosts, p + 1, curCost + toppingCosts[p], res, target);
+		dfs(toppingCosts, p + 1, curCost, res, target);
+	}
+
+	int closestCost(vector<int>& baseCosts, vector<int>& toppingCosts, int target) {
+		int res = *min_element(baseCosts.begin(), baseCosts.end());
+		for (auto& b : baseCosts) {
+			dfs(toppingCosts, 0, b, res, target);
 		}
 		return res;
 	}
