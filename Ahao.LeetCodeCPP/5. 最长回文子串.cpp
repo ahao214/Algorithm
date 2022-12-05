954377dce1a8f43006c2e4846807323f7e5add31
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-05 15:22:12 +0800

    5. 最长回文子串

--- Ahao.LeetCodeCPP/5. 最长回文子串.cpp
+++ Ahao.LeetCodeCPP/5. 最长回文子串.cpp
@@ -28,3 +28,36 @@ public:
 		return res;
 	}
 };
+
+
+
+/*
+5. 最长回文子串
+给你一个字符串 s，找到 s 中最长的回文子串。
+*/
+class Solution {
+public:
+	string longestPalindrome(string s) {
+		int len = 0;
+		string ans;
+		for (int k = 0; k < s.size(); k++)
+		{
+			int i = k, j = k + 1;
+			while (i >= 0 && j < s.size() && s[i] == s[j]) i--, j++;
+			if (j - i - 1 > len)
+			{
+				len = j - i - 1;
+				ans = s.substr(i + 1, len);
+			}
+
+			i = k - 1, j = k + 1;
+			while (i >= 0 && j < s.size() && s[i] == s[j]) i--, j++;
+			if (j - i - 1 > len)
+			{
+				len = j - i - 1;
+				ans = s.substr(i + 1, len);
+			}
+		}
+		return ans;
+	}
+};
