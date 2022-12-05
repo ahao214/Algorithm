44d0259d4f5deea9cc7fe6c38a644602c75707e8
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-05 11:22:54 +0800

    227. 基本计算器 II

---
+++ Ahao.LeetCodeCPP/227. 基本计算器 II.cpp
@@ -0,0 +1,45 @@
+#include<vector>
+#include <numeric>
+#include <ctype.h>
+#include <string>
+
+using namespace std;
+
+
+/*
+227. 基本计算器 II
+给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+整数除法仅保留整数部分。
+*/
+class Solution {
+public:
+    int calculate(string s) {
+        vector<int> stk;
+        char preSign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (isdigit(s[i])) {
+                num = num * 10 + int(s[i] - '0');
+            }
+            if (!isdigit(s[i]) && s[i] != ' ' || i == n - 1) {
+                switch (preSign) {
+                case '+':
+                    stk.push_back(num);
+                    break;
+                case '-':
+                    stk.push_back(-num);
+                    break;
+                case '*':
+                    stk.back() *= num;
+                    break;
+                default:
+                    stk.back() /= num;
+                }
+                preSign = s[i];
+                num = 0;
+            }
+        }
+        return accumulate(stk.begin(), stk.end(), 0);
+    }
+};
