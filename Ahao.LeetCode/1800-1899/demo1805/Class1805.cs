b5ea6337428ed3ae1aff1bd87f1ff351fb55da9a
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-06 09:39:07 +0800

    1805. 字符串中不同整数的数目

---
+++ Ahao.LeetCode/1800-1899/demo1805/Class1805.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1805
+{
+    public class Class1805
+    {
+
+    }
+}
---
+++ Ahao.LeetCode/1800-1899/demo1805/JS1805.js
@@ -0,0 +1,26 @@
+﻿var numDifferentIntegers = function (word) {
+    const set = new Set();
+    let n = word.length, p1 = 0, p2;
+    while (true) {
+        while (p1 < n && !isDigit(word[p1])) {
+            p1++;
+        }
+        if (p1 === n) {
+            break;
+        }
+        p2 = p1;
+        while (p2 < n && isDigit(word[p2])) {
+            p2++;
+        }
+        while (p2 - p1 > 1 && word[p1] === '0') {
+            p1++;
+        }
+        set.add(word.slice(p1, p2));
+        p1 = p2;
+    }
+    return set.size;
+};
+
+const isDigit = (ch) => {
+    return '0' <= ch && ch <= '9';
+}
