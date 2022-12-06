42fd65fe89c914404ac94c13633c88ca5e4e0f30
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-06 11:11:30 +0800

    49. 字母异位词分组

--- Ahao.LeetCodeCPP/49. 字母异位词分组.cpp
+++ Ahao.LeetCodeCPP/49. 字母异位词分组.cpp
@@ -5,13 +5,13 @@ using namespace std;
 #include <unordered_map>
 #include <queue>
 
-class Solution {
-	/*
-	49. 字母异位词分组
-	给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+/*
+49. 字母异位词分组
+给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
 
-	字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
-	*/
+字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+*/
+class Solution {
 public:
 	vector<vector<string>> groupAnagrams(vector<string>& strs) {
 		unordered_map<string, vector<string>> record;
@@ -27,5 +27,33 @@ public:
 			ans.push_back(it->second);
 		}
 		return ans;
+	}
+};
+
+
+
+/*
+49. 字母异位词分组
+给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+
+字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+*/
+class Solution {
+public:
+	vector<vector<string>> groupAnagrams(vector<string>& strs) {
+		unordered_map<string, vector<string>> dict;
+		for (auto& str : strs)
+		{
+			string key = str;
+			sort(key.begin(), key.end());
+			dict[key].push_back(str);
+		}
+
+		vector<vector<string>> res;
+		for (auto i = dict.begin(); i != dict.end(); i++)
+		{
+			res.push_back(i->second);
+		}
+		return res;
 	}
 };
