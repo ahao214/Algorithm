186d70e2af2bdb9da704deaf28e6491e19311683
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-06 13:57:34 +0800

    38. 外观数列

--- Ahao.LeetCodeCPP/38. 外观数列.cpp
+++ Ahao.LeetCodeCPP/38. 外观数列.cpp
@@ -20,3 +20,37 @@ public:
 	}
 };
 
+
+/*
+38. 外观数列
+给定一个正整数 n ，输出外观数列的第 n 项。
+
+「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+
+你可以将其视作是由递归公式定义的数字字符串序列：
+
+countAndSay(1) = "1"
+countAndSay(n) 是对 countAndSay(n-1) 的描述，然后转换成另一个数字字符串。
+*/
+class Solution {
+public:
+	string countAndSay(int n) {
+		string s = "1";
+		for (int i = 0; i < n - 1; i++)
+		{
+			string ns;
+			//遍历连续的
+			for (int j = 0; j < s.size(); j++)
+			{
+				int k = j;
+				while (k < s.size() && s[k] == s[j]) k++;
+				ns += to_string(k - j) + s[j];
+				j = k - 1;
+			}
+			s = ns;
+		}
+		return s;
+	}
+};
+
+
