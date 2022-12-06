27152979ab24de28089a8261688624f59df8bea0
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-06 13:40:37 +0800

    38. 外观数列

--- Ahao.LeetCodeCPP/38. 外观数列.cpp
+++ Ahao.LeetCodeCPP/38. 外观数列.cpp
@@ -1,13 +1,12 @@
 using namespace std;
 #include<string>
 
-
+/*
+38. 外观数列
+给定一个正整数 n ，输出外观数列的第 n 项。
+「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+*/
 class Solution {
-	/*
-	38. 外观数列
-	给定一个正整数 n ，输出外观数列的第 n 项。
-	「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
-	*/
 public:
 	string countAndSay(int n) {
 		if (n == 1) return "1";
