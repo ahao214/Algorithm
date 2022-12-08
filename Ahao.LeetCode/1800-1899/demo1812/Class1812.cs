26c888925556d8505dcec9c48e426291a042c395
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-08 09:34:56 +0800

    1812. 判断国际象棋棋盘中一个格子的颜色

---
+++ Ahao.LeetCode/1800-1899/demo1812/Class1812.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1812
+{
+    public class Class1812
+    {
+
+    }
+}
---
+++ Ahao.LeetCode/1800-1899/demo1812/JS1812.js
@@ -0,0 +1,3 @@
+﻿var squareIsWhite = function (coordinates) {
+    return ((coordinates[0].charCodeAt() - 'a'.charCodeAt() + 1) + (coordinates[1].charCodeAt() - '0'.charCodeAt())) % 2 === 1;
+};
