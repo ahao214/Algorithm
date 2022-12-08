a54777a05b49f5bda792db3061da8a280daedf95
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-08 11:24:27 +0800

    98. 验证二叉搜索树

--- Ahao.LeetCodeCPP/98. 验证二叉搜索树.cpp
+++ Ahao.LeetCodeCPP/98. 验证二叉搜索树.cpp
@@ -53,3 +53,30 @@ public:
 		return true;
 	}
 };
+
+
+
+/*
+98. 验证二叉搜索树
+给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+
+有效 二叉搜索树定义如下：
+
+节点的左子树只包含 小于 当前节点的数。
+节点的右子树只包含 大于 当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+*/
+class Solution {
+public:
+	bool isValidBST(TreeNode* root) {
+		return dfs(root, INT_MIN, INT_MAX);
+	}
+
+	bool dfs(TreeNode* root, long long minv, long long maxv)
+	{
+		if (!root) return true;
+		if (root->val < minv || root->val > maxv) return false;
+
+		return dfs(root->left, minv, root->val) && dfs(root->right, root->val, maxv);
+	}
+};
