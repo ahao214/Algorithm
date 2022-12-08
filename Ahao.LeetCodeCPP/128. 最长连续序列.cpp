cb764c16394d7a17dbf4d1f43c76fcaab2ae6452
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-08 11:27:53 +0800

    128. 最长连续序列

---
+++ Ahao.LeetCodeCPP/128. 最长连续序列.cpp
@@ -0,0 +1,29 @@
+#include<vector>
+#include <unordered_map>
+
+using namespace std;
+
+
+
+/*
+128. 最长连续序列
+给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+
+请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+*/
+class Solution {
+public:
+	int longestConsecutive(vector<int>& nums) {
+		int res = 0;
+		unordered_map<int, int> tr_left, tr_right;
+		for (auto x : nums)
+		{
+			int left = tr_right[x - 1];
+			int right = tr_left[x + 1];
+			tr_left[x - left] = max(tr_left[x - left], left + 1 + right);
+			tr_right[x + right] = max(tr_right[x + right], left + 1 + right);
+			res = max(res, left + 1 + right);
+		}
+		return res;
+	}
+};
