94af0e41265bcc52d2d148092a794d6da5d56b69
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-08 09:35:16 +0800

    1812. 判断国际象棋棋盘中一个格子的颜色

--- Ahao.LeetCode/1800-1899/demo1812/Class1812.cs
+++ Ahao.LeetCode/1800-1899/demo1812/Class1812.cs
@@ -8,6 +8,9 @@ namespace Ahao.LeetCode._1800_1899.demo1812
 {
     public class Class1812
     {
-
+        public bool SquareIsWhite(string coordinates)
+        {
+            return ((coordinates[0] - 'a' + 1) + (coordinates[1] - '0')) % 2 == 1;
+        }
     }
 }
