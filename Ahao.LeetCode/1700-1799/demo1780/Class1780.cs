71b168678da27282fd758400f58b1a897c98ea4a
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-09 09:36:53 +0800

    1780. 判断一个数字是否可以表示成三的幂的和

---
+++ Ahao.LeetCode/1700-1799/demo1780/Class1780.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1780
+{
+    public class Class1780
+    {
+        public bool CheckPowersOfThree(int n)
+        {
+            while (n != 0)
+            {
+                if (n % 3 == 2)
+                {
+                    return false;
+                }
+                n /= 3;
+            }
+            return true;
+        }
+    }
+}
