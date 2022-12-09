cec1e0df236305208d9fe7c05059b653bfdca4ad
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-09 09:58:56 +0800

    652. 寻找重复的子树

--- Ahao.LeetCodeCPP/652. 寻找重复的子树.cpp
+++ Ahao.LeetCodeCPP/652. 寻找重复的子树.cpp
@@ -13,6 +13,17 @@ using namespace std;
 
 如果两棵树具有相同的结构和相同的结点值，则它们是重复的。
 */
+
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
 	unordered_map<string, int> hash;
