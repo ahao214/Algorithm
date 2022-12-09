174f35e6902d1909b3fb2e9b6db4df8cfea8bacb
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-09 10:38:04 +0800

    350. 两个数组的交集 II

---
+++ Ahao.LeetCodeCPP/350. 两个数组的交集 II.cpp
@@ -0,0 +1,30 @@
+#include<vector>
+#include <unordered_set>
+
+using namespace std;
+
+
+
+/*
+350. 两个数组的交集 II
+给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序。
+*/
+class Solution {
+public:
+	vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+		if (nums1.size() > nums2.size()) return intersect(nums2, nums1);
+		unordered_multiset<int> hash;
+		for (auto x : nums1) hash.insert(x);
+		vector<int> res;
+		for (auto x : nums2)
+		{
+			if (hash.count(x))
+			{
+				res.push_back(x);
+				auto it = hash.find(x);
+				hash.erase(it);
+			}
+		}
+		return res;
+	}
+};
