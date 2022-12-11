5f09118485e7061d2c410ebfc32d4902adcd779f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-11 09:03:46 +0800

    1827. 最少操作使数组递增

--- Ahao.LeetCodeCPP/1827. 最少操作使数组递增.cpp
+++ Ahao.LeetCodeCPP/1827. 最少操作使数组递增.cpp
@@ -25,3 +25,16 @@ public:
 		return res;
 	}
 };
+
+
+class Solution {
+public:
+	int minOperations(vector<int>& nums) {
+		int pre = nums[0] - 1, res = 0;
+		for (int num : nums) {
+			pre = max(pre + 1, num);
+			res += pre - num;
+		}
+		return res;
+	}
+};
