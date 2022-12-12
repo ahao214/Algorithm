746b16022a626f0505e1c6c077e97b1a364aef32
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-12 09:44:27 +0800

    1781. 所有子字符串美丽值之和

---
+++ Ahao.LeetCode/1700-1799/demo1781/JS1781.js
@@ -0,0 +1,19 @@
+﻿var beautySum = function (s) {
+    let res = 0;
+    for (let i = 0; i < s.length; i++) {
+        const cnt = new Array(26).fill(0);
+        let maxFreq = 0;
+        for (let j = i; j < s.length; j++) {
+            cnt[s[j].charCodeAt() - 'a'.charCodeAt()]++;
+            maxFreq = Math.max(maxFreq, cnt[s[j].charCodeAt() - 'a'.charCodeAt()]);
+            let minFreq = s.length;
+            for (let k = 0; k < 26; k++) {
+                if (cnt[k] > 0) {
+                    minFreq = Math.min(minFreq, cnt[k]);
+                }
+            }
+            res += maxFreq - minFreq;
+        }
+    }
+    return res;
+};
