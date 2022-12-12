edb63740aa37569aa6de8d60b69b8b10d14c3ba8
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-12 14:45:35 +0800

    24. 两两交换链表中的节点

--- Ahao.LeetCodeCPP/24. 两两交换链表中的节点.cpp
+++ Ahao.LeetCodeCPP/24. 两两交换链表中的节点.cpp
@@ -29,3 +29,28 @@ struct ListNode {
 	ListNode(int x) : val(x), next(nullptr) {}
 	ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
+
+
+
+/*
+24. 两两交换链表中的节点
+给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。
+你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+*/
+class Solution {
+public:
+	ListNode* swapPairs(ListNode* head) {
+		auto dummy = new ListNode(-1);
+		dummy->next = head;
+
+		for (auto p = dummy; p->next && p->next->next;)
+		{
+			auto a = p->next, b = a->next;
+			p->next = b;
+			a->next = b->next;
+			b->next = a;
+			p = a;
+		}
+		return dummy->next;
+	}
+};
