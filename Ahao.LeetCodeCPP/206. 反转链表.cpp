f1ac67d1d78106c94bc9fae6a7c29e4983f23c19
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-12 10:23:40 +0800

    206. 反转链表

---
+++ Ahao.LeetCodeCPP/206. 反转链表.cpp
@@ -0,0 +1,35 @@
+#include<vector>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+
+/*
+206. 反转链表
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+*/
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		if (!head) return NULL;
+
+		auto a = head, b = a->next;
+
+		while (b)
+		{
+			//c临时保存b的下一个节点
+			auto c = b->next;
+			b->next = a;
+			a = b, b = c;
+		}
+		head->next = NULL;
+		return a;
+	}
+};
