dc2e1e292a169bb12311f563985f60ef9debd152
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-12 09:43:33 +0800

    1781. 所有子字符串美丽值之和

--- Ahao.LeetCodeCPP/1781. 所有子字符串美丽值之和.cpp
+++ Ahao.LeetCodeCPP/1781. 所有子字符串美丽值之和.cpp
@@ -1,4 +1,5 @@
 #include<string>
+#include <vector>
 
 using namespace std;
 
@@ -32,6 +33,31 @@ public:
 					}
 				}
 				res += maxv - minv;
+			}
+		}
+		return res;
+	}
+};
+
+
+
+class Solution {
+public:
+	int beautySum(string s) {
+		int res = 0;
+		for (int i = 0; i < s.size(); i++) {
+			vector<int> cnt(26);
+			int maxFreq = 0;
+			for (int j = i; j < s.size(); j++) {
+				cnt[s[j] - 'a']++;
+				maxFreq = max(maxFreq, cnt[s[j] - 'a']);
+				int minFreq = s.size();
+				for (int k = 0; k < 26; k++) {
+					if (cnt[k] > 0) {
+						minFreq = min(minFreq, cnt[k]);
+					}
+				}
+				res += maxFreq - minFreq;
 			}
 		}
 		return res;
