0ede8bab232a959fe3022bdbb7ee609e60f9a2bc
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-13 11:46:12 +0800

    83. 删除排序链表中的重复元素

--- Ahao.LeetCodeCPP/83. 删除排序链表中的重复元素.cpp
+++ Ahao.LeetCodeCPP/83. 删除排序链表中的重复元素.cpp
@@ -39,3 +39,26 @@ public:
 		return head;
 	}
 };
+
+
+
+
+/*
+83. 删除排序链表中的重复元素
+给定一个已排序的链表的头 head ，
+删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+*/
+class Solution {
+public:
+	ListNode* deleteDuplicates(ListNode* head) {
+		auto cur = head;
+		while (cur)
+		{
+			if (cur->next && cur->next->val == cur->val)
+				cur->next = cur->next->next;
+			else
+				cur = cur->next;
+		}
+		return head;
+	}
+};
