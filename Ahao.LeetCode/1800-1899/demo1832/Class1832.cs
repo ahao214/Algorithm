1d751a027213f7dc45e36fec94aa997633d874ce
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-13 09:42:39 +0800

    1832. 判断句子是否为全字母句

---
+++ Ahao.LeetCode/1800-1899/demo1832/Class1832.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1832
+{
+    public class Class1832
+    {
+        public bool CheckIfPangram(string sentence)
+        {
+            bool[] exist = new bool[26];
+            foreach (char c in sentence)
+            {
+                exist[c - 'a'] = true;
+            }
+            foreach (bool x in exist)
+            {
+                if (!x)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
