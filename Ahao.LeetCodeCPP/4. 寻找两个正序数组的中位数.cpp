887512c7a89ea2c61ea50d18f32ba12de4e4c0ad
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-14 13:30:04 +0800

    4. 寻找两个正序数组的中位数

--- Ahao.LeetCodeCPP/4. 寻找两个正序数组的中位数.cpp
+++ Ahao.LeetCodeCPP/4. 寻找两个正序数组的中位数.cpp
@@ -44,3 +44,37 @@ public:
 		}
 	}
 };
+
+
+
+/*
+4. 寻找两个正序数组的中位数
+给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数 。
+
+算法的时间复杂度应该为 O(log (m+n)) 。
+*/
+class Solution {
+public:
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+		int N1 = nums1.size();
+		int N2 = nums2.size();
+		if (N1 < N2) return findMedianSortedArrays(nums2, nums1);
+
+		int low = 0, high = N2 * 2;
+		while (low <= high)
+		{
+			int mid2 = (low + high) / 2;
+			int mid1 = N1 + N2 - mid2;
+
+			double L1 = (mid1 == 0) ? INT_MIN : nums1[(mid1 - 1) / 2];
+			double L2 = (mid2 == 0) ? INT_MIN : nums2[(mid2 - 1) / 2];
+			double R1 = (mid1 == N1 * 2) ? INT_MAX : nums1[(mid1) / 2];
+			double R2 = (mid2 == N2 * 2) ? INT_MAX : nums2[(mid2) / 2];
+
+			if (L1 > R2) low = mid2 + 1;
+			else if (L2 > R1) high = mid2 - 1;
+			else return (max(L1, L2) + min(R1, R2)) / 2;
+		}
+		return -1;
+	}
+};
