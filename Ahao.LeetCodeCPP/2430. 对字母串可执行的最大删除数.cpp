32133f75e6aa41c032632623c906e333e680fbf8
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-14 11:44:36 +0800

    275. H 指数 II

---
+++ Ahao.LeetCodeCPP/2430. 对字母串可执行的最大删除数.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+#include <string>
+
+using namespace std;
+
+
+
+/*
+2430. 对字母串可执行的最大删除数
+给你一个仅由小写英文字母组成的字符串 s 。在一步操作中，你可以：
+
+删除 整个字符串 s ，或者
+对于满足 1 <= i <= s.length / 2 的任意 i ，如果 s 中的 前 i 个字母和接下来的 i 个字母 相等 ，删除 前 i 个字母。
+例如，如果 s = "ababc" ，那么在一步操作中，你可以删除 s 的前两个字母得到 "abc" ，因为 s 的前两个字母和接下来的两个字母都等于 "ab" 。
+
+返回删除 s 所需的最大操作数。
+*/
+class Solution {
+public:
+    int deleteString(string s) {
+
+    }
+};
