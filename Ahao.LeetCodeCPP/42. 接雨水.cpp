f7cb0919f2bf4542ee4a0bd3719e85bdc10288cc
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-15 16:13:48 +0800

    42. 接雨水

--- Ahao.LeetCodeCPP/42. 接雨水.cpp
+++ Ahao.LeetCodeCPP/42. 接雨水.cpp
@@ -2,13 +2,38 @@ using namespace std;
 #include<vector>
 
 
-
+/*
+42. 接雨水
+给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，
+计算按此排列的柱子，下雨之后能接多少雨水。
+*/
 class Solution {
-	/*
-	42. 接雨水
-	给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，
-	计算按此排列的柱子，下雨之后能接多少雨水。
-	*/
+public:
+	int trap(vector<int>& height) {
+		int l = 0, r = height.size() - 1;
+		int lmax = 0, rmax = 0, ans = 0;
+		while (l < r) {
+			lmax = max(lmax, height[l]);
+			rmax = max(rmax, height[r]);
+			if (lmax < rmax) {
+				ans += lmax - height[l];
+				l++;
+			}
+			else {
+				ans += rmax - height[r];
+				r--;
+			}
+		}
+		return ans;
+	}
+};
+
+
+/*
+42. 接雨水
+给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+*/
+class Solution {
 public:
 	int trap(vector<int>& height) {
 		int l = 0, r = height.size() - 1;
