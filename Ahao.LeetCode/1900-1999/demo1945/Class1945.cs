f6dcce8eb2b2e3bf6f26312e944a537854f29dd5
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-15 09:37:47 +0800

    1945. 字符串转化后的各位数字之和

---
+++ Ahao.LeetCode/1900-1999/demo1945/Class1945.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1900_1999.demo1945
+{
+    public class Class1945
+    {
+        public int GetLucky(string s, int k)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char ch in s)
+            {
+                sb.Append(ch - 'a' + 1);
+            }
+            string digits = sb.ToString();
+            for (int i = 1; i <= k && digits.Length > 1; ++i)
+            {
+                int sum = 0;
+                foreach (char ch in digits)
+                {
+                    sum += ch - '0';
+                }
+                digits = sum.ToString();
+            }
+
+            return int.Parse(digits);
+        }
+    }
+}
