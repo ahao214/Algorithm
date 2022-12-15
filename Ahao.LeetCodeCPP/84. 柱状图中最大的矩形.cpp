3881917a19f07b3ab0d693121448d8c30522fa58
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-15 16:13:13 +0800

    84. 柱状图中最大的矩形

--- Ahao.LeetCodeCPP/84. 柱状图中最大的矩形.cpp
+++ Ahao.LeetCodeCPP/84. 柱状图中最大的矩形.cpp
@@ -83,3 +83,38 @@ public:
 		return res;
 	}
 };
+
+
+
+/*
+84. 柱状图中最大的矩形
+给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+
+求在该柱状图中，能够勾勒出来的矩形的最大面积。
+*/
+class Solution {
+public:
+	int largestRectangleArea(vector<int>& heights) {
+		int n = heights.size(), ans = 0;
+		heights.push_back(-1);
+		stack<int> stk;
+		for (int i = 0; i <= n; i++)
+		{
+			while (!stk.empty() && heights[i] < heights[stk.top()])
+			{
+				int cur = stk.top();
+				stk.pop();
+				if (stk.empty())
+				{
+					ans = max(ans, heights[cur] * i);
+				}
+				else
+				{
+					ans = max(ans, heights[cur] * (i - stk.top() - 1));
+				}
+			}
+			stk.push(i);
+		}
+		return ans;
+	}
+};
