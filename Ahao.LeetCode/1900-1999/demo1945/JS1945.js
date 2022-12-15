0f471d2510a68a25fb507ddab0ea18ed2c5fc946
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-15 09:38:16 +0800

    1945. 字符串转化后的各位数字之和

---
+++ Ahao.LeetCode/1900-1999/demo1945/JS1945.js
@@ -0,0 +1,18 @@
+﻿var getLucky = function (s, k) {
+    let sb = '';
+    for (let i = 0; i < s.length; ++i) {
+        const ch = s[i];
+        sb += '' + ch.charCodeAt() - 'a'.charCodeAt() + 1;
+    }
+    let digits = sb.toString();
+    for (let i = 1; i <= k && digits.length > 1; ++i) {
+        let sum = 0;
+        for (let j = 0; j < digits.length; ++j) {
+            const ch = digits[j];
+            sum += ch.charCodeAt() - '0'.charCodeAt();
+        }
+        digits = '' + sum;
+    }
+
+    return 0 + digits;
+};
