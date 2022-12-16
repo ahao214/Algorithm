e3d24dac5fc33d0e7f8c526e04962e10cd6a17a8
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-16 09:43:02 +0800

    1785. 构成特定和需要添加的最少元素

--- Ahao.LeetCodeCPP/1785. 构成特定和需要添加的最少元素.cpp
+++ Ahao.LeetCodeCPP/1785. 构成特定和需要添加的最少元素.cpp
@@ -20,3 +20,16 @@ public:
 		return (abs(sum - goal) + limit - 1) / limit;
 	}
 };
+
+
+class Solution {
+public:
+	int minElements(vector<int>& nums, int limit, int goal) {
+		long long sum = 0;
+		for (auto x : nums) {
+			sum += x;
+		}
+		long long diff = abs(sum - goal);
+		return (diff + limit - 1) / limit;
+	}
+};
