5d3e26f52060e5b4ece0faa2d4bad5af8521980b
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-16 09:44:40 +0800

    1785. 构成特定和需要添加的最少元素

---
+++ Ahao.LeetCode/1700-1799/demo1785/JS1785.js
@@ -0,0 +1,5 @@
+﻿var minElements = function (nums, limit, goal) {
+    const sum = _.sum(nums);
+    const diff = Math.abs(sum - goal);
+    return Math.abs(Math.floor((diff + limit - 1) / limit));
+};
