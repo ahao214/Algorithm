9775037ae4a8d27ec892eb07b872147147e8e22b
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-16 10:24:19 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置

--- Ahao.LeetCodeCPP/34. 在排序数组中查找元素的第一个和最后一个位置.cpp
+++ Ahao.LeetCodeCPP/34. 在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -1,13 +1,14 @@
 using namespace std;
 #include<vector>
 
-class Solution {
-	/*
-	34. 在排序数组中查找元素的第一个和最后一个位置
-	给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
 
-	如果数组中不存在目标值 target，返回 [-1, -1]。
-	*/
+/*
+34. 在排序数组中查找元素的第一个和最后一个位置
+给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+如果数组中不存在目标值 target，返回 [-1, -1]。
+*/
+class Solution {
 public:
 	vector<int> searchRange(vector<int>& nums, int target) {
 		if (nums.empty()) return { -1,-1 };
