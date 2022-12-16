3fbb455903eace1851fd58ed8c935cd93ef9c618
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-16 09:44:12 +0800

    1785. 构成特定和需要添加的最少元素

---
+++ Ahao.LeetCode/1700-1799/demo1785/Class1785.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1785
+{
+    public class Class1785
+    {
+        public int MinElements(int[] nums, int limit, int goal)
+        {
+            long sum = 0;
+            foreach (int x in nums)
+            {
+                sum += x;
+            }
+            long diff = Math.Abs(sum - goal);
+            return (int)((diff + limit - 1) / limit);
+        }
+
+    }
+}
