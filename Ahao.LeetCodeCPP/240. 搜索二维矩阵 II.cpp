028e456f66a1d3084d01a3eb8aff7fc00e270a36
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-16 10:10:05 +0800

    240. 搜索二维矩阵 II

--- Ahao.LeetCodeCPP/240. 搜索二维矩阵 II.cpp
+++ Ahao.LeetCodeCPP/240. 搜索二维矩阵 II.cpp
@@ -27,3 +27,26 @@ public:
 		return false;
 	}
 };
+
+
+/*
+240. 搜索二维矩阵 II
+编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+每行的元素从左到右升序排列。
+每列的元素从上到下升序排列。
+*/
+class Solution {
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+		if (matrix.empty() || matrix[0].empty()) return false;
+		int i = 0, j = matrix[0].size() - 1;
+		while (i < matrix.size() && j >= 0)
+		{
+			int t = matrix[i][j];
+			if (t == target) return true;
+			if (t > target) j--;
+			else i++;
+		}
+		return false;
+	}
+};
