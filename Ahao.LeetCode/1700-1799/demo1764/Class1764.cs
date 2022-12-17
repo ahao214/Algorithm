05b418e0cddda9bea767074a4bd50b3ac38bbfb1
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-17 09:17:26 +0800

    1764. 通过连接另一个数组的子数组得到一个数组

---
+++ Ahao.LeetCode/1700-1799/demo1764/Class1764.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1764
+{
+    public class Class1764
+    {
+        public bool CanChoose(int[][] groups, int[] nums)
+        {
+            int i = 0;
+            for (int k = 0; k < nums.Length && i < groups.Length;)
+            {
+                if (Check(groups[i], nums, k))
+                {
+                    k += groups[i].Length;
+                    i++;
+                }
+                else
+                {
+                    k++;
+                }
+            }
+            return i == groups.Length;
+        }
+
+        public bool Check(int[] g, int[] nums, int k)
+        {
+            if (k + g.Length > nums.Length)
+            {
+                return false;
+            }
+            for (int j = 0; j < g.Length; j++)
+            {
+                if (g[j] != nums[k + j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
