89f0507a4d6703e5c0f437a62bb583cce4b79a0f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-19 10:58:26 +0800

    338. 比特位计数

--- Ahao.LeetCodeCPP/338. 比特位计数.cpp
+++ Ahao.LeetCodeCPP/338. 比特位计数.cpp
@@ -28,3 +28,22 @@ public:
 		return res;
 	}
 };
+
+
+
+/*
+338. 比特位计数
+给你一个整数 n ，对于 0 <= i <= n 中的每个 i ，计算其二进制表示中 1 的个数 ，返回一个长度为 n + 1 的数组 ans 作为答案。
+*/
+class Solution {
+public:
+	vector<int> countBits(int n) {
+		vector<int> dp(n + 1);
+		dp[0] = 0;
+		for (int i = 1; i <= n; i++)
+		{
+			dp[i] = dp[i >> 1] + (i & 1);
+		}
+		return dp;
+	}
+};
