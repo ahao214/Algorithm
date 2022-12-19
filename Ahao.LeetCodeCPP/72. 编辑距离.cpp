e39b8ccd0a27563ad715dee8e8ac61cf6a6579bf
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-19 13:41:27 +0800

    72. 编辑距离

--- Ahao.LeetCodeCPP/72. 编辑距离.cpp
+++ Ahao.LeetCodeCPP/72. 编辑距离.cpp
@@ -47,3 +47,33 @@ public:
 	}
 };
 
+
+/*
+72. 编辑距离
+给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数  。
+
+你可以对一个单词进行如下三种操作：
+
+插入一个字符
+删除一个字符
+替换一个字符
+*/
+class Solution {
+public:
+	int minDistance(string word1, string word2) {
+		int n = word1.size(), m = word2.size();
+		vector<vector<int>> f(n + 1, vector<int>(m + 1));
+		for (int i = 0; i <= n; i++) f[i][0] = i;
+		for (int i = 0; i <= m; i++) f[0][i] = i;
+
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= m; j++)
+			{
+				f[i][j] = min(f[i - 1][j], f[i][j - 1]) + 1;
+				f[i][j] = min(f[i][j], f[i - 1][j - 1] + (word1[i - 1] != word2[j - 1]));
+			}
+		}
+		return f[n][m];
+	}
+};
