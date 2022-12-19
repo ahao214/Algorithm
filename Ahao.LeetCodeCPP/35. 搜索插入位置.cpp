f5ede4332b49f58881389a0e67fcbf7a77e6f51c
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-19 09:55:00 +0800

    35. 搜索插入位置

--- Ahao.LeetCodeCPP/35. 搜索插入位置.cpp
+++ Ahao.LeetCodeCPP/35. 搜索插入位置.cpp
@@ -24,3 +24,33 @@ public:
 		return r;
 	}
 };
+
+
+
+/*
+35. 搜索插入位置
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+请必须使用时间复杂度为 O(log n) 的算法。
+*/
+class Solution {
+public:
+	int searchInsert(vector<int>& nums, int target) {
+		if (nums.empty() || nums.back() < target) return nums.size();
+		int left = 0, right = nums.size() - 1;
+		while (left < right)
+		{
+			int mid = left + right >> 1;
+			if (nums[mid] >= target)
+			{
+				right = mid;
+			}
+			else
+			{
+				left = mid + 1;
+			}
+		}
+		return right;
+	}
+};
