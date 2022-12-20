da1e87374c6b68f62e6d295832fa943e71686cfb
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-20 09:38:47 +0800

    1760. 袋子里最少数目的球

---
+++ Ahao.LeetCode/1700-1799/demo1760/Class1760.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1760
+{
+    public class Class1760
+    {
+        public int MinimumSize(int[] nums, int maxOperations)
+        {
+            int left = 1, right = nums.Max();
+            int ans = 0;
+            while (left <= right)
+            {
+                int y = (left + right) / 2;
+                long ops = 0;
+                foreach (int x in nums)
+                {
+                    ops += (x - 1) / y;
+                }
+                if (ops <= maxOperations)
+                {
+                    ans = y;
+                    right = y - 1;
+                }
+                else
+                {
+                    left = y + 1;
+                }
+            }
+            return ans;
+        }
+    }
+}
