e6c70a731d19124f64e65889610f05c34b662b13
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-20 10:10:20 +0800

    63. 不同路径 II

--- Ahao.LeetCodeCPP/63. 不同路径 II.cpp
+++ Ahao.LeetCodeCPP/63. 不同路径 II.cpp
@@ -38,5 +38,37 @@ public:
 		}
 		return f[m - 1][n - 1];
 	}
+};
 
-}
+
+/*
+63. 不同路径 II
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+
+现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+
+网格中的障碍物和空位置分别用 1 和 0 来表示。
+*/
+class Solution {
+public:
+	int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+		int n = obstacleGrid.size(), m = obstacleGrid[0].size();
+		vector<vector<int>> f(n, vector<int>(m));
+		f[0][0] = 1;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				if (i || j) f[i][j] == 0;
+				if (!obstacleGrid[i][j])
+				{
+					if (i) f[i][j] += f[i - 1][j];
+					if (j) f[i][j] += f[i][j - 1];
+				}
+			}
+		}
+		return f[n - 1][m - 1];
+	}
+};
