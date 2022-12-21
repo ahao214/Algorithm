5bf2adfc19d82b4e00192567e51df327de9926e1
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-21 09:53:10 +0800

    1753. 移除石子的最大得分

---
+++ Ahao.LeetCode/1700-1799/demo1753/Class1753.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1753
+{
+    public class Class1753
+    {
+        public int MaximumScore(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            int maxVal = Math.Max(Math.Max(a, b), c);
+            return Math.Min(sum - maxVal, sum / 2);
+        }
+
+    }
+}
