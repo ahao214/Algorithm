219c00b82cbb114304a3b8ccd499607d19c76c96
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-21 09:51:28 +0800

    1753. 移除石子的最大得分

--- Ahao.LeetCodeCPP/1753. 移除石子的最大得分.cpp
+++ Ahao.LeetCodeCPP/1753. 移除石子的最大得分.cpp
@@ -25,3 +25,18 @@ public:
 		return (a + b + c - x) / 2;
 	}
 };
+
+class Solution {
+public:
+	int maximumScore(int a, int b, int c) {
+		int sum = a + b + c;
+		int maxVal = max({ a, b, c });
+		if (sum - maxVal < maxVal) {
+			return sum - maxVal;
+		}
+		else {
+			return sum / 2;
+		}
+	}
+};
+
