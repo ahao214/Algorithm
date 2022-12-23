77d48a2be397bc717a667f2443918e38896749cc
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-23 14:34:13 +0800

    371. 两整数之和

---
+++ Ahao.LeetCodeCPP/371. 两整数之和.cpp
@@ -0,0 +1,19 @@
+﻿#include<vector>
+
+using namespace std;
+
+
+
+/*
+371. 两整数之和
+给你两个整数 a 和 b ，不使用 运算符 + 和 - ​​​​​​​，计算并返回两整数之和。
+*/
+class Solution {
+public:
+	int getSum(int a, int b) {
+		if (!b) return a;
+		unsigned int carry = (unsigned int)(a & b) << 1;
+		unsigned sum = a ^ b;
+		return getSum(sum, carry);
+	}
+};
