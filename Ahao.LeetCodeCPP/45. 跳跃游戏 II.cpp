167ef359553823b304e342cd2c9d93a2a7d9338e
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-23 15:36:23 +0800

    45. 跳跃游戏 II

--- Ahao.LeetCodeCPP/45. 跳跃游戏 II.cpp
+++ Ahao.LeetCodeCPP/45. 跳跃游戏 II.cpp
@@ -30,3 +30,31 @@ public:
 	}
 };
 
+
+/*
+45. 跳跃游戏 II
+给你一个非负整数数组 nums ，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
+假设你总是可以到达数组的最后一个位置。
+*/
+class Solution {
+public:
+	int jump(vector<int>& nums) {
+		if (nums.size() == 1) return 0;
+		int left = 0, right = 0, step = 0;
+		while (left <= right) {
+			int maxR = 0;
+			for (int i = left; i <= right; i++)
+				maxR = max(maxR, i + nums[i]);
+			left = right + 1, right = maxR;
+			step++;
+			if (right >= (int)nums.size() - 1)
+				break;
+		}
+		return step;
+	}
+};
