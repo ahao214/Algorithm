ab88009f3cb858e60f012fce615df839f7881cfa
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-23 09:32:09 +0800

    2011. 执行操作后的变量值

--- Ahao.LeetCode/2000-2099/demo2011/Class2011.cs
+++ Ahao.LeetCode/2000-2099/demo2011/Class2011.cs
@@ -8,6 +8,21 @@ namespace Ahao.LeetCode._2000_2099.demo2011
 {
     public class Class2011
     {
-
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            int x = 0;
+            foreach (string op in operations)
+            {
+                if (op == "X++" || op == "++X")
+                {
+                    x++;
+                }
+                else
+                {
+                    x--;
+                }
+            }
+            return x;
+        }
     }
 }
