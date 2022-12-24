57e08d7307c740de8f064ff584203b6da8a88c9f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-24 13:16:28 +0800

    55. 跳跃游戏

--- Ahao.LeetCodeCPP/55. 跳跃游戏.cpp
+++ Ahao.LeetCodeCPP/55. 跳跃游戏.cpp
@@ -2,16 +2,15 @@ using namespace std;
 
 #include<vector>
 
+/*
+55. 跳跃游戏
+给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
 
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+判断你是否能够到达最后一个下标。
+*/
 class Solution {
-	/*
-	55. 跳跃游戏
-	给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
-
-	数组中的每个元素代表你在该位置可以跳跃的最大长度。
-
-	判断你是否能够到达最后一个下标。
-	*/
 public:
 	bool canJump(vector<int>& nums) {
 		for (int i = 0, maxPos = 0; i < nums.size(); i++)
