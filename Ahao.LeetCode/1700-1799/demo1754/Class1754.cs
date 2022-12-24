44e08e18e27326922137a3acfe6e100f9c81f652
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-24 12:49:49 +0800

    1754. 构造字典序最大的合并字符串

---
+++ Ahao.LeetCode/1700-1799/demo1754/Class1754.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1754
+{
+    public class Class1754
+    {
+        public string LargestMerge(string word1, string word2)
+        {
+            StringBuilder merge = new StringBuilder();
+            int i = 0, j = 0;
+            while (i < word1.Length || j < word2.Length)
+            {
+                if (i < word1.Length && word1.Substring(i).CompareTo(word2.Substring(j)) > 0)
+                {
+                    merge.Append(word1[i]);
+                    i++;
+                }
+                else
+                {
+                    merge.Append(word2[j]);
+                    j++;
+                }
+            }
+            return merge.ToString();
+        }
+    }
+}
