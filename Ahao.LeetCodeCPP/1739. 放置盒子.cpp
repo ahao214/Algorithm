ad5314163b0e691c796d5f1c5697fa4be83b031c
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-25 11:03:09 +0800

    1739. 放置盒子

--- Ahao.LeetCodeCPP/1739. 放置盒子.cpp
+++ Ahao.LeetCodeCPP/1739. 放置盒子.cpp
@@ -32,3 +32,23 @@ public:
 		return res;
 	}
 };
+
+
+class Solution {
+public:
+	int minimumBoxes(int n) {
+		int cur = 1, i = 1, j = 1;
+		while (n > cur) {
+			n -= cur;
+			i++;
+			cur += i;
+		}
+		cur = 1;
+		while (n > cur) {
+			n -= cur;
+			j++;
+			cur++;
+		}
+		return (i - 1) * i / 2 + j;
+	}
+};
