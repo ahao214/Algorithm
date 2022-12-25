f35338e4582e0e07d89930dce5bdbcbf6068cfce
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-25 11:02:06 +0800

    1739. 放置盒子

--- Ahao.LeetCode/1700-1799/demo1739/JS1739.js
+++ Ahao.LeetCode/1700-1799/demo1739/JS1739.js
@@ -13,3 +13,35 @@
     }
     return (i - 1) * i / 2 + j;
 };
+
+
+
+//二分查找
+var minimumBoxes = function (n) {
+    let i = 0, j = 0;
+    let low = 1, high = Math.min(n, 100000);
+    while (low < high) {
+        const mid = (low + high) >> 1;
+        const sum = mid * (mid + 1) * (mid + 2) / 6;
+        if (sum >= n) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    i = low;
+    n -= (i - 1) * i * (i + 1) / 6;
+    low = 1;
+    high = i;
+    while (low < high) {
+        const mid = (low + high) >> 1;
+        const sum = mid * (mid + 1) / 2;
+        if (sum >= n) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    j = low;
+    return (i - 1) * i / 2 + j;
+};
