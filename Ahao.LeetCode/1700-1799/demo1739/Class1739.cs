953cbc601a4fd2b31911a27b8542af5fc16a7b6f
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-25 11:01:11 +0800

    1739. 放置盒子

---
+++ Ahao.LeetCode/1700-1799/demo1739/Class1739.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1739
+{
+    public class Class1739
+    {
+
+        public int MinimumBoxes(int n)
+        {
+            int i = 0, j = 0;
+            int low = 1, high = Math.Min(n, 100000);
+            while (low < high)
+            {
+                int mid = (low + high) >> 1;
+                long sum = (long)mid * (mid + 1) * (mid + 2) / 6;
+                if (sum >= n)
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            i = low;
+            n = (int)(n - (long)(i - 1) * i * (i + 1) / 6);
+            low = 1;
+            high = i;
+            while (low < high)
+            {
+                int mid = (low + high) >> 1;
+                long sum = (long)mid * (mid + 1) / 2;
+                if (sum >= n)
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            j = low;
+            return (i - 1) * i / 2 + j;
+        }
+    }
+}
