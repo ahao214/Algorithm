118db800128c12ce42b6bc58f9f44dbc13d7f86e
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-26 09:55:01 +0800

    1759. 统计同构子字符串的数目

---
+++ Ahao.LeetCode/1700-1799/demo1759/Class1759.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1759
+{
+    public class Class1759
+    {
+        public int CountHomogenous(string s)
+        {
+            const int MOD = 1000000007;
+            long res = 0;
+            char prev = s[0];
+            int cnt = 0;
+            foreach (char c in s)
+            {
+                if (c == prev)
+                {
+                    cnt++;
+                }
+                else
+                {
+                    res += (long)(cnt + 1) * cnt / 2;
+                    cnt = 1;
+                    prev = c;
+                }
+            }
+            res += (long)(cnt + 1) * cnt / 2;
+            return (int)(res % MOD);
+        }
+    }
+}
