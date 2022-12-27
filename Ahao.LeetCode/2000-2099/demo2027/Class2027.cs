4360b8313b543dca00bc946b3ade153f0f4791c1
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-27 09:49:28 +0800

    2027. 转换字符串的最少操作次数

---
+++ Ahao.LeetCode/2000-2099/demo2027/Class2027.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2000_2099.demo2027
+{
+    public class Class2027
+    {
+        public int MinimumMoves(string s)
+        {
+            int covered = -1, res = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'X' && i > covered)
+                {
+                    res++;
+                    covered = i + 2;
+                }
+            }
+            return res;
+        }
+    }
+}
