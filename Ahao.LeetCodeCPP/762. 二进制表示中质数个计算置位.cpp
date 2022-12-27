7e9a22033259c6233cdc2d97912340316256d51b
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-27 10:39:55 +0800

    762. 二进制表示中质数个计算置位

--- Ahao.LeetCodeCPP/762. 二进制表示中质数个计算置位.cpp
+++ Ahao.LeetCodeCPP/762. 二进制表示中质数个计算置位.cpp
@@ -1,3 +1,4 @@
+#include <unordered_set>
 using namespace std;
 
 
@@ -35,5 +36,31 @@ public:
 		}
 		return ans;
 	}
+};
 
+
+/*
+762. 二进制表示中质数个计算置位
+给定两个整数L和R ，找到闭区间[L, R]范围内，计算置位位数为质数的整数个数。
+
+（注意，计算置位代表二进制表示中1的个数。
+例如 21 的二进制表示10101有3个计算置位。还有，1 不是质数。）
+
+示例 1:
+*/
+class Solution {
+public:
+	int countPrimeSetBits(int left, int right) {
+		unordered_set<int> primes({ 2,3,5,7,11,13,17,19 });
+
+		int res = 0;
+		for (int i = left; i <= right; i++) {
+			int s = 0;
+			for (int k = i; k; k >>= 1)
+				s += k & 1;
+			if (primes.count(s))
+				res++;
+		}
+		return res;
+	}
 };
