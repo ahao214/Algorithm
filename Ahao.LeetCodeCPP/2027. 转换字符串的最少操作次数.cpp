042c8ba8d7bded806e9a45ad3383dfeef6d4c5ee
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-27 09:44:52 +0800

    2027. 转换字符串的最少操作次数

--- Ahao.LeetCodeCPP/2027. 转换字符串的最少操作次数.cpp
+++ Ahao.LeetCodeCPP/2027. 转换字符串的最少操作次数.cpp
@@ -14,14 +14,14 @@ using namespace std;
 
 class Solution {
 public:
-    int minimumMoves(string s) {
-        int covered = -1, res = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == 'X' && i > covered) {
-                res += 1;
-                covered = i + 2;
-            }
-        }
-        return res;
-    }
+	int minimumMoves(string s) {
+		int covered = -1, res = 0;
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == 'X' && i > covered) {
+				res += 1;
+				covered = i + 2;
+			}
+		}
+		return res;
+	}
 };
