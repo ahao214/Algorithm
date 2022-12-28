4ba826f5a2f512da979c09d046d4ec0f4463afeb
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-28 09:22:18 +0800

    1750. 删除字符串两端相同字符后的最短长度

---
+++ Ahao.LeetCode/1700-1799/demo1750/Class1750.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1750
+{
+    public class Class1750
+    {
+
+        public int MinimumLength(string s)
+        {
+            int n = s.Length;
+            int left = 0, right = n - 1;
+            while (left < right && s[left] == s[right])
+            {
+                char c = s[left];
+                while (left <= right && s[left] == c)
+                {
+                    left++;
+                }
+                while (left <= right && s[right] == c)
+                {
+                    right--;
+                }
+            }
+            return right - left + 1;
+        }
+    }
+}
