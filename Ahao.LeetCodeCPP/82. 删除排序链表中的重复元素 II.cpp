46a9d277eddbc0a2fa6566f3450d97df76017d20
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-28 14:57:21 +0800

    82. 删除排序链表中的重复元素 II

--- Ahao.LeetCodeCPP/82. 删除排序链表中的重复元素 II.cpp
+++ Ahao.LeetCodeCPP/82. 删除排序链表中的重复元素 II.cpp
@@ -31,11 +31,40 @@ public:
 };
 
 
+
 struct ListNode {
 	int val;
 	ListNode* next;
 	ListNode() : val(0), next(nullptr) {}
 	ListNode(int x) : val(x), next(nullptr) {}
 	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
 
+/*
+82. 删除排序链表中的重复元素 II
+给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
+*/
+class Solution {
+public:
+	ListNode* deleteDuplicates(ListNode* head) {
+		ListNode dummy(0, head);
+		ListNode* prev = &dummy;
+		while (head)
+		{
+			if (head->next && head->val == head->next->val)
+			{
+				while (head->next && head->val == head->next->val)
+				{
+					head = head->next;
+				}
+				prev->next = head->next;
+			}
+			else
+			{
+				prev = head;
+			}
+			head = head->next;
+		}
+		return dummy.next;
+	}
 };
