68622ad472ebbaa02d5c375094fc59d96f90e01c
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-28 14:58:12 +0800

    101. 对称二叉树

--- Ahao.LeetCodeCPP/101. 对称二叉树.cpp
+++ Ahao.LeetCodeCPP/101. 对称二叉树.cpp
@@ -1,5 +1,6 @@
 #include<vector>
 #include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -53,6 +54,59 @@ public:
 			if (l->val != r->val) return false;
 
 			l = l->right, r = r->left;
+		}
+		return true;
+	}
+};
+
+
+
+/*
+101. 对称二叉树
+给你一个二叉树的根节点 root ， 检查它是否轴对称。
+*/
+class Solution {
+public:
+	bool dfs(TreeNode* n1, TreeNode* n2)
+	{
+		if (!n1 && !n2)
+			return true;
+		if (!n1 || !n2)
+			return false;
+		return n1->val == n2->val && dfs(n1->left, n2->right) && dfs(n1->right, n2->left);
+	}
+
+	bool isSymmetric(TreeNode* root) {
+		if (!root)//为空
+		{
+			return true;
+		}
+		return dfs(root->left, root->right);
+	}
+
+	bool isSymmetric(TreeNode* root)
+	{
+		if (!root)
+			return true;
+		queue<TreeNode*> q;
+		q.push(root);
+		q.push(root);
+		while (!q.empty())
+		{
+			TreeNode* n1 = q.front();
+			q.pop();
+			TreeNode* n2 = q.front();
+			q.pop();
+			if (!n1 && !n2)
+				continue;
+			if (!n1 || !n2)
+				return false;
+			if (n1->val != n2->val)
+				return false;
+			q.push(n1->left);
+			q.push(n2->right);
+			q.push(n1->right);
+			q.push(n2->left);
 		}
 		return true;
 	}
