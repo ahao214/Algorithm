fc757c235c31864a04b5e1a407aa675c5b4081a7
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-29 09:19:30 +0800

    2032. 至少在两个数组中出现的值

---
+++ Ahao.LeetCode/2000-2099/demo2032/Class2032.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2000_2099.demo2032
+{
+    public class Class2032
+    {
+        public IList<int> TwoOutOfThree(int[] nums1, int[] nums2, int[] nums3)
+        {
+            IDictionary<int, int> dictionary = new Dictionary<int, int>();
+            foreach (int i in nums1)
+            {
+                dictionary.TryAdd(i, 1);
+            }
+            foreach (int i in nums2)
+            {
+                dictionary.TryAdd(i, 0);
+                dictionary[i] |= 2;
+            }
+            foreach (int i in nums3)
+            {
+                dictionary.TryAdd(i, 0);
+                dictionary[i] |= 4;
+            }
+            IList<int> res = new List<int>();
+            foreach (KeyValuePair<int, int> pair in dictionary)
+            {
+                int k = pair.Key, v = pair.Value;
+                if ((v & (v - 1)) != 0)
+                {
+                    res.Add(k);
+                }
+            }
+            return res;
+        }
+    }
+}
