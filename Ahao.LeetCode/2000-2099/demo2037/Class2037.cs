97ad2bc2b428321e8c5012228e96c4256c134bd1
liupenghao@fulan.com.cn <haochen214@163.com>
2022-12-31 09:40:30 +0800

    2037. 使每位学生都有座位的最少移动次数

---
+++ Ahao.LeetCode/2000-2099/demo2037/Class2037.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2000_2099.demo2037
+{
+    public class Class2037
+    {
+        public int MinMovesToSeat(int[] seats, int[] students)
+        {
+            Array.Sort(seats);
+            Array.Sort(students);
+            int res = 0;
+            for (int i = 0; i < seats.Length; i++)
+            {
+                res += Math.Abs(seats[i] - students[i]);
+            }
+            return res;
+        }
+    }
+}
