96645374662082012efe16b8cbb5c9c76a492114
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-01 14:14:45 +0800

    6279. 数组乘积中的不同质因数数目

---
+++ Ahao.LeetCode/6200-6299/demo6279/Class6279.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._6200_6299.demo6279
+{
+    public class Class6279
+    {
+        public int DistinctPrimeFactors(int[] nums)
+        {
+            ISet<int> st = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                int tmp = num;
+                for (int i = 2; i <= tmp; i++)
+                {
+                    if (tmp % i == 0)
+                    {
+                        st.Add(i);
+                    }
+                    while (tmp % i == 0)
+                    {
+                        tmp /= i;
+                    }
+                }
+            }
+            return st.Count;
+        }
+    }
+}
