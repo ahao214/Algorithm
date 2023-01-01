ddb57beb95973da713d66878d1ce8e00778ee38c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-01 13:52:38 +0800

    6278. 统计能整除数字的位数

---
+++ Ahao.LeetCode/6000-6099/demo6278/Class6278.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._6000_6099.demo6278
+{
+    public class Class6278
+    {
+        public int CountDigits(int num)
+        {
+            int res = 0;
+            int oldNum = num;
+            while (num != 0)
+            {
+                if (oldNum % (num % 10) == 0)
+                {
+                    res++;
+                }
+                num /= 10;
+            }
+            return res;
+        }
+    }
+}
