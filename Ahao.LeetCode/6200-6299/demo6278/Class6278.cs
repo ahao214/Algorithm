40641b10846791e70d13e6c8b4f1aded2ea77d14
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-01 13:56:40 +0800

    6278. 统计能整除数字的位数

--- Ahao.LeetCode/6200-6299/demo6278/Class6278.cs
+++ Ahao.LeetCode/6200-6299/demo6278/Class6278.cs
@@ -22,5 +22,26 @@ namespace Ahao.LeetCode._6200_6299.demo6278
             }
             return res;
         }
+
+        public int CountDigits2(int num)
+        {
+            int[] N = new int[10];
+            int sum = 0;
+            int tmp = num;
+            while (num > 0)
+            {
+                int a = num % 10;
+                N[a]++;
+                num /= 10;
+            }
+            for (int i = 1; i < N.Length; i++)
+            {
+                if (tmp % i == 0 && N[i] > 0)
+                {
+                    sum += N[i];
+                }
+            }
+            return sum;
+        }
     }
 }
