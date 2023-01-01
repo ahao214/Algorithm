8cf4076210450736e5f7fa407ea4fc6b245d352d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-01 12:20:24 +0800

    2351. 第一个出现两次的字母

---
+++ Ahao.LeetCode/2300-2399/demo2351/Class2351.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2351
+{
+    public class Class2351
+    {
+        public char RepeatedCharacter(string s)
+        {
+            int[] arr = new int[26];
+            foreach (char c in s)
+            {
+                arr[c - 'a']++;
+                if (arr[c - 'a'] == 2)
+                {
+                    return c;
+                }
+            }
+            return ' ';
+        }
+    }
+}
