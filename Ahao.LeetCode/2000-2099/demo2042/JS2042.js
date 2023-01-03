829ff822e88ffbf0f03d5b3fa1e729c108d0462a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-03 09:37:59 +0800

    2042. 检查句子中的数字是否递增

--- Ahao.LeetCode/2000-2099/demo2042/JS2042.js
+++ Ahao.LeetCode/2000-2099/demo2042/JS2042.js
@@ -1,1 +1,23 @@
-﻿
+﻿var areNumbersAscending = function (s) {
+    let pre = 0, pos = 0;
+    while (pos < s.length) {
+        if (isDigit(s[pos])) {
+            let cur = 0;
+            while (pos < s.length && isDigit(s[pos])) {
+                cur = cur * 10 + s[pos].charCodeAt() - '0'.charCodeAt();
+                pos++;
+            }
+            if (cur <= pre) {
+                return false;
+            }
+            pre = cur;
+        } else {
+            pos++;
+        }
+    }
+    return true;
+};
+
+const isDigit = (ch) => {
+    return parseFloat(ch).toString() === "NaN" ? false : true;
+}
