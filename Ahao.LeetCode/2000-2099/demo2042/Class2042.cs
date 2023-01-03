e453e24a86aaeb8a829da62032b9fbe23ffb485a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-03 09:37:39 +0800

    2042. 检查句子中的数字是否递增

---
+++ Ahao.LeetCode/2000-2099/demo2042/Class2042.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2000_2099.demo2042
+{
+    public class Class2042
+    {
+        public bool AreNumbersAscending(string s)
+        {
+            int pre = 0, pos = 0;
+            while (pos < s.Length)
+            {
+                if (char.IsDigit(s[pos]))
+                {
+                    int cur = 0;
+                    while (pos < s.Length && char.IsDigit(s[pos]))
+                    {
+                        cur = cur * 10 + s[pos] - '0';
+                        pos++;
+                    }
+                    if (cur <= pre)
+                    {
+                        return false;
+                    }
+                    pre = cur;
+                }
+                else
+                {
+                    pos++;
+                }
+            }
+            return true;
+        }
+    }
+}
