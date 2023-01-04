6d4cc356df24cea59ef2f3017757771e1ad1f1f7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-04 09:39:48 +0800

    1802. 有界数组中指定下标处的最大值

--- Ahao.LeetCode/1800-1899/demo1802/JS1802.js
+++ Ahao.LeetCode/1800-1899/demo1802/JS1802.js
@@ -27,3 +27,35 @@ const cal = (big, length) => {
         return Math.floor(big * (big - 1) / 2) + ones;
     }
 };
+
+
+//数学推导
+var maxValue = function (n, index, maxSum) {
+    let left = index;
+    let right = n - index - 1;
+    if (left > right) {
+        let temp = left;
+        left = right;
+        right = temp;
+    }
+
+    let upper = ((left + 1) * (left + 1) - 3 * (left + 1)) / 2 + left + 1 + (left + 1) + ((left + 1) * (left + 1) - 3 * (left + 1)) / 2 + right + 1;
+    if (upper >= maxSum) {
+        let a = 1;
+        let b = -2;
+        let c = left + right + 2 - maxSum;
+        return Math.floor((-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a));
+    }
+
+    upper = (2 * (right + 1) - left - 1) * left / 2 + (right + 1) + ((right + 1) * (right + 1) - 3 * (right + 1)) / 2 + right + 1;
+    if (upper >= maxSum) {
+        let a = 1.0 / 2;
+        let b = left + 1 - 3.0 / 2;
+        let c = right + 1 + (-left - 1) * left / 2 - maxSum;
+        return Math.floor((-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a));
+    } else {
+        let a = left + right + 1;;
+        let b = (-left * left - left - right * right - right) / 2 - maxSum;
+        return Math.floor(-b / a);
+    }
+};
