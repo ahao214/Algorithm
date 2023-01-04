c7c5abfd7e47f0bed0c6af350633e32d2654a6fd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-04 09:39:16 +0800

    1802. 有界数组中指定下标处的最大值

---
+++ Ahao.LeetCode/1800-1899/demo1802/Class1802.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1802
+{
+    public class Class1802
+    {
+        /// <summary>
+        /// 贪心+二分查找
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="index"></param>
+        /// <param name="maxSum"></param>
+        /// <returns></returns>
+        public int MaxValue(int n, int index, int maxSum)
+        {
+            int left = 1, right = maxSum;
+            while (left < right)
+            {
+                int mid = (left + right + 1) / 2;
+                if (Valid(mid, n, index, maxSum))
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return left;
+        }
+
+        public bool Valid(int mid, int n, int index, int maxSum)
+        {
+            int left = index;
+            int right = n - index - 1;
+            return mid + Cal(mid, left) + Cal(mid, right) <= maxSum;
+        }
+
+        public long Cal(int big, int length)
+        {
+            if (length + 1 < big)
+            {
+                int small = big - length;
+                return (long)(big - 1 + small) * length / 2;
+            }
+            else
+            {
+                int ones = length - (big - 1);
+                return (long)big * (big - 1) / 2 + ones;
+            }
+        }
+
+
+
+    }
+}
