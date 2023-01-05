a87d4ca874a57a400247f86a065e26a97996156b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-05 09:37:03 +0800

    1803. 统计异或值在范围内的数对有多少

--- Ahao.LeetCodeCPP/1803. 统计异或值在范围内的数对有多少.cpp
+++ Ahao.LeetCodeCPP/1803. 统计异或值在范围内的数对有多少.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 /*
+1803. 统计异或值在范围内的数对有多少
 
+给你一个整数数组 nums （下标 从 0 开始 计数）以及两个整数：low 和 high ，请返回 漂亮数对 的数目。
+
+漂亮数对 是一个形如 (i, j) 的数对，其中 0 <= i < j < nums.length 且 low <= (nums[i] XOR nums[j]) <= high 。
 */
 
 struct Trie {
