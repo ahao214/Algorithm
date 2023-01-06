2277e9161e0e039e1d975ee7062ad5254bcd1b56
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-06 09:29:45 +0800

    2180. 统计各位数字之和为偶数的整数个数

--- Ahao.LeetCodeCPP/2180. 统计各位数字之和为偶数的整数个数.cpp
+++ Ahao.LeetCodeCPP/2180. 统计各位数字之和为偶数的整数个数.cpp
@@ -27,3 +27,22 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int countEven(int num) {
+        int res = 0;
+        for (int i = 1; i <= num; i++) {
+            int x = i, sum = 0;
+            while (x) {
+                sum += x % 10;
+                x /= 10;
+            }
+            if (sum % 2 == 0) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
