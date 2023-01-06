51a34beac1df3ad15073160fa620c5148e72b92a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-06 09:40:59 +0800

    2180. 统计各位数字之和为偶数的整数个数

---
+++ Ahao.LeetCode/2100-2199/demo2180/JS2180.js
@@ -0,0 +1,14 @@
+﻿var countEven = function (num) {
+    let res = 0;
+    for (let i = 1; i <= num; i++) {
+        let x = i, sum = 0;
+        while (x !== 0) {
+            sum += x % 10;
+            x = Math.floor(x / 10);
+        }
+        if (sum % 2 === 0) {
+            res++;
+        }
+    }
+    return res;
+};
