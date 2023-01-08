2827b54b93572c13ab25be79ae3e6bc77b2b0400
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-08 13:20:07 +0800

    6283. 正整数和负整数的最大计数 显示英文描述

---
+++ Ahao.LeetCode/6200-6299/demo6283/Class6283.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._6200_6299.demo6283
+{
+    public class Class6283
+    {
+        public int MaximumCount(int[] nums)
+        {
+            int positiveCount = 0;
+            int negativeCount = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    positiveCount++;
+                }
+                if (nums[i] < 0)
+                {
+                    negativeCount++;
+                }
+            }
+            return Math.Max(positiveCount, negativeCount);
+        }
+    }
+}
