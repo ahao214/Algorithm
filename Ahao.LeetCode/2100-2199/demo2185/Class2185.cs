d119fa8d8eafa2281d215ec753a8dfbb8ca5ed58
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-08 13:05:22 +0800

    2185. 统计包含给定前缀的字符串

---
+++ Ahao.LeetCode/2100-2199/demo2185/Class2185.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2100_2199.demo2185
+{
+    public class Class2185
+    {
+        public int PrefixCount(string[] words, string pref)
+        {
+            int res = 0;
+            int len = pref.Length;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length >= len)
+                {
+                    if (words[i].Substring(0, len) == pref)
+                        res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
