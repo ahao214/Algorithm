c965905b4614d9fb6f94e270d630c2bda71417ad
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-08 13:32:02 +0800

    6285. 执行 K 次操作后的最大分数

---
+++ Ahao.LeetCode/6200-6299/demo6285/Class6285.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._6200_6299.demo6285
+{
+    public class Class6285
+    {
+        public long MaxKelements(int[] nums, int k)
+        {
+            long res = 0;
+            while (k > 0)
+            {
+                Array.Sort(nums);
+                res += nums[nums.Length-1];
+                
+            }
+
+            return res;
+        }
+    }
+}
