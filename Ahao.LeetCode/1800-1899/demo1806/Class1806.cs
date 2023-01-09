641fc4e709c9da40a39c2610899b7049bdd20896
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-09 09:37:02 +0800

    1806. 还原排列的最少操作步数

---
+++ Ahao.LeetCode/1800-1899/demo1806/Class1806.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1806
+{
+    public class Class1806
+    {
+        public int ReinitializePermutation(int n)
+        {
+            int[] perm = new int[n];
+            int[] target = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                perm[i] = i;
+                target[i] = i;
+            }
+            int step = 0;
+            while (true)
+            {
+                int[] arr = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    if ((i & 1) != 0)
+                    {
+                        arr[i] = perm[n / 2 + (i - 1) / 2];
+                    }
+                    else
+                    {
+                        arr[i] = perm[i / 2];
+                    }
+                }
+                perm = arr;
+                step++;
+                if (Enumerable.SequenceEqual(perm, target))
+                {
+                    break;
+                }
+            }
+            return step;
+        }
+    }
+}
