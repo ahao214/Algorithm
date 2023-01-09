d434f8f3ec461b88cc4ca1adceb0fec666bf26de
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-09 09:37:30 +0800

    1806. 还原排列的最少操作步数

---
+++ Ahao.LeetCode/1800-1899/demo1806/JS1806.js
@@ -0,0 +1,21 @@
+﻿var reinitializePermutation = function (n) {
+    let perm = new Array(n).fill(0).map((_, i) => i);
+    const target = new Array(n).fill(0).map((_, i) => i);
+    let step = 0;
+    while (true) {
+        const arr = new Array(n).fill(0);
+        for (let i = 0; i < n; i++) {
+            if ((i & 1) !== 0) {
+                arr[i] = perm[Math.floor(n / 2) + Math.floor((i - 1) / 2)];
+            } else {
+                arr[i] = perm[Math.floor(i / 2)];
+            }
+        }
+        perm = arr;
+        step++;
+        if (perm.toString() === target.toString()) {
+            break;
+        }
+    }
+    return step;
+};
