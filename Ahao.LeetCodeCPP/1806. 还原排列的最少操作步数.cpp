34cadae2f018312e0652ab77c024d750245a8d56
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-09 09:35:45 +0800

    1806. 还原排列的最少操作步数

--- Ahao.LeetCodeCPP/1806. 还原排列的最少操作步数.cpp
+++ Ahao.LeetCodeCPP/1806. 还原排列的最少操作步数.cpp
@@ -1,4 +1,5 @@
 ﻿#include<vector>
+#include <numeric>
 
 using namespace std;
 
@@ -49,5 +50,34 @@ public:
 			if (check(q)) break;
 		}
 		return cnt;
+	}
+};
+
+
+
+class Solution {
+public:
+	int reinitializePermutation(int n) {
+		vector<int> perm(n), target(n);
+		iota(perm.begin(), perm.end(), 0);
+		iota(target.begin(), target.end(), 0);
+		int step = 0;
+		while (true) {
+			vector<int> arr(n);
+			for (int i = 0; i < n; i++) {
+				if (i & 1) {
+					arr[i] = perm[n / 2 + (i - 1) / 2];
+				}
+				else {
+					arr[i] = perm[i / 2];
+				}
+			}
+			perm = move(arr);
+			step++;
+			if (perm == target) {
+				break;
+			}
+		}
+		return step;
 	}
 };
