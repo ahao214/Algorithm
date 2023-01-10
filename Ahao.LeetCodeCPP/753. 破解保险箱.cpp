124271dbda96bb1f467f58f7b68c85fc32cd2d35
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-10 09:26:00 +0800

    753. 破解保险箱

---
+++ Ahao.LeetCodeCPP/753. 破解保险箱.cpp
@@ -0,0 +1,45 @@
+#include<vector>
+#include <string>
+#include<unordered_set>
+
+using namespace std;
+
+/*
+753. 破解保险箱
+
+有一个需要密码才能打开的保险箱。密码是 n 位数, 密码的每一位是 k 位序列 0, 1, ..., k-1 中的一个 。
+
+你可以随意输入密码，保险箱会自动记住最后 n 位输入，如果匹配，则能够打开保险箱。
+
+举个例子，假设密码是 "345"，你可以输入 "012345" 来打开它，只是你输入了 6 个字符.
+
+请返回一个能打开保险箱的最短字符串。
+*/
+
+class Solution {
+private:
+    unordered_set<int> seen;
+    string ans;
+    int highest;
+    int k;
+
+public:
+    void dfs(int node) {
+        for (int x = 0; x < k; ++x) {
+            int nei = node * 10 + x;
+            if (!seen.count(nei)) {
+                seen.insert(nei);
+                dfs(nei % highest);
+                ans += (x + '0');
+            }
+        }
+    }
+
+    string crackSafe(int n, int k) {
+        highest = pow(10, n - 1);
+        this->k = k;
+        dfs(0);
+        ans += string(n - 1, '0');
+        return ans;
+    }
+};
