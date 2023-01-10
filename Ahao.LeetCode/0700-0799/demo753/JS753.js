d0d151c5ed3770863f9d028153589bbfa999fed2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-10 09:28:07 +0800

    753. 破解保险箱

---
+++ Ahao.LeetCode/0700-0799/demo753/JS753.js
@@ -0,0 +1,21 @@
+﻿var crackSafe = function (n, k) {
+    highest = Math.pow(10, n - 1);
+    let ans = '';
+    const seen = new Set();
+    const dfs = (node) => {
+        for (let x = 0; x < k; ++x) {
+            let nei = node * 10 + x;
+            if (!seen.has(nei)) {
+                seen.add(nei);
+                dfs(nei % highest);
+                ans += x;
+            }
+        }
+    };
+
+    dfs(0);
+    for (let i = 1; i < n; i++) {
+        ans += '0';
+    }
+    return ans;
+}
