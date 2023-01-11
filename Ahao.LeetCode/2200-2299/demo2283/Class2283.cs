036734df28b0b55a2777d61480f7091c8622926d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-11 09:28:40 +0800

    2283. 判断一个数的数字计数是否等于数位的值

---
+++ Ahao.LeetCode/2200-2299/demo2283/Class2283.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2200_2299.demo2283
+{
+    public class Class2283
+    {
+        public bool DigitCount(string num)
+        {
+            IDictionary<int, int> h = new Dictionary<int, int>();
+            int n = num.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int digit = num[i] - '0';
+                h.TryAdd(digit, 0);
+                h[digit]++;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int v = num[i] - '0';
+                h.TryAdd(i, 0);
+                if (h[i] != v)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
