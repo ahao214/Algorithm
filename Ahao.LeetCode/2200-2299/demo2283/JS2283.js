27b17c09ab9e417aeee16d77627f4eba7c0aca87
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-11 09:29:30 +0800

    2283. 判断一个数的数字计数是否等于数位的值

---
+++ Ahao.LeetCode/2200-2299/demo2283/JS2283.js
@@ -0,0 +1,15 @@
+﻿var digitCount = function (num) {
+    const h = new Map();
+    const n = num.length;
+    for (let i = 0; i < n; i++) {
+        const digit = num[i].charCodeAt() - '0'.charCodeAt();
+        h.set(digit, (h.get(digit) || 0) + 1);
+    }
+    for (let i = 0; i < n; i++) {
+        const v = num[i].charCodeAt() - '0'.charCodeAt();
+        if ((h.get(i) || 0) !== v) {
+            return false;
+        }
+    }
+    return true;
+};
