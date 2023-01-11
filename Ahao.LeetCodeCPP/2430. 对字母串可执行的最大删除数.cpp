db69b07e300841251103337dcbd48bf0fdebe369
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-11 15:06:22 +0800

    2430. 对字母串可执行的最大删除数

--- Ahao.LeetCodeCPP/2430. 对字母串可执行的最大删除数.cpp
+++ Ahao.LeetCodeCPP/2430. 对字母串可执行的最大删除数.cpp
@@ -16,8 +16,29 @@ using namespace std;
 返回删除 s 所需的最大操作数。
 */
 class Solution {
+	int lcs[4005][4005];
+	int dp[4005];
 public:
-    int deleteString(string s) {
+	int deleteString(string s) {
+		int n = s.size();
+		for (int i = n - 1; i >= 0; i--) {
+			for (int j = n - 1; j >= 0; j--) {
+				if (s[i] == s[j]) {
+					lcs[i][j] = lcs[i + 1][j + 1] + 1;
+				}
+			}
+		}
+		for (int i = n - 1; i >= 0; i--) {
+			dp[i] = 1;
+			for (int j = i + 1; j < n; j++) {
+				if (lcs[i][j] >= j - i) {
+					dp[i] = max(dp[i], dp[j] + 1);
+				}
+				else {
 
-    }
+				}
+			}
+		}
+		return dp[0];
+	}
 };
