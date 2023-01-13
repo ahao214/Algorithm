c896c2cf2c5dc4f67499b524e266a8ccd82e0c21
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-13 09:35:53 +0800

    2287. 重排字符形成目标字符串

--- Ahao.LeetCode/2200-2299/demo2287/Class2287.cs
+++ Ahao.LeetCode/2200-2299/demo2287/Class2287.cs
@@ -39,5 +39,32 @@ namespace Ahao.LeetCode._2200_2299.demo2287
             }
             return ans;
         }
+
+
+
+        public int RearrangeCharacters2(string s, string target)
+        {
+            //最多26个字母，s长度为100
+            var sList = new int[26];
+            var targetList = new int[26];
+            var res = int.MaxValue;
+
+            //统计s和target中各字母的个数
+            foreach (char item in s)
+                sList[item - 'a']++;
+
+            foreach (char item in target)
+                targetList[item - 'a']++;
+
+            //计算各个字母
+            for (int i = 0; i < 26; i++)
+            {
+                //target存在此字母时，结果为s中的个数除以target中的个数，更新最小值
+                if (targetList[i] != 0)
+                    res = Math.Min(res, sList[i] / targetList[i]);
+            }
+
+            return res;
+        }
     }
 }
