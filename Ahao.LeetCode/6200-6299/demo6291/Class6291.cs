fcb7b68fb9d0684dc5a9e67bb38a4a81e80027ce
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-15 10:40:14 +0800

    6291. 数组元素和与数字和的绝对差

---
+++ Ahao.LeetCode/6200-6299/demo6291/Class6291.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._6200_6299.demo6291
+{
+    public class Class6291
+    {
+        public int DifferenceOfSum(int[] nums)
+        {
+            int eleSum = 0;
+            int bitSum = 0;
+            eleSum = nums.Sum();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bitSum += BitSum(nums[i]);
+            }
+            return Math.Abs(eleSum - bitSum);
+        }
+
+        public int BitSum(int num)
+        {
+            int bitSum = 0;
+            while (num != 0)
+            {
+                bitSum += num % 10;
+                num /= 10;
+            }
+            return bitSum;
+        }
+    }
+}
