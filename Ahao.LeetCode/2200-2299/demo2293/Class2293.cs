14cd52d4224f63969b55ff5e1e2d223e65f91bc0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-15 11:11:58 +0800

    2293. 极大极小游戏

---
+++ Ahao.LeetCode/2200-2299/demo2293/Class2293.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2200_2299.demo2293
+{
+    public class Class2293
+    {
+        /// <summary>
+        /// 递归
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int MinMaxGame(int[] nums)
+        {
+            int n = nums.Length;
+            if (n == 1)
+            {
+                return nums[0];
+            }
+            int[] newNums = new int[n / 2];
+            for (int i = 0; i < newNums.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    newNums[i] = Math.Min(nums[2 * i], nums[2 * i + 1]);
+                }
+                else
+                {
+                    newNums[i] = Math.Max(nums[2 * i], nums[2 * i + 1]);
+                }
+            }
+            return MinMaxGame(newNums);
+        }
+
+    }
+}
