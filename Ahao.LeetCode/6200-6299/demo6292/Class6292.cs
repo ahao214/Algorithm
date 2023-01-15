e2ef485b7fb1db9113b6c6d73b1dc00ae6abbd12
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-15 11:03:27 +0800

    6292. 子矩阵元素加 1

--- Ahao.LeetCode/6200-6299/demo6292/Class6292.cs
+++ Ahao.LeetCode/6200-6299/demo6292/Class6292.cs
@@ -10,7 +10,26 @@ namespace Ahao.LeetCode._6200_6299.demo6292
     {
         public int[][] RangeAddQueries(int n, int[][] queries)
         {
+            int[][] result = new int[n][];
 
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = new int[n];
+            }
+
+            int len = queries.Length;
+            for (int i = 0; i < len; i++)
+            {
+                for (int m = queries[i][0]; m <= queries[i][2]; m++)
+                {
+                    for (int j = queries[i][1]; j <= queries[i][3]; j++)
+                    {
+                        result[m][j] += 1;
+                    }
+                }
+            }
+
+            return result;
         }
 
     }
