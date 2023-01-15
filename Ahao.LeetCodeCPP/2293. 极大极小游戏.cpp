6fec8876e9a0648850b5aa1c797ed16ec15cc545
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-15 11:10:15 +0800

    2293. 极大极小游戏

--- Ahao.LeetCodeCPP/2293. 极大极小游戏.cpp
+++ Ahao.LeetCodeCPP/2293. 极大极小游戏.cpp
@@ -36,3 +36,26 @@ public:
 		return minMaxGame(newNums);
 	}
 };
+
+
+
+class Solution {
+public:
+	int minMaxGame(vector<int>& nums) {
+		int n = nums.size();
+		while (n != 1) {
+			vector<int> newNums(n / 2);
+			for (int i = 0; i < newNums.size(); i++) {
+				if (i % 2 == 0) {
+					newNums[i] = min(nums[2 * i], nums[2 * i + 1]);
+				}
+				else {
+					newNums[i] = max(nums[2 * i], nums[2 * i + 1]);
+				}
+			}
+			nums = newNums;
+			n /= 2;
+		}
+		return nums[0];
+	}
+};
