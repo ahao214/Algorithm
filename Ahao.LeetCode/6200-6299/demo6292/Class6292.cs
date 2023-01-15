8ac2e3ab9cb7e3b3ee4f8904ffa8413f0d833b59
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-15 10:44:43 +0800

    6292. 子矩阵元素加 1

---
+++ Ahao.LeetCode/6200-6299/demo6292/Class6292.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._6200_6299.demo6292
+{
+    public class Class6292
+    {
+        public int[][] RangeAddQueries(int n, int[][] queries)
+        {
+
+        }
+
+    }
+}
