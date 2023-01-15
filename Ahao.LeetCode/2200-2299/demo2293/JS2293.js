4ce0783326f09a0ad9d55a9ae6e3a061d5e2caaf
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-15 11:13:41 +0800

    2293. 极大极小游戏

---
+++ Ahao.LeetCode/2200-2299/demo2293/JS2293.js
@@ -0,0 +1,35 @@
+﻿//递归
+var minMaxGame = function (nums) {
+    const n = nums.length;
+    if (n === 1) {
+        return nums[0];
+    }
+    const newNums = new Array(Math.floor(n / 2)).fill(0);
+    for (let i = 0; i < newNums.length; i++) {
+        if (i % 2 === 0) {
+            newNums[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+        } else {
+            newNums[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+        }
+    }
+    return minMaxGame(newNums);
+};
+
+
+//模拟
+var minMaxGame = function (nums) {
+    let n = nums.length;
+    while (n !== 1) {
+        const newNums = new Array(Math.floor(n / 2)).fill(0);
+        for (let i = 0; i < newNums.length; i++) {
+            if (i % 2 === 0) {
+                newNums[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+            } else {
+                newNums[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+            }
+        }
+        nums = newNums;
+        n = Math.floor(n / 2);
+    }
+    return nums[0];
+};
