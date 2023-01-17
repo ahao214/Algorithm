b42872eb61fd431ce0f1e8c771b164bbe393f92c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-17 09:32:17 +0800

    1814. 统计一个数组中好对子的数目

---
+++ Ahao.LeetCode/1800-1899/demo1814/Class1814.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1814
+{
+    public class Class1814
+    {
+        public int CountNicePairs(int[] nums)
+        {
+            const int MOD = 1000000007;
+            int res = 0;
+            IDictionary<int, int> h = new Dictionary<int, int>();
+            foreach (int i in nums)
+            {
+                int temp = i, j = 0;
+                while (temp > 0)
+                {
+                    j = j * 10 + temp % 10;
+                    temp /= 10;
+                }
+                h.TryAdd(i - j, 0);
+                res = (res + h[i - j]) % MOD;
+                h[i - j]++;
+            }
+            return res;
+        }
+    }
+}
