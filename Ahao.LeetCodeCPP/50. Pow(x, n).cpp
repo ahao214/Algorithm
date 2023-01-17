7c21aa3175a647bc4ab1d1e25b0e50a88cbdc706
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-17 10:43:17 +0800

    50. Pow(x, n)

--- Ahao.LeetCodeCPP/50. Pow(x, n).cpp
+++ Ahao.LeetCodeCPP/50. Pow(x, n).cpp
@@ -1,11 +1,12 @@
 using namespace std;
 
 
+
+/*
+50. Pow(x, n)
+实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，x^n ）。
+*/
 class Solution {
-	/*
-	50. Pow(x, n)
-	实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，x^n ）。
-	*/
 public:
 	double myPow(double x, int n) {
 		long long N = n;
@@ -18,5 +19,37 @@ public:
 			N >>= 1;
 		}
 		return n < 0 ? 1 / ans : ans;
+	}
+};
+
+
+
+class Solution {
+	double pow(double x, long n) {
+		if (n == 0) {
+			return 1;
+		}
+		double half = pow(x, n / 2);
+		if (n % 2 == 0) {
+			return half * half;
+		}
+		else
+		{
+			return half * half * x;
+		}
+	}
+
+public:
+	double myPow(double x, int n) {
+		if (x == 0 || x == 1) {
+			return x;
+		}
+		if (n >= 0) {
+			return pow(x, n);
+		}
+		else
+		{
+			return 1 / pow(x, (long)n * (-1));
+		}
 	}
 };
