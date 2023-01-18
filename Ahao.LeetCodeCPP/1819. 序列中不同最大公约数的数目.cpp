831b2df1acc761e9869f66a5fbdfeb701cc4dc78
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-18 09:42:53 +0800

    1819. 序列中不同最大公约数的数目

--- Ahao.LeetCodeCPP/1819. 序列中不同最大公约数的数目.cpp
+++ Ahao.LeetCodeCPP/1819. 序列中不同最大公约数的数目.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 /*
 1819. 序列中不同最大公约数的数目
+
 给你一个由正整数组成的数组 nums 。
 
 数字序列的 最大公约数 定义为序列中所有整数的共有约数中的最大整数。
@@ -43,5 +44,41 @@ public:
             }
         }
         return ans;
+    }
+};
+
+
+
+
+class Solution {
+    int g[200001];
+public:
+    int countDifferentSubsequenceGCDs(vector<int>& nums) {
+        for (auto num : nums)
+        {
+            for (int i = 1; i <= sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    if (g[i] == 0)
+                        g[i] = num;
+                    else
+                        g[i] = gcd(g[i], num);
+                    if (num / i != i)
+                    {
+                        if (g[num / i] == 0)
+                            g[num / i] = num;
+                        else
+                            g[num / i] = gcd(g[num / i], num);
+                    }
+                }
+            }
+        }
+
+        int count = 0;
+        for (int f = 1; f <= 200000; f++)
+            if (f == g[f])
+                count++;
+        return count;
     }
 };
