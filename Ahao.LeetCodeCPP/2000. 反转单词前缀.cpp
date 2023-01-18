6e548764a91ac5e882eee061f330c4415f95702e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-18 10:51:32 +0800

    2000. 反转单词前缀

--- Ahao.LeetCodeCPP/2000. 反转单词前缀.cpp
+++ Ahao.LeetCodeCPP/2000. 反转单词前缀.cpp
@@ -26,3 +26,15 @@ public:
 		return word;
 	}
 };
+
+
+class Solution {
+public:
+	string reversePrefix(string word, char ch) {
+		int index = word.find(ch);
+		if (index != string::npos) {
+			reverse(word.begin(), word.begin() + index + 1);
+		}
+		return word;
+	}
+};
