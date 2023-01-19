32a96760f4519778789f2db58fb42cd9789b51ed
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-19 09:19:39 +0800

    2299. 强密码检验器 II

---
+++ Ahao.LeetCode/2200-2299/deom2299/JS2299.js
@@ -0,0 +1,46 @@
+﻿var strongPasswordCheckerII = function (password) {
+    if (password.length < 8) {
+        return false;
+    }
+
+    const specials = new Set();
+    specials.add('!');
+    specials.add('@');
+    specials.add('#');
+    specials.add('$');
+    specials.add('%');
+    specials.add('^');
+    specials.add('&');
+    specials.add('*');
+    specials.add('(');
+    specials.add(')');
+    specials.add('-');
+    specials.add('+');
+    const n = password.length;
+    let hasLower = false, hasUpper = false, hasDigit = false, hasSpecial = false;
+    for (let i = 0; i < n; ++i) {
+        if (i !== n - 1 && password[i] === password[i + 1]) {
+            return false;
+        }
+
+        const ch = password[i];
+        if (isLowerCase(ch)) {
+            hasLower = true;
+        } else if (isUpperCase(ch)) {
+            hasUpper = true;
+        } else if (isDigit(ch)) {
+            hasDigit = true;
+        } else if (specials.has(ch)) {
+            hasSpecial = true;
+        }
+    }
+    return hasLower && hasUpper && hasDigit && hasSpecial;
+};
+
+const isDigit = (ch) => {
+    return parseFloat(ch).toString() === "NaN" ? false : true;
+}
+
+const isLowerCase = str => 'a' <= str && str <= 'z';
+
+const isUpperCase = str => 'A' <= str && str <= 'Z';
