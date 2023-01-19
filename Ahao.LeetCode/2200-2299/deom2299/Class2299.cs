92d0a88f66684119730b9abc9e0975c28f9cf6e2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-19 09:19:13 +0800

    2299. 强密码检验器 II

---
+++ Ahao.LeetCode/2200-2299/deom2299/Class2299.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2200_2299.deom2299
+{
+    public class Class2299
+    {
+        public bool StrongPasswordCheckerII(string password)
+        {
+            if (password.Length < 8)
+            {
+                return false;
+            }
+
+            ISet<char> specials = new HashSet<char>() { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+' };
+            int n = password.Length;
+            bool hasLower = false, hasUpper = false, hasDigit = false, hasSpecial = false;
+            for (int i = 0; i < n; ++i)
+            {
+                if (i != n - 1 && password[i] == password[i + 1])
+                {
+                    return false;
+                }
+
+                char ch = password[i];
+                if (char.IsLower(ch))
+                {
+                    hasLower = true;
+                }
+                else if (char.IsUpper(ch))
+                {
+                    hasUpper = true;
+                }
+                else if (char.IsDigit(ch))
+                {
+                    hasDigit = true;
+                }
+                else if (specials.Contains(ch))
+                {
+                    hasSpecial = true;
+                }
+            }
+
+            return hasLower && hasUpper && hasDigit && hasSpecial;
+        }
+
+    }
+}
