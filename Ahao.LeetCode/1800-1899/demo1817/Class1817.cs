0a5af2ae0a6c01dfa3a8fc482ef3437d11a29ee2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-20 09:50:08 +0800

    1817. 查找用户活跃分钟数

---
+++ Ahao.LeetCode/1800-1899/demo1817/Class1817.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1800_1899.demo1817
+{
+    public class Class1817
+    {
+        public int[] FindingUsersActiveMinutes(int[][] logs, int k)
+        {
+            IDictionary<int, ISet<int>> activeMinutes = new Dictionary<int, ISet<int>>();
+            foreach (int[] log in logs)
+            {
+                int id = log[0], time = log[1];
+                activeMinutes.TryAdd(id, new HashSet<int>());
+                activeMinutes[id].Add(time);
+            }
+            int[] answer = new int[k];
+            foreach (ISet<int> minutes in activeMinutes.Values)
+            {
+                int activeCount = minutes.Count;
+                answer[activeCount - 1]++;
+            }
+            return answer;
+        }
+
+    }
+}
