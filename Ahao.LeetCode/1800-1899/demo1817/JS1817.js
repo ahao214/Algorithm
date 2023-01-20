6229881ec78313e7f8ced6051ceaffc8dee9efda
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-20 09:51:35 +0800

    1817. 查找用户活跃分钟数

---
+++ Ahao.LeetCode/1800-1899/demo1817/JS1817.js
@@ -0,0 +1,15 @@
+﻿var findingUsersActiveMinutes = function (logs, k) {
+    const activeMinutes = new Map();
+    for (const [id, time] of logs) {
+        if (!activeMinutes.has(id)) {
+            activeMinutes.set(id, new Set());
+        }
+        activeMinutes.get(id).add(time);
+    }
+    const answer = new Array(k).fill(0);
+    for (const minutes of activeMinutes.values()) {
+        const activeCount = minutes.size;
+        answer[activeCount - 1]++;
+    }
+    return answer;
+};
