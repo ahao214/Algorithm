504cfb5bdab4dda1c369bef0ba113b59484d8a3f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-21 10:55:29 +0800

    1824. 最少侧跳次数

--- Ahao.LeetCodeCPP/1824. 最少侧跳次数.cpp
+++ Ahao.LeetCodeCPP/1824. 最少侧跳次数.cpp
@@ -1,4 +1,5 @@
 #include<vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -93,5 +94,34 @@ public:
 		for (int j = 1; j <= 3; j++)
 			res = min(res, dp[n][j]);
 		return res;
+	}
+};
+
+
+
+class Solution {
+	static constexpr int inf = 0x3f3f3f3f;
+public:
+	int minSideJumps(vector<int>& obstacles) {
+		int n = obstacles.size() - 1;
+		vector<int> d = { 1, 0, 1 };
+		for (int i = 1; i <= n; i++) {
+			int minCnt = inf;
+			for (int j = 0; j < 3; j++) {
+				if (j == obstacles[i] - 1) {
+					d[j] = inf;
+				}
+				else {
+					minCnt = min(minCnt, d[j]);
+				}
+			}
+			for (int j = 0; j < 3; j++) {
+				if (j == obstacles[i] - 1) {
+					continue;
+				}
+				d[j] = min(d[j], minCnt + 1);
+			}
+		}
+		return *min_element(d.begin(), d.end());
 	}
 };
