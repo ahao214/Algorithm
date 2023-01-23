8ebe6eeb472fe0144aaff5573cfe11f84212a480
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-23 11:12:04 +0800

    2303. 计算应缴税款总额

---
+++ Ahao.LeetCodeCPP/2303. 计算应缴税款总额.cpp
@@ -0,0 +1,35 @@
+#include<vector>
+
+using namespace std;
+
+/*
+2303. 计算应缴税款总额
+
+给你一个下标从 0 开始的二维整数数组 brackets ，其中 brackets[i] = [upperi, percenti] ，表示第 i 个税级的上限是 upperi ，征收的税率为 percenti 。税级按上限 从低到高排序（在满足 0 < i < brackets.length 的前提下，upperi-1 < upperi）。
+
+税款计算方式如下：
+
+不超过 upper0 的收入按税率 percent0 缴纳
+接着 upper1 - upper0 的部分按税率 percent1 缴纳
+然后 upper2 - upper1 的部分按税率 percent2 缴纳
+以此类推
+给你一个整数 income 表示你的总收入。返回你需要缴纳的税款总额。与标准答案误差不超 10-5 的结果将被视作正确答案。
+*/
+
+class Solution {
+public:
+    double calculateTax(vector<vector<int>>& brackets, int income) {
+        double totalTax = 0;
+        int lower = 0;
+        for (auto& bracket : brackets) {
+            int upper = bracket[0], percent = bracket[1];
+            int tax = (min(income, upper) - lower) * percent;
+            totalTax += tax;
+            if (income <= upper) {
+                break;
+            }
+            lower = upper;
+        }
+        return (double)totalTax / 100.0;
+    }
+};
