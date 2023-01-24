d73cf2d574071be1fedf4d1c2313ebd4d050dc6b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-24 12:14:53 +0800

    1828. 统计一个圆中点的数目

---
+++ Ahao.LeetCode/1800-1899/demo1828/JS1828.js
@@ -0,0 +1,14 @@
+﻿var countPoints = function (points, queries) {
+    const m = points.length, n = queries.length;
+    const ans = new Array(n).fill(0);
+    for (let i = 0; i < n; ++i) {
+        let cx = queries[i][0], cy = queries[i][1], cr = queries[i][2];
+        for (let j = 0; j < m; ++j) {
+            let px = points[j][0], py = points[j][1];
+            if ((cx - px) * (cx - px) + (cy - py) * (cy - py) <= cr * cr) {
+                ++ans[i];
+            }
+        }
+    }
+    return ans;
+};
