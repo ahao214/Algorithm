6b2d096025d8f4cc9f0200fbe7a7d4959b9cbec4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-26 11:50:08 +0800

    1663. 具有给定数值的最小字符串

--- Ahao.LeetCodeCPP/1663. 具有给定数值的最小字符串.cpp
+++ Ahao.LeetCodeCPP/1663. 具有给定数值的最小字符串.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 /*
 1663. 具有给定数值的最小字符串
+
 小写字符 的 数值 是它在字母表中的位置（从 1 开始），因此 a 的数值为 1 ，b 的数值为 2 ，c 的数值为 3 ，以此类推。
 
 字符串由若干小写字符组成，字符串的数值 为各字符的数值之和。例如，字符串 "abe" 的数值等于 1 + 2 + 5 = 8 。
@@ -36,5 +37,20 @@ public:
 			}
 		}
 		return res;
+	}
+};
+
+
+
+class Solution {
+public:
+	string getSmallestString(int n, int k) {
+		string ans;
+		for (int i = 1; i <= n; i++) {
+			int lower = max(1, k - (n - i) * 26);
+			k -= lower;
+			ans.push_back('a' + lower - 1);
+		}
+		return ans;
 	}
 };
