1a168d54b9c2c3983034f64062ae81ee3eb3c4a2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-26 11:51:54 +0800

    1663. 具有给定数值的最小字符串

---
+++ Ahao.LeetCode/1600-1699/demo1663/Class1663.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1663
+{
+    public class Class1663
+    {
+        public string GetSmallestString(int n, int k)
+        {
+            StringBuilder ans = new StringBuilder();
+            for (int i = 1; i <= n; i++)
+            {
+                int lower = Math.Max(1, k - (n - i) * 26);
+                k -= lower;
+                ans.Append((char)('a' + lower - 1));
+            }
+            return ans.ToString();
+        }
+
+
+    }
+}
