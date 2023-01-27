25ca86af1148c90ecae903733bf3cf93d74b52d7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-27 14:38:00 +0800

    2309. 兼具大小写的最好英文字母

--- Ahao.LeetCodeCPP/2309. 兼具大小写的最好英文字母.cpp
+++ Ahao.LeetCodeCPP/2309. 兼具大小写的最好英文字母.cpp
@@ -28,3 +28,26 @@ public:
         return "";
     }
 };
+
+
+//Œª‘ÀÀ„
+class Solution {
+public:
+    string greatestLetter(string s) {
+        int lower = 0, upper = 0;
+        for (auto c : s) {
+            if (islower(c)) {
+                lower |= 1 << (c - 'a');
+            }
+            else {
+                upper |= 1 << (c - 'A');
+            }
+        }
+        for (int i = 25; i >= 0; i--) {
+            if (lower & upper & (1 << i)) {
+                return string(1, 'A' + i);
+            }
+        }
+        return "";
+    }
+};
