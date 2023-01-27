4fbfb2e5767f19836f201de493552b7a35e4f52d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-27 14:39:29 +0800

    2309. 兼具大小写的最好英文字母

---
+++ Ahao.LeetCode/2300-2399/demo2309/Class2309.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2309
+{
+    public class Class2309
+    {
+        /// <summary>
+        /// 哈希表
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string GreatestLetter(string s)
+        {
+            ISet<char> ht = new HashSet<char>();
+            foreach (char c in s)
+            {
+                ht.Add(c);
+            }
+            for (int i = 25; i >= 0; i--)
+            {
+                if (ht.Contains((char)('a' + i)) && ht.Contains((char)('A' + i)))
+                {
+                    return ((char)('A' + i)).ToString();
+                }
+            }
+            return "";
+        }
+
+    }
+}
