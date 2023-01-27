f06cb387aaa63bf45d93f7ca53ee726c1b6e46ea
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-27 14:39:57 +0800

    2309. 兼具大小写的最好英文字母

---
+++ Ahao.LeetCode/2300-2399/demo2309/JS2309.js
@@ -0,0 +1,16 @@
+﻿//哈希表
+var greatestLetter = function (s) {
+    const ht = new Set();
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        ht.add(c);
+    }
+    for (let i = 25; i >= 0; i--) {
+        if (ht.has(String.fromCharCode('a'.charCodeAt() + i)) && ht.has(String.fromCharCode('A'.charCodeAt() + i))) {
+            return String.fromCharCode('A'.charCodeAt() + i);
+        }
+    }
+    return "";
+};
+
+
