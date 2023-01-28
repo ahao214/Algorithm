05873e7c8b0f11f2ca01e8460a03fc7e8bce8728
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-28 09:56:30 +0800

    1664. 生成平衡数组的方案数

--- Ahao.LeetCodeCPP/1664. 生成平衡数组的方案数.cpp
+++ Ahao.LeetCodeCPP/1664. 生成平衡数组的方案数.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 /*
 1664. 生成平衡数组的方案数
+
 给你一个整数数组 nums 。你需要选择 恰好 一个下标（下标从 0 开始）并删除对应的元素。请注意剩下元素的下标可能会因为删除操作而发生改变。
 
 比方说，如果 nums = [6,1,7,4,1] ，那么：
@@ -44,4 +45,42 @@ public:
 		}
 		return res;
 	}
+};
+
+
+
+//动态规划
+class Solution {
+public:
+    int waysToMakeFair(vector<int>& nums) {
+        int odd1 = 0, even1 = 0;
+        int odd2 = 0, even2 = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i & 1) {
+                odd2 += nums[i];
+            }
+            else {
+                even2 += nums[i];
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i & 1) {
+                odd2 -= nums[i];
+            }
+            else {
+                even2 -= nums[i];
+            }
+            if (odd1 + even2 == odd2 + even1) {
+                ++res;
+            }
+            if (i & 1) {
+                odd1 += nums[i];
+            }
+            else {
+                even1 += nums[i];
+            }
+        }
+        return res;
+    }
 };
