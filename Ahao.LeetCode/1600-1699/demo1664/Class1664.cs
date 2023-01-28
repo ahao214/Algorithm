8508efa65083986990280fcbaa5b84daf99368c4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-28 10:08:03 +0800

    1664. 生成平衡数组的方案数

---
+++ Ahao.LeetCode/1600-1699/demo1664/Class1664.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1664
+{
+    public class Class1664
+    {
+
+        public int WaysToMakeFair(int[] nums)
+        {
+            int odd1 = 0, even1 = 0;
+            int odd2 = 0, even2 = 0;
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if ((i & 1) != 0)
+                {
+                    odd2 += nums[i];
+                }
+                else
+                {
+                    even2 += nums[i];
+                }
+            }
+            int res = 0;
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if ((i & 1) != 0)
+                {
+                    odd2 -= nums[i];
+                }
+                else
+                {
+                    even2 -= nums[i];
+                }
+                if (odd1 + even2 == odd2 + even1)
+                {
+                    ++res;
+                }
+                if ((i & 1) != 0)
+                {
+                    odd1 += nums[i];
+                }
+                else
+                {
+                    even1 += nums[i];
+                }
+            }
+            return res;
+        }
+    }
+}
