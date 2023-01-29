90416dd0bdf47dbd24d35d8def2f6ef736e54c9c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-29 09:47:48 +0800

    2315. 统计星号

---
+++ Ahao.LeetCode/2300-2399/demo2315/JS2315.js
@@ -0,0 +1,13 @@
+﻿var countAsterisks = function (s) {
+    let valid = true;
+    let res = 0;
+    for (let i = 0; i < s.length; i++) {
+        let c = s[i];
+        if (c === '|') {
+            valid = !valid;
+        } else if (c === '*' && valid) {
+            res++;
+        }
+    }
+    return res;
+};
