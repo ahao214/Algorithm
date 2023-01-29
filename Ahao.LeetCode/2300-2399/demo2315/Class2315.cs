37a39a918de02bfce058395620fed85a3ec67aa0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-29 09:47:19 +0800

    2315. 统计星号

---
+++ Ahao.LeetCode/2300-2399/demo2315/Class2315.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2315
+{
+    public class Class2315
+    {
+        public int CountAsterisks(string s)
+        {
+            bool valid = true;
+            int res = 0;
+            foreach (char c in s)
+            {
+                if (c == '|')
+                {
+                    valid = !valid;
+                }
+                else if (c == '*' && valid)
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+}
