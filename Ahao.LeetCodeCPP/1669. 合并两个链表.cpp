36925d245e55e672916a0a8d7b8acf3e0d171df0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-30 09:42:34 +0800

    1669. 合并两个链表

--- Ahao.LeetCodeCPP/1669. 合并两个链表.cpp
+++ Ahao.LeetCodeCPP/1669. 合并两个链表.cpp
@@ -16,6 +16,7 @@ struct ListNode {
 
 /*
 1669. 合并两个链表
+
 给你两个链表 list1 和 list2 ，它们包含的元素分别为 n 个和 m 个。
 
 请你将 list1 中下标从 a 到 b 的全部节点都删除，并将list2 接在被删除节点的位置。
@@ -34,3 +35,25 @@ public:
 		return list1;
 	}
 };
+
+
+class Solution {
+public:
+	ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+		ListNode* preA = list1;
+		for (int i = 0; i < a - 1; i++) {
+			preA = preA->next;
+		}
+		ListNode* preB = preA;
+		for (int i = 0; i < b - a + 2; i++) {
+			preB = preB->next;
+		}
+		preA->next = list2;
+		while (list2->next != nullptr) {
+			list2 = list2->next;
+		}
+		list2->next = preB;
+		return list1;
+	}
+};
+
