1288ec69e27a2207dffcf68ad7fa5ef642cc301d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-31 09:40:51 +0800

    2319. 判断矩阵是否是一个 X 矩阵

---
+++ Ahao.LeetCodeCPP/2319. 判断矩阵是否是一个 X 矩阵.cpp
@@ -0,0 +1,36 @@
+#include<vector>
+
+using namespace std;
+
+
+/*
+2319. 判断矩阵是否是一个 X 矩阵
+
+如果一个正方形矩阵满足下述 全部 条件，则称之为一个 X 矩阵 ：
+
+矩阵对角线上的所有元素都 不是 0
+矩阵中所有其他元素都是 0
+给你一个大小为 n x n 的二维整数数组 grid ，表示一个正方形矩阵。如果 grid 是一个 X 矩阵 ，返回 true ；否则，返回 false 。
+*/
+
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == j || (i + j) == (n - 1)) {
+                    if (grid[i][j] == 0) {
+                        return false;
+                    }
+                }
+                else if (grid[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+
