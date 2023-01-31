9e13139cba1f7a7f83fdcfba4c0ee7c1b5faff4e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-31 09:43:52 +0800

    2319. 判断矩阵是否是一个 X 矩阵

---
+++ Ahao.LeetCode/2300-2399/demo2319/Class2319.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2319
+{
+    public class Class2319
+    {
+
+        public bool CheckXMatrix(int[][] grid)
+        {
+            int n = grid.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (i == j || (i + j) == (n - 1))
+                    {
+                        if (grid[i][j] == 0)
+                        {
+                            return false;
+                        }
+                    }
+                    else if (grid[i][j] != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+}
