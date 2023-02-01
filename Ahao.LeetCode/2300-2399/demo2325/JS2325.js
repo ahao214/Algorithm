029be8a306539ef4a5d57f5210cd4188aa285641
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-01 09:43:33 +0800

    2325. 解密消息

---
+++ Ahao.LeetCode/2300-2399/demo2325/JS2325.js
@@ -0,0 +1,23 @@
+﻿var decodeMessage = function (key, message) {
+    let cur = 'a';
+    const rules = new Map();
+
+    for (let i = 0; i < key.length; ++i) {
+        const c = key[i];
+        if (c !== ' ' && !rules.has(c)) {
+            rules.set(c, cur);
+            cur = String.fromCharCode(cur.charCodeAt() + 1);
+        }
+    }
+
+    let ret = '';
+    for (let i = 0; i < message.length; ++i) {
+        let c = message[i];
+        if (c !== ' ') {
+            c = rules.get(c);
+        }
+        ret += c;
+    }
+
+    return ret;
+};
