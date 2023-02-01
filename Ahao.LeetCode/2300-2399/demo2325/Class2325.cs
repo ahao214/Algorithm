b6bcd761885a0aaa8630ff907aa498148ba4836d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-01 09:46:31 +0800

    2325. 解密消息

--- Ahao.LeetCode/2300-2399/demo2325/Class2325.cs
+++ Ahao.LeetCode/2300-2399/demo2325/Class2325.cs
@@ -39,5 +39,30 @@ namespace Ahao.LeetCode._2300_2399.demo2325
             return sb.ToString();
         }
 
+
+        public string DecodeMessage2(string key, string message)
+        {
+            string res = "";
+            Dictionary<char, char> dic = new Dictionary<char, char>();
+
+            int cnt = 0;
+            for (int i = 0; i < key.Length && cnt < 26; i++)
+            {
+                if (!dic.ContainsKey(key[i]))
+                {
+                    if (key[i] == ' ') continue;
+                    dic.Add(key[i], (char)('a' + cnt++));//字符映射
+                }
+            }
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i] == ' ') res += " ";
+                else res += dic[message[i]];//叠加字符       
+            }
+            return res;
+        }
+
+
     }
 }
