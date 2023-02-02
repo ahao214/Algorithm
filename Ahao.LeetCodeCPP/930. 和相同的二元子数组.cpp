d2c738e079ea98e2a98a405995754cd39550121f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-02 14:17:51 +0800

    930. 和相同的二元子数组

--- Ahao.LeetCodeCPP/930. 和相同的二元子数组.cpp
+++ Ahao.LeetCodeCPP/930. 和相同的二元子数组.cpp
@@ -1,10 +1,12 @@
 #include<vector>
+#include <unordered_map>
 
 using namespace std;
 
 
 /*
 930. 和相同的二元子数组
+
 给你一个二元数组nums ，和一个整数 goal ，请你统计并返回有多少个和为 goal的
 非空子数组。
 
@@ -23,6 +25,29 @@ public:
 		{
 			if (sum[i] >= goal) res += f[sum[i] - goal];
 			f[sum[i]]++;
+		}
+		return res;
+	}
+};
+
+
+
+
+/*
+930. 和相同的二元子数组
+*/
+class Solution {
+public:
+	int numSubarraysWithSum(vector<int>& nums, int goal) {
+		int n = nums.size();
+		unordered_map<int, int> cnt;
+		cnt[0]++;
+		int res = 0;
+		for (int i = 1, s = 0; i <= n; i++)
+		{
+			s += nums[i - 1];
+			res += cnt[s - goal];
+			cnt[s] ++;
 		}
 		return res;
 	}
