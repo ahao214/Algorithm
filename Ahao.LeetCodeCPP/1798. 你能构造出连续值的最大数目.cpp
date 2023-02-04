d65edd298963b4435c6652c51a5740c201ddb0b6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-04 11:03:34 +0800

    1798. 你能构造出连续值的最大数目

--- Ahao.LeetCodeCPP/1798. 你能构造出连续值的最大数目.cpp
+++ Ahao.LeetCodeCPP/1798. 你能构造出连续值的最大数目.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 /*
 1798. 你能构造出连续值的最大数目
+
 给你一个长度为 n 的整数数组 coins ，它代表你拥有的 n 个硬币。第 i 个硬币的值为 coins[i] 。如果你从这些硬币中选出一部分硬币，它们的和为 x ，那么称，你可以 构造 出 x 。
 
 请返回从 0 开始（包括 0 ），你最多能 构造 出多少个连续整数。
@@ -26,5 +27,23 @@ public:
 			sum += x;
 		}
 		return sum + 1;
+	}
+};
+
+
+
+//贪心
+class Solution {
+public:
+	int getMaximumConsecutive(vector<int>& coins) {
+		int res = 1;
+		sort(coins.begin(), coins.end());
+		for (auto& i : coins) {
+			if (i > res) {
+				break;
+			}
+			res += i;
+		}
+		return res;
 	}
 };
