8e1874f4ec574d04a30e97dd6d9809522ebbbaaf
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-04 11:05:30 +0800

    1798. 你能构造出连续值的最大数目

---
+++ Ahao.LeetCode/1700-1799/demo1798/JS1798.js
@@ -0,0 +1,11 @@
+﻿var getMaximumConsecutive = function (coins) {
+    let res = 1;
+    coins.sort((a, b) => a - b);
+    for (const i of coins) {
+        if (i > res) {
+            break;
+        }
+        res += i;
+    }
+    return res;
+};
