dc13f6f123cc1050420dcc8480f7cf47db2950fd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-04 11:05:03 +0800

    1798. 你能构造出连续值的最大数目

---
+++ Ahao.LeetCode/1700-1799/demo1798/Class1798.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1798
+{
+    public class Class1798
+    {
+        public int GetMaximumConsecutive(int[] coins)
+        {
+            int res = 1;
+            Array.Sort(coins);
+            foreach (int i in coins)
+            {
+                if (i > res)
+                {
+                    break;
+                }
+                res += i;
+            }
+            return res;
+        }
+    }
+}
