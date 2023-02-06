88ca9cef988cdb1cff53cc1a944a680edd302ccd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-06 09:51:52 +0800

    2331. 计算布尔二叉树的值

--- Ahao.LeetCode/2300-2399/demo2331/JS2331.js
+++ Ahao.LeetCode/2300-2399/demo2331/JS2331.js
@@ -8,3 +8,10 @@
         return evaluateTree(root.left) && evaluateTree(root.right);
     }
 };
+
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
