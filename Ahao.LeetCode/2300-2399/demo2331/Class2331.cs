1ec021afd2307ba4ecf96a7a532c3aabaea13bf6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-06 09:49:47 +0800

    2331. 计算布尔二叉树的值

---
+++ Ahao.LeetCode/2300-2399/demo2331/Class2331.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2331
+{
+    public class Class2331
+    {
+        public bool EvaluateTree(TreeNode root)
+        {
+            if (root.left == null)
+            {
+                return root.val == 1;
+            }
+            if (root.val == 2)
+            {
+                return EvaluateTree(root.left) || EvaluateTree(root.right);
+            }
+            else
+            {
+                return EvaluateTree(root.left) && EvaluateTree(root.right);
+            }
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
