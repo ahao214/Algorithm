51519f47f9248be61902d423996e7c9202020643
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-09 15:13:20 +0800

    9. 回文数

--- Ahao.LeetCode/0001-0099/demo09/Class9.cs
+++ Ahao.LeetCode/0001-0099/demo09/Class9.cs
@@ -6,12 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Simple.demo9
 {
-    /*
-     9. 回文数
-    给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 
-    回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
-    例如，121是回文，而 123 不是。
-     */
     public class Class9
     {
         public bool IsPalindrome(int x)
