781dec4a338b9c0414583ba8bf25dab440229998
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-09 14:00:39 +0800

    4. 寻找两个正序数组的中位数

--- Ahao.LeetCode/0001-0099/demo04/Class4.cs
+++ Ahao.LeetCode/0001-0099/demo04/Class4.cs
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Hard.demo4
 {
-    /// <summary>
-    /// 4. 寻找两个正序数组的中位数
-    /// 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
-    /// </summary>
+
     public class Class4
     {
         //从nums1中取出k1个数字，从nums2中取出k2个数字
