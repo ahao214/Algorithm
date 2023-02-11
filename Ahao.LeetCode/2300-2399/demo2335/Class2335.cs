98bdfa2bda5e8895f49a833d7c9b3bff4d5678ee
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-11 13:19:57 +0800

    2335. 装满杯子需要的最短总时长

---
+++ Ahao.LeetCode/2300-2399/demo2335/Class2335.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2335
+{
+    public class Class2335
+    {
+        public int FillCups(int[] amount)
+        {
+            Array.Sort(amount);
+            if (amount[2] > amount[1] + amount[0])
+            {
+                return amount[2];
+            }
+            return (amount[0] + amount[1] + amount[2] + 1) / 2;
+        }
+
+    }
+}
