abc53377fd0202d641d2cfe710dfe3ed940d2bc7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-12 10:16:26 +0800

    14. 最长公共前缀

--- Ahao.LeetCode/0001-0099/demo14/Class14.cs
+++ Ahao.LeetCode/0001-0099/demo14/Class14.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Simple.demo14
 {
-    /*
-     14. 最长公共前缀
-    编写一个函数来查找字符串数组中的最长公共前缀
-    如果不存在公共前缀，返回空字符串 ""
-     */
     public class Class14
     {
         public string LongestCommonPrefix(string[] strs)
