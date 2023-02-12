521003694bf0b26ad05945c16379cacca793d5e1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-12 10:18:53 +0800

    16. 最接近的三数之和

--- Ahao.LeetCode/0001-0099/demo16/Class16.cs
+++ Ahao.LeetCode/0001-0099/demo16/Class16.cs
@@ -6,12 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Medium.demo16
 {
-    /*
-     16. 最接近的三数之和
-     给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
-     返回这三个数的和。
-     假定每组输入只存在恰好一个解。
-     */
     public class Class16
     {
         public int ThreeSumClosest(int[] nums, int target)
