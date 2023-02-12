ac17ad44cd69b5e062361519a94b2e1fadf1459c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-12 10:17:21 +0800

    15. 三数之和

--- Ahao.LeetCode/0001-0099/demo15/Class15.cs
+++ Ahao.LeetCode/0001-0099/demo15/Class15.cs
@@ -6,12 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Medium.demo15
 {
-    /*
-    15. 三数之和
-给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
-
-注意：答案中不可以包含重复的三元组。
-     */
     public class Class15
     {
         public IList<IList<int>> ThreeSum(int[] nums)
