30158ddd36b95f19f5c6e849cb18dbffa2965c87
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-13 12:35:30 +0800

    1234. 替换子串得到平衡字符串

---
+++ Ahao.LeetCode/1200-1299/demo1234/Class1234.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1200_1299.demo1234
+{
+    public class Class1234
+    {
+
+        public int BalancedString(string s)
+        {
+            int[] cnt = new int[26];
+            foreach (char c in s)
+            {
+                cnt[Idx(c)]++;
+            }
+
+            int partial = s.Length / 4;
+            int res = s.Length;
+
+            if (Check(cnt, partial))
+            {
+                return 0;
+            }
+            for (int l = 0, r = 0; l < s.Length; l++)
+            {
+                while (r < s.Length && !Check(cnt, partial))
+                {
+                    cnt[Idx(s[r])]--;
+                    r++;
+                }
+                if (!Check(cnt, partial))
+                {
+                    break;
+                }
+                res = Math.Min(res, r - l);
+                cnt[Idx(s[l])]++;
+            }
+            return res;
+        }
+
+        public int Idx(char c)
+        {
+            return c - 'A';
+        }
+
+        public bool Check(int[] cnt, int partial)
+        {
+            if (cnt[Idx('Q')] > partial || cnt[Idx('W')] > partial || cnt[Idx('E')] > partial || cnt[Idx('R')] > partial)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
