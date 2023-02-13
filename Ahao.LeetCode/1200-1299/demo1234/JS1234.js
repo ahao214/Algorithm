dc4baacfe25ed39d6cc58c0c60016d3695d7e4fd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-13 12:36:07 +0800

    1234. 替换子串得到平衡字符串

---
+++ Ahao.LeetCode/1200-1299/demo1234/JS1234.js
@@ -0,0 +1,37 @@
+﻿var balancedString = function(s) {
+    const cnt = new Array(26).fill(0);
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        cnt[idx(c)]++;
+    }
+
+    const partial = s.length / 4;
+    let res = s.length;
+
+    if (check(cnt, partial)) {
+        return 0;
+    }
+    for (let l = 0, r = 0; l < s.length; l++) {
+        while (r < s.length && !check(cnt, partial)) {
+            cnt[idx(s[r])]--;
+            r++;
+        }
+        if (!check(cnt, partial)) {
+            break;
+        }
+        res = Math.min(res, r - l);
+        cnt[idx(s[l])]++;
+    }
+    return res;
+}
+
+const idx = (c) => {
+    return c.charCodeAt() - 'A'.charCodeAt();
+}
+
+const check = (cnt, partial) => {
+    if (cnt[idx('Q')] > partial || cnt[idx('W')] > partial || cnt[idx('E')] > partial || cnt[idx('R')] > partial) {
+        return false;
+    }
+    return true;
+};
