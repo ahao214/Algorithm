cf99efc0eb290bb6322a3990fdaf0bee50f23b24
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-14 12:34:34 +0800

    1124. 表现良好的最长时间段

--- Ahao.LeetCode/1100-1199/demo1124/Class1124.cs
+++ Ahao.LeetCode/1100-1199/demo1124/Class1124.cs
@@ -39,6 +39,37 @@ namespace Ahao.LeetCode._1100_1199.demo1124
             return res;
         }
 
+        /// <summary>
+        /// 哈希表
+        /// </summary>
+        /// <param name="hours"></param>
+        /// <returns></returns>
+        public int LongestWPI2(int[] hours)
+        {
+            int n = hours.Length;
+            IDictionary<int, int> dictionary = new Dictionary<int, int>();
+            int s = 0, res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                s += hours[i] > 8 ? 1 : -1;
+                if (s > 0)
+                {
+                    res = Math.Max(res, i + 1);
+                }
+                else
+                {
+                    if (dictionary.ContainsKey(s - 1))
+                    {
+                        res = Math.Max(res, i - dictionary[s - 1]);
+                    }
+                }
+                if (!dictionary.ContainsKey(s))
+                {
+                    dictionary.Add(s, i);
+                }
+            }
+            return res;
+        }
 
     }
 }
