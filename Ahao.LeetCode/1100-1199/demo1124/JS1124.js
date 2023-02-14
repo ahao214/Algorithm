5294ed1fc9e67ab30188cbe9219fdd9d5cf3c50c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-14 12:34:05 +0800

    1124. 表现良好的最长时间段

--- Ahao.LeetCode/1100-1199/demo1124/JS1124.js
+++ Ahao.LeetCode/1100-1199/demo1124/JS1124.js
@@ -18,3 +18,25 @@ var longestWPI = function (hours) {
     }
     return res;
 };
+
+
+//哈希表
+var longestWPI = function (hours) {
+    const n = hours.length;
+    const map = new Map();
+    let s = 0, res = 0;
+    for (let i = 0; i < n; i++) {
+        s += hours[i] > 8 ? 1 : -1;
+        if (s > 0) {
+            res = Math.max(res, i + 1);
+        } else {
+            if (map.has(s - 1)) {
+                res = Math.max(res, i - map.get(s - 1));
+            }
+        }
+        if (!map.has(s)) {
+            map.set(s, i);
+        }
+    }
+    return res;
+};
