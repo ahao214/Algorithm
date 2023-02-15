ea749aa98f35dd80d721214f25e7dc9370257e2a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-15 12:49:30 +0800

    1250. 检查「好数组」

--- Ahao.LeetCode/1200-1299/demo1250/Class1250.cs
+++ Ahao.LeetCode/1200-1299/demo1250/Class1250.cs
@@ -8,6 +8,29 @@ namespace Ahao.LeetCode._1200_1299.demo1250
 {
     public class Class1250
     {
+        public bool IsGoodArray(int[] nums)
+        {
+            int divisor = nums[0];
+            foreach (int num in nums)
+            {
+                divisor = GCD(divisor, num);
+                if (divisor == 1)
+                {
+                    break;
+                }
+            }
+            return divisor == 1;
+        }
 
+        public int GCD(int num1, int num2)
+        {
+            while (num2 != 0)
+            {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp % num2;
+            }
+            return num1;
+        }
     }
 }
