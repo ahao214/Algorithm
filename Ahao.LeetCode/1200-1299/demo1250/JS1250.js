f3c0e6c5374e06e04a1c2c02cf07abb5239402cd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-15 12:49:43 +0800

    1250. 检查「好数组」

--- Ahao.LeetCode/1200-1299/demo1250/JS1250.js
+++ Ahao.LeetCode/1200-1299/demo1250/JS1250.js
@@ -1,1 +1,19 @@
-﻿
+﻿var isGoodArray = function (nums) {
+    let divisor = nums[0];
+    for (const num of nums) {
+        divisor = gcd(divisor, num);
+        if (divisor === 1) {
+            break;
+        }
+    }
+    return divisor === 1;
+}
+
+const gcd = (num1, num2) => {
+    while (num2 !== 0) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp % num2;
+    }
+    return num1;
+};
