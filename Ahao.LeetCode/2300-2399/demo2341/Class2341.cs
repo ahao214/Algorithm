ac19e69a890dc69098c6b8ac3931b867dfe87798
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-16 14:34:02 +0800

    2341. 数组能形成多少数对

---
+++ Ahao.LeetCode/2300-2399/demo2341/Class2341.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2341
+{
+    public class Class2341
+    {
+        public int[] NumberOfPairs(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int pairCount = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dic.ContainsKey(nums[i]))
+                {
+                    dic.Add(nums[i], 1);
+                }
+                else
+                {
+                    pairCount += 1;
+                    dic.Remove(nums[i]);
+                }
+            }
+            return new int[] { pairCount, dic.Count };
+        }
+
+    }
+}
