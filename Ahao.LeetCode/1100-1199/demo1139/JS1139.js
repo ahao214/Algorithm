7e39583527bd3e7e4280b02809321dfa6e8c77d2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-17 00:04:32 +0800

    1139. 最大的以 1 为边界的正方形

---
+++ Ahao.LeetCode/1100-1199/demo1139/JS1139.js
@@ -0,0 +1,20 @@
+﻿var largest1BorderedSquare = function (grid) {
+    const m = grid.length, n = grid[0].length;
+    const left = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+    const up = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+    let maxBorder = 0;
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (grid[i - 1][j - 1] === 1) {
+                left[i][j] = left[i][j - 1] + 1;
+                up[i][j] = up[i - 1][j] + 1;
+                let border = Math.min(left[i][j], up[i][j]);
+                while (left[i - border + 1][j] < border || up[i][j - border + 1] < border) {
+                    border--;
+                }
+                maxBorder = Math.max(maxBorder, border);
+            }
+        }
+    }
+    return maxBorder * maxBorder;
+};
