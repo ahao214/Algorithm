c18d7f2ff5ba1ea6db9fe45b9893197e93f76a0e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-16 14:35:23 +0800

    2341. 数组能形成多少数对

---
+++ Ahao.LeetCode/2300-2399/demo2341/JS2341.js
@@ -0,0 +1,11 @@
+﻿var numberOfPairs = function(nums) {
+    const cnt = new Map();
+    let res = 0;
+    for (const num of nums) {
+        cnt.set(num, !(cnt.get(num) || false));
+        if (!cnt.get(num)) {
+            res++;
+        }
+    }
+    return [res, nums.length - 2 * res];
+};
