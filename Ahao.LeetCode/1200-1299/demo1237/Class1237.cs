53c3da6957eaac3f7fda99c600de26e72c3da946
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-18 11:59:35 +0800

    1237. 找出给定方程的正整数解

--- Ahao.LeetCode/1200-1299/demo1237/Class1237.cs
+++ Ahao.LeetCode/1200-1299/demo1237/Class1237.cs
@@ -96,7 +96,10 @@ namespace Ahao.LeetCode._1200_1299.demo1237
         // Returns f(x, y) for any given positive integers x and y.
         // Note that f(x, y) is increasing with respect to both x and y.
         // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
-        public int F(int x, int y);
+        public int F(int x, int y)
+        {
+            return x + y;
+        }
     }
 
 }
