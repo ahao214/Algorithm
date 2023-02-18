118cacca96d8c6e0ba8ce60ee78062b6d5a82805
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-19 00:07:42 +0800

    1792. 最大平均通过率

---
+++ Ahao.LeetCode/1700-1799/demo1792/Class1792.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1792
+{
+    public class Class1792
+    {
+
+
+    }
+}
