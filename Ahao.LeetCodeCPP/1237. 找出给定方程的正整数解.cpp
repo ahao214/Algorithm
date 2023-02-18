f22122c78df21e8257660430b426ecbdbc7a5263
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-18 11:53:59 +0800

    1237. 找出给定方程的正整数解

--- Ahao.LeetCodeCPP/1237. 找出给定方程的正整数解.cpp
+++ Ahao.LeetCodeCPP/1237. 找出给定方程的正整数解.cpp
@@ -46,8 +46,52 @@ public:
 class CustomFunction {
 public:
 	// Returns f(x, y) for any given positive integers x and y.
-    // Note that f(x, y) is increasing with respect to both x and y.
+	// Note that f(x, y) is increasing with respect to both x and y.
 	// i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
 	int f(int x, int y);
 
+};
+
+
+//二分查找
+class Solution {
+public:
+	vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+		vector<vector<int>> res;
+		for (int x = 1; x <= 1000; x++) {
+			int yleft = 1, yright = 1000;
+			while (yleft <= yright) {
+				int ymiddle = (yleft + yright) / 2;
+				if (customfunction.f(x, ymiddle) == z) {
+					res.push_back({ x, ymiddle });
+					break;
+				}
+				if (customfunction.f(x, ymiddle) > z) {
+					yright = ymiddle - 1;
+				}
+				else {
+					yleft = ymiddle + 1;
+				}
+			}
+		}
+		return res;
+	}
+};
+
+
+//双指针
+class Solution {
+public:
+	vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+		vector<vector<int>> res;
+		for (int x = 1, y = 1000; x <= 1000 && y >= 1; x++) {
+			while (y >= 1 && customfunction.f(x, y) > z) {
+				y--;
+			}
+			if (y >= 1 && customfunction.f(x, y) == z) {
+				res.push_back({ x, y });
+			}
+		}
+		return res;
+	}
 };
