32121406920486ce9ac6dfa4b23422d72256328c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-20 13:28:23 +0800

    2347. 最好的扑克手牌

---
+++ Ahao.LeetCode/2300-2399/demo2347/JS2347.js
@@ -0,0 +1,22 @@
+﻿var bestHand = function (ranks, suits) {
+    const suitsSet = new Set();
+    for (const suit of suits) {
+        suitsSet.add(suit);
+    }
+    if (suitsSet.size === 1) {
+        return "Flush";
+    }
+    const h = new Map();
+    for (const rank of ranks) {
+        h.set(rank, (h.get(rank) || 0) + 1);
+    }
+    if (h.size === 5) {
+        return "High Card";
+    }
+    for (const value of h.values()) {
+        if (value > 2) {
+            return "Three of a Kind";
+        }
+    }
+    return "Pair";
+};
