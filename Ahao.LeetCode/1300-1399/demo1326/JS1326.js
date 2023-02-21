e784c21a5aa2e9787bd48886d2306dce773bf48f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-21 10:12:19 +0800

    1326. 灌溉花园的最少水龙头数目

---
+++ Ahao.LeetCode/1300-1399/demo1326/JS1326.js
@@ -0,0 +1,45 @@
+﻿//动态规划
+var minTaps = function (n, ranges) {
+    const intervals = new Array(n + 1).fill(new Array());
+    for (let i = 0; i <= n; i++) {
+        const start = Math.max(0, i - ranges[i]);
+        const end = Math.min(n, i + ranges[i]);
+        intervals[i] = [start, end];
+    }
+    intervals.sort((a, b) => a[0] - b[0]);
+    const dp = new Array(n + 1).fill(Number.MAX_VALUE);
+    dp[0] = 0;
+    for (const interval of intervals) {
+        let start = interval[0], end = interval[1];
+        if (dp[start] === Number.MAX_VALUE) {
+            return -1;
+        }
+        for (let j = start; j <= end; j++) {
+            dp[j] = Math.min(dp[j], dp[start] + 1);
+        }
+    }
+    return dp[n];
+};
+
+
+//贪心算法
+var minTaps = function (n, ranges) {
+    const rightMost = new Array(n + 1).fill(0).map((_, i) => i);
+    for (let i = 0; i <= n; i++) {
+        const start = Math.max(0, i - ranges[i]);
+        const end = Math.min(n, i + ranges[i]);
+        rightMost[start] = Math.max(rightMost[start], end);
+    }
+    let last = 0, ret = 0, pre = 0;
+    for (let i = 0; i < n; i++) {
+        last = Math.max(last, rightMost[i]);
+        if (i === last) {
+            return -1;
+        }
+        if (i === pre) {
+            ret++;
+            pre = last;
+        }
+    }
+    return ret;
+};
