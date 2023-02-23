fd84f78e64997e50590470c5e55d4eb88463bdfe
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-23 12:52:52 +0800

    1238. 循环码排列

--- Ahao.LeetCode/1200-1299/demo1238/Class1238.cs
+++ Ahao.LeetCode/1200-1299/demo1238/Class1238.cs
@@ -8,6 +8,12 @@ namespace Ahao.LeetCode._1200_1299.demo1238
 {
     public class Class1238
     {
+        /// <summary>
+        /// 归纳法
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="start"></param>
+        /// <returns></returns>
         public IList<int> CircularPermutation(int n, int start)
         {
             IList<int> ret = new List<int>();
