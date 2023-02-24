bba33954522bf4529732f92697c4c6b0d4cea4fc
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-24 13:53:04 +0800

    23. 合并K个升序链表

--- Ahao.LeetCode/0001-0099/demo23/Class23.cs
+++ Ahao.LeetCode/0001-0099/demo23/Class23.cs
@@ -6,12 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Hard.demo23
 {
-    /*
-     23. 合并K个升序链表
-给你一个链表数组，每个链表都已经按升序排列。
-
-请你将所有链表合并到一个升序链表中，返回合并后的链表。
-     */
     public class Class23
     {
         public ListNode MergeKLists(ListNode[] lists)
