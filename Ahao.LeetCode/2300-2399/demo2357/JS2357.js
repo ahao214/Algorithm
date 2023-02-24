6acbfcee8cd3ba7bfb59b1fe2c307a14ac5b9a27
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-24 13:43:39 +0800

    2357. 使数组中所有元素都等于零

---
+++ Ahao.LeetCode/2300-2399/demo2357/JS2357.js
@@ -0,0 +1,32 @@
+﻿var minimumOperations = function (nums) {
+    let ans = 0;
+    nums.sort((a, b) => a - b);
+    const length = nums.length;
+    for (let i = 0; i < length; i++) {
+        if (nums[i] > 0) {
+            subtract(nums, nums[i], i);
+            ans++;
+        }
+    }
+    return ans;
+}
+
+const subtract = (nums, x, startIndex) => {
+    const length = nums.length;
+    for (let i = startIndex; i < length; i++) {
+        nums[i] -= x;
+    }
+};
+
+
+//哈希集合
+var minimumOperations = function (nums) {
+    const set = new Set();
+    for (const num of nums) {
+        if (num > 0) {
+            set.add(num);
+        }
+    }
+    return set.size;
+};
+
