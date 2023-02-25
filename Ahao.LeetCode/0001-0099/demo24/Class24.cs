ea0f05f8df133bbc7e5ddb03b3cd5a18247ff6a8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-25 16:25:52 +0800

    24. 两两交换链表中的节点

--- Ahao.LeetCode/0001-0099/demo24/Class24.cs
+++ Ahao.LeetCode/0001-0099/demo24/Class24.cs
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Medium.demo24
 {
-    /*
-     24. 两两交换链表中的节点
-给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
-     */
+
     public class Class24
     {
         public ListNode SwapPairs(ListNode head)
