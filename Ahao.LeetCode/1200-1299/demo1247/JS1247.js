10c7c0b5054536b16c0a6e1b4d40b1bf95aabb14
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-25 13:02:04 +0800

    1247. 交换字符使得字符串相同

---
+++ Ahao.LeetCode/1200-1299/demo1247/JS1247.js
@@ -0,0 +1,17 @@
+﻿var minimumSwap = function (s1, s2) {
+    let xy = 0, yx = 0;
+    const n = s1.length;
+    for (let i = 0; i < n; i++) {
+        const a = s1[i], b = s2[i];
+        if (a === 'x' && b === 'y') {
+            xy++;
+        }
+        if (a === 'y' && b === 'x') {
+            yx++;
+        }
+    }
+    if ((xy + yx) % 2 === 1) {
+        return -1;
+    }
+    return Math.floor(xy / 2) + Math.floor(yx / 2) + xy % 2 + yx % 2;
+};
