97b82a32ec2445d5458e48ca38625ddb5be32037
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-25 13:01:35 +0800

    1247. 交换字符使得字符串相同

--- Ahao.LeetCode/1200-1299/demo1247/Class1247.cs
+++ Ahao.LeetCode/1200-1299/demo1247/Class1247.cs
@@ -8,6 +8,27 @@ namespace Ahao.LeetCode._1200_1299.demo1247
 {
     public class Class1247
     {
-
+        public int MinimumSwap(string s1, string s2)
+        {
+            int xy = 0, yx = 0;
+            int n = s1.Length;
+            for (int i = 0; i < n; i++)
+            {
+                char a = s1[i], b = s2[i];
+                if (a == 'x' && b == 'y')
+                {
+                    xy++;
+                }
+                if (a == 'y' && b == 'x')
+                {
+                    yx++;
+                }
+            }
+            if ((xy + yx) % 2 == 1)
+            {
+                return -1;
+            }
+            return xy / 2 + yx / 2 + xy % 2 + yx % 2;
+        }
     }
 }
