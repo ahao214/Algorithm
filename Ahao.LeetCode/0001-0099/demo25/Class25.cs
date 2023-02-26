61d1adda7f66cbeb8d2ec84ba140a812845d0e5c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-26 13:02:58 +0800

    25. K 个一组翻转链表

--- Ahao.LeetCode/0001-0099/demo25/Class25.cs
+++ Ahao.LeetCode/0001-0099/demo25/Class25.cs
@@ -6,12 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode.Hard.demo25
 {
-    /*
-     25. K 个一组翻转链表
-    给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
-    k是一个正整数，它的值小于或等于链表的长度。
-    如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
-    */
     public class Class25
     {
         public ListNode ReverseKGroup(ListNode head, int k)
