de11b1318b019250af517387e26458922be9f4dd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-27 13:24:18 +0800

    1144. 递减元素使数组呈锯齿状

---
+++ Ahao.LeetCode/1100-1199/demo1144/JS1144.js
@@ -0,0 +1,18 @@
+﻿var movesToMakeZigzag = function (nums) {
+    return Math.min(help(nums, 0), help(nums, 1));
+}
+
+const help = (nums, pos) => {
+    let res = 0;
+    for (let i = pos; i < nums.length; i += 2) {
+        let a = 0;
+        if (i - 1 >= 0) {
+            a = Math.max(a, nums[i] - nums[i - 1] + 1);
+        }
+        if (i + 1 < nums.length) {
+            a = Math.max(a, nums[i] - nums[i + 1] + 1);
+        }
+        res += a;
+    }
+    return res;
+};
