b2a2a23ff1973a06525d3a2044ece6c13e1bf411
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-28 13:41:38 +0800

    2363. 合并相似的物品

--- Ahao.LeetCode/2300-2399/demo2363/Class2363.cs
+++ Ahao.LeetCode/2300-2399/demo2363/Class2363.cs
@@ -48,5 +48,26 @@ namespace Ahao.LeetCode._2300_2399.demo2363
         }
 
 
+
+        public IList<IList<int>> MergeSimilarItems3(int[][] items1, int[][] items2)
+        {
+            int[] table = new int[1001];
+            int n1 = items1.Length, n2 = items2.Length;
+            for (int i = 0; i < n1; i++)
+                table[items1[i][0]] += items1[i][1];
+            for (int i = 0; i < n2; i++)
+                table[items2[i][0]] += items2[i][1];
+
+            IList<IList<int>> res = new List<IList<int>>();
+            for (int i = 0; i < 1001; i++)
+            {
+                if (table[i] > 0)
+                    res.Add(new List<int>() { i, table[i] });
+            }
+            return res;
+        }
+
+
+
     }
 }
