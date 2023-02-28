7c19c0d1aa05c741e93787bc5ace0e787926e24b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-28 13:39:03 +0800

    2363. 合并相似的物品

---
+++ Ahao.LeetCode/2300-2399/demo2363/JS2363.js
@@ -0,0 +1,19 @@
+﻿var mergeSimilarItems = function (items1, items2) {
+    const map = new Map();
+    for (const v of items1) {
+        map.set(v[0], (map.get(v[0]) || 0) + v[1]);
+    }
+    for (const v of items2) {
+        map.set(v[0], (map.get(v[0]) || 0) + v[1]);
+    }
+
+    const res = [];
+    for (const [k, v] of map.entries()) {
+        const pair = [];
+        pair.push(k);
+        pair.push(v);
+        res.push(pair);
+    }
+    res.sort((a, b) => a[0] - b[0]);
+    return res;
+};
