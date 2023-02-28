733215c385ba3d6fe6cd6aa069bc3cb565af80a7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-02-28 14:49:45 +0800

    337. 打家劫舍 III

--- Ahao.LeetCode/0300-0399/demo337/Class337.cs
+++ Ahao.LeetCode/0300-0399/demo337/Class337.cs
@@ -24,6 +24,29 @@ namespace Ahao.LeetCode._0300_0399.demo337
             return Math.Max(money, Rob(root.left) + Rob(root.right));
         }
 
+
+
+
+        public int rob3(TreeNode root)
+        {
+            int[] res = robAction1(root);
+            return Math.Max(res[0], res[1]);
+        }
+
+        int[] robAction1(TreeNode root)
+        {
+            int[] res = new int[2];
+            if (root == null)
+                return res;
+
+            int[] left = robAction1(root.left);
+            int[] right = robAction1(root.right);
+
+            res[0] = Math.Max(left[0], left[1]) + Math.Max(right[0], right[1]);
+            res[1] = root.val + left[0] + right[0];
+            return res;
+        }
+
     }
 
     public class TreeNode
