b7938800e2b48552f1e37684bf98935f23df6d8f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-01 15:18:20 +0800

    2373. 矩阵中的局部最大值

---
+++ Ahao.LeetCode/2300-2399/demo2373/Class2373.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2373
+{
+    public class Class2373
+    {
+        public int[][] LargestLocal(int[][] grid)
+        {
+            int n = grid.Length;
+            int[][] res = new int[n - 2][];
+            for (int i = 0; i < n - 2; i++)
+            {
+                res[i] = new int[n - 2];
+                for (int j = 0; j < n - 2; j++)
+                {
+                    res[i][j] = GetMaxValue(grid, i, j);
+                }
+            }
+            return res;
+        }
+
+        public int GetMaxValue(int[][] grid, int startX, int startY)
+        {
+            int maxValue = int.MinValue;
+            for (int i = startX; i < startX + 3; i++)
+            {
+                for (int j = startY; j < startY + 3; j++)
+                {
+                    maxValue = Math.Max(maxValue, grid[i][j]);
+                }
+            }
+            return maxValue;
+        }
+    }
+}
