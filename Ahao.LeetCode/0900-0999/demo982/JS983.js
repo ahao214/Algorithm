3bf622e3db77869c870a7ba9a26ad664682ae04e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-04 13:28:30 +0800

    982. 按位与为零的三元组

--- Ahao.LeetCode/0900-0999/demo982/JS983.js
+++ Ahao.LeetCode/0900-0999/demo982/JS983.js
@@ -18,4 +18,21 @@ var countTriplets = function (nums) {
 };
 
 
-//枚举+
+//枚举+子集优化
+var countTriplets = function (nums) {
+    const cnt = new Array(1 << 16).fill(0);
+    for (const x of nums) {
+        for (const y of nums) {
+            ++cnt[x & y];
+        }
+    }
+    let ans = 0;
+    for (let x of nums) {
+        x = x ^ 0xffff;
+        for (let sub = x; sub !== 0; sub = (sub - 1) & x) {
+            ans += cnt[sub];
+        }
+        ans += cnt[0];
+    }
+    return ans;
+};
