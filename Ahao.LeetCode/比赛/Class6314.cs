861d3e7946c7688d00bdd5abe9daae15dbd2b5e5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-04 22:52:10 +0800

    6314. 统计可能的树根数目

---
+++ Ahao.LeetCode/比赛/Class6314.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.比赛
+{
+    /*
+     6314. 统计可能的树根数目 
+
+Alice 有一棵 n 个节点的树，节点编号为 0 到 n - 1 。树用一个长度为 n - 1 的二维整数数组 edges 表示，其中 edges[i] = [ai, bi] ，表示树中节点 ai 和 bi 之间有一条边。
+
+Alice 想要 Bob 找到这棵树的根。她允许 Bob 对这棵树进行若干次 猜测 。每一次猜测，Bob 做如下事情：
+
+选择两个 不相等 的整数 u 和 v ，且树中必须存在边 [u, v] 。
+Bob 猜测树中 u 是 v 的 父节点 。
+Bob 的猜测用二维整数数组 guesses 表示，其中 guesses[j] = [uj, vj] 表示 Bob 猜 uj 是 vj 的父节点。
+
+Alice 非常懒，她不想逐个回答 Bob 的猜测，只告诉 Bob 这些猜测里面 至少 有 k 个猜测的结果为 true 。
+
+给你二维整数数组 edges ，Bob 的所有猜测和整数 k ，请你返回可能成为树根的 节点数目 。如果没有这样的树，则返回 0
+     */
+    public class Class6314
+    {
+        public int RootCount(int[][] edges, int[][] guesses, int k)
+        {
+            int res = 0;
+
+            return res;
+        }
+    }
+}
