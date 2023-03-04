f124ed798523c213a25541863bd0497be51ab53c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-04 13:27:28 +0800

    982. 按位与为零的三元组

--- Ahao.LeetCode/0900-0999/demo982/Class982.cs
+++ Ahao.LeetCode/0900-0999/demo982/Class982.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode._0900_0999.demo982
 {
-    class Class982
+    public class Class982
     {
+        public int CountTriplets(int[] nums)
+        {
+            int res = 0;
+            int n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        if ((nums[i] & nums[j] & nums[k]) == 0)
+                            res++;
+                    }
+                }
+            }
+            return res;
+        }
     }
 }
