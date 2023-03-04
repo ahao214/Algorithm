b7e8eb6fa02d15baebaeb4baba7711e2d91d7ff4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-04 22:35:16 +0800

    6312. 最小和分割

---
+++ Ahao.LeetCode/比赛/Class6312.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.比赛
+{
+    /*
+     6312. 最小和分割 
+
+给你一个正整数 num ，请你将它分割成两个非负整数 num1 和 num2 ，满足：
+
+num1 和 num2 直接连起来，得到 num 各数位的一个排列。
+换句话说，num1 和 num2 中所有数字出现的次数之和等于 num 中所有数字出现的次数。
+num1 和 num2 可以包含前导 0 。
+请你返回 num1 和 num2 可以得到的和的 最小 值。
+
+注意：
+
+num 保证没有前导 0 。
+num1 和 num2 中数位顺序可以与 num 中数位顺序不同。
+
+示例 1：
+
+输入：num = 4325
+输出：59
+解释：我们可以将 4325 分割成 num1 = 24 和 num2 = 35 ，和为 59 ，59 是最小和。
+     */
+    public class Class6312
+    {
+        public int SplitNum(int num)
+        {
+            int res = 0;
+
+
+
+            return res;
+        }
+    }
+}
