f7c42db3bf0efba18843273cb8f01fb6c3cf4ce6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-04 22:39:41 +0800

    6311. 统计染色格子数

---
+++ Ahao.LeetCode/比赛/Class6311.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.比赛
+{
+    /*
+     6311. 统计染色格子数 
+
+有一个无穷大的二维网格图，一开始所有格子都未染色。给你一个正整数 n ，表示你需要执行以下步骤 n 分钟：
+
+第一分钟，将 任一 格子染成蓝色。
+之后的每一分钟，将与蓝色格子相邻的 所有 未染色格子染成蓝色。
+     */
+    public class Class6311
+    {
+        public long ColoredCells(int n)
+        {
+            long res = 0;
+
+
+
+            return res;
+        }
+    }
+}
