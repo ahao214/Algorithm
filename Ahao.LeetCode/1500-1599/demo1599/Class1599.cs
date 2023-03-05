513d48bc0efde5739459600ca58275ca73acee33
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-05 09:18:17 +0800

    1599. 经营摩天轮的最大利润

--- Ahao.LeetCode/1500-1599/demo1599/Class1599.cs
+++ Ahao.LeetCode/1500-1599/demo1599/Class1599.cs
@@ -43,5 +43,60 @@ namespace Ahao.LeetCode._1500_1599.demo1599
             return maxV <= 0 ? -1 : res;
         }
 
+
+
+        public int MinOperationsMaxProfit2(int[] customers, int boardingCost, int runningCost)
+        {
+            int ans = -1;
+            int maxProfit = 0;
+            int totalProfit = 0;
+            int operations = 0;
+            int customersCount = 0;
+            int n = customers.Length;
+            for (int i = 0; i < n; i++)
+            {
+                operations++;
+                customersCount += customers[i];
+                int curCustomers = Math.Min(customersCount, 4);
+                customersCount -= curCustomers;
+                totalProfit += boardingCost * curCustomers - runningCost;
+                if (totalProfit > maxProfit)
+                {
+                    maxProfit = totalProfit;
+                    ans = operations;
+                }
+            }
+            if (customersCount == 0)
+            {
+                return ans;
+            }
+            int profitEachTime = boardingCost * 4 - runningCost;
+            if (profitEachTime <= 0)
+            {
+                return ans;
+            }
+            if (customersCount > 0)
+            {
+                int fullTimes = customersCount / 4;
+                totalProfit += profitEachTime * fullTimes;
+                operations += fullTimes;
+                if (totalProfit > maxProfit)
+                {
+                    maxProfit = totalProfit;
+                    ans = operations;
+                }
+                int remainingCustomers = customersCount % 4;
+                int remainingProfit = boardingCost * remainingCustomers - runningCost;
+                totalProfit += remainingProfit;
+                if (totalProfit > maxProfit)
+                {
+                    maxProfit = totalProfit;
+                    operations++;
+                    ans++;
+                }
+            }
+            return ans;
+        }
+
     }
 }
