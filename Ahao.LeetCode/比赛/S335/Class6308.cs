97b7506d81f73da1512f57ab0bd68245874aab18
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-05 11:51:58 +0800

    6308. 二叉树中的第 K 大层和

--- Ahao.LeetCode/比赛/S335/Class6308.cs
+++ Ahao.LeetCode/比赛/S335/Class6308.cs
@@ -16,15 +16,70 @@ namespace Ahao.LeetCode.比赛.S335
 返回树中第 k 大的层和（不一定不同）。如果树少于 k 层，则返回 -1 。
 
 注意，如果两个节点与根节点的距离相同，则认为它们在同一层。
+
+输入：root = [5,8,9,2,1,3,7,4,6], k = 2
+输出：13
+解释：树中每一层的层和分别是：
+- Level 1: 5
+- Level 2: 8 + 9 = 17
+- Level 3: 2 + 1 + 3 + 7 = 13
+- Level 4: 4 + 6 = 10
+第 2 大的层和等于 13 。
      */
     class Class6308
     {
         public long KthLargestLevelSum(TreeNode root, int k)
         {
-            long res = 0;
+            List<IList<int>> result = new List<IList<int>>();
+            int count = 0;
+            int maxDepth = 0;
+            if (root == null)
+            {
+                return 0;
+            }
+            GetMore(root);
+            ISet<int> st = new HashSet<int>();
+            for (int i = 0;i < result.Count; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < result[i].Count; j++)
+                    sum += result[i][j];
+                st.Add(sum);
+            }
+            
+            int[] arr = st.ToArray();
+            Array.Sort(arr);
+            if (k > arr.Length)
+                return -1;
 
-            return res;
+            for (int i = arr.Length - 1; i >= 0; i--,k--)
+            {
+                if (k == 0)
+                    return arr[i];
+            }
+            return -1;
+
+            void GetMore(TreeNode node)
+            {
+                count++;
+                if (count > maxDepth)
+                {
+                    maxDepth = count;
+                    result.Add(new List<int>());
+                }
+                result[count - 1].Add(node.val);
+                if (node.left != null)
+                {
+                    GetMore(node.left);
+                }
+                if (node.right != null)
+                {
+                    GetMore(node.right);
+                }
+                count--;
+            }
         }
+        
     }
 
     public class TreeNode
