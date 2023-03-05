8224e17127e589e027cbda8826840f7d1886fe05
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-05 11:08:47 +0800

    6308. 二叉树中的第 K 大层和

---
+++ Ahao.LeetCode/比赛/S335/Class6308.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.比赛.S335
+{
+    /*
+     6308. 二叉树中的第 K 大层和 
+
+给你一棵二叉树的根节点 root 和一个正整数 k 。
+
+树中的 层和 是指 同一层 上节点值的总和。
+
+返回树中第 k 大的层和（不一定不同）。如果树少于 k 层，则返回 -1 。
+
+注意，如果两个节点与根节点的距离相同，则认为它们在同一层。
+     */
+    class Class6308
+    {
+        public long KthLargestLevelSum(TreeNode root, int k)
+        {
+            long res = 0;
+
+            return res;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
