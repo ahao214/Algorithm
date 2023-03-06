87c8f1bd67a88789dbcae2b188771248bf44ee0f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-06 13:19:11 +0800

    1653. 使字符串平衡的最少删除次数

---
+++ Ahao.LeetCode/1600-1699/demo1653/Class1653.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1653
+{
+    public class Class1653
+    {
+
+        public int MinimumDeletions(string s)
+        {
+            int leftb = 0, righta = 0;
+            foreach (char c in s)
+            {
+                if (c == 'a')
+                {
+                    righta++;
+                }
+            }
+            int res = righta;
+            foreach (char c in s)
+            {
+                if (c == 'a')
+                {
+                    righta--;
+                }
+                else
+                {
+                    leftb++;
+                }
+                res = Math.Min(res, leftb + righta);
+            }
+            return res;
+        }
+
+    }
+}
