0605c59297c93955936ce4f7e2822160451ecf25
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-06 13:20:24 +0800

    763. 划分字母区间

--- Ahao.LeetCodeCPP/763. 划分字母区间.cpp
+++ Ahao.LeetCodeCPP/763. 划分字母区间.cpp
@@ -1,5 +1,6 @@
 #include<vector>
 #include<string>
+#include <unordered_map>
 
 using namespace std;
 
@@ -8,7 +9,11 @@ using namespace std;
 /*
 763. 划分字母区间
 
-字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
+给你一个字符串 s 。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。
+
+注意，划分结果需要满足：将所有划分结果按顺序连接，得到的字符串仍然是 s 。
+
+返回一个表示每个字符串片段的长度的列表。
 */
 class Solution {
 public:
@@ -27,6 +32,31 @@ public:
 			{
 				res.push_back(end - start + 1);
 				start = end + 1;
+			}
+		}
+		return res;
+	}
+};
+
+
+
+
+
+class Solution {
+public:
+	vector<int> partitionLabels(string s) {
+		unordered_map<char, int> last;
+		for (int i = 0; i < s.size(); i++)
+			last[s[i]] = i;
+		vector<int> res;
+		int start = 0, end = 0;
+		for (int i = 0; i < s.size(); i++)
+		{
+			end = max(end, last[s[i]]);
+			if (i == end)
+			{
+				res.push_back(end - start + 1);
+				start = end = i + 1;
 			}
 		}
 		return res;
