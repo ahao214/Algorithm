6a748e208fda5c4d51f1b24a37f5276acd1f7fef
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-06 13:19:35 +0800

    1653. 使字符串平衡的最少删除次数

---
+++ Ahao.LeetCode/1600-1699/demo1653/JS1653.js
@@ -0,0 +1,19 @@
+﻿var minimumDeletions = function(s) {
+    let leftb = 0, righta = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'a') {
+            righta++;
+        }
+    }
+    let res = righta;
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        if (c === 'a') {
+            righta--;
+        } else {
+            leftb++;
+        }
+        res = Math.min(res, leftb + righta);
+    }
+    return res;
+};
