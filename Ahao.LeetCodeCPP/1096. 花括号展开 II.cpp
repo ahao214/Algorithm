dee7a630d2c9f048129959ff0896f7e3eb638a57
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-07 14:21:23 +0800

    1096. 花括号展开 II

---
+++ Ahao.LeetCodeCPP/1096. 花括号展开 II.cpp
@@ -0,0 +1,93 @@
+﻿#include<vector>
+#include <string>
+#include<set>
+
+using namespace std;
+
+/*
+1096. 花括号展开 II
+
+如果你熟悉 Shell 编程，那么一定了解过花括号展开，它可以用来生成任意字符串。
+
+花括号展开的表达式可以看作一个由 花括号、逗号 和 小写英文字母 组成的字符串，定义下面几条语法规则：
+
+如果只给出单一的元素 x，那么表达式表示的字符串就只有 "x"。R(x) = {x}
+例如，表达式 "a" 表示字符串 "a"。
+而表达式 "w" 就表示字符串 "w"。
+当两个或多个表达式并列，以逗号分隔，我们取这些表达式中元素的并集。R({e_1,e_2,...}) = R(e_1) ∪ R(e_2) ∪ ...
+例如，表达式 "{a,b,c}" 表示字符串 "a","b","c"。
+而表达式 "{{a,b},{b,c}}" 也可以表示字符串 "a","b","c"。
+要是两个或多个表达式相接，中间没有隔开时，我们从这些表达式中各取一个元素依次连接形成字符串。R(e_1 + e_2) = {a + b for (a, b) in R(e_1) × R(e_2)}
+例如，表达式 "{a,b}{c,d}" 表示字符串 "ac","ad","bc","bd"。
+表达式之间允许嵌套，单一元素与表达式的连接也是允许的。
+例如，表达式 "a{b,c,d}" 表示字符串 "ab","ac","ad"​​​​​​。
+例如，表达式 "a{b,c}{d,e}f{g,h}" 可以表示字符串 "abdfg", "abdfh", "abefg", "abefh", "acdfg", "acdfh", "acefg", "acefh"。
+给出表示基于给定语法规则的表达式 expression，返回它所表示的所有字符串组成的有序列表。
+
+假如你希望以「集合」的概念了解此题，也可以通过点击 “显示英文描述” 获取详情。
+*/
+
+
+
+class Solution {
+    string expression;
+    int idx;
+
+    // item -> letter | { expr }
+    set<string> item() {
+        set<string> ret;
+        if (expression[idx] == '{') {
+            idx++;
+            ret = expr();
+        }
+        else {
+            ret = { string(1, expression[idx]) };
+        }
+        idx++;
+        return move(ret);
+    }
+
+    // term -> item | item term
+    set<string> term() {
+        // 初始化空集合，与之后的求解结果求笛卡尔积
+        set<string> ret = { "" };
+        // item 的开头是 { 或小写字母，只有符合时才继续匹配
+        while (idx < expression.size() && (expression[idx] == '{' || isalpha(expression[idx]))) {
+            auto sub = item();
+            set<string> tmp;
+            for (auto& left : ret) {
+                for (auto& right : sub) {
+                    tmp.insert(left + right);
+                }
+            }
+            ret = move(tmp);
+        }
+        return move(ret);
+    }
+
+    // expr -> term | term, expr
+    set<string> expr() {
+        set<string> ret;
+        while (true) {
+            // 与 term() 求解结果求并集
+            ret.merge(term());
+            // 如果匹配到逗号则继续，否则结束匹配
+            if (idx < expression.size() && expression[idx] == ',') {
+                idx++;
+                continue;
+            }
+            else {
+                break;
+            }
+        }
+        return move(ret);
+    }
+
+public:
+    vector<string> braceExpansionII(string expression) {
+        this->expression = expression;
+        this->idx = 0;
+        auto ret = expr();
+        return { ret.begin(), ret.end() };
+    }
+};
