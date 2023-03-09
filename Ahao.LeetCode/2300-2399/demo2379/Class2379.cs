02dcb33561628012f893b4a8117354c595a6b2b5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-09 13:47:59 +0800

    2379. 得到 K 个黑块的最少涂色次数

--- Ahao.LeetCode/2300-2399/demo2379/Class2379.cs
+++ Ahao.LeetCode/2300-2399/demo2379/Class2379.cs
@@ -10,6 +10,26 @@ namespace Ahao.LeetCode._2300_2399.demo2379
     {
         public int MinimumRecolors(string blocks, int k)
         {
+            int l = 0, r = 0, cnt = 0;
+            while (r < k)
+            {
+                cnt += blocks[r] == 'W' ? 1 : 0;
+                r++;
+            }
+            int res = cnt;
+            while (r < blocks.Length)
+            {
+                cnt += blocks[r] == 'W' ? 1 : 0;
+                cnt -= blocks[l] == 'W' ? 1 : 0;
+                res = Math.Min(res, cnt);
+                l++;
+                r++;
+            }
+            return res;
+        }
+
+        public int MinimumRecolors2(string blocks, int k)
+        {
             int res = int.MaxValue;
             int left = 0, right = k;
             while (right <= blocks.Length)
@@ -31,5 +51,11 @@ namespace Ahao.LeetCode._2300_2399.demo2379
                 count += s[i] == 'W' ? 1 : 0;
             return count;
         }
+
+
+
+
+
+
     }
 }
