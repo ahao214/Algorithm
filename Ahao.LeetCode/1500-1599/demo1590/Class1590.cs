932eaa12ca46399359609bfb727df443897d732e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-10 13:28:50 +0800

    1590. 使数组和能被 P 整除

---
+++ Ahao.LeetCode/1500-1599/demo1590/Class1590.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1500_1599.demo1590
+{
+    public class Class1590
+    {
+
+    }
+}
