d8df70ea17851d366495f61d4f38c80763f22250
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-11 13:08:34 +0800

    754. 到达终点数字

--- Ahao.LeetCodeCPP/754. 到达终点数字.cpp
+++ Ahao.LeetCodeCPP/754. 到达终点数字.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+/*
+754. 到达终点数字
+
+在一根无限长的数轴上，你站在0的位置。终点在target的位置。
+
+你可以做一些数量的移动 numMoves :
+
+每次你可以选择向左或向右移动。
+第 i 次移动（从  i == 1 开始，到 i == numMoves ），在选择的方向上走 i 步。
+给定整数 target ，返回 到达目标所需的 最小 移动次数(即最小 numMoves ) 。
+*/
+
 
 class Solution {
 public:
@@ -13,5 +25,20 @@ public:
             target -= k;
         }
         return target % 2 == 0 ? k : k + 1 + k % 2;
+    }
+};
+
+
+class Solution {
+public:
+    int reachNumber(int target) {
+        target = abs(target);
+        int n = 0, sum = 0;
+        while (sum < target || (sum - target) % 2)
+        {
+            n++;
+            sum += n;
+        }
+        return n;
     }
 };
