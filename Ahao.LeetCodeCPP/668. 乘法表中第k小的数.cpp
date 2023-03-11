3cec7b0dd8f8e55b533a71d8f269a60be98290c9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-11 13:10:05 +0800

    668. 乘法表中第k小的数

--- Ahao.LeetCodeCPP/668. 乘法表中第k小的数.cpp
+++ Ahao.LeetCodeCPP/668. 乘法表中第k小的数.cpp
@@ -1,5 +1,17 @@
 using namespace std;
 
+
+/*
+668. 乘法表中第k小的数
+
+几乎每一个人都用 乘法表。但是你能在乘法表中快速找到第 k 小的数字吗？
+
+乘法表是大小为 m x n 的一个整数矩阵，其中 mat[i][j] == i * j（下标从 1 开始）。
+
+给你三个整数 m、n 和 k，请你在大小为 m x n 的乘法表中，找出并返回第 k 小的数字。
+*/
+
+
 class Solution {
 public:
 	int findKthNumber(int m, int n, int k) {
