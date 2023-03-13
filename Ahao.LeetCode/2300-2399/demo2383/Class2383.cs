34cccb81093c504b77d19399deeeeb9bd0b296f8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-13 16:04:33 +0800

    2383. 赢得比赛需要的最少训练时长

---
+++ Ahao.LeetCode/2300-2399/demo2383/Class2383.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2383
+{
+   public class Class2383
+    {
+        public int MinNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience)
+        {
+            int res = 0;
+            int sumEnergy = energy.Sum();
+            if(sumEnergy >= initialEnergy)
+            {
+                res = sumEnergy - initialEnergy + 1;
+            }
+            foreach (var item in experience)
+            {
+                if(item >= initialExperience)
+                {
+                    res += item - initialExperience + 1;
+                    initialExperience = 2 * item + 1;
+                }
+                else
+                {
+                    initialExperience += item;
+                }
+            }
+            return res;
+        }
+    }
+}
