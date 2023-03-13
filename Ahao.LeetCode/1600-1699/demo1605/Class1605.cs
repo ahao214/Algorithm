b23d12504fcbc148782e027a28f40fa35910301e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-14 00:10:42 +0800

    1605. 给定行和列的和求可行矩阵

---
+++ Ahao.LeetCode/1600-1699/demo1605/Class1605.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1605
+{
+    public class Class1605
+    {
+
+        public int[][] RestoreMatrix(int[] rowSum, int[] colSum)
+        {
+            int n = rowSum.Length, m = colSum.Length;
+            int[][] matrix = new int[n][];
+            for (int row = 0; row < n; ++row)
+            {
+                matrix[row] = new int[m];
+            }
+            int i = 0, j = 0;
+            while (i < n && j < m)
+            {
+                int v = Math.Min(rowSum[i], colSum[j]);
+                matrix[i][j] = v;
+                rowSum[i] -= v;
+                colSum[j] -= v;
+                if (rowSum[i] == 0)
+                {
+                    ++i;
+                }
+                if (colSum[j] == 0)
+                {
+                    ++j;
+                }
+            }
+            return matrix;
+        }
+
+    }
+}
