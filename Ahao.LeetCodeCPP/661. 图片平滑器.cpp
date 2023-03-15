56d113364a5553430cfec17e5e74e934454ecec9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-15 22:40:26 +0800

    661. 图片平滑器

--- Ahao.LeetCodeCPP/661. 图片平滑器.cpp
+++ Ahao.LeetCodeCPP/661. 图片平滑器.cpp
@@ -59,3 +59,30 @@ public:
         return ans;
     }
 };
+
+
+
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int n = img.size(), m = img[0].size();
+        vector<vector<int>> res = img;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                int s = 0, c = 0;
+                for (int x = i - 1; x <= i + 1; x++)
+                {
+                    for (int y = j - 1; y <= j + 1; y++)
+                    {
+                        if (x >= 0 && x < n && y >= 0 && y < m)
+                            s += img[x][y], c++;
+                    }
+                }
+                res[i][j] = s / c;
+            }
+        }
+        return res;
+    }
+};
