a1fba0b7b909729a0305255e0f925557929c2d70
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-20 00:19:55 +0800

    1012. 至少有 1 位重复的数字

---
+++ Ahao.LeetCodeCPP/1012. 至少有 1 位重复的数字.cpp
@@ -0,0 +1,43 @@
+#include<vector>
+#include <string>
+
+using namespace std;
+
+/*
+1012. 至少有 1 位重复的数字
+
+给定正整数 n，返回在 [1, n] 范围内具有 至少 1 位 重复数字的正整数的个数。
+*/
+
+
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int f(int mask, const string& sn, int i, bool same) {
+        if (i == sn.size()) {
+            return 1;
+        }
+        if (!same && dp[i][mask] >= 0) {
+            return dp[i][mask];
+        }
+        int res = 0, t = same ? (sn[i] - '0') : 9;
+        for (int k = 0; k <= t; k++) {
+            if (mask & (1 << k)) {
+                continue;
+            }
+            res += f(mask == 0 && k == 0 ? mask : mask | (1 << k), sn, i + 1, same && k == t);
+        }
+        if (!same) {
+            dp[i][mask] = res;
+        }
+        return res;
+    }
+
+    int numDupDigitsAtMostN(int n) {
+        string sn = to_string(n);
+        dp.resize(sn.size(), vector<int>(1 << 10, -1));
+        return n + 1 - f(0, sn, 0, true);
+    }
+};
