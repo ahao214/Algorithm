fc31260a31ca6bf57ad9b2e85499aae3e1ef3470
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-21 00:10:06 +0800

    2469. 温度转换

---
+++ Ahao.LeetCode/2400-2499/demo2469/Class2469.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2469
+{
+    public class Class2469
+    {
+        public double[] ConvertTemperature(double celsius)
+        {
+            double[] res = new double[2];
+            res[0] = celsius + 273.15;
+            res[1] = celsius * 1.80 + 32.00;
+            return res;
+        }
+    }
+}
