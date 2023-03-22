f253084cb7af04795994630cbe1874666ca159fd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-22 21:31:47 +0800

    1626. 无矛盾的最佳球队

--- Ahao.LeetCode/1600-1699/demo1626/Class1626.cs
+++ Ahao.LeetCode/1600-1699/demo1626/Class1626.cs
@@ -8,7 +8,6 @@ namespace Ahao.LeetCode._1600_1699.demo1626
 {
     public class Class1626
     {
-
         public int BestTeamScore(int[] scores, int[] ages)
         {
             int n = scores.Length;
@@ -34,6 +33,57 @@ namespace Ahao.LeetCode._1600_1699.demo1626
             }
             return res;
         }
+
+
+        int maxAge;
+        int[] t;
+        int[][] people;
+
+        public int BestTeamScore2(int[] scores, int[] ages)
+        {
+            maxAge = ages.Max();
+            t = new int[maxAge + 1];
+            int res = 0;
+            int n = scores.Length;
+            people = new int[n][];
+            for (int i = 0; i < n; ++i)
+            {
+                people[i] = new int[] { scores[i], ages[i] };
+            }
+            Array.Sort(people, (a, b) => a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+
+            for (int i = 0; i < n; ++i)
+            {
+                int score = people[i][0], age = people[i][1], cur = score + Query(age);
+                Update(age, cur);
+                res = Math.Max(res, cur);
+            }
+            return res;
+        }
+
+        public int lowbit(int x)
+        {
+            return x & (-x);
+        }
+
+        public void Update(int i, int val)
+        {
+            for (; i <= maxAge; i += lowbit(i))
+            {
+                t[i] = Math.Max(t[i], val);
+            }
+        }
+
+        public int Query(int i)
+        {
+            int ret = 0;
+            for (; i > 0; i -= lowbit(i))
+            {
+                ret = Math.Max(ret, t[i]);
+            }
+            return ret;
+        }
+
 
     }
 }
