cfa343bddd22cae4f5d7e81bb75e8953119c2f97
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-22 21:29:38 +0800

    1626. 无矛盾的最佳球队

--- Ahao.LeetCodeCPP/1626. 无矛盾的最佳球队.cpp
+++ Ahao.LeetCodeCPP/1626. 无矛盾的最佳球队.cpp
@@ -63,3 +63,45 @@ public:
 		return res;
 	}
 };
+
+
+class Solution {
+public:
+	int max_age;
+	vector<int> t;
+	vector<pair<int, int>> people;
+	int lowbit(int x) {
+		return x & (-x);
+	}
+
+	void update(int i, int val) {
+		for (; i <= max_age; i += lowbit(i)) {
+			t[i] = max(t[i], val);
+		}
+	}
+
+	int query(int i) {
+		int ret = 0;
+		for (; i > 0; i -= lowbit(i)) {
+			ret = max(ret, t[i]);
+		}
+		return ret;
+	}
+
+	int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+		max_age = *max_element(ages.begin(), ages.end());
+		t = vector<int>(max_age + 1, 0);
+		int res = 0;
+		for (int i = 0; i < scores.size(); ++i) {
+			people.push_back({ scores[i], ages[i] });
+		}
+		sort(people.begin(), people.end());
+
+		for (int i = 0; i < people.size(); ++i) {
+			int score = people[i].first, age = people[i].second, cur = score + query(age);
+			update(age, cur);
+			res = max(res, cur);
+		}
+		return res;
+	}
+};
