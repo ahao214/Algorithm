df951a3fd2d7a738db0e0afb0dd37c282a9e4e79
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-26 00:28:21 +0800

    2395. 和相等的子数组

---
+++ Ahao.LeetCode/2300-2399/demo2395/Class2395.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2395
+{
+    public class Class2395
+    {
+        public bool FindSubarrays(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (!dic.ContainsKey(nums[i] + nums[i + 1]))
+                {
+                    dic.Add(nums[i] + nums[i + 1], 1);
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
