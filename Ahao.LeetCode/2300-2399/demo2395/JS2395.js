105384fd41011971d6feb9e67c1dbab34730001c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-26 00:29:27 +0800

    2395. 和相等的子数组

---
+++ Ahao.LeetCode/2300-2399/demo2395/JS2395.js
@@ -0,0 +1,12 @@
+﻿var findSubarrays = function (nums) {
+    const n = nums.length;
+    const seen = new Set();
+    for (let i = 0; i < n - 1; ++i) {
+        let sum = nums[i] + nums[i + 1];
+        if (seen.has(sum)) {
+            return true;
+        }
+        seen.add(sum);
+    }
+    return false;
+};
