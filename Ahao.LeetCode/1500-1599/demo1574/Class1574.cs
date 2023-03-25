b8cd9a68f2d81ecb6b73db19b835fa26877f0a48
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-25 20:11:52 +0800

    1574. 删除最短的子数组使剩余数组有序

---
+++ Ahao.LeetCode/1500-1599/demo1574/Class1574.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1500_1599.demo1574
+{
+    public class Class1574
+    {
+        public int FindLengthOfShortestSubarray(int[] arr)
+        {
+            int n = arr.Length, j = n - 1;
+            while (j > 0 && arr[j - 1] <= arr[j])
+            {
+                j--;
+            }
+            if (j == 0)
+            {
+                return 0;
+            }
+            int res = j;
+            for (int i = 0; i < n; i++)
+            {
+                while (j < n && arr[j] < arr[i])
+                {
+                    j++;
+                }
+                res = Math.Min(res, j - i - 1);
+                if (i + 1 < n && arr[i] > arr[i + 1])
+                {
+                    break;
+                }
+            }
+            return res;
+        }
+    }
+}
