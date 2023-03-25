8dca2b66d05ac36b76e63083e2f9634716738128
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-25 20:12:21 +0800

    1574. 删除最短的子数组使剩余数组有序

---
+++ Ahao.LeetCode/1500-1599/demo1574/JS1574.js
@@ -0,0 +1,20 @@
+﻿var findLengthOfShortestSubarray = function(arr) {
+    let n = arr.length, j = n - 1;
+    while (j > 0 && arr[j - 1] <= arr[j]) {
+        j--;
+    }
+    if (j === 0) {
+        return 0;
+    }
+    let res = j;
+    for (let i = 0; i < n; i++) {
+        while (j < n && arr[j] < arr[i]) {
+            j++;
+        }
+        res = Math.min(res, j - i - 1);
+        if (i + 1 < n && arr[i] > arr[i + 1]) {
+            break;
+        }
+    }
+    return res;
+};
