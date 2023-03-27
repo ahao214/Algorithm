1ff2935e779e0d933a90e6f267a57361f3d771e5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-27 18:41:49 +0800

    1638. 统计只差一个字符的子串数目

--- Ahao.LeetCode/1600-1699/demo1638/Class1638.cs
+++ Ahao.LeetCode/1600-1699/demo1638/Class1638.cs
@@ -35,5 +35,45 @@ namespace Ahao.LeetCode._1600_1699.demo1638
             return ans;
         }
 
+
+        public int CountSubstrings2(string s, string t)
+        {
+            int m = s.Length, n = t.Length;
+            int[][] dpl = new int[m + 1][];
+            int[][] dpr = new int[m + 1][];
+            for (int i = 0; i <= m; i++)
+            {
+                dpl[i] = new int[n + 1];
+                dpr[i] = new int[n + 1];
+            }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dpl[i + 1][j + 1] = s[i] == t[j] ? (dpl[i][j] + 1) : 0;
+                }
+            }
+            for (int i = m - 1; i >= 0; i--)
+            {
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    dpr[i][j] = s[i] == t[j] ? (dpr[i + 1][j + 1] + 1) : 0;
+                }
+            }
+            int ans = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (s[i] != t[j])
+                    {
+                        ans += (dpl[i][j] + 1) * (dpr[i + 1][j + 1] + 1);
+                    }
+                }
+            }
+            return ans;
+        }
+
+
     }
 }
---
+++ Ahao.LeetCode/1600-1699/demo1638/JS1638.js
@@ -0,0 +1,44 @@
+﻿var countSubstrings = function (s, t) {
+    const m = s.length, n = t.length;
+    let ans = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            let diff = 0;
+            for (let k = 0; i + k < m && j + k < n; k++) {
+                diff += s[i + k] === t[j + k] ? 0 : 1;
+                if (diff > 1) {
+                    break;
+                } else if (diff === 1) {
+                    ans++;
+                }
+            }
+        }
+    }
+    return ans;
+};
+
+
+var countSubstrings = function (s, t) {
+    const m = s.length, n = t.length;
+    const dpl = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+    const dpr = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            dpl[i + 1][j + 1] = s[i] === t[j] ? (dpl[i][j] + 1) : 0;
+        }
+    }
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            dpr[i][j] = s[i] === t[j] ? (dpr[i + 1][j + 1] + 1) : 0;
+        }
+    }
+    let ans = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (s[i] !== t[j]) {
+                ans += (dpl[i][j] + 1) * (dpr[i + 1][j + 1] + 1);
+            }
+        }
+    }
+    return ans;
+};
