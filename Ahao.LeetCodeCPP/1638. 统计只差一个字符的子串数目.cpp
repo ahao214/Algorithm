2b8f987298f4f1a1e8c1a22ae546355b33f2f0e9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-27 18:38:55 +0800

    1638. 统计只差一个字符的子串数目

--- Ahao.LeetCodeCPP/1638. 统计只差一个字符的子串数目.cpp
+++ Ahao.LeetCodeCPP/1638. 统计只差一个字符的子串数目.cpp
@@ -7,6 +7,14 @@ using namespace std;
 
 /*
 1638. 统计只差一个字符的子串数目
+
+给你两个字符串 s 和 t ，请你找出 s 中的非空子串的数目，这些子串满足替换 一个不同字符 以后，是 t 串的子串。换言之，请你找到 s 和 t 串中 恰好 只有一个字符不同的子字符串对的数目。
+
+比方说， "computer" and "computation" 只有一个字符不同： 'e'/'a' ，所以这一对子字符串会给答案加 1 。
+
+请你返回满足上述条件的不同子字符串对数目。
+
+一个 子字符串 是一个字符串中连续的字符。
 */
 class Solution {
 	int dp1[105][105];
@@ -48,3 +56,28 @@ public:
 		return res;
 	}
 };
+
+
+class Solution {
+public:
+	int countSubstrings(string s, string t) {
+		int m = s.size(), n = t.size();
+		int ans = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				int diff = 0;
+				for (int k = 0; i + k < m && j + k < n; k++) {
+					diff += s[i + k] == t[j + k] ? 0 : 1;
+					if (diff > 1) {
+						break;
+					}
+					else if (diff == 1) {
+						ans++;
+					}
+				}
+			}
+		}
+		return ans;
+	}
+};
+
