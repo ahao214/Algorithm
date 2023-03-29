0106b90f26f6cfb01ee9f9da202509c15346ccb9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-29 18:55:12 +0800

    1641. 统计字典序元音字符串的数目

---
+++ Ahao.LeetCode/1600-1699/demo1641/Class1641.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1641
+{
+    public class Class1641
+    {
+        public int CountVowelStrings(int n)
+        {
+            int[] dp = new int[5];
+            Array.Fill(dp, 1);
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < 5; j++)
+                {
+                    dp[j] += dp[j - 1];
+                }
+            }
+            return dp.Sum();
+        }
+
+    }
+}
