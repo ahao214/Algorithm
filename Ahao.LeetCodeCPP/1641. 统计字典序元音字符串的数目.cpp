48067cba0f3b13ed8f16c8c5f9607212e5597c35
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-29 18:57:44 +0800

    1641. 统计字典序元音字符串的数目

--- Ahao.LeetCodeCPP/1641. 统计字典序元音字符串的数目.cpp
+++ Ahao.LeetCodeCPP/1641. 统计字典序元音字符串的数目.cpp
@@ -1,4 +1,5 @@
 #include<vector>
+#include <numeric>
 
 using namespace std;
 
@@ -6,6 +7,10 @@ using namespace std;
 
 /*
 1641. 统计字典序元音字符串的数目
+
+给你一个整数 n，请返回长度为 n 、仅由元音 (a, e, i, o, u) 组成且按 字典序排列 的字符串数量。
+
+字符串 s 按 字典序排列 需要满足：对于所有有效的 i，s[i] 在字母表中的位置总是与 s[i+1] 相同或在 s[i+1] 之前。
 */
 class Solution {
 	int dp[51][5];
@@ -47,3 +52,27 @@ public:
 };
 
 
+
+
+//组合数字
+class Solution {
+public:
+	int countVowelStrings(int n) {
+		return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+	}
+};
+
+
+//动态规划
+class Solution {
+public:
+	int countVowelStrings(int n) {
+		vector<int> dp(5, 1);
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < 5; j++) {
+				dp[j] += dp[j - 1];
+			}
+		}
+		return accumulate(dp.begin(), dp.end(), 0);
+	}
+};
