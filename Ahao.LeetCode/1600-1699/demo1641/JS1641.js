5e597996f6de6e654908d800d30407e9e4e30b06
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-29 18:55:58 +0800

    1641. 统计字典序元音字符串的数目

---
+++ Ahao.LeetCode/1600-1699/demo1641/JS1641.js
@@ -0,0 +1,16 @@
+﻿var countVowelStrings = function(n) {
+    const dp = new Array(5).fill(1);
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j < 5; j++) {
+            dp[j] += dp[j - 1];
+        }
+    }
+    return _.sum(dp);
+};
+
+
+//组合数字
+var countVowelStrings = function (n) {
+    return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+};
+
