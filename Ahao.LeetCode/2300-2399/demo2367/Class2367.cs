b6e26bcefc47d23b9db68ea65179e4333fdee9d3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-03-31 19:54:35 +0800

    2367. 算术三元组的数目

--- Ahao.LeetCode/2300-2399/demo2367/Class2367.cs
+++ Ahao.LeetCode/2300-2399/demo2367/Class2367.cs
@@ -51,5 +51,35 @@ namespace Ahao.LeetCode._2300_2399.demo2367
             return ans;
         }
 
+
+
+        public int ArithmeticTriplets3(int[] nums, int diff)
+        {
+            int ans = 0;
+            int n = nums.Length;
+            for (int i = 0, j = 1, k = 2; i < n - 2 && j < n - 1 && k < n; i++)
+            {
+                j = Math.Max(j, i + 1);
+                while (j < n - 1 && nums[j] - nums[i] < diff)
+                {
+                    j++;
+                }
+                if (j >= n - 1 || nums[j] - nums[i] > diff)
+                {
+                    continue;
+                }
+                k = Math.Max(k, j + 1);
+                while (k < n && nums[k] - nums[j] < diff)
+                {
+                    k++;
+                }
+                if (k < n && nums[k] - nums[j] == diff)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+
     }
 }
