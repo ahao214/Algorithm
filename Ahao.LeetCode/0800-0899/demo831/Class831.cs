b1af5cd1e9efa8c21827c1b1e08295e1147a17bd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-01 08:02:04 +0800

    831. 隐藏个人信息

---
+++ Ahao.LeetCode/0800-0899/demo831/Class831.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo831
+{
+    public class Class831
+    {
+
+        string[] country = { "", "+*-", "+**-", "+***-" };
+
+        public string MaskPII(string s)
+        {
+            int at = s.IndexOf("@");
+            if (at > 0)
+            {
+                s = s.ToLower();
+                return (s[0] + "*****" + s.Substring(at - 1)).ToLower();
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (char.IsDigit(c))
+                {
+                    sb.Append(c);
+                }
+            }
+            s = sb.ToString();
+            return country[s.Length - 10] + "***-***-" + s.Substring(s.Length - 4);
+        }
+    }
+}
---
+++ Ahao.LeetCode/0800-0899/demo831/JS831.js
@@ -0,0 +1,18 @@
+﻿const country = ["", "+*-", "+**-", "+***-"];
+
+var maskPII = function (s) {
+    const at = s.indexOf("@");
+    if (at > 0) {
+        s = s.toLowerCase();
+        return (s[0] + "*****" + s.substring(at - 1)).toLowerCase();
+    }
+    let sb = "";
+    for (let i = 0; i < s.length; i++) {
+        const c = s.charAt(i);
+        if ('0' <= c && c <= '9') {
+            sb += c;
+        }
+    }
+    s = sb.toString();
+    return country[s.length - 10] + "***-***-" + s.substring(s.length - 4);
+};
