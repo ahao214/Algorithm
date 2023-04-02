7a8464cc4b180609a585892f620416f0e89116e4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-03 00:24:47 +0800

    1053. 交换一次的先前排列

---
+++ Ahao.LeetCode/1000-1099/demo1053/Class1053.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1053
+{
+    public class Class1053
+    {
+        public int[] PrevPermOpt1(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    int j = n - 1;
+                    while (arr[j] >= arr[i] || arr[j] == arr[j - 1])
+                        j--;
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                    break;
+                }
+            }
+            return arr;
+        }
+    }
+}
