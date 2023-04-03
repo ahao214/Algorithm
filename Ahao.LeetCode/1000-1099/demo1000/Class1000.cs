38798260695da87fbd947aa3f30ae9409327f8bf
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-04 00:05:05 +0800

    1000. 合并石头的最低成本

---
+++ Ahao.LeetCode/1000-1099/demo1000/Class1000.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1000
+{
+    public class Class1000
+    {
+
+    }
+}
