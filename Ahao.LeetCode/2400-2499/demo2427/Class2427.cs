ca9e35d180d988836ca0a13e81ef13c5672a89d1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-05 00:20:47 +0800

    2427. 公因子的数目

---
+++ Ahao.LeetCode/2400-2499/demo2427/Class2427.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2427
+{
+    public class Class2427
+    {
+        public int CommonFactors(int a, int b)
+        {
+            int res = 0;
+            List<int> lst = new List<int>();
+            for(int i = 1;i<= a; i++)
+            {
+                if (a % i == 0)
+                    lst.Add(i);
+            }
+
+            for(int i = 1;i <= b; i++)
+            {
+                if (b % i == 0 && lst.Contains(i))
+                    res++;
+            }
+            return res;
+        }
+    }
+}
