5a798a4cfabf565cf731cef037301afaba76ae57
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-05 00:22:44 +0800

    2427. 公因子的数目

--- Ahao.LeetCode/2400-2499/demo2427/Class2427.cs
+++ Ahao.LeetCode/2400-2499/demo2427/Class2427.cs
@@ -12,18 +12,62 @@ namespace Ahao.LeetCode._2400_2499.demo2427
         {
             int res = 0;
             List<int> lst = new List<int>();
-            for(int i = 1;i<= a; i++)
+            for (int i = 1; i <= a; i++)
             {
                 if (a % i == 0)
                     lst.Add(i);
             }
 
-            for(int i = 1;i <= b; i++)
+            for (int i = 1; i <= b; i++)
             {
                 if (b % i == 0 && lst.Contains(i))
                     res++;
             }
             return res;
+        }
+
+
+        public int CommonFactors2(int a, int b)
+        {
+            int ans = 0;
+            for (int x = 1; x <= Math.Min(a, b); ++x)
+            {
+                if (a % x == 0 && b % x == 0)
+                {
+                    ++ans;
+                }
+            }
+            return ans;
+        }
+
+
+        public int CommonFactors3(int a, int b)
+        {
+            int c = GCD(a, b), ans = 0;
+            for (int x = 1; x * x <= c; ++x)
+            {
+                if (c % x == 0)
+                {
+                    ++ans;
+                    if (x * x != c)
+                    {
+                        ++ans;
+                    }
+                }
+            }
+            return ans;
+        }
+
+        public int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                a %= b;
+                a ^= b;
+                b ^= a;
+                a ^= b;
+            }
+            return a;
         }
     }
 }
