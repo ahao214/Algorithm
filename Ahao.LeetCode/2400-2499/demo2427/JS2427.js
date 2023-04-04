1ea46a2b48fe6a2c08d4f3c9a4eac5d73221f313
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-05 00:21:31 +0800

    2427. 公因子的数目

---
+++ Ahao.LeetCode/2400-2499/demo2427/JS2427.js
@@ -0,0 +1,34 @@
+﻿var commonFactors = function (a, b) {
+    let ans = 0;
+    for (let x = 1; x <= Math.min(a, b); ++x) {
+        if (a % x === 0 && b % x === 0) {
+            ++ans;
+        }
+    }
+    return ans;
+};
+
+
+
+var commonFactors = function (a, b) {
+    let c = gcd(a, b), ans = 0;
+    for (let x = 1; x * x <= c; ++x) {
+        if (c % x === 0) {
+            ++ans;
+            if (x * x !== c) {
+                ++ans;
+            }
+        }
+    }
+    return ans;
+}
+
+const gcd = (a, b) => {
+    while (b !== 0) {
+        a %= b;
+        a ^= b;
+        b ^= a;
+        a ^= b;
+    }
+    return a;
+};
