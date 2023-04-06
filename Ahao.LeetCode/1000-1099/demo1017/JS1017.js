c049e341bdf6d8e517eeb7c90acf294f59d0642f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-06 15:09:35 +0800

    1017. 负二进制转换

---
+++ Ahao.LeetCode/1000-1099/demo1017/JS1017.js
@@ -0,0 +1,31 @@
+﻿var baseNeg2 = function(n) {
+    if (n === 0) {
+        return "0";
+    }
+    const bits = new Array(32).fill(0);
+    for (let i = 0; i < 32 && n !== 0; i++) {
+        if ((n & 1) !== 0) {
+            bits[i]++;
+            if ((i & 1) !== 0) {
+                bits[i + 1]++;
+            }
+        }
+        n >>= 1;
+    }
+    let carry = 0;
+    for (let i = 0; i < 32; i++) {
+        const val = carry + bits[i];
+        bits[i] = val & 1;
+        carry = (val - bits[i]) / (-2);
+    }
+    let pos = 31;
+    let res = "";
+    while (pos >= 0 && bits[pos] === 0) {
+        pos--;
+    }
+    while (pos >= 0) {
+        res += bits[pos];
+        pos--;
+    }
+    return res;
+};
