0e70d8711b603819b11556e46ce1721b076d7e41
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-07 09:49:25 +0800

    1040. 移动石子直到连续 II

---
+++ Ahao.LeetCode/1000-1099/demo1040/Class1040.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1040
+{
+    public class Class1040
+    {
+
+
+
+    }
+}
