a71be05099d637de73eda6de8b6219edf2a443eb
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-07 09:49:46 +0800

    1040. 移动石子直到连续 II

--- Ahao.LeetCode/1000-1099/demo1040/Class1040.cs
+++ Ahao.LeetCode/1000-1099/demo1040/Class1040.cs
@@ -8,7 +8,33 @@ namespace Ahao.LeetCode._1000_1099.demo1040
 {
     public class Class1040
     {
-
+        public int[] NumMovesStonesII(int[] stones)
+        {
+            int n = stones.Length;
+            Array.Sort(stones);
+            if (stones[n - 1] - stones[0] + 1 == n)
+            {
+                return new int[] { 0, 0 };
+            }
+            int ma = Math.Max(stones[n - 2] - stones[0] + 1, stones[n - 1] - stones[1] + 1) - (n - 1);
+            int mi = n;
+            for (int i = 0, j = 0; i < n && j + 1 < n; ++i)
+            {
+                while (j + 1 < n && stones[j + 1] - stones[i] + 1 <= n)
+                {
+                    ++j;
+                }
+                if (j - i + 1 == n - 1 && stones[j] - stones[i] + 1 == n - 1)
+                {
+                    mi = Math.Min(mi, 2);
+                }
+                else
+                {
+                    mi = Math.Min(mi, n - (j - i + 1));
+                }
+            }
+            return new int[] { mi, ma };
+        }
 
 
     }
