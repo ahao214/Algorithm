d82cfed27668913f3f58c4c66ac68afbd48fe40d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-09 00:21:30 +0800

    2399. 检查相同字母间的距离

---
+++ Ahao.LeetCode/2300-2399/demo2399/JS2399.js
@@ -0,0 +1,24 @@
+﻿var checkDistances = function (s, distance) {
+    let n = s.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (s[i] == s[j] && distance[s.charCodeAt(i) - 'a'.charCodeAt(0)] != j - i - 1) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+
+var checkDistances = function (s, distance) {
+    let firstIndex = new Array(26).fill(0);
+    for (let i = 0; i < s.length; i++) {
+        let idx = s.charCodeAt(i) - 'a'.charCodeAt(0);
+        if (firstIndex[idx] != 0 && i - firstIndex[idx] != distance[idx]) {
+            return false;
+        }
+        firstIndex[idx] = i + 1;
+    }
+    return true;
+};
