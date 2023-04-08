4079ffac5a4d7719ed3aaddafbe332e6074c5547
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-09 00:23:01 +0800

    2399. 检查相同字母间的距离

---
+++ Ahao.LeetCode/2300-2399/demo2399/Class2399.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2399
+{
+    public class Class2399
+    {
+
+        public bool CheckDistances(string s, int[] distance)
+        {
+            int n = s.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (s[i] == s[j] && distance[s[i] - 'a'] != j - i - 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+        public bool CheckDistances2(string s, int[] distance)
+        {
+            int[] firstIndex = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                int idx = s[i] - 'a';
+                if (firstIndex[idx] != 0 && i - firstIndex[idx] != distance[idx])
+                {
+                    return false;
+                }
+                firstIndex[idx] = i + 1;
+            }
+            return true;
+        }
+
+    }
+}
