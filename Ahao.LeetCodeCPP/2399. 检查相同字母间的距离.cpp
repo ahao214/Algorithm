269062aa3ac73d137d20a3e7ca640f0e06d79aeb
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-09 00:24:15 +0800

    2399. 检查相同字母间的距离

---
+++ Ahao.LeetCodeCPP/2399. 检查相同字母间的距离.cpp
@@ -0,0 +1,48 @@
+#include<vector>
+#include <string>
+
+using namespace std;
+
+/*
+2399. 检查相同字母间的距离
+
+给你一个下标从 0 开始的字符串 s ，该字符串仅由小写英文字母组成，s 中的每个字母都 恰好 出现 两次 。另给你一个下标从 0 开始、长度为 26 的的整数数组 distance 。
+
+字母表中的每个字母按从 0 到 25 依次编号（即，'a' -> 0, 'b' -> 1, 'c' -> 2, ... , 'z' -> 25）。
+
+在一个 匀整 字符串中，第 i 个字母的两次出现之间的字母数量是 distance[i] 。如果第 i 个字母没有在 s 中出现，那么 distance[i] 可以 忽略 。
+
+如果 s 是一个 匀整 字符串，返回 true ；否则，返回 false 。
+*/
+
+class Solution {
+public:
+    bool checkDistances(string s, vector<int>& distance) {
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (s[i] == s[j] && distance[s[i] - 'a'] != j - i - 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+
+
+class Solution {
+public:
+    bool checkDistances(string s, vector<int>& distance) {
+        vector<int> firstIndex(26);
+        for (int i = 0; i < s.size(); i++) {
+            int idx = s[i] - 'a';
+            if (firstIndex[idx] && i - firstIndex[idx] != distance[idx]) {
+                return false;
+            }
+            firstIndex[idx] = i + 1;
+        }
+        return true;
+    }
+};
