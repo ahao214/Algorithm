5832bd12dbfcfc373e7a44d6e1037571b1885285
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-11 17:46:39 +0800

    1041. 困于环中的机器人

--- Ahao.LeetCode/1000-1099/demo1041/Class1041.cs
+++ Ahao.LeetCode/1000-1099/demo1041/Class1041.cs
@@ -36,5 +36,34 @@ namespace Ahao.LeetCode._1000_1099.demo1041
             return direcIndex != 0 || (x == 0 && y == 0);
         }
 
+
+
+
+        int[][] dirs = { new int[] { 0, 1 }, new int[] { 1, 0 }, new int[] { 0, -1 }, new int[] { -1, 0 } };
+        public bool IsRobotBounded2(string instructions)
+        {
+            int dirIndex = 0;
+            int xPosition = 0, yPosition = 0;
+            int length = instructions.Length;
+            for (int i = 0; i < length; i++)
+            {
+                char instruction = instructions[i];
+                if (instruction == 'G')
+                {
+                    xPosition += dirs[dirIndex][0];
+                    yPosition += dirs[dirIndex][1];
+                }
+                else if (instruction == 'L')
+                {
+                    dirIndex = (dirIndex + 3) % 4;
+                }
+                else if (instruction == 'R')
+                {
+                    dirIndex = (dirIndex + 1) % 4;
+                }
+            }
+            return (xPosition == 0 && yPosition == 0) || dirIndex != 0;
+        }
+
     }
 }
