66960f3ab4e0e164550286c1e82e740a2a45f849
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-11 17:40:56 +0800

    1041. 困于环中的机器人

---
+++ Ahao.LeetCode/1000-1099/demo1041/JS1041.js
@@ -0,0 +1,20 @@
+﻿var isRobotBounded = function (instructions) {
+    const direc = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    let direcIndex = 0
+    let x = 0, y = 0
+    const n = instructions.length
+    for (let i = 0; i < n; i++) {
+        let instruction = instructions[i]
+        if (instruction === 'G') {
+            x += direc[direcIndex][0]
+            y += direc[direcIndex][1]
+        } else if (instruction === 'L') {
+            direcIndex += 3
+            direcIndex %= 4
+        } else {
+            direcIndex++
+            direcIndex %= 4
+        }
+    }
+    return direcIndex !== 0 || (x === 0 && y === 0)
+}
