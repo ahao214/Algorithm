6a0e689b79ec8e335ab809d5b9a3c47451b58039
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-13 12:45:18 +0800

    2404. 出现最频繁的偶数元素

---
+++ Ahao.LeetCode/2400-2499/demo2404/Class2404.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2404
+{
+    public class Class2404
+    {
+        public int MostFrequentEven(int[] nums)
+        {
+            IDictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (var x in nums)
+            {
+                if (x % 2 == 0)
+                {
+                    dic.TryAdd(x, 0);
+                    dic[x]++;
+                }
+            }
+            int result = -1;
+            int count = 0;
+            foreach (KeyValuePair<int, int> pair in dic)
+            {
+                if ((result == -1 || pair.Value > count) || result > pair.Key && pair.Value == count)
+                {
+                    result = pair.Key;
+                    count = pair.Value;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Ahao.LeetCode/2400-2499/demo2404/JS2404.js
@@ -0,0 +1,16 @@
+﻿var mostFrequentEven = function(nums) {
+    let count = new Map();
+    for (let x of nums) {
+        if (x % 2 == 0) {
+            count.set(x, (count.get(x) || 0) + 1);
+        }
+    }
+    let res = -1, ct = 0;
+    for (let [k, v] of count) {
+        if (res == -1 || v > ct || v == ct && k < res) {
+            res = k;
+            ct = v;
+        }
+    }
+    return res;
+};
