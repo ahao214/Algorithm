9e06d2310cc73e6625d161cb9c9cf5a28ed074b5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-18 11:37:30 +0800

    1026. 节点与其祖先之间的最大差值

--- Ahao.LeetCode/1000-1099/demo1026/JS1026.js
+++ Ahao.LeetCode/1000-1099/demo1026/JS1026.js
@@ -1,1 +1,15 @@
-﻿
+﻿var maxAncestorDiff = function (root) {
+    return dfs(root, root.val, root.val);
+};
+
+function dfs(root, mi, ma) {
+    if (root === null) {
+        return 0;
+    }
+    var diff = Math.max(Math.abs(root.val - mi), Math.abs(root.val - ma));
+    mi = Math.min(mi, root.val);
+    ma = Math.max(ma, root.val);
+    diff = Math.max(diff, dfs(root.left, mi, ma));
+    diff = Math.max(diff, dfs(root.right, mi, ma));
+    return diff;
+}
