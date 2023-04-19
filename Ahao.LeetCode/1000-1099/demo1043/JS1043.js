fc0d3668c50fb94158f84f931a56c3e3a6568bbc
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-19 16:43:26 +0800

    1043. 分隔数组以得到最大和

---
+++ Ahao.LeetCode/1000-1099/demo1043/JS1043.js
@@ -0,0 +1,15 @@
+﻿var maxSumAfterPartitioning = function(arr, k) {
+    const n = arr.length;
+    const d = new Array(n + 1).fill(0);
+    for (let i = 1; i <= n; i++) {
+        let maxValue = arr[i - 1];
+        for (let j = i - 1; j >= Math.max(0, i - k); j--) {
+            d[i] = Math.max(d[i], d[j] + maxValue * (i - j));
+            if (j > 0) {
+                maxValue = Math.max(maxValue, arr[j - 1]);
+            }
+        }
+    }
+    return d[n];
+
+};
