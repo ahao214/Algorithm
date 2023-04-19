5589e1e9b437c5ee7726945f0c78f2f3e64d15b3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-19 16:43:03 +0800

    1043. 分隔数组以得到最大和

---
+++ Ahao.LeetCode/1000-1099/demo1043/Class1043.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1043
+{
+    public class Class1043
+    {
+        public int MaxSumAfterPartitioning(int[] arr, int k)
+        {
+            int n = arr.Length;
+            int[] d = new int[n + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                int maxValue = arr[i - 1];
+                for (int j = i - 1; j >= 0 && j >= i - k; j--)
+                {
+                    d[i] = Math.Max(d[i], d[j] + maxValue * (i - j));
+                    if (j > 0)
+                    {
+                        maxValue = Math.Max(maxValue, arr[j - 1]);
+                    }
+                }
+            }
+            return d[n];
+        }
+
+
+    }
+}
