47cabc089c0184e465a31e2d499a9437d59c1880
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-21 12:40:00 +0800

    2413. 最小偶倍数

---
+++ Ahao.LeetCode/2400-2499/demo2413/Class2413.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2413
+{
+    public class Class2413
+    {
+        public int SmallestEvenMultiple(int n)
+        {
+            return n % 2 == 0 ? n : 2 * n;
+        }
+    }
+}
