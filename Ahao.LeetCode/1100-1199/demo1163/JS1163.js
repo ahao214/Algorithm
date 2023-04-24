46f3179ed0d3187932c611024be019ee85b2db48
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-24 14:02:42 +0800

    1163. 按字典序排在最后的子串

---
+++ Ahao.LeetCode/1100-1199/demo1163/JS1163.js
@@ -0,0 +1,17 @@
+﻿var lastSubstring = function(s) {
+    let i = 0, j = 1, n = s.length;
+    while (j < n) {
+        let k = 0;
+        while (j + k < n && s[i + k] === s[j + k]) {
+            k++;
+        }
+        if (j + k < n && s[i + k] < s[j + k]) {
+            let t = i;
+            i = j;
+            j = Math.max(j + 1, t + k + 1);
+        } else {
+            j = j + k + 1;
+        }
+    }
+    return s.substring(i, n);
+};
