f6da4e3adf96749846caf4d10bb87dee603da9b1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-25 12:22:32 +0800

    2418. 按身高排序

---
+++ Ahao.LeetCode/2400-2499/demo2418/Class2418.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2418
+{
+    public class Class2418
+    {
+
+        public string[] SortPeople(string[] names, int[] heights)
+        {
+            int n = names.Length;
+            int[] indices = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                indices[i] = i;
+            }
+            Array.Sort(indices, (a, b) => heights[b] - heights[a]);
+            string[] res = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                res[i] = names[indices[i]];
+            }
+            return res;
+        }
+
+
+    }
+}
