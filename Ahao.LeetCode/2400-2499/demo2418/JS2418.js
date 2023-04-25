895478407905e099253b72aafe1b4eab4c2827e0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-25 12:22:54 +0800

    2418. 按身高排序

---
+++ Ahao.LeetCode/2400-2499/demo2418/JS2418.js
@@ -0,0 +1,10 @@
+﻿var sortPeople = function (names, heights) {
+    const n = names.length;
+    const indices = Array.from({ length: n }, (_, i) => i);
+    indices.sort((a, b) => heights[b] - heights[a]);
+    const res = new Array(n);
+    for (let i = 0; i < n; i++) {
+        res[i] = names[indices[i]];
+    }
+    return res;
+};
