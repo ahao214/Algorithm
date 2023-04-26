e4b52c9a071094693d6608f006b40c11a8dbad0a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-26 10:29:33 +0800

    1031. 两个非重叠子数组的最大和

---
+++ Ahao.LeetCode/1000-1099/demo1031/JS1031.js
@@ -0,0 +1,17 @@
+﻿var maxSumTwoNoOverlap = function (nums, firstLen, secondLen) {
+    return Math.max(help(nums, firstLen, secondLen), help(nums, secondLen, firstLen));
+};
+
+function help(nums, firstLen, secondLen) {
+    let suml = nums.slice(0, firstLen).reduce((acc, val) => acc + val, 0);
+    let maxSumL = suml;
+    let sumr = nums.slice(firstLen, firstLen + secondLen).reduce((acc, val) => acc + val, 0);
+    let res = maxSumL + sumr;
+    for (let i = firstLen + secondLen, j = firstLen; i < nums.length; i++, j++) {
+        suml += nums[j] - nums[j - firstLen];
+        maxSumL = Math.max(maxSumL, suml);
+        sumr += nums[i] - nums[i - secondLen];
+        res = Math.max(res, maxSumL + sumr);
+    }
+    return res;
+}
