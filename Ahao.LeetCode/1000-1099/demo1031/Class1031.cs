2b141d020928ce25d5cfce222538387c90f7e125
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-26 10:28:58 +0800

    1031. 两个非重叠子数组的最大和

---
+++ Ahao.LeetCode/1000-1099/demo1031/Class1031.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1031
+{
+    public class Class1031
+    {
+        public int MaxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen)
+        {
+            return Math.Max(Help(nums, firstLen, secondLen), Help(nums, secondLen, firstLen));
+        }
+
+        public int Help(int[] nums, int firstLen, int secondLen)
+        {
+            int suml = 0;
+            for (int i = 0; i < firstLen; ++i)
+            {
+                suml += nums[i];
+            }
+            int maxSumL = suml;
+            int sumr = 0;
+            for (int i = firstLen; i < firstLen + secondLen; ++i)
+            {
+                sumr += nums[i];
+            }
+            int res = maxSumL + sumr;
+            for (int i = firstLen + secondLen, j = firstLen; i < nums.Length; ++i, ++j)
+            {
+                suml += nums[j] - nums[j - firstLen];
+                maxSumL = Math.Max(maxSumL, suml);
+                sumr += nums[i] - nums[i - secondLen];
+                res = Math.Max(res, maxSumL + sumr);
+            }
+            return res;
+        }
+    }
+}
