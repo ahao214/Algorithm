22639a691c8e38be215f0a26d1d11dcd99ed8c22
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-26 10:30:42 +0800

    1031. 两个非重叠子数组的最大和

--- Ahao.LeetCodeCPP/1031. 两个非重叠子数组的最大和.cpp
+++ Ahao.LeetCodeCPP/1031. 两个非重叠子数组的最大和.cpp
@@ -1,4 +1,5 @@
 #include<vector>
+#include <numeric>
 
 using namespace std;
 
@@ -34,5 +35,27 @@ public:
 
 	int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
 		return max(work(nums, firstLen, secondLen), work(nums, secondLen, firstLen));
+	}
+};
+
+
+class Solution {
+public:
+	int help(vector<int>& nums, int firstLen, int secondLen) {
+		int suml = accumulate(nums.begin(), nums.begin() + firstLen, 0);
+		int maxSumL = suml;
+		int sumr = accumulate(nums.begin() + firstLen, nums.begin() + firstLen + secondLen, 0);
+		int res = maxSumL + sumr;
+		for (int i = firstLen + secondLen, j = firstLen; i < nums.size(); ++i, ++j) {
+			suml += nums[j] - nums[j - firstLen];
+			maxSumL = max(maxSumL, suml);
+			sumr += nums[i] - nums[i - secondLen];
+			res = max(res, maxSumL + sumr);
+		}
+		return res;
+	}
+
+	int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
+		return max(help(nums, firstLen, secondLen), help(nums, secondLen, firstLen));
 	}
 };
