37269ffdf446bc673620d19a3c7e63b27d7aa32d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-27 10:51:37 +0800

    1048. 最长字符串链

---
+++ Ahao.LeetCode/1000-1099/demo1048/JS1048.js
@@ -0,0 +1,16 @@
+﻿var longestStrChain = function (words) {
+    const cnt = new Map();
+    words.sort((a, b) => a.length - b.length);
+    let res = 0;
+    for (const word of words) {
+        cnt.set(word, 1);
+        for (let i = 0; i < word.length; i++) {
+            const prev = word.substring(0, i) + word.substring(i + 1);
+            if (cnt.has(prev)) {
+                cnt.set(word, Math.max(cnt.get(word), cnt.get(prev) + 1));
+            }
+        }
+        res = Math.max(res, cnt.get(word));
+    }
+    return res;
+};
