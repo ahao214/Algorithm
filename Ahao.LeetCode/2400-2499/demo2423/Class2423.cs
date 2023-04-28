11b6a3dd6c43459213b65ecaa960f87da0294055
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-29 07:52:06 +0800

    2423. 删除字符使频率相同

---
+++ Ahao.LeetCode/2400-2499/demo2423/Class2423.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2423
+{
+    public class Class2423
+    {
+
+        public bool EqualFrequency(string word)
+        {
+            int[] charCount = new int[26];
+            foreach (char c in word)
+            {
+                charCount[c - 'a']++;
+            }
+            for (int i = 0; i < 26; i++)
+            {
+                if (charCount[i] == 0)
+                {
+                    continue;
+                }
+                charCount[i]--;
+                HashSet<int> frequency = new HashSet<int>();
+                foreach (int f in charCount)
+                {
+                    if (f > 0)
+                    {
+                        frequency.Add(f);
+                    }
+                }
+                if (frequency.Count == 1)
+                {
+                    return true;
+                }
+                charCount[i]++;
+            }
+            return false;
+        }
+
+
+
+        public bool EqualFrequency2(string word)
+        {
+            int[] charCount = new int[26];
+            foreach (char c in word)
+            {
+                charCount[c - 'a'] += 1;
+            }
+            Dictionary<int, int> freqCount = new Dictionary<int, int>();
+            foreach (int c in charCount)
+            {
+                if (c > 0)
+                {
+                    if (freqCount.ContainsKey(c))
+                    {
+                        freqCount[c] += 1;
+                    }
+                    else
+                    {
+                        freqCount.Add(c, 1);
+                    }
+                }
+            }
+            foreach (int c in charCount)
+            {
+                if (c == 0)
+                {
+                    continue;
+                }
+                freqCount[c] -= 1;
+                if (freqCount[c] == 0)
+                {
+                    freqCount.Remove(c);
+                }
+                if (c - 1 > 0)
+                {
+                    if (freqCount.ContainsKey(c - 1))
+                    {
+                        freqCount[c - 1] += 1;
+                    }
+                    else
+                    {
+                        freqCount.Add(c - 1, 1);
+                    }
+                }
+                if (freqCount.Count == 1)
+                {
+                    return true;
+                }
+                if (c - 1 > 0)
+                {
+                    freqCount[c - 1] -= 1;
+                    if (freqCount[c - 1] == 0)
+                    {
+                        freqCount.Remove(c - 1);
+                    }
+                }
+                if (freqCount.ContainsKey(c))
+                {
+                    freqCount[c] += 1;
+                }
+                else
+                {
+                    freqCount.Add(c, 1);
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/2400-2499/demo2423/JS2423.js
@@ -0,0 +1,61 @@
+﻿var equalFrequency = function (word) {
+    const charCount = new Array(26).fill(0);
+    for (let c of word) {
+        charCount[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+    for (let i = 0; i < 26; i++) {
+        if (charCount[i] == 0) {
+            continue;
+        }
+        charCount[i]--;
+        const frequency = new Set();
+        for (const f of charCount) {
+            if (f > 0) {
+                frequency.add(f);
+            }
+        }
+        if (frequency.size == 1) {
+            return true;
+        }
+        charCount[i]++;
+    }
+    return false;
+};
+
+
+
+var equalFrequency = function (word) {
+    let charCount = new Array(26).fill(0);
+    for (let c of word) {
+        charCount[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+    let freqCount = new Map();
+    for (let c of charCount) {
+        if (c > 0) {
+            freqCount.set(c, (freqCount.get(c) || 0) + 1);
+        }
+    }
+    for (let c of charCount) {
+        if (c == 0) {
+            continue;
+        }
+        freqCount.set(c, freqCount.get(c) - 1);
+        if (freqCount.get(c) == 0) {
+            freqCount.delete(c);
+        }
+        if (c - 1 > 0) {
+            freqCount.set(c - 1, (freqCount.get(c - 1) || 0) + 1);
+        }
+        if (freqCount.size == 1) {
+            return true;
+        }
+        if (c - 1 > 0) {
+            freqCount.set(c - 1, freqCount.get(c - 1) - 1);
+            if (freqCount.get(c - 1) == 0) {
+                freqCount.delete(c - 1);
+            }
+        }
+        freqCount.set(c, (freqCount.get(c) || 0) + 1);
+    }
+    return false;
+};
