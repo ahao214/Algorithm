bf352da3c17197df15972d8671e83e54f94c3fcc
liupenghao@fulan.com.cn <haochen214@163.com>
2023-04-30 12:43:25 +0800

    1033. 移动石子直到连续

---
+++ Ahao.LeetCode/1000-1099/demo1033/JS1033.js
@@ -0,0 +1,12 @@
+﻿var numMovesStones = function(a, b, c) {
+    let x = Math.min(Math.min(a, b), c);
+    let z = Math.max(Math.max(a, b), c);
+    let y = a + b + c - x - z;
+    let res = [2, z - x - 2];
+    if (z - y == 1 && y - x == 1) {
+        res[0] = 0;
+    } else if (z - y <= 2 || y - x <= 2) {
+        res[0] = 1;
+    }
+    return res;
+};
