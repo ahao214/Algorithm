5d4b8e2507fd01b46a5d27e326a753d422326db2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-01 08:47:00 +0800

    1376. 通知所有员工所需的时间

--- Ahao.LeetCode/1300-1399/demo1376/Class1376.cs
+++ Ahao.LeetCode/1300-1399/demo1376/Class1376.cs
@@ -38,5 +38,42 @@ namespace Ahao.LeetCode._1300_1399.demo1376
         }
 
 
+
+
+        public int NumOfMinutes2(int n, int headID, int[] manager, int[] informTime)
+        {
+            IDictionary<int, IList<int>> g = new Dictionary<int, IList<int>>();
+            // 构建邻接表
+            for (int i = 0; i < n; i++)
+            {
+                g.TryAdd(manager[i], new List<int>());
+                g[manager[i]].Add(i);
+            }
+            Queue<int[]> queue = new Queue<int[]>();
+            // 将起点放入队列
+            queue.Enqueue(new int[] { headID, 0 });
+            int res = 0;
+            while (queue.Count > 0)
+            {
+                int[] arr = queue.Dequeue();
+                int tmpId = arr[0], val = arr[1];
+                // 如果当前节点没有下属，说明到达了叶子节点，更新结果
+                if (!g.ContainsKey(tmpId))
+                {
+                    res = Math.Max(res, val);
+                }
+                else
+                {
+                    // 将当前节点的下属加入队列，更新时间
+                    foreach (int ne in g[tmpId])
+                    {
+                        queue.Enqueue(new int[] { ne, val + informTime[tmpId] });
+                    }
+                }
+            }
+            return res;
+        }
+
+
     }
 }
