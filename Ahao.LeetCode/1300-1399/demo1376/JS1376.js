8285e6bb4e26eb604b84995056b01d37a21ff1b5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-01 08:46:32 +0800

    1376. 通知所有员工所需的时间

--- Ahao.LeetCode/1300-1399/demo1376/JS1376.js
+++ Ahao.LeetCode/1300-1399/demo1376/JS1376.js
@@ -28,3 +28,42 @@
     // 从总负责人开始遍历整棵树，并计算总通知时间
     return dfs(headID, informTime, g);
 }
+
+
+
+
+var numOfMinutes = function (n, headID, manager, informTime) {
+    // 初始化哈希表
+    const g = new Map();
+    for (let i = 0; i < n; i++) {
+        if (!g.has(manager[i])) {
+            g.set(manager[i], []);
+        }
+        g.get(manager[i]).push(i);
+    }
+    // 初始化队列
+    const queue = [];
+    queue.push([headID, 0]);
+
+    // 初始化结果
+    let res = 0;
+
+    // BFS
+    while (queue.length) {
+        const arr = queue.shift();
+        const tmpId = arr[0], val = arr[1];
+        if (!g.has(tmpId)) {
+            // 如果当前节点没有下属，更新结果
+            res = Math.max(res, val);
+        } else {
+            // 遍历当前节点的下属，将其加入队列
+            for (const ne of g.get(tmpId)) {
+                queue.push([ne, val + informTime[tmpId]]);
+            }
+        }
+    }
+    return res;
+}
+
+
+
