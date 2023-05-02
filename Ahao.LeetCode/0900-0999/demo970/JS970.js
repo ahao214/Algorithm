08c18c811d55f8b9bea9d3cdef609d548f94ffcd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-02 12:36:51 +0800

    970. 强整数

--- Ahao.LeetCode/0900-0999/demo970/JS970.js
+++ Ahao.LeetCode/0900-0999/demo970/JS970.js
@@ -1,1 +1,21 @@
-﻿
+﻿var powerfulIntegers = function (x, y, bound) {
+    const set = new Set();
+    let value1 = 1;
+    for (let i = 0; i < 21; i++) {
+        let value2 = 1;
+        for (let j = 0; j < 21; j++) {
+            let value = value1 + value2;
+            if (value <= bound) {
+                set.add(value);
+            } else {
+                break;
+            }
+            value2 *= y;
+        }
+        if (value1 > bound) {
+            break;
+        }
+        value1 *= x;
+    }
+    return [...set];
+};
