98fff7d9749919148371325f6b76f4ddab02e7ff
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-03 09:46:03 +0800

    1003. 检查替换后的词是否有效

---
+++ Ahao.LeetCode/1000-1099/demo1003/JS1003.js
@@ -0,0 +1,11 @@
+﻿var isValid = function (s) {
+    const stk = [];
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        stk.push(c);
+        if (stk.length >= 3 && stk.slice(stk.length - 3).join("") === "abc") {
+            stk.splice(stk.length - 3, 3);
+        }
+    }
+    return stk.length === 0;
+};
