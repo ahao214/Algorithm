61ed146acbd6c47c6fa35a270e8a5178a208e4a9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-04 12:47:14 +0800

    2106. 摘水果

--- Ahao.LeetCodeCPP/2106. 摘水果.cpp
+++ Ahao.LeetCodeCPP/2106. 摘水果.cpp
@@ -45,3 +45,40 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int left = 0;
+        int right = 0;
+        int n = fruits.size();
+        int sum = 0;
+        int ans = 0;
+
+        auto step = [&](int left, int right) -> int {
+            if (fruits[right][0] <= startPos) {
+                return startPos - fruits[left][0];
+            }
+            else if (fruits[left][0] >= startPos) {
+                return fruits[right][0] - startPos;
+            }
+            else {
+                return min(abs(startPos - fruits[right][0]), abs(startPos - fruits[left][0])) + \
+                    fruits[right][0] - fruits[left][0];
+            }
+        };
+        // 每次固定住窗口右边界
+        while (right < n) {
+            sum += fruits[right][1];
+            // 移动左边界
+            while (left <= right && step(left, right) > k) {
+                sum -= fruits[left][1];
+                left++;
+            }
+            ans = max(ans, sum);
+            right++;
+        }
+        return ans;
+    }
+};
