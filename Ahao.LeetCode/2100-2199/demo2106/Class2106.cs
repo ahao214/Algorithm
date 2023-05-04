5baab21fe4485adb43a65e47ee70bbebb0388356
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-04 12:45:37 +0800

    2106. 摘水果

---
+++ Ahao.LeetCode/2100-2199/demo2106/Class2106.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2100_2199.demo2106
+{
+    class Class2106
+    {
+    }
+}
--- Ahao.LeetCode/2100-2199/demo2106/JS2106.js
+++ Ahao.LeetCode/2100-2199/demo2106/JS2106.js
@@ -1,1 +1,59 @@
-﻿
+﻿var maxTotalFruits = function (fruits, startPos, k) {
+    const n = fruits.length;
+    const sum = new Array(n + 1).fill(0);
+    const indices = new Array(n).fill(0);
+    sum[0] = 0;
+    for (let i = 0; i < n; i++) {
+        sum[i + 1] = sum[i] + fruits[i][1];
+        indices[i] = fruits[i][0];
+    }
+    let ans = 0;
+    for (let x = 0; x <= Math.floor(k / 2); x++) {
+        /* 向左走 x 步，再向右走 k - x 步 */
+        let y = k - 2 * x;
+        let left = startPos - x;
+        let right = startPos + y;
+        let start = lowerBound(indices, 0, n - 1, left);
+        let end = upperBound(indices, 0, n - 1, right);
+        ans = Math.max(ans, sum[end] - sum[start]);
+        /* 向右走 x 步，再向左走 k - x 步 */
+        y = k - 2 * x;
+        left = startPos - y;
+        right = startPos + x;
+        start = lowerBound(indices, 0, n - 1, left);
+        end = upperBound(indices, 0, n - 1, right);
+        ans = Math.max(ans, sum[end] - sum[start]);
+    }
+    return ans;
+}
+
+const lowerBound = (arr, left, right, val) => {
+    let res = right + 1;
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2);
+        if (arr[mid] >= val) {
+            res = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return res;
+}
+
+const upperBound = (arr, left, right, val) => {
+    let res = right + 1;
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2);
+        if (arr[mid] > val) {
+            res = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return res;
+};
+
+
+
