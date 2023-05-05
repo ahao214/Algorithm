b7f6b99a1b278230f1f97f1750c0eedbae556996
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-05 13:03:43 +0800

    2432. 处理用时最长的那个任务的员工

--- Ahao.LeetCode/2400-2499/demo2432/JS2432.js
+++ Ahao.LeetCode/2400-2499/demo2432/JS2432.js
@@ -1,1 +1,12 @@
-﻿
+﻿var hardestWorker = function (n, logs) {
+    let ans = logs[0][0], maxCost = logs[0][1];
+    for (let i = 1; i < logs.length; i++) {
+        const idx = logs[i][0];
+        const cost = logs[i][1] - logs[i - 1][1];
+        if (cost > maxCost || (cost === maxCost && idx < ans)) {
+            ans = idx;
+            maxCost = cost;
+        }
+    }
+    return ans;
+};
