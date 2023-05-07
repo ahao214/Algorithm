5254f8dd430422949372d4fae04aa196e91f5e98
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-07 14:01:57 +0800

    1010. 总持续时间可被 60 整除的歌曲

---
+++ Ahao.LeetCode/1000-1099/demo1010/Class1010.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1010
+{
+    public class Class1010
+    {
+        public int NumPairsDivisibleBy60(int[] time)
+        {
+            int res = 0;
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (var item in time)
+            {
+                int count = 0;
+                dic.TryGetValue((60 - item % 60) % 60, out count);
+                res += count;
+                if (!dic.ContainsKey(item % 60))
+                {
+                    dic.Add(item % 60, 1);
+                }
+                else
+                {
+                    dic[item % 60]++;
+                }
+            }
+            return res;
+        }
+
+    }
+}
