2501bb445906cd8c793a08ce2b71d96d9a8f5f89
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-07 14:03:09 +0800

    1010. 总持续时间可被 60 整除的歌曲

---
+++ Ahao.LeetCode/1000-1099/demo1010/JS1010.js
@@ -0,0 +1,12 @@
+﻿var numPairsDivisibleBy60 = function (time) {
+    const cnt = new Array(60).fill(0);
+    for (let t of time) {
+        cnt[t % 60]++;
+    }
+    let res = 0;
+    for (let i = 1; i < 30; i++) {
+        res += cnt[i] * cnt[60 - i];
+    }
+    res += cnt[0] * (cnt[0] - 1) / 2 + cnt[30] * (cnt[30] - 1) / 2;
+    return res;
+}
