0b7216409a6aaaddd21a73c5c1dc9003cefeecdf
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-09 11:55:40 +0800

    2437. 有效时间的数目

--- Ahao.LeetCode/2400-2499/demo2437/Class2437.cs
+++ Ahao.LeetCode/2400-2499/demo2437/Class2437.cs
@@ -18,10 +18,10 @@ namespace Ahao.LeetCode._2400_2499.demo2437
             {
                 hour = 24;
             }
-            else if (strs[0] == '?' && strs[1] !='?')
+            else if (strs[0] == '?' && strs[1] != '?')
             {
                 hour = strs[1] < '4' ? 3 : 2;
-            }            
+            }
             else if (strs[0] != '?' && strs[1] == '?')
             {
                 hour = strs[0] < '2' ? 10 : 4;
@@ -40,10 +40,51 @@ namespace Ahao.LeetCode._2400_2499.demo2437
                 minute = 10;
             }
 
-
             return hour * minute;
-
-
         }
+
+
+
+        int res = 0;
+
+        public int CountTime2(string time)
+        {
+            char[] arr = time.ToCharArray();
+            DFS(arr, 0);
+            return res;
+        }
+
+        public void DFS(char[] arr, int pos)
+        {
+            if (pos == arr.Length)
+            {
+                if (Check(arr))
+                {
+                    res++;
+                }
+                return;
+            }
+            if (arr[pos] == '?')
+            {
+                for (int i = 0; i <= 9; i++)
+                {
+                    arr[pos] = (char)('0' + i);
+                    DFS(arr, pos + 1);
+                    arr[pos] = '?';
+                }
+            }
+            else
+            {
+                DFS(arr, pos + 1);
+            }
+        }
+
+        public bool Check(char[] arr)
+        {
+            int hh = (arr[0] - '0') * 10 + arr[1] - '0';
+            int mm = (arr[3] - '0') * 10 + arr[4] - '0';
+            return hh < 24 && mm < 60;
+        }
+
     }
 }
