9179d0b2cb39d64466835ed9e51efc8fedc39aec
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-09 11:57:33 +0800

    2437. 有效时间的数目

--- Ahao.LeetCode/2400-2499/demo2437/Class2437.cs
+++ Ahao.LeetCode/2400-2499/demo2437/Class2437.cs
@@ -86,5 +86,36 @@ namespace Ahao.LeetCode._2400_2499.demo2437
             return hh < 24 && mm < 60;
         }
 
+
+
+        public int CountTime3(string time)
+        {
+            int countHour = 0;
+            int countMinute = 0;
+            for (int i = 0; i < 24; i++)
+            {
+                int hiHour = i / 10;
+                int loHour = i % 10;
+                if ((time[0] == '?' || time[0] == hiHour + '0') &&
+                    (time[1] == '?' || time[1] == loHour + '0'))
+                {
+                    countHour++;
+                }
+            }
+            for (int i = 0; i < 60; i++)
+            {
+                int hiMinute = i / 10;
+                int loMinute = i % 10;
+                if ((time[3] == '?' || time[3] == hiMinute + '0') &&
+                    (time[4] == '?' || time[4] == loMinute + '0'))
+                {
+                    countMinute++;
+                }
+            }
+            return countHour * countMinute;
+        }
+
+
+
     }
 }
