4add969c6dc446e7ca8167034ac7ebf3a43541a6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-10 10:59:50 +0800

    1015. 可被 K 整除的最小整数

---
+++ Ahao.LeetCodeCPP/1015. 可被 K 整除的最小整数.cpp
@@ -0,0 +1,33 @@
+#include <unordered_set>
+#include<vector>
+
+using namespace std;
+
+/*
+1015. 可被 K 整除的最小整数
+
+给定正整数 k ，你需要找出可以被 k 整除的、仅包含数字 1 的最 小 正整数 n 的长度。
+
+返回 n 的长度。如果不存在这样的 n ，就返回-1。
+
+注意： n 不符合 64 位带符号整数。
+*/
+
+
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        int resid = 1 % k, len = 1; // resid为余数，len为数字长度，初始值为1
+        unordered_set<int> st; // 创建一个无序集合，用于存储余数
+        st.insert(resid); // 插入余数1
+        while (resid != 0) { // 当余数为0时退出循环
+            resid = (resid * 10 + 1) % k; // 计算下一个余数
+            len++; // 数字长度+1
+            if (st.find(resid) != st.end()) { // 如果余数重复出现，则无解
+                return -1;
+            }
+            st.insert(resid); // 将余数插入集合
+        }
+        return len; // 返回数字长度
+    }
+};
