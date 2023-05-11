d41b98405b063efdab1ea7d561846d9ce2aa6dd7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-11 11:40:50 +0800

    1016. 子串能表示从 1 到 N 数字的二进制串

---
+++ Ahao.LeetCode/1000-1099/demo1016/JS1016.js
@@ -0,0 +1,28 @@
+﻿var queryString = function (s, n) {
+    if (n === 1) {
+        return [...s].indexOf('1') !== -1;
+    }
+    let k = 30;
+    while ((1 << k) >= n) {
+        --k;
+    }
+    if (s.length < (1 << (k - 1)) + k - 1 || s.length < n - (1 << k) + k + 1) {
+        return false;
+    }
+    return help(s, k, 1 << (k - 1), (1 << k) - 1) && help(s, k + 1, 1 << k, n);
+}
+
+const help = (s, k, mi, ma) => {
+    const set = new Set();
+    let t = 0;
+    for (let r = 0; r < s.length; ++r) {
+        t = t * 2 + (s[r].charCodeAt() - '0'.charCodeAt());
+        if (r >= k) {
+            t -= (s[r - k].charCodeAt() - '0'.charCodeAt()) << k;
+        }
+        if (r >= k - 1 && t >= mi && t <= ma) {
+            set.add(t);
+        }
+    }
+    return set.size === ma - mi + 1;
+};
