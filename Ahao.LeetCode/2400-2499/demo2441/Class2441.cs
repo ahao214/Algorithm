9f11e54d76befd55b764f64f5d7851a9739fd37d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-13 12:37:17 +0800

    2441. 与对应负数同时存在的最大正整数

---
+++ Ahao.LeetCode/2400-2499/demo2441/Class2441.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2441
+{
+    public class Class2441
+    {
+        public int FindMaxK(int[] nums)
+        {
+            int res = -1;
+            Array.Sort(nums);
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left < right)
+            {
+                if (nums[left] + nums[right] == 0)
+                {
+                    return nums[right];
+                }
+                else if (nums[left] + nums[right] > 0)
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+            return -1;
+
+        }
+
+    }
+}
