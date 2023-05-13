8269ad91c9f172fd8466a44e1fa7711706774583
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-13 12:38:10 +0800

    2441. 与对应负数同时存在的最大正整数

---
+++ Ahao.LeetCode/2400-2499/demo2441/JS2441.js
@@ -0,0 +1,11 @@
+﻿var findMaxK = function (nums) {
+    let k = -1;
+    for (const x of nums) {
+        for (const y of nums) {
+            if (-x === y) {
+                k = Math.max(k, x);
+            }
+        }
+    }
+    return k;
+};
