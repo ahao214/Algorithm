f8f0fb917d253c97024b3f8ff633c3bc0045dd9f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-14 13:31:50 +0800

    1054. 距离相等的条形码

--- Ahao.LeetCodeCPP/1054. 距离相等的条形码.cpp
+++ Ahao.LeetCodeCPP/1054. 距离相等的条形码.cpp
@@ -49,3 +49,36 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> rearrangeBarcodes(vector<int>& barcodes) {
+        int length = barcodes.size();
+        if (length < 2) {
+            return barcodes;
+        }
+
+        unordered_map<int, int> counts;
+        int maxCount = 0;
+        for (int b : barcodes) {
+            maxCount = max(maxCount, ++counts[b]);
+        }
+
+        int evenIndex = 0, oddIndex = 1, halfLength = length / 2;
+        vector<int> res(length);
+        for (auto& [x, cx] : counts) {
+            while (cx > 0 && cx <= halfLength && oddIndex < length) {
+                res[oddIndex] = x;
+                cx--;
+                oddIndex += 2;
+            }
+            while (cx > 0) {
+                res[evenIndex] = x;
+                cx--;
+                evenIndex += 2;
+            }
+        }
+        return res;
+    }
+};
