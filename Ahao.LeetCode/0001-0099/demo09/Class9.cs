22523d86326a2b8127ecdc9169e35514df77d1ed
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-14 15:36:33 +0800

    9. 回文数

--- Ahao.LeetCode/0001-0099/demo09/Class9.cs
+++ Ahao.LeetCode/0001-0099/demo09/Class9.cs
@@ -28,5 +28,26 @@ namespace Ahao.LeetCode.Simple.demo9
             }
             return x == r || x == r / 10;
         }
+
+        public bool IsPalindrome2(int x)
+        {
+            if (x < 0) return false;
+            int help = 1;
+            while (x / help >= 10)
+            {
+                help *= 10;
+            }
+
+            while (x != 0)
+            {
+                if (x / help != x % 10)
+                {
+                    return false;
+                }
+                x = (x % help) / 10;
+                help /= 100;
+            }
+            return true;
+        }
     }
 }
