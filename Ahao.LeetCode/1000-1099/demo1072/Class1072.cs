bccdb8dd7ea4a4bf07187c5ec43623319e2872e1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-15 09:07:57 +0800

    1072. 按列翻转得到最大值等行数

---
+++ Ahao.LeetCode/1000-1099/demo1072/Class1072.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1072
+{
+    public class Class1072
+    {
+        public int MaxEqualRowsAfterFlips(int[][] matrix)
+        {
+            int m = matrix.Length, n = matrix[0].Length;
+            IDictionary<string, int> dictionary = new Dictionary<string, int>();
+            for (int i = 0; i < m; i++)
+            {
+                char[] arr = new char[n];
+                Array.Fill(arr, '0');
+                for (int j = 0; j < n; j++)
+                {
+                    // 如果 matrix[i][0] 为 1，则对该行元素进行翻转
+                    arr[j] = (char)('0' + (matrix[i][j] ^ matrix[i][0]));
+                }
+                string s = new string(arr);
+                dictionary.TryAdd(s, 0);
+                dictionary[s]++;
+            }
+            int res = 0;
+            foreach (KeyValuePair<string, int> pair in dictionary)
+            {
+                res = Math.Max(res, pair.Value);
+            }
+            return res;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1000-1099/demo1072/JS1072.js
@@ -0,0 +1,18 @@
+﻿var maxEqualRowsAfterFlips = function(matrix) {
+    let m = matrix.length, n = matrix[0].length;
+    let map = {};
+    for (let i = 0; i < m; i++) {
+        let arr = new Array(n).fill('0');
+        for (let j = 0; j < n; j++) {
+            // 如果 matrix[i][0] 为 1，则对该行元素进行翻转
+            arr[j] = '0' + (matrix[i][j] ^ matrix[i][0]);
+        }
+        let s = arr.join('');
+        map[s] = (map[s] || 0) + 1;
+    }
+    let res = 0;
+    for (let key in map) {
+        res = Math.max(res, map[key]);
+    }
+    return res;
+}
