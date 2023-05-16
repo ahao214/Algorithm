7479f78db578a6a39feb8b4e50a0f7b2216ded98
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-16 13:36:05 +0800

    1335. 工作计划的最低难度

---
+++ Ahao.LeetCode/1300-1399/demo1335/Class1335.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1300_1399.demo1335
+{
+    public class Class1335
+    {
+
+
+    }
+}
---
+++ Ahao.LeetCode/1300-1399/demo1335/JS1335.js
@@ -0,0 +1,48 @@
+﻿var minDifficulty = function (jobDifficulty, d) {
+    const n = jobDifficulty.length;
+    if (n < d) {
+        return -1;
+    }
+    const dp = new Array(d + 1).fill(0).map(() => new Array(n).fill(0x3f3f3f3f));
+    let ma = 0;
+    for (let i = 0; i < n; ++i) {
+        ma = Math.max(ma, jobDifficulty[i]);
+        dp[0][i] = ma;
+    }
+    for (let i = 1; i < d; ++i) {
+        for (let j = i; j < n; ++j) {
+            ma = 0;
+            for (let k = j; k >= i; --k) {
+                ma = Math.max(ma, jobDifficulty[k]);
+                dp[i][j] = Math.min(dp[i][j], ma + dp[i - 1][k - 1]);
+            }
+        }
+    }
+    return dp[d - 1][n - 1];
+};
+
+
+
+var minDifficulty = function (jobDifficulty, d) {
+    const n = jobDifficulty.length;
+    if (n < d) {
+        return -1;
+    }
+    let dp = new Array(n).fill(0);
+    for (let i = 0, j = 0; i < n; ++i) {
+        j = Math.max(j, jobDifficulty[i]);
+        dp[i] = j;
+    }
+    for (let i = 1; i < d; ++i) {
+        const ndp = new Array(n).fill(0x3f3f3f3f);
+        for (let j = i; j < n; ++j) {
+            let ma = 0;
+            for (let k = j; k >= i; --k) {
+                ma = Math.max(ma, jobDifficulty[k]);
+                ndp[j] = Math.min(ndp[j], ma + dp[k - 1]);
+            }
+        }
+        dp = ndp;
+    }
+    return dp[n - 1];
+};
