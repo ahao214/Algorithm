2a001b0d0e9e2fe625a904e17a768faf75bb93e8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-17 11:05:26 +0800

    2446. 判断两个事件是否存在冲突

---
+++ Ahao.LeetCode/2400-2499/demo2446/Class2446.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2446
+{
+    public class Class2446
+    {
+
+        public bool HaveConflict(string[] event1, string[] event2)
+        {
+            return !(event1[1].CompareTo(event2[0]) < 0 || event2[1].CompareTo(event1[0]) < 0);
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/2400-2499/demo2446/JS2446.js
@@ -0,0 +1,3 @@
+﻿var haveConflict = function (event1, event2) {
+    return !(event1[1] < event2[0] || event2[1] < event1[0]);
+};
