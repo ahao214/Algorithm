0a2c246fd689f6dfbe880bbdb44abf7aa1eb6cb7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-19 00:05:06 +0800

    1079. 活字印刷

--- Ahao.LeetCode/1000-1099/demo1079/Class1079.cs
+++ Ahao.LeetCode/1000-1099/demo1079/Class1079.cs
@@ -9,6 +9,42 @@ namespace Ahao.LeetCode._1000_1099.demo1079
     public class Class1079
     {
 
+        public int NumTilePossibilities(string tiles)
+        {
+            IDictionary<char, int> count = new Dictionary<char, int>();
+            foreach (char t in tiles)
+            {
+                if (count.ContainsKey(t))
+                {
+                    count[t]++;
+                }
+                else
+                {
+                    count.Add(t, 1);
+                }
+            }
+            ISet<char> tile = new HashSet<char>(count.Keys);
+            return DFS(tiles.Length, count, tile) - 1;
+        }
+
+        private int DFS(int i, IDictionary<char, int> count, ISet<char> tile)
+        {
+            if (i == 0)
+            {
+                return 1;
+            }
+            int res = 1;
+            foreach (char t in tile)
+            {
+                if (count[t] > 0)
+                {
+                    count[t]--;
+                    res += DFS(i - 1, count, tile);
+                    count[t]++;
+                }
+            }
+            return res;
+        }
 
     }
 }
