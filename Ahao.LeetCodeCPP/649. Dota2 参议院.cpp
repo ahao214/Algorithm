6ac14229b9b3e453522f6016587be513f56b18fe
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-18 23:48:39 +0800

    649. Dota2 参议院

---
+++ Ahao.LeetCodeCPP/649. Dota2 参议院.cpp
@@ -0,0 +1,31 @@
+#include<vector>
+#include<string>
+
+using namespace std;
+
+
+
+/*
+649. Dota2 ≤Œ“È‘∫
+*/
+class Solution {
+public:
+	string predictPartyVictory(string senate) {
+		queue<int> r, d;
+		for (int i = 0; i < senate.size(); i++)
+		{
+			if (senate[i] == 'R') r.push(i);
+			else d.push(i);
+		}
+
+		int n = senate.size();
+		while (r.size() && d.size())
+		{
+			if (r.front() < d.front()) r.push(r.front() + n);
+			else d.push(d.front() + n);
+			r.pop(), d.pop();
+		}
+		if (r.size()) return "Radiant";
+		return "Dire";
+	}
+};
