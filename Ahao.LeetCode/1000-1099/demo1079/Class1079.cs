61c7f2615f843971e2d9f538ba28c0f9a8368710
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-19 00:04:44 +0800

    1079. 活字印刷

---
+++ Ahao.LeetCode/1000-1099/demo1079/Class1079.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1079
+{
+    public class Class1079
+    {
+
+
+    }
+}
--- Ahao.LeetCodeCPP/1079. 活字印刷.cpp
+++ Ahao.LeetCodeCPP/1079. 活字印刷.cpp
@@ -1,4 +1,6 @@
 #include<vector>
+#include <unordered_map>
+#include <set>
 
 using namespace std;
 
@@ -10,3 +12,32 @@ using namespace std;
 
 注意：本题中，每个活字字模只能使用一次。
 */
+
+class Solution {
+public:
+    int numTilePossibilities(string tiles) {
+        unordered_map<char, int> count;
+        set<char> tile;
+        int n = tiles.length();
+        for (char c : tiles) {
+            count[c]++;
+            tile.insert(c);
+        }
+        return dfs(count, tile, n) - 1;
+    }
+
+    int dfs(unordered_map<char, int>& count, set<char>& tile, int i) {
+        if (i == 0) {
+            return 1;
+        }
+        int res = 1;
+        for (char t : tile) {
+            if (count[t] > 0) {
+                count[t]--;
+                res += dfs(count, tile, i - 1);
+                count[t]++;
+            }
+        }
+        return res;
+    }
+};
