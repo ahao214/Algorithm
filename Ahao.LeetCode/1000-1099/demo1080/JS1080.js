3207353d86a5e536bbff7f5eec6364329fb1bc11
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-22 00:10:23 +0800

    1080. 根到叶路径上的不足节点

--- Ahao.LeetCode/1000-1099/demo1080/JS1080.js
+++ Ahao.LeetCode/1000-1099/demo1080/JS1080.js
@@ -1,1 +1,29 @@
-﻿
+﻿var sufficientSubset = function (root, limit) {
+    const haveSufficient = checkSufficientLeaf(root, 0, limit);
+    return haveSufficient ? root : null;
+};
+
+var checkSufficientLeaf = function (node, sum, limit) {
+    if (node == null) {
+        return false;
+    }
+    if (node.left == null && node.right == null) {
+        return node.val + sum >= limit;
+    }
+    const haveSufficientLeft = checkSufficientLeaf(node.left, sum + node.val, limit);
+    const haveSufficientRight = checkSufficientLeaf(node.right, sum + node.val, limit);
+    if (!haveSufficientLeft) {
+        node.left = null;
+    }
+    if (!haveSufficientRight) {
+        node.right = null;
+    }
+    return haveSufficientLeft || haveSufficientRight;
+};
+
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
