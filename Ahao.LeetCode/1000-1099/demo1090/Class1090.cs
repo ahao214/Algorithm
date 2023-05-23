bd4e404b844b42f5d0b557436ea14f555e81770b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-23 12:57:25 +0800

    1090. 受标签影响的最大值

---
+++ Ahao.LeetCode/1000-1099/demo1090/Class1090.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1090
+{
+    public class Class1090
+    {
+
+        public int LargestValsFromLabels(int[] values, int[] labels, int numWanted, int useLimit)
+        {
+            int n = values.Length;
+            int[] id = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                id[i] = i;
+            }
+            Array.Sort(id, (a, b) => values[b] - values[a]);
+
+            int ans = 0, choose = 0;
+            IDictionary<int, int> cnt = new Dictionary<int, int>();
+            for (int i = 0; i < n && choose < numWanted; ++i)
+            {
+                int label = labels[id[i]];
+                if (cnt.ContainsKey(label) && cnt[label] == useLimit)
+                {
+                    continue;
+                }
+                ++choose;
+                ans += values[id[i]];
+                cnt.TryAdd(label, 0);
+                ++cnt[label];
+            }
+            return ans;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1000-1099/demo1090/JS1090.js
@@ -0,0 +1,21 @@
+﻿var largestValsFromLabels = function (values, labels, numWanted, useLimit) {
+    const n = values.length;
+    const idx = Array.from(Array(n), (_, i) => i);
+    idx.sort((i, j) => values[j] - values[i]);
+
+    let ans = 0, choose = 0;
+    const cnt = {};
+    for (let i = 0; i < n; i++) {
+        const label = labels[idx[i]];
+        if (cnt[label] === useLimit) {
+            continue;
+        }
+        choose++;
+        ans += values[idx[i]];
+        cnt[label] = (cnt[label] || 0) + 1;
+        if (choose === numWanted) {
+            break;
+        }
+    }
+    return ans;
+}
