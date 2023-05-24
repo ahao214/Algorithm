208c289be75c1d571c2cf06b0c999726e8e27cb2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-25 00:14:29 +0800

    2451. 差值数组不同的字符串

---
+++ Ahao.LeetCode/2400-2499/demo2451/Class2451.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2451
+{
+    public class Class2451
+    {
+
+        public string OddString(string[] words)
+        {
+            int[] diff0 = Get(words[0]);
+            int[] diff1 = Get(words[1]);
+            if (Enumerable.SequenceEqual(diff0, diff1))
+            {
+                for (int i = 2; i < words.Length; i++)
+                {
+                    if (!Enumerable.SequenceEqual(diff0, Get(words[i])))
+                    {
+                        return words[i];
+                    }
+                }
+            }
+            return Enumerable.SequenceEqual(diff0, Get(words[2])) ? words[1] : words[0];
+        }
+
+        public int[] Get(string word)
+        {
+            int[] diff = new int[word.Length - 1];
+            for (int i = 0; i + 1 < word.Length; i++)
+            {
+                diff[i] = word[i + 1] - word[i];
+            }
+            return diff;
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/2400-2499/demo2451/JS2451.js
@@ -0,0 +1,20 @@
+﻿function get(word) {
+    let diff = new Array(word.length - 1);
+    for (let i = 0; i + 1 < word.length; i++) {
+        diff[i] = word.charCodeAt(i + 1) - word.charCodeAt(i);
+    }
+    return diff.toString();
+}
+
+var oddString = function (words) {
+    let diff0 = get(words[0]);
+    let diff1 = get(words[1]);
+    if (diff0 === diff1) {
+        for (let i = 2; i < words.length; i++) {
+            if (diff0 != get(words[i])) {
+                return words[i];
+            }
+        }
+    }
+    return diff0 === get(words[2]) ? words[1] : words[0]
+}
