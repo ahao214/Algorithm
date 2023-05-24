d3aeba487b55496b1b09fdd7983ac7c1bf99006c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-24 14:19:52 +0800

    1377. T 秒后青蛙的位置

--- Ahao.LeetCodeCPP/1377. T 秒后青蛙的位置.cpp
+++ Ahao.LeetCodeCPP/1377. T 秒后青蛙的位置.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 /*
 1377. T 秒后青蛙的位置
+
 给你一棵由 n 个顶点组成的无向树，顶点编号从 1 到 n。青蛙从 顶点 1 开始起跳。规则如下：
 
 在一秒内，青蛙从它所在的当前顶点跳到另一个 未访问 过的顶点（如果它们直接相连）。
@@ -53,5 +54,34 @@ public:
 				res = max(res, dfs(s, u, t - 1, target, p * 1.0 / k));
 		}
 		return res;
+	}
+};
+
+
+class Solution {
+public:
+	double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
+		vector<vector<int>> G(n + 1);
+		for (int i = 0; i < edges.size(); ++i) {
+			G[edges[i][0]].push_back(edges[i][1]);
+			G[edges[i][1]].push_back(edges[i][0]);
+		}
+		vector<bool> visited(n + 1, false);
+		return dfs(G, visited, 1, t, target);
+	}
+
+	double dfs(vector<vector<int>>& G, vector<bool>& visited, int i, int t, int target) {
+		int nxt = i == 1 ? G[i].size() : G[i].size() - 1;
+		if (t == 0 || nxt == 0) {
+			return i == target ? 1.0 : 0.0;
+		}
+		visited[i] = true;
+		double ans = 0.0;
+		for (int j : G[i]) {
+			if (!visited[j]) {
+				ans += dfs(G, visited, j, t - 1, target);
+			}
+		}
+		return ans / nxt;
 	}
 };
