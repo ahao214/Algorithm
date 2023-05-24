6e0db98d1bed846f1e497aca03383e824a311ff7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-24 14:18:41 +0800

    1377. T 秒后青蛙的位置

---
+++ Ahao.LeetCode/1300-1399/demo1377/Class1377.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1300_1399.demo1377
+{
+    public class Class1377
+    {
+
+        public double FrogPosition(int n, int[][] edges, int t, int target)
+        {
+            List<List<int>> g = new List<List<int>>();
+            for (int i = 0; i <= n; i++)
+            {
+                g.Add(new List<int>());
+            }
+            for (int i = 0; i < edges.Length; i++)
+            {
+                g[edges[i][0]].Add(edges[i][1]);
+                g[edges[i][1]].Add(edges[i][0]);
+            }
+            bool[] seen = new bool[n + 1];
+            return Dfs(g, seen, 1, t, target);
+        }
+
+        private double Dfs(List<List<int>> g, bool[] seen, int i, int t, int target)
+        {
+            int nxt = i == 1 ? g[i].Count : g[i].Count - 1;
+            if (t == 0 || nxt == 0)
+            {
+                return i == target ? 1.0 : 0.0;
+            }
+            seen[i] = true;
+            double ans = 0.0;
+            foreach (int j in g[i])
+            {
+                if (!seen[j])
+                {
+                    ans += Dfs(g, seen, j, t - 1, target);
+                }
+            }
+            return ans / nxt;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1300-1399/demo1377/JS1377.js
@@ -0,0 +1,27 @@
+﻿var frogPosition = function (n, edges, t, target) {
+    let G = [];
+    for (let i = 0; i <= n; i++) {
+        G.push([]);
+    }
+    for (let e of edges) {
+        G[e[0]].push(e[1]);
+        G[e[1]].push(e[0]);
+    }
+    let seen = new Array(n).fill(false);
+    return dfs(G, seen, 1, t, target);
+}
+
+function dfs(G, seen, i, t, target) {
+    let nxt = i == 1 ? G[i].length : G[i].length - 1;
+    if (t == 0 || nxt == 0) {
+        return i == target ? 1.0 : 0.0;
+    }
+    seen[i] = true;
+    let ans = 0.0;
+    for (let j of G[i]) {
+        if (!seen[j]) {
+            ans += dfs(G, seen, j, t - 1, target);
+        }
+    }
+    return ans / nxt;
+}
