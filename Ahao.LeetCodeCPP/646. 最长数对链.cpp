5bf183e3648d9a6e8ec672d4b19956140e6b0122
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-26 19:27:01 +0800

    646. 最长数对链

---
+++ Ahao.LeetCodeCPP/646. 最长数对链.cpp
@@ -0,0 +1,29 @@
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+
+/*
+646. 最长数对链
+*/
+class Solution {
+public:
+	int findLongestChain(vector<vector<int>>& pairs) {
+		sort(pairs.begin(), pairs.end(), [](vector<int>& a, vector<int>& b) {
+			return a[1] < b[1];
+			});
+
+		int res = 1, ed = pairs[0][1];
+		for (auto& p : pairs)
+		{
+			if (p[0] > ed)
+			{
+				res++;
+				ed = p[1];
+			}
+		}
+		return res;
+	}
+};
