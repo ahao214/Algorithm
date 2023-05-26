72cc3d47701c12e8a211ca8227196eb342fb8701
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-26 19:25:56 +0800

    647. 回文子串

---
+++ Ahao.LeetCodeCPP/647. 回文子串.cpp
@@ -0,0 +1,33 @@
+#include<vector>
+#include <string>
+
+using namespace std;
+
+
+/*
+647. 回文子串
+*/
+class Solution {
+public:
+	int countSubstrings(string s) {
+		int res = 0;
+		for (int i = 0; i < s.size(); i++)
+		{
+			//枚举长度为奇数的情况
+			for (int j = i, k = i; j >= 0 && k < s.size(); j--, k++)
+			{
+				if (s[j] != s[k])
+					break;
+				res++;
+			}
+			//偶数情况
+			for (int j = i, k = i + 1; j >= 0 && k < s.size(); j--, k++)
+			{
+				if (s[j] != s[k])
+					break;
+				res++;
+			}
+		}
+		return res;
+	}
+};
