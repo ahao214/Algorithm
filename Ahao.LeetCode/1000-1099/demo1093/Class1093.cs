a5e0340c0629bcca179f1b5bdba4a3b5ea31f021
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-27 12:35:04 +0800

    1093. 大样本统计

---
+++ Ahao.LeetCode/1000-1099/demo1093/Class1093.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1093
+{
+    public class Class1093
+    {
+
+        public double[] SampleStats(int[] count)
+        {
+            int n = count.Length;
+            int total = count.Sum();
+            double mean = 0.0;
+            double median = 0.0;
+            int minnum = 256;
+            int maxnum = 0;
+            int mode = 0;
+
+            int left = (total + 1) / 2;
+            int right = (total + 2) / 2;
+            int cnt = 0;
+            int maxfreq = 0;
+            long sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += (long)count[i] * i;
+                if (count[i] > maxfreq)
+                {
+                    maxfreq = count[i];
+                    mode = i;
+                }
+                if (count[i] > 0)
+                {
+                    if (minnum == 256)
+                    {
+                        minnum = i;
+                    }
+                    maxnum = i;
+                }
+                if (cnt < right && cnt + count[i] >= right)
+                {
+                    median += i;
+                }
+                if (cnt < left && cnt + count[i] >= left)
+                {
+                    median += i;
+                }
+                cnt += count[i];
+            }
+            mean = (double)sum / total;
+            median = median / 2.0;
+            return new double[] { minnum, maxnum, mean, median, mode };
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1000-1099/demo1093/JS1093.js
@@ -0,0 +1,38 @@
+﻿var sampleStats = function (count) {
+    let n = count.length;
+    let total = count.reduce((acc, cur) => acc + cur, 0);
+    let mean = 0.0;
+    let median = 0.0;
+    let min_num = 256;
+    let max_num = 0;
+    let mode = 0;
+
+    let left = parseInt((total + 1) / 2);
+    let right = parseInt((total + 2) / 2);
+    let cnt = 0;
+    let maxfreq = 0;
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += count[i] * i;
+        if (count[i] > maxfreq) {
+            maxfreq = count[i];
+            mode = i;
+        }
+        if (count[i] > 0) {
+            if (min_num == 256) {
+                min_num = i;
+            }
+            max_num = i;
+        }
+        if (cnt < right && cnt + count[i] >= right) {
+            median += i;
+        }
+        if (cnt < left && cnt + count[i] >= left) {
+            median += i;
+        }
+        cnt += count[i];
+    }
+    mean = sum / total;
+    median = median / 2.0;
+    return [min_num, max_num, mean, median, mode];
+}
