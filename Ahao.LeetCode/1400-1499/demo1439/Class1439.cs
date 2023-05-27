d89977a7b32840319b93aeaca3e619750110b6d7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-28 00:12:10 +0800

    1439. 有序矩阵中的第 k 个最小数组和

--- Ahao.LeetCode/1400-1499/demo1439/Class1439.cs
+++ Ahao.LeetCode/1400-1499/demo1439/Class1439.cs
@@ -9,6 +9,109 @@ namespace Ahao.LeetCode._1400_1499.demo1439
     public class Class1439
     {
 
+        public int KthSmallest(int[][] mat, int k)
+        {
+            int m = mat.Length;
+            int[] prev = mat[0];
+            for (int i = 1; i < m; ++i)
+            {
+                prev = Merge(prev, mat[i], k);
+            }
+            return prev[k - 1];
+        }
 
+        public int[] Merge(int[] f, int[] g, int k)
+        {
+            if (g.Length > f.Length)
+            {
+                return Merge(g, f, k);
+            }
+
+            PriorityQueue<int[], int> pq = new PriorityQueue<int[], int>();
+            for (int i = 0; i < g.Length; ++i)
+            {
+                pq.Enqueue(new int[] { 0, i, f[0] + g[i] }, f[0] + g[i]);
+            }
+
+            IList<int> list = new List<int>();
+            while (k > 0 && pq.Count > 0)
+            {
+                int[] entry = pq.Dequeue();
+                list.Add(entry[2]);
+                if (entry[0] + 1 < f.Length)
+                {
+                    pq.Enqueue(new int[] { entry[0] + 1, entry[1], f[entry[0] + 1] + g[entry[1]] }, f[entry[0] + 1] + g[entry[1]]);
+                }
+                --k;
+            }
+
+            return list.ToArray();
+        }
+
+
+
+        public int KthSmallest2(int[][] mat, int k)
+        {
+            int m = mat.Length;
+            int[] prev = mat[0];
+            for (int i = 1; i < m; ++i)
+            {
+                prev = Merge2(prev, mat[i], k);
+            }
+            return prev[k - 1];
+        }
+
+        public int[] Merge2(int[] f, int[] g, int k)
+        {
+            int left = f[0] + g[0], right = f[f.Length - 1] + g[g.Length - 1], thres = 0;
+            k = Math.Min(k, f.Length * g.Length);
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                int rptr = g.Length - 1, cnt = 0;
+                for (int lptr = 0; lptr < f.Length; ++lptr)
+                {
+                    while (rptr >= 0 && f[lptr] + g[rptr] > mid)
+                    {
+                        --rptr;
+                    }
+                    cnt += rptr + 1;
+                }
+                if (cnt >= k)
+                {
+                    thres = mid;
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            IList<int> list = new List<int>();
+            int index = 0;
+            for (int i = 0; i < f.Length; ++i)
+            {
+                for (int j = 0; j < g.Length; ++j)
+                {
+                    int sum = f[i] + g[j];
+                    if (sum < thres)
+                    {
+                        list.Add(sum);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            while (list.Count < k)
+            {
+                list.Add(thres);
+            }
+            int[] ans = list.ToArray();
+            Array.Sort(ans);
+            return ans;
+        }
     }
 }
