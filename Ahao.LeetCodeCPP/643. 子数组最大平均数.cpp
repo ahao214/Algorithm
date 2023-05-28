8313787282ce2bd17943cbb28dcd04c4a7b1fcbe
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-28 20:46:31 +0800

    643. 子数组最大平均数 I

--- Ahao.LeetCodeCPP/643. 子数组最大平均数.cpp
+++ Ahao.LeetCodeCPP/643. 子数组最大平均数.cpp
@@ -28,3 +28,22 @@ public:
 		return res / k;
 	}
 };
+
+
+class Solution {
+public:
+	double findMaxAverage(vector<int>& nums, int k) {
+		double res = -1e5;
+		for (int i = 0, j = 0, s = 0; i < nums.size(); i++)
+		{
+			s += nums[i];
+			if (i - j + 1 > k) s -= nums[j++];
+			if (i >= k - 1)
+			{
+				res = max(res, s / (double)k);
+			}
+		}
+		return res;
+	}
+};
+
