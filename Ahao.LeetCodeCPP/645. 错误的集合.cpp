3c036fe3482c1e251095848cffb0780c65ddd640
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-28 20:45:00 +0800

    645. 错误的集合

---
+++ Ahao.LeetCodeCPP/645. 错误的集合.cpp
@@ -0,0 +1,28 @@
+#include<vector>
+
+using namespace std;
+
+
+/*
+645. ´íÎóµÄ¼¯ºÏ
+*/
+class Solution {
+public:
+	vector<int> findErrorNums(vector<int>& nums) {
+		vector<int> res(2);
+		for (auto x : nums)
+		{
+			int k = abs(x);
+			if (nums[k - 1] < 0)
+				res[0] = k;
+			nums[k - 1] *= -1;
+		}
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (nums[i] > 0 && i + 1 != res[0])
+				res[1] = i + 1;
+		}
+		return res;
+	}
+};
