7b0e226679f39862d13f2966f9ece6dd3108d5ad
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-29 14:21:15 +0800

    2455. 可被三整除的偶数的平均值

---
+++ Ahao.LeetCode/2400-2499/demo2455/Class2455.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2455
+{
+    public class Class2455
+    {
+
+        public int AverageValue(int[] nums)
+        {
+            int total = 0;
+            int count = 0;
+            int n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int a = nums[i];
+                if (a % 6 == 0)
+                {
+                    total += a;
+                    count++;
+                }
+            }
+            return count > 0 ? total / count : 0;
+        }
+
+
+
+
+    }
+}
