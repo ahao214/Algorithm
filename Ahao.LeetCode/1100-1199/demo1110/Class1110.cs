fb6e0304f89042767e88a46a980444632b983fc1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-30 09:07:58 +0800

    1110. 删点成林

---
+++ Ahao.LeetCode/1100-1199/demo1110/Class1110.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1100_1199.demo1110
+{
+    public class Class1110
+    {
+
+
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1100-1199/demo1110/JS1110.js
@@ -0,0 +1,30 @@
+﻿function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+
+var delNodes = function (root, to_delete) {
+    const toDeleteSet = new Set(to_delete);
+    const roots = [];
+    dfs(root, true, toDeleteSet, roots);
+    return roots;
+}
+
+function dfs(node, isRoot, toDeleteSet, roots) {
+    if (!node) {
+        return null;
+    }
+    const deleted = toDeleteSet.has(node.val);
+    node.left = dfs(node.left, deleted, toDeleteSet, roots);
+    node.right = dfs(node.right, deleted, toDeleteSet, roots);
+    if (deleted) {
+        return null;
+    } else {
+        if (isRoot) {
+            roots.push(node);
+        }
+        return node;
+    }
+}
