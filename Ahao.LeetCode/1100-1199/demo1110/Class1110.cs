4d4a93c20e6c055eb111bc0b32269055e97b664c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-30 09:08:25 +0800

    1110. 删点成林

--- Ahao.LeetCode/1100-1199/demo1110/Class1110.cs
+++ Ahao.LeetCode/1100-1199/demo1110/Class1110.cs
@@ -9,6 +9,41 @@ namespace Ahao.LeetCode._1100_1199.demo1110
     public class Class1110
     {
 
+        public IList<TreeNode> DelNodes(TreeNode root, int[] to_delete)
+        {
+            ISet<int> toDeleteSet = new HashSet<int>();
+            foreach (int val in to_delete)
+            {
+                toDeleteSet.Add(val);
+            }
+            IList<TreeNode> roots = new List<TreeNode>();
+            DFS(root, true, toDeleteSet, roots);
+            return roots;
+        }
+
+        public TreeNode DFS(TreeNode node, bool isRoot, ISet<int> toDeleteSet, IList<TreeNode> roots)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            bool deleted = toDeleteSet.Contains(node.val);
+            node.left = DFS(node.left, deleted, toDeleteSet, roots);
+            node.right = DFS(node.right, deleted, toDeleteSet, roots);
+            if (deleted)
+            {
+                return null;
+            }
+            else
+            {
+                if (isRoot)
+                {
+                    roots.Add(node);
+                }
+                return node;
+            }
+        }
+
 
     }
 
