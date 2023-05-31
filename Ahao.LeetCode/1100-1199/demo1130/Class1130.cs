f2915ca62f60544af566b86d159b124b0a1e3b36
liupenghao@fulan.com.cn <haochen214@163.com>
2023-05-31 15:00:59 +0800

    1130. 叶值的最小代价生成树

---
+++ Ahao.LeetCode/1100-1199/demo1130/Class1130.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1100_1199.demo1130
+{
+    public class Class1130
+    {
+        public int MctFromLeafValues(int[] arr)
+        {
+            int n = arr.Length;
+            int[][] dp = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = new int[n];
+                Array.Fill(dp[i], int.MaxValue / 4);
+            }
+            int[][] mval = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                mval[i] = new int[n];
+            }
+            for (int j = 0; j < n; j++)
+            {
+                mval[j][j] = arr[j];
+                dp[j][j] = 0;
+                for (int i = j - 1; i >= 0; i--)
+                {
+                    mval[i][j] = Math.Max(arr[i], mval[i + 1][j]);
+                    for (int k = i; k < j; k++)
+                    {
+                        dp[i][j] = Math.Min(dp[i][j], dp[i][k] + dp[k + 1][j] + mval[i][k] * mval[k + 1][j]);
+                    }
+                }
+            }
+            return dp[0][n - 1];
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/1100-1199/demo1130/JS1130.js
@@ -0,0 +1,18 @@
+﻿var mctFromLeafValues = function (arr) {
+    const n = arr.length;
+    const dp = Array(n).fill(0).map(() => Array(n).fill(Infinity));
+    const mval = Array(n).fill(0).map(() => Array(n));
+    for (let j = 0; j < n; j++) {
+        mval[j][j] = arr[j];
+        dp[j][j] = 0;
+    }
+    for (let i = n - 1; i >= 0; i--) {
+        for (let j = i + 1; j < n; j++) {
+            mval[i][j] = Math.max(arr[i], mval[i + 1][j]);
+            for (let k = i; k < j; k++) {
+                dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j] + mval[i][k] * mval[k + 1][j]);
+            }
+        }
+    }
+    return dp[0][n - 1];
+}
