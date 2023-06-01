2c252962fa0196713f2013575cdd9477ecfdf4a7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-01 22:15:50 +0800

    525. 连续数组

--- Ahao.LeetCodeCPP/525. 连续数组.cpp
+++ Ahao.LeetCodeCPP/525. 连续数组.cpp
@@ -24,3 +24,26 @@ public:
 		return res;
 	}
 };
+
+
+
+
+class Solution {
+public:
+	int findMaxLength(vector<int>& nums) {
+		int n = nums.size();
+		unordered_map<int, int> hash;
+		hash[0] = 0;
+		int res = 0;
+		for (int i = 1, one = 0, zero = 0; i <= n; i++)
+		{
+			int x = nums[i - 1];
+			if (x == 0) zero++;
+			else one++;
+			int s = one - zero;
+			if (hash.count(s)) res = max(res, i - hash[s]);
+			else hash[s] = i;
+		}
+		return res;
+	}
+};
