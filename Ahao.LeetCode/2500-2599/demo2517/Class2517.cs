b33106f4b86100cfc84a1b24ff40341712477c36
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-01 13:48:17 +0800

    2517. 礼盒的最大甜蜜度

--- Ahao.LeetCode/2500-2599/demo2517/Class2517.cs
+++ Ahao.LeetCode/2500-2599/demo2517/Class2517.cs
@@ -9,6 +9,41 @@ namespace Ahao.LeetCode._2500_2599.demo2517
     public class Class2517
     {
 
+        public int MaximumTastiness(int[] price, int k)
+        {
+            Array.Sort(price);
+            int n = price.Length;
+            int left = 0;
+            int right = price[n - 1] - price[0];
+            while (left < right)
+            {
+                int mid = (left + right + 1) / 2;
+                if (Check(price, k, mid))
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return left;
+        }
+
+        public bool Check(int[] price, int k, int tastiness)
+        {
+            int prev = int.MinValue / 2;
+            int cnt = 0;
+            foreach (int p in price)
+            {
+                if (p - prev >= tastiness)
+                {
+                    cnt++;
+                    prev = p;
+                }
+            }
+            return cnt >= k;
+        }
 
     }
 }
