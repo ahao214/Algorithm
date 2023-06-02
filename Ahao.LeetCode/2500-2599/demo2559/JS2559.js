65a23ce124311bfbaa2de3c2519395bec2b8f74c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-02 10:09:36 +0800

    2559. 统计范围内的元音字符串数

---
+++ Ahao.LeetCode/2500-2599/demo2559/JS2559.js
@@ -0,0 +1,22 @@
+﻿var vowelStrings = function (words, queries) {
+    let n = words.length;
+    let prefixSums = new Array(n + 1).fill(0);
+    for (let i = 0; i < n; i++) {
+        let value = isVowelString(words[i]) ? 1 : 0;
+        prefixSums[i + 1] = prefixSums[i] + value;
+    }
+    let ans = [];
+    for (let i = 0; i < queries.length; i++) {
+        let start = queries[i][0], end = queries[i][1];
+        ans.push(prefixSums[end + 1] - prefixSums[start]);
+    }
+    return ans;
+}
+
+function isVowelString(word) {
+    return isVowelLetter(word[0]) && isVowelLetter(word[word.length - 1]);
+}
+
+function isVowelLetter(c) {
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
