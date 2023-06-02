5c56968d67e11890901a9546c172f9ac2a3d1577
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-02 20:10:05 +0800

    522. 最长特殊序列 II

--- Ahao.LeetCodeCPP/522. 最长特殊序列 II.cpp
+++ Ahao.LeetCodeCPP/522. 最长特殊序列 II.cpp
@@ -34,3 +34,41 @@ public:
         return ans;
     }
 };
+
+
+
+/*
+522. 最长特殊序列 II
+*/
+class Solution {
+public:
+    bool check(string& a, string& b)
+    {
+        int k = 0;
+        for (auto c : b)
+        {
+            if (k < a.size() && c == a[k])
+                k++;
+        }
+        return k == a.size();
+    }
+
+    int findLUSlength(vector<string>& strs) {
+        int res = -1;
+        for (int i = 0; i < strs.size(); i++)
+        {
+            bool isSub = false;
+            for (int j = 0; j < strs.size(); j++)
+            {
+                if (i != j && check(strs[i], strs[j]))
+                {
+                    isSub = true;
+                    break;
+                }
+            }
+            if (!isSub)
+                res = max(res, (int)strs[i].size());
+        }
+        return res;
+    }
+};
