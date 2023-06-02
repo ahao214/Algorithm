e8ef886bdac65ec792abc60ec20997ad03d48492
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-03 00:23:22 +0800

    1156. 单字符重复子串的最大长度

--- Ahao.LeetCode/1100-1199/demo1156/Class1156.cs
+++ Ahao.LeetCode/1100-1199/demo1156/Class1156.cs
@@ -9,7 +9,47 @@ namespace Ahao.LeetCode._1100_1199.demo1156
     public class Class1156
     {
 
-        public int MaxRepOpt1(String text)
+        public int MaxRepOpt1(string text)
+        {
+            IDictionary<char, int> count = new Dictionary<char, int>();
+            foreach (char c in text)
+            {
+                count.TryAdd(c, 0);
+                count[c]++;
+            }
+
+            int res = 0;
+            for (int i = 0; i < text.Length;)
+            {
+                // step1: 找出当前连续的一段 [i, j)
+                int j = i;
+                while (j < text.Length && text[j] == text[i])
+                {
+                    j++;
+                }
+                int curCnt = j - i;
+
+                // step2: 如果这一段长度小于该字符出现的总数，并且前面或后面有空位，则使用 curCnt + 1 更新答案
+                if (curCnt < (count.ContainsKey(text[i]) ? count[text[i]] : 0) && (j < text.Length || i > 0))
+                {
+                    res = Math.Max(res, curCnt + 1);
+                }
+
+                // step3: 找到这一段后面与之相隔一个不同字符的另一段 [j + 1, k)，如果不存在则 k = j + 1 
+                int k = j + 1;
+                while (k < text.Length && text[k] == text[i])
+                {
+                    k++;
+                }
+                res = Math.Max(res, Math.Min(k - i, count.ContainsKey(text[i]) ? count[text[i]] : 0));
+                i = j;
+            }
+            return res;
+        }
+
+
+
+        public int MaxRepOpt1_2(String text)
         {
             char[] chars = text.ToCharArray();
             int[] count = new int[26];
