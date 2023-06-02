cd02a7cfcd2718f553057949dec43e36be79796a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-03 00:20:41 +0800

    1156. 单字符重复子串的最大长度

---
+++ Ahao.LeetCode/1100-1199/demo1156/Class1156.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1100_1199.demo1156
+{
+    public class Class1156
+    {
+
+        public int MaxRepOpt1(String text)
+        {
+            char[] chars = text.ToCharArray();
+            int[] count = new int[26];
+            int n = chars.Length, cnt = 1, res = 1;
+            // 记录每种字符出现次数
+            foreach (var c in chars)
+            {
+                count[c - 'a']++;
+            }
+            // 选定字符开始比对
+            char last = chars[0];
+            // 从第二个开始匹配
+            for (int i = 1; i < n; i++)
+            {
+                // 匹配成功 当前最长++
+                if (chars[i] == last)
+                {
+                    cnt++;
+                }
+                else
+                {
+                    // 从i+1往后找
+                    int index = i + 1;
+                    // aaabaaa这种只是相隔一个字符的情况，继续往下遍历
+                    while (index < n && chars[index] == last)
+                    {
+                        ++cnt;
+                        ++index;
+                    }
+                    // 如果不是相隔一个字符的情况则更新结果
+                    if (count[last - 'a'] > cnt) ++cnt;
+                    ans = Math.Max(ans, cnt);
+                    // 当前最长重新设置为1，待比对字符更新为当前字符
+                    cnt = 1;
+                    last = chars[i];
+                }
+            }
+            // 遍历完成后确定最终答案，选中的字符当前最大长度若小于出现评率则可+1
+            // 如 abbaaaa 最终last = chars[3] cnt = 4
+            if (count[last - 'a'] > cnt) ++cnt;
+            ans = Math.Max(ans, cnt);
+            return ans;
+        }
+
+
+    }
+}
