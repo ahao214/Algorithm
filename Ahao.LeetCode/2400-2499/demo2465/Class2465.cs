ad62c9a33aaf15e32c161079d3c4d2cced3cc6f7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-04 14:00:32 +0800

    2465. 不同的平均值数目

---
+++ Ahao.LeetCode/2400-2499/demo2465/Class2465.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2465
+{
+    public class Class2465
+    {
+        public int DistinctAverages(int[] nums)
+        {
+            ISet<int> st = new HashSet<int>();
+            Array.Sort(nums);
+            for (int i = 0, j = nums.Length - 1; i < j; i++, j--)
+            {
+                st.Add(nums[i] + nums[j]);
+            }
+            return st.Count();
+
+        }
+    }
+}
