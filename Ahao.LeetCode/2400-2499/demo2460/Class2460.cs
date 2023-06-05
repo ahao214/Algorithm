f4b93d98c9fcae9ea98dbb603e56e082ef07d648
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-05 10:44:54 +0800

    2460. 对数组执行操作

--- Ahao.LeetCode/2400-2499/demo2460/Class2460.cs
+++ Ahao.LeetCode/2400-2499/demo2460/Class2460.cs
@@ -29,5 +29,32 @@ namespace Ahao.LeetCode._2400_2499.demo2460
         }
 
 
+
+        public int[] ApplyOperations2(int[] nums)
+        {
+            int n = nums.Length;
+            for (int i = 0, j = 0; i < n; i++)
+            {
+                if (i + 1 < n && nums[i] == nums[i + 1])
+                {
+                    nums[i] *= 2;
+                    nums[i + 1] = 0;
+                }
+                if (nums[i] != 0)
+                {
+                    Swap(nums, i, j);
+                    j++;
+                }
+            }
+            return nums;
+        }
+
+        public void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
     }
 }
