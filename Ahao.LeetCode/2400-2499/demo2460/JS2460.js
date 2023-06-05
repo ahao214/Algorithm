10246dbfa75306ce34d23945011b6854c2d4407b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-05 10:44:29 +0800

    2460. 对数组执行操作

---
+++ Ahao.LeetCode/2400-2499/demo2460/JS2460.js
@@ -0,0 +1,15 @@
+﻿var applyOperations = function (nums) {
+    let n = nums.length;
+    let j = 0;
+    for (let i = 0; i < n; i++) {
+        if (i + 1 < n && nums[i] == nums[i + 1]) {
+            nums[i] *= 2;
+            nums[i + 1] = 0;
+        }
+        if (nums[i] != 0) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            j++;
+        }
+    }
+    return nums;
+};
