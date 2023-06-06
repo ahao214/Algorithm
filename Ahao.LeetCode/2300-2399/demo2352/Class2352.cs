b17372ec8cc6fdf86b4e8b53e500fc94f5566494
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-06 13:36:46 +0800

    2352. 相等行列对

---
+++ Ahao.LeetCode/2300-2399/demo2352/Class2352.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2352
+{
+    public class Class2352
+    {
+
+
+    }
+}
