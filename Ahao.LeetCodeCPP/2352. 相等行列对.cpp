939a082c867af9f88b01dfd33aa70fb103ff1b31
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-06 13:37:12 +0800

    2352. 相等行列对

--- Ahao.LeetCodeCPP/2352. 相等行列对.cpp
+++ Ahao.LeetCodeCPP/2352. 相等行列对.cpp
@@ -4,5 +4,35 @@ using namespace std;
 
 
 /*
+2352. 相等行列对
 
+给你一个下标从 0 开始、大小为 n x n 的整数矩阵 grid ，返回满足 Ri 行和 Cj 列相等的行列对 (Ri, Cj) 的数目。
+
+如果行和列以相同的顺序包含相同的元素（即相等的数组），则认为二者是相等的。
 */
+
+
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        int res = 0, n = grid.size();
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (equal(row, col, grid)) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    bool equal(int row, int col, vector<vector<int>>& grid) {
+        int n = grid.size();
+        for (int i = 0; i < n; i++) {
+            if (grid[row][i] != grid[i][col]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
