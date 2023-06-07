e6335ae6040189303e53ce16af1930b073103212
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-07 12:16:36 +0800

    2611. 老鼠和奶酪

---
+++ Ahao.LeetCode/2600-2699/demo2611/JS2611.js
@@ -0,0 +1,14 @@
+﻿var miceAndCheese = function (reward1, reward2, k) {
+    let ans = 0;
+    let n = reward1.length;
+    let diffs = new Array(n);
+    for (let i = 0; i < n; i++) {
+        ans += reward2[i];
+        diffs[i] = reward1[i] - reward2[i];
+    }
+    diffs.sort((a, b) => a - b);
+    for (let i = 1; i <= k; i++) {
+        ans += diffs[n - i];
+    }
+    return ans;
+}
