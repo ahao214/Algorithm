e6ff77d38cb5e4ab39c4b9acfda3e2e8ec686c6d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-07 12:18:18 +0800

    2611. 老鼠和奶酪

--- Ahao.LeetCode/2600-2699/demo2611/JS2611.js
+++ Ahao.LeetCode/2600-2699/demo2611/JS2611.js
@@ -12,3 +12,98 @@
     }
     return ans;
 }
+
+
+class Heap {
+    constructor() {
+        this.heap = [];
+    }
+
+    push(value) {
+        this.heap.push(value);
+        this.bubbleUp(this.heap.length - 1);
+    }
+
+    poll() {
+        const result = this.heap[0];
+        const end = this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heap[0] = end;
+            this.sinkDown(0);
+        }
+        return result;
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+
+    bubbleUp(index) {
+        const element = this.heap[index];
+        while (index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+            const parent = this.heap[parentIndex];
+            if (element >= parent) {
+                break;
+            }
+            this.heap[parentIndex] = element;
+            this.heap[index] = parent;
+            index = parentIndex;
+        }
+    }
+
+    sinkDown(index) {
+        const element = this.heap[index];
+        const length = this.heap.length;
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.heap[leftChildIndex];
+                if (leftChild < element) {
+                    swap = leftChildIndex;
+                }
+            }
+
+            if (rightChildIndex < length) {
+                rightChild = this.heap[rightChildIndex];
+                if ((swap === null && rightChild < element) ||
+                    (swap !== null && rightChild < leftChild)) {
+                    swap = rightChildIndex;
+                }
+            }
+
+            if (swap === null) {
+                break;
+            }
+
+            this.heap[index] = this.heap[swap];
+            this.heap[swap] = element;
+            index = swap;
+        }
+    }
+}
+
+var miceAndCheese = function (reward1, reward2, k) {
+    let ans = 0;
+    let n = reward1.length;
+    let pq = new Heap();
+    for (let i = 0; i < n; i++) {
+        ans += reward2[i];
+        pq.push(reward1[i] - reward2[i]);
+        if (pq.size() > k) {
+            pq.poll();
+        }
+    }
+    while (!pq.isEmpty()) {
+        ans += pq.poll();
+    }
+    return ans;
+}
