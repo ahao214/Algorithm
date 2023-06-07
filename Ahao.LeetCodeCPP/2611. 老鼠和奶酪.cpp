ad6845ba0df36cc046ed81199d4eb4941c77812c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-07 12:18:01 +0800

    2611. 老鼠和奶酪

--- Ahao.LeetCodeCPP/2611. 老鼠和奶酪.cpp
+++ Ahao.LeetCodeCPP/2611. 老鼠和奶酪.cpp
@@ -1,5 +1,6 @@
 #include<vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -30,6 +31,28 @@ public:
         sort(diffs.begin(), diffs.end());
         for (int i = 1; i <= k; i++) {
             ans += diffs[n - i];
+        }
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
+        int ans = 0;
+        int n = reward1.size();
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < n; i++) {
+            ans += reward2[i];
+            pq.emplace(reward1[i] - reward2[i]);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        while (!pq.empty()) {
+            ans += pq.top();
+            pq.pop();
         }
         return ans;
     }
