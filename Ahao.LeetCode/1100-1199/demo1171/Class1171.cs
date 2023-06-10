c1290d2e10fb15ee5ca4866748e0843063d2dd47
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-11 00:18:21 +0800

    1171. 从链表中删去总和值为零的连续节点

--- Ahao.LeetCode/1100-1199/demo1171/Class1171.cs
+++ Ahao.LeetCode/1100-1199/demo1171/Class1171.cs
@@ -30,6 +30,45 @@ namespace Ahao.LeetCode._1100_1199.demo1171
             return dummy.next;
         }
 
+
+        public ListNode RemoveZeroSumSublists2(ListNode head)
+        {
+            // <sum, lastNode>
+            Dictionary<int, ListNode> sumMap = new Dictionary<int, ListNode>();
+            ListNode dummy = new ListNode(-1, head);
+            ListNode p = head;
+            int sum = 0;
+            sumMap.Add(0, dummy);
+            while (p != null)
+            {
+                sum += p.val;
+                if (sumMap.ContainsKey(sum))
+                {
+                    ListNode node = sumMap.GetValueOrDefault(sum);
+                    ListNode del = node.next;
+                    node.next = p.next;
+
+                    int dSum = sum;
+                    while (del != p)
+                    {
+                        dSum += del.val;
+                        sumMap.Remove(dSum);
+                        del = del.next;
+                    }
+
+                }
+                else
+                {
+                    sumMap.Add(sum, p);
+                }
+                p = p.next;
+            }
+            return dummy.next;
+
+        }
+
+
+
     }
 
     public class ListNode
