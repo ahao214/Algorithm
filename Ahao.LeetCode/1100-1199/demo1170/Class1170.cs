b8b5e3b69bc4b8dc3d3f3e744505357666c09d38
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-10 08:20:06 +0800

    1170. 比较字符串最小字母出现频次

---
+++ Ahao.LeetCode/1100-1199/demo1170/Class1170.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1100_1199.demo1170
+{
+    public class Class1170
+    {
+
+        public int[] NumSmallerByFrequency(string[] queries, string[] words)
+        {
+            int[] count = new int[12];
+            foreach (string s in words)
+            {
+                count[F(s)]++;
+            }
+            for (int i = 9; i >= 1; i--)
+            {
+                count[i] += count[i + 1];
+            }
+            int[] res = new int[queries.Length];
+            for (int i = 0; i < queries.Length; i++)
+            {
+                string s = queries[i];
+                res[i] = count[F(s) + 1];
+            }
+            return res;
+        }
+
+        public int F(string s)
+        {
+            int cnt = 0;
+            char ch = 'z';
+            foreach (char c in s)
+            {
+                if (c < ch)
+                {
+                    ch = c;
+                    cnt = 1;
+                }
+                else if (c == ch)
+                {
+                    cnt++;
+                }
+            }
+            return cnt;
+        }
+
+
+    }
+}
