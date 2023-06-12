083332b7576477ae5d2e9efc708896891e6962e5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-13 00:54:05 +0800

    2475. 数组中不等三元组的数目

--- Ahao.LeetCodeCPP/2475. 数组中不等三元组的数目.cpp
+++ Ahao.LeetCodeCPP/2475. 数组中不等三元组的数目.cpp
@@ -1,5 +1,6 @@
 #include<vector>
 #include <algorithm>
+#include <unordered_map>
 
 
 using namespace std;
@@ -49,3 +50,18 @@ public:
 };
 
 
+//class Solution {
+//public:
+//    int unequalTriplets(vector<int>& nums) {
+//        unordered_map<int, int> count;
+//        for (auto x : nums) {
+//            count[x]++;
+//        }
+//        int res = 0, n = nums.size(), t = 0;
+//        for (auto [_, v] : count) {
+//            res += t * v * (n - t - v);
+//            t += v;
+//        }
+//        return res;
+//    }
+//};
