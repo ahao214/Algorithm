fc35d86b307bf70529a09e7bdbed3e5527c076a6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-13 00:52:00 +0800

    2475. 数组中不等三元组的数目

--- Ahao.LeetCode/2400-2499/demo2475/Class2475.cs
+++ Ahao.LeetCode/2400-2499/demo2475/Class2475.cs
@@ -28,5 +28,29 @@ namespace Ahao.LeetCode._2400_2499.demo2475
             return res;
         }
 
+
+
+
+        public int UnequalTriplets2(int[] nums)
+        {
+            Array.Sort(nums);
+            int res = 0, n = nums.Length;
+            for (int i = 0, j = 0; i < n; i = j)
+            {
+                while (j < n && nums[j] == nums[i])
+                {
+                    j++;
+                }
+                res += i * (j - i) * (n - j);
+            }
+            return res;
+        }
+
+        public int UnequalTriplets3(int[] nums)
+        {
+        
+        }
+
+
     }
 }
