a0e4af3c377eb044763dc79df6f788b39cc9a12c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-14 00:08:22 +0800

    1375. 二进制字符串前缀一致的次数

---
+++ Ahao.LeetCode/1300-1399/demo1375/Class1375.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1300_1399.demo1375
+{
+    public class Class1375
+    {
+
+        public int NumTimesAllBlue(int[] flips)
+        {
+            int n = flips.Length;
+            int ans = 0, right = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                right = Math.Max(right, flips[i]);
+                if (right == i + 1)
+                {
+                    ++ans;
+                }
+            }
+            return ans;
+        }
+
+
+    }
+}
