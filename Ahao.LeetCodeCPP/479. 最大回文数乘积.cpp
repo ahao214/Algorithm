1440a6f2772b64dda454c27aef9404c3f8037905
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-14 23:31:27 +0800

    479. 最大回文数乘积

--- Ahao.LeetCodeCPP/479. 最大回文数乘积.cpp
+++ Ahao.LeetCodeCPP/479. 最大回文数乘积.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 
+/*
+479. 最大回文数乘积
+*/
+
 class Solution {
 public:
     int largestPalindrome(int n) {
@@ -16,6 +20,29 @@ public:
             }
             for (long x = upper; x * x >= p; --x) {
                 if (p % x == 0) { // x 是 p 的因子
+                    return p % 1337;
+                }
+            }
+        }
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int largestPalindrome(int n) {
+        if (n == 1) return 9;
+        for (int half = pow(10, n) - 1; ; half--) { // 1、枚举回文数左半部分half
+            // 2、构造回文数p
+            long p = half;
+            for (int i = half; i > 0; i /= 10) {
+                p = p * 10 + i % 10;
+            }
+            // 3、判断该回位数能否被整除
+            for (long i = pow(10, n) - 1; i * i >= p; i--) {
+                if (p % i == 0) {
                     return p % 1337;
                 }
             }
