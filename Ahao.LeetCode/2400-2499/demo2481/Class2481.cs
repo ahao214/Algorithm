70ef4de5fdba20ff48cd3461270c8eecb6d7c586
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-17 00:10:32 +0800

    2481. 分割圆的最少切割次数

---
+++ Ahao.LeetCode/2400-2499/demo2481/Class2481.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2481
+{
+    class Class2481
+    {
+    }
+}
