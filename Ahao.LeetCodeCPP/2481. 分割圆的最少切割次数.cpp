06e432e733ffe6b1506cfbfa32a15694e71291ac
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-17 00:21:45 +0800

    2481. 分割圆的最少切割次数

--- Ahao.LeetCodeCPP/2481. 分割圆的最少切割次数.cpp
+++ Ahao.LeetCodeCPP/2481. 分割圆的最少切割次数.cpp
@@ -16,3 +16,16 @@ using namespace std;
 
 给你一个整数 n ，请你返回将圆切割成相等的 n 等分的 最少 切割次数。
 */
+
+class Solution {
+public:
+    int numberOfCuts(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return n / 2;
+        }
+        return n;
+    }
+};
