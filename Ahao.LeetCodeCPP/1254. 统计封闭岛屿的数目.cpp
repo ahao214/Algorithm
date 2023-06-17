e8e1110c65ad88d1b238f142c37f8b2fa6999277
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-18 00:05:53 +0800

    1254. 统计封闭岛屿的数目

---
+++ Ahao.LeetCodeCPP/1254. 统计封闭岛屿的数目.cpp
@@ -0,0 +1,53 @@
+#include<vector>
+#include <queue>
+
+using namespace std;
+
+/*
+1254. 统计封闭岛屿的数目
+
+二维矩阵 grid 由 0 （土地）和 1 （水）组成。岛是由最大的4个方向连通的 0 组成的群，封闭岛是一个 完全 由1包围（左、上、右、下）的岛。
+
+请返回 封闭岛屿 的数目。
+*/
+
+class Solution {
+public:
+    static constexpr int dir[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+    int closedIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    queue<pair<int, int>> qu;
+                    grid[i][j] = 1;
+                    bool closed = true;
+
+                    qu.push(make_pair(i, j));
+                    while (!qu.empty()) {
+                        auto [cx, cy] = qu.front();
+                        qu.pop();
+                        if (cx == 0 || cy == 0 || cx == m - 1 || cy == n - 1) {
+                            closed = false;
+                        }
+                        for (int i = 0; i < 4; i++) {
+                            int nx = cx + dir[i][0];
+                            int ny = cy + dir[i][1];
+                            if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 0) {
+                                grid[nx][ny] = 1;
+                                qu.emplace(nx, ny);
+                            }
+                        }
+                    }
+                    if (closed) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
