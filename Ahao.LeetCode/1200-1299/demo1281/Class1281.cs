6a32fe057e75265811f7a54731fae8bbc1c37f34
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-18 00:18:14 +0800

    1281. 整数的各位积和之差

---
+++ Ahao.LeetCode/1200-1299/demo1281/Class1281.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1200_1299.demo1281
+{
+    public class Class1281
+    {
+
+        public int SubtractProductAndSum(int n)
+        {
+            int product = 1, sum = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                product *= digit;
+                sum += digit;
+                n /= 10;
+            }
+            return product - sum;
+        }
+
+
+    }
+}
