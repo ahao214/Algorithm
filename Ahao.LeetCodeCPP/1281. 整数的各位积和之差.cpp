fff713d58dde4acfbc4be1bd679db408e2ffd7c2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-18 00:18:41 +0800

    1281. 整数的各位积和之差

--- Ahao.LeetCodeCPP/1281. 整数的各位积和之差.cpp
+++ Ahao.LeetCodeCPP/1281. 整数的各位积和之差.cpp
@@ -10,3 +10,16 @@ using namespace std;
 */
 
 
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int add = 0, mul = 1;
+        while (n > 0) {
+            int digit = n % 10;
+            n /= 10;
+            add += digit;
+            mul *= digit;
+        }
+        return mul - add;
+    }
+};
