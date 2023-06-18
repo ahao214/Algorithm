10bf82fb54d238ddfd9267fc79137c9967c59867
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-19 00:20:25 +0800

    1262. 可被三整除的最大和

--- Ahao.LeetCode/1200-1299/demo1262/JS1262.js
+++ Ahao.LeetCode/1200-1299/demo1262/JS1262.js
@@ -31,3 +31,47 @@ const getSum = (list, start, end) => {
 
 
 
+var maxSumDivThree = function (nums) {
+    const v = [[], [], []];
+    for (const num of nums) {
+        v[num % 3].push(num);
+    }
+    v[1].sort((a, b) => b - a);
+    v[2].sort((a, b) => b - a);
+
+    const tot = nums.reduce((sum, num) => sum + num, 0);
+    let remove = Infinity;
+
+    if (tot % 3 === 0) {
+        remove = 0;
+    } else if (tot % 3 === 1) {
+        if (v[1].length >= 1) {
+            remove = Math.min(remove, v[1][v[1].length - 1]);
+        }
+        if (v[2].length >= 2) {
+            remove = Math.min(remove, v[2][v[2].length - 2] + v[2][v[2].length - 1]);
+        }
+    } else {
+        if (v[1].length >= 2) {
+            remove = Math.min(remove, v[1][v[1].length - 2] + v[1][v[1].length - 1]);
+        }
+        if (v[2].length >= 1) {
+            remove = Math.min(remove, v[2][v[2].length - 1]);
+        }
+    }
+
+    return tot - remove;
+}
+
+
+var maxSumDivThree = function (nums) {
+    let f = [0, Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER];
+    for (const num of nums) {
+        const g = [...f];
+        for (let i = 0; i < 3; ++i) {
+            g[(i + num % 3) % 3] = Math.max(g[(i + num % 3) % 3], f[i] + num);
+        }
+        f = g;
+    }
+    return f[0];
+};
