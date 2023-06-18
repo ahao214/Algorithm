b1285a92f79cbcfd4d48e74514e05f5c97c41b1f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-19 00:18:28 +0800

    1262. 可被三整除的最大和

--- Ahao.LeetCode/1200-1299/demo1262/Class1262.cs
+++ Ahao.LeetCode/1200-1299/demo1262/Class1262.cs
@@ -6,8 +6,51 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode._1200_1299.demo1262
 {
-   public  class Class1262
+    public class Class1262
     {
+
+        public int MaxSumDivThree(int[] nums)
+        {
+            // 使用 v[0], v[1], v[2] 分别表示 a, b, c
+            IList<int>[] v = new IList<int>[3];
+            for (int i = 0; i < 3; ++i)
+            {
+                v[i] = new List<int>();
+            }
+            foreach (int num in nums)
+            {
+                v[num % 3].Add(num);
+            }
+            ((List<int>)v[1]).Sort((a, b) => b - a);
+            ((List<int>)v[2]).Sort((a, b) => b - a);
+
+            int ans = 0;
+            int lb = v[1].Count, lc = v[2].Count;
+            for (int cntb = lb - 2; cntb <= lb; ++cntb)
+            {
+                if (cntb >= 0)
+                {
+                    for (int cntc = lc - 2; cntc <= lc; ++cntc)
+                    {
+                        if (cntc >= 0 && (cntb - cntc) % 3 == 0)
+                        {
+                            ans = Math.Max(ans, GetSum(v[1], 0, cntb) + GetSum(v[2], 0, cntc));
+                        }
+                    }
+                }
+            }
+            return ans + GetSum(v[0], 0, v[0].Count);
+        }
+
+        public int GetSum(IList<int> list, int start, int end)
+        {
+            int sum = 0;
+            for (int i = start; i < end; ++i)
+            {
+                sum += list[i];
+            }
+            return sum;
+        }
 
 
     }
---
+++ Ahao.LeetCodeCPP/1262. 可被三整除的最大和.cpp
@@ -0,0 +1,11 @@
+#include<vector>
+
+using namespace std;
+
+
+/*
+1262. 可被三整除的最大和
+
+给你一个整数数组 nums，请你找出并返回能被三整除的元素最大和。
+*/
+
