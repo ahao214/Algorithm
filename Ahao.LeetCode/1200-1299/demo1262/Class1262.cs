63d0957529f43a9bc604a6f6aa5be52cac17a46b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-19 00:20:52 +0800

    1262. 可被三整除的最大和

--- Ahao.LeetCode/1200-1299/demo1262/Class1262.cs
+++ Ahao.LeetCode/1200-1299/demo1262/Class1262.cs
@@ -53,5 +53,25 @@ namespace Ahao.LeetCode._1200_1299.demo1262
         }
 
 
+
+
+        public int MaxSumDivThree3(int[] nums)
+        {
+            int[] f = { 0, int.MinValue, int.MinValue };
+            foreach (int num in nums)
+            {
+                int[] g = new int[3];
+                Array.Copy(f, 0, g, 0, 3);
+                for (int i = 0; i < 3; ++i)
+                {
+                    g[(i + num % 3) % 3] = Math.Max(g[(i + num % 3) % 3], f[i] + num);
+                }
+                f = g;
+            }
+            return f[0];
+        }
+
+
+
     }
 }
