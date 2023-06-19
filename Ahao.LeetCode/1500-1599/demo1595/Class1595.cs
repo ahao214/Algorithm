bffec28dc5ee7ea8cc97cf40c5c418143f98fd70
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-20 00:14:22 +0800

    1595. 连通两组点的最小成本

--- Ahao.LeetCode/1500-1599/demo1595/Class1595.cs
+++ Ahao.LeetCode/1500-1599/demo1595/Class1595.cs
@@ -37,5 +37,36 @@ namespace Ahao.LeetCode._1500_1599.demo1595
             return dp[size1][m - 1];
         }
 
+
+
+
+        public int ConnectTwoGroups2(IList<IList<int>> cost)
+        {
+            int size1 = cost.Count, size2 = cost[0].Count, m = 1 << size2;
+            int[] dp1 = new int[m];
+            Array.Fill(dp1, int.MaxValue / 2);
+            int[] dp2 = new int[m];
+            dp1[0] = 0;
+            for (int i = 1; i <= size1; i++)
+            {
+                for (int s = 0; s < m; s++)
+                {
+                    dp2[s] = int.MaxValue / 2;
+                    for (int k = 0; k < size2; k++)
+                    {
+                        if ((s & (1 << k)) == 0)
+                        {
+                            continue;
+                        }
+                        dp2[s] = Math.Min(dp2[s], dp2[s ^ (1 << k)] + cost[i - 1][k]);
+                        dp2[s] = Math.Min(dp2[s], dp1[s] + cost[i - 1][k]);
+                        dp2[s] = Math.Min(dp2[s], dp1[s ^ (1 << k)] + cost[i - 1][k]);
+                    }
+                }
+                Array.Copy(dp2, 0, dp1, 0, m);
+            }
+            return dp1[m - 1];
+        }
+
     }
 }
