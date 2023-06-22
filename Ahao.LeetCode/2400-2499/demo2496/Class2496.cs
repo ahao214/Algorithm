a564c0538cfd0afe85100626808dee562df2a1d4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-23 00:31:37 +0800

    2496. 数组中字符串的最大值

---
+++ Ahao.LeetCode/2400-2499/demo2496/Class2496.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2496
+{
+    public class Class2496
+    {
+        public int MaximumValue(string[] strs)
+        {
+            int res = 0;
+
+            foreach (var s in strs)
+            {
+                bool isDigits = true;
+                foreach (var ch in s)
+                {
+                    isDigits &= char.IsDigit(ch);
+                }
+                if (isDigits)
+                {
+                    res = Math.Max(res, Convert.ToInt32(s));
+                }
+                else
+                {
+                    res = Math.Max(res, s.Length);
+                }
+
+            }
+            return res;
+
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/2400-2499/demo2496/JS2496.js
@@ -0,0 +1,11 @@
+﻿var maximumValue = function (strs) {
+    let res = 0;
+    for (const s of strs) {
+        let isDigits = true;
+        for (const c of s) {
+            isDigits &= c >= '0' && c <= '9';
+        }
+        res = Math.max(res, isDigits ? Number(s) : s.length);
+    }
+    return res;
+};
