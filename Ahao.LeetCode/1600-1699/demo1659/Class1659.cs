ec883c66a65c453c4e90fce4ae7530ec775acda5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-24 00:09:05 +0800

    1659. 最大化网格幸福感

--- Ahao.LeetCode/1600-1699/demo1659/Class1659.cs
+++ Ahao.LeetCode/1600-1699/demo1659/Class1659.cs
@@ -6,8 +6,132 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode._1600_1699.demo1659
 {
-   public  class Class1659
+    public class Class1659
     {
+
+        const int T = 243, N = 5, M = 6;
+        int n, m, tot;
+        int[][] maskBits;
+        int[] ivCount;
+        int[] evCount;
+        int[] innerScore;
+        int[][] interScore;
+        int[][][][] d;
+        // 邻居间的分数
+        static int[][] score = {
+        new int[]{0, 0, 0},
+        new int[]{0, -60, -10},
+        new int[]{0, -10, 40} };
+
+
+
+        public int GetMaxGridHappiness(int m, int n, int introvertsCount, int extrovertsCount)
+        {
+            this.n = n;
+            this.m = m;
+            // 状态总数为 3^n
+            this.tot = (int)Math.Pow(3, n);
+            this.maskBits = new int[T][];
+            for (int i = 0; i < T; i++)
+            {
+                this.maskBits[i] = new int[N];
+            }
+            this.ivCount = new int[T];
+            this.evCount = new int[T];
+            this.innerScore = new int[T];
+            this.interScore = new int[T][];
+            for (int i = 0; i < T; i++)
+            {
+                this.interScore[i] = new int[T];
+            }
+            this.d = new int[N][][][];
+
+            InitData();
+            // 记忆化搜索数组，初始化为 -1，表示未赋值
+            for (int i = 0; i < N; i++)
+            {
+                d[i] = new int[T][][];
+                for (int j = 0; j < T; j++)
+                {
+                    d[i][j] = new int[M + 1][];
+                    for (int k = 0; k <= M; k++)
+                    {
+                        d[i][j][k] = new int[M + 1];
+                        Array.Fill(d[i][j][k], -1);
+                    }
+                }
+            }
+            return DFS(0, 0, introvertsCount, extrovertsCount);
+        }
+
+        public void InitData()
+        {
+            // 计算行内分数
+            for (int mask = 0; mask < tot; mask++)
+            {
+                int maskTmp = mask;
+                for (int i = 0; i < n; i++)
+                {
+                    int x = maskTmp % 3;
+                    maskBits[mask][i] = x;
+                    maskTmp /= 3;
+                    if (x == 1)
+                    {
+                        ivCount[mask]++;
+                        innerScore[mask] += 120;
+                    }
+                    else if (x == 2)
+                    {
+                        evCount[mask]++;
+                        innerScore[mask] += 40;
+                    }
+                    if (i > 0)
+                    {
+                        innerScore[mask] += score[x][maskBits[mask][i - 1]];
+                    }
+                }
+            }
+            // 计算行间分数
+            for (int i = 0; i < tot; i++)
+            {
+                for (int j = 0; j < tot; j++)
+                {
+                    interScore[i][j] = 0;
+                    for (int k = 0; k < n; k++)
+                    {
+                        interScore[i][j] += score[maskBits[i][k]][maskBits[j][k]];
+                    }
+                }
+            }
+        }
+
+        public int DFS(int row, int premask, int iv, int ev)
+        {
+            if (row == m || (iv == 0 && ev == 0))
+            {
+                return 0;
+            }
+            // 如果该状态已经计算过答案，则直接返回
+            if (d[row][premask][iv][ev] != -1)
+            {
+                return d[row][premask][iv][ev];
+            }
+            // 合法状态，初始值为 0
+            int res = 0;
+            for (int mask = 0; mask < tot; mask++)
+            {
+                // mask 包含的内向人数不能超过 iv ，外向人数不能超过 ev
+                if (ivCount[mask] > iv || evCount[mask] > ev)
+                {
+                    continue;
+                }
+                res = Math.Max(res, DFS(row + 1, mask, iv - ivCount[mask], ev - evCount[mask])
+                                + innerScore[mask]
+                                + interScore[premask][mask]);
+            }
+            d[row][premask][iv][ev] = res;
+            return res;
+        }
 
 
     }
