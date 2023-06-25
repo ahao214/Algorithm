0a6d1a07bad4dc4c4de8507ddfe4c6dde78736e3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-26 00:03:13 +0800

    2485. 找出中枢整数

---
+++ Ahao.LeetCode/2400-2499/demo2485/Class2485.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2485
+{
+    public class Class2485
+    {
+
+
+    }
+}
