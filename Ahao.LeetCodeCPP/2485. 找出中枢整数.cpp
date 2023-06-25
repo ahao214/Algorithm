6bed7d491f8f8998dae8100de4dbb234ba49dbfd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-26 00:27:30 +0800

    2485. 找出中枢整数

--- Ahao.LeetCodeCPP/2485. 找出中枢整数.cpp
+++ Ahao.LeetCodeCPP/2485. 找出中枢整数.cpp
@@ -11,3 +11,16 @@ using namespace std;
 1 和 x 之间的所有元素之和等于 x 和 n 之间所有元素之和。
 返回中枢整数 x 。如果不存在中枢整数，则返回 -1 。题目保证对于给定的输入，至多存在一个中枢整数。
 */
+
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int t = (n * n + n) / 2;
+        int x = sqrt(t);
+        if (x * x == t) {
+            return x;
+        }
+        return -1;
+    }
+};
+
