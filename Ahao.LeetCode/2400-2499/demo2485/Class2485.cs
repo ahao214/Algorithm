9766a6ed26429401e26c360c7c331532d60ec02e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-26 00:26:28 +0800

    2485. 找出中枢整数

--- Ahao.LeetCode/2400-2499/demo2485/Class2485.cs
+++ Ahao.LeetCode/2400-2499/demo2485/Class2485.cs
@@ -9,6 +9,23 @@ namespace Ahao.LeetCode._2400_2499.demo2485
     public class Class2485
     {
 
+        public int PivotInteger(int n)
+        {
+            int res = -1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int leftSum = i * (i + 1) / 2;
+                int rightSum = (n - i + 1) * (i + n) / 2;
+                if (leftSum == rightSum)
+                {
+                    return i;
+                }
+            }
+
+            return res;
+        }
+
 
     }
 }
