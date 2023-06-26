90765f891699805210752c626881d6aaa246d821
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-27 07:59:02 +0800

    1186. 删除一次得到子数组最大和

---
+++ Ahao.LeetCodeCPP/1186. 删除一次得到子数组最大和.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+
+using namespace std;
+
+
+/*
+1186. 删除一次得到子数组最大和
+
+给你一个整数数组，返回它的某个 非空 子数组（连续元素）在执行一次可选的删除操作后，所能得到的最大元素总和。换句话说，你可以从原数组中选出一个子数组，并可以决定要不要从中删除一个元素（只能删一次哦），（删除后）子数组中至少应当有一个元素，然后该子数组（剩下）的元素总和是所有子数组之中最大的。
+
+注意，删除一个元素后，子数组 不能为空。
+*/
+
+
+class Solution {
+public:
+    int maximumSum(vector<int>& arr) {
+        int dp0 = arr[0], dp1 = 0, res = arr[0];
+        for (int i = 1; i < arr.size(); i++) {
+            dp1 = max(dp0, dp1 + arr[i]);
+            dp0 = max(dp0, 0) + arr[i];
+            res = max(res, max(dp0, dp1));
+        }
+        return res;
+    }
+};
