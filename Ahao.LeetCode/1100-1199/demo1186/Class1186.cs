80a8083949d02834bbaa4afc41bd036f4d8649b1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-27 07:57:56 +0800

    1186. 删除一次得到子数组最大和

---
+++ Ahao.LeetCode/1100-1199/demo1186/Class1186.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1100_1199.demo1186
+{
+    public class Class1186
+    {
+
+        public int MaximumSum(int[] arr)
+        {
+            int dp0 = arr[0], dp1 = 0, res = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                dp1 = Math.Max(dp0, dp1 + arr[i]);
+                dp0 = Math.Max(dp0, 0) + arr[i];
+                res = Math.Max(res, Math.Max(dp0, dp1));
+            }
+            return res;
+        }
+
+
+    }
+}
