5cb92675edea595fddb98a2fd9260d88ddff09a1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-29 00:40:49 +0800

    1253. 重构 2 行二进制矩阵

--- Ahao.LeetCode/1200-1299/demo1253/Class1253.cs
+++ Ahao.LeetCode/1200-1299/demo1253/Class1253.cs
@@ -69,5 +69,60 @@ namespace Ahao.LeetCode._1200_1299.demo1253
             return res;
         }
 
+
+        public IList<IList<int>> ReconstructMatrix2(int upper, int lower, int[] colsum)
+        {
+            int n = colsum.Length;
+            int sum = 0, two = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                if (colsum[i] == 2)
+                {
+                    ++two;
+                }
+                sum += colsum[i];
+            }
+            if (sum != upper + lower || Math.Min(upper, lower) < two)
+            {
+                return new List<IList<int>>();
+            }
+            upper -= two;
+            lower -= two;
+            IList<IList<int>> res = new List<IList<int>>();
+            for (int i = 0; i < 2; ++i)
+            {
+                res.Add(new List<int>());
+            }
+            for (int i = 0; i < n; ++i)
+            {
+                if (colsum[i] == 2)
+                {
+                    res[0].Add(1);
+                    res[1].Add(1);
+                }
+                else if (colsum[i] == 1)
+                {
+                    if (upper > 0)
+                    {
+                        res[0].Add(1);
+                        res[1].Add(0);
+                        --upper;
+                    }
+                    else
+                    {
+                        res[0].Add(0);
+                        res[1].Add(1);
+                    }
+                }
+                else
+                {
+                    res[0].Add(0);
+                    res[1].Add(0);
+                }
+            }
+            return res;
+        }
+
+
     }
 }
