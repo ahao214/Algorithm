cd7ac1ceeb24dcf41a41d0d09c6f17eb1b396878
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-29 00:41:04 +0800

    1253. 重构 2 行二进制矩阵

--- Ahao.LeetCode/1200-1299/demo1253/JS1253.js
+++ Ahao.LeetCode/1200-1299/demo1253/JS1253.js
@@ -1,1 +1,30 @@
-﻿
+﻿var reconstructMatrix = function (upper, lower, colsum) {
+    let n = colsum.length;
+    let sumVal = 0;
+    let twoNum = 0;
+    for (let i = 0; i < n; i++) {
+        if (colsum[i] == 2) {
+            twoNum++;
+        }
+        sumVal += colsum[i];
+    }
+    if (sumVal != upper + lower || Math.min(upper, lower) < twoNum) {
+        return [];
+    }
+    upper -= twoNum;
+    lower -= twoNum;
+    let res = Array.from({ length: 2 }, () => new Array(n).fill(0));
+    for (let i = 0; i < n; i++) {
+        if (colsum[i] == 2) {
+            res[0][i] = res[1][i] = 1;
+        } else if (colsum[i] == 1) {
+            if (upper > 0) {
+                res[0][i] = 1;
+                upper--;
+            } else {
+                res[1][i] = 1;
+            }
+        }
+    }
+    return res;
+}
