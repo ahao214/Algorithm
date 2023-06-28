0399f8c3cde9d79fab2d2773622763554e2c9a53
liupenghao@fulan.com.cn <haochen214@163.com>
2023-06-29 00:39:51 +0800

    1253. 重构 2 行二进制矩阵

---
+++ Ahao.LeetCode/1200-1299/demo1253/Class1253.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1200_1299.demo1253
+{
+    public class Class1253
+    {
+        public IList<IList<int>> ReconstructMatrix(int upper, int lower, int[] colsum)
+        {
+            IList<IList<int>> res = new List<IList<int>>();
+            int sum = colsum.Sum();
+            int twoCount = 0;
+
+            int n = colsum.Length;
+            int[] rowOne = new int[n];
+            int[] rowTwo = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (colsum[i] == 2)
+                {
+                    twoCount++;
+                }
+            }
+
+            if (sum != upper + lower || Math.Min(upper, lower) < twoCount)
+            {
+                return res;
+            }
+
+            upper -= twoCount;
+            lower -= twoCount;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (colsum[i] == 2)
+                {
+                    rowOne[i] = 1;
+                    rowTwo[i] = 1;
+                }
+                else if (colsum[i] == 0)
+                {
+                    rowOne[i] = 0;
+                    rowTwo[i] = 0;
+                }
+                else if (colsum[i] == 1)
+                {
+                    if (upper > 0)
+                    {
+                        rowOne[i] = 1;
+                        rowTwo[i] = 0;
+                        upper--;
+                    }
+                    else
+                    {
+                        rowOne[i] = 0;
+                        rowTwo[i] = 1;
+                        lower--;
+                    }
+                }
+
+            }
+
+
+            res.Add(rowOne.ToList());
+            res.Add(rowTwo.ToList());
+            return res;
+        }
+
+    }
+}
