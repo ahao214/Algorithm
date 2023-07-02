77e99a8b445ec399667629beaa3b3d5e7d4a37a3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-03 00:06:26 +0800

    445. 两数相加 II

---
+++ Ahao.LeetCode/0400-0499/demo445/Class445.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0400_0499.demo445
+{
+    public class Class445
+    {
+
+
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
