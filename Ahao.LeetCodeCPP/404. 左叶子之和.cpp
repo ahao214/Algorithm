b7414c6c29d3465838ce6837a3a0809224d7e6c2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-02 12:58:29 +0800

    404. 左叶子之和

---
+++ Ahao.LeetCodeCPP/404. 左叶子之和.cpp
@@ -0,0 +1,40 @@
+#include<vector>
+
+using namespace std;
+
+
+
+/*
+404. ×óÒ¶×ÓÖ®ºÍ
+*/
+class Solution {
+public:
+	int res = 0;
+	int sumOfLeftLeaves(TreeNode* root) {
+		dfs(root);
+		return res;
+	}
+
+	void dfs(TreeNode* root)
+	{
+		if (!root) return;
+		if (root->left)
+		{
+			if (!root->left->left && !root->left->right)
+				res += root->left->val;
+		}
+		dfs(root->left);
+		dfs(root->right);
+	}
+};
+
+
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
