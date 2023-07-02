ff1a49de35a31bac31e8f7a305a4570f5fa9eef6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-02 12:57:12 +0800

    406. 根据身高重建队列

--- Ahao.LeetCodeCPP/406. 根据身高重建队列.cpp
+++ Ahao.LeetCodeCPP/406. 根据身高重建队列.cpp
@@ -35,3 +35,57 @@ public:
         return ans;
     }
 };
+
+
+
+
+/*
+406. 根据身高重建队列
+*/
+class Solution {
+public:
+	int n;
+	vector<int> tr;
+	int lowbit(int x)
+	{
+		return x & -x;
+	}
+	void add(int x, int v)
+	{
+		for (int i = x; i <= n; i += lowbit(i))
+			tr[i] += v;
+	}
+	int query(int x)
+	{
+		int res = 0;
+		for (int i = x; i; i -= lowbit(i))
+			res += tr[i];
+		return res;
+	}
+	vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+		n = people.size();
+		tr.resize(n + 1);
+		sort(people.begin(), people.end(), [](vector<int>a, vector<int> b) {
+			if (a[0] != b[0]) return a[0] < b[0];
+			return a[1] > b[1];
+			});
+
+		vector<vector<int>> res(n);
+		for (auto p : people)
+		{
+			int h = p[0], k = p[1];
+			int left = 1, right = n;
+			while (left < right)
+			{
+				int mid = (left + right) >> 1;
+				if (mid - query(mid) >= k + 1)
+					right = mid;
+				else
+					left = mid + 1;
+			}
+			res[right - 1] = p;
+			add(right, 1);
+		}
+		return res;
+	}
+};
