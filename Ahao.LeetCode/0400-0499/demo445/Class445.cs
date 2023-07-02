982ca63c2d382c6375c76ae9a103205fbf19aa7d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-03 00:07:10 +0800

    445. 两数相加 II

--- Ahao.LeetCode/0400-0499/demo445/Class445.cs
+++ Ahao.LeetCode/0400-0499/demo445/Class445.cs
@@ -9,6 +9,36 @@ namespace Ahao.LeetCode._0400_0499.demo445
     public class Class445
     {
 
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            Stack<int> stack1 = new Stack<int>();
+            Stack<int> stack2 = new Stack<int>();
+            while (l1 != null)
+            {
+                stack1.Push(l1.val);
+                l1 = l1.next;
+            }
+            while (l2 != null)
+            {
+                stack2.Push(l2.val);
+                l2 = l2.next;
+            }
+            int carry = 0;
+            ListNode ans = null;
+            while (stack1.Count > 0 || stack2.Count > 0 || carry != 0)
+            {
+                int a = stack1.Count == 0 ? 0 : stack1.Pop();
+                int b = stack2.Count == 0 ? 0 : stack2.Pop();
+                int cur = a + b + carry;
+                carry = cur / 10;
+                cur %= 10;
+                ListNode curnode = new ListNode(cur);
+                curnode.next = ans;
+                ans = curnode;
+            }
+            return ans;
+        }
+
 
     }
 
