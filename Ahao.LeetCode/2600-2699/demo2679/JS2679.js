03981a6bdfce51da82c13d29cf912ec6a3a6204b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-04 08:54:26 +0800

    2679. 矩阵中的和

--- Ahao.LeetCode/2600-2699/demo2679/JS2679.js
+++ Ahao.LeetCode/2600-2699/demo2679/JS2679.js
@@ -1,1 +1,16 @@
-﻿
+﻿var matrixSum = function (nums) {
+    let res = 0;
+    let m = nums.length;
+    let n = nums[0].length;
+    for (let i = 0; i < m; i++) {
+        nums[i].sort((a, b) => a - b);
+    }
+    for (let j = 0; j < n; j++) {
+        let maxVal = 0;
+        for (let i = 0; i < m; i++) {
+            maxVal = Math.max(maxVal, nums[i][j]);
+        }
+        res += maxVal;
+    }
+    return res;
+}
