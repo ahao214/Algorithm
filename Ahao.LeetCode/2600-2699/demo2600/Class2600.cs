bc0dd251e20a4fd51a836cd6e5cda34cc7548744
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-05 09:21:16 +0800

    2600. K 件物品的最大和

---
+++ Ahao.LeetCode/2600-2699/demo2600/Class2600.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2600_2699.demo2600
+{
+    public class Class2600
+    {
+
+        public int KItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k)
+        {
+            if (k <= numOnes)
+            {
+                return k;
+            }
+            else if (k <= numOnes + numZeros)
+            {
+                return numOnes;
+            }
+            else
+            {
+                return numOnes - (k - numOnes - numZeros);
+            }
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/2600-2699/demo2600/JS2600.js
@@ -0,0 +1,10 @@
+﻿var kItemsWithMaximumSum = function (numOnes, numZeros, numNegOnes, k) {
+    if (k <= numOnes) {
+        return k;
+    } else if (k <= numOnes + numZeros) {
+        return numOnes;
+    } else {
+        return numOnes - (k - numOnes - numZeros);
+    }
+};
+
