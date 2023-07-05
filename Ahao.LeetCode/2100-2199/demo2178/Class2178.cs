ce18a2eb9224ddc19f95a91642a584d69a25194a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-06 00:08:02 +0800

    2178. 拆分成最多数目的正偶数之和

---
+++ Ahao.LeetCode/2100-2199/demo2178/Class2178.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2100_2199.demo2178
+{
+    public class Class2178
+    {
+
+        public List<long> MaximumEvenSplit(long finalSum)
+        {
+            List<long> res = new List<long>();
+            if (finalSum % 2 > 0)
+            {
+                return res;
+            }
+            for (int i = 2; i <= finalSum; i += 2)
+            {
+                res.Add(i);
+                finalSum -= i;
+            }
+            res[res.Count - 1] += finalSum;
+            return res;
+        }
+
+    }
+}
