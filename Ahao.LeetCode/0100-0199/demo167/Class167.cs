b5bbd9f15695294af2f9dab4f1d255668fa59275
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-08 00:05:25 +0800

    167. 两数之和 II - 输入有序数组

---
+++ Ahao.LeetCode/0100-0199/demo167/Class167.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0100_0199.demo167
+{
+    internal class Class167
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+            while (left < right)
+            {
+                if (numbers[left] + numbers[right] < target)
+                {
+                    left++;
+                }
+                else if (numbers[left] + numbers[right] > target)
+                {
+                    right--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return new int[] { left + 1, right + 1 };
+        }
+
+    }
+}
