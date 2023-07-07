ebe655bf39167ae42a209703762bcac458552091
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-08 00:07:32 +0800

    167. 两数之和 II - 输入有序数组

--- Ahao.LeetCode/0100-0199/demo167/JS167.js
+++ Ahao.LeetCode/0100-0199/demo167/JS167.js
@@ -1,1 +1,18 @@
-﻿
+﻿var twoSum = function (numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+
+    if (numbers.length <= 2) {
+        return [++left, ++right];
+    }
+
+    while (left < right) {
+        if (numbers[left] + numbers[right] > target) {
+            right--;
+        } else if (numbers[left] + numbers[right] < target) {
+            left++;
+        } else {
+            return [++left, ++right];
+        }
+    }
+};
