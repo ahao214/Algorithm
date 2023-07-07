703766db24c0107afc37f41ed2be8bd566acdb6a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-08 00:10:44 +0800

    167. 两数之和 II - 输入有序数组

---
+++ Ahao.LeetCode/0100-0199/demo167/TS167.ts
@@ -0,0 +1,54 @@
+﻿//Map
+function twoSum(numbers: number[], target: number): number[] {
+    let map = new Map()
+    for (let i = 0; i < numbers.length; i++) {
+        map.set(numbers[i], i)
+    }
+    for (let i = 0; i < numbers.length; i++) {
+        if (map.has(target - numbers[i])) {
+            return [i + 1, map.get(target - numbers[i]) + 1]
+        }
+    }
+};
+
+
+
+//双指针
+function twoSumTwoPoint(numbers: number[], target: number): number[] {
+    let left = 0, right = numbers.length - 1;
+    while (left < right) {
+        if (numbers[left] + numbers[right] === target) {
+            return [++left, ++right];
+        }
+        if (numbers[left] + numbers[right] > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+};
+
+
+//二分查找
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSumBinary(numbers: number[], target: number): number[] {
+    for (let i = 0; i < numbers.length; i++) {
+        let targ = target - numbers[i]
+        let l = i + 1, r = numbers.length - 1
+        while (l <= r) {
+            let mid = Math.floor((l + r) / 2)
+            let num = numbers[mid]
+            if (targ === num) return [i + 1, mid + 1]
+            if (targ > num) {
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+    }
+};
+
