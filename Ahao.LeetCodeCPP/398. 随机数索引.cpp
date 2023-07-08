51035c86fca10dcfb3f7753a0d2e935c46530881
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-09 00:06:50 +0800

    398. 随机数索引

--- Ahao.LeetCodeCPP/398. 随机数索引.cpp
+++ Ahao.LeetCodeCPP/398. 随机数索引.cpp
@@ -1,6 +1,34 @@
-using namespace std;
 #include<vector>
 #include<unordered_map>
+
+using namespace std;
+
+
+
+
+/*
+398. 随机数索引
+
+给你一个可能含有 重复元素 的整数数组 nums ，请你随机输出给定的目标数字 target 的索引。你可以假设给定的数字一定存在于数组中。
+
+实现 Solution 类：
+
+Solution(int[] nums) 用数组 nums 初始化对象。
+int pick(int target) 从 nums 中选出一个满足 nums[i] == target 的随机索引 i 。如果存在多个有效的索引，则每个索引的返回概率应当相等。
+*/
+class Solution {
+public:
+    unordered_map<int, vector<int>> hash;
+
+    Solution(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++)
+            hash[nums[i]].push_back(i);
+    }
+
+    int pick(int target) {
+        return hash[target][rand() % hash[target].size()];
+    }
+};
 
 class Solution {
     unordered_map<int, vector<int>> pos;
