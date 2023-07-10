f0ffe6c09dd875c3254a9cc8cdcc06ede39fd000
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-11 00:23:01 +0800

    1979. 找出数组的最大公约数

---
+++ Ahao.LeetCode/1900-1999/demo1979/Class1979.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1900_1999.demo1979
+{
+    public class Class1979
+    {
+        public int FindGCD(int[] nums)
+        {
+            int maxValue = int.MinValue;
+            int minValue = int.MaxValue;
+            foreach (var item in nums)
+            {
+                maxValue = Math.Max(item, maxValue);
+                minValue = Math.Min(item, minValue);
+            }
+            return gcd(maxValue, minValue);
+        }
+
+        public int gcd(int a, int b)
+        {
+            return b == 0 ? a : gcd(b, a % b);
+        }
+
+    }
+}
