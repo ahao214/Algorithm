2b5d9e5e7878b7a6d6e0272ce9f8983dd716c93d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-11 00:25:14 +0800

    1979. 找出数组的最大公约数

--- Ahao.LeetCode/1900-1999/demo1979/JS1979.js
+++ Ahao.LeetCode/1900-1999/demo1979/JS1979.js
@@ -1,1 +1,15 @@
-﻿
+﻿/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findGCD = function (nums) {
+    let max = Math.max(...nums)
+    let min = Math.min(...nums)
+    // console.log(min, max);
+    // 辗转相除法 求 最大公约数
+    function gcd(a, b) {
+        let r = a % b // 余数
+        return b === 0 ? a : gcd(b, r)
+    }
+    return gcd(min, max)
+};
