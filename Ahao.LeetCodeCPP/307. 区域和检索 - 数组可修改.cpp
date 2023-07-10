614ebea0d09ccc4fb5b292bbb78cdfeecf9685cc
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-10 13:56:19 +0800

    307. 区域和检索 - 数组可修改

--- Ahao.LeetCodeCPP/307. 区域和检索 - 数组可修改.cpp
+++ Ahao.LeetCodeCPP/307. 区域和检索 - 数组可修改.cpp
@@ -33,3 +33,64 @@ public:
         return sum1 + sum2 + sum3;
     }
 };
+
+
+
+
+/*
+307. 区域和检索 - 数组可修改
+
+给你一个数组 nums ，请你完成两类查询。
+
+其中一类查询要求 更新 数组 nums 下标对应的值
+另一类查询要求返回数组 nums 中索引 left 和索引 right 之间（ 包含 ）的nums元素的 和 ，其中 left <= right
+实现 NumArray 类：
+
+NumArray(int[] nums) 用整数数组 nums 初始化对象
+void update(int index, int val) 将 nums[index] 的值 更新 为 val
+int sumRange(int left, int right) 返回数组 nums 中索引 left 和索引 right 之间（ 包含 ）的nums元素的 和 （即，nums[left] + nums[left + 1], ..., nums[right]）
+*/
+class NumArray {
+public:
+	int n;
+	vector<int> tr, num;
+	int lowbit(int x)
+	{
+		return x & -x;
+	}
+
+	int query(int x)
+	{
+		int res = 0;
+		for (int i = x; i; i -= lowbit(i))
+			res += tr[i];
+		return res;
+	}
+
+	void add(int x, int v)
+	{
+		for (int i = x; i <= n; i += lowbit(i))
+			tr[i] += v;
+	}
+
+	NumArray(vector<int>& nums) {
+		num = nums;
+		n = num.size();
+		tr.resize(n + 1);
+		for (int i = 1; i <= n; i++)
+		{
+			tr[i] = num[i - 1];
+			for (int j = i - 1; j > i - lowbit(i); j -= lowbit(j))
+				tr[i] += tr[j];
+		}
+	}
+
+	void update(int index, int val) {
+		add(index + 1, val - num[index]);
+		num[index] = val;
+	}
+
+	int sumRange(int left, int right) {
+		return query(right + 1) - query(left);
+	}
+};
