dc6eff9596e9637665cb20b8a81b0bd3c7978677
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-12 14:00:01 +0800

    2544. 交替数字和

---
+++ Ahao.LeetCode/2500-2599/demo2544/Class2544.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2500_2599.demo2544
+{
+    internal class Class2544
+    {
+
+        public int AlternateDigitSum(int n)
+        {
+            int res = 0, sign = 1;
+            while (n > 0)
+            {
+                res += n % 10 * sign;
+                sign = -sign;
+                n /= 10;
+            }
+            return res * -sign;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/2500-2599/demo2544/JS2544.js
@@ -0,0 +1,9 @@
+﻿var alternateDigitSum = function(n) {
+    let res = 0, sign = 1;
+    while (n > 0) {
+        res += n % 10 * sign;
+        sign = -sign;
+        n = Math.floor(n / 10);
+    }
+    return -sign * res;
+};
