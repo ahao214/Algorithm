df7d8ee1066f621ccc60a8d80251d8870316a287
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-12 14:01:05 +0800

    2544. 交替数字和

---
+++ Ahao.LeetCodeCPP/2544. 交替数字和.cpp
@@ -0,0 +1,27 @@
+#include<vector>
+
+using namespace std;
+
+/*
+2544. 交替数字和
+
+给你一个正整数 n 。n 中的每一位数字都会按下述规则分配一个符号：
+
+最高有效位 上的数字分配到 正 号。
+剩余每位上数字的符号都与其相邻数字相反。
+返回所有数字及其对应符号的和。
+*/
+
+
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+        int res = 0, sign = 1;
+        while (n > 0) {
+            res += n % 10 * sign;
+            sign = -sign;
+            n /= 10;
+        }
+        return -sign * res;
+    }
+};
