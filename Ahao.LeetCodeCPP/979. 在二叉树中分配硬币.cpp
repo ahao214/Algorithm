56ea813d08f83bf45fff7571396b0e2403c6bada
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-14 12:55:43 +0800

    979. 在二叉树中分配硬币

--- Ahao.LeetCodeCPP/979. 在二叉树中分配硬币.cpp
+++ Ahao.LeetCodeCPP/979. 在二叉树中分配硬币.cpp
@@ -1,4 +1,5 @@
 #include<vector>
+#include <functional>
 
 using namespace std;
 
@@ -34,4 +35,32 @@ public:
 	int distributeCoins(TreeNode* root) {
 		return dfs(root)[2];
 	}
+};
+
+
+
+class Solution {
+public:
+    int distributeCoins(TreeNode* root) {
+        int move = 0;
+
+        function<int(const TreeNode*)> dfs = [&](const TreeNode* root) -> int {
+            int moveleft = 0;
+            int moveright = 0;
+            if (root == nullptr) {
+                return 0;
+            }
+            if (root->left) {
+                moveleft = dfs(root->left);
+            }
+            if (root->right) {
+                moveright = dfs(root->right);
+            }
+            move += abs(moveleft) + abs(moveright);
+            return moveleft + moveright + root->val - 1;
+        };
+
+        dfs(root);
+        return move;
+    }
 };
