532dbfd2d82a07264a65108ff1b2549e8a307bae
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-14 12:55:59 +0800

    979. 在二叉树中分配硬币

--- Ahao.LeetCode/0900-0999/demo979/JS979.js
+++ Ahao.LeetCode/0900-0999/demo979/JS979.js
@@ -3,3 +3,29 @@
     this.left = (left === undefined ? null : left)
     this.right = (right === undefined ? null : right)
 }
+
+
+
+let move;
+
+var distributeCoins = function (root) {
+    move = 0;
+    dfs(root);
+    return move;
+};
+
+function dfs(root) {
+    let moveleft = 0;
+    let moveright = 0;
+    if (root === null) {
+        return 0;
+    }
+    if (root.left !== null) {
+        moveleft = dfs(root.left);
+    }
+    if (root.right !== null) {
+        moveright = dfs(root.right);
+    }
+    move += Math.abs(moveleft) + Math.abs(moveright);
+    return moveleft + moveright + root.val - 1;
+}
