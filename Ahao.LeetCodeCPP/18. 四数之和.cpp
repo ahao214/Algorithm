abe09e642a7d09aa18bbcc566d7725e4ed75b889
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-15 00:20:00 +0800

    18. 四数之和

--- Ahao.LeetCodeCPP/18. 四数之和.cpp
+++ Ahao.LeetCodeCPP/18. 四数之和.cpp
@@ -2,17 +2,17 @@ using namespace std;
 #include<vector>
 #include<algorithm>
 
+/*
+18. 四数之和
 
+给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
+
+0 <= a, b, c, d < n
+a、b、c 和 d 互不相同
+nums[a] + nums[b] + nums[c] + nums[d] == target
+你可以按 任意顺序 返回答案 。
+*/
 class Solution {
-	/*
-	18. 四数之和
-	给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
-
-	0 <= a, b, c, d < n
-	a、b、c 和 d 互不相同
-	nums[a] + nums[b] + nums[c] + nums[d] == target
-	你可以按 任意顺序 返回答案 。
-	*/
 public:
 	vector<vector<int>> fourSum(vector<int>& nums, int target) {
 		vector<vector<int>> ans;
@@ -22,7 +22,7 @@ public:
 			for (int j = i + 1; j < nums.size(); j++) {
 				if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 				int l = j + 1, r = nums.size() - 1;
-				int sum = target - nums[i] - nums[j];
+				long sum = target - nums[i] - nums[j];
 				while (l < r) {
 					if (nums[l] + nums[r] == sum) {
 						ans.push_back({ nums[i],nums[j],nums[l],nums[r] });
@@ -40,6 +40,64 @@ public:
 			}
 		}
 		return ans;
+	}
+};
+
+
+
+class Solution {
+public:
+	vector<vector<int>> fourSum(vector<int>& nums, int target) {
+		vector<vector<int>> quadruplets;
+		if (nums.size() < 4) {
+			return quadruplets;
+		}
+		sort(nums.begin(), nums.end());
+		int length = nums.size();
+		for (int i = 0; i < length - 3; i++) {
+			if (i > 0 && nums[i] == nums[i - 1]) {
+				continue;
+			}
+			if ((long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+				break;
+			}
+			if ((long)nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+				continue;
+			}
+			for (int j = i + 1; j < length - 2; j++) {
+				if (j > i + 1 && nums[j] == nums[j - 1]) {
+					continue;
+				}
+				if ((long)nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+					break;
+				}
+				if ((long)nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+					continue;
+				}
+				int left = j + 1, right = length - 1;
+				while (left < right) {
+					long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+					if (sum == target) {
+						quadruplets.push_back({ nums[i], nums[j], nums[left], nums[right] });
+						while (left < right && nums[left] == nums[left + 1]) {
+							left++;
+						}
+						left++;
+						while (left < right && nums[right] == nums[right - 1]) {
+							right--;
+						}
+						right--;
+					}
+					else if (sum < target) {
+						left++;
+					}
+					else {
+						right--;
+					}
+				}
+			}
+		}
+		return quadruplets;
 	}
 };
 
