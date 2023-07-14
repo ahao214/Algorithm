7853c4321abc6f5f0031c5f8e09e7981dca34829
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-14 12:54:23 +0800

    979. 在二叉树中分配硬币

---
+++ Ahao.LeetCode/0900-0999/demo979/Class979.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0900_0999.demo979
+{
+    internal class Class979
+    {
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
