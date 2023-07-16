f7d243cfb114d42f6fc8066c6aaa88c04ce223b7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-17 00:14:39 +0800

    415. 字符串相加

---
+++ Ahao.LeetCodeCPP/415. 字符串相加.cpp
@@ -0,0 +1,32 @@
+#include<vector>
+#include <string>
+
+using namespace std;
+
+/*
+415. 字符串相加
+
+给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和并同样以字符串形式返回。
+
+你不能使用任何內建的用于处理大整数的库（比如 BigInteger）， 也不能直接将输入的字符串转换为整数形式。
+*/
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+        string ans = "";
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? num1[i] - '0' : 0;
+            int y = j >= 0 ? num2[j] - '0' : 0;
+            int result = x + y + add;
+            ans.push_back('0' + result % 10);
+            add = result / 10;
+            i -= 1;
+            j -= 1;
+        }
+        // 计算完以后的答案需要翻转过来
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
