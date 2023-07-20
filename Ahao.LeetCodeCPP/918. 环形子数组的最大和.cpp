86507b7830f1996db864a3a32c6557c921cd1cba
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-20 13:10:39 +0800

    918. 环形子数组的最大和

--- Ahao.LeetCodeCPP/918. 环形子数组的最大和.cpp
+++ Ahao.LeetCodeCPP/918. 环形子数组的最大和.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 /*
 918. 环形子数组的最大和
+
 给定一个长度为 n 的环形整数数组 nums ，返回 nums 的非空 子数组 的最大可能和 。
 
 环形数组 意味着数组的末端将会与开头相连呈环状。形式上， nums[i] 的下一个元素是 nums[(i + 1) % n] ， nums[i] 的前一个元素是 nums[(i - 1 + n) % n] 。
@@ -56,3 +57,36 @@ public:
 		return res;
 	}
 };
+
+
+
+
+class Solution {
+public:
+	int maxSubarraySumCircular(vector<int>& nums) {
+		int n = nums.size();
+		vector<int> leftMax(n);
+		// 对坐标为 0 处的元素单独处理，避免考虑子数组为空的情况
+		leftMax[0] = nums[0];
+		int leftSum = nums[0];
+		int pre = nums[0];
+		int res = nums[0];
+		for (int i = 1; i < n; i++) {
+			pre = max(pre + nums[i], nums[i]);
+			res = max(res, pre);
+			leftSum += nums[i];
+			leftMax[i] = max(leftMax[i - 1], leftSum);
+		}
+
+		// 从右到左枚举后缀，固定后缀，选择最大前缀
+		int rightSum = 0;
+		for (int i = n - 1; i > 0; i--) {
+			rightSum += nums[i];
+			res = max(res, rightSum + leftMax[i - 1]);
+		}
+		return res;
+	}
+};
+
+
+
