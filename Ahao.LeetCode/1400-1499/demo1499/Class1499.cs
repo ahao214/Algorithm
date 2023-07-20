e42e98283c5513c6ddb43c96c7243acbe36db89e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-21 00:14:42 +0800

    1499. 满足不等式的最大值

--- Ahao.LeetCode/1400-1499/demo1499/Class1499.cs
+++ Ahao.LeetCode/1400-1499/demo1499/Class1499.cs
@@ -8,5 +8,26 @@ namespace Ahao.LeetCode._1400_1499.demo1499
 {
     internal class Class1499
     {
+
+        public int FindMaxValueOfEquation(int[][] points, int k)
+        {
+            int res = int.MinValue;
+            PriorityQueue<Tuple<int, int>, int> heap = new PriorityQueue<Tuple<int, int>, int>();
+            foreach (int[] point in points)
+            {
+                int x = point[0], y = point[1];
+                while (heap.Count > 0 && x - heap.Peek().Item2 > k)
+                {
+                    heap.Dequeue();
+                }
+                if (heap.Count > 0)
+                {
+                    res = Math.Max(res, x + y - heap.Peek().Item1);
+                }
+                heap.Enqueue(new Tuple<int, int>(x - y, x), x - y);
+            }
+            return res;
+        }
+
     }
 }
--- Ahao.LeetCode/1400-1499/demo1499/JS1499.js
+++ Ahao.LeetCode/1400-1499/demo1499/JS1499.js
@@ -1,1 +1,84 @@
-﻿
+﻿var findMaxValueOfEquation = function (points, k) {
+    let res = Number.MIN_SAFE_INTEGER;
+    const heap = new MinHeap((a, b) => a[0] < b[0]);
+    for (const point of points) {
+        const x = point[0], y = point[1];
+        while (heap.size !== 0 && x - heap.peek()[1] > k) {
+            heap.poll();
+        }
+        if (heap.size !== 0) {
+            res = Math.max(res, x + y - heap.peek()[0]);
+        }
+        heap.add([x - y, x]);
+    }
+    return res;
+};
+
+class MinHeap {
+    constructor(compareFunc = (a, b) => a < b) {
+        this.compare = compareFunc;
+        this.heap = [];
+    }
+
+    get size() {
+        return this.heap.length;
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    add(value) {
+        this.heap.push(value);
+        this.heapifyUp();
+    }
+
+    poll() {
+        if (this.size === 0) {
+            return null;
+        }
+        if (this.size === 1) {
+            return this.heap.pop();
+        }
+        const max = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown();
+        return max;
+    }
+
+    heapifyUp() {
+        let currentIndex = this.size - 1;
+        while (currentIndex > 0) {
+            const parentIndex = Math.floor((currentIndex - 1) / 2);
+            if (this.compare(this.heap[currentIndex], this.heap[parentIndex])) {
+                [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
+                currentIndex = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    heapifyDown() {
+        let currentIndex = 0;
+        while (currentIndex < this.size) {
+            let largestIndex = currentIndex;
+            const leftChildIndex = 2 * currentIndex + 1;
+            const rightChildIndex = 2 * currentIndex + 2;
+            if (leftChildIndex < this.size && this.compare(this.heap[leftChildIndex], this.heap[largestIndex])) {
+                largestIndex = leftChildIndex;
+            }
+            if (rightChildIndex < this.size && this.compare(this.heap[rightChildIndex], this.heap[largestIndex])) {
+                largestIndex = rightChildIndex;
+            }
+            if (largestIndex !== currentIndex) {
+                [this.heap[currentIndex], this.heap[largestIndex]] = [this.heap[largestIndex], this.heap[currentIndex]];
+                currentIndex = largestIndex;
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+
