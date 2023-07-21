80c0feecde83276c651db9ff651bf52d238468f6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-22 00:08:20 +0800

    860. 柠檬水找零

--- Ahao.LeetCode/0800-0899/demo860/Class860.cs
+++ Ahao.LeetCode/0800-0899/demo860/Class860.cs
@@ -10,7 +10,41 @@ namespace Ahao.LeetCode._0800_0899.demo860
     {
         public bool LemonadeChange(int[] bills)
         {
-        
+            int five = 0, ten = 0;
+            foreach (var item in bills)
+            {
+                if (item == 5)
+                {
+                    five++;
+                }
+                else if (item == 10)
+                {
+                    if (five == 0)
+                    {
+                        return false;
+                    }
+                    five--;
+                    ten++;
+                }
+                else
+                {
+                    if (five > 0 && ten > 0)
+                    {
+                        five--;
+                        ten--;
+                    }
+                    else if (five >= 3)
+                    {
+                        five -= 3;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+            }
+            return true;
         }
     }
 }
