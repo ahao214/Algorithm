ff6fce27a98f34b9e5c89f4283559121e874488b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-24 13:32:36 +0800

    771. 宝石与石头

--- Ahao.LeetCode/0700-0799/demo771/Class771.cs
+++ Ahao.LeetCode/0700-0799/demo771/Class771.cs
@@ -8,5 +8,50 @@ namespace Ahao.LeetCode._0700_0799.demo771
 {
     internal class Class771
     {
+
+        public int NumJewelsInStones(String jewels, String stones)
+        {
+            int jewelsCount = 0;
+            int jewelsLength = jewels.Length, stonesLength = stones.Length;
+            for (int i = 0; i < stonesLength; i++)
+            {
+                string stone = stones.Substring(i, 1);
+                for (int j = 0; j < jewelsLength; j++)
+                {
+                    string jewel = jewels.Substring(j, 1);
+                    if (stone == jewel)
+                    {
+                        jewelsCount++;
+                        break;
+                    }
+                }
+            }
+            return jewelsCount;
+        }
+
+
+
+        public int NumJewelsInStones2(String jewels, String stones)
+        {
+            int jewelsCount = 0;
+            ISet<string> jewelsSet = new HashSet<string>();
+            int jewelsLength = jewels.Length, stonesLength = stones.Length;
+            for (int i = 0; i < jewelsLength; i++)
+            {
+                string jewel = jewels.Substring(i, 1);
+                jewelsSet.Add(jewel);
+            }
+            for (int i = 0; i < stonesLength; i++)
+            {
+                string stone = stones.Substring(i, 1);
+                if (jewelsSet.Contains(stone))
+                {
+                    jewelsCount++;
+                }
+            }
+            return jewelsCount;
+        }
+
+
     }
 }
