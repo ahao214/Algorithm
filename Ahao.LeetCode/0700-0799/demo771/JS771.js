76b5b7a484434c9cfff6f28a362403598ef800ab
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-24 13:26:01 +0800

    771. 宝石与石头

---
+++ Ahao.LeetCode/0700-0799/demo771/JS771.js
@@ -0,0 +1,20 @@
+﻿var numJewelsInStones = function(jewels, stones) {
+    jewels = jewels.split('');
+    return stones.split('').reduce((prev, val) => {
+        for (const ch of jewels) {
+            if (ch === val) {
+                return prev + 1;
+            }
+        }
+        return prev;
+    }, 0);
+};
+
+
+
+var numJewelsInStones = function (jewels, stones) {
+    const jewelsSet = new Set(jewels.split(''));
+    return stones.split('').reduce((prev, val) => {
+        return prev + jewelsSet.has(val);
+    }, 0);
+};
