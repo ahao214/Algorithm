86b84dfa0726c0cdabdfe06980924b40c9662d1a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-25 11:18:55 +0800

    2208. 将数组和减半的最少操作次数

---
+++ Ahao.LeetCode/2200-2299/demo2208/Class2208.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2200_2299.demo2208
+{
+    internal class Class2208
+    {
+
+        public int HalveArray(int[] nums)
+        {
+            PriorityQueue<double, double> pq = new PriorityQueue<double, double>();
+            foreach (int num in nums)
+            {
+                pq.Enqueue(num, -num);
+            }
+            int res = 0;
+            double sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+            double sum2 = 0.0;
+            while (sum2 < sum / 2)
+            {
+                double x = pq.Dequeue();
+                sum2 += x / 2;
+                pq.Enqueue(x / 2, -x / 2);
+                res++;
+            }
+            return res;
+        }
+    }
+}
---
+++ Ahao.LeetCode/2200-2299/demo2208/JS2208.js
@@ -0,0 +1,16 @@
+﻿var halveArray = function (nums) {
+    const pq = new MaxPriorityQueue();
+    for (const num of nums) {
+        pq.enqueue(num);
+    }
+    let res = 0;
+    let sum1 = nums.reduce((acc, curr) => acc + curr, 0);
+    let sum2 = 0;
+    while (sum2 < sum1 / 2) {
+        const x = pq.dequeue().element;
+        sum2 += x / 2;
+        pq.enqueue(x / 2);
+        res++;
+    }
+    return res;
+};
