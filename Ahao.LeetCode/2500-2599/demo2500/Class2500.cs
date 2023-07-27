2597028e1061e0a8f0bbb0d96930c41855b13feb
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-27 13:52:38 +0800

    2500. 删除每行中的最大值

---
+++ Ahao.LeetCode/2500-2599/demo2500/Class2500.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2500_2599.demo2500
+{
+    internal class Class2500
+    {
+        public int DeleteGreatestValue(int[][] grid)
+        {
+            int res = 0;
+            foreach (var item in grid)
+            {
+                Array.Sort(item);
+            }
+            int m = grid.Length;
+            int n = grid[0].Length;
+            for (int j = 0; j < n; j++)
+            {
+                int max = int.MinValue;
+                for (int i = 0; i < m; i++)
+                {
+                    max = Math.Max(max, grid[i][j]);
+                }
+                res += max;
+            }
+            return res;
+        }
+    }
+}
---
+++ Ahao.LeetCode/2500-2599/demo2500/JS2500.js
@@ -0,0 +1,15 @@
+﻿var deleteGreatestValue = function (grid) {
+    const m = grid.length, n = grid[0].length;
+    for (let i = 0; i < m; i++) {
+        grid[i].sort((a, b) => a - b);
+    }
+    let res = 0;
+    for (let j = 0; j < n; j++) {
+        let mx = 0;
+        for (let i = 0; i < m; i++) {
+            mx = Math.max(mx, grid[i][j]);
+        }
+        res += mx;
+    }
+    return res;
+};
