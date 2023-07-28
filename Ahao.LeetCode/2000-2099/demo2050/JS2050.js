d73f7c9ad648aa665a5ebbddefcc519a4e4e27aa
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-28 12:24:39 +0800

    2050. 并行课程 III

---
+++ Ahao.LeetCode/2000-2099/demo2050/JS2050.js
@@ -0,0 +1,28 @@
+﻿var minimumTime = function (n, relations, time) {
+    let res = 0;
+    let prev = Array(n + 1).fill(0);
+    for (let i = 0; i <= n; i++) {
+        prev[i] = [];
+    }
+    for (var relation of relations) {
+        let x = relation[0], y = relation[1];
+        prev[y].push(x);
+    }
+    let memo = {};
+    for (let i = 1; i <= n; i++) {
+        res = Math.max(res, dp(i, time, prev, memo));
+    }
+    return res;
+};
+
+function dp(i, time, prev, memo) {
+    if (!memo[i]) {
+        let cur = 0;
+        for (let p of prev[i]) {
+            cur = Math.max(cur, dp(p, time, prev, memo));
+        }
+        cur += time[i - 1];
+        memo[i] = cur;
+    }
+    return memo[i];
+}
