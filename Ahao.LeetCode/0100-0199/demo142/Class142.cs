ffaa312d5525efb26df58d8699f4a698d3763e4f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-07-30 13:02:02 +0800

    142. 环形链表 II

--- Ahao.LeetCode/0100-0199/demo142/Class142.cs
+++ Ahao.LeetCode/0100-0199/demo142/Class142.cs
@@ -8,5 +8,39 @@ namespace Ahao.LeetCode._0100_0199.demo142
 {
     internal class Class142
     {
+        public ListNode DetectCycle(ListNode head)
+        {
+            var p1 = head;
+            var p2 = head;
+            while (p2 != null)
+            {
+                p1 = p1.next;
+                p2 = p2.next?.next;
+                if (p1 == p2)
+                    break;
+            }
+            if (p2 == null)
+                return null;
+
+            var node = head;
+            while (node != p1)
+            {
+                node = node.next;
+                p1 = p1.next;
+            }
+            return node;
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
     }
 }
