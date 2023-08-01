f9a865e4878d9cc3e25192e8730b2258975a5898
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-01 15:19:35 +0800

    打印两个有序链表的公共部分

---
+++ Ahao.CodingInterviewGuide/Chapter02/P01/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.CodingInterviewGuide.Chapter02.P01
+{
+    public class Node
+    {
+        public int Value;
+        public Node Next;
+        public Node(int data)
+        {
+            this.Value = data;
+        }
+
+        public void PrintCommonPart(Node head1, Node head2)
+        {
+            Console.WriteLine("Common Part:");
+            while (head1 != null && head2 != null)
+            {
+                if (head1.Value < head2.Value)
+                {
+                    head1 = head1.Next;
+                }
+                else if (head2.Value < head1.Value)
+                {
+                    head2 = head2.Next;
+                }
+                else
+                {
+                    Console.WriteLine(head1.Value + " ");
+                    head1 = head1.Next;
+                    head2 = head2.Next;
+                }
+            }
+            Console.WriteLine();
+
+        }
+    }
+
+
+}
