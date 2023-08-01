a1dfa15a81785a7b0fdb8f8c476e889dae2b1fe9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-01 12:25:56 +0800

    2681. 英雄的力量

--- Ahao.LeetCode/2600-2699/demo2681/Class2681.cs
+++ Ahao.LeetCode/2600-2699/demo2681/Class2681.cs
@@ -28,5 +28,43 @@ namespace Ahao.LeetCode._2600_2699.demo2681
             return res;
         }
 
+
+
+        public int SumOfPower2(int[] nums)
+        {
+            Array.Sort(nums);
+            int dp = 0, preSum = 0;
+            int res = 0, mod = 1000000007;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dp = (nums[i] + preSum) % mod;
+                preSum = (preSum + dp) % mod;
+                res = (int)((res + (long)nums[i] * nums[i] % mod * dp) % mod);
+                if (res < 0)
+                {
+                    res += mod;
+                }
+            }
+            return res;
+        }
+
+
+
+        public int SumOfPower3(int[] nums)
+        {
+            int MOD = 1000000007;
+            int length = nums.Length;
+            Array.Sort(nums);
+            long ans = 0;
+            long sum = 0;
+            for (int i = 0; i < length; i++)
+            {
+                ans = (ans + (sum + nums[i]) % MOD * nums[i] % MOD * nums[i] % MOD) % MOD;
+                sum = (sum * 2 % MOD + nums[i]) % MOD;
+            }
+            return (int)ans;
+        }
+
+
     }
 }
--- Ahao.LeetCode/2600-2699/demo2681/JS2681.js
+++ Ahao.LeetCode/2600-2699/demo2681/JS2681.js
@@ -1,1 +1,13 @@
-﻿
+﻿var sumOfPower = function (nums) {
+    let n = nums.length;
+    nums.sort((a, b) => a - b);
+    let dp = 0, preSum = 0;
+    let res = 0, mod = 1e9 + 7;
+    for (let i = 0; i < n; i++) {
+        dp = (nums[i] + preSum) % mod;
+        preSum = (preSum + dp) % mod;
+        res = (res + Number(BigInt(nums[i]) * BigInt(nums[i]) * BigInt(dp) % BigInt(mod))) % mod;
+    }
+    return res;
+};
+
