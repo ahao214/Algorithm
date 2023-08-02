e393d78c349f9f0c6f5cafc69ee3397eb17df8fc
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-02 11:37:56 +0800

    822. 翻转卡片游戏

--- Ahao.LeetCode/0800-0899/demo822/Class822.cs
+++ Ahao.LeetCode/0800-0899/demo822/Class822.cs
@@ -11,6 +11,36 @@ namespace Ahao.LeetCode._0800_0899.demo822
 
         public int Flipgame(int[] fronts, int[] backs)
         {
+            var same = new HashSet<int>();
+            for (int i = 0; i < fronts.Length; i++)
+            {
+                if (fronts[i] == backs[i])
+                {
+                    same.Add(fronts[i]);
+                }
+            }
+            int res = 3000;
+            foreach (var x in fronts)
+            {
+                if (x < res && !same.Contains(x))
+                {
+                    res = x;
+                }
+            }
+            foreach (var x in backs)
+            {
+                if (x < res && !same.Contains(x))
+                {
+                    res = x;
+                }
+            }
+            return res % 3000;
+        }
+
+
+
+        public int Flipgame2(int[] fronts, int[] backs)
+        {
             var res = int.MaxValue;
             HashSet<int> set = new();
             for (var i = 0; i < fronts.Length; ++i)
