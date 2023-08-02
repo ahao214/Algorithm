703936bca4fa08705f3766ad6b05a6a1a44d4611
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-02 11:37:06 +0800

    822. 翻转卡片游戏

---
+++ Ahao.LeetCode/0800-0899/demo822/Class822.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo822
+{
+    internal class Class822
+    {
+
+        public int Flipgame(int[] fronts, int[] backs)
+        {
+            var res = int.MaxValue;
+            HashSet<int> set = new();
+            for (var i = 0; i < fronts.Length; ++i)
+            {
+                if (fronts[i] == backs[i])
+                    set.Add(fronts[i]);
+            }
+
+            for (var i = 0; i < fronts.Length; ++i)
+            {
+                if (!set.Contains(backs[i]))
+                {
+                    res = Math.Min(res, backs[i]);
+                }
+                if (!set.Contains(fronts[i]))
+                {
+                    res = Math.Min(res, fronts[i]);
+                }
+            }
+
+            return res == int.MaxValue ? 0 : res;
+
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/0800-0899/demo822/JS822.js
@@ -0,0 +1,20 @@
+﻿var flipgame = function (fronts, backs) {
+    const same = new Set();
+    for (let i = 0; i < fronts.length; i++) {
+        if (fronts[i] === backs[i]) {
+            same.add(fronts[i]);
+        }
+    }
+    let res = 3000;
+    for (let x of fronts) {
+        if (x < res && !same.has(x)) {
+            res = x;
+        }
+    }
+    for (let x of backs) {
+        if (x < res && !same.has(x)) {
+            res = x;
+        }
+    }
+    return res % 3000;
+};
