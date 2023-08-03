4ce507492e2adc7eba8db4b44c2f7ef7e3d50ad4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-03 22:59:13 +0800

    722. 删除注释

--- Ahao.LeetCode/0700-0799/demo722/Class722.cs
+++ Ahao.LeetCode/0700-0799/demo722/Class722.cs
@@ -51,5 +51,54 @@ namespace Ahao.LeetCode._0700_0799.demo722
             }
             return res;
         }
+
+
+
+        public List<String> RemoveComments2(String[] source)
+        {
+            String str = String.Join("\n", source);
+            StringBuilder sb = new StringBuilder();
+
+            while (str.Length > 0)
+            {
+                int s = str.IndexOf("/*"), d = str.IndexOf("//");
+                if (s == -1 && d == -1)
+                {
+                    sb.Append(str);
+                    break;
+                }
+                if (s != -1 && (d == -1 || s < d))
+                {
+                    // /*形式注释
+                    sb.Append(str.Substring(0, s));
+                    str = str.Substring(s + 2);
+                    int e = str.IndexOf("*/");
+                    str = str.Substring(e + 2);
+                }
+                else if (d != -1 && (s == -1 || d < s))
+                {
+                    // //形式注释
+                    sb.Append(str.Substring(0, d));
+                    str = str.Substring(d + 2);
+                    int e = str.IndexOf("\n");
+                    if (e == -1)
+                    {
+                        break;
+                    }
+                    str = str.Substring(e);
+                }
+            }
+            String[] strs = sb.ToString().Split("\n");
+            List<String> ans = new List<string>();
+            foreach (var item in strs)
+            {
+                if (item.Length > 0)
+                {
+                    ans.Add(item);
+                }
+            }
+            return ans;
+        }
+
     }
 }
