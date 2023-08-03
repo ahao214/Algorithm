8572016fcc041585de6f5e0d301b01a34a281924
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-03 23:13:57 +0800

    单链表

---
+++ Ahao.CodingInterviewGuide/Chapter02/P02/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.CodingInterviewGuide.Chapter02.P02
+{
+    public class Node
+    {
+        public int Value;
+        public Node Next;
+        public Node(int data)
+        {
+            this.Value = data;
+        }
+
+
+
+    }
+}
