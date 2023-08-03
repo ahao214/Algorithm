3cf42b5d50770f0b9d06ee800158aff227d4b858
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-03 23:08:12 +0800

    如何仅用递归函数和栈操作逆序一个栈

---
+++ Ahao.CodingInterviewGuide/Chapter01/P03/RecursionAndStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.CodingInterviewGuide.Chapter01.P03
+{
+    public class RecursionAndStack
+    {
+        /// <summary>
+        /// 递归函数一
+        /// </summary>
+        /// <param name="stk"></param>
+        /// <returns></returns>
+        public static int GetAndRemoveLastElement(Stack<int> stk)
+        {
+            int result = stk.Pop();
+            if (stk.Count == 0)
+            {
+                return result;
+            }
+            else
+            {
+                int last = GetAndRemoveLastElement(stk);
+                stk.Push(result);
+                return last;
+            }
+        }
+
+
+        /// <summary>
+        /// 递归函数二
+        /// </summary>
+        /// <param name="stk"></param>
+        public static void Reverse(Stack<int> stk)
+        {
+            if (stk.Count == 0)
+            {
+                return;
+            }
+            int i = GetAndRemoveLastElement(stk);
+            Reverse(stk);
+            stk.Push(i);
+        }
+
+    }
+}
