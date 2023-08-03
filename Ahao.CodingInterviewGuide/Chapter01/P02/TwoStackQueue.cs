8e5ad122760d5e4d291e23becae0405979a0651f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-03 22:58:54 +0800

    由两个栈组成的队列

--- Ahao.CodingInterviewGuide/Chapter01/P02/TwoStackQueue.cs
+++ Ahao.CodingInterviewGuide/Chapter01/P02/TwoStackQueue.cs
@@ -6,10 +6,55 @@ using System.Threading.Tasks;
 
 namespace Ahao.CodingInterviewGuide.Chapter01.P02
 {
-    internal class TwoStackQueue
+    public class TwoStackQueue
     {
+        public Stack<int> stackPush;
+        public Stack<int> stackPop;
+
+        public TwoStackQueue()
+        {
+            stackPush = new Stack<int>();
+            stackPop = new Stack<int>();
+        }
+
+        //push栈向pop栈倒入数据
+        private void PushToPop()
+        {
+            if (stackPop.Count == 0)
+            {
+                while (stackPush.Count != 0)
+                {
+                    stackPop.Push(stackPop.Pop());
+                }
+            }
+        }
 
 
+        public void Add(int pushInt)
+        {
+            stackPush.Push(pushInt);
+            PushToPop();
+        }
+
+        public int Poll()
+        {
+            if (stackPop.Count == 0 && stackPush.Count == 0)
+            {
+                throw new ArgumentException("Queue is empty");
+            }
+            PushToPop();
+            return stackPop.Pop();
+        }
+
+        public int Peek()
+        {
+            if (stackPop.Count == 0 && stackPush.Count == 0)
+            {
+                throw new ArgumentException("Queue is empty");
+            }
+            PushToPop();
+            return stackPop.Peek();
+        }
 
     }
 }
