faf13c82497340bd3f453f029f8b3714c032ef48
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-03 23:18:53 +0800

    递归-先序遍历

---
+++ Ahao.CodingInterviewGuide/Chapter03/P01/递归方法/PreOrder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.CodingInterviewGuide.Chapter03.P01.递归方法
+{
+
+    public class PreOrder
+    {
+        /// <summary>
+        /// 先序遍历
+        /// </summary>
+        /// <param name="head"></param>
+        public void PreOrderRecur(Node head)
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            Console.WriteLine(head.value + " ");
+            PreOrderRecur(head.left);
+            PreOrderRecur(head.right);
+        }
+    }
+}
