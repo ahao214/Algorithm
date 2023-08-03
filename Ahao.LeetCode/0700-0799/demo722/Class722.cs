2217a68d77a84254e192ca1ec18a899cfc8a9db4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-03 11:57:08 +0800

    722. 删除注释

---
+++ Ahao.LeetCode/0700-0799/demo722/Class722.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo722
+{
+    internal class Class722
+    {
+
+        public IList<string> RemoveComments(string[] source)
+        {
+            IList<string> res = new List<string>();
+            StringBuilder newLine = new StringBuilder();
+            bool inBlock = false;
+            foreach (string line in source)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (inBlock)
+                    {
+                        if (i + 1 < line.Length && line[i] == '*' && line[i + 1] == '/')
+                        {
+                            inBlock = false;
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        if (i + 1 < line.Length && line[i] == '/' && line[i + 1] == '*')
+                        {
+                            inBlock = true;
+                            i++;
+                        }
+                        else if (i + 1 < line.Length && line[i] == '/' && line[i + 1] == '/')
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            newLine.Append(line[i]);
+                        }
+                    }
+                }
+                if (!inBlock && newLine.Length > 0)
+                {
+                    res.Add(newLine.ToString());
+                    newLine.Length = 0;
+                }
+            }
+            return res;
+        }
+    }
+}
