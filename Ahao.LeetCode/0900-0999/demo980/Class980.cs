c610a1a1dd6851284d8f5d1911807c238defb629
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-04 12:14:25 +0800

    980. 不同路径 III

---
+++ Ahao.LeetCode/0900-0999/demo980/Class980.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0900_0999.demo980
+{
+    public class Class980
+    {
+
+        static int[][] dirs = { new int[] { -1, 0 }, new int[] { 1, 0 }, new int[] { 0, -1 }, new int[] { 0, 1 } };
+
+        public int UniquePathsIII(int[][] grid)
+        {
+            int r = grid.Length, c = grid[0].Length;
+            int si = 0, sj = 0, n = 0;
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    if (grid[i][j] == 0)
+                    {
+                        n++;
+                    }
+                    else if (grid[i][j] == 1)
+                    {
+                        n++;
+                        si = i;
+                        sj = j;
+                    }
+                }
+            }
+            return DFS(grid, si, sj, n);
+        }
+
+        public int DFS(int[][] grid, int i, int j, int n)
+        {
+            if (grid[i][j] == 2)
+            {
+                return n == 0 ? 1 : 0;
+            }
+            int r = grid.Length, c = grid[0].Length;
+            int t = grid[i][j];
+            grid[i][j] = -1;
+            int res = 0;
+            foreach (int[] dir in dirs)
+            {
+                int ni = i + dir[0], nj = j + dir[1];
+                if (ni >= 0 && ni < r && nj >= 0 && nj < c && (grid[ni][nj] == 0 || grid[ni][nj] == 2))
+                {
+                    res += DFS(grid, ni, nj, n - 1);
+                }
+            }
+            grid[i][j] = t;
+            return res;
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/0900-0999/demo980/JS980.js
@@ -0,0 +1,35 @@
+﻿const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+var uniquePathsIII = function (grid) {
+    const r = grid.length, c = grid[0].length;
+    let si = 0, sj = 0, n = 0;
+    for (let i = 0; i < r; i++) {
+        for (let j = 0; j < c; j++) {
+            if (grid[i][j] === 0) {
+                n++;
+            } else if (grid[i][j] === 1) {
+                n++;
+                si = i;
+                sj = j;
+            }
+        }
+    }
+    return dfs(grid, si, sj, n);
+};
+
+function dfs(grid, i, j, n) {
+    if (grid[i][j] === 2) {
+        return n === 0 ? 1 : 0;
+    }
+    const r = grid.length, c = grid[0].length, t = grid[i][j];
+    grid[i][j] = -1;
+    let res = 0;
+    for (const dir of dirs) {
+        const ni = i + dir[0], nj = j + dir[1];
+        if (ni >= 0 && ni < r && nj >= 0 && nj < c && (grid[ni][nj] === 0 || grid[ni][nj] === 2)) {
+            res += dfs(grid, ni, nj, n - 1);
+        }
+    }
+    grid[i][j] = t;
+    return res;
+}
