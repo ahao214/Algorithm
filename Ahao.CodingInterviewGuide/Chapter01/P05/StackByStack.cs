55db0977d81756f69178b5c1ea79ac0f7b85ee00
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-05 14:20:24 +0800

    用一个栈实现另一个栈的排序

---
+++ Ahao.CodingInterviewGuide/Chapter01/P05/StackByStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.CodingInterviewGuide.Chapter01.P05
+{
+    public class StackByStack
+    {
+        public void SortStackByStack(Stack<int> stack)
+        {
+            Stack<int> help = new Stack<int>();
+            while (stack.Count != 0)
+            {
+                int cur = stack.Pop();
+                while (help.Count != 0 && help.Peek() < cur)
+                {
+                    stack.Push(help.Pop());
+                }
+                help.Push(cur);
+            }
+            while (help.Count != 0)
+            {
+                stack.Push(help.Pop());
+            }
+        }
+
+    }
+}
