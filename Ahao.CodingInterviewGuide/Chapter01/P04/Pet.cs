1c86d77a12d6fdb13102301210bfd760db42fc1f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-05 14:13:41 +0800

    猫狗队列

---
+++ Ahao.CodingInterviewGuide/Chapter01/P04/Pet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.CodingInterviewGuide.Chapter01.P04
+{
+    public class Pet
+    {
+        private string type;
+        public Pet(string type)
+        {
+            this.type = type;
+        }
+
+        public string GetPetType()
+        {
+            return this.type;
+        }
+    }
+}
