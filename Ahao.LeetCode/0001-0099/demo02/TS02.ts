6c6e98021b9362ec840e3c5f0da677d5ada0f78c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-06 16:35:11 +0800

    2. 两数相加

--- Ahao.LeetCode/0001-0099/demo02/TS02.ts
+++ Ahao.LeetCode/0001-0099/demo02/TS02.ts
@@ -32,3 +32,24 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
     }
     return newList.next;
 };
+
+
+function addTwoNumbers2(l1: ListNode | null, l2: ListNode | null,): ListNode | null {
+    const dummy = new ListNode();
+    let cur = dummy;
+    let sum = 0;
+    while (sum !== 0 || l1 !== null || l2 !== null) {
+        if (l1 != null) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2 != null) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        cur.next = new ListNode(sum % 10);
+        cur = cur.next;
+        sum = Math.floor(sum / 10);
+    }
+    return dummy.next;
+}
