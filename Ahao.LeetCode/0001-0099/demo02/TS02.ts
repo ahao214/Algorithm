1684aa2a21802493502fc273622ae37e371fee94
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-06 16:30:54 +0800

    2. 两数相加

---
+++ Ahao.LeetCode/0001-0099/demo02/TS02.ts
@@ -0,0 +1,34 @@
+﻿
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
+
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    const newList = new ListNode(0);
+    let cur = newList;
+    let curry = 0;
+    while (l1 || l2) {
+        const val1 = l1?.val ?? 0;
+        const val2 = l2?.val ?? 0;
+        const val = val1 + val2 + curry;
+        curry = Math.floor(val / 10);
+        cur.next = new ListNode(val % 10);
+        if (l1) {
+            l1 = l1.next;
+        }
+        if (l2) {
+            l2 = l2.next;
+        }
+        cur = cur.next;
+    }
+    if (curry) {
+        cur.next = new ListNode(curry);
+    }
+    return newList.next;
+};
