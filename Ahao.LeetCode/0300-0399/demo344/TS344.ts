7efc5a69d2b1ecfd7067ee1a3646df8aa8de3bd4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-07 12:31:22 +0800

    344. 反转字符串

--- Ahao.LeetCode/0300-0399/demo344/TS344.ts
+++ Ahao.LeetCode/0300-0399/demo344/TS344.ts
@@ -1,1 +1,11 @@
-﻿
+﻿function reverseString(s: string[]): void {
+    let i: number = 0;
+    let j: number = s.length - 1;
+    while (i < j) {
+        let tmp: string = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+        i++;
+        j--;
+    }
+};
