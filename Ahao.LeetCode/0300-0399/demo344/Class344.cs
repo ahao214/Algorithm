adf9a9cd8675853a7ea0d840d900285016cc478b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-07 12:29:14 +0800

    344. 反转字符串

--- Ahao.LeetCode/0300-0399/demo344/Class344.cs
+++ Ahao.LeetCode/0300-0399/demo344/Class344.cs
@@ -8,5 +8,20 @@ namespace Ahao.LeetCode._0300_0399.demo344
 {
     internal class Class344
     {
+
+        public void ReverseString(char[] s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+            while (i < j)
+            {
+                char tmp = s[i];
+                s[i] = s[j];
+                s[j] = tmp;
+                i++;
+                j--;
+            }
+        }
     }
+
 }
