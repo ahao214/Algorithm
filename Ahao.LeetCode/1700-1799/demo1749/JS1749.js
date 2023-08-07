13faf99979d631c344a3abe72b129bdedd72a147
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-08 00:11:28 +0800

    1749. 任意子数组和的绝对值的最大值

--- Ahao.LeetCode/1700-1799/demo1749/JS1749.js
+++ Ahao.LeetCode/1700-1799/demo1749/JS1749.js
@@ -1,1 +1,13 @@
-﻿
+﻿var maxAbsoluteSum = function (nums) {
+    let positiveMax = 0, negativeMin = 0;
+    let positiveSum = 0, negativeSum = 0;
+    for (let num of nums) {
+        positiveSum += num
+        positiveMax = Math.max(positiveMax, positiveSum)
+        positiveSum = Math.max(0, positiveSum)
+        negativeSum += num
+        negativeMin = Math.min(negativeMin, negativeSum)
+        negativeSum = Math.min(0, negativeSum)
+    }
+    return Math.max(positiveMax, -negativeMin)
+};
--- Ahao.LeetCode/1700-1799/demo1749/TS1749.ts
+++ Ahao.LeetCode/1700-1799/demo1749/TS1749.ts
@@ -1,3 +1,11 @@
 ﻿function maxAbsoluteSum(nums: number[]): number {
-
+    let preMax: number = 0;
+    let preMin: number = 0;
+    let prefix: number = 0;
+    nums.forEach((item) => {
+        prefix += item;
+        preMax = Math.max(preMax, prefix);
+        preMin = Math.min(preMin, prefix);
+    })
+    return preMax - preMin;
 };
