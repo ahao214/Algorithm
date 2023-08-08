6c7e81fa212c7e058d478b89518c2ebf61f8d8f7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-09 00:09:18 +0800

    1281. 整数的各位积和之差

--- Ahao.LeetCode/1200-1299/demo1281/JS1281.js
+++ Ahao.LeetCode/1200-1299/demo1281/JS1281.js
@@ -1,1 +1,10 @@
-﻿
+﻿var subtractProductAndSum = function (n) {
+    let m = 1, s = 0;
+    while (n > 0) {
+        let x = n % 10;
+        n = Math.floor(n / 10);
+        m *= x;
+        s += x;
+    }
+    return m - s;
+}
---
+++ Ahao.LeetCode/1200-1299/demo1281/TS1281.ts
@@ -0,0 +1,11 @@
+﻿function subtractProductAndSum(n: number): number {
+    const list = n.toString().split('');
+    let mul = 1;
+    let sum = 0;
+    for (let i of list) {
+        const num = Number(i);
+        mul *= num;
+        sum += num;
+    }
+    return mul - sum;
+};
