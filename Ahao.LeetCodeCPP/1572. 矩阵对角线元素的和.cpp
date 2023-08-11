3e05498b1892d646aff8005cb47b5287ca9523b0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-11 10:56:57 +0800

    1572. 矩阵对角线元素的和

---
+++ Ahao.LeetCodeCPP/1572. 矩阵对角线元素的和.cpp
@@ -0,0 +1,38 @@
+#include<vector>
+using namespace std;
+
+/*
+1572. 矩阵对角线元素的和
+
+给你一个正方形矩阵 mat，请你返回矩阵对角线元素的和。
+
+请你返回在矩阵主对角线上的元素和副对角线上且不在主对角线上元素的和。
+*/
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size(), sum = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == j || i + j == n - 1) {
+                    sum += mat[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+};
+
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size(), sum = 0, mid = n / 2;
+        for (int i = 0; i < n; ++i) {
+            sum += mat[i][i] + mat[i][n - 1 - i];
+        }
+        return sum - mat[mid][mid] * (n & 1);
+    }
+};
+
