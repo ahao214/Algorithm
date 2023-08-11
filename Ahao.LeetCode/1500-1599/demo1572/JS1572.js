f5ec750d3bf7072a6b4feda94d7633afd9ebf282
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-11 10:58:13 +0800

    1572. 矩阵对角线元素的和

--- Ahao.LeetCode/1500-1599/demo1572/JS1572.js
+++ Ahao.LeetCode/1500-1599/demo1572/JS1572.js
@@ -10,3 +10,14 @@
     }
     return sum;
 };
+
+
+
+var diagonalSum = function (mat) {
+    const n = mat.length, mid = Math.floor(n / 2);
+    let sum = 0;
+    for (let i = 0; i < n; ++i) {
+        sum += mat[i][i] + mat[i][n - 1 - i];
+    }
+    return sum - mat[mid][mid] * (n & 1);
+};
