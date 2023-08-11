93adc31e00294ddd5165557aa3abcd9956dc5f16
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-11 10:54:39 +0800

    1572. 矩阵对角线元素的和

---
+++ Ahao.LeetCode/1500-1599/demo1572/Class1572.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1500_1599.demo1572
+{
+    public class Class1572
+    {
+        public int DiagonalSum(int[][] mat)
+        {
+            int res = 0;
+            int n = mat.Length;
+            int primaryDia = 0;
+            int secondaryDia = 0;
+            for (int i = 0; i < n; i++)
+            {
+                primaryDia += mat[i][i];
+                secondaryDia += mat[i][n - i - 1];
+            }
+            if (n % 2 == 0)
+            {
+                res = primaryDia + secondaryDia;
+            }
+            else
+            {
+                res = primaryDia + secondaryDia - mat[n / 2][n / 2];
+            }
+
+            return res;
+        }
+
+    }
+}
