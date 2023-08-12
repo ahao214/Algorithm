0a0a021549568cf89e23c6359e7de5dabe3942d8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-12 12:54:04 +0800

    23. 合并K个升序链表

--- Ahao.LeetCodeCPP/23. 合并K个升序链表.cpp
+++ Ahao.LeetCodeCPP/23. 合并K个升序链表.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 /*
 23. 合并K个升序链表
+
 给你一个链表数组，每个链表都已经按升序排列。
 
 请你将所有链表合并到一个升序链表中，返回合并后的链表。
@@ -69,4 +70,96 @@ struct ListNode {
 	ListNode() : val(0), next(nullptr) {}
 	ListNode(int x) : val(x), next(nullptr) {}
 	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+
+
+
+class Solution {
+public:
+	struct Status {
+		int val;
+		ListNode* ptr;
+		bool operator < (const Status& rhs) const {
+			return val > rhs.val;
+		}
+	};
+
+	priority_queue <Status> q;
+
+	ListNode* mergeKLists(vector<ListNode*>& lists) {
+		for (auto node : lists) {
+			if (node) q.push({ node->val, node });
+		}
+		ListNode head, * tail = &head;
+		while (!q.empty()) {
+			auto f = q.top(); q.pop();
+			tail->next = f.ptr;
+			tail = tail->next;
+			if (f.ptr->next) q.push({ f.ptr->next->val, f.ptr->next });
+		}
+		return head.next;
+	}
+};
+
+
+
+
+class Solution {
+public:
+	ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+		if ((!a) || (!b)) return a ? a : b;
+		ListNode head, * tail = &head, * aPtr = a, * bPtr = b;
+		while (aPtr && bPtr) {
+			if (aPtr->val < bPtr->val) {
+				tail->next = aPtr; aPtr = aPtr->next;
+			}
+			else {
+				tail->next = bPtr; bPtr = bPtr->next;
+			}
+			tail = tail->next;
+		}
+		tail->next = (aPtr ? aPtr : bPtr);
+		return head.next;
+	}
+
+	ListNode* merge(vector <ListNode*>& lists, int l, int r) {
+		if (l == r) return lists[l];
+		if (l > r) return nullptr;
+		int mid = (l + r) >> 1;
+		return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+	}
+
+	ListNode* mergeKLists(vector<ListNode*>& lists) {
+		return merge(lists, 0, lists.size() - 1);
+	}
+};
+
+
+
+class Solution {
+public:
+	ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+		if ((!a) || (!b)) return a ? a : b;
+		ListNode head, * tail = &head, * aPtr = a, * bPtr = b;
+		while (aPtr && bPtr) {
+			if (aPtr->val < bPtr->val) {
+				tail->next = aPtr; aPtr = aPtr->next;
+			}
+			else {
+				tail->next = bPtr; bPtr = bPtr->next;
+			}
+			tail = tail->next;
+		}
+		tail->next = (aPtr ? aPtr : bPtr);
+		return head.next;
+	}
+
+	ListNode* mergeKLists(vector<ListNode*>& lists) {
+		ListNode* ans = nullptr;
+		for (size_t i = 0; i < lists.size(); ++i) {
+			ans = mergeTwoLists(ans, lists[i]);
+		}
+		return ans;
+	}
 };
