38df34438c38ecd7d24bccd75c445c7d29cb1ba3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-12 13:13:13 +0800

    单调栈结构

---
+++ Ahao.CodingInterviewGuide/Chapter01/P08/SingleStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.CodingInterviewGuide.Chapter01.P08
+{
+    public class SingleStack
+    {
+        public int[][] RightWay(int[] arr)
+        {
+            int n = arr.Length;
+            int[][] res = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                res[i] = new int[2];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int leftLessIndex = -1;
+                int rightLessIndex = -1;
+                int cur = i - 1;
+                while (cur >= 0)
+                {
+                    if (arr[cur] < arr[i])
+                    {
+                        leftLessIndex = cur;
+                        break;
+                    }
+                    cur--;
+                }
+                cur = i + 1;
+                while (cur < n)
+                {
+                    if (arr[cur] < arr[i])
+                    {
+                        rightLessIndex = cur;
+                        break;
+                    }
+                    cur++;
+                }
+                res[i][0] = leftLessIndex;
+                res[i][1] = rightLessIndex;
+            }
+            return res;
+        }
+
+
+
+    }
+}
