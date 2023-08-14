4686802a18c5dceebf20c9a6847189d706294e1f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-15 00:06:15 +0800

    833. 字符串中的查找与替换

--- Ahao.LeetCode/0800-0899/demo833/Class833.cs
+++ Ahao.LeetCode/0800-0899/demo833/Class833.cs
@@ -6,8 +6,51 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode._0800_0899.demo833
 {
-    public  class Class833
+    public class Class833
     {
+
+        public string FindReplaceString(string s, int[] indices, string[] sources, string[] targets)
+        {
+            int n = s.Length, m = indices.Length;
+
+            IList<int> ops = new List<int>();
+            for (int i = 0; i < m; i++)
+            {
+                ops.Add(i);
+            }
+            ((List<int>)ops).Sort((i, j) => indices[i] - indices[j]);
+
+            StringBuilder ans = new StringBuilder();
+            int pt = 0;
+            for (int i = 0; i < n;)
+            {
+                while (pt < m && indices[ops[pt]] < i)
+                {
+                    pt++;
+                }
+                bool succeed = false;
+                while (pt < m && indices[ops[pt]] == i)
+                {
+                    if (s.Substring(i, Math.Min(sources[ops[pt]].Length, n - i)).Equals(sources[ops[pt]]))
+                    {
+                        succeed = true;
+                        break;
+                    }
+                    pt++;
+                }
+                if (succeed)
+                {
+                    ans.Append(targets[ops[pt]]);
+                    i += sources[ops[pt]].Length;
+                }
+                else
+                {
+                    ans.Append(s[i]);
+                    i++;
+                }
+            }
+            return ans.ToString();
+        }
 
 
     }
---
+++ Ahao.LeetCode/0800-0899/demo833/JS833.js
@@ -0,0 +1,37 @@
+﻿var findReplaceString = function (s, indices, sources, targets) {
+    const n = s.length;
+    const m = indices.length;
+
+    const ops = new Array(m);
+    for (let i = 0; i < m; i++) {
+        ops[i] = i;
+    }
+    ops.sort((i, j) => indices[i] - indices[j]);
+
+    let ans = "";
+    let pt = 0;
+    for (let i = 0; i < n;) {
+        while (pt < m && indices[ops[pt]] < i) {
+            pt++;
+        }
+        let found = false;
+        while (pt < m && indices[ops[pt]] == i) {
+            if (s.substring(i, i + sources[ops[pt]].length) === sources[ops[pt]]) {
+                found = true;
+                break;
+            }
+            pt++;
+        }
+        if (found) {
+            ans += targets[ops[pt]];
+            i += sources[ops[pt]].length;
+        } else {
+            ans += s[i];
+            i++;
+        }
+    }
+    return ans;
+};
+
+
+
