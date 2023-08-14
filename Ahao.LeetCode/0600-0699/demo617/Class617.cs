36f66f4c155f48fb8cb5bb908b411d3f5cfa939b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-14 13:02:26 +0800

    617. 合并二叉树

---
+++ Ahao.LeetCode/0600-0699/demo617/Class617.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0600_0699.demo617
+{
+    public class Class617
+    {
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null)
+            {
+                return t2;
+            }
+            if (t2 == null)
+            {
+                return t1;
+            }
+            TreeNode merged = new TreeNode(t1.val + t2.val);
+            merged.left = MergeTrees(t1.left, t2.left);
+            merged.right = MergeTrees(t1.right, t2.right);
+            return merged;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+}
