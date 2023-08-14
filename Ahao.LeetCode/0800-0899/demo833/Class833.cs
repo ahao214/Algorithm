dd1063b01de269b50f2fc4e29397077ac0196783
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-15 00:07:38 +0800

    833. 字符串中的查找与替换

--- Ahao.LeetCode/0800-0899/demo833/Class833.cs
+++ Ahao.LeetCode/0800-0899/demo833/Class833.cs
@@ -53,5 +53,46 @@ namespace Ahao.LeetCode._0800_0899.demo833
         }
 
 
+
+        public string FindReplaceString2(string s, int[] indices, string[] sources, string[] targets)
+        {
+            int n = s.Length, m = indices.Length;
+
+            IDictionary<int, IList<int>> ops = new Dictionary<int, IList<int>>();
+            for (int i = 0; i < m; ++i)
+            {
+                ops.TryAdd(indices[i], new List<int>());
+                ops[indices[i]].Add(i);
+            }
+
+            StringBuilder ans = new StringBuilder();
+            for (int i = 0; i < n;)
+            {
+                bool succeed = false;
+                if (ops.ContainsKey(i))
+                {
+                    foreach (int pt in ops[i])
+                    {
+                        if (s.Substring(i, Math.Min(i + sources[pt].Length, n) - i).Equals(sources[pt]))
+                        {
+                            succeed = true;
+                            ans.Append(targets[pt]);
+                            i += sources[pt].Length;
+                            break;
+                        }
+                    }
+                }
+                if (!succeed)
+                {
+                    ans.Append(s[i]);
+                    ++i;
+                }
+            }
+            return ans.ToString();
+        }
+
+
+
+
     }
 }
--- Ahao.LeetCode/0800-0899/demo833/JS833.js
+++ Ahao.LeetCode/0800-0899/demo833/JS833.js
@@ -35,3 +35,35 @@
 
 
 
+var findReplaceString = function (s, indices, sources, targets) {
+    const n = s.length, m = indices.length;
+
+    const ops = new Map();
+    for (let i = 0; i < m; i++) {
+        if (!ops.has(indices[i])) {
+            ops.set(indices[i], []);
+        }
+        ops.get(indices[i]).push(i);
+    }
+
+    let ans = "";
+    for (let i = 0; i < n;) {
+        let succeed = false;
+        if (ops.has(i)) {
+            console.log(ops.get(i))
+            for (const pt of ops.get(i)) {
+                if (s.substring(i, Math.min(i + sources[pt].length, n)) === sources[pt]) {
+                    succeed = true;
+                    ans += targets[pt];
+                    i += sources[pt].length;
+                    break;
+                }
+            }
+        }
+        if (!succeed) {
+            ans += s[i];
+            ++i;
+        }
+    }
+    return ans;
+};
