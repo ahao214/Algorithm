07dbe5b7da8f1129c1b75bc3a0a0fdeb10f2a345
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-17 00:08:14 +0800

    1444. 切披萨的方案数

---
+++ Ahao.LeetCode/1400-1499/demo1444/Class1444.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1400_1499.demo1444
+{
+    public class Class1444
+    {
+        public int Ways(string[] pizza, int k)
+        {
+            int m = pizza.Length, n = pizza[0].Length, mod = 1_000_000_007;
+            int[][] apples = new int[m + 1][];
+            for (int i = 0; i <= m; i++)
+            {
+                apples[i] = new int[n + 1];
+            }
+            int[][][] dp = new int[k + 1][][];
+            for (int i = 0; i <= k; i++)
+            {
+                dp[i] = new int[m + 1][];
+                for (int j = 0; j <= m; j++)
+                {
+                    dp[i][j] = new int[n + 1];
+                }
+            }
+
+            // 预处理
+            for (int i = m - 1; i >= 0; i--)
+            {
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    apples[i][j] = apples[i][j + 1] + apples[i + 1][j] - apples[i + 1][j + 1] + (pizza[i][j] == 'A' ? 1 : 0);
+                    dp[1][i][j] = apples[i][j] > 0 ? 1 : 0;
+                }
+            }
+
+            for (int ki = 2; ki <= k; ki++)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        // 水平方向切
+                        for (int i2 = i + 1; i2 < m; i2++)
+                        {
+                            if (apples[i][j] > apples[i2][j])
+                            {
+                                dp[ki][i][j] = (dp[ki][i][j] + dp[ki - 1][i2][j]) % mod;
+                            }
+                        }
+                        // 垂直方向切
+                        for (int j2 = j + 1; j2 < n; j2++)
+                        {
+                            if (apples[i][j] > apples[i][j2])
+                            {
+                                dp[ki][i][j] = (dp[ki][i][j] + dp[ki - 1][i][j2]) % mod;
+                            }
+                        }
+                    }
+                }
+            }
+            return dp[k][0][0];
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/1400-1499/demo1444/JS1444.js
@@ -0,0 +1,39 @@
+﻿var ways = function (pizza, k) {
+    const m = pizza.length, n = pizza[0].length, mod = 1_000_000_007;
+    const apples = Array(m + 1).fill(0).map(() => Array(n + 1).fill(0));
+    const dp = Array(k + 1).fill(0).map(() => Array(m + 1).fill(0).map(() => Array(n + 1).fill(0)));
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            apples[i][j] = apples[i][j + 1] + apples[i + 1][j] - apples[i + 1][j + 1] + (pizza[i].charAt(j) == 'A' ? 1 : 0);
+            dp[1][i][j] = apples[i][j] > 0 ? 1 : 0;
+        }
+    }
+
+    // 预处理
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            apples[i][j] = apples[i][j + 1] + apples[i + 1][j] - apples[i + 1][j + 1] + (pizza[i].charAt(j) == 'A' ? 1 : 0);
+            dp[1][i][j] = apples[i][j] > 0 ? 1 : 0;
+        }
+    }
+
+    for (let ki = 2; ki <= k; ki++) {
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < n; j++) {
+                // 水平方向切
+                for (let i2 = i + 1; i2 < m; i2++) {
+                    if (apples[i][j] > apples[i2][j]) {
+                        dp[ki][i][j] = (dp[ki][i][j] + dp[ki - 1][i2][j]) % mod;
+                    }
+                }
+                // 垂直方向切
+                for (let j2 = j + 1; j2 < n; j2++) {
+                    if (apples[i][j] > apples[i][j2]) {
+                        dp[ki][i][j] = (dp[ki][i][j] + dp[ki - 1][i][j2]) % mod;
+                    }
+                }
+            }
+        }
+    }
+    return dp[k][0][0];
+};
