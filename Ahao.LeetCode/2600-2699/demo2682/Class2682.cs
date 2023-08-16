18309f7fbdc4828851e6c4ef75e29f155b5aba5a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-16 11:45:13 +0800

    2682. 找出转圈游戏输家

---
+++ Ahao.LeetCode/2600-2699/demo2682/Class2682.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2600_2699.demo2682
+{
+    public class Class2682
+    {
+        public int[] CircularGameLosers(int n, int k)
+        {
+            bool[] visit = new bool[n];
+            for (int i = k, j = 0; !visit[j]; i += k)
+            {
+                visit[j] = true;
+                j = (j + i) % n;
+            }
+            IList<int> list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (!visit[i])
+                {
+                    list.Add(i + 1);
+                }
+            }
+            int[] ans = new int[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                ans[i] = list[i];
+            }
+            return ans;
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/2600-2699/demo2682/JS2682.js
@@ -0,0 +1,14 @@
+﻿var circularGameLosers = function (n, k) {
+    let visit = new Array(n).fill(false);
+    for (let i = k, j = 0; !visit[j]; i += k) {
+        visit[j] = true;
+        j = (j + i) % n;
+    }
+    let ans = [];
+    for (let i = 0; i < n; i++) {
+        if (!visit[i]) {
+            ans.push(i + 1);
+        }
+    }
+    return ans;
+};
