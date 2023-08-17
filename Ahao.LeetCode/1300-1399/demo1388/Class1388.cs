9877aed97f4364a280d96247ae4f971ec542cb86
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-18 00:04:08 +0800

    1388. 3n 块披萨

---
+++ Ahao.LeetCode/1300-1399/demo1388/Class1388.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1300_1399.demo1388
+{
+    internal class Class1388
+    {
+    }
+}
