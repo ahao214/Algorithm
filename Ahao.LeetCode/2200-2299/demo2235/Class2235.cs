0e604a72ebee66666ff57cff90423f0772c82a33
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-19 13:19:36 +0800

    2235. 两整数相加

---
+++ Ahao.LeetCode/2200-2299/demo2235/Class2235.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2200_2299.demo2235
+{
+    public class Class2235
+    {
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
