ca9f8b8136f5047734ea137a1bdd173198879896
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-20 00:09:25 +0800

    2236. 判断根结点是否等于子结点之和

---
+++ Ahao.LeetCode/2200-2299/demo2236/Class2236.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2200_2299.demo2236
+{
+    public class Class2236
+    {
+        public bool CheckTree(TreeNode root)
+        {
+            if (root.val == root.left.val + root.right.val)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+}
---
+++ Ahao.LeetCode/2200-2299/demo2236/JS2236.js
@@ -0,0 +1,10 @@
+﻿
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+var checkTree = function (root) {
+    return root.val === root.left.val + root.right.val;
+};
