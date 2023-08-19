916787426131050507dd7673e3ae6d7c31ffc127
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-19 13:22:31 +0800

    2235. 两整数相加

---
+++ Ahao.LeetCodeCPP/2235. 两整数相加.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+
+using namespace std;
+
+/*
+2235. 两整数相加
+
+给你两个整数 num1 和 num2，返回这两个整数的和。
+*/
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
