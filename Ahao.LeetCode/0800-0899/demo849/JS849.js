3d081bd13858101caeb1b11fc721f441194734ab
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-22 14:06:44 +0800

    849. 到最近的人的最大距离

--- Ahao.LeetCode/0800-0899/demo849/JS849.js
+++ Ahao.LeetCode/0800-0899/demo849/JS849.js
@@ -1,1 +1,20 @@
-﻿
+﻿var maxDistToClosest = function (seats) {
+    let res = 0, l = 0;
+    while (l < seats.length && seats[l] === 0) {
+        l++;
+    }
+    res = Math.max(res, l);
+    while (l < seats.length) {
+        let r = l + 1;
+        while (r < seats.length && seats[r] === 0) {
+            r++;
+        }
+        if (r === seats.length) {
+            res = Math.max(res, r - l - 1);
+        } else {
+            res = Math.max(res, parseInt((r - l) / 2));
+        }
+        l = r;
+    }
+    return res;
+};
