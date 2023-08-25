6db4f0470360c24b5ca7c793bcd874c9f7144926
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-25 14:33:07 +0800

    1448. 统计二叉树中好节点的数目

---
+++ Ahao.LeetCode/1400-1499/demo1448/Class1448.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1400_1499.demo1448
+{
+    public class Class1448
+    {
+
+        public int GoodNodes(TreeNode root)
+        {
+            return DFS(root, int.MinValue);
+        }
+
+        public int DFS(TreeNode root, int pathMax)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int res = 0;
+            if (root.val >= pathMax)
+            {
+                res++;
+                pathMax = root.val;
+            }
+            res += DFS(root.left, pathMax) + DFS(root.right, pathMax);
+            return res;
+        }
+
+
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
