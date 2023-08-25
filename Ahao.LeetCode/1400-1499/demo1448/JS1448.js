76b87fe6bc19dadb0edca037e5f3a96d331c34d2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-25 14:33:50 +0800

    1448. 统计二叉树中好节点的数目

--- Ahao.LeetCode/1400-1499/demo1448/JS1448.js
+++ Ahao.LeetCode/1400-1499/demo1448/JS1448.js
@@ -1,1 +1,22 @@
-﻿
+﻿function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+
+var goodNodes = function (root) {
+    const dfs = (root, path_max) => {
+        if (root == null) {
+            return 0;
+        }
+        let res = 0;
+        if (root.val >= path_max) {
+            res++;
+            path_max = root.val;
+        }
+        res += dfs(root.left, path_max) + dfs(root.right, path_max);
+        return res;
+    }
+    return dfs(root, -Infinity);
+};
