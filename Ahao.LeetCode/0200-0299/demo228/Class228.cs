5b9606d91f20b5b9f55589541d6e7f58e452532c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-26 00:10:47 +0800

    228. 汇总区间

--- Ahao.LeetCode/0200-0299/demo228/Class228.cs
+++ Ahao.LeetCode/0200-0299/demo228/Class228.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace Ahao.LeetCode._0200_0299.demo228
 {
-    public  class Class228
+    public class Class228
     {
         public IList<string> SummaryRanges(int[] nums)
         {
-
+            List<String> ret = new List<String>();
+            int i = 0;
+            int n = nums.Length;
+            while (i < n)
+            {
+                int low = i;
+                i++;
+                while (i < n && nums[i] == nums[i - 1] + 1)
+                {
+                    i++;
+                }
+                int high = i - 1;
+                StringBuilder tmp = new StringBuilder(nums[low].ToString());
+                if (low < high)
+                {
+                    tmp.Append("->");
+                    tmp.Append(nums[high].ToString());
+                }
+                ret.Add(tmp.ToString());
+            }
+            return ret;
         }
 
 
