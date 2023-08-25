a3f726474c65a0813a2aaf59aae7db8e40525ad3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-26 00:06:38 +0800

    228. 汇总区间

---
+++ Ahao.LeetCode/0200-0299/demo228/Class228.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0200_0299.demo228
+{
+    public  class Class228
+    {
+        public IList<string> SummaryRanges(int[] nums)
+        {
+
+        }
+
+
+    }
+}
