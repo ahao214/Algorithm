cfdb1f04d764b1092e58f615e4ef59407526bd7c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-29 00:10:31 +0800

    823. 带因子的二叉树

---
+++ Ahao.LeetCode/0800-0899/demo823/Class823.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0800_0899.demo823
+{
+    public class Class823
+    {
+        public int NumFactoredBinaryTrees(int[] arr)
+        {
+            Array.Sort(arr);
+            int n = arr.Length;
+            long[] dp = new long[n];
+            long res = 0, mod = 1000000007;
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = 1;
+                for (int left = 0, right = i - 1; left <= right; left++)
+                {
+                    while (right >= left && (long)arr[left] * arr[right] > arr[i])
+                    {
+                        right--;
+                    }
+                    if (right >= left && (long)arr[left] * arr[right] == arr[i])
+                    {
+                        if (right != left)
+                        {
+                            dp[i] = (dp[i] + dp[left] * dp[right] * 2) % mod;
+                        }
+                        else
+                        {
+                            dp[i] = (dp[i] + dp[left] * dp[right]) % mod;
+                        }
+                    }
+                }
+                res = (res + dp[i]) % mod;
+            }
+            return (int)res;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/0800-0899/demo823/JS823.js
@@ -0,0 +1,23 @@
+﻿var numFactoredBinaryTrees = function (arr) {
+    const n = arr.length;
+    const mod = 1e9 + 7;
+    const dp = new Array(n).fill(1)
+    arr.sort((a, b) => a - b);
+    let res = 0;
+    for (let i = 0; i < n; i++) {
+        for (let left = 0, right = i - 1; left <= right; left++) {
+            while (right >= left && arr[left] * arr[right] > arr[i]) {
+                right--;
+            }
+            if (right >= left && arr[left] * arr[right] == arr[i]) {
+                if (right != left) {
+                    dp[i] = (dp[i] + dp[left] * dp[right] * 2) % mod;
+                } else {
+                    dp[i] = (dp[i] + dp[left] * dp[right]) % mod;
+                }
+            }
+        }
+        res = (res + dp[i]) % mod;
+    }
+    return res;
+};
