7c02e0624a4963a6f1b1982b46289784e203c18a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-29 00:19:04 +0800

    823. 带因子的二叉树

--- Ahao.LeetCode/0800-0899/demo823/Class823.cs
+++ Ahao.LeetCode/0800-0899/demo823/Class823.cs
@@ -40,5 +40,76 @@ namespace Ahao.LeetCode._0800_0899.demo823
             return (int)res;
         }
 
+
+
+
+        public int NumFactoredBinaryTrees2(int[] arr)
+        {
+            const int MODULO = 1000000007;
+            Array.Sort(arr);
+            IDictionary<int, int> indices = new Dictionary<int, int>();
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                indices.Add(arr[i], i);
+            }
+            long[] dp = new long[n];
+            Array.Fill(dp, 1);
+            for (int i = 1; i < n; i++)
+            {
+                int root = arr[i];
+                for (int j = 0; j < i && (long)arr[j] * arr[j] <= arr[i]; j++)
+                {
+                    int left = arr[j];
+                    if (root % left == 0 && indices.ContainsKey(root / left))
+                    {
+                        int right = root / left;
+                        int k = indices[right];
+                        if (j == k)
+                        {
+                            dp[i] = (dp[i] + dp[j] * dp[k]) % MODULO;
+                        }
+                        else
+                        {
+                            dp[i] = (dp[i] + dp[j] * dp[k] * 2) % MODULO;
+                        }
+                    }
+                }
+            }
+            int total = 0;
+            for (int i = 0; i < n; i++)
+            {
+                total = (total + (int)dp[i]) % MODULO;
+            }
+            return total;
+        }
+
+
+
+
+        public int NumFactoredBinaryTrees3(int[] arr)
+        {
+            const int MOD = 1_000_000_007;
+            long res = 0;
+            var dict = new Dictionary<int, long>();
+            Array.Sort(arr);
+            foreach (var num in arr)
+            {
+                long value = 1;
+                foreach (var key in dict.Keys)
+                {
+                    if (num % key == 0 && dict.ContainsKey(num / key))
+                    {
+                        value += dict[key] * dict[num / key];
+                    }
+                }
+                dict.Add(num, value);
+                res += value;
+                res %= MOD;
+            }
+            return (int)res;
+        }
+
+
     }
 }
