f1c84b4d7167b0a39c60dc8a4783b22d3d53ed30
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-29 00:11:33 +0800

    823. 带因子的二叉树

---
+++ Ahao.LeetCodeCPP/823. 带因子的二叉树.cpp
@@ -0,0 +1,43 @@
+#include<vector>
+#include <algorithm>
+
+using namespace std;
+
+
+/*
+823. 带因子的二叉树
+
+给出一个含有不重复整数元素的数组 arr ，每个整数 arr[i] 均大于 1。
+
+用这些整数来构建二叉树，每个整数可以使用任意次数。其中：每个非叶结点的值应等于它的两个子结点的值的乘积。
+
+满足条件的二叉树一共有多少个？答案可能很大，返回 对 109 + 7 取余 的结果。
+*/
+
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        vector<long long> dp(n);
+        long long res = 0, mod = 1e9 + 7;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int left = 0, right = i - 1; left <= right; left++) {
+                while (right >= left && (long long)arr[left] * arr[right] > arr[i]) {
+                    right--;
+                }
+                if (right >= left && (long long)arr[left] * arr[right] == arr[i]) {
+                    if (right != left) {
+                        dp[i] = (dp[i] + dp[left] * dp[right] * 2) % mod;
+                    }
+                    else {
+                        dp[i] = (dp[i] + dp[left] * dp[right]) % mod;
+                    }
+                }
+            }
+            res = (res + dp[i]) % mod;
+        }
+        return res;
+    }
+};
