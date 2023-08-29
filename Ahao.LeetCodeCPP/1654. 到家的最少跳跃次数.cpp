35a7ba9a8b40099daa4ef6e3b080f40ec3a0cc54
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-30 00:12:13 +0800

    1654. 到家的最少跳跃次数

--- Ahao.LeetCodeCPP/1654. 到家的最少跳跃次数.cpp
+++ Ahao.LeetCodeCPP/1654. 到家的最少跳跃次数.cpp
@@ -1,11 +1,13 @@
 #include<vector>
 #include <queue>
+#include <unordered_set>
 
 using namespace std;
 
 
 /*
 1654. 到家的最少跳跃次数
+
 有一只跳蚤的家在数轴上的位置 x 处。请你帮助它从位置 0 出发，到达它的家。
 
 跳蚤跳跃的规则如下：
@@ -50,6 +52,41 @@ public:
             }
         }
 
+        return -1;
+    }
+};
+
+
+class Solution {
+public:
+    int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+        queue<tuple<int, int, int>> q;
+        unordered_set<int> visited;
+        q.emplace(0, 1, 0);
+        visited.emplace(0);
+        int lower = 0, upper = max(*max_element(forbidden.begin(), forbidden.end()) + a, x) + b;
+        unordered_set<int> forbiddenSet(forbidden.begin(), forbidden.end());
+        while (!q.empty()) {
+            auto [position, direction, step] = q.front();
+            q.pop();
+            if (position == x) {
+                return step;
+            }
+            int nextPosition = position + a;
+            int nextDirection = 1;
+            if (lower <= nextPosition && nextPosition <= upper && !visited.count(nextPosition * nextDirection) && !forbiddenSet.count(nextPosition)) {
+                visited.emplace(nextPosition * nextDirection);
+                q.emplace(nextPosition, nextDirection, step + 1);
+            }
+            if (direction == 1) {
+                nextPosition = position - b;
+                nextDirection = -1;
+                if (lower <= nextPosition && nextPosition <= upper && !visited.count(nextPosition * nextDirection) && !forbiddenSet.count(nextPosition)) {
+                    visited.emplace(nextPosition * nextDirection);
+                    q.emplace(nextPosition, nextDirection, step + 1);
+                }
+            }
+        }
         return -1;
     }
 };
