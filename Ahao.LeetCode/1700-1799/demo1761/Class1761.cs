8fe1afaf4281c660b95cf20ae798231f76e4163b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-31 00:06:43 +0800

    1761. 一个图中连通三元组的最小度数

--- Ahao.LeetCode/1700-1799/demo1761/Class1761.cs
+++ Ahao.LeetCode/1700-1799/demo1761/Class1761.cs
@@ -46,5 +46,63 @@ namespace Ahao.LeetCode._1700_1799.demo1761
             return ans == int.MaxValue ? -1 : ans;
         }
 
+
+        public int MinTrioDegree2(int n, int[][] edges)
+        {
+            // 原图
+            ISet<int>[] g = new ISet<int>[n];
+            for (int i = 0; i < n; ++i)
+            {
+                g[i] = new HashSet<int>();
+            }
+            // 定向后的图
+            IList<int>[] h = new IList<int>[n];
+            for (int i = 0; i < n; ++i)
+            {
+                h[i] = new List<int>();
+            }
+            int[] degree = new int[n];
+
+            foreach (int[] edge in edges)
+            {
+                int x = edge[0] - 1, y = edge[1] - 1;
+                g[x].Add(y);
+                g[y].Add(x);
+                ++degree[x];
+                ++degree[y];
+            }
+            foreach (int[] edge in edges)
+            {
+                int x = edge[0] - 1, y = edge[1] - 1;
+                if (degree[x] < degree[y] || (degree[x] == degree[y] && x < y))
+                {
+                    h[x].Add(y);
+                }
+                else
+                {
+                    h[y].Add(x);
+                }
+            }
+
+            int ans = int.MaxValue;
+            for (int i = 0; i < n; ++i)
+            {
+                foreach (int j in h[i])
+                {
+                    foreach (int k in h[j])
+                    {
+                        if (g[i].Contains(k))
+                        {
+                            ans = Math.Min(ans, degree[i] + degree[j] + degree[k] - 6);
+                        }
+                    }
+                }
+            }
+
+            return ans == int.MaxValue ? -1 : ans;
+        }
+
+
+
     }
 }
