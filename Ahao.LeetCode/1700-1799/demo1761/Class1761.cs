5b60c02e99d9a5af1e11fc6258bdf4990d9b46f1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-31 00:05:52 +0800

    1761. 一个图中连通三元组的最小度数

---
+++ Ahao.LeetCode/1700-1799/demo1761/Class1761.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1700_1799.demo1761
+{
+    public class Class1761
+    {
+        public int MinTrioDegree(int n, int[][] edges)
+        {
+            int[][] g = new int[n][];
+            for (int i = 0; i < n; ++i)
+            {
+                g[i] = new int[n];
+            }
+            int[] degree = new int[n];
+
+            foreach (int[] edge in edges)
+            {
+                int x = edge[0] - 1, y = edge[1] - 1;
+                g[x][y] = g[y][x] = 1;
+                ++degree[x];
+                ++degree[y];
+            }
+
+            int ans = int.MaxValue;
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = i + 1; j < n; ++j)
+                {
+                    if (g[i][j] == 1)
+                    {
+                        for (int k = j + 1; k < n; ++k)
+                        {
+                            if (g[i][k] == 1 && g[j][k] == 1)
+                            {
+                                ans = Math.Min(ans, degree[i] + degree[j] + degree[k] - 6);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return ans == int.MaxValue ? -1 : ans;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1700-1799/demo1761/JS1761.js
@@ -0,0 +1,24 @@
+﻿var minTrioDegree = function (n, edges) {
+    const degree = Array(n).fill(0);
+    const g = Array(n).fill(0).map(() => new Array(n).fill(0));
+
+    for (const [x, y] of edges) {
+        g[x - 1][y - 1] = g[y - 1][x - 1] = 1;
+        degree[x - 1]++;
+        degree[y - 1]++;
+    }
+
+    let ans = Infinity;
+    for (let i = 0; i < n; ++i) {
+        for (let j = i + 1; j < n; ++j) {
+            if (g[i][j] == 1) {
+                for (let k = j + 1; k < n; ++k) {
+                    if (g[i][k] == 1 && g[j][k] == 1) {
+                        ans = Math.min(ans, degree[i] + degree[j] + degree[k] - 6);
+                    }
+                }
+            }
+        }
+    }
+    return ans == Infinity ? -1 : ans;
+};
