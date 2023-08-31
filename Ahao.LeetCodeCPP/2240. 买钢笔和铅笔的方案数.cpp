f5d6cc4bce81f0cffee7790b32cf1fc160d44e84
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-01 00:12:01 +0800

    2240. 买钢笔和铅笔的方案数

---
+++ Ahao.LeetCodeCPP/2240. 买钢笔和铅笔的方案数.cpp
@@ -0,0 +1,28 @@
+#include<vector>
+
+using namespace std;
+
+
+/*
+2240. 买钢笔和铅笔的方案数
+
+给你一个整数 total ，表示你拥有的总钱数。同时给你两个整数 cost1 和 cost2 ，分别表示一支钢笔和一支铅笔的价格。你可以花费你部分或者全部的钱，去买任意数目的两种笔。
+
+请你返回购买钢笔和铅笔的 不同方案数目 。
+*/
+
+
+class Solution {
+public:
+    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        if (cost1 < cost2) {
+            return waysToBuyPensPencils(total, cost2, cost1);
+        }
+        long res = 0, cnt = 0;
+        while (cnt * cost1 <= total) {
+            res += (total - cnt * cost1) / cost2 + 1;
+            cnt++;
+        }
+        return res;
+    }
+};
