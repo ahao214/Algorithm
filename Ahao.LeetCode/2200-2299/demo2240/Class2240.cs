f7714b052951297916aaa1fdf3dace382c5bd050
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-01 00:09:17 +0800

    2240. 买钢笔和铅笔的方案数

---
+++ Ahao.LeetCode/2200-2299/demo2240/Class2240.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2200_2299.demo2240
+{
+    public class Class2240
+    {
+        public long WaysToBuyPensPencils(int total, int cost1, int cost2)
+        {
+            if (cost1 < cost2)
+            {
+                return WaysToBuyPensPencils(total, cost2, cost1);
+            }
+            long res = 0, cnt = 0;
+            while (cnt * cost1 <= total)
+            {
+                res += (total - cnt * cost1) / cost2 + 1;
+                cnt++;
+            }
+            return res;
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/2200-2299/demo2240/JS2240.js
@@ -0,0 +1,11 @@
+﻿var waysToBuyPensPencils = function (total, cost1, cost2) {
+    if (cost1 < cost2) {
+        return waysToBuyPensPencils(total, cost2, cost1);
+    }
+    let res = 0, cnt = 0;
+    while (cnt * cost1 <= total) {
+        res += parseInt((total - cnt * cost1) / cost2) + 1;
+        cnt++;
+    }
+    return res;
+};
