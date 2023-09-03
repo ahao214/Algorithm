e6dad1f7f362c265b1fac3bb92104a6778519cb1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-03 15:15:52 +0800

    1921. 消灭怪物的最大数量

---
+++ Ahao.LeetCode/1900-1999/demo1921/Class1921.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1900_1999.demo1921
+{
+    public class Class1921
+    {
+        public int EliminateMaximum(int[] dist, int[] speed)
+        {
+            int n = dist.Length;
+            int[] minTime = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                minTime[i] = (dist[i] - 1) / speed[i] + 1;
+            }
+            Array.Sort(minTime);
+
+            for (int i = 0; i < n; i++)
+            {
+                if (minTime[i] <= i)
+                {
+                    return i;
+                }
+            }
+            return n;
+        }
+
+    }
+}
