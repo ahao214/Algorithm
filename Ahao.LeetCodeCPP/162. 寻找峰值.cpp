bc9bf5434619773d628c2eed7d5bd4394b6f9fa9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-04 09:23:12 +0800

    162. 寻找峰值

--- Ahao.LeetCodeCPP/162. 寻找峰值.cpp
+++ Ahao.LeetCodeCPP/162. 寻找峰值.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 /*
 162. 寻找峰值
+
 峰值元素是指其值严格大于左右相邻值的元素。
 
 给你一个整数数组 nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
@@ -30,6 +31,25 @@ public:
 			{
 				right = mid - 1;
 			}
+		}
+		return right;
+	}
+};
+
+
+
+
+class Solution {
+public:
+	int findPeakElement(vector<int>& nums) {
+		int left = 0, right = nums.size() - 1;
+		while (left < right)
+		{
+			int mid = left + right >> 1;
+			if (nums[mid] > nums[mid + 1])
+				right = mid;
+			else
+				left = mid + 1;
 		}
 		return right;
 	}
