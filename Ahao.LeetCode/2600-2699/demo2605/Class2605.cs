34d64a73ed6ec8807b3b9d65d6390661a3a84a06
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-05 00:20:50 +0800

    1,2,3,4,6,8

---
+++ Ahao.LeetCode/2600-2699/demo2605/Class2605.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2600_2699.demo2605
+{
+    public class Class2605
+    {
+        public int MinNumber(int[] nums1, int[] nums2)
+        {
+            Array.Sort(nums1);
+            Array.Sort(nums2);
+
+            int n1 = nums1.Length;
+            int n2 = nums2.Length;
+            int i = 0;
+            int j = 0;
+            while (i < n1 && j < n2)
+            {
+                if (nums1[i] == nums2[j])
+                {
+                    return nums1[i];
+                }
+                else if (nums1[i] > nums2[j])
+                {
+                    j++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return nums1[0] < nums2[0] ? nums1[0] * 10 + nums2[0] : nums2[0] * 10 + nums1[0];
+        }
+    }
+}
