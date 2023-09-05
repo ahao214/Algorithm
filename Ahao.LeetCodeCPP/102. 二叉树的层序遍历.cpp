17eca3fbfb56f24f8501348c58c6154186a391c0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-06 00:22:19 +0800

    102. 二叉树的层序遍历

--- Ahao.LeetCodeCPP/102. 二叉树的层序遍历.cpp
+++ Ahao.LeetCodeCPP/102. 二叉树的层序遍历.cpp
@@ -19,6 +19,7 @@ struct TreeNode {
 
 /*
 102. 二叉树的层序遍历
+
 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。
 （即逐层地，从左到右访问所有节点）。
 */
@@ -36,6 +37,33 @@ public:
 			int len = q.size();
 			vector<int> level;
 			for (int i = 0; i < len; i++)
+			{
+				auto t = q.front();
+				q.pop();
+				level.push_back(t->val);
+				if (t->left) q.push(t->left);
+				if (t->right) q.push(t->right);
+			}
+			res.push_back(level);
+		}
+		return res;
+	}
+};
+
+
+
+class Solution {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>> res;
+		queue<TreeNode*> q;
+		if (root) q.push(root);
+
+		while (q.size())
+		{
+			vector<int> level;
+			int len = q.size();
+			while (len--)
 			{
 				auto t = q.front();
 				q.pop();
