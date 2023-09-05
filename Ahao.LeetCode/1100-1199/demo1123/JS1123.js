d3ec07312f91a1b3063a6023e8780dbad69d25bb
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-06 00:06:15 +0800

    1123. 最深叶节点的最近公共祖先

--- Ahao.LeetCode/1100-1199/demo1123/JS1123.js
+++ Ahao.LeetCode/1100-1199/demo1123/JS1123.js
@@ -6,5 +6,25 @@
 
 
 
+var lcaDeepestLeaves = function (root) {
+    return f(root)[1];
+};
+
+function f(root) {
+    if (!root) {
+        return [0, root];
+    }
+
+    let [d1, lca1] = f(root.left);
+    let [d2, lca2] = f(root.right);
+
+    if (d1 > d2) {
+        return [d1 + 1, lca1];
+    }
+    if (d1 < d2) {
+        return [d2 + 1, lca2];
+    }
+    return [d1 + 1, root];
+}
 
 
