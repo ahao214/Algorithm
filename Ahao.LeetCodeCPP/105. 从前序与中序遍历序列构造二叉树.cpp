f492299f014a9e5e861783d0e20be2ca16f3b39c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-06 00:17:53 +0800

    105. 从前序与中序遍历序列构造二叉树

--- Ahao.LeetCodeCPP/105. 从前序与中序遍历序列构造二叉树.cpp
+++ Ahao.LeetCodeCPP/105. 从前序与中序遍历序列构造二叉树.cpp
@@ -16,6 +16,7 @@ struct TreeNode {
 
 /*
 105. 从前序与中序遍历序列构造二叉树
+
 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历，
 inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
 */
@@ -41,3 +42,27 @@ public:
 		return root;
 	}
 };
+
+
+
+class Solution {
+public:
+	unordered_map<int, int> pos;
+
+	TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+		for (int i = 0; i < inorder.size(); i++)
+			pos[inorder[i]] = i;
+		return build(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
+	}
+
+	TreeNode* build(vector<int>& preorder, vector<int>& inorder, int pl, int pr, int il, int ir)
+	{
+		if (pl > pr) return NULL;
+		auto root = new TreeNode(preorder[pl]);
+		int k = pos[root->val];
+		root->left = build(preorder, inorder, pl + 1, pl + k - il, il, k - 1);
+		root->right = build(preorder, inorder, pl + k - il + 1, pr, k + 1, ir);
+		return root;
+	}
+};
+
