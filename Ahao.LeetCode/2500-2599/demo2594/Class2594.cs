6f4e68a9190c2d451b452177d44f59fb373a3428
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-07 00:12:03 +0800

    2594. 修车的最少时间

--- Ahao.LeetCode/2500-2599/demo2594/Class2594.cs
+++ Ahao.LeetCode/2500-2599/demo2594/Class2594.cs
@@ -8,7 +8,34 @@ namespace Ahao.LeetCode._2500_2599.demo2594
 {
     public  class Class2594
     {
+            public long RepairCars(int[] ranks, int cars)
+            {
+                long l = 1, r = 1l * ranks[0] * cars * cars;
+                while (l < r)
+                {
+                    long m = l + r >> 1;
+                    if (Check(ranks, cars, m))
+                    {
+                        r = m;
+                    }
+                    else
+                    {
+                        l = m + 1;
+                    }
+                }
+                return l;
+            }
 
+            public bool Check(int[] ranks, int cars, long m)
+            {
+                long cnt = 0;
+                foreach (int x in ranks)
+                {
+                    cnt += (long)Math.Sqrt(m / x);
+                }
+                return cnt >= cars;
+            }
+        
 
     }
 }
