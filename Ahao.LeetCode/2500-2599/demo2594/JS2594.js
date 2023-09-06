4c92416900b5ae51f18847518e4b4b2cc6cada8f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-07 00:11:13 +0800

    2594. 修车的最少时间

---
+++ Ahao.LeetCode/2500-2599/demo2594/JS2594.js
@@ -0,0 +1,21 @@
+﻿var repairCars = function (ranks, cars) {
+    let l = 1;
+    let r = ranks[0] * cars * cars;
+    const check = (m) => {
+        let cnt = 0;
+        for (const x of ranks) {
+            cnt += Math.floor(Math.sqrt(m / x));
+        }
+        return cnt >= cars;
+    }
+
+    while (l < r) {
+        const m = (l + r) >> 1;
+        if (check(m)) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+    return l;
+};
