5fbf9655b8b59a20d8e6fe561f7ced3f4ba91e54
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-09 00:09:25 +0800

    173. 二叉搜索树迭代器

--- Ahao.LeetCodeCPP/173. 二叉搜索树迭代器.cpp
+++ Ahao.LeetCodeCPP/173. 二叉搜索树迭代器.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 /*
 173. 二叉搜索树迭代器
+
 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
 BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
 boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
@@ -54,6 +55,7 @@ public:
 
 /*
 173. 二叉搜索树迭代器
+
 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）
 的迭代器：
 BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。
@@ -93,5 +95,34 @@ public:
 
 	bool hasNext() {
 		return !stk.empty();
+	}
+};
+
+
+
+
+class BSTIterator {
+	stack<TreeNode*> stk;
+	TreeNode* cur;
+public:
+	BSTIterator(TreeNode* root) {
+		cur = root;
+	}
+
+	int next() {
+		while (cur)
+		{
+			stk.push(cur);
+			cur = cur->left;
+		}
+		cur = stk.top();
+		stk.pop();
+		int res = cur->val;
+		cur = cur->right;
+		return res;
+	}
+
+	bool hasNext() {
+		return cur || !stk.empty();
 	}
 };
