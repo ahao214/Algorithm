3da75f4f2b16e98fd5e9fca2b04b999b37a886d4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-09 00:03:28 +0800

    207. 课程表

--- Ahao.LeetCodeCPP/207. 课程表.cpp
+++ Ahao.LeetCodeCPP/207. 课程表.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 /*
 207. 课程表
+
 你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
 
 在选修某些课程之前需要一些先修课程。
@@ -53,3 +54,84 @@ public:
 		return cnt == numCourses;
 	}
 };
+
+
+
+class Solution {
+private:
+	vector<vector<int>> edges;
+	vector<int> indeg;
+
+public:
+	bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+		edges.resize(numCourses);
+		indeg.resize(numCourses);
+		for (const auto& info : prerequisites) {
+			edges[info[1]].push_back(info[0]);
+			++indeg[info[0]];
+		}
+
+		queue<int> q;
+		for (int i = 0; i < numCourses; ++i) {
+			if (indeg[i] == 0) {
+				q.push(i);
+			}
+		}
+
+		int visited = 0;
+		while (!q.empty()) {
+			++visited;
+			int u = q.front();
+			q.pop();
+			for (int v : edges[u]) {
+				--indeg[v];
+				if (indeg[v] == 0) {
+					q.push(v);
+				}
+			}
+		}
+
+		return visited == numCourses;
+	}
+};
+
+
+class Solution {
+private:
+	vector<vector<int>> edges;
+	vector<int> visited;
+	bool valid = true;
+
+public:
+	void dfs(int u) {
+		visited[u] = 1;
+		for (int v : edges[u]) {
+			if (visited[v] == 0) {
+				dfs(v);
+				if (!valid) {
+					return;
+				}
+			}
+			else if (visited[v] == 1) {
+				valid = false;
+				return;
+			}
+		}
+		visited[u] = 2;
+	}
+
+	bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+		edges.resize(numCourses);
+		visited.resize(numCourses);
+		for (const auto& info : prerequisites) {
+			edges[info[1]].push_back(info[0]);
+		}
+		for (int i = 0; i < numCourses && valid; ++i) {
+			if (!visited[i]) {
+				dfs(i);
+			}
+		}
+		return valid;
+	}
+};
+
