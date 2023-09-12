ae2d8ec77a5bbd339742defa43f164fe0ba16d35
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-12 13:49:28 +0800

    1462. 课程表 IV

--- Ahao.LeetCode/1400-1499/demo1462/Class1462.cs
+++ Ahao.LeetCode/1400-1499/demo1462/Class1462.cs
@@ -60,6 +60,53 @@ namespace Ahao.LeetCode._1400_1499.demo1462
         }
 
 
+        public IList<bool> CheckIfPrerequisite2(int numCourses, int[][] prerequisites, int[][] queries)
+        {
+            IList<int>[] g = new IList<int>[numCourses];
+            for (int i = 0; i < numCourses; i++)
+            {
+                g[i] = new List<int>();
+            }
+            bool[] vi = new bool[numCourses];
+            bool[][] isPre = new bool[numCourses][];
+            for (int i = 0; i < numCourses; i++)
+            {
+                isPre[i] = new bool[numCourses];
+            }
+            foreach (int[] p in prerequisites)
+            {
+                g[p[0]].Add(p[1]);
+            }
+            for (int i = 0; i < numCourses; ++i)
+            {
+                DFS(g, isPre, vi, i);
+            }
+            IList<bool> res = new List<bool>();
+            foreach (int[] query in queries)
+            {
+                res.Add(isPre[query[0]][query[1]]);
+            }
+            return res;
+        }
+
+        public void DFS(IList<int>[] g, bool[][] isPre, bool[] vi, int cur)
+        {
+            if (vi[cur])
+            {
+                return;
+            }
+            vi[cur] = true;
+            foreach (int ne in g[cur])
+            {
+                DFS(g, isPre, vi, ne);
+                isPre[cur][ne] = true;
+                for (int i = 0; i < isPre.Length; ++i)
+                {
+                    isPre[cur][i] = isPre[cur][i] | isPre[ne][i];
+                }
+            }
+        }
+
 
 
     }
