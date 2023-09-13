0f509a07375ba12c80079c113a15a339b75978b4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-13 14:38:34 +0800

    2596. 检查骑士巡视方案

--- Ahao.LeetCode/2500-2599/demo2596/JS2596.js
+++ Ahao.LeetCode/2500-2599/demo2596/JS2596.js
@@ -1,1 +1,21 @@
-﻿\
+﻿var checkValidGrid = function (grid) {
+    if (grid[0][0] != 0) {
+        return false;
+    }
+    const n = grid.length;
+    let indices = Array(n * n).fill().map(() => Array(2));
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            indices[grid[i][j]][0] = i;
+            indices[grid[i][j]][1] = j;
+        }
+    }
+    for (let i = 1; i < n * n; i++) {
+        let dx = Math.abs(indices[i][0] - indices[i - 1][0]);
+        let dy = Math.abs(indices[i][1] - indices[i - 1][1]);
+        if (dx * dy != 2) {
+            return false;
+        }
+    }
+    return true;
+};
