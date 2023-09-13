ee4eb29d3e6864f98eb023691d17e97c3367a24b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-13 14:36:39 +0800

    2596. 检查骑士巡视方案

---
+++ Ahao.LeetCode/2500-2599/demo2596/Class2596.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2500_2599.demo2596
+{
+    internal class Class2596
+    {
+
+
+
+    }
+}
