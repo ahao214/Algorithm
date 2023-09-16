a96fd896c0b46525e9eef158bee36f1a151fb8e8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-16 20:44:05 +0800

    160. 相交链表

--- Ahao.LeetCodeCPP/160. 相交链表.cpp
+++ Ahao.LeetCodeCPP/160. 相交链表.cpp
@@ -46,3 +46,18 @@ public:
 		return p;
 	}
 };
+
+
+
+class Solution {
+public:
+	ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+		ListNode* p = headA, * q = headB;
+		while (p != q)
+		{
+			p = p ? p->next : headB;
+			q = q ? q->next : headA;
+		}
+		return p;
+	}
+};
