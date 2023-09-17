c720eac4dc676a1e8fedaa1b452c8dd19813f5a6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-17 18:24:25 +0800

    154. 寻找旋转排序数组中的最小值 II

--- Ahao.LeetCodeCPP/154. 寻找旋转排序数组中的最小值 II.cpp
+++ Ahao.LeetCodeCPP/154. 寻找旋转排序数组中的最小值 II.cpp
@@ -32,3 +32,23 @@ public:
 		return nums[right];
 	}
 };
+
+
+
+class Solution {
+public:
+	int findMin(vector<int>& nums) {
+		int left = 0, right = nums.size() - 1;
+		while (left < right && nums[left] == nums[right])
+			left++;
+		while (left < right)
+		{
+			int mid = left + (right - left) / 2;
+			if (nums[mid] <= nums.back())
+				right = mid;
+			else
+				left = mid + 1;
+		}
+		return nums[right];
+	}
+};
