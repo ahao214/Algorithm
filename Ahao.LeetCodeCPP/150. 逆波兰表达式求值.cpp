5f773a6b87dc427e30652d5841362c49889c7b92
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-18 18:10:38 +0800

    150. 逆波兰表达式求值

---
+++ Ahao.LeetCodeCPP/150. 逆波兰表达式求值.cpp
@@ -0,0 +1,38 @@
+#include<vector>
+
+using namespace std;
+
+
+
+/*
+150. 逆波兰表达式求值
+*/
+//时间O(N) 空间O(N)
+class Solution {
+public:
+	int evalRPN(vector<string>& tokens) {
+		stack<int> stk;
+		for (string& token : tokens)
+		{
+			if (token == "+" || token == "-" || token == "*" || token == "/")
+			{
+				int b = stk.top();
+				stk.pop();
+				int a = stk.top();
+				stk.pop();
+				if (token == "+")
+					stk.push(a + b);
+				else if (token == "-")
+					stk.push(a - b);
+				else if (token == "*")
+					stk.push(a * b);
+				else
+					stk.push(a / b);
+			}
+			else {
+				stk.push(stoi(token));
+			}
+		}
+		return stk.top();
+	}
+};
