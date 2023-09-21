51cf8ab8bf0e6680c594434a7753a2fa437dcb1b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-22 00:18:38 +0800

    2591. 将钱分给最多的儿童

--- Ahao.LeetCode/2500-2599/demo2591/JS2591.js
+++ Ahao.LeetCode/2500-2599/demo2591/JS2591.js
@@ -1,1 +1,13 @@
-﻿
+﻿var distMoney = function (money, children) {
+    if (money < children) {
+        return -1;
+    }
+    money -= children
+    let cnt = Math.min(Math.floor(money / 7), children);
+    money -= cnt * 7;
+    children -= cnt;
+    if ((children == 0 && money > 0) || (children == 1 && money == 3)) {
+        cnt -= 1;
+    }
+    return cnt;
+};
