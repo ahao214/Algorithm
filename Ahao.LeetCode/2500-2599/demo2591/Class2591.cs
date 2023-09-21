61b8a48505a08d3ac1996c8c730e942ffe29e990
liupenghao@fulan.com.cn <haochen214@163.com>
2023-09-22 00:17:10 +0800

    2591. 将钱分给最多的儿童

---
+++ Ahao.LeetCode/2500-2599/demo2591/Class2591.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2500_2599.demo2591
+{
+    internal class Class2591
+    {
+        public int DistMoney(int money, int children)
+        {
+            int res = -1;
+            if (money < children)
+            {
+                return res;
+            }
+            else if (money > 8 * children)
+            {
+                res = children - 1;
+            }
+
+            else if (money == 8 * children - 4)
+            {
+                res = children - 2;
+            }
+            else
+            {
+                res = (money - children) / 7;
+            }
+
+            return res;
+        }
+    }
+}
