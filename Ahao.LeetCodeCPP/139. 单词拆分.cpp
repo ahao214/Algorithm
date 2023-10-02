549b9d9502d81b84221a5d07dc18577f77efbfd3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-02 12:54:51 +0800

    139. 单词拆分

--- Ahao.LeetCodeCPP/139. 单词拆分.cpp
+++ Ahao.LeetCodeCPP/139. 单词拆分.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 /*
 139. 单词拆分
+
 给你一个字符串 s 和一个字符串列表 wordDict 作为字典。请你判断是否可以利用字典中出现的单词拼接出 s 。
 
 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
@@ -32,6 +33,32 @@ public:
 					break;
 				}
 			}
+		}
+		return f[n];
+	}
+};
+
+
+
+
+
+class Solution {
+public:
+	bool wordBreak(string s, vector<string>& wordDict) {
+		unordered_set<string> st(wordDict.begin(), wordDict.end());
+
+		int n = s.length();
+		s = ' ' + s;
+		vector<bool> f(n + 1);
+		f[0] = true;
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = i; j > 0; j--)
+				if (f[j - 1] && st.count(s.substr(j, i - j + 1)))
+				{
+					f[i] = true;
+					break;
+				}
 		}
 		return f[n];
 	}
