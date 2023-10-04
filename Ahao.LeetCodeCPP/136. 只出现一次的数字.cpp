168a9535e46260b04a034e6cc8c1d3f53fb18130
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-04 20:44:04 +0800

    136. 只出现一次的数字

--- Ahao.LeetCodeCPP/136. 只出现一次的数字.cpp
+++ Ahao.LeetCodeCPP/136. 只出现一次的数字.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 /*
 136. 只出现一次的数字
+
 给定一个非空整数数组，除了某个元素只出现一次以外，
 其余每个元素均出现两次。找出那个只出现了一次的元素。
 */
@@ -15,6 +16,17 @@ public:
 		int res = 0;
 		for (auto x : nums)
 			res ^= x;
+		return res;
+	}
+};
+
+
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		int res = 0;
+		for (auto& num : nums)
+			res ^= num;
 		return res;
 	}
 };
