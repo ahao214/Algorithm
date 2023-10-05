db67b3a3d71098111ec95fd7eafb9f5790a68240
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-05 17:35:51 +0800

    309. 买卖股票的最佳时机含冷冻期

--- Ahao.LeetCode/0300-0399/demo307 - 复制/Class309.cs
+++ Ahao.LeetCode/0300-0399/demo307 - 复制/Class309.cs
@@ -35,5 +35,36 @@ namespace Ahao.LeetCode._0300_0399.demo309
             return Math.Max(f[n - 1][1], f[n - 1][2]);
         }
 
+
+
+
+
+
+        public int maxProfit2(int[] prices)
+        {
+            if (prices.Length == 0)
+            {
+                return 0;
+            }
+
+            int n = prices.Length;
+            int f0 = -prices[0];
+            int f1 = 0;
+            int f2 = 0;
+            for (int i = 1; i < n; ++i)
+            {
+                int newf0 = Math.Max(f0, f2 - prices[i]);
+                int newf1 = f0 + prices[i];
+                int newf2 = Math.Max(f1, f2);
+                f0 = newf0;
+                f1 = newf1;
+                f2 = newf2;
+            }
+
+            return Math.Max(f1, f2);
+        }
+
+
+
     }
 }
