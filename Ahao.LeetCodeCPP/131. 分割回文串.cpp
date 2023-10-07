12441e6e14b2c89f9c847e6bdadabf2b3fd318ab
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-07 16:01:48 +0800

    131. 分割回文串

--- Ahao.LeetCodeCPP/131. 分割回文串.cpp
+++ Ahao.LeetCodeCPP/131. 分割回文串.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 /*
 131. 分割回文串
+
 给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是 回文串 。返回 s 所有可能的分割方案。
 
 回文串 是正着读和反着读都一样的字符串。
@@ -51,5 +52,46 @@ public:
 			path.pop_back();
 		}
 		dfs(now + s[u], u + 1, s);
+	}
+};
+
+
+
+class Solution {
+	int n;
+	vector<vector<bool>> f;
+	vector<vector<string>> ans;
+	vector<string> path;
+public:
+	vector<vector<string>> partition(string s) {
+		n = s.length();
+		f = vector<vector<bool>>(n, vector<bool>(n));
+		for (int j = 0; j < n; j++)
+		{
+			for (int i = 0; i <= j; i++)
+			{
+				if (i == j) f[i][j] = true;
+				else
+					f[i][j] = (s[i] == s[j]) && (i + 1 > j - 1 || f[i + 1][j - 1]);
+			}
+		}
+		dfs(s, 0);
+		return ans;
+	}
+
+	void dfs(string& s, int idx)
+	{
+		if (idx == n)
+		{
+			ans.push_back(path);
+			return;
+		}
+		for (int i = idx; i < n; i++)
+		{
+			if (!f[idx][i]) continue;
+			path.push_back(s.substr(idx, i - idx + 1));
+			dfs(s, i + 1);
+			path.pop_back();
+		}
 	}
 };
