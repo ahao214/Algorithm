0e714dcebb6851121150d24452759f0abfb5210c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-09 00:06:07 +0800

    2578. 最小和分割

---
+++ Ahao.LeetCode/2500-2599/demo2578/Class2578.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2500_2599.demo2578
+{
+    internal class Class2578
+    {
+
+        public int SplitNum(int num)
+        {
+            char[] stnum = num.ToString().ToCharArray();
+            Array.Sort(stnum);
+            int num1 = 0, num2 = 0;
+            for (int i = 0; i < stnum.Length; ++i)
+            {
+                if (i % 2 == 0)
+                {
+                    num1 = num1 * 10 + (stnum[i] - '0');
+                }
+                else
+                {
+                    num2 = num2 * 10 + (stnum[i] - '0');
+                }
+            }
+            return num1 + num2;
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/2500-2599/demo2578/JS2578.js
@@ -0,0 +1,8 @@
+﻿var splitNum = function (num) {
+    const stnum = [...String(num)].map(Number).sort((a, b) => a - b);
+    let num1 = 0, num2 = 0;
+    stnum.forEach((val, i) => {
+        i % 2 == 0 ? num1 = num1 * 10 + val : num2 = num2 * 10 + val;
+    });
+    return num1 + num2;
+};
