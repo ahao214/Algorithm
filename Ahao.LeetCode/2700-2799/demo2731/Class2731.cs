497e7e868ae4bd485f37a714258dacb5ad00abf1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-10 00:15:09 +0800

    2731. 移动机器人

---
+++ Ahao.LeetCode/2700-2799/demo2731/Class2731.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2700_2799.demo2731
+{
+    internal class Class2731
+    {
+    }
+}
