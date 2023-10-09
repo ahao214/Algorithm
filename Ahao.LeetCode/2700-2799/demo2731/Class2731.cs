3f14a0d0f6df6aa843b7f42d3e8d0709a3456b07
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-10 00:15:33 +0800

    2731. 移动机器人

--- Ahao.LeetCode/2700-2799/demo2731/Class2731.cs
+++ Ahao.LeetCode/2700-2799/demo2731/Class2731.cs
@@ -8,5 +8,34 @@ namespace Ahao.LeetCode._2700_2799.demo2731
 {
     internal class Class2731
     {
+
+        const int MOD = 1000000007;
+
+        public int SumDistance(int[] nums, string s, int d)
+        {
+            int n = nums.Length;
+            long[] pos = new long[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] == 'L')
+                {
+                    pos[i] = (long)nums[i] - d;
+                }
+                else
+                {
+                    pos[i] = (long)nums[i] + d;
+                }
+            }
+            Array.Sort(pos);
+            long res = 0;
+            for (int i = 1; i < n; i++)
+            {
+                res += 1L * (pos[i] - pos[i - 1]) * i % MOD * (n - i) % MOD;
+                res %= MOD;
+            }
+            return (int)res;
+        }
+
+
     }
 }
