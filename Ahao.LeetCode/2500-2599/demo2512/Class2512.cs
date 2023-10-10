1e02dcb1bdad08d75fa452530c916e0a068f2e94
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-11 00:18:50 +0800

    2512. 奖励最顶尖的 K 名学生

--- Ahao.LeetCode/2500-2599/demo2512/Class2512.cs
+++ Ahao.LeetCode/2500-2599/demo2512/Class2512.cs
@@ -40,5 +40,36 @@ namespace Ahao.LeetCode._2500_2599.demo2512
             return topK;
         }
 
+
+
+
+        public IList<int> TopStudents2(string[] positive_feedback, string[] negative_feedback, string[] report, int[] student_id, int k)
+        {
+            var rp = new PriorityQueue<int, int[]>(
+                Comparer<int[]>.Create((a, b) => a[1] == b[1] ? a[0] - b[0] : b[1] - a[1])
+            );
+            var map = new Dictionary<string, int>();
+            foreach (var s in positive_feedback) { if (!map.ContainsKey(s)) map.Add(s, 3); }
+            foreach (var s in negative_feedback) { if (!map.ContainsKey(s)) map.Add(s, -1); }
+            int n = student_id.Length, score = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                score = 0;
+                var tmp = report[i].Split(' ');
+                foreach (var s in tmp)
+                {
+                    if (map.ContainsKey(s)) score += map[s];
+                }
+                rp.Enqueue(student_id[i], new int[] { student_id[i], score });
+            }
+            var list = new List<int>();
+            for (int i = 0; i < k; i++)
+            {
+                list.Add(rp.Dequeue());
+            }
+            return list;
+        }
+
+
     }
 }
