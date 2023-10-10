400cf3da9593ffae1c51111b5af3f59e6d528d42
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-11 00:10:10 +0800

    2512. 奖励最顶尖的 K 名学生

---
+++ Ahao.LeetCode/2500-2599/demo2512/Class2512.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2500_2599.demo2512
+{
+    public class Class2512
+    {
+        
+    }
+}
