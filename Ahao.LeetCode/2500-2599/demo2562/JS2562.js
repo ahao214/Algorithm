5d6024f4e1f8105e05a3dd264035c0051e146255
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-12 00:15:15 +0800

    2562. 找出数组的串联值

--- Ahao.LeetCode/2500-2599/demo2562/JS2562.js
+++ Ahao.LeetCode/2500-2599/demo2562/JS2562.js
@@ -1,1 +1,11 @@
-﻿
+﻿var findTheArrayConcVal = function (nums) {
+    let ans = 0;
+    for (let i = 0, j = nums.length - 1; i <= j; i++, j--) {
+        if (i < j) {
+            ans += parseInt(nums[i].toString() + nums[j].toString());
+        } else {
+            ans += nums[i];
+        }
+    }
+    return ans
+};
