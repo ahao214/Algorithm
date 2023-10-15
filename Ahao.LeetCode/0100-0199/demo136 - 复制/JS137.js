53dadbdeeeafcd58020bb03fe3f2c79ce9eb3a55
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-15 19:38:24 +0800

    137. 只出现一次的数字 II

--- Ahao.LeetCode/0100-0199/demo136 - 复制/JS137.js
+++ Ahao.LeetCode/0100-0199/demo136 - 复制/JS137.js
@@ -30,3 +30,24 @@ var singleNumber = function (nums) {
 };
 
 
+var singleNumber = function (nums) {
+    let a = 0, b = 0;
+    for (const num of nums) {
+        const aNext = (~a & b & num) | (a & ~b & ~num), bNext = ~a & (b ^ num);
+        a = aNext;
+        b = bNext;
+    }
+    return b;
+};
+
+
+
+
+var singleNumber = function (nums) {
+    let a = 0, b = 0;
+    for (const num of nums) {
+        b = ~a & (b ^ num);
+        a = ~b & (a ^ num);
+    }
+    return b;
+};
