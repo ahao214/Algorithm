27f5ac1af54a4cb52f0a1bc9836e88863bb7ddc1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-15 19:41:41 +0800

    137. 只出现一次的数字 II

--- Ahao.LeetCodeCPP/137. 只出现一次的数字 II.cpp
+++ Ahao.LeetCodeCPP/137. 只出现一次的数字 II.cpp
@@ -63,3 +63,27 @@ public:
 	}
 };
 
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		int a = 0, b = 0;
+		for (int num : nums) {
+			b = ~a & (b ^ num);
+			a = ~b & (a ^ num);
+		}
+		return b;
+	}
+};
+
+
+
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		int a = 0, b = 0;
+		for (int num : nums) {
+			tie(a, b) = pair{ (~a & b & num) | (a & ~b & ~num), ~a & (b ^ num) };
+		}
+		return b;
+	}
+};
