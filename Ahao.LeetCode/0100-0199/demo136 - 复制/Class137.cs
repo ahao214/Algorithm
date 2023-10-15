7dfe4ce0c5c50afb7f3641fc6cd268b9ba81f8fe
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-15 19:39:44 +0800

    137. 只出现一次的数字 II

--- Ahao.LeetCode/0100-0199/demo136 - 复制/Class137.cs
+++ Ahao.LeetCode/0100-0199/demo136 - 复制/Class137.cs
@@ -9,7 +9,28 @@ namespace Ahao.LeetCode._0100_0199.demo137
 
     public class Class137
     {
-        
+
+
+        public int SingleNumber(int[] nums)
+        {
+            int ans = 0;
+            for (int i = 0; i < 32; ++i)
+            {
+                int total = 0;
+
+                foreach (var num in nums)
+                {
+                    total += ((num >> i) & 1);
+                }
+                if (total % 3 != 0)
+                {
+                    ans |= (1 << i);
+                }
+            }
+            return ans;
+        }
+
+
 
     }
 }
