14d4d4d49d2a98b4d281eaa9fa41b6d184054357
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-16 18:06:12 +0800

    260. 只出现一次的数字 III

---
+++ Ahao.LeetCode/0200-0299/demo260/Class210.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0200_0299.demo260
+{
+    internal class Class260
+    {
+
+        public int[] SingleNumber(int[] nums)
+        {
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (freq.ContainsKey(num))
+                {
+                    ++freq[num];
+                }
+                else
+                {
+                    freq.Add(num, 1);
+                }
+            }
+            int[] ans = new int[2];
+            int index = 0;
+            foreach (KeyValuePair<int, int> pair in freq)
+            {
+                if (pair.Value == 1)
+                {
+                    ans[index++] = pair.Key;
+                }
+            }
+            return ans;
+        }
+
+
+        public int[] SingleNumber2(int[] nums)
+        {
+            int xorsum = 0;
+            foreach (int num in nums)
+            {
+                xorsum ^= num;
+            }
+            // 防止溢出
+            int lsb = (xorsum == int.MinValue ? xorsum : xorsum & (-xorsum));
+            int type1 = 0, type2 = 0;
+            foreach (int num in nums)
+            {
+                if ((num & lsb) != 0)
+                {
+                    type1 ^= num;
+                }
+                else
+                {
+                    type2 ^= num;
+                }
+            }
+            return new int[] { type1, type2 };
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/0200-0299/demo260/TS260.ts
@@ -0,0 +1,13 @@
+﻿function singleNumber(nums: number[]): number[] {
+    const freq = new Map();
+    for (const num of nums) {
+        freq.set(num, (freq.get(num) || 0) + 1);
+    }
+    const ans: number[] = [];
+    for (const [num, occ] of freq.entries()) {
+        if (occ === 1) {
+            ans.push(num);
+        }
+    }
+    return ans;
+};
