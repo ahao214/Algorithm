141655adbf0fb43c00f5c3f0a8045629128357f6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-17 08:42:37 +0800

    2652. 倍数求和

---
+++ Ahao.LeetCode/2600-2699/demo2652/Class2651.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2600_2699.demo2652
+{
+    internal class Class2652
+    {
+
+        public int SumOfMultiples(int n)
+        {
+            int result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+
+
+
+
+        public int f(int n, int m)
+        {
+            return (m + n / m * m) * (n / m) / 2;
+        }
+        public int SumOfMultiples2(int n)
+        {
+            return f(n, 3) + f(n, 5) + f(n, 7) - f(n, 3 * 5) - f(n, 3 * 7) - f(n, 5 * 7) + f(n, 3 * 5 * 7);
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/2600-2699/demo2652/JS2652.js
@@ -0,0 +1,19 @@
+﻿var sumOfMultiples = function (n) {
+    let res = 0;
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+            res += i;
+        }
+    }
+    return res;
+};
+
+
+
+var f = function (n, m) {
+    return (m + Math.floor(n / m) * m) * Math.floor(n / m) / 2;
+}
+var sumOfMultiples = function (n) {
+    return f(n, 3) + f(n, 5) + f(n, 7) - f(n, 3 * 5) - f(n, 3 * 7) - f(n, 5 * 7) + f(n, 3 * 5 * 7);
+};
+
