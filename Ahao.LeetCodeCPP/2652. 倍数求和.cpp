c7132c9a084c967d41de61271fa0af6a05b60ad2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-17 08:39:04 +0800

    2652. 倍数求和

---
+++ Ahao.LeetCodeCPP/2652. 倍数求和.cpp
@@ -0,0 +1,40 @@
+#include<vector>
+
+using namespace std;
+
+
+/*
+2652. 倍数求和
+
+给你一个正整数 n ，请你计算在 [1，n] 范围内能被 3、5、7 整除的所有整数之和。
+
+返回一个整数，用于表示给定范围内所有满足约束条件的数字之和。
+
+*/
+
+
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                res += i;
+            }
+        }
+        return res;
+    }
+};
+
+
+
+class Solution {
+public:
+    int f(int n, int m) {
+        return (m + n / m * m) * (n / m) / 2;
+    }
+
+    int sumOfMultiples(int n) {
+        return f(n, 3) + f(n, 5) + f(n, 7) - f(n, 3 * 5) - f(n, 3 * 7) - f(n, 5 * 7) + f(n, 3 * 5 * 7);
+    }
+};
