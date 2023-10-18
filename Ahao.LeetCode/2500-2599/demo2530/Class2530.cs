2f99a55791c086318e92d848a84f4a86f59083bb
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-18 15:39:17 +0800

    2530. 执行 K 次操作后的最大分数

---
+++ Ahao.LeetCode/2500-2599/demo2530/Class2530.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ahao.LeetCode._2500_2599.demo2530
+{
+    public class Class2530
+    {
+
+        public long MaxKelements(int[] nums, int k)
+        {
+            long res = 0;
+            int count = 0, n = nums.Length;
+            while (count < k)
+            {
+                Array.Sort(nums);
+                int i = n - 1, min = (nums[n - 1] + 2) / 3;
+                while (i >= 0 && count < k && nums[i] >= min)
+                {
+                    res += nums[i--];
+                    count++;
+                }
+                if (count == k) break;
+                while (++i < n) nums[i] = (nums[i] + 2) / 3;
+            }
+            return res;
+        }
+
+
+    }
+}
