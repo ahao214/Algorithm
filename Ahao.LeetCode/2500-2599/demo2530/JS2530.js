c501391ee57af607b38a7ca07655082d43a39522
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-18 15:39:51 +0800

    2530. 执行 K 次操作后的最大分数

--- Ahao.LeetCode/2500-2599/demo2530/JS2530.js
+++ Ahao.LeetCode/2500-2599/demo2530/JS2530.js
@@ -1,1 +1,13 @@
-﻿
+﻿var maxKelements = function (nums, k) {
+    q = new MaxPriorityQueue();
+    let ans = 0;
+    for (const num of nums) {
+        q.enqueue(num);
+    }
+    for (let i = 0; i < k; i++) {
+        const x = q.dequeue().element;
+        ans += x;
+        q.enqueue(Math.ceil(x / 3));
+    }
+    return ans;
+};
