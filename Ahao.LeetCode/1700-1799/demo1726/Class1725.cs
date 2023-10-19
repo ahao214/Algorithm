c1ab53e169adc43b52646b7c523ea32c12bb8714
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-19 08:01:51 +0800

    1726. 同积元组

--- Ahao.LeetCode/1700-1799/demo1726/Class1725.cs
+++ Ahao.LeetCode/1700-1799/demo1726/Class1725.cs
@@ -8,7 +8,28 @@ namespace Ahao.LeetCode.Simple
 {
     public class Class1726
     {
-        
+        public int TupleSameProduct(int[] nums)
+        {
+            int n = nums.Length;
+            IDictionary<int, int> cnt = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    int key = nums[i] * nums[j];
+                    cnt.TryAdd(key, 0);
+                    cnt[key]++;
+                }
+            }
+            int ans = 0;
+            foreach (KeyValuePair<int, int> pair in cnt)
+            {
+                int v = pair.Value;
+                ans += v * (v - 1) * 4;
+            }
+            return ans;
+        }
+
 
     }
 }
