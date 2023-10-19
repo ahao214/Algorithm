d98f0867ea1c607bf3f6aa48fa08aec949731ae4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-19 08:01:12 +0800

    1726. 同积元组

---
+++ Ahao.LeetCode/1700-1799/demo1726/Class1725.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple
+{
+    public class Class1726
+    {
+        
+
+    }
+}
