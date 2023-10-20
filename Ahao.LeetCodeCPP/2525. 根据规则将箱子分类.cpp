f1172424b6f785977e6f9e0a6b0c691636aa644c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-20 14:40:31 +0800

    2525. 根据规则将箱子分类

---
+++ Ahao.LeetCodeCPP/2525. 根据规则将箱子分类.cpp
@@ -0,0 +1,43 @@
+#include<vector>
+#include<string>
+
+using namespace std;
+
+
+/*
+2525. 根据规则将箱子分类
+
+给你四个整数 length ，width ，height 和 mass ，分别表示一个箱子的三个维度和质量，请你返回一个表示箱子 类别 的字符串。
+
+如果满足以下条件，那么箱子是 "Bulky" 的：
+箱子 至少有一个 维度大于等于 104 。
+或者箱子的 体积 大于等于 109 。
+如果箱子的质量大于等于 100 ，那么箱子是 "Heavy" 的。
+如果箱子同时是 "Bulky" 和 "Heavy" ，那么返回类别为 "Both" 。
+如果箱子既不是 "Bulky" ，也不是 "Heavy" ，那么返回类别为 "Neither" 。
+如果箱子是 "Bulky" 但不是 "Heavy" ，那么返回类别为 "Bulky" 。
+如果箱子是 "Heavy" 但不是 "Bulky" ，那么返回类别为 "Heavy" 。
+注意，箱子的体积等于箱子的长度、宽度和高度的乘积。
+*/
+
+
+
+class Solution {
+public:
+    string categorizeBox(int length, int width, int height, int mass) {
+        long long maxd = max(length, max(width, height)), vol = 1L * length * width * height;
+        bool isBulky = maxd >= 10000 || vol >= 1000000000, isHeavy = mass >= 100;
+        if (isBulky && isHeavy) {
+            return "Both";
+        }
+        else if (isBulky) {
+            return "Bulky";
+        }
+        else if (isHeavy) {
+            return "Heavy";
+        }
+        else {
+            return "Neither";
+        }
+    }
+};
