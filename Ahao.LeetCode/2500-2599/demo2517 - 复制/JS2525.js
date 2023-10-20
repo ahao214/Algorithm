028369dcc1ab7622822ae9740b6d4995d2c125f0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-20 14:39:09 +0800

    2525. 根据规则将箱子分类

--- Ahao.LeetCode/2500-2599/demo2517 - 复制/JS2525.js
+++ Ahao.LeetCode/2500-2599/demo2517 - 复制/JS2525.js
@@ -1,1 +1,15 @@
-﻿
+﻿var categorizeBox = function (length, width, height, mass) {
+    const maxd = Math.max(length, width, height);
+    const vol = length * width * height;
+    const isBulky = maxd >= 10000 || vol >= 10 ** 9;
+    const isHeavy = mass >= 100;
+    if (isBulky && isHeavy) {
+        return 'Both';
+    } else if (isBulky) {
+        return 'Bulky';
+    } else if (isHeavy) {
+        return 'Heavy';
+    } else {
+        return 'Neither';
+    }
+};
