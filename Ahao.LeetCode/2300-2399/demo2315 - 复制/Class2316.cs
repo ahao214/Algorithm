2a8a33e25761136afae7aebbffc6b5d0118b8e73
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-21 00:22:17 +0800

    2316. 统计无向图中无法互相到达点对数

--- Ahao.LeetCode/2300-2399/demo2315 - 复制/Class2316.cs
+++ Ahao.LeetCode/2300-2399/demo2315 - 复制/Class2316.cs
@@ -79,5 +79,154 @@ namespace Ahao.LeetCode._2300_2399.demo2316
             }
 
         }
+
+
+
+
+        Dictionary<int, List<int>> m;
+        bool[] visited;
+        int count;
+        public long CountPairs2(int n, int[][] edges)
+        {
+            // vector<int> groups;
+            visited = new bool[n];
+            m = new Dictionary<int, List<int>>();
+            foreach (var v in edges)
+            {
+                if (!m.ContainsKey(v[0]))
+                {
+                    m.Add(v[0], new List<int>() { v[1] });
+                }
+                else
+                {
+                    m[v[0]].Add(v[1]);
+                }
+                if (!m.ContainsKey(v[1]))
+                {
+                    m.Add(v[1], new List<int>() { v[0] });
+                }
+                else
+                {
+                    m[v[1]].Add(v[0]);
+                }
+            }
+            long ret = 0;
+            int counted = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                if (visited[i])
+                {
+                    continue;
+                }
+                count = 0;
+
+                // dfs(i);
+                var q = new Queue<int>();
+                q.Enqueue(i);
+                visited[i] = true;
+                while (q.Count > 0)
+                {
+                    var point = q.Dequeue();
+                    ++count;
+                    if (!m.ContainsKey(point))
+                    {
+                        continue;
+                    }
+                    foreach (var p in m[point])
+                    {
+                        if (visited[p])
+                        {
+                            continue;
+                        }
+                        q.Enqueue(p);
+                        visited[p] = true;
+                    }
+                }
+
+                counted += count;
+                ret += 1L * count * (n - counted);
+            }
+
+            return ret;
+        }
+
+        void dfs(int point)
+        {
+            if (!m.ContainsKey(point))
+            {
+                return;
+            }
+            foreach (var p in m[point])
+            {
+                if (visited[p])
+                {
+                    continue;
+                }
+                ++count;
+                visited[p] = true;
+                dfs(p);
+            }
+        }
+
+
+
+
+
+
+        static int n = 100010;
+        int[] p = new int[n];
+        int[] size = new int[n];
+
+        int find(int x)
+        {
+            if (p[x] != x)
+            {
+                p[x] = find(p[x]);
+            }
+            return p[x];
+        }
+
+        void union(int a, int b)
+        {
+            int fa = find(a), fb = find(b);
+            if (fa != fb)
+            {
+                p[fa] = fb;
+                size[fb] += size[fa];
+            }
+        }
+
+        public long CountPairs3(int n, int[][] edges)
+        {
+            long res = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                p[i] = i;
+                size[i] = 1;
+            }
+
+            foreach (var edge in edges)
+            {
+                union(edge[0], edge[1]);
+            }
+            List<int> list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (p[i] == i)
+                {
+                    list.Add(size[i]);
+                }
+            }
+            int m = list.Count;
+            for (int i = 0; i < m - 1; i++)
+            {
+                n -= list[i];
+                res += (long)list[i] * n;
+
+            }
+
+            return res;
+        }
     }
 }
