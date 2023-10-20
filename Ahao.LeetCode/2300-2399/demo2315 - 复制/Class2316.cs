4693c67e262edbe106fde2d4fccf9b31220b70ea
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-21 00:02:44 +0800

    2316. 统计无向图中无法互相到达点对数

---
+++ Ahao.LeetCode/2300-2399/demo2315 - 复制/Class2316.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2316
+{
+    public class Class2316
+    {
+
+    }
+}
