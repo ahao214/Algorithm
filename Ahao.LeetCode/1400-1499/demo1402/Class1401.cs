992647b1ec227fd873b9943a141110d4928a0c38
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-22 00:12:06 +0800

    1402. 做菜顺序

--- Ahao.LeetCode/1400-1499/demo1402/Class1401.cs
+++ Ahao.LeetCode/1400-1499/demo1402/Class1401.cs
@@ -8,10 +8,53 @@ namespace Ahao.LeetCode._1400_1499.demo1402
 {
     public class Class1402
     {
+        public int MaxSatisfaction(int[] satisfaction)
+        {
+            int n = satisfaction.Length;
+            int[][] dp = new int[n + 1][];
+            for (int i = 0; i <= n; i++)
+            {
+                dp[i] = new int[n + 1];
+            }
+            Array.Sort(satisfaction);
+            int res = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + satisfaction[i - 1] * j;
+                    if (j < i)
+                    {
+                        dp[i][j] = Math.Max(dp[i - 1][j], dp[i][j]);
+                    }
+                    res = Math.Max(res, dp[i][j]);
+                }
+            }
+            return res;
+        }
 
-        
 
 
+
+
+        public int MaxSatisfaction2(int[] satisfaction)
+        {
+            Array.Sort(satisfaction, (a, b) => b - a);
+            int presum = 0, ans = 0;
+            foreach (int si in satisfaction)
+            {
+                if (presum + si > 0)
+                {
+                    presum += si;
+                    ans += presum;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return ans;
+        }
 
     }
 }
