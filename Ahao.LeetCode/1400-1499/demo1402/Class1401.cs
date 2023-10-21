18112eb5566000b0eee6d9ec7de7f7871057faed
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-22 00:09:59 +0800

    1402. 做菜顺序

---
+++ Ahao.LeetCode/1400-1499/demo1402/Class1401.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1400_1499.demo1402
+{
+    public class Class1402
+    {
+
+        
+
+
+
+    }
+}
---
+++ Ahao.LeetCode/1400-1499/demo1402/JS1402.js
@@ -0,0 +1,33 @@
+﻿var maxSatisfaction = function (satisfaction) {
+    const n = satisfaction.length;
+    const dp = new Array(n + 1).fill(0).map(() => new Array(n + 1).fill(0));
+    satisfaction.sort((a, b) => a - b);
+    let res = 0;
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            dp[i][j] = dp[i - 1][j - 1] + satisfaction[i - 1] * j;
+            if (j < i) {
+                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+            }
+            res = Math.max(res, dp[i][j])
+        }
+    }
+    return res;
+};
+
+
+
+
+var maxSatisfaction = function (satisfaction) {
+    satisfaction.sort((a, b) => b - a);
+    let presum = 0, ans = 0;
+    for (si of satisfaction) {
+        if (presum + si > 0) {
+            presum += si;
+            ans += presum;
+        } else {
+            break;
+        }
+    }
+    return ans;
+};
