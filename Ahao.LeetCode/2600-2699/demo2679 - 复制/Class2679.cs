dbe9cb5a008aeb5a31bec6d99b10a6f43f580f8d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-23 00:05:46 +0800

    2678. 老人的数目

--- Ahao.LeetCode/2600-2699/demo2679 - 复制/Class2679.cs
+++ Ahao.LeetCode/2600-2699/demo2679 - 复制/Class2679.cs
@@ -8,7 +8,19 @@ namespace Ahao.LeetCode._2600_2699.demo2678
 {
     public class Class2678
     {
-
+        public int CountSeniors(string[] details)
+        {
+            int res = 0;
+            foreach (var item in details)
+            {
+                int age = Convert.ToInt32(item.Substring(11, 2));
+                if (age > 60)
+                {
+                    res += 1;
+                }
+            }
+            return res;
+        }
 
 
     }
