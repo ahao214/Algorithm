2195c791477ec021e956327b8db2d077c4a7f08f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-23 00:01:57 +0800

    2678. 老人的数目

---
+++ Ahao.LeetCode/2600-2699/demo2679 - 复制/Class2679.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2600_2699.demo2678
+{
+    public class Class2678
+    {
+
+
+
+    }
+}
