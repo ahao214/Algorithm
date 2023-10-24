c4ca26ece8c3bfdd74ac7e00c9525a936ed37d60
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-25 00:14:25 +0800

    2698. 求一个整数的惩罚数

--- Ahao.LeetCode/2600-2699/demo2698/Class2698.cs
+++ Ahao.LeetCode/2600-2699/demo2698/Class2698.cs
@@ -56,5 +56,43 @@ namespace Ahao.LeetCode._2600_2699.demo2698
             return false;
         }
 
+
+        public int PunishmentNumber2(int n)
+        {
+            int res = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                string s = (i * i).ToString();
+                if (DFS(s, 0, 0, i))
+                {
+                    res += i * i;
+                }
+            }
+            return res;
+        }
+
+        public bool DFS(string s, int pos, int tot, int target)
+        {
+            if (pos == s.Length)
+            {
+                return tot == target;
+            }
+            int sum = 0;
+            for (int i = pos; i < s.Length; i++)
+            {
+                sum = sum * 10 + s[i] - '0';
+                if (sum + tot > target)
+                {
+                    break;
+                }
+                if (DFS(s, i + 1, sum + tot, target))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
     }
 }
