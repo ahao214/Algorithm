7d2d1ba86b453b127705d9d72bdcaac4ee925bf3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-25 00:13:45 +0800

    2698. 求一个整数的惩罚数

--- Ahao.LeetCode/2600-2699/demo2698/JS2698.js
+++ Ahao.LeetCode/2600-2699/demo2698/JS2698.js
@@ -1,1 +1,26 @@
-﻿
+﻿var punishmentNumber = function (n) {
+    const dfs = (s, pos, tot, target) => {
+        if (pos == s.length) {
+            return tot == target;
+        }
+        let sum = 0;
+        for (let i = pos; i < s.length; i++) {
+            sum = sum * 10 + parseInt(s[i]);
+            if (tot + sum > target) {
+                break;
+            }
+            if (dfs(s, i + 1, tot + sum, target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    let res = 0;
+    for (let i = 1; i <= n; i++) {
+        let s = (i * i).toString();
+        if (dfs(s, 0, 0, i)) {
+            res += i * i;
+        }
+    }
+    return res;
+};
