b861d076c11800b36f70e6c9a7de3a029773227a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-25 00:13:28 +0800

    2698. 求一个整数的惩罚数

---
+++ Ahao.LeetCode/2600-2699/demo2698/Class2698.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2600_2699.demo2698
+{
+    public class Class2698
+    {
+        public int PunishmentNumber(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (Check(i))
+                    sum += i * i;
+            }
+            return sum;
+        }
+
+        private bool Check(int n)
+        {
+            string s = (n * n).ToString();
+            int len = s.Length;
+            if (len == 1) return n == int.Parse(s);
+            //例如s=1296时，将s当作1,2,9,6 那么现在我们枚举这三个位置的逗号是否选择即可
+            //我们可以用二进制111分别表示逗号是否选择，1为选择，0为不选
+            //那么如何枚举二进制111之内的所有情况呢，我们枚举值（1<<3-1=7）内的所有非负整数即可
+            int max = 1 << (len - 1);
+            for (int i = 0; i < max; i++)
+            {
+                int sum = 0;
+                int cutLen = 1;
+                int temp = i;
+                for (int j = len - 1; j >= 1; j--)
+                {
+                    int val = temp & 1;
+                    temp = temp >> 1;
+                    if (val == 1)
+                    {
+                        string subStr = s.Substring(j, cutLen);
+                        sum += int.Parse(subStr);
+                        if (sum > n) break;
+                        cutLen = 1;
+                    }
+                    else
+                    {
+                        cutLen++;
+                    }
+                }
+                if (sum > n) continue;
+                sum += int.Parse(s.Substring(0, cutLen));
+                if (sum == n) return true;
+            }
+            return false;
+        }
+
+    }
+}
