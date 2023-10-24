18dbfca0005b1625758ffa8c4225f1826aa5c5f4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-24 08:38:56 +0800

    1155. 掷骰子等于目标和的方法数

--- Ahao.LeetCode/1100-1199/demo1155/Class1155.cs
+++ Ahao.LeetCode/1100-1199/demo1155/Class1155.cs
@@ -32,6 +32,76 @@ namespace Ahao.LeetCode.Simple.demo1155
             return (int)dp[n][target];
         }
 
+        const int MOD = 1000000007;
+
+        public int NumRollsToTarget2(int n, int k, int target)
+        {
+            int[][] f = new int[n + 1][];
+            for (int i = 0; i <= n; ++i)
+            {
+                f[i] = new int[target + 1];
+            }
+            f[0][0] = 1;
+            for (int i = 1; i <= n; ++i)
+            {
+                for (int j = 0; j <= target; ++j)
+                {
+                    for (int x = 1; x <= k; ++x)
+                    {
+                        if (j - x >= 0)
+                        {
+                            f[i][j] = (f[i][j] + f[i - 1][j - x]) % MOD;
+                        }
+                    }
+                }
+            }
+            return f[n][target];
+        }
+
+
+        public int NumRollsToTarget3(int n, int k, int target)
+        {
+            int[] f = new int[target + 1];
+            f[0] = 1;
+            for (int i = 1; i <= n; ++i)
+            {
+                int[] g = new int[target + 1];
+                for (int j = 0; j <= target; ++j)
+                {
+                    for (int x = 1; x <= k; ++x)
+                    {
+                        if (j - x >= 0)
+                        {
+                            g[j] = (g[j] + f[j - x]) % MOD;
+                        }
+                    }
+                }
+                f = g;
+            }
+            return f[target];
+        }
+
+
+        public int NumRollsToTarget4(int n, int k, int target)
+        {
+            int[] f = new int[target + 1];
+            f[0] = 1;
+            for (int i = 1; i <= n; ++i)
+            {
+                for (int j = target; j >= 0; --j)
+                {
+                    f[j] = 0;
+                    for (int x = 1; x <= k; ++x)
+                    {
+                        if (j - x >= 0)
+                        {
+                            f[j] = (f[j] + f[j - x]) % MOD;
+                        }
+                    }
+                }
+            }
+            return f[target];
+        }
 
     }
 }
