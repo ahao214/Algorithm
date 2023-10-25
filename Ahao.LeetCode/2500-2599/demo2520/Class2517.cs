a3a9c52be99c7567752bc7ba484b72d4c3f7667c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-26 00:08:55 +0800

    2520. 统计能整除数字的位数

---
+++ Ahao.LeetCode/2500-2599/demo2520/Class2517.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2500_2599.demo2520
+{
+    public class Class2520
+    {
+        public int CountDigits(int num)
+        {
+            int res = 0;
+            int cur = num;
+            while (num != 0)
+            {
+                int n = num % 10;
+                if (cur % n == 0)
+                {
+                    res++;
+                }
+                num /= 10;
+            }
+            return res;
+
+        }
+
+    }
+}
