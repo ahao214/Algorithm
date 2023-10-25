9be45677065ff58831cb4f1bb27e61c2096d874c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-26 00:10:56 +0800

    2520. 统计能整除数字的位数

--- Ahao.LeetCode/2500-2599/demo2520/Class2517.cs
+++ Ahao.LeetCode/2500-2599/demo2520/Class2517.cs
@@ -25,5 +25,21 @@ namespace Ahao.LeetCode._2500_2599.demo2520
 
         }
 
+        public int CountDigits2(int num)
+        {
+            int t = num;
+            int res = 0;
+            while (t != 0)
+            {
+                if (num % (t % 10) == 0)
+                {
+                    res++;
+                }
+                t /= 10;
+            }
+            return res;
+        }
+
+
     }
 }
