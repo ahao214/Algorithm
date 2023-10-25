f79896a51089da7af4df4dbf2e56bb20fd158c5c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-26 00:09:09 +0800

    2520. 统计能整除数字的位数

--- Ahao.LeetCode/2500-2599/demo2520/JS2520.js
+++ Ahao.LeetCode/2500-2599/demo2520/JS2520.js
@@ -1,1 +1,11 @@
-﻿
+﻿var countDigits = function (num) {
+    let t = num;
+    let res = 0;
+    while (t) {
+        if (num % (t % 10) == 0) {
+            res += 1;
+        }
+        t = Math.floor(t / 10);
+    }
+    return res;
+};
