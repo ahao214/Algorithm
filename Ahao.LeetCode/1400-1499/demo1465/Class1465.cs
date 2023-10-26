583bd73458adb364651ec77125725aab9ef06bd8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-27 00:05:17 +0800

    1465. 切割后面积最大的蛋糕

--- Ahao.LeetCode/1400-1499/demo1465/Class1465.cs
+++ Ahao.LeetCode/1400-1499/demo1465/Class1465.cs
@@ -8,9 +8,24 @@ namespace Ahao.LeetCode._1400_1499.demo1465
 {
     internal class Class1465
     {
-        
 
+        public int MaxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts)
+        {
+            Array.Sort(horizontalCuts);
+            Array.Sort(verticalCuts);
+            return (int)((long)CalMax(horizontalCuts, h) * CalMax(verticalCuts, w) % 1000000007);
+        }
 
+        public int CalMax(int[] arr, int boardr)
+        {
+            int res = 0, pre = 0;
+            foreach (int i in arr)
+            {
+                res = Math.Max(i - pre, res);
+                pre = i;
+            }
+            return Math.Max(res, boardr - pre);
+        }
 
     }
 }
