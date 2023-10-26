2c2609d7da3ce2b7dc099bc44905eaa8a2e7a557
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-27 00:04:54 +0800

    1465. 切割后面积最大的蛋糕

--- Ahao.LeetCodeCPP/1465. 切割后面积最大的蛋糕.cpp
+++ Ahao.LeetCodeCPP/1465. 切割后面积最大的蛋糕.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 /*
 1465. 切割后面积最大的蛋糕
+
 矩形蛋糕的高度为 h 且宽度为 w，给你两个整数数组 horizontalCuts 和 verticalCuts，其中 horizontalCuts[i] 是从矩形蛋糕顶部到第  i 个水平切口的距离，类似地， verticalCuts[j] 是从矩形蛋糕的左侧到第 j 个竖直切口的距离。
 
 请你按数组 horizontalCuts 和 verticalCuts 中提供的水平和竖直位置切割后，请你找出 面积最大 的那份蛋糕，并返回其 面积 。由于答案可能是一个很大的数字，因此需要将结果对 10^9 + 7 取余后返回。
@@ -31,3 +32,23 @@ public:
 		return (long long)x * y % 1000000007;
 	}
 };
+
+
+class Solution {
+public:
+	int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+		int mod = 1e9 + 7;
+		sort(horizontalCuts.begin(), horizontalCuts.end());
+		sort(verticalCuts.begin(), verticalCuts.end());
+		auto calMax = [](vector<int>& arr, int boardr) -> int {
+			int res = 0, pre = 0;
+			for (int i : arr) {
+				res = max(i - pre, res);
+				pre = i;
+			}
+			return max(res, boardr - pre);
+		};
+		return (long long)calMax(horizontalCuts, h) * calMax(verticalCuts, w) % mod;
+	}
+};
+
