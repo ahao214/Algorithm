4d9a796b549b49028e3f6d0f4cb77a80a3dcf15d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-28 00:04:28 +0800

    2558. 从数量最多的堆取走礼物

---
+++ Ahao.LeetCode/2500-2599/demo2558/Class2558.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2500_2599.demo2558
+{
+    public class Class2558
+    {
+
+        
+
+
+    }
+}
---
+++ Ahao.LeetCode/2500-2599/demo2558/JS2558.js
@@ -0,0 +1,16 @@
+﻿var pickGifts = function (gifts, k) {
+    const pq = new MaxPriorityQueue();
+    gifts.map(v => {
+        pq.enqueue(v);
+    });
+    while (k > 0) {
+        let x = pq.dequeue().element;
+        pq.enqueue(Math.floor(Math.sqrt(x)));
+        k--;
+    }
+    let res = 0;
+    while (pq.size() > 0) {
+        res += pq.dequeue().element;
+    }
+    return res;
+};
