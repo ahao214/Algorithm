ca4a2f4b1288d8a7f81067ef4b472eb9bda859a9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-28 00:05:11 +0800

    2558. 从数量最多的堆取走礼物

--- Ahao.LeetCode/2500-2599/demo2558/Class2558.cs
+++ Ahao.LeetCode/2500-2599/demo2558/Class2558.cs
@@ -8,9 +8,44 @@ namespace Ahao.LeetCode._2500_2599.demo2558
 {
     public class Class2558
     {
+        public long PickGifts(int[] gifts, int k)
+        {
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            foreach (int gift in gifts)
+            {
+                pq.Enqueue(gift, -gift);
+            }
+            while (k > 0)
+            {
+                k--;
+                int x = pq.Dequeue();
+                pq.Enqueue((int)Math.Sqrt(x), -(int)Math.Sqrt(x));
+            }
+            long res = 0;
+            while (pq.Count > 0)
+            {
+                res += pq.Dequeue();
+            }
+            return res;
+        }
 
-        
-
+        public long PickGifts2(int[] gifts, int k)
+        {
+            int n = gifts.Length;
+            long res = 0;
+            while (k > 0)
+            {
+                Array.Sort(gifts);
+                k--;
+                int val = Convert.ToInt32(Math.Floor(Math.Sqrt(gifts[n - 1])));
+                gifts[n - 1] = val;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                res += gifts[i];
+            }
+            return res;
+        }
 
     }
 }
