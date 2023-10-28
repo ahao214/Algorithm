ab3d8cc664324969b23228b12e07e792107e0361
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-29 00:08:39 +0800

    274. H 指数

---
+++ Ahao.LeetCode/0200-0299/demo274/Class277.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0200_0299.demo277
+{
+    public class Class277
+    {
+        public int findCelebrity(int n)
+        {
+            int cand = 0;   //谁可能成为明星，谁就是cand
+            for (int i = 0; i < n; i++)
+            {
+                if (knows(cand, i))
+                    cand = i;
+            }
+
+            for (int i = 0; i < cand; i++)
+            {
+                if (knows(cand, i))
+                    return -1;
+            }
+            //是不是所有的人都认识cand
+            for (int i = 0; i < n; i++)
+            {
+                if (!knows(i, cand))
+                    return -1;
+            }
+            return cand;
+        }
+
+        public bool knows(int x, int y)
+        {
+            return true;
+        }
+    }
+}
---
+++ Ahao.LeetCode/0200-0299/demo274/JS274.js
@@ -0,0 +1,58 @@
+﻿var hIndex = function (citations) {
+    citations.sort((a, b) => a - b);
+    let h = 0, i = citations.length - 1;
+    while (i >= 0 && citations[i] > h) {
+        h++;
+        i--;
+    }
+    return h;
+};
+
+
+
+var hIndex = function (citations) {
+    let n = citations.length, tot = 0;
+    const counter = new Array(n + 1).fill(0);
+    for (let i = 0; i < n; i++) {
+        if (citations[i] >= n) {
+            counter[n]++;
+        } else {
+            counter[citations[i]]++;
+        }
+    }
+    for (let i = n; i >= 0; i--) {
+        tot += counter[i];
+        if (tot >= i) {
+            return i;
+        }
+    }
+    return 0;
+};
+
+
+
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function (citations) {
+    let left = 0, right = citations.length
+    while (left < right) {
+        // +1 防止死循环
+        let mid = Math.floor((left + right + 1) / 2)
+        let cnt = 0
+        for (let v of citations) {
+            if (v >= mid) {
+                cnt += 1
+            }
+        }
+        if (cnt >= mid) {
+            // 要找的答案在 [mid,right] 区间内
+            left = mid
+        } else {
+            // 要找的答案在 [0,mid) 区间内
+            right = mid - 1
+        }
+    }
+    return left
+};
