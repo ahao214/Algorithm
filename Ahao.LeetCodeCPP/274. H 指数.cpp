3441c4880e6ca9cab74a35fbc2f5840e681f05ea
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-29 00:06:31 +0800

    274. H 指数

--- Ahao.LeetCodeCPP/274. H 指数.cpp
+++ Ahao.LeetCodeCPP/274. H 指数.cpp
@@ -24,3 +24,27 @@ public:
         return h;
     }
 };
+
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size(), tot = 0;
+        vector<int> counter(n + 1);
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n) {
+                counter[n]++;
+            }
+            else {
+                counter[citations[i]]++;
+            }
+        }
+        for (int i = n; i >= 0; i--) {
+            tot += counter[i];
+            if (tot >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+};
