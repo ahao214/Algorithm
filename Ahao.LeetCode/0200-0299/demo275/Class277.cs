b199549f9f8e54c800f734f7697527f63f4e221e
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-30 00:07:28 +0800

    275. H 指数 II

--- Ahao.LeetCode/0200-0299/demo275/Class277.cs
+++ Ahao.LeetCode/0200-0299/demo275/Class277.cs
@@ -8,6 +8,24 @@ namespace Ahao.LeetCode._0200_0299.demo275
 {
     public class Class275
     {
-        
+        public int HIndex(int[] citations)
+        {
+            int n = citations.Length;
+            int left = 0, right = n - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (citations[mid] >= n - mid)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return n - left;
+        }
+
     }
 }
