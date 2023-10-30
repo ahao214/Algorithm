8ffb0e4712d2294737617a98d3de81690d41d1c2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-31 00:06:44 +0800

    2003. 每棵子树内缺失的最小基因值

---
+++ Ahao.LeetCode/2000-2099/demo2003/Class2000.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode.Simple.demo2003
+{
+    public class Class2003
+    {
+        public int[] SmallestMissingValueSubtree(int[] parents, int[] nums)
+        {
+            int n = parents.Length;
+            IList<int>[] children = new IList<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                children[i] = new List<int>();
+            }
+            for (int i = 1; i < n; i++)
+            {
+                children[parents[i]].Add(i);
+            }
+
+            int[] res = new int[n];
+            Array.Fill(res, 1);
+            ISet<int>[] geneSet = new ISet<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                geneSet[i] = new HashSet<int>();
+            }
+            DFS(0, res, nums, children, geneSet);
+            return res;
+        }
+
+        public int DFS(int node, int[] res, int[] nums, IList<int>[] children, ISet<int>[] geneSet)
+        {
+            geneSet[node].Add(nums[node]);
+            foreach (int child in children[node])
+            {
+                res[node] = Math.Max(res[node], DFS(child, res, nums, children, geneSet));
+                if (geneSet[node].Count < geneSet[child].Count)
+                {
+                    ISet<int> temp = geneSet[node];
+                    geneSet[node] = geneSet[child];
+                    geneSet[child] = temp;
+                }
+                foreach (int val in geneSet[child])
+                {
+                    geneSet[node].Add(val);
+                }
+            }
+            while (geneSet[node].Contains(res[node]))
+            {
+                res[node]++;
+            }
+            return res[node];
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/2000-2099/demo2003/JS2003.js
@@ -0,0 +1,72 @@
+﻿var smallestMissingValueSubtree = function (parents, nums) {
+    const n = parents.length;
+    const children = new Array(n).fill(0).map(() => new Array());
+    for (let i = 1; i < n; i++) {
+        children[parents[i]].push(i);
+    }
+
+    const res = new Array(n).fill(1);
+    const geneSet = new Array(n).fill(0).map(() => new Set());
+    var dfs = function (node) {
+        geneSet[node].add(nums[node]);
+        for (const child of children[node]) {
+            res[node] = Math.max(res[node], dfs(child));
+            if (geneSet[node].length < geneSet[child].length) {
+                const tmp = geneSet[node];
+                geneSet[node] = geneSet[child];
+                geneSet[child] = tmp;
+            }
+            for (const x of geneSet[child]) {
+                geneSet[node].add(x);
+            }
+        }
+        while (geneSet[node].has(res[node])) {
+            res[node]++;
+        }
+        return res[node];
+    }
+    dfs(0);
+    return res;
+};
+
+
+
+
+var smallestMissingValueSubtree = function (parents, nums) {
+    const n = parents.length;
+    const children = new Array(n).fill(0).map(() => new Array());
+    for (let i = 1; i < n; i++) {
+        children[parents[i]].push(i);
+    }
+
+    const geneSet = new Set();
+    const visited = new Array(n).fill(0);
+    const dfs = function (node) {
+        if (visited[node]) {
+            return;
+        }
+        visited[node] = 1;
+        geneSet.add(nums[node]);
+        for (const child of children[node]) {
+            dfs(child);
+        }
+    };
+
+    const res = new Array(n).fill(1);
+    let iNode = 1, node = -1;
+    for (let i = 0; i < n; i++) {
+        if (nums[i] == 1) {
+            node = i;
+            break;
+        }
+    }
+    while (node != -1) {
+        dfs(node);
+        while (geneSet.has(iNode)) {
+            iNode++;
+        }
+        res[node] = iNode;
+        node = parents[node];
+    }
+    return res;
+};
