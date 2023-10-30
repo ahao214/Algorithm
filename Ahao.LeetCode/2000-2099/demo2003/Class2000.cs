901a3e8a71469b9cf9e17a0ebd96e4d6e55a90fd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-10-31 00:07:40 +0800

    2003. 每棵子树内缺失的最小基因值

--- Ahao.LeetCode/2000-2099/demo2003/Class2000.cs
+++ Ahao.LeetCode/2000-2099/demo2003/Class2000.cs
@@ -56,5 +56,62 @@ namespace Ahao.LeetCode.Simple.demo2003
             return res[node];
         }
 
+
+
+        public int[] SmallestMissingValueSubtree1(int[] parents, int[] nums)
+        {
+            int n = parents.Length;
+            IList<int>[] children = new IList<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                children[i] = new List<int>();
+            }
+            for (int i = 1; i < n; i++)
+            {
+                children[parents[i]].Add(i);
+            }
+
+            ISet<int> geneSet = new HashSet<int>();
+            bool[] visited = new bool[n];
+
+            int[] res = new int[n];
+            Array.Fill(res, 1);
+            int iNode = 1, node = -1;
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    node = i;
+                    break;
+                }
+            }
+            while (node != -1)
+            {
+                DFS1(node, nums, children, geneSet, visited);
+                while (geneSet.Contains(iNode))
+                {
+                    iNode++;
+                }
+                res[node] = iNode;
+                node = parents[node];
+            }
+            return res;
+        }
+
+        public void DFS1(int node, int[] nums, IList<int>[] children, ISet<int> geneSet, bool[] visited)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+            visited[node] = true;
+            geneSet.Add(nums[node]);
+            foreach (int child in children[node])
+            {
+                DFS1(child, nums, children, geneSet, visited);
+            }
+        }
+
+
     }
 }
