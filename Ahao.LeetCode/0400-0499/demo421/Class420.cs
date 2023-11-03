d0482aef3266a6a47e0a5df18cd9210fd1842250
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-04 00:12:41 +0800

    421. 数组中两个数的最大异或值

--- Ahao.LeetCode/0400-0499/demo421/Class420.cs
+++ Ahao.LeetCode/0400-0499/demo421/Class420.cs
@@ -55,5 +55,102 @@ namespace Ahao.LeetCode._0400_0499.demo421
         }
 
 
+
+
+        // 字典树的根节点
+        Trie root = new Trie();
+        // 最高位的二进制位编号为 30
+        const int HIGH_BIT2 = 30;
+
+        public int FindMaximumXOR2(int[] nums)
+        {
+            int n = nums.Length;
+            int x = 0;
+            for (int i = 1; i < n; ++i)
+            {
+                // 将 nums[i-1] 放入字典树，此时 nums[0 .. i-1] 都在字典树中
+                Add(nums[i - 1]);
+                // 将 nums[i] 看作 ai，找出最大的 x 更新答案
+                x = Math.Max(x, Check(nums[i]));
+            }
+            return x;
+        }
+
+        public void Add(int num)
+        {
+            Trie cur = root;
+            for (int k = HIGH_BIT; k >= 0; --k)
+            {
+                int bit = (num >> k) & 1;
+                if (bit == 0)
+                {
+                    if (cur.left == null)
+                    {
+                        cur.left = new Trie();
+                    }
+                    cur = cur.left;
+                }
+                else
+                {
+                    if (cur.right == null)
+                    {
+                        cur.right = new Trie();
+                    }
+                    cur = cur.right;
+                }
+            }
+        }
+
+        public int Check(int num)
+        {
+            Trie cur = root;
+            int x = 0;
+            for (int k = HIGH_BIT; k >= 0; --k)
+            {
+                int bit = (num >> k) & 1;
+                if (bit == 0)
+                {
+                    // a_i 的第 k 个二进制位为 0，应当往表示 1 的子节点 right 走
+                    if (cur.right != null)
+                    {
+                        cur = cur.right;
+                        x = x * 2 + 1;
+                    }
+                    else
+                    {
+                        cur = cur.left;
+                        x = x * 2;
+                    }
+                }
+                else
+                {
+                    // a_i 的第 k 个二进制位为 1，应当往表示 0 的子节点 left 走
+                    if (cur.left != null)
+                    {
+                        cur = cur.left;
+                        x = x * 2 + 1;
+                    }
+                    else
+                    {
+                        cur = cur.right;
+                        x = x * 2;
+                    }
+                }
+            }
+            return x;
+        }
+
+
+        class Trie
+        {
+            // 左子树指向表示 0 的子节点
+            public Trie left = null;
+            // 右子树指向表示 1 的子节点
+            public Trie right = null;
+        }
+
+
+
+
     }
 }
