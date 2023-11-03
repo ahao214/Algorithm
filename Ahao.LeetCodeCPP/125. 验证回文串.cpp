5bbd90abbc50d60c353b08bc778b29e8692ef299
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-03 23:27:35 +0800

    125. 验证回文串

--- Ahao.LeetCodeCPP/125. 验证回文串.cpp
+++ Ahao.LeetCodeCPP/125. 验证回文串.cpp
@@ -27,3 +27,18 @@ public:
 	}
 };
 
+
+class Solution {
+public:
+
+	bool isPalindrome(string s) {
+		int left = 0, right = s.length() - 1;
+		while (left < right)
+		{
+			while (left < right && !isalnum(s[left])) left++;
+			while (left < right && !isalnum(s[right])) right--;
+			if (tolower(s[left++]) != tolower(s[right--])) return false;
+		}
+		return true;
+	}
+};
