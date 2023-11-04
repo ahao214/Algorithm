a4407f58411e4d87c94e5689a14dc5e7a94b113b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-05 00:09:41 +0800

    187. 重复的DNA序列

---
+++ Ahao.LeetCode/0100-0199/demo187/Class188.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0100_0199.demo187
+{
+    public class Class187
+    {
+        const int L = 10;
+
+        public IList<string> FindRepeatedDnaSequences(string s)
+        {
+            IList<string> ans = new List<string>();
+            Dictionary<string, int> cnt = new Dictionary<string, int>();
+            int n = s.Length;
+            for (int i = 0; i <= n - L; ++i)
+            {
+                string sub = s.Substring(i, L);
+                if (!cnt.ContainsKey(sub))
+                {
+                    cnt.Add(sub, 1);
+                }
+                else
+                {
+                    ++cnt[sub];
+                }
+                if (cnt[sub] == 2)
+                {
+                    ans.Add(sub);
+                }
+            }
+            return ans;
+        }
+
+
+
+
+
+    }
+}
