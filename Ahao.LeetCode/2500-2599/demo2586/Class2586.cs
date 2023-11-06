662879d49656c3bc942290ff0e59f511fda82250
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-07 00:26:49 +0800

    2586. 统计范围内的元音字符串数

--- Ahao.LeetCode/2500-2599/demo2586/Class2586.cs
+++ Ahao.LeetCode/2500-2599/demo2586/Class2586.cs
@@ -32,5 +32,25 @@ namespace Ahao.LeetCode._2500_2599.demo2586
             return false;
         }
 
+
+
+        public int VowelStrings2(string[] words, int left, int right)
+        {
+            var c = new HashSet<char> { 'a', 'e', 'o', 'i', 'u' };
+            return words[left..(right + 1)].Count(v => c.Contains(v[0]) && c.Contains(v[^1]));
+        }
+
+
+
+        public int VowelStrings3(string[] words, int left, int right)
+        {
+            int ans = 0;
+            for (int i = left; i <= right; i++)
+            {
+                if (("aeiou".Contains(words[i][0])) && ("aeiou".Contains(words[i][words[i].Length - 1]))) ans++;
+            }
+            return ans;
+        }
+
     }
 }
