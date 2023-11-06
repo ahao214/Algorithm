dadc400734c410c18ef34dbd84d559fe9b7efdac
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-07 00:23:48 +0800

    2586. 统计范围内的元音字符串数

--- Ahao.LeetCode/2500-2599/demo2586/JS2586.js
+++ Ahao.LeetCode/2500-2599/demo2586/JS2586.js
@@ -1,1 +1,10 @@
-﻿
+﻿var vowelStrings = function (words, left, right) {
+    let ans = 0;
+    for (let i = left; i <= right; i++) {
+        const s = words[i];
+        if ("aeiou".includes(s[0]) && "aeiou".includes(s[s.length - 1])) {
+            ans++;
+        }
+    }
+    return ans;
+};
