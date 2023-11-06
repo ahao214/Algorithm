19acf1c8b06d0962146f5df4ec4c6a8e9c8b068a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-06 12:40:31 +0800

    318. 最大单词长度乘积

--- Ahao.LeetCode/0300-0399/demo318/Class318.cs
+++ Ahao.LeetCode/0300-0399/demo318/Class318.cs
@@ -9,6 +9,33 @@ namespace Ahao.LeetCode.Hard.demo318
     public class Class318
     {
 
+        public int MaxProduct(string[] words)
+        {
+            int length = words.Length;
+            int[] masks = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                String word = words[i];
+                int wordLength = word.Length;
+                for (int j = 0; j < wordLength; j++)
+                {
+                    masks[i] |= 1 << (word[j] - 'a');
+                }
+            }
+            int maxProd = 0;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if ((masks[i] & masks[j]) == 0)
+                    {
+                        maxProd = Math.Max(maxProd, words[i].Length * words[j].Length);
+                    }
+                }
+            }
+            return maxProd;
+        }
+
 
 
     }
