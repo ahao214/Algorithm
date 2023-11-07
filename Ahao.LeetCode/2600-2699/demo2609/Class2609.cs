46d5ea42844690d5c9047109e1174e9ce2470f3c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-08 00:20:17 +0800

    2609. 最长平衡子字符串

---
+++ Ahao.LeetCode/2600-2699/demo2609/Class2609.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2600_2699.demo2609
+{
+    public class Class2609
+    {
+        public int FindTheLongestBalancedSubstring(string s)
+        {
+            int res = 0;
+            if (string.IsNullOrEmpty(s) || s.Length == 0)
+            {
+                return 0;
+            }
+
+            char[] chars = s.ToCharArray();
+            int countZero = 0;
+            int n = chars.Length;
+            int count = 0;           
+            for (int i = 0; i < n; i++)
+            {
+                if (chars[i] == '0')
+                {
+                    countZero++;
+                    count++;
+                }
+                else
+                {
+                    if (countZero > 0)
+                    {
+                        countZero--;
+                        count++;
+                    }
+                }
+                if (countZero == 0)
+                {
+                    res = Math.Max(res, count);
+                    count = 0;
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Ahao.LeetCode/2600-2699/demo2609/JS2609.js
@@ -0,0 +1,16 @@
+﻿var findTheLongestBalancedSubstring = function (s) {
+    let res = 0;
+    const count = new Array(2).fill(0);
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '1') {
+            count[1]++;
+            res = Math.max(res, 2 * Math.min(count[0], count[1]));
+        } else if (i == 0 || s[i - 1] == '1') {
+            count[0] = 1;
+            count[1] = 0;
+        } else {
+            count[0]++;
+        }
+    }
+    return res;
+};
