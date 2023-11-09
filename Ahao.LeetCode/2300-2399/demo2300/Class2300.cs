a3ff0791e95436b50b0b4f33be7ca18e807f62b2
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-10 00:22:41 +0800

    2300. 咒语和药水的成功对数

--- Ahao.LeetCode/2300-2399/demo2300/Class2300.cs
+++ Ahao.LeetCode/2300-2399/demo2300/Class2300.cs
@@ -75,5 +75,114 @@ namespace Ahao.LeetCode._2300_2399.demo2300
         }
 
 
+
+        public int[] SuccessfulPairs3(int[] spells, int[] potions, long success)
+        {
+            Array.Sort(potions);
+            int[] res = new int[spells.Length];
+            for (int i = 0; i < spells.Length; ++i)
+            {
+                int spell = spells[i];
+                // 计算最小的药水大小
+                long minBase = (success + spell - 1) / spell;
+                res[i] = twoSeach(potions, minBase);
+            }
+            return res;
+        }
+
+        // 查找第一个大于等于 "最小药水" 的下标，并计算满足条件的药水数量
+        private int twoSeach(int[] potions, long target)
+        {
+            int l = 0;
+            int r = potions.Length - 1;
+            while (l < r)
+            {
+                int mid = l + (r - l) / 2;
+                if (potions[mid] >= target)
+                {
+                    r = mid;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            return potions[l] >= target ? potions.Length - l : 0;
+        }
+
+
+
+        public int[] SuccessfulPairs4(int[] spells, int[] potions, long success)
+        {
+            int[] result = new int[spells.Length];
+            Array.Sort(potions);
+            for (int i = 0; i < spells.Length; i++)
+            {
+                int left = 0, right = potions.Length - 1;
+                int power = spells[i];
+                long mul = 0;
+                int ret = potions.Length;
+                while (left <= right)
+                {
+                    int mid = left + (right - left) / 2;
+                    mul = potions[mid] * (long)power;
+                    if (mul >= success)
+                    {
+                        ret = mid;
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                result[i] = potions.Length - ret;
+            }
+            return result;
+        }
+
+
+
+
+        public int[] SuccessfulPairs5(int[] spells, int[] potions, long success)
+        {
+            //排序，为二分作准备
+            Array.Sort(potions);
+            int n = spells.Length;
+            var result = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                //与spells[i]相乘可大于等于success的值
+                var t = (double)success / spells[i];
+
+                var left = 0;
+                var right = potions.Length - 1;
+                var mid = 0;
+                //二分查找大于等于t的最小下标
+                while (left <= right)
+                {
+                    mid = (left + right) / 2;
+                    if (potions[mid] < t)
+                        left = mid + 1;
+                    else if (mid - 1 < 0 || potions[mid] >= t && potions[mid - 1] < t)
+                        break;
+                    else if (potions[mid] >= t)
+                        right = mid;
+
+                    //left超出长度说明没有大于等于t的值，直接赋值给mid跳出循环
+                    if (left > potions.Length - 1)
+                    {
+                        mid = left;
+                        break;
+                    }
+                }
+
+                //mid以及mid右边的数均符合条件
+                result[i] += potions.Length - mid;
+            }
+            return result;
+        }
+
+
     }
 }
