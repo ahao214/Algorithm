67ee437895620e1b1cb6f2d1c8bfb7c2974ea487
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-10 00:08:21 +0800

    2300. 咒语和药水的成功对数

---
+++ Ahao.LeetCode/2300-2399/demo2300/Class2300.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2300_2399.demo2300
+{
+    public class Class2300
+    {
+        public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
+        {
+            int n = spells.Length;
+            int m = potions.Length;
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    if (spells[i] * potions[j] >= success)
+                    {
+                        count++;
+                    }
+                }
+                result[i] = count;
+            }
+            return result;
+        }
+    }
+}
