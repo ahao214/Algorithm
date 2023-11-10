1c57006d8490b6e3eb3eb8cf1c7756774ec21af8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-11 00:20:34 +0800

    765. 情侣牵手

---
+++ Ahao.LeetCode/0700-0799/demo765/Class764.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._0700_0799.demo765
+{
+    public class Class765
+    {
+
+    }
+}
---
+++ Ahao.LeetCode/0700-0799/demo765/JS765.js
@@ -0,0 +1,45 @@
+﻿/**
+ * @param {number[]} row
+ * @return {number}
+ */
+var minSwapsCouples = function (row) {
+    const n = row.length;
+    const tot = n / 2;
+    const f = new Array(tot).fill(0).map((element, index) => index);
+
+    for (let i = 0; i < n; i += 2) {
+        const l = Math.floor(row[i] / 2);
+        const r = Math.floor(row[i + 1] / 2);
+        add(f, l, r);
+    }
+    const map = new Map();
+    for (let i = 0; i < tot; i++) {
+        const fx = getf(f, i);
+        if (map.has(fx)) {
+            map.set(fx, map.get(fx) + 1);
+        } else {
+            map.set(fx, 1)
+        }
+    }
+
+    let ret = 0;
+    for (const [f, sz] of map.entries()) {
+        ret += sz - 1;
+    }
+    return ret;
+};
+
+const getf = (f, x) => {
+    if (f[x] === x) {
+        return x;
+    }
+    const newf = getf(f, f[x]);
+    f[x] = newf;
+    return newf;
+}
+
+const add = (f, x, y) => {
+    const fx = getf(f, x);
+    const fy = getf(f, y);
+    f[fx] = fy;
+}
