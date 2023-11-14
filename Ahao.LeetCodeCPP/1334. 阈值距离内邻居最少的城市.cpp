385355923c6f9cff6de21c473deaa635959ae4a0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-14 09:33:34 +0800

    1334. 阈值距离内邻居最少的城市

---
+++ Ahao.LeetCodeCPP/1334. 阈值距离内邻居最少的城市.cpp
@@ -0,0 +1,46 @@
+#include<vector>
+
+using namespace std;
+
+/*
+1334. 阈值距离内邻居最少的城市
+
+有 n 个城市，按从 0 到 n-1 编号。给你一个边数组 edges，其中 edges[i] = [fromi, toi, weighti] 代表 fromi 和 toi 两个城市之间的双向加权边，距离阈值是一个整数 distanceThreshold。
+
+返回能通过某些路径到达其他城市数目最少、且路径距离 最大 为 distanceThreshold 的城市。如果有多个这样的城市，则返回编号最大的城市。
+
+注意，连接城市 i 和 j 的路径的距离等于沿该路径的所有边的权重之和。
+*/
+
+
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        pair<int, int> ans(INT_MAX / 2, -1);
+        vector<vector<int>> mp(n, vector<int>(n, INT_MAX / 2));
+        for (auto& eg : edges) {
+            int from = eg[0], to = eg[1], weight = eg[2];
+            mp[from][to] = mp[to][from] = weight;
+        }
+        for (int k = 0; k < n; ++k) {
+            mp[k][k] = 0;
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    mp[i][j] = min(mp[i][j], mp[i][k] + mp[k][j]);
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = 0; j < n; ++j) {
+                if (mp[i][j] <= distanceThreshold) {
+                    cnt++;
+                }
+            }
+            if (cnt <= ans.first) {
+                ans = { cnt, i };
+            }
+        }
+        return ans.second;
+    }
+};
