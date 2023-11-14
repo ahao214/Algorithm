b60c9e489de1bf7a8c101cd311e4c07352ec06d9
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-14 09:32:14 +0800

    1334. 阈值距离内邻居最少的城市

---
+++ Ahao.LeetCode/1300-1399/demo1334/JS1334.js
@@ -0,0 +1,28 @@
+﻿var findTheCity = function (n, edges, distanceThreshold) {
+    let ans = [Infinity, -1]
+    const mp = new Array(n).fill(0).map(() => new Array(n).fill(Infinity));
+
+    for (const [from, to, weight] of edges) {
+        mp[from][to] = mp[to][from] = weight;
+    }
+    for (let k = 0; k < n; k++) {
+        mp[k][k] = 0;
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                mp[i][j] = Math.min(mp[i][j], mp[i][k] + mp[k][j]);
+            }
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        let cnt = 0;
+        for (let j = 0; j < n; j++) {
+            if (mp[i][j] <= distanceThreshold) {
+                cnt++;
+            }
+        }
+        if (cnt <= ans[0]) {
+            ans = [cnt, i];
+        }
+    }
+    return ans[1];
+};
