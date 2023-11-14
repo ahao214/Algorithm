6a1a3ad0469d7a584a147cceb42d74045e90c710
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-14 09:32:34 +0800

    1334. 阈值距离内邻居最少的城市

--- Ahao.LeetCode/1300-1399/demo1334/Class1334.cs
+++ Ahao.LeetCode/1300-1399/demo1334/Class1334.cs
@@ -8,8 +8,50 @@ namespace Ahao.LeetCode.Simple.demo1334
 {
     public class Class1334
     {
+        public int FindTheCity(int n, int[][] edges, int distanceThreshold)
+        {
+            int[] ans = { int.MaxValue / 2, -1 };
+            int[][] mp = new int[n][];
+            for (int i = 0; i < n; ++i)
+            {
+                mp[i] = new int[n];
+                Array.Fill(mp[i], int.MaxValue / 2);
+            }
+            foreach (int[] eg in edges)
+            {
+                int from = eg[0], to = eg[1], weight = eg[2];
+                mp[from][to] = mp[to][from] = weight;
+            }
+            for (int k = 0; k < n; ++k)
+            {
+                mp[k][k] = 0;
+                for (int i = 0; i < n; ++i)
+                {
+                    for (int j = 0; j < n; ++j)
+                    {
+                        mp[i][j] = Math.Min(mp[i][j], mp[i][k] + mp[k][j]);
+                    }
+                }
+            }
+            for (int i = 0; i < n; ++i)
+            {
+                int cnt = 0;
+                for (int j = 0; j < n; ++j)
+                {
+                    if (mp[i][j] <= distanceThreshold)
+                    {
+                        cnt++;
+                    }
+                }
+                if (cnt <= ans[0])
+                {
+                    ans[0] = cnt;
+                    ans[1] = i;
+                }
+            }
+            return ans[1];
+        }
 
-      
 
     }
 }
