a278b9af4cef5020db366f1ab6229105019dec52
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-14 09:34:45 +0800

    1334. 阈值距离内邻居最少的城市

--- Ahao.LeetCode/1300-1399/demo1334/Class1334.cs
+++ Ahao.LeetCode/1300-1399/demo1334/Class1334.cs
@@ -53,5 +53,63 @@ namespace Ahao.LeetCode.Simple.demo1334
         }
 
 
+        public int FindTheCity2(int n, int[][] edges, int distanceThreshold)
+        {
+            int[] ans = { int.MaxValue / 2, -1 };
+            int[][] dis = new int[n][];
+            bool[][] vis = new bool[n][];
+            int[][] mp = new int[n][];
+            for (int i = 0; i < n; ++i)
+            {
+                dis[i] = new int[n];
+                vis[i] = new bool[n];
+                mp[i] = new int[n];
+                Array.Fill(dis[i], int.MaxValue / 2);
+                Array.Fill(mp[i], int.MaxValue / 2);
+            }
+            foreach (int[] eg in edges)
+            {
+                int from = eg[0], to = eg[1], weight = eg[2];
+                mp[from][to] = mp[to][from] = weight;
+            }
+            for (int i = 0; i < n; ++i)
+            {
+                dis[i][i] = 0;
+                for (int j = 0; j < n; ++j)
+                {
+                    int t = -1;
+                    for (int k = 0; k < n; ++k)
+                    {
+                        if (!vis[i][k] && (t == -1 || dis[i][k] < dis[i][t]))
+                        {
+                            t = k;
+                        }
+                    }
+                    for (int k = 0; k < n; ++k)
+                    {
+                        dis[i][k] = Math.Min(dis[i][k], dis[i][t] + mp[t][k]);
+                    }
+                    vis[i][t] = true;
+                }
+            }
+            for (int i = 0; i < n; ++i)
+            {
+                int cnt = 0;
+                for (int j = 0; j < n; ++j)
+                {
+                    if (dis[i][j] <= distanceThreshold)
+                    {
+                        cnt++;
+                    }
+                }
+                if (cnt <= ans[0])
+                {
+                    ans[0] = cnt;
+                    ans[1] = i;
+                }
+            }
+            return ans[1];
+        }
+
     }
 }
--- Ahao.LeetCode/1300-1399/demo1334/JS1334.js
+++ Ahao.LeetCode/1300-1399/demo1334/JS1334.js
@@ -26,3 +26,45 @@
     }
     return ans[1];
 };
+
+
+
+
+var findTheCity = function (n, edges, distanceThreshold) {
+    let ans = [Infinity, -1];
+    const dis = new Array(n).fill(0).map(() => new Array(n).fill(Infinity));
+    const mp = new Array(n).fill(0).map(() => new Array(n).fill(Infinity));
+    const vis = new Array(n).fill(0).map(() => new Array(n).fill(false));
+
+    for (const [from, to, weight] of edges) {
+        mp[from][to] = mp[to][from] = weight;
+    }
+    for (let i = 0; i < n; i++) {
+        dis[i][i] = 0;
+        for (let j = 0; j < n; j++) {
+            let t = -1;
+            for (let k = 0; k < n; ++k) {
+                if (!vis[i][k] && (t == -1 || dis[i][k] < dis[i][t])) {
+                    t = k;
+                }
+            }
+            for (let k = 0; k < n; ++k) {
+                dis[i][k] = Math.min(dis[i][k], dis[i][t] + mp[t][k]);
+            }
+            vis[i][t] = true;
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        let cnt = 0;
+        for (let j = 0; j < n; j++) {
+            if (dis[i][j] <= distanceThreshold) {
+                cnt++;
+            }
+        }
+        if (cnt <= ans[0]) {
+            ans = [cnt, i];
+        }
+    }
+    return ans[1];
+};
+
