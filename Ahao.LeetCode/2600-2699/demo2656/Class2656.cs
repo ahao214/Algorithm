b65d2dc281c1b29febae2611c4c0b90e8f31440a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-15 10:20:26 +0800

    2656. K 个元素的最大和

---
+++ Ahao.LeetCode/2600-2699/demo2656/Class2656.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2600_2699.demo2656
+{
+    internal class Class2656
+    {
+        public int MaximizeSum(int[] nums, int k)
+        {
+            int maxValue = nums.Max();
+            return (2 * maxValue + k - 1) * k / 2;
+        }
+
+
+
+    }
+}
