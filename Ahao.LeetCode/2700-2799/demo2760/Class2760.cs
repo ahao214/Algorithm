09813ba417e4b58af78206d86795afcc03acf0bc
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-16 11:18:16 +0800

    2760. 最长奇偶子数组

---
+++ Ahao.LeetCode/2700-2799/demo2760/Class2760.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2700_2799.demo2760
+{
+    public class Class2760
+    {
+        public int LongestAlternatingSubarray(int[] nums, int threshold)
+        {
+            int res = 0, n = nums.Length;
+            for (int l = 0; l < n; l++)
+            {
+                for (int r = l; r < n; r++)
+                {
+                    if (IsSatisfied(nums, l, r, threshold))
+                    {
+                        res = Math.Max(res, r - l + 1);
+                    }
+                }
+            }
+            return res;
+        }
+
+        public bool IsSatisfied(int[] nums, int l, int r, int threshold)
+        {
+            if (nums[l] % 2 != 0)
+            {
+                return false;
+            }
+            for (int i = l; i <= r; i++)
+            {
+                if (nums[i] > threshold || (i < r && nums[i] % 2 == nums[i + 1] % 2))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+
+
+
+    }
+}
---
+++ Ahao.LeetCode/2700-2799/demo2760/JS2760.js
@@ -0,0 +1,23 @@
+﻿var isSatisfied = function (nums, l, r, threshold) {
+    if (nums[l] % 2 != 0) {
+        return false;
+    }
+    for (let i = l; i <= r; i++) {
+        if (nums[i] > threshold || (i < r && nums[i] % 2 == nums[i + 1] % 2)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+var longestAlternatingSubarray = function (nums, threshold) {
+    let res = 0;
+    for (let l = 0; l < nums.length; l++) {
+        for (let r = 0; r < nums.length; r++) {
+            if (isSatisfied(nums, l, r, threshold)) {
+                res = Math.max(res, r - l + 1);
+            }
+        }
+    }
+    return res;
+};
