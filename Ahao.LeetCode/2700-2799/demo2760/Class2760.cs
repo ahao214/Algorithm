e3cf8c2fecf2b27b8102c62ea1452756a3372832
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-16 11:18:50 +0800

    2760. 最长奇偶子数组

--- Ahao.LeetCode/2700-2799/demo2760/Class2760.cs
+++ Ahao.LeetCode/2700-2799/demo2760/Class2760.cs
@@ -42,6 +42,30 @@ namespace Ahao.LeetCode._2700_2799.demo2760
 
 
 
+        public int LongestAlternatingSubarray2(int[] nums, int threshold)
+        {
+            int res = 0, dp = 0;
+            for (int l = nums.Length - 1; l >= 0; l--)
+            {
+                if (nums[l] > threshold)
+                {
+                    dp = 0;
+                }
+                else if (l == nums.Length - 1 || nums[l] % 2 != nums[l + 1] % 2)
+                {
+                    dp++;
+                }
+                else
+                {
+                    dp = 1;
+                }
+                if (nums[l] % 2 == 0)
+                {
+                    res = Math.Max(res, dp);
+                }
+            }
+            return res;
+        }
 
 
     }
--- Ahao.LeetCode/2700-2799/demo2760/JS2760.js
+++ Ahao.LeetCode/2700-2799/demo2760/JS2760.js
@@ -21,3 +21,22 @@ var longestAlternatingSubarray = function (nums, threshold) {
     }
     return res;
 };
+
+
+
+var longestAlternatingSubarray = function (nums, threshold) {
+    let res = 0, dp = 0;
+    for (let l = nums.length - 1; l >= 0; l--) {
+        if (nums[l] > threshold) {
+            dp = 0;
+        } else if (l == nums.length - 1 || nums[l] % 2 != nums[l + 1] % 2) {
+            dp++;
+        } else {
+            dp = 1;
+        }
+        if (nums[l] % 2 == 0) {
+            res = Math.max(res, dp);
+        }
+    }
+    return res;
+};
