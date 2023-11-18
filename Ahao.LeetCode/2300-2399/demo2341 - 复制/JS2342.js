078902eb28a368764c335497746be9e1f546a247
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-18 10:00:42 +0800

    2342. 数位和相等数对的最大和

--- Ahao.LeetCode/2300-2399/demo2341 - 复制/JS2342.js
+++ Ahao.LeetCode/2300-2399/demo2341 - 复制/JS2342.js
@@ -1,1 +1,16 @@
-﻿
+﻿var maximumSum = function (nums) {
+    let ans = -1;
+    const mx = Array(82).fill(0); // 至多 9 个 9 相加
+    for (const num of nums) {
+        let s = 0; // num 的数位和
+        for (let x = num; x; x = Math.floor(x / 10)) { // 枚举 num 的每个数位
+            s += x % 10;
+        }
+        if (mx[s]) { // 说明左边也有数位和等于 s 的元素
+            ans = Math.max(ans, mx[s] + num); // 更新答案的最大值
+        }
+        mx[s] = Math.max(mx[s], num); // 维护数位和等于 s 的最大元素
+    }
+    return ans;
+};
+
