bc9bf8e927ea88caf733d13b43d19f69fd6cc405
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-21 09:24:24 +0800

    2216. 美化数组的最少删除数

---
+++ Ahao.LeetCode/2200-2299/demo2216/JS2216.js
@@ -0,0 +1,14 @@
+﻿var minDeletion = function (nums) {
+    let ans = 0, n = nums.length, check = true;
+    for (let i = 0; i + 1 < n; i++) {
+        if (nums[i] == nums[i + 1] && check) {
+            ans++;
+        } else {
+            check = !check;
+        }
+    }
+    if ((n - ans) % 2 != 0) {
+        ans++;
+    }
+    return ans;
+};
