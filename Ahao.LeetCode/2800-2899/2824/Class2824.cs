8f4f4f2d54e5463399832d074c44b4be277024de
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 00:07:32 +0800

    2824. 统计和小于目标的下标对数目

--- Ahao.LeetCode/2800-2899/2824/Class2824.cs
+++ Ahao.LeetCode/2800-2899/2824/Class2824.cs
@@ -24,6 +24,52 @@ namespace Ahao.LeetCode._2800_2899._2824
         }
 
 
+        public int CountPairs2(IList<int> nums, int target)
+        {
+            ((List<int>)nums).Sort();
+            int res = 0;
+            for (int i = 1; i < nums.Count; i++)
+            {
+                int k = BinarySearch(nums, 0, i - 1, target - nums[i]);
+                res += k;
+            }
+            return res;
+        }
+
+        public int BinarySearch(IList<int> nums, int lo, int hi, int target)
+        {
+            int res = hi + 1;
+            while (lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                if (nums[mid] >= target)
+                {
+                    res = mid;
+                    hi = mid - 1;
+                }
+                else
+                {
+                    lo = mid + 1;
+                }
+            }
+            return res;
+        }
+
+
+        public int CountPairs3(IList<int> nums, int target)
+        {
+            ((List<int>)nums).Sort();
+            int res = 0;
+            for (int i = 0, j = nums.Count - 1; i < j; i++)
+            {
+                while (i < j && nums[i] + nums[j] >= target)
+                {
+                    j--;
+                }
+                res += j - i;
+            }
+            return res;
+        }
 
     }
 }
