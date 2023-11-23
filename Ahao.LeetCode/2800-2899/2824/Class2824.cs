6779a29c442d333ea565e944f0e5d182f2935b15
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-24 00:05:50 +0800

    2824. 统计和小于目标的下标对数目

---
+++ Ahao.LeetCode/2800-2899/2824/Class2824.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2800_2899._2824
+{
+    public class Class2824
+    {
+        public int CountPairs(IList<int> nums, int target)
+        {
+            int res = 0;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                for (int j = i + 1; j < nums.Count; j++)
+                {
+                    if (nums[i] + nums[j] < target)
+                        res++;
+                }
+            }
+            return res;
+        }
+
+
+
+    }
+}
