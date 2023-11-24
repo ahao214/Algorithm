6590ee5ed7b81b51ecfe1f26cc5d05cf50cfc4ca
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-25 00:05:51 +0800

    1457. 二叉树中的伪回文路径

---
+++ Ahao.LeetCode/1400-1499/demo1457/Class1457.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1400_1499.demo1457
+{
+    public class Class1457
+    {
+
+        public int PseudoPalindromicPaths(TreeNode root)
+        {
+            int[] counter = new int[10];
+            return DFS(root, counter);
+        }
+
+        public int DFS(TreeNode root, int[] counter)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            counter[root.val]++;
+            int res = 0;
+            if (root.left == null && root.right == null)
+            {
+                if (IsPseudoPalindrome(counter))
+                {
+                    res = 1;
+                }
+            }
+            else
+            {
+                res = DFS(root.left, counter) + DFS(root.right, counter);
+            }
+            counter[root.val]--;
+            return res;
+        }
+
+        public bool IsPseudoPalindrome(int[] counter)
+        {
+            int odd = 0;
+            foreach (int value in counter)
+            {
+                if (value % 2 == 1)
+                {
+                    odd++;
+                }
+            }
+            return odd <= 1;
+        }
+
+
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1400-1499/demo1457/JS1457.js
@@ -0,0 +1,37 @@
+﻿function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+var pseudoPalindromicPaths = function (root) {
+    let counter = new Array(10).fill(0);
+    return dfs(root, counter);
+};
+
+var dfs = function (root, counter) {
+    if (!root) {
+        return 0;
+    }
+    counter[root.val]++;
+    let res = 0;
+    if (!root.left && !root.right) {
+        if (isPseudoPalindrome(counter)) {
+            res = 1;
+        }
+    } else {
+        res = dfs(root.left, counter) + dfs(root.right, counter);
+    }
+    counter[root.val]--;
+    return res;
+}
+
+var isPseudoPalindrome = function (counter) {
+    let odd = 0;
+    for (value of counter) {
+        if (value % 2 == 1) {
+            odd++;
+        }
+    }
+    return odd <= 1;
+}
