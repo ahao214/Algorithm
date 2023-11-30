8dd065e5e6b24ac84af2e979fa6221c62f4f01f8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-30 11:34:54 +0800

    1657. 确定两个字符串是否接近

---
+++ Ahao.LeetCode/1600-1699/demo1657/Class1657.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1600_1699.demo1657
+{
+    public class Class1657
+    {
+        public bool CloseStrings(string word1, string word2)
+        {
+            int[] count1 = new int[26], count2 = new int[26];
+            foreach (char c in word1)
+            {
+                count1[c - 'a']++;
+            }
+            foreach (char c in word2)
+            {
+                count2[c - 'a']++;
+            }
+            for (int i = 0; i < 26; i++)
+            {
+                if (count1[i] > 0 && count2[i] == 0 || count1[i] == 0 && count2[i] > 0)
+                {
+                    return false;
+                }
+            }
+            Array.Sort(count1);
+            Array.Sort(count2);
+            return Enumerable.SequenceEqual(count1, count2);
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1600-1699/demo1657/JS1657.js
@@ -0,0 +1,17 @@
+﻿var closeStrings = function (word1, word2) {
+    let count1 = new Array(26).fill(0), count2 = new Array(26).fill(0);
+    for (let i = 0; i < word1.length; i++) {
+        count1[word1.charCodeAt(i) - 97]++;
+    }
+    for (let i = 0; i < word2.length; i++) {
+        count2[word2.charCodeAt(i) - 97]++;
+    }
+    for (let i = 0; i < 26; i++) {
+        if (count1[i] > 0 && count2[i] == 0 || count1[i] == 0 && count2[i] > 0) {
+            return false;
+        }
+    }
+    count1.sort();
+    count2.sort();
+    return count1.toString() == count2.toString();
+};
