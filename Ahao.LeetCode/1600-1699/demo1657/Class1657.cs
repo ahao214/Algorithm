cf8025bb1c6d288b6630a78820d97b354f4d2d55
liupenghao@fulan.com.cn <haochen214@163.com>
2023-11-30 11:42:04 +0800

    1657. 确定两个字符串是否接近

--- Ahao.LeetCode/1600-1699/demo1657/Class1657.cs
+++ Ahao.LeetCode/1600-1699/demo1657/Class1657.cs
@@ -10,6 +10,8 @@ namespace Ahao.LeetCode._1600_1699.demo1657
     {
         public bool CloseStrings(string word1, string word2)
         {
+            if (word1.Length != word2.Length)
+                return false;
             int[] count1 = new int[26], count2 = new int[26];
             foreach (char c in word1)
             {
@@ -31,5 +33,65 @@ namespace Ahao.LeetCode._1600_1699.demo1657
             return Enumerable.SequenceEqual(count1, count2);
         }
 
+
+
+        public bool CloseStrings2(string word1, string word2)
+        {
+            if (word1.Length != word2.Length)
+                return false;
+            Dictionary<char, int> dic1 = new Dictionary<char, int>();
+            Dictionary<char, int> dic2 = new Dictionary<char, int>();
+            foreach (var i in word1)
+            {
+                if (dic1.ContainsKey(i))
+                {
+                    dic1[i]++;
+                }
+                else
+                {
+                    dic1.Add(i, 1);
+                }
+            }
+            foreach (var i in word2)
+            {
+                if (dic2.ContainsKey(i))
+                {
+                    dic2[i]++;
+                }
+                else
+                {
+                    dic2.Add(i, 1);
+                }
+            }
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+
+            foreach (var kvp in dic1)
+            {
+                if (!dic2.ContainsKey(kvp.Key))
+                    return false;
+            }
+            foreach (var kvp in dic1)
+            {
+                list1.Add(kvp.Value);
+            }
+
+            foreach (var kvp in dic2)
+            {
+                list2.Add(kvp.Value);
+            }
+
+            list1.Sort();
+            list2.Sort();
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                if (list1[i] != list2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
