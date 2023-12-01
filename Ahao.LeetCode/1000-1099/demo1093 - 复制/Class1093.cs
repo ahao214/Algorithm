e4e5d029dcae4ce2810d42c6855645f07ad30103
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 00:03:49 +0800

    1094. 拼车

--- Ahao.LeetCode/1000-1099/demo1093 - 复制/Class1093.cs
+++ Ahao.LeetCode/1000-1099/demo1093 - 复制/Class1093.cs
@@ -8,10 +8,31 @@ namespace Ahao.LeetCode._1000_1099.demo1094
 {
     public class Class1094
     {
+        public bool CarPooling(int[][] trips, int capacity)
+        {
+            int toMax = 0;
+            foreach (int[] trip in trips)
+            {
+                toMax = Math.Max(toMax, trip[2]);
+            }
 
-       
+            int[] diff = new int[toMax + 1];
+            foreach (int[] trip in trips)
+            {
+                diff[trip[1]] += trip[0];
+                diff[trip[2]] -= trip[0];
+            }
 
-
-
+            int count = 0;
+            for (int i = 0; i <= toMax; ++i)
+            {
+                count += diff[i];
+                if (count > capacity)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
