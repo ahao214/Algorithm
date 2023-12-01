a9d6a02d89b4c84038c42c8c1f5065a5718515c5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-02 00:03:18 +0800

    1094. 拼车

---
+++ Ahao.LeetCode/1000-1099/demo1093 - 复制/Class1093.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1094
+{
+    public class Class1094
+    {
+
+       
+
+
+
+    }
+}
---
+++ Ahao.LeetCode/1000-1099/demo1093 - 复制/JS1094.js
@@ -0,0 +1,21 @@
+﻿var carPooling = function (trips, capacity) {
+    let to_max = 0;
+    for (const trip of trips) {
+        to_max = Math.max(to_max, trip[2]);
+    }
+
+    const diff = new Array(to_max + 1).fill(0);
+    for (const trip of trips) {
+        diff[trip[1]] += trip[0];
+        diff[trip[2]] -= trip[0];
+    }
+
+    let count = 0;
+    for (let i = 0; i <= to_max; ++i) {
+        count += diff[i];
+        if (count > capacity) {
+            return false;
+        }
+    }
+    return true;
+};
