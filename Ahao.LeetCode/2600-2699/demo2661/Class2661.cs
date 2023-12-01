121ecfcd90845f3b6d046fe424295a911f8330c3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-01 10:11:28 +0800

    2661. 找出叠涂元素

---
+++ Ahao.LeetCode/2600-2699/demo2661/Class2661.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2600_2699.demo2661
+{
+    public class Class2661
+    {
+        public int FirstCompleteIndex(int[] arr, int[][] mat)
+        {
+            int n = mat.Length;
+            int m = mat[0].Length;
+            IDictionary<int, Tuple<int, int>> dictionary = new Dictionary<int, Tuple<int, int>>();
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    dictionary.Add(mat[i][j], new Tuple<int, int>(i, j));
+                }
+            }
+            int[] rowCnt = new int[n];
+            int[] colCnt = new int[m];
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                Tuple<int, int> v = dictionary[arr[i]];
+                ++rowCnt[v.Item1];
+                if (rowCnt[v.Item1] == m)
+                {
+                    return i;
+                }
+                ++colCnt[v.Item2];
+                if (colCnt[v.Item2] == n)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Ahao.LeetCode/2600-2699/demo2661/JS2661.js
@@ -0,0 +1,23 @@
+﻿var firstCompleteIndex = function (arr, mat) {
+    let n = mat.length, m = mat[0].length;
+    let mp = new Map();
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            mp.set(mat[i][j], [i, j]);
+        }
+    }
+    let rowCnt = new Array(n).fill(0);
+    let colCnt = new Array(m).fill(0);
+    for (let i = 0; i < arr.length; i++) {
+        let v = mp.get(arr[i]);
+        rowCnt[v[0]]++;
+        if (rowCnt[v[0]] == m) {
+            return i;
+        }
+        colCnt[v[1]]++;
+        if (colCnt[v[1]] == n) {
+            return i;
+        }
+    }
+    return -1;
+};
