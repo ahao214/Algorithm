49f14e668f30d46b511f905ab60d4059368bbb85
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-04 10:25:40 +0800

    1038. 从二叉搜索树到更大和树

---
+++ Ahao.LeetCodeCPP/1038. 从二叉搜索树到更大和树.cpp
@@ -0,0 +1,42 @@
+#include<vector>
+
+using namespace std;
+
+
+/*
+1038. 从二叉搜索树到更大和树
+
+给定一个二叉搜索树 root (BST)，请将它的每个节点的值替换成树中大于或者等于该节点值的所有节点值之和。
+
+提醒一下， 二叉搜索树 满足下列约束条件：
+
+节点的左子树仅包含键 小于 节点键的节点。
+节点的右子树仅包含键 大于 节点键的节点。
+左右子树也必须是二叉搜索树。
+*/
+
+class Solution {
+public:
+	int sum = 0;
+
+	TreeNode* bstToGst(TreeNode* root) {
+		if (root != nullptr) {
+			bstToGst(root->right);
+			sum += root->val;
+			root->val = sum;
+			bstToGst(root->left);
+		}
+		return root;
+	}
+};
+
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
