a61bc5eb1bf4c9e9fc326a1ada01a52c43243959
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-04 10:24:27 +0800

    1038. 从二叉搜索树到更大和树

---
+++ Ahao.LeetCode/1000-1099/demo1038/Class1037.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1000_1099.demo1038
+{
+    public class Class1038
+    {
+        int sum = 0;
+        public TreeNode BstToGst(TreeNode root)
+        {            
+            if (root != null)
+            {
+                BstToGst(root.right);
+                sum += root.val;
+                root.val = sum;
+                BstToGst(root.left);
+            }
+            return root;
+        }
+
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
