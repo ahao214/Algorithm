600c18bdc8d1458cc79ed6eddd266238f2a2d36b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-05 00:05:33 +0800

    2477. 到达首都的最少油耗

---
+++ Ahao.LeetCode/2400-2499/demo2477/Class2477.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2477
+{
+    public class Class2477
+    {
+        long res = 0;
+
+        public long MinimumFuelCost(int[][] roads, int seats)
+        {
+            int n = roads.Length;
+            IList<int>[] g = new IList<int>[n + 1];
+            for (int i = 0; i <= n; i++)
+            {
+                g[i] = new List<int>();
+            }
+            foreach (int[] e in roads)
+            {
+                g[e[0]].Add(e[1]);
+                g[e[1]].Add(e[0]);
+            }
+            DFS(0, -1, seats, g);
+            return res;
+        }
+
+        public int DFS(int cur, int fa, int seats, IList<int>[] g)
+        {
+            int peopleSum = 1;
+            foreach (int ne in g[cur])
+            {
+                if (ne != fa)
+                {
+                    int peopleCnt = DFS(ne, cur, seats, g);
+                    peopleSum += peopleCnt;
+                    res += (peopleCnt + seats - 1) / seats;
+                }
+            }
+            return peopleSum;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/2400-2499/demo2477/JS2477.js
@@ -0,0 +1,22 @@
+﻿var minimumFuelCost = function (roads, seats) {
+    const n = roads.length;
+    const g = new Array(n + 1).fill(0).map(() => new Array());
+    for (const e of roads) {
+        g[e[0]].push(e[1]);
+        g[e[1]].push(e[0]);
+    }
+    let res = 0;
+    var dfs = function (cur, fa) {
+        let peopleSum = 1;
+        for (const ne of g[cur]) {
+            if (ne != fa) {
+                const peopleCnt = dfs(ne, cur);
+                peopleSum += peopleCnt;
+                res += Math.floor((peopleCnt + seats - 1) / seats);
+            }
+        }
+        return peopleSum;
+    }
+    dfs(0, -1);
+    return res;
+};
