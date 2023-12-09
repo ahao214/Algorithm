42e9088e9d92b8a766088d13f40bc1bb96f264eb
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-09 10:20:46 +0800

    2048. 下一个更大的数值平衡数

--- Ahao.LeetCode/2000-2099/demo2047 - 复制/Class2048.cs
+++ Ahao.LeetCode/2000-2099/demo2047 - 复制/Class2048.cs
@@ -37,5 +37,38 @@ namespace Ahao.LeetCode.Simple.demo2048
             return balance[i];
         }
 
+
+
+        public int NextBeautifulNumber2(int n)
+        {
+            for (int i = n + 1; i <= 1224444; i++)
+            {
+                if (IsBalance(i))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private bool IsBalance(int x)
+        {
+            int[] count = new int[10];
+            while (x > 0)
+            {
+                count[x % 10]++;
+                x /= 10;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (count[i] > 0 && count[i] != i)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
     }
 }
