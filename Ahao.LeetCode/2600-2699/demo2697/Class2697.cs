e716226800f1846a27da0e7f43d748368c23cbf0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-13 10:12:58 +0800

    2697. 字典序最小回文串

--- Ahao.LeetCode/2600-2699/demo2697/Class2697.cs
+++ Ahao.LeetCode/2600-2699/demo2697/Class2697.cs
@@ -8,12 +8,21 @@ namespace Ahao.LeetCode._2600_2699.demo2697
 {
     public class Class2697
     {
-        
-
+        public string MakeSmallestPalindrome(string s)
+        {
+            char[] arr = s.ToCharArray();
+            int left = 0, right = arr.Length - 1;
+            while (left < right)
+            {
+                if (arr[left] != arr[right])
+                {
+                    arr[left] = arr[right] = (char)Math.Min(arr[left], arr[right]);
+                }
+                ++left;
+                --right;
+            }
+            return new string(arr);
+        }
 
     }
-
-
-
-
 }
