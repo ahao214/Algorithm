a3b3cc6ddabaf9993153ef111e0154f11ceac4da
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-19 00:17:26 +0800

    1901. 寻找峰值 II

--- Ahao.LeetCode/1900-1999/demo1901/JS1901.js
+++ Ahao.LeetCode/1900-1999/demo1901/JS1901.js
@@ -1,1 +1,28 @@
-﻿
+﻿var maxElement = function (arr) {
+    let i = 0;
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[i] < arr[j]) {
+            i = j;
+        }
+    }
+    return i;
+}
+
+var findPeakGrid = function (mat) {
+    const m = mat.length;
+    let low = 0, high = m - 1;
+    while (low <= high) {
+        let i = Math.floor((low + high) / 2);
+        let j = maxElement(mat[i]);
+        if (i - 1 >= 0 && mat[i][j] < mat[i - 1][j]) {
+            high = i - 1;
+            continue;
+        }
+        if (i + 1 < m && mat[i][j] < mat[i + 1][j]) {
+            low = i + 1;
+            continue;
+        }
+        return [i, j];
+    }
+    return []; // impossible
+};
