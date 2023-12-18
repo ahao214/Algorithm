56aa56ee99781bb7906cb9fa498a1f70ee75e3fe
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-19 00:21:14 +0800

    1901. 寻找峰值 II

--- Ahao.LeetCode/1900-1999/demo1901/Class1901.cs
+++ Ahao.LeetCode/1900-1999/demo1901/Class1901.cs
@@ -39,5 +39,119 @@ namespace Ahao.LeetCode._1900_1999.demo1901
             return new int[0]; // impossible
         }
 
+
+
+        public int[] FindPeakGrid2(int[][] mat)
+        {
+            int m = mat.Length;
+            int low = 0, high = m - 1, maxIndex = 0;
+            while (low < high)
+            {
+                int mid = low + (high - low) / 2;
+                maxIndex = GetMaxIndex(mat[mid]);
+                if (mat[mid][maxIndex] > mat[mid + 1][maxIndex])
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            maxIndex = GetMaxIndex(mat[low]);
+            return new int[] { low, maxIndex };
+        }
+
+        public int GetMaxIndex(int[] row)
+        {
+            int index = 0, maxNum = 0;
+            int n = row.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (row[i] > maxNum)
+                {
+                    maxNum = row[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+
+        int max = 0;
+        int[] pos = new int[] { };
+
+        public int[] FindPeakGrid3(int[][] mat)
+        {
+            for (int i = 0; i < mat.Length; i++)
+            {
+                for (int j = 0; j < mat[0].Length; j++)
+                    Dfs(mat, i, j);
+            }
+            return pos;
+        }
+
+        public void Dfs(int[][] mat, int i, int j)
+        {
+            if (i < 0 || j < 0 || i > mat.Length - 1 || j > mat[0].Length - 1 || mat[i][j] == -1)
+                return;
+
+            if (mat[i][j] > max)
+            {
+                max = mat[i][j];
+                pos = new int[] { i, j };
+                return;
+            }
+            mat[i][j] = -1;
+            Dfs(mat, i + 1, j);
+            Dfs(mat, i - 1, j);
+            Dfs(mat, i, j + 1);
+            Dfs(mat, i, j - 1);
+        }
+
+
+        public int[] FindPeakGrid4(int[][] mat)
+        {
+            var m = mat.Length;
+            var n = mat[0].Length;
+            var result = new int[2];
+            var found = false;
+            while (!found)
+            {
+                var i = result[0];
+                var j = result[1];
+                if (result[0] > 0 && mat[result[0] - 1][result[1]] > mat[i][j])
+                {
+                    i = result[0] - 1;
+                }
+
+                if (result[0] < m - 1 && mat[result[0] + 1][result[1]] > mat[i][j])
+                {
+                    i = result[0] + 1;
+                }
+
+                if (result[1] > 0 && mat[result[0]][result[1] - 1] > mat[i][j])
+                {
+                    i = result[0];
+                    j = result[1] - 1;
+                }
+
+                if (result[1] < n - 1 && mat[result[0]][result[1] + 1] > mat[i][j])
+                {
+                    i = result[0];
+                    j = result[1] + 1;
+                }
+
+                if (i == result[0] && j == result[1]) found = true;
+                result[0] = i;
+                result[1] = j;
+            }
+
+            return result;
+        }
+
+
+
+
     }
 }
