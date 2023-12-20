94b97a590d0bb733b940f7a58eb697248977744c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-20 09:52:41 +0800

    2828. 判别首字母缩略词

--- Ahao.LeetCode/2800-2899/2828/JS2828.js
+++ Ahao.LeetCode/2800-2899/2828/JS2828.js
@@ -1,1 +1,11 @@
-﻿
+﻿var isAcronym = function (words, s) {
+    if (words.length != s.length) {
+        return false;
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (words[i][0] != s[i]) {
+            return false;
+        }
+    }
+    return true;
+};
