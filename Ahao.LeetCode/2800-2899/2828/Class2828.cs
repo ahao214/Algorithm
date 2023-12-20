562acdc3830b7a39ffca0e80a4e33d9f97df260c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-20 09:53:05 +0800

    2828. 判别首字母缩略词

--- Ahao.LeetCode/2800-2899/2828/Class2828.cs
+++ Ahao.LeetCode/2800-2899/2828/Class2828.cs
@@ -19,5 +19,23 @@ namespace Ahao.LeetCode._2800_2899._2828
             return sb.ToString() == s;
         }
 
+
+        public bool IsAcronym2(IList<string> words, string s)
+        {
+            if (words.Count != s.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (words[i][0] != s[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
     }
 }
