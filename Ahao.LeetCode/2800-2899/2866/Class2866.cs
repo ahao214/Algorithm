c7f5c056c6a2d8dc8f51a0b44faf77afe3005496
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-21 10:19:38 +0800

    2866. 美丽塔 II

---
+++ Ahao.LeetCode/2800-2899/2866/Class2866.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2800_2899._2866
+{
+    public class Class2866
+    {
+        public long MaximumSumOfHeights(IList<int> maxHeights)
+        {
+            int n = maxHeights.Count;
+            long res = 0;
+            long[] prefix = new long[n];
+            long[] suffix = new long[n];
+            Stack<int> stack1 = new Stack<int>();
+            Stack<int> stack2 = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                while (stack1.Count > 0 && maxHeights[i] < maxHeights[stack1.Peek()])
+                {
+                    stack1.Pop();
+                }
+                if (stack1.Count == 0)
+                {
+                    prefix[i] = (long)(i + 1) * maxHeights[i];
+                }
+                else
+                {
+                    prefix[i] = prefix[stack1.Peek()] + (long)(i - stack1.Peek()) * maxHeights[i];
+                }
+                stack1.Push(i);
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                while (stack2.Count > 0 && maxHeights[i] < maxHeights[stack2.Peek()])
+                {
+                    stack2.Pop();
+                }
+                if (stack2.Count == 0)
+                {
+                    suffix[i] = (long)(n - i) * maxHeights[i];
+                }
+                else
+                {
+                    suffix[i] = suffix[stack2.Peek()] + (long)(stack2.Peek() - i) * maxHeights[i];
+                }
+                stack2.Push(i);
+                res = Math.Max(res, prefix[i] + suffix[i] - maxHeights[i]);
+            }
+            return res;
+        }
+
+    }
+}
