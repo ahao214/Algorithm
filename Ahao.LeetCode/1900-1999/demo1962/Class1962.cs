0b68dc4fb7e16a37288554fab196e6fc56d65963
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-23 00:04:46 +0800

    1962. 移除石子使总数最小

---
+++ Ahao.LeetCode/1900-1999/demo1962/Class1962.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1900_1999.demo1962
+{
+    public class Class1962
+    {
+
+        public int MinStoneSum(int[] piles, int k)
+        {
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            foreach (int pile in piles)
+            {
+                pq.Enqueue(pile, -pile);
+            }
+            for (int i = 0; i < k; i++)
+            {
+                int pile = pq.Dequeue();
+                pile -= pile / 2;
+                pq.Enqueue(pile, -pile);
+            }
+            int sum = 0;
+            while (pq.Count > 0)
+            {
+                sum += pq.Dequeue();
+            }
+            return sum;
+        }
+    }
+}
---
+++ Ahao.LeetCode/1900-1999/demo1962/JS1962.js
@@ -0,0 +1,18 @@
+﻿var minStoneSum = function (piles, k) {
+    const pq = new MaxPriorityQueue();
+    for (const pile of piles) {
+        pq.enqueue(pile, pile);
+    }
+    for (let i = 0; i < k; i++) {
+        let pile = pq.front().element;
+        pq.dequeue();
+        pile -= Math.floor(pile / 2);
+        pq.enqueue(pile, pile);
+    }
+    let sum = 0;
+    while (!pq.isEmpty()) {
+        sum += pq.front().element;
+        pq.dequeue();
+    }
+    return sum;
+};
