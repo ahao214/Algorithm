eb610ef8e1ceda3cc7e04902a27c4eed69824484
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-24 00:08:26 +0800

    1954. 收集足够苹果的最小花园周长

---
+++ Ahao.LeetCode/1900-1999/demo1954/Class1954.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1900_1999.demo1954
+{
+    public class Class1954
+    {
+
+        public long MinimumPerimeter(long neededApples)
+        {
+            long n = 1;
+            while (2 * n * (n + 1) * (2 * n + 1) < neededApples)
+            {
+                n++;
+            }
+            return n * 8;
+        }
+
+
+        public long MinimumPerimeter2(long neededApples)
+        {
+            long left = 1, right = 100000, ans = 0;
+            while (left <= right)
+            {
+                long mid = (left + right) / 2;
+                if (2 * mid * (mid + 1) * (mid * 2 + 1) >= neededApples)
+                {
+                    ans = mid;
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return ans * 8;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/1900-1999/demo1954/JS1945.js
@@ -0,0 +1,22 @@
+﻿var minimumPerimeter = function (neededApples) {
+    let n = 1;
+    while (2 * n * (n + 1) * (2 * n + 1) < neededApples) {
+        n++;
+    }
+    return n * 8;
+};
+
+
+var minimumPerimeter = function (neededApples) {
+    let left = 1, right = 100000, ans = 0;
+    while (left <= right) {
+        const mid = (left + right) >> 1;
+        if (2 * mid * (mid + 1) * (mid * 2 + 1) >= neededApples) {
+            ans = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return ans * 8;
+};
