9ff69395855f277a75e7a835c1adc136cd25c881
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-25 11:06:40 +0800

    1276. 不浪费原料的汉堡制作方案

---
+++ Ahao.LeetCode/1200-1299/demo1276/Class1276.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1200_1299.demo1276
+{
+    public class Class1276
+    {
+        public IList<int> NumOfBurgers(int tomatoSlices, int cheeseSlices)
+        {
+            if (tomatoSlices % 2 != 0 || tomatoSlices < cheeseSlices * 2 || cheeseSlices * 4 < tomatoSlices)
+            {
+                return new List<int>();
+            }
+            IList<int> ans = new List<int>();
+            ans.Add(tomatoSlices / 2 - cheeseSlices);
+            ans.Add(cheeseSlices * 2 - tomatoSlices / 2);
+            return ans;
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/1200-1299/demo1276/JS1276.js
@@ -0,0 +1,6 @@
+﻿var numOfBurgers = function (tomatoSlices, cheeseSlices) {
+    if (tomatoSlices % 2 != 0 || tomatoSlices < cheeseSlices * 2 || cheeseSlices * 4 < tomatoSlices) {
+        return []
+    }
+    return [(tomatoSlices >> 1) - cheeseSlices, cheeseSlices * 2 - (tomatoSlices >> 1)];
+};
