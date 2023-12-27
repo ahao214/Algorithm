69e197fcd1ff6f4db0f43b8185e680dc2bf6bec3
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-27 10:29:42 +0800

    2660. 保龄球游戏的获胜者

--- Ahao.LeetCode/2600-2699/demo2661 - 复制/Class2660.cs
+++ Ahao.LeetCode/2600-2699/demo2661 - 复制/Class2660.cs
@@ -44,5 +44,31 @@ namespace Ahao.LeetCode._2600_2699.demo2660
             return res;
         }
 
+
+        public int IsWinner2(int[] player1, int[] player2)
+        {
+            int s1 = Score(player1);
+            int s2 = Score(player2);
+            return s1 == s2 ? 0 : s1 > s2 ? 1 : 2;
+        }
+
+        public int Score(int[] player)
+        {
+            int res = 0;
+            for (int i = 0; i < player.Length; i++)
+            {
+                if ((i > 0 && player[i - 1] == 10) || (i > 1 && player[i - 2] >= 10))
+                {
+                    res += 2 * player[i];
+                }
+                else
+                {
+                    res += player[i];
+                }
+            }
+            return res;
+        }
+
+
     }
 }
--- Ahao.LeetCode/2600-2699/demo2661 - 复制/JS2660.js
+++ Ahao.LeetCode/2600-2699/demo2661 - 复制/JS2660.js
@@ -1,1 +1,16 @@
-﻿
+﻿var isWinner = function (player1, player2) {
+    const score = function (player) {
+        let res = 0;
+        for (let i = 0; i < player.length; i++) {
+            if (i > 0 && player[i - 1] == 10 || i > 1 && player[i - 2] == 10) {
+                res += 2 * player[i];
+            } else {
+                res += player[i];
+            }
+        }
+        return res;
+    }
+    const s1 = score(player1);
+    const s2 = score(player2);
+    return s1 == s2 ? 0 : s1 > s2 ? 1 : 2;
+};
