12713024d98d2f2d82dbce41bcd19db7751f8476
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-28 00:11:51 +0800

    2735. 收集巧克力

---
+++ Ahao.LeetCode/2700-2799/demo2735/Class2735.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2700_2799.demo2735
+{
+    public class Class2735
+    {
+
+        public long MinCost(int[] nums, int x)
+        {
+            int n = nums.Length;
+            int[] f = new int[n];
+            Array.Copy(nums, 0, f, 0, n);
+            long ans = GetSum(f);
+            for (int k = 1; k < n; ++k)
+            {
+                for (int i = 0; i < n; ++i)
+                {
+                    f[i] = Math.Min(f[i], nums[(i + k) % n]);
+                }
+                ans = Math.Min(ans, (long)k * x + GetSum(f));
+            }
+            return ans;
+        }
+
+        public long GetSum(int[] f)
+        {
+            long sum = 0;
+            foreach (int num in f)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+
+
+
+    }
+}
---
+++ Ahao.LeetCode/2700-2799/demo2735/JS2735.js
@@ -0,0 +1,14 @@
+﻿var minCost = function (nums, x) {
+    let n = nums.length;
+    const f = [...nums];
+    let ans = nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    for (let k = 1; k < n; k++) {
+        for (let i = 0; i < n; i++) {
+            f[i] = Math.min(f[i], nums[(i + k) % n])
+        }
+        sum = f.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        ans = Math.min(ans, k * x + sum);
+    }
+    return ans;
+};
