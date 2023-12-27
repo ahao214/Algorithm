5ce7c9c0804641502593b287dbba33d3faa626c5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-27 10:28:34 +0800

    2660. 保龄球游戏的获胜者

---
+++ Ahao.LeetCode/2600-2699/demo2661 - 复制/Class2660.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RTools_NTS.Util;
+
+namespace Ahao.LeetCode._2600_2699.demo2660
+{
+    public class Class2660
+    {
+        public int IsWinner(int[] player1, int[] player2)
+        {
+            int sumOne = 0;
+            int sumTow = 0;
+            for (int i = 0; i < player1.Length; i++)
+            {
+                if ((i - 1 >= 0 && player1[i - 1] == 10) || (i - 2 >= 0 && player1[i - 2] == 10))
+                {
+                    sumOne += player1[i] * 2;
+                }
+                else
+                {
+                    sumOne += player1[i];
+                }
+            }
+            for (int i = 0; i < player2.Length; i++)
+            {
+                if ((i - 1 >= 0 && player2[i - 1] == 10) || (i - 2 >= 0 && player2[i - 2] == 10))
+                {
+                    sumTow += player2[i] * 2;
+                }
+                else
+                {
+                    sumTow += player2[i];
+                }
+            }
+            int res = 0;
+
+            if (sumOne > sumTow)
+                res = 1;
+            if (sumOne < sumTow)
+                res = 2;
+            return res;
+        }
+
+    }
+}
