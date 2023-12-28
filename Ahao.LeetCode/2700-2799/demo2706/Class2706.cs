445cb056284ac610b5e8c253b4674ae09c6df268
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-29 00:11:05 +0800

    2706. 购买两块巧克力

---
+++ Ahao.LeetCode/2700-2799/demo2706/Class2706.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2700_2799.demo2706
+{
+    internal class Class2706
+    {
+
+        public int BuyChoco(int[] prices, int money)
+        {
+            Array.Sort(prices);
+            int sum = prices[0] + prices[1];
+            if (sum <= money)
+                return money - sum;
+            return money;
+        }
+
+
+    }
+}
