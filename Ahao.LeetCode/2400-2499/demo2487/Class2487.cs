ef6a734f6e44e8bf1027de405fb4f6b0882c6ed7
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-03 00:34:18 +0800

    2487. 从链表中移除节点

---
+++ Ahao.LeetCode/2400-2499/demo2487/Class2487.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2400_2499.demo2487
+{
+    public class Class2487
+    {
+        public ListNode RemoveNodes(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            head.next = RemoveNodes(head.next);
+            if (head.next != null && head.val < head.next.val)
+            {
+                return head.next;
+            }
+            else
+            {
+                return head;
+            }
+        }
+
+        public ListNode RemoveNodes2(ListNode head)
+        {
+            Stack<ListNode> stack = new Stack<ListNode>();
+            for (; head != null; head = head.next)
+            {
+                stack.Push(head);
+            }
+            for (; stack.Count > 0; stack.Pop())
+            {
+                if (head == null || stack.Peek().val >= head.val)
+                {
+                    stack.Peek().next = head;
+                    head = stack.Peek();
+                }
+            }
+            return head;
+        }
+
+
+        public ListNode RemoveNodes3(ListNode head)
+        {
+            head = Reverse(head);
+            for (ListNode p = head; p.next != null;)
+            {
+                if (p.val > p.next.val)
+                {
+                    p.next = p.next.next;
+                }
+                else
+                {
+                    p = p.next;
+                }
+            }
+            return Reverse(head);
+        }
+
+        public ListNode Reverse(ListNode head)
+        {
+            ListNode dummy = new ListNode();
+            while (head != null)
+            {
+                ListNode p = head;
+                head = head.next;
+                p.next = dummy.next;
+                dummy.next = p;
+            }
+            return dummy.next;
+        }
+
+
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val =0,ListNode next=null)
+        {
+            this.val = val;
+            this.next = null;
+        }
+    }
+}
---
+++ Ahao.LeetCode/2400-2499/demo2487/JS2487.js
@@ -0,0 +1,56 @@
+﻿function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+var removeNodes = function (head) {
+    if (head == null) {
+        return null;
+    }
+    head.next = removeNodes(head.next);
+    if (head.next != null && head.val < head.next.val) {
+        return head.next;
+    } else {
+        return head;
+    }
+};
+
+
+var removeNodes = function (head) {
+    let st = new Array();
+    for (; head != null; head = head.next) {
+        st.push(head);
+    }
+    for (; st.length > 0; st.pop()) {
+        if (head == null || st[st.length - 1].val >= head.val) {
+            st[st.length - 1].next = head;
+            head = st[st.length - 1];
+        }
+    }
+    return head;
+};
+
+
+
+var reverse = function (head) {
+    let dummy = new ListNode();
+    while (head != null) {
+        let p = head;
+        head = head.next;
+        p.next = dummy.next;
+        dummy.next = p;
+    }
+    return dummy.next;
+};
+
+var removeNodes = function (head) {
+    head = reverse(head);
+    for (let p = head; p.next != null;) {
+        if (p.val > p.next.val) {
+            p.next = p.next.next;
+        } else {
+            p = p.next;
+        }
+    }
+    return reverse(head);
+};
