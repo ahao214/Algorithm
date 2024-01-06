d8a71729342e2af45cd0f496ca5d98e9e296582b
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-06 11:00:46 +0800

    2487. 从链表中移除节点

---
+++ Ahao.LeetCode/2800-2899/2807/Class2807.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2800_2899._2807
+{
+    public class Class2807
+    {
+        public ListNode InsertGreatestCommonDivisors(ListNode head)
+        {
+            ListNode node = head;
+            while (node.next != null)
+            {
+                node.next = new ListNode(GCD(node.val, node.next.val), node.next);
+                node = node.next.next;
+            }
+            return head;
+        }
+
+        public int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int tmp = a % b;
+                a = b;
+                b = tmp;
+            }
+            return a;
+        }
+
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Ahao.LeetCode/2800-2899/2807/JS2807.js
@@ -0,0 +1,18 @@
+﻿function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+
+
+let gcd = (a, b) => b == 0 ? a : gcd(b, a % b)
+
+var insertGreatestCommonDivisors = function (head) {
+    let node = head
+    while (node.next != null) {
+        node.next = new ListNode(gcd(node.val, node.next.val), node.next);
+        node = node.next.next;
+    }
+    return head;
+};
+
