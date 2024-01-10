9bcb9a9d8d103d823fd31b594dfff799f20be667
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-10 11:34:49 +0800

    2696. 删除子串后的字符串最小长度

---
+++ Ahao.LeetCode/2600-2699/demo2696/Class2696.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2600_2699.demo2696
+{
+    public class Class2696
+    {
+        public int MinLength(string s)
+        {
+            int length = 0;
+            while (length != s.Length)
+            {
+                length = s.Length;
+                s = s.Replace("AB", "").Replace("CD", "");
+            }
+            return length;
+        }
+
+
+    }
+}
