d50dd18da85fb6ad4a8bb7dcca4339c4609fec8f
liupenghao@fulan.com.cn <haochen214@163.com>
2024-05-11 15:56:26 +0800

    使用for循环计算一个n!的阶乘程序

---
+++ ch01_03/Program.cs
@@ -0,0 +1,18 @@
+﻿// 使用for循环计算一个n!的阶乘程序
+
+
+int sum = 1;
+Console.Write("请从键盘输入n=");
+int n = int.Parse(Console.ReadLine());
+// 用for循环计算 n!
+for (int i = 1; i < n + 1; i++)
+{
+    for (int j = i; j > 0; j--)
+    {
+        sum = sum * j;
+    }
+    Console.WriteLine(i + "!=" + sum);
+    sum = 1;
+}
+Console.ReadLine();
+
