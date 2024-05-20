7f08c44baeac005304be081c4eb371895bd8cbdf
liupenghao@fulan.com.cn <haochen214@163.com>
2024-05-20 12:46:49 +0800

    1542. 找出最长的超赞子字符串

---
+++ Ahao.LeetCode/1500-1599/demo1542/Class1542.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._1500_1599.demo1542
+{
+    internal class Class1542
+    {
+    }
+}
