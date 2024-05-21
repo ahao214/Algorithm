0f4b37cbc4668321718ea2ccc2e45b140707a35e
liupenghao@fulan.com.cn <haochen214@163.com>
2024-05-22 00:07:03 +0800

    2225. 找出输掉零场或一场比赛的玩家

---
+++ Ahao.LeetCode/2200-2299/demo2225/JS2225.js
@@ -0,0 +1,19 @@
+﻿var findWinners = function (matches) {
+    const freq = new Map();
+    for (const [winner, loser] of matches) {
+        if (!freq.has(winner)) {
+            freq.set(winner, 0);
+        }
+        freq.set(loser, (freq.get(loser) || 0) + 1);
+    }
+
+    const ans = [[], []];
+    for (const [key, value] of freq) {
+        if (value < 2) {
+            ans[value].push(key);
+        }
+    }
+    ans[0].sort((a, b) => a - b);
+    ans[1].sort((a, b) => a - b);
+    return ans;
+};
---
+++ Ahao.LeetCode/2200-2299/demo2225/TS2225.ts
@@ -0,0 +1,20 @@
+﻿function findWinnersTS(matches: number[][]): number[][] {
+    const freq: Map<number, number> = new Map();
+    for (const [winner, loser] of matches) {
+        if (!freq.has(winner)) {
+            freq.set(winner, 0);
+        }
+        freq.set(loser, (freq.get(loser) || 0) + 1);
+    }
+
+    const ans: number[][] = [[], []];
+    for (const [key, value] of freq) {
+        if (value < 2) {
+            ans[value].push(key);
+        }
+    }
+
+    ans[0].sort((a, b) => a - b);
+    ans[1].sort((a, b) => a - b);
+    return ans;
+};
