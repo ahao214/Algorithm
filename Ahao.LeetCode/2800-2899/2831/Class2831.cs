a70e50262ae49a84f26129ece042c5113755b36e
liupenghao@fulan.com.cn <haochen214@163.com>
2024-05-23 00:04:01 +0800

    2831. 找出最长等值子数组

---
+++ Ahao.LeetCode/2800-2899/2831/Class2831.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2800_2899._2831
+{
+    public class Class2831
+    {
+        public int LongestEqualSubarray(IList<int> nums, int k)
+        {
+            int n = nums.Count;
+            IDictionary<int, IList<int>> pos = new Dictionary<int, IList<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                pos.TryAdd(nums[i], new List<int>());
+                pos[nums[i]].Add(i);
+            }
+            int ans = 0;
+            foreach (IList<int> vec in pos.Values)
+            {
+                for (int i = 0, j = 0; i < vec.Count; i++)
+                {
+                    /* 缩小窗口，直到不同元素数量小于等于 k */
+                    while (vec[i] - vec[j] - (i - j) > k)
+                    {
+                        j++;
+                    }
+                    ans = Math.Max(ans, i - j + 1);
+                }
+            }
+            return ans;
+        }
+
+
+    }
+
+
+}
---
+++ Ahao.LeetCode/2800-2899/2831/JS2831.js
@@ -0,0 +1,21 @@
+﻿var longestEqualSubarray = function (nums, k) {
+    let n = nums.length;
+    let pos = new Map();
+    for (let i = 0; i < n; i++) {
+        if (!pos.has(nums[i])) {
+            pos.set(nums[i], []);
+        }
+        pos.get(nums[i]).push(i);
+    }
+    let ans = 0;
+    for (let vec of pos.values()) {
+        for (let i = 0, j = 0; i < vec.length; i++) {
+            /* 缩小窗口，直到不同元素数量小于等于 k */
+            while (vec[i] - vec[j] - (i - j) > k) {
+                j++;
+            }
+            ans = Math.max(ans, i - j + 1);
+        }
+    }
+    return ans;
+};
