2fdea4bab48bbcee0e85ad6f9838618d74ffe6f2
liupenghao@fulan.com.cn <haochen214@163.com>
2024-05-25 14:22:49 +0800

    2903. 找出满足差值条件的下标 I

---
+++ Ahao.LeetCode/2900-2999/2903/Class2903.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2900_2999._2903
+{
+    internal class Class2903
+    {
+        public int[] FindIndices(int[] nums, int indexDifference, int valueDifference)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i; j < nums.Length; j++)
+                {
+                    if (j - i >= indexDifference && Math.Abs(nums[j] - nums[i]) >= valueDifference)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+
+
+        public int[] FindIndices2(int[] nums, int indexDifference, int valueDifference)
+        {
+            int minIndex = 0, maxIndex = 0;
+            for (int j = indexDifference; j < nums.Length; j++)
+            {
+                int i = j - indexDifference;
+                if (nums[i] < nums[minIndex])
+                {
+                    minIndex = i;
+                }
+                if (nums[j] - nums[minIndex] >= valueDifference)
+                {
+                    return new int[] { minIndex, j };
+                }
+                if (nums[i] > nums[maxIndex])
+                {
+                    maxIndex = i;
+                }
+                if (nums[maxIndex] - nums[j] >= valueDifference)
+                {
+                    return new int[] { maxIndex, j };
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+
+
+
+    }
+}
---
+++ Ahao.LeetCode/2900-2999/2903/TS2903.ts
@@ -0,0 +1,31 @@
+﻿function findIndicesTS(nums: number[], indexDifference: number, valueDifference: number): number[] {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i; j < nums.length; j++) {
+            if (j - i >= indexDifference && Math.abs(nums[j] - nums[i]) >= valueDifference) {
+                return [i, j];
+            }
+        }
+    }
+    return [-1, -1];
+};
+
+
+function findIndicesTS2(nums: number[], indexDifference: number, valueDifference: number): number[] {
+    let minIndex = 0, maxIndex = 0;
+    for (let j = indexDifference; j < nums.length; j++) {
+        let i = j - indexDifference;
+        if (nums[i] < nums[minIndex]) {
+            minIndex = i;
+        }
+        if (nums[j] - nums[minIndex] >= valueDifference) {
+            return [minIndex, j];
+        }
+        if (nums[i] > nums[maxIndex]) {
+            maxIndex = i;
+        }
+        if (nums[maxIndex] - nums[j] >= valueDifference) {
+            return [maxIndex, j];
+        }
+    }
+    return [-1, -1];
+};
