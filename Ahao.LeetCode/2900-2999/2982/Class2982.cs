c090fe7d1b8c8bb2c3d083add1515c2deaa9dbb4
liupenghao@fulan.com.cn <haochen214@163.com>
2024-05-30 20:02:55 +0800

    2982. 找出出现至少三次的最长特殊子字符串 II

---
+++ Ahao.LeetCode/2900-2999/2982/Class2982.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2900_2999._2982
+{
+    internal class Class2982
+    {
+        public int MaximumLength(string s)
+        {
+            int n = s.Length;
+            IDictionary<char, IList<int>> cnt = new Dictionary<char, IList<int>>();
+
+            for (int i = 0, j = 0; i < n; i = j)
+            {
+                while (j < n && s[j] == s[i])
+                {
+                    j++;
+                }
+                cnt.TryAdd(s[i], new List<int>());
+                cnt[s[i]].Add(j - i);
+            }
+
+            int res = -1;
+            foreach (IList<int> vec in cnt.Values)
+            {
+                int lo = 1, hi = n - 2;
+                while (lo <= hi)
+                {
+                    int mid = (lo + hi) >> 1;
+                    int count = 0;
+                    foreach (int x in vec)
+                    {
+                        if (x >= mid)
+                        {
+                            count += x - mid + 1;
+                        }
+                    }
+                    if (count >= 3)
+                    {
+                        res = Math.Max(res, mid);
+                        lo = mid + 1;
+                    }
+                    else
+                    {
+                        hi = mid - 1;
+                    }
+                }
+            }
+            return res;
+        }
+
+
+    }
+}
---
+++ Ahao.LeetCode/2900-2999/2982/JS2982.js
@@ -0,0 +1,36 @@
+﻿var maximumLength = function (s) {
+    const n = s.length;
+    const cnt = new Map();
+
+    for (let i = 0, j = 0; i < n; i = j) {
+        while (j < n && s[j] === s[i]) {
+            j++;
+        }
+        const len = j - i;
+        if (!cnt.has(s[i])) {
+            cnt.set(s[i], []);
+        }
+        cnt.get(s[i]).push(len);
+    }
+
+    let res = -1;
+    for (const vec of cnt.values()) {
+        let lo = 1, hi = n - 2;
+        while (lo <= hi) {
+            const mid = (lo + hi) >> 1;
+            let count = 0;
+            for (const x of vec) {
+                if (x >= mid) {
+                    count += x - mid + 1;
+                }
+            }
+            if (count >= 3) {
+                res = Math.max(res, mid);
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+    }
+    return res;
+};
