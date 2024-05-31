96759e05d717417b171a8019f4ebe8046cf554be
liupenghao@fulan.com.cn <haochen214@163.com>
2024-05-31 14:55:48 +0800

    2965. 找出缺失和重复的数字

---
+++ Ahao.LeetCode/2900-2999/2965/Class2965.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2900_2999._2965
+{
+    internal class Class2965
+    {
+
+        public int[] FindMissingAndRepeatedValues(int[][] grid)
+        {
+            int n = grid.Length;
+            int[] count = new int[n * n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    count[grid[i][j]]++;
+                }
+            }
+
+            int[] res = new int[2];
+            for (int i = 1; i <= n * n; i++)
+            {
+                if (count[i] == 2)
+                {
+                    res[0] = i;
+                }
+                if (count[i] == 0)
+                {
+                    res[1] = i;
+                }
+            }
+            return res;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/2900-2999/2965/TS2965.ts
@@ -0,0 +1,12 @@
+﻿function findMissingAndRepeatedValuesTS(grid: number[][]): number[] {
+    const n = grid.length;
+    const count = Array(n * n + 1).fill(0);
+    count[0] = -1;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            count[grid[i][j]]++;
+        }
+    }
+    return [count.indexOf(2), count.indexOf(0)];
+};
+
