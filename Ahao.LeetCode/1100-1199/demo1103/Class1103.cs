1ff9686a97b7ab9239b9551708d7aec30d98a7a3
liupenghao@fulan.com.cn <haochen214@163.com>
2024-06-03 18:49:04 +0800

    1103. 分糖果 II

--- Ahao.LeetCode/1100-1199/demo1103/Class1103.cs
+++ Ahao.LeetCode/1100-1199/demo1103/Class1103.cs
@@ -22,5 +22,31 @@ namespace Ahao.LeetCode._1100_1199.demo1103
             return ans;
         }
 
+
+
+        public int[] DistributeCandies2(int candies, int num_people)
+        {
+            int n = num_people;
+            int p = (int)(Math.Sqrt(2 * candies + 0.25) - 0.5);
+            int remaining = (int)(candies - (p + 1) * p * 0.5);
+            int rows = p / n, cols = p % n;
+
+            int[] d = new int[n];
+            for (int i = 0; i < n; ++i)
+            {
+                // complete rows
+                d[i] = (i + 1) * rows + (int)(rows * (rows - 1) * 0.5) * n;
+                // cols in the last row
+                if (i < cols)
+                {
+                    d[i] += i + 1 + rows * n;
+                }
+            }
+            // remaining candies        
+            d[cols] += remaining;
+            return d;
+        }
+
+
     }
 }
