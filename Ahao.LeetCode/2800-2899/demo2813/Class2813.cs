e4f8a3dbf31c941d404cc30082a291e89fdf391c
liupenghao@fulan.com.cn <haochen214@163.com>
2024-06-13 14:21:50 +0800

    2813. 子序列最大优雅度

---
+++ Ahao.LeetCode/2800-2899/demo2813/Class2813.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2800_2899.demo2813
+{
+    internal class Class2813
+    {
+
+        public long FindMaximumElegance(int[][] items, int k)
+        {
+            Array.Sort(items, (item0, item1) => item1[0] - item0[0]);
+            ISet<int> categorySet = new HashSet<int>();
+            long profit = 0, res = 0;
+            Stack<int> st = new Stack<int>();
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (i < k)
+                {
+                    profit += items[i][0];
+                    if (!categorySet.Add(items[i][1]))
+                    {
+                        st.Push(items[i][0]);
+                    }
+                }
+                else if (st.Count > 0 && categorySet.Add(items[i][1]))
+                {
+                    profit += items[i][0] - st.Pop();
+                }
+                res = Math.Max(res, profit + (long)categorySet.Count * categorySet.Count);
+            }
+            return res;
+        }
+
+    }
+}
---
+++ Ahao.LeetCode/2800-2899/demo2813/JS2813.js
@@ -0,0 +1,21 @@
+﻿var findMaximumElegance = function (items, k) {
+    items.sort((item0, item1) => item1[0] - item0[0])
+    let categorySet = new Set();
+    let profit = 0, res = 0;
+    let st = [];
+    for (let i = 0; i < items.length; i++) {
+        if (i < k) {
+            profit += items[i][0];
+            if (!categorySet.has(items[i][1])) {
+                categorySet.add(items[i][1]);
+            } else {
+                st.push(items[i][0]);
+            }
+        } else if (st.length > 0 && !categorySet.has(items[i][1])) {
+            profit += items[i][0] - st.pop();
+            categorySet.add(items[i][1]);
+        }
+        res = Math.max(res, profit + categorySet.size * categorySet.size);
+    }
+    return res;
+};
