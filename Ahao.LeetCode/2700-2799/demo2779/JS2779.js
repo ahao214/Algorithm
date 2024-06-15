65fa17371f383b153e4abb67a7d6627a1d036c84
liupenghao@fulan.com.cn <haochen214@163.com>
2024-06-15 12:24:54 +0800

    2779. 数组的最大美丽值

---
+++ Ahao.LeetCode/2700-2799/demo2779/JS2779.js
@@ -0,0 +1,28 @@
+﻿var maximumBeauty = function (nums, k) {
+    let res = 0, n = nums.length;
+    nums.sort((x, y) => x - y);
+    for (let i = 0, j = 0; i < n; i++) {
+        while (nums[i] - 2 * k > nums[j]) {
+            j++;
+        }
+        res = Math.max(res, i - j + 1);
+    }
+    return res;
+};
+
+
+
+var maximumBeauty = function (nums, k) {
+    let m = Math.max(...nums);
+    let diff = new Array(m + 2).fill(0);
+    for (let i = 0; i < nums.length; i++) {
+        diff[Math.max(nums[i] - k, 0)]++;
+        diff[Math.min(nums[i] + k + 1, m + 1)]--;
+    }
+    let res = 0, count = 0;
+    for (let i = 0; i < diff.length; i++) {
+        count += diff[i];
+        res = Math.max(res, count);
+    }
+    return res;
+};
---
+++ Ahao.LeetCode/2700-2799/demo2779/TS2779.ts
@@ -0,0 +1,28 @@
+﻿function maximumBeautyTS(nums: number[], k: number): number {
+    let res = 0, n = nums.length;
+    nums.sort((x, y) => x - y);
+    for (let i = 0, j = 0; i < n; i++) {
+        while (nums[i] - 2 * k > nums[j]) {
+            j++;
+        }
+        res = Math.max(res, i - j + 1);
+    }
+    return res;
+};
+
+
+function maximumBeautyTS2(nums: number[], k: number): number {
+    let m = Math.max(...nums);
+    let diff = new Array(m + 2).fill(0);
+    for (let i = 0; i < nums.length; i++) {
+        diff[Math.max(nums[i] - k, 0)]++;
+        diff[Math.min(nums[i] + k + 1, m + 1)]--;
+    }
+    let res = 0, count = 0;
+    for (let i = 0; i < diff.length; i++) {
+        count += diff[i];
+        res = Math.max(res, count);
+    }
+    return res;
+};
+
