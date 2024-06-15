35f56f00c5ad87d3d8de8b93267e70d26633370e
liupenghao@fulan.com.cn <haochen214@163.com>
2024-06-15 12:27:30 +0800

    2779. 数组的最大美丽值

--- Ahao.LeetCode/2700-2799/demo2779/Class2779.cs
+++ Ahao.LeetCode/2700-2799/demo2779/Class2779.cs
@@ -8,5 +8,45 @@ namespace Ahao.LeetCode._2700_2799.demo2779
 {
     internal class Class2779
     {
+
+        public int MaximumBeauty(int[] nums, int k)
+        {
+            int res = 0;
+            int n = nums.Length;
+            Array.Sort(nums);
+            for (int i = 0, j = 0; i < n; i++)
+            {
+                while (nums[i] - 2 * k > nums[j])
+                {
+                    j++;
+                }
+                res = Math.Max(res, i = j + 1);
+            }
+            return res;
+        }
+
+
+        public int MaximumBeauty2(int[] nums, int k)
+        {
+            int m = 0;
+            foreach (int x in nums)
+            {
+                m = Math.Max(m, x);
+            }
+            int[] diff = new int[m + 2];
+            foreach (int x in nums)
+            {
+                diff[Math.Max(x - k, 0)]++;
+                diff[Math.Min(x + k + 1, m + 1)]--;
+            }
+            int res = 0, count = 0;
+            foreach (int x in diff)
+            {
+                count += x;
+                res = Math.Max(res, count);
+            }
+            return res;
+        }
+
     }
 }
