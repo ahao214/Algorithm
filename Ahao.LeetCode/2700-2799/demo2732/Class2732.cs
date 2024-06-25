3d9d61dce9c0f00ad99c69b524a24937fa8946e1
liupenghao@fulan.com.cn <haochen214@163.com>
2024-06-25 17:10:06 +0800

    2732. 找到矩阵中的好子集

---
+++ Ahao.LeetCode/2700-2799/demo2732/Class2732.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahao.LeetCode._2700_2799.demo2732
+{
+    internal class Class2732
+    {
+
+        public IList<int> GoodSubsetofBinaryMatrix(int[][] grid)
+        {
+            IList<int> ans = new List<int>();
+            IDictionary<int, int> dic = new Dictionary<int, int>();
+            int m = grid.Length;
+            int n = grid[0].Length;
+
+            for (int j = 0; j < m; j++)
+            {
+                int st = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    st |= (grid[j][i] << i);
+                }
+                if (!dic.ContainsKey(st))
+                {
+                    dic.Add(st, j);
+                }
+                else
+                {
+                    dic[st] = j;
+                }
+            }
+
+            if (dic.ContainsKey(0))
+            {
+                ans.Add(dic[0]);
+                return ans;
+            }
+
+            foreach (KeyValuePair<int, int> pair1 in dic)
+            {
+                int x = pair1.Key, i = pair1.Value;
+                foreach (KeyValuePair<int, int> pair2 in dic)
+                {
+                    int y = pair2.Key, j = pair2.Value;
+                    if ((x & y) == 0)
+                    {
+                        IList<int> list = new List<int>();
+                        list.Add(Math.Min(i, j));
+                        list.Add(Math.Max(i, j));
+                        return list;
+                    }
+                }
+            }
+
+            return ans;
+        }
+
+
+
+
+    }
+}
---
+++ Ahao.LeetCode/2700-2799/demo2732/TS2732.ts
@@ -0,0 +1,28 @@
+﻿function goodSubsetofBinaryMatrixTS(grid: number[][]): number[] {
+    let ans: number[] = [];
+    let mp: Map<number, number> = new Map();
+    let m: number = grid.length;
+    let n: number = grid[0].length;
+    for (let j = 0; j < m; j++) {
+        let st: number = 0;
+        for (let i = 0; i < n; i++) {
+            st |= (grid[j][i] << i);
+        }
+        mp.set(st, j);
+    }
+
+    if (mp.has(0)) {
+        ans.push(mp.get(0)!);
+        return ans;
+    }
+
+    for (let [x, i] of mp) {
+        for (let [y, j] of mp) {
+            if (!(x & y)) {
+                return [Math.min(i, j), Math.max(i, j)];
+            }
+        }
+    }
+
+    return ans;
+};
