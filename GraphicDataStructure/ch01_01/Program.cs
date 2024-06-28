41a8009ffe088b1bafc5f79cae24fca73aa46602
liupenghao@fulan.com.cn <haochen214@163.com>
2024-06-28 17:22:29 +0800

    阶乘函数

---
+++ GraphicDataStructure/ch01_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+namespace ch01_01;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        WriteLine($"5!={Fac(5)}");
+        ReadKey();
+    }
+
+    /// <summary>
+    /// 阶乘函数
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    static int Fac(int n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return n * Fac(n - 1);
+    }
+}
