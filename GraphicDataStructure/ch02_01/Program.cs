5d6d254a3c0550f31681e8544ec7cefe9d492ac2
liupenghao@fulan.com.cn <haochen214@163.com>
2024-06-28 17:26:49 +0800

    使用一维数组寻找并存储范围为1到MAX内的所有质数

---
+++ GraphicDataStructure/ch02_01/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+
+namespace ch02_01;
+
+internal class Program
+{
+    // 使用一维数组寻找并存储范围为1到MAX内的所有质数
+    // 所谓质数(Prime Number)是指不能被1和它本身以外的其他整数整除的整数
+
+    static void Main(string[] args)
+    {
+        
+    }
+}
