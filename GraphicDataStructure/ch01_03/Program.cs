58db3343fa29ae531b0e262dff6bfc5db2ccb654
liupenghao@fulan.com.cn <haochen214@163.com>
2024-06-28 17:25:29 +0800

    用for循环计算 n!

---
+++ GraphicDataStructure/ch01_03/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+namespace ch01_03;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 1;
+        Console.Write("请从键盘输入n=");
+        int n = int.Parse(ReadLine());
+        // 用for循环计算 n!
+        for (int i = 1; i < n + 1; i++)
+        {
+            for (int j = i; j > 0; j--)
+            {
+                sum = sum * j;
+            }
+            WriteLine(i + "!=" + sum);
+            sum = 1;
+        }
+        ReadLine();
+    }
+}
